6c021562884c254bf5384e725b7c84d1ecc445b3
andre <andre@nowhere>
1999-11-29 09:34:34 +0000

    *** empty log message ***

--- Utilities/ipPic/ipFRead.c
+++ Utilities/ipPic/ipFRead.c
@@ -7,7 +7,10 @@
  *   and swaps to the other endianess
  *
  * $Log$
- * Revision 1.4  1999/11/28 16:27:20  andre
+ * Revision 1.5  1999/11/29 09:34:34  andre
+ * *** empty log message ***
+ *
+ * Revision 1.4  1999/11/28  16:27:20  andre
  * *** empty log message ***
  *
  * Revision 1.3  1999/11/27  19:15:08  andre
@@ -48,6 +51,12 @@ ipFReadCvt( void *ptr, size_t size, size_t nitems, FILE *stream )
 
   return( bytes_return );
 }
+
+#ifdef USE_ZLIB
+ipBool_t _ipPicCanReadCompressed = ipTrue;
+#else
+ipBool_t _ipPicCanReadCompressed = ipFalse;
+#endif
 
 size_t
 ipPicFReadCvt( void *ptr, size_t size, size_t nitems, ipPicFile_t stream )

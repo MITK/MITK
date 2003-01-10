51f919802392b84288093309dd312ddbafedc7ed
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-01-10 10:27:20 +0000

    no message

--- Core/DataStructures/mitkCommon.h
+++ Core/DataStructures/mitkCommon.h
@@ -5,11 +5,10 @@
 #include "itkProcessObject.h"
 #include "ipPic.h"
 #include "vtkImageData.h"
-#include <ar/ar.h>
-#include <ar/ar.h>
+#include "DataTreeNode.h"
 #include <Tree/LinkedTree.h>
 
-typedef	Tree<mitk::DataTreeNode::Pointer> DataTree;
-typedef	TreeIterator<mitk::DataTreeNode::Pointer> DataTreeIterator;
+//typedef	Tree<mitk::DataTreeNode::Pointer> DataTree;
+//typedef	TreeIterator<mitk::DataTreeNode::Pointer> DataTreeIterator;
 
 
--- Core/DataStructures/mitkDataTreeNode.h
+++ Core/DataStructures/mitkDataTreeNode.h
@@ -2,15 +2,27 @@
 #define DATATREENODE_H_HEADER_INCLUDED_C1E14338
 
 #include "mitkCommon.h"
+#include "ImageSource.h"
 #include "Mapper.h"
 
 namespace mitk {
 
 //##ModelId=3E031E2C0143
-class DataTreeNode
+class DataTreeNode : public ImageSource
 {
   public:
-    //##ModelId=3D6A0E8C02CC
+	/** Standard class typedefs. */
+	typedef DataTreeNode       Self;
+	typedef ImageSource        Superclass;
+	typedef itk::SmartPointer<Self>  Pointer;
+	typedef itk::SmartPointer<const Self>  ConstPointer;
+	  
+	/** Run-time type information (and related methods). */
+	itkTypeMacro(DataTreeNode,ImageSource);
+
+    itkNewMacro(Self);  
+
+	//##ModelId=3D6A0E8C02CC
     mitk::Mapper* GetMapper();
 
   protected:

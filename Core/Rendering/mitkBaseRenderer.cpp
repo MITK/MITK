8a3016c44fe9343a7c9b0767e96df43e189e06c0
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2003-01-24 15:13:51 +0000

    iterate over TreeNodes

--- Core/Rendering/mitkBaseRenderer.cpp
+++ Core/Rendering/mitkBaseRenderer.cpp
@@ -1,7 +1,14 @@
 #include "BaseRenderer.h"
+#include "Geometry3D.h"
 
 //##ModelId=3D6A1791038B
 void mitk::BaseRenderer::setData(mitk::DataTreeIterator* iterator)
 {
+  while(iterator->hasNext()) {
+    LevelWindow lw;
+    unsigned int dummy[] = {10,10,10};
+    Geometry3D geometry(3,dummy);
+    iterator->next()->GetMapper(defaultMapper)->Paint(geometry,0,0,lw);
+  } 
 }
 
--- Core/Rendering/mitkBaseRenderer.h
+++ Core/Rendering/mitkBaseRenderer.h
@@ -9,13 +9,14 @@ namespace mitk {
 //##ModelId=3C6E9AA90306
 class BaseRenderer
 {
+  
   public:
     //##ModelId=3D6A1791038B
-	  void setData(mitk::DataTreeIterator* iterator);
-
+       void setData(mitk::DataTreeIterator* iterator);
+  static const MapperSlotId defaultMapper = 1;
   private:
     //##ModelId=3D6A17780230
-    int mapperID;
+    MapperSlotId mapperID;
 
 };
 

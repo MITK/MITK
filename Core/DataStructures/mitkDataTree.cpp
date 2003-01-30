f11a46130e24d468655cf05e96fb8fe0524e6bdf
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-01-30 09:54:01 +0000

    DataTree is now a class, not just a typedef.

---
+++ Core/DataStructures/mitkDataTree.cpp
@@ -0,0 +1,13 @@
+#include "DataTree.h"
+
+//##ModelId=3E38F46A0190
+mitk::DataTree::DataTree()
+{
+}
+
+
+//##ModelId=3E38F46A01AE
+mitk::DataTree::~DataTree()
+{
+}
+
--- Core/DataStructures/mitkDataTree.h
+++ Core/DataStructures/mitkDataTree.h
@@ -1,10 +1,42 @@
+#ifndef DATATREE_H_HEADER_INCLUDED_C1C7797C
+#define DATATREE_H_HEADER_INCLUDED_C1C7797C
+
+#include "mitkCommon.h"
+
 #include "DataTreeNode.h"
 
 #include <Tree/LinkedTree.h>
 
 namespace mitk {
 
-typedef	::LinkedTree<DataTreeNode::Pointer> DataTree;
+//##ModelId=3E38F35101A0
+class DataTree : public itk::Object, public ::LinkedTree<DataTreeNode::Pointer>
+{
+   public:
+	/** Standard class typedefs. */
+	typedef DataTree                 Self;
+	typedef itk::Object              Superclass;
+	typedef itk::SmartPointer<Self>  Pointer;
+	typedef itk::SmartPointer<const Self>  ConstPointer;
+
+	/** Method for creation through the object factory. */
+	itkNewMacro(Self);
+
+	/** Run-time type information (and related methods). */
+	itkTypeMacro(DataTree,itk::Object);
+ protected:
+    //##ModelId=3E38F46A0190
+    DataTree();
+
+    //##ModelId=3E38F46A01AE
+    virtual ~DataTree();
+
+};
+
 typedef	::TreeIterator<DataTreeNode::Pointer> DataTreeIterator;
 
-}
+} // namespace mitk
+
+
+
+#endif /* DATATREE_H_HEADER_INCLUDED_C1C7797C */

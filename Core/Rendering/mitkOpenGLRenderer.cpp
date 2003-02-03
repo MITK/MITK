136fb1ada5506643028e58c7ddfa948f736a0dd0
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-02-03 09:15:41 +0000

    former VtkRenderer.

---
+++ Core/Rendering/mitkOpenGLRenderer.cpp
@@ -0,0 +1,217 @@
+#include "mitkOpenGLRenderer.h"
+#include "Mapper.h"
+#include "ImageMapper2D.h"
+#include "BaseVtkMapper2D.h"
+#include "BaseVtkMapper3D.h"
+#include "LevelWindow.h"
+
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkLight.h>
+
+//##ModelId=3E33ECF301AD
+mitk::OpenGLRenderer::OpenGLRenderer()
+{
+
+}
+
+//##ModelId=3E3D28AB0018
+void mitk::OpenGLRenderer::SetData(mitk::DataTreeIterator* iterator)
+{
+    mitk::BaseRenderer::SetData(iterator);
+
+    m_VtkRenderWindow->RemoveRenderer(m_VtkRenderer);
+    m_VtkRenderer->Delete();
+
+    m_VtkRenderer = vtkRenderer::New();
+    m_VtkRenderWindow->AddRenderer( this->m_VtkRenderer );
+
+    m_Light->Delete();
+    m_Light = vtkLight::New();
+    m_VtkRenderer->AddLight( m_Light );
+
+    bool first=true; //FIXME provisorium
+//    try
+    mitk::DataTreeIterator* it=m_DataTreeIterator->clone();
+    {
+        while(it->hasNext())
+        {
+            if(first)
+            {
+                BaseData::Pointer data=it->get()->GetData();
+                if(data!=NULL)
+                {
+                    Image::Pointer image = dynamic_cast<Image*>(data.GetPointer());
+                    if(image!=NULL)
+                    {
+                        m_ScreenGeometry->SetWorldGeometry(image->GetGeometry2D(0, 0));
+                        first=false;
+                    }
+                }
+            }
+//            mitk::LevelWindow lw;
+            unsigned int dummy[] = {10,10,10};
+            //Geometry3D geometry(3,dummy);
+            mitk::Mapper::Pointer mapper = it->next()->GetMapper(defaultMapper);
+            if(mapper!=NULL)
+            {
+//                mapper->Update();
+                BaseVtkMapper2D* anVtkMapper2D;
+                anVtkMapper2D=dynamic_cast<BaseVtkMapper2D*>(mapper.GetPointer());
+                if(anVtkMapper2D!=NULL)
+                {
+                    anVtkMapper2D->Update();
+                    m_VtkRenderer->AddProp(anVtkMapper2D->GetProp());
+                }
+                else
+                {
+                    BaseVtkMapper3D* anVtkMapper3D;
+                    anVtkMapper3D=dynamic_cast<BaseVtkMapper3D*>(mapper.GetPointer());
+                    if(anVtkMapper3D!=NULL)
+                    {
+                        anVtkMapper3D->Update();
+                        m_VtkRenderer->AddProp(anVtkMapper3D->GetProp());
+                    }
+                }
+            }
+        }
+    }
+//    catch( ::itk::ExceptionObject ee)
+//    {
+//        printf("%s\n",ee.what());
+////        itkGenericOutputMacro(ee->what());
+//    }
+//    catch( ...)
+//    {
+//            printf("test\n");
+//    }
+    delete it;
+}
+
+//##ModelId=3E330D260255
+void mitk::OpenGLRenderer::Update()
+{
+    mitk::DataTreeIterator* it=m_DataTreeIterator->clone();
+    while(it->hasNext())
+    {
+  //      mitk::LevelWindow lw;
+        unsigned int dummy[] = {10,10,10};
+        //Geometry3D geometry(3,dummy);
+        mitk::Mapper::Pointer mapper = it->next()->GetMapper(defaultMapper);
+        if(mapper!=NULL)
+        {
+            Mapper2D* mapper2d=dynamic_cast<Mapper2D*>(mapper.GetPointer());
+            if(mapper2d!=NULL)
+            {
+                mapper2d->SetScreenGeometry(m_ScreenGeometry);
+                mapper2d->Update();
+            }
+            else
+            if(dynamic_cast<BaseVtkMapper3D*>(mapper.GetPointer()))
+                mapper->Update();
+        }
+    }
+
+    delete it;
+}
+//##ModelId=3E330D2903CC
+void mitk::OpenGLRenderer::Render()
+{
+    mitk::DataTreeIterator* it=m_DataTreeIterator->clone();
+    while(it->hasNext())
+    {
+        mitk::Mapper::Pointer mapper = it->next()->GetMapper(defaultMapper);
+        if(mapper!=NULL)
+        {
+            ImageMapper2D* imagemapper2d=dynamic_cast<ImageMapper2D*>(mapper.GetPointer());
+            if(imagemapper2d!=NULL)
+                imagemapper2d->Paint(m_RenderWindow);
+        }
+    }
+
+    delete it;
+//    m_VtkRenderWindow->Render();
+}
+
+/*!
+\brief Initialize the OpenGLRenderer
+
+This method is called from the two Constructors
+*/
+void mitk::OpenGLRenderer::InitRenderer(mitk::RenderWindow* renderwindow) 
+{
+    BaseRenderer::InitRenderer(renderwindow);
+
+    m_InitNeeded = true;
+    m_ResizeNeeded = true;
+    m_VtkRenderWindow = vtkRenderWindow::New();
+    m_VtkRenderer = vtkRenderer::New();
+    m_VtkRenderWindow->AddRenderer( m_VtkRenderer );
+    m_Light = vtkLight::New();
+    m_VtkRenderer->AddLight( m_Light );
+	//we should disable vtk doublebuffering, but then it doesn't work
+    //m_VtkRenderWindow->SwapBuffersOff();
+}
+
+/*!
+\brief Destructs the OpenGLRenderer.
+*/
+//##ModelId=3E33ECF301B7
+mitk::OpenGLRenderer::~OpenGLRenderer() {
+    m_VtkRenderer->Delete();
+    m_VtkRenderWindow->Delete();
+}
+
+/*!
+\brief Initialize the OpenGL Window
+*/
+//##ModelId=3E33145B0096
+void mitk::OpenGLRenderer::Initialize( ) {
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glColor3f(1.0, 0.0, 0.0);
+}
+
+/*!
+\brief Resize the OpenGL Window
+*/
+//##ModelId=3E33145B00D2
+void mitk::OpenGLRenderer::Resize( int w, int h) {
+//    glViewport (0, 0, w, h);
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    gluOrtho2D( 0.0, w, 0.0, h );
+    glMatrixMode( GL_MODELVIEW );
+
+    m_ScreenGeometry->SetSizeInScreenUnits(w, h);
+//    m_ScreenGeometry->Fit();
+    Update();
+//    m_VtkRenderWindow->SetSize(w,h); //FIXME?
+}
+
+/*!
+\brief Render the scene
+*/
+//##ModelId=3E33145B005A
+void mitk::OpenGLRenderer::Paint( )
+{
+    Render();
+	glFlush();
+}
+
+
+//##ModelId=3E3314B0005C
+void mitk::OpenGLRenderer::SetWindowId(void * id)
+{
+    m_VtkRenderWindow->SetWindowId( id );
+}
+
+
+//##ModelId=3E3799420227
+void mitk::OpenGLRenderer::InitSize(int w, int h)
+{
+    m_VtkRenderWindow->SetSize(w,h);
+    m_ScreenGeometry->Fit();
+    Modified();
+    Update();
+}
+
---
+++ Core/Rendering/mitkOpenGLRenderer.h
@@ -0,0 +1,90 @@
+#ifndef MITKOPENGLRENDERER_H_HEADER_INCLUDED_C1C29F6D
+#define MITKOPENGLRENDERER_H_HEADER_INCLUDED_C1C29F6D
+
+#include "mitkCommon.h"
+#include "BaseRenderer.h"
+#include "DataTree.h"
+#include "ImageSliceSelector.h"
+
+class vtkRenderer;
+class vtkRenderWindow;
+class vtkLight;
+
+namespace mitk {
+
+//##ModelId=3C6E9AD90215
+class OpenGLRenderer : public BaseRenderer
+{
+  public:
+    /** Standard class typedefs. */
+    //##ModelId=3E33ECF2026A
+    typedef OpenGLRenderer              Self;
+    //##ModelId=3E33ECF20275
+    typedef BaseRenderer             Superclass;
+    //##ModelId=3E33ECF20288
+    typedef itk::SmartPointer<Self>  Pointer;
+    //##ModelId=3E33ECF20292
+    typedef itk::SmartPointer<const Self>  ConstPointer;
+
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+
+    /** Run-time type information (and related methods). */
+    itkTypeMacro(BaseRenderer,OpenGLRenderer);
+
+    //##ModelId=3E3D28AB0018
+    void SetData(mitk::DataTreeIterator* iterator);
+
+    //##ModelId=3E330D260255
+    virtual void Update();
+
+    //##ModelId=3E330D2903CC
+    virtual void Render();
+
+    //##ModelId=3E3314B0005C
+    virtual void SetWindowId(void *id);
+
+    //##ModelId=3E3799420227
+    virtual void InitSize(int w, int h);
+
+  protected:
+    //##ModelId=3E33145B005A
+    virtual void Paint();
+
+    //##ModelId=3E33145B0096
+    virtual void Initialize();
+
+    //##ModelId=3E33145B00D2
+    virtual void Resize(int w, int h);
+
+    //##ModelId=3E33145B0226
+    virtual void InitRenderer(mitk::RenderWindow* renderwindow);
+
+    //##ModelId=3E33ECF301AD
+    OpenGLRenderer();
+
+    //##ModelId=3E33ECF301B7
+    virtual ~OpenGLRenderer();
+
+  private:
+    //##ModelId=3E33145A0315
+    bool m_InitNeeded;
+
+    //##ModelId=3E33145A0347
+    bool m_ResizeNeeded;
+
+    //##ModelId=3E33145A0383
+    vtkRenderer* m_VtkRenderer;
+
+    //##ModelId=3E33145A03BF
+    vtkRenderWindow* m_VtkRenderWindow;
+
+    //##ModelId=3E33145B001E
+    vtkLight* m_Light;
+};
+
+} // namespace mitk
+
+
+
+#endif /* MITKOPENGLRENDERER_H_HEADER_INCLUDED_C1C29F6D */

094f7c68fed2495d1be2ca1eab2d5937be854fba
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-02-03 09:34:51 +0000

    header defining (or typdef-ing) vector classes.

---
+++ Core/DataStructures/mitkVector.h
@@ -0,0 +1,19 @@
+#ifndef MITKVECTOR_H_HEADER_INCLUDED_C1EBD0AD
+#define MITKVECTOR_H_HEADER_INCLUDED_C1EBD0AD
+
+#include "SpaceGeometry.h"
+
+namespace mitk {
+
+typedef View3f   PlaneView;
+typedef Vector3f Vector3D;
+typedef Point3f  Point3D;
+typedef Vector2f Vector2D;
+typedef Point2f  Point2D;
+typedef Vector4f  Vector4D;
+typedef Matrix4f  Matrix4D;
+
+} // namespace mitk
+
+
+#endif /* MITKVECTOR_H_HEADER_INCLUDED_C1EBD0AD */

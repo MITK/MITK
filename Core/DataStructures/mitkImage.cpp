5345e0a48ee9314d03c094361c12b134baf9b406
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-02-03 09:16:18 +0000

    former Image.

---
+++ Core/DataStructures/mitkImage.cpp
@@ -0,0 +1,638 @@
+#include "mitkImage.h"
+
+//##ModelId=3DCBC2B50345
+const mitk::PixelType& mitk::Image::GetType(int n) const
+{
+	return m_PixelType;
+}
+
+//##ModelId=3DCBC5AA0112
+unsigned int mitk::Image::GetDimension() const
+{
+	return m_Dimension;
+}
+
+//##ModelId=3DCBC6040068
+unsigned int mitk::Image::GetDimension(int i) const
+{
+	if((i>=0) && (i<m_Dimension))
+		return m_Dimensions[i];
+	return -1;
+}
+
+//##ModelId=3DCBE46F026D
+mitk::DICOM *mitk::Image::GetDicom() const
+{
+	itkExceptionMacro("DICOM not yet supported."); 
+	return NULL;
+}
+
+//##ModelId=3E0B494802D6
+void* mitk::Image::GetData()
+{
+	if(m_Initialized==false)
+	{
+		if(GetSource()==NULL)
+			return NULL;
+		GetSource()->UpdateOutputInformation();
+	}
+	m_CompleteData=GetChannelData();
+	return m_CompleteData->GetData();
+}
+
+//##ModelId=3DCBEF2902C6
+vtkImageData* mitk::Image::GetVtkImageData()
+{
+	if(m_Initialized==false)
+	{
+		if(GetSource()==NULL)
+			return NULL;
+		GetSource()->UpdateOutputInformation();
+	}
+	m_CompleteData=GetChannelData();
+	if(m_CompleteData==NULL) 
+		return NULL;
+	return m_CompleteData->GetVtkImageData();
+}
+
+//##ModelId=3DCBE2B802E4
+ipPicDescriptor* mitk::Image::GetPic()
+{
+	if(m_Initialized==false)
+	{
+		if(GetSource()==NULL)
+			return NULL;
+		GetSource()->UpdateOutputInformation();
+	}
+	m_CompleteData=GetChannelData();
+	if(m_CompleteData==NULL) 
+		return NULL;
+	return m_CompleteData->GetPicDescriptor();
+}
+
+//##ModelId=3E0B4A6A01EC
+mitk::ImageDataItem::Pointer mitk::Image::GetSliceData(int s, int t, int n)
+{
+	if(IsValidSlice(s,t,n)==false) return NULL;
+
+	// slice directly available?
+	int pos=GetSliceIndex(s,t,n);
+	if(m_Slices[pos]!=NULL)
+		return m_Slices[pos];
+
+	// is slice available as part of a volume that is available?
+	ImageDataItemPointer sl, ch, vol;
+	vol=m_Volumes[GetVolumeIndex(t,n)];
+	if((vol!=NULL) && (vol->IsComplete()))
+	{
+		sl=new ImageDataItem(*vol, 2, s*m_OffsetTable[2]*m_PixelType.GetBpe()/8);
+		sl->SetComplete(true);
+		return m_Slices[pos]=sl;
+	}
+
+	// is slice available as part of a channel that is available?
+	ch=m_Channels[n];
+	if((ch!=NULL) && (ch->IsComplete()))
+	{
+		sl=new ImageDataItem(*ch, 2, (s*m_OffsetTable[2]+t*m_OffsetTable[3])*m_PixelType.GetBpe()/8);
+		sl->SetComplete(true);
+		return m_Slices[pos]=sl;
+	}
+
+	// slice is unavailable. Can we calculate it?
+	if(GetSource()!=NULL)
+	{
+		// ... wir müssen rechnen!!! ....
+		m_RequestedRegion.SetIndex(0, 0);
+		m_RequestedRegion.SetIndex(1, 0);
+		m_RequestedRegion.SetIndex(2, s);
+		m_RequestedRegion.SetIndex(3, t);
+		m_RequestedRegion.SetIndex(4, n);
+		m_RequestedRegion.SetSize(0, m_Dimensions[0]);
+		m_RequestedRegion.SetSize(1, m_Dimensions[1]);
+		m_RequestedRegion.SetSize(2, 1);
+		m_RequestedRegion.SetSize(3, 1);
+		m_RequestedRegion.SetSize(4, 1);
+		m_RequestedRegionInitialized=true;
+		GetSource()->Update();
+		if(IsSliceSet(s,t,n))
+			//yes: now we can call ourselves without the risk of a endless loop (see if above)
+			return GetSliceData(s,t,n);
+		else
+			return NULL;
+	}
+	else
+	{
+		return AllocateSliceData(s,t,n);
+	}
+}
+
+//##ModelId=3E0B4A82001A
+mitk::ImageDataItem::Pointer mitk::Image::GetVolumeData(int t, int n)
+{
+	if(IsValidVolume(t,n)==false) return NULL;
+
+	ImageDataItemPointer ch, vol;
+
+	// volume directly available?
+	int pos=GetVolumeIndex(t,n);
+	vol=m_Volumes[pos];
+	if((vol!=NULL) && (vol->IsComplete()))
+		return vol;
+
+	// is volume available as part of a channel that is available?
+	ch=m_Channels[n];
+	if((ch!=NULL) && (ch->IsComplete()))
+	{
+		vol=new ImageDataItem(*ch, 3, (t*m_OffsetTable[3])*m_PixelType.GetBpe()/8);
+		return m_Volumes[pos]=vol;
+	}
+
+	// let's see if all slices of the volume are set, so that we can (could) combine them to a volume
+	bool complete=true;
+	unsigned int s;
+	for(s=0;s<m_Dimensions[2];++s)
+		if(m_Slices[GetSliceIndex(s,t,n)]==NULL)
+		{
+			complete=false;
+			break;
+		}
+	if(complete)
+	{
+		// ok, let's combine the slices!
+		vol=new ImageDataItem(m_PixelType, 3, m_Dimensions);
+		vol->SetComplete(true);
+		int size=m_OffsetTable[3]*m_PixelType.GetBpe()/8;
+		for(s=0;s<m_Dimensions[2];++s)
+		{
+			int posSl;
+			ImageDataItemPointer sl;
+			posSl=GetSliceIndex(s,t,n);
+
+			sl=m_Slices[posSl];
+			sl=new ImageDataItem(*vol, 2, s*m_OffsetTable[2]*m_PixelType.GetBpe()/8);
+			sl->SetComplete(true);
+			memcpy(static_cast<char*>(vol->GetData())+sl->GetOffset(), sl->GetData(), size);
+			m_Slices[posSl]=sl;
+		}
+		return m_Volumes[pos]=vol;
+	}
+
+	// volume is unavailable. Can we calculate it?
+	if(GetSource()!=NULL)
+	{
+		// ... wir müssen rechnen!!! ....
+		m_RequestedRegion.SetIndex(0, 0);
+		m_RequestedRegion.SetIndex(1, 0);
+		m_RequestedRegion.SetIndex(2, 0);
+		m_RequestedRegion.SetIndex(3, t);
+		m_RequestedRegion.SetIndex(4, n);
+		m_RequestedRegion.SetSize(0, m_Dimensions[0]);
+		m_RequestedRegion.SetSize(1, m_Dimensions[1]);
+		m_RequestedRegion.SetSize(2, m_Dimensions[2]);
+		m_RequestedRegion.SetSize(3, m_Dimensions[3]);
+		m_RequestedRegion.SetSize(4, 1);
+		m_RequestedRegionInitialized=true;
+		GetSource()->Update();
+		if(IsVolumeSet(t,n))
+			//yes: now we can call ourselves without the risk of a endless loop (see if above)
+			return GetVolumeData(t,n);
+		else
+			return NULL;
+	}
+	else
+	{
+		return AllocateVolumeData(t,n);
+	}
+}
+
+//##ModelId=3E0B4A9100BC
+mitk::ImageDataItem::Pointer mitk::Image::GetChannelData(int n)
+{
+	if(IsValidChannel(n)==false) return NULL;
+	ImageDataItemPointer ch, vol;
+	ch=m_Channels[n];
+	if((ch!=NULL) && (ch->IsComplete()))
+		return ch;
+
+	// let's see if all volumes are set, so that we can (could) combine them to a channel
+	if(IsChannelSet(n))
+	{
+		// if there is only one time frame we do not need to combine anything
+		if(m_Dimensions[3]<=1)
+		{
+			vol=GetVolumeData(0,n);
+			ch=new ImageDataItem(*vol, 3);
+			ch->SetComplete(true);
+		}
+		else
+		{
+			// ok, let's combine the volumes!
+			ch=new ImageDataItem(m_PixelType, m_Dimension, m_Dimensions);
+			ch->SetComplete(true);
+			int size=m_OffsetTable[m_Dimension]*m_PixelType.GetBpe()/8;
+			unsigned int t;
+			for(t=0;t<m_Dimensions[3];++t)
+			{
+				int posVol;
+				ImageDataItemPointer vol;
+
+				posVol=GetVolumeIndex(t,n);
+				vol=GetVolumeData(t,n);
+
+				// copy data of volume in channel
+				memcpy(static_cast<char*>(ch->GetData())+vol->GetOffset(), vol->GetData(), size);
+
+				// replace old volume with reference to channel
+				vol=new ImageDataItem(*vol, 3, t*m_OffsetTable[3]*m_PixelType.GetBpe()/8);
+				m_Volumes[posVol]=vol;
+			}
+		}
+		return m_Channels[n]=ch;
+	}
+	
+	// channel is unavailable. Can we calculate it?
+	if(GetSource()!=NULL)
+	{
+		// ... wir müssen rechnen!!! ....
+		m_RequestedRegion.SetIndex(0, 0);
+		m_RequestedRegion.SetIndex(1, 0);
+		m_RequestedRegion.SetIndex(2, 0);
+		m_RequestedRegion.SetIndex(3, 0);
+		m_RequestedRegion.SetIndex(4, n);
+		m_RequestedRegion.SetSize(0, m_Dimensions[0]);
+		m_RequestedRegion.SetSize(1, m_Dimensions[1]);
+		m_RequestedRegion.SetSize(2, m_Dimensions[2]);
+		m_RequestedRegion.SetSize(3, m_Dimensions[3]);
+		m_RequestedRegion.SetSize(4, m_ChannelNumber);
+		m_RequestedRegionInitialized=true;
+		GetSource()->Update();
+		// did it work?
+		if(IsChannelSet(n))
+			//yes: now we can call ourselves without the risk of a endless loop (see if above)
+			return GetChannelData(n);
+		else
+			return NULL;
+	}
+	else
+	{
+		return AllocateChannelData(n);
+	}
+}
+
+//##ModelId=3E1012990305
+bool mitk::Image::IsSliceSet(int s, int t, int n) const
+{
+	if(IsValidSlice(s,t,n)==false) return false;
+
+	if(m_Slices[GetSliceIndex(s,t,n)]!=NULL)
+		return true;
+
+	ImageDataItemPointer ch, vol;
+	vol=m_Volumes[GetVolumeIndex(t,n)];
+	if((vol!=NULL) && (vol->IsComplete()))
+		return true;
+	ch=m_Channels[n];
+	if((ch!=NULL) && (ch->IsComplete()))
+		return true;
+	return false;
+}
+
+//##ModelId=3E10139001BF
+bool mitk::Image::IsVolumeSet(int t, int n) const
+{
+	if(IsValidVolume(t,n)==false) return false;
+	ImageDataItemPointer ch, vol;
+
+	// volume directly available?
+	vol=m_Volumes[GetVolumeIndex(t,n)];
+	if((vol!=NULL) && (vol->IsComplete()))
+		return true;
+
+	// is volume available as part of a channel that is available?
+	ch=m_Channels[n];
+	if((ch!=NULL) && (ch->IsComplete()))
+		return true;
+
+	// let's see if all slices of the volume are set, so that we can (could) combine them to a volume
+	unsigned int s;
+	for(s=0;s<m_Dimensions[2];++s)
+		if(m_Slices[GetSliceIndex(s,t,n)]==NULL)
+			return false;
+	return true;
+}
+
+//##ModelId=3E1550E700E2
+bool mitk::Image::IsChannelSet(int n) const
+{
+	if(IsValidChannel(n)==false) return false;
+	ImageDataItemPointer ch, vol;
+	ch=m_Channels[n];
+	if((ch!=NULL) && (ch->IsComplete()))
+		return true;
+	// let's see if all volumes are set, so that we can (could) combine them to a channel
+	unsigned int t;
+	for(t=0;t<m_Dimensions[3];++t)
+		if(IsVolumeSet(t,n)==false)
+			return false;
+	return true;
+}
+
+//##ModelId=3E10148003D7
+bool mitk::Image::SetSlice(void *data, int s, int t, int n)
+{
+	if(IsValidSlice(s,t,n)==false) return false;
+	ImageDataItemPointer sl;
+	if(IsSliceSet(s,t,n))
+	{
+		sl=GetSliceData(s,t,n);
+		memcpy(sl->GetData(), data, m_OffsetTable[2]*m_PixelType.GetBpe()/8);
+		//we have changed the data: call Modified()! 
+		Modified();
+	}
+	else
+	{
+		sl=AllocateSliceData(s,t,n);
+		if(sl==NULL) return false;
+		memcpy(sl->GetData(), data, m_OffsetTable[2]*m_PixelType.GetBpe()/8);		
+		//we just added a missing slice, which is not regarded as modification.
+		//Therefore, we do not call Modified()!
+	}
+	return true;
+}
+
+//##ModelId=3E1014A00211
+bool mitk::Image::SetVolume(void *data, int t, int n)
+{
+	if(IsValidVolume(t,n)==false) return false;
+	ImageDataItemPointer vol;
+	if(IsVolumeSet(t,n))
+	{
+		vol=GetVolumeData(t,n);
+		memcpy(vol->GetData(), data, m_OffsetTable[3]*m_PixelType.GetBpe()/8);
+		vol->SetComplete(true);
+		//we have changed the data: call Modified()! 
+		Modified();
+	}
+	else
+	{
+		vol=AllocateVolumeData(t,n);
+		if(vol==NULL) return false;
+		memcpy(vol->GetData(), data, m_OffsetTable[3]*m_PixelType.GetBpe()/8);
+		vol->SetComplete(true);
+		//we just added a missing Volume, which is not regarded as modification.
+		//Therefore, we do not call Modified()!
+	}
+	return true;
+}
+
+//##ModelId=3E1027F8023D
+bool mitk::Image::SetPicSlice(ipPicDescriptor *pic, int s, int t, int n)
+{
+	if(pic==NULL) return false;
+	if(pic->dim!=2) return false;
+	if((pic->n[0]!=m_Dimensions[0]) || (pic->n[1]!=m_Dimensions[1])) return false;
+	return SetSlice(pic->data,s,t,n); //FIXME: add geometry!
+}
+
+//##ModelId=3E102818024D
+bool mitk::Image::SetPicVolume(ipPicDescriptor *pic, int t, int n)
+{
+	if(pic==NULL) return false;
+	if(pic->dim!=3) return false;
+	if((pic->n[0]!=m_Dimensions[0]) || (pic->n[1]!=m_Dimensions[1]) || (pic->n[2]!=m_Dimensions[2])) return false;
+	return SetVolume(pic->data,t,n); //FIXME: check dim; add geometry!
+}
+
+//##ModelId=3E102AE9004B
+void mitk::Image::Initialize(const mitk::PixelType& type, unsigned int dimension, unsigned int *dimensions, unsigned int channels)
+{
+	m_Dimension=dimension;
+	if(m_Dimensions!=NULL)
+		delete m_Dimensions;
+	m_Dimensions=new unsigned int[m_Dimension>4?m_Dimension:4];
+	memcpy(m_Dimensions, dimensions, sizeof(unsigned int)*m_Dimension);
+	if(m_Dimension<4)
+	{
+		unsigned int i, *p;
+		for(i=0,p=m_Dimensions+m_Dimension;i<4-m_Dimension;++i, ++p)
+			*p=1;
+	}
+	m_ChannelNumber=channels;
+
+	m_PixelType=type;
+	m_Geometry3D=new Geometry3D(dimension, dimensions);
+
+	ImageDataItemPointer dnull=NULL;
+
+	m_Channels.reserve(m_ChannelNumber);
+	m_Channels.assign(m_ChannelNumber, dnull);
+
+	m_Volumes.reserve(m_ChannelNumber*m_Dimensions[3]);
+	m_Volumes.assign(m_ChannelNumber*m_Dimensions[3], dnull);
+
+	m_Slices.reserve(m_ChannelNumber*m_Dimensions[3]*m_Dimensions[2]);
+	m_Slices.assign(m_ChannelNumber*m_Dimensions[3]*m_Dimensions[2], dnull);
+
+	ComputeOffsetTable();
+
+    SetRequestedRegionToLargestPossibleRegion();
+
+	m_Initialized = true;
+}
+
+//##ModelId=3E102D2601DF
+void mitk::Image::Initialize(ipPicDescriptor* pic, int channels, int sDim, int tDim)
+{
+	if(pic==NULL) return;
+
+	m_Dimension=pic->dim;
+	if(m_Dimensions!=NULL)
+		delete m_Dimensions;
+	m_Dimensions=new unsigned int[m_Dimension>4?m_Dimension:4];
+	memcpy(m_Dimensions, pic->n, sizeof(unsigned int)*m_Dimension);
+	if(m_Dimension<4)
+	{
+		unsigned int i, *p;
+		for(i=0,p=m_Dimensions+m_Dimension;i<4-m_Dimension;++i, ++p)
+			*p=1;
+	}
+	m_ChannelNumber=channels;
+
+	if((m_Dimension>2) && (sDim>=0))
+		m_Dimensions[2]=sDim;
+	if((m_Dimension>3) && (tDim>=0))
+		m_Dimensions[3]=tDim;
+
+	m_PixelType=PixelType(pic);
+	m_Geometry3D=new Geometry3D(m_Dimension, m_Dimensions);
+
+    itkWarningMacro("Initialize() by pic assumes a plane, evenly spaced geometry starting at (0,0,0)."); 
+    m_Geometry3D->SetSpacing(pic);
+    m_Geometry3D->SetGeometry2D(pic, 0, 0);
+    m_Geometry3D->SetEvenlySpaced();
+
+	ImageDataItemPointer dnull=NULL;
+
+	m_Channels.reserve(m_ChannelNumber);
+	m_Channels.assign(m_ChannelNumber, dnull);
+
+	m_Volumes.reserve(m_ChannelNumber*m_Dimensions[3]);
+	m_Volumes.assign(m_ChannelNumber*m_Dimensions[3], dnull);
+
+	m_Slices.reserve(m_ChannelNumber*m_Dimensions[3]*m_Dimensions[2]);
+	m_Slices.assign(m_ChannelNumber*m_Dimensions[3]*m_Dimensions[2], dnull);
+
+	ComputeOffsetTable();
+
+    SetRequestedRegionToLargestPossibleRegion();
+
+	m_Initialized = true;
+}
+
+//##ModelId=3E155CF000F6
+bool mitk::Image::IsValidSlice(int s, int t, int n) const
+{
+	return ((s>=0) && (s<m_Dimensions[2]) && (t>=0) && (t<m_Dimensions[3]) && (n>=0) && (n<m_ChannelNumber));
+}
+
+//##ModelId=3E155D2501A7
+bool mitk::Image::IsValidVolume(int t, int n) const
+{
+	return IsValidSlice(0, t, n);
+}
+
+//##ModelId=3E157C53030B
+bool mitk::Image::IsValidChannel(int n) const
+{
+	return IsValidSlice(0, 0, n);
+}
+
+//##ModelId=3E19EA110396
+bool mitk::Image::IsInitialized() const
+{
+	return m_Initialized;
+}
+
+//##ModelId=3E155E7A0374
+void mitk::Image::ComputeOffsetTable()
+{
+	if(m_OffsetTable!=NULL)
+		delete m_OffsetTable;
+
+	m_OffsetTable=new unsigned int[m_Dimension>4 ? m_Dimension+1 : 4+1];
+	
+	unsigned int i;
+	int num=1;
+	m_OffsetTable[0] = 1;
+	for (i=0; i < m_Dimension; ++i)
+	{
+		num *= m_Dimensions[i];
+		m_OffsetTable[i+1] = num;
+	}
+	for (;i < 4; ++i)
+		m_OffsetTable[i+1] = num;
+}
+
+//##ModelId=3E155C940248
+int mitk::Image::GetSliceIndex(int s, int t, int n) const
+{
+	if(IsValidSlice(s,t,n)==false) return false;
+	return s+t*m_Dimensions[3]+n*m_ChannelNumber; //??
+}
+
+//##ModelId=3E155C76012D
+int mitk::Image::GetVolumeIndex(int t, int n) const
+{
+	if(IsValidVolume(t,n)==false) return false;
+	return t+n*m_ChannelNumber; //??
+}
+
+//##ModelId=3E1569310328
+mitk::ImageDataItem::Pointer mitk::Image::AllocateSliceData(int s, int t, int n)
+{
+	int pos;
+	pos=GetSliceIndex(s,t,n);
+
+	// is slice available as part of a volume that is available?
+	ImageDataItemPointer sl, ch, vol;
+	vol=m_Volumes[GetVolumeIndex(t,n)];
+	if((vol!=NULL) && (vol->IsComplete()))
+	{
+		sl=new ImageDataItem(*vol, 2, s*m_OffsetTable[2]*m_PixelType.GetBpe()/8);
+		sl->SetComplete(true);
+		return m_Slices[pos]=sl;
+	}
+
+	// is slice available as part of a channel that is available?
+	ch=m_Channels[n];
+	if((ch!=NULL) && (ch->IsComplete()))
+	{
+		sl=new ImageDataItem(*ch, 2, (s*m_OffsetTable[2]+t*m_OffsetTable[3])*m_PixelType.GetBpe()/8);
+		sl->SetComplete(true);
+		return m_Slices[pos]=sl;
+	}
+
+	// allocate new volume (instead of a single slice to keep data together!)
+	vol=AllocateVolumeData(t,n);
+	sl=new ImageDataItem(*vol, 2, s*m_OffsetTable[2]*m_PixelType.GetBpe()/8);
+	sl->SetComplete(true);
+	return m_Slices[pos]=sl;
+
+	////ALTERNATIVE:
+	//// allocate new slice
+	//sl=new ImageDataItem(m_PixelType, 2, m_Dimensions);
+	//m_Slices[pos]=sl;
+	//return vol;
+}
+
+//##ModelId=3E15694500EC
+mitk::ImageDataItem::Pointer mitk::Image::AllocateVolumeData(int t, int n)
+{
+	int pos;
+	pos=GetVolumeIndex(t,n);
+
+	// is volume available as part of a channel that is available?
+	ImageDataItemPointer ch, vol;
+	ch=m_Channels[n];
+	if(ch!=NULL)
+	{
+		vol=new ImageDataItem(*ch, 3, (t*m_OffsetTable[3])*m_PixelType.GetBpe()/8);
+		return m_Volumes[pos]=vol;
+	}
+
+	// allocate new volume
+	vol=new ImageDataItem(m_PixelType, 3, m_Dimensions);
+	m_Volumes[pos]=vol;
+	return vol;
+}
+
+//##ModelId=3E1569500322
+mitk::ImageDataItem::Pointer mitk::Image::AllocateChannelData(int n)
+{
+	// allocate new channel
+	ImageDataItemPointer ch=new ImageDataItem(m_PixelType, m_Dimension, m_Dimensions);
+	m_Channels[n]=ch;
+	return ch;
+}
+
+//##ModelId=3E15F6C60103
+mitk::Image::Image() : 
+	m_Dimension(0), m_Dimensions(NULL), m_OffsetTable(NULL),
+		m_CompleteData(NULL), m_PixelType(NULL), m_Initialized(false)
+{
+
+}
+
+//##ModelId=3E15F6CA014F
+mitk::Image::~Image()
+{
+
+}
+
+//##ModelId=3E1A11530384
+unsigned int* mitk::Image::GetDimensions() const
+{
+	return m_Dimensions;
+}
+
---
+++ Core/DataStructures/mitkImage.h
@@ -0,0 +1,193 @@
+#ifndef MITKIMAGE_H_HEADER_INCLUDED_C1C2FCD2
+#define MITKIMAGE_H_HEADER_INCLUDED_C1C2FCD2
+
+#include "mitkCommon.h"
+#include "SlicedData.h"
+#include "DICOM.h"
+#include "ImageDataItem.h"
+#include "PixelType.h"
+#include "BaseData.h"
+
+namespace mitk {
+
+class SubImageSelector;
+
+//##ModelId=3DCBC1E300FE
+//##Documentation
+//## Image class for storing images. Can be asked for header information, the
+//## data vector, ipPicDescriptors or vtkImageData objects. If not the complete
+//## data is required, the appropriate SubImageSelector class should be used
+//## for access.
+//## Image organizes sets of slices (s x 2D), volumes (t x 3D) and channels (n
+//## x ND). Channels are for different kind of data (with the same Geometry!),
+//## e.g., morphology in channel 0, velocities in channel 1.
+class Image : public SlicedData
+{
+    friend class SubImageSelector;
+
+  public:
+	/** Standard class typedefs. */
+    //##ModelId=3E0B6CF700ED
+	typedef Image       Self;
+    //##ModelId=3E0B6CF7015B
+	typedef BaseData	Superclass;
+    //##ModelId=3E0B6CF701AB
+	typedef itk::SmartPointer<Self>  Pointer;
+    //##ModelId=3E0B6CF70206
+	typedef itk::SmartPointer<const Self>  ConstPointer;
+	  
+	/** Run-time type information (and related methods). */
+	itkTypeMacro(Image,SlicedData);
+
+    //##ModelId=3E18748C0045
+    itkNewMacro(Self);  
+
+	/** Smart Pointer type to a ImageDataItem. */
+    //##ModelId=3E0B7B3C00F0
+	typedef ImageDataItem::Pointer ImageDataItemPointer;
+
+	/** STL Array of SmartPointers to ImageDataItems */
+    //##ModelId=3E0B7B3C0172
+	typedef std::vector<ImageDataItemPointer> ImageDataItemPointerArray;
+
+  public:
+
+	//##ModelId=3DCBC2B50345
+    const mitk::PixelType& GetType(int n = 0) const;
+
+    //##ModelId=3DCBC5AA0112
+    unsigned int GetDimension() const;
+
+    //##ModelId=3DCBC6040068
+    unsigned int GetDimension(int i) const;
+
+    //##ModelId=3DCBE46F026D
+    virtual mitk::DICOM *GetDicom() const;
+
+
+    //##ModelId=3E0B494802D6
+    virtual void* GetData();
+
+    //##ModelId=3DCBEF2902C6
+    virtual vtkImageData* GetVtkImageData();
+
+    //##ModelId=3DCBE2B802E4
+    virtual ipPicDescriptor* GetPic();
+
+	//##ModelId=3E1012990305
+    virtual bool IsSliceSet(int s = 0, int t = 0, int n = 0) const;
+
+    //##ModelId=3E10139001BF
+    virtual bool IsVolumeSet(int t = 0, int n = 0) const;
+
+    //##ModelId=3E1550E700E2
+    virtual bool IsChannelSet(int n = 0) const;
+  
+    //##ModelId=3E10148003D7
+    virtual bool SetSlice(void *data, int s = 0, int t = 0, int n = 0);
+
+    //##ModelId=3E1014A00211
+    virtual bool SetVolume(void *data, int t = 0, int n = 0);
+
+    //##ModelId=3E1027F8023D
+    //##Documentation
+    //## set @a pic as slice @a s at time @a t in channel @a n. The
+    //## corresponding @ Geomety2D is updated according to the information
+    //## provided in the tags of @a pic.
+    //## @return @a false : dimensions and/or data-type of @a pic does not
+    //## comply with image 
+    //## @a true success
+    virtual bool SetPicSlice(ipPicDescriptor *pic, int s = 0, int t = 0, int n = 0);
+
+
+    //##ModelId=3E102818024D
+    //##Documentation
+    //## set @a pic as volume at time @a t in channel @a n. The corresponding
+    //## @a Geomety3D and depending @a Geometry2D entries are updated according
+    //## to the information provided in the tags of @a pic.
+    //## @return @a false : dimensions and/or data-type of @a pic does not
+    //## comply with image 
+    //## @a true success
+    virtual bool SetPicVolume(ipPicDescriptor *pic, int t = 0, int n = 0);
+    //##ModelId=3E102AE9004B
+    //##Documentation
+    //## initialize new (or re-initialize) image
+    virtual void Initialize(const mitk::PixelType& type, unsigned int dimension, unsigned int *dimensions, unsigned int channels = 1);
+
+
+    //##ModelId=3E102D2601DF
+    //##Documentation
+    //## initialize new (or re-initialize) image by @a pic. Dimensions and @a
+    //## Geometry3D /@a Geometry2D  are set according to the tags in @a pic.
+    //## @param tDim override time dimension (@a n[3]) in @a pic (if >0 and <)
+    virtual void Initialize(ipPicDescriptor* pic, int channels = 1, int tDim = -1, int sDim = -1);
+    //##ModelId=3E155CF000F6
+    virtual bool IsValidSlice(int s = 0, int t = 0, int n = 0) const;
+
+
+    //##ModelId=3E155D2501A7
+    virtual bool IsValidVolume(int t = 0, int n = 0) const;
+    //##ModelId=3E157C53030B
+    virtual bool IsValidChannel(int n = 0) const;
+
+    //##ModelId=3E19EA110396
+	virtual bool IsInitialized() const;
+    //##ModelId=3E1A11530384
+    unsigned int* GetDimensions() const;
+
+
+  protected:
+    //##ModelId=3E0B4A6A01EC
+    virtual mitk::ImageDataItem::Pointer GetSliceData(int s = 0, int t = 0, int n = 0);
+    //##ModelId=3E0B4A82001A
+    virtual mitk::ImageDataItem::Pointer GetVolumeData(int t = 0, int n = 0);
+    //##ModelId=3E0B4A9100BC
+    virtual mitk::ImageDataItem::Pointer GetChannelData(int n = 0);
+    //##ModelId=3E155C940248
+    int GetSliceIndex(int s = 0, int t = 0, int n = 0) const;
+
+
+    //##ModelId=3E155C76012D
+    int GetVolumeIndex(int t = 0, int n = 0) const;
+    //##ModelId=3E155E7A0374
+    void ComputeOffsetTable();
+    //##ModelId=3E1569310328
+    virtual mitk::ImageDataItem::Pointer AllocateSliceData(int s = 0, int t = 0, int n = 0);
+
+
+    //##ModelId=3E15694500EC
+    virtual mitk::ImageDataItem::Pointer AllocateVolumeData(int t = 0, int n = 0);
+
+    //##ModelId=3E1569500322
+    //##ModelId=3E1569500322
+    virtual mitk::ImageDataItem::Pointer AllocateChannelData(int n = 0);
+    //##ModelId=3E15F6C60103
+    Image();
+    //##ModelId=3E15F6CA014F
+    ~Image();
+
+    //##ModelId=3E0B7B3C0245
+	mutable ImageDataItemPointerArray m_Channels;
+    //##ModelId=3E0B7B3C0263
+	mutable ImageDataItemPointerArray m_Volumes;
+    //##ModelId=3E0B4C1C02C3
+	mutable ImageDataItemPointerArray m_Slices;
+
+    //##ModelId=3E156293019B
+	unsigned int m_Dimension;
+    //##ModelId=3E0B890702C4
+	unsigned int *m_Dimensions;
+    //##ModelId=3E0B8907036E
+	unsigned int *m_OffsetTable;
+    //##ModelId=3E10151F001F
+    ImageDataItem::Pointer m_CompleteData;
+    //##ModelId=3E13FE96005F
+	PixelType m_PixelType;
+
+    //##ModelId=3E19EA110292
+	bool m_Initialized;
+};
+
+} // namespace mitk
+
+#endif /* MITKIMAGE_H_HEADER_INCLUDED_C1C2FCD2 */

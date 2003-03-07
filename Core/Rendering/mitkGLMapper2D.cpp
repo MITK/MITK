e3e0a7bd0b351570832e23faf91dfac4c564c42d
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-03-07 04:17:32 +0000

    abstract base class for 2D-GL-mapper (mapper having a Paint method).

---
+++ Core/Rendering/mitkGLMapper2D.cpp
@@ -0,0 +1,18 @@
+#include "mitkGLMapper2D.h"
+
+//##ModelId=3E681470037E
+mitk::GLMapper2D::GLMapper2D()
+{
+}
+
+
+//##ModelId=3E681470039C
+mitk::GLMapper2D::~GLMapper2D()
+{
+}
+
+//##ModelId=3E67E9390346
+void mitk::GLMapper2D::Paint(mitk::BaseRenderer *renderer)
+{
+}
+
---
+++ Core/Rendering/mitkGLMapper2D.h
@@ -0,0 +1,30 @@
+#ifndef MITKGLMAPPER2D_H_HEADER_INCLUDED_C197C872
+#define MITKGLMAPPER2D_H_HEADER_INCLUDED_C197C872
+
+#include "mitkCommon.h"
+#include "Mapper2D.h"
+#include "BaseRenderer.h"
+
+namespace mitk {
+
+//##ModelId=3E6814230160
+class GLMapper2D : public Mapper2D
+{
+  public:
+
+
+    //##ModelId=3E67E9390346
+    virtual void Paint(mitk::BaseRenderer *renderer) = 0;
+  protected:
+    //##ModelId=3E681470037E
+    GLMapper2D();
+    //##ModelId=3E681470039C
+    virtual ~GLMapper2D();
+
+};
+
+} // namespace mitk
+
+
+
+#endif /* MITKGLMAPPER2D_H_HEADER_INCLUDED_C197C872 */

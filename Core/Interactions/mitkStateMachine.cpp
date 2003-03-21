005c600638c253c4c47f8cbdb11e8787deabdd73
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2003-03-21 12:14:23 +0000

    no message

--- Core/Interactions/mitkStateMachine.cpp
+++ Core/Interactions/mitkStateMachine.cpp
@@ -8,6 +8,7 @@ mitk::StateMachine::StateMachine(std::string type)
 //##ModelId=3E5B2DE30378
 bool mitk::StateMachine::SetEvent(StateEvent const* stateEvent)
 {
+	return true;
 }
 
 //##ModelId=3E5B2E660087
@@ -19,5 +20,6 @@ std::string mitk::StateMachine::GetName() const
 //##ModelId=3E5B2E170228
 bool mitk::StateMachine::Operation(int sideEffectId)
 {
+	return true;
 }
 

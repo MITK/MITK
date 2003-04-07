a49d0e87b653dcdd1c80551ea7fd34068fbbbbf4
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2003-04-07 12:38:27 +0000

    changed Constructor  ...int button, int buttonState...

--- Core/Interactions/mitkPositionEvent.cpp
+++ Core/Interactions/mitkPositionEvent.cpp
@@ -2,7 +2,7 @@
 
 //##ModelId=3E5B7CF900F0
 mitk::PositionEvent::PositionEvent(int type, int button, int buttonState, int key, const Vector3f& position)
-: m_Position(position), Event(type, buttonNumber, key)
+: m_Position(position), Event(type, button, buttonState, key)
 {}
 
 //##ModelId=3E5B79DE0133

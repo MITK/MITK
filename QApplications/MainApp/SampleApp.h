17e1395facbf8a324dfb45458f3ece288be015fe
boettger <boettger@nowhere>
2003-04-08 08:53:52 +0000

    Initial revision

---
+++ QApplications/MainApp/SampleApp.h
@@ -0,0 +1,29 @@
+#include "QmitkMainTemplate.h"
+
+
+/*!
+	\brief This application demonstrates how to code a new MITK application 
+
+*/
+class SampleApp : public QmitkMainTemplate {
+	Q_OBJECT
+
+public:
+		/*!
+			\brief Default constructor
+		*/
+    SampleApp( QWidget* parent = 0, const char* name = 0, WFlags fl = WType_TopLevel );
+    
+		/*!
+			\brief Default destructor
+		*/
+    ~SampleApp();
+
+	/*!
+		\brief Overrides initializeFunctionality() from QmitKMainTemplate
+
+		In this method the user adds the application specific functionalities
+		to the application template
+	 */
+	virtual void initializeFunctionality();
+};

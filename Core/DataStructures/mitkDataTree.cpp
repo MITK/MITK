d6faf0e1e89568ba88f0bd543cfda169b1ebb0f7
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-04-25 13:24:15 +0000

    dummy istream operator added for DataTreeNode::Pointer. Only MSVC++ 6 insists on its existance, while it is not known why the other compilers don't need it.

--- Core/DataStructures/mitkDataTree.cpp
+++ Core/DataStructures/mitkDataTree.cpp
@@ -1,48 +1,102 @@
 #include "DataTree.h"
 
+
 //##ModelId=3E38F46A0190
+
 mitk::DataTree::DataTree() : 
+
     DataTreeBase( )
+
 {
+
 }
+
+
+
 
 
 //##ModelId=3E38F46A01AE
+
 mitk::DataTree::~DataTree()
+
 {
+
 }
 
+
+
 /**
+
  *
+
  */
+
 //##ModelId=3E3FE0430148
+
 TreeIterator<mitk::DataTreeNode::Pointer>* mitk::DataTree::GetNext( const char* propertyKey, const mitk::BaseProperty* property,  TreeIterator<mitk::DataTreeNode::Pointer>* startPosition ){
 
+
+
 	if ( startPosition == NULL )
+
 		startPosition = preorderIterator();
 
+
+
 	TreeIterator<mitk::DataTreeNode::Pointer>* pos = startPosition->clone();
+
 	mitk::DataTreeNode::Pointer dtn;
+
 	
+
+
 
 	while ( pos->hasNext() ) {
+
 	
+
 		dtn = pos->next();
+
 		mitk::PropertyList::Pointer propertyList = dtn->GetPropertyList();
 
+
+
 		if ( property == propertyList->GetProperty( propertyKey ) )
+
 			return pos;			
+
 	}
+
 	
+
 	delete pos;
+
 	return NULL;
+
 }
+
+
 
 /**
+
  *
+
  */
+
 //##ModelId=3EA6ADB7029F
+
 void mitk::DataTree::treeChanged( TreeIterator<DataTreeNode::Pointer>& changedTreePosition ) {
 
+
+
 	Modified();
+
 }
+
+MBI_STD::istream& operator>>( MBI_STD::istream& i, mitk::DataTreeNode::Pointer& dtn ) {
+
+	dtn = mitk::DataTreeNode::New();
+   //i >> av.get();
+   return i;
+}
+
--- Core/DataStructures/mitkDataTree.h
+++ Core/DataStructures/mitkDataTree.h
@@ -1,56 +1,109 @@
 #ifndef DATATREE_H_HEADER_INCLUDED_C1C7797C
+
 #define DATATREE_H_HEADER_INCLUDED_C1C7797C
+
+
 
 #include "mitkCommon.h"
 
+
+
 #include "DataTreeNode.h"
+
+
 
 #include <Tree/LinkedTree.h>
 
+
+
 namespace mitk {
+
 //##ModelId=3E3959140012
+
 typedef ::LinkedTree<DataTreeNode::Pointer> DataTreeBase;
 
+
+
 //##ModelId=3E38F35101A0
+
 class DataTree : public itk::Object, public DataTreeBase
+
 {
+
    public:
+
 	/** Standard class typedefs. */
+
     //##ModelId=3E38FFC80202
+
 	typedef DataTree                 Self;
+
     //##ModelId=3E38FFC80220
+
 	typedef itk::Object              Superclass;
+
     //##ModelId=3E38FFC8023E
+
 	typedef itk::SmartPointer<Self>  Pointer;
+
     //##ModelId=3E38FFC80266
+
 	typedef itk::SmartPointer<const Self>  ConstPointer;
 
+
+
 	/** Method for creation through the object factory. */
+
 	itkNewMacro(Self);
 
+
+
 	/** Run-time type information (and related methods). */
+
 	itkTypeMacro(DataTree,itk::Object);
 
+
+
     //##ModelId=3E3FE0430148
+
 	TreeIterator<mitk::DataTreeNode::Pointer>* GetNext( const char* propertyName, const mitk::BaseProperty* property,  TreeIterator<mitk::DataTreeNode::Pointer>* startPosition = NULL );
 
+
+
  protected:
+
     //##ModelId=3E38F46A0190
+
     DataTree();
 
+
+
     //##ModelId=3E38F46A01AE
+
     virtual ~DataTree();
 
+
+
     //##ModelId=3EA6ADB7029F
+
 	void treeChanged( TreeIterator<DataTreeNode::Pointer>& changedTreePosition );	
+
+
 
 };
 
+
+
 //##ModelId=3E394E99028E
+
 typedef	::TreeIterator<DataTreeNode::Pointer> DataTreeIterator;
+
+
 
 } // namespace mitk
 
 
+MBI_STD::istream& operator>>( MBI_STD::istream& i, mitk::DataTreeNode::Pointer& dtn );
 
 #endif /* DATATREE_H_HEADER_INCLUDED_C1C7797C */
+

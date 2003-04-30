db1e6ba62e901a62b4d845c642f9f04128c48f21
boettger <boettger@nowhere>
2003-04-30 14:56:49 +0000

    added static method ComputeBoundingBox(mitk::DataTreeIterator * it)

--- Core/DataStructures/mitkDataTree.cpp
+++ Core/DataStructures/mitkDataTree.cpp
@@ -1,95 +1,83 @@
 #include "DataTree.h"
 
-
 //##ModelId=3E38F46A0190
-
 mitk::DataTree::DataTree() : 
-
     DataTreeBase( )
-
 {
-
 	addTreeChangeListener( this );
 }
 
 
-
-
-
 //##ModelId=3E38F46A01AE
-
 mitk::DataTree::~DataTree()
-
 {
-
 }
 
-
-
 /**
-
  *
-
  */
-
 //##ModelId=3E3FE0430148
-
 TreeIterator<mitk::DataTreeNode::Pointer>* mitk::DataTree::GetNext( const char* propertyKey, const mitk::BaseProperty* property,  TreeIterator<mitk::DataTreeNode::Pointer>* startPosition ){
 
-
-
 	if ( startPosition == NULL )
-
 		startPosition = preorderIterator();
 
-
-
 	TreeIterator<mitk::DataTreeNode::Pointer>* pos = startPosition->clone();
-
 	mitk::DataTreeNode::Pointer dtn;
-
 	
-
-
 
 	while ( pos->hasNext() ) {
-
 	
-
 		dtn = pos->next();
-
 		mitk::PropertyList::Pointer propertyList = dtn->GetPropertyList();
 
-
-
 		if ( property == propertyList->GetProperty( propertyKey ) )
-
 			return pos;			
-
 	}
-
 	
-
 	delete pos;
-
 	return NULL;
-
 }
 
-
-
 /**
-
  *
-
  */
-
 //##ModelId=3EA6ADB7029F
-
 void mitk::DataTree::treeChanged( TreeIterator<DataTreeNode::Pointer>& changedTreePosition ) {
 
-
-
 	Modified();
+}
 
+mitk::BoundingBox::Pointer mitk::DataTree::ComputeBoundingBox(mitk::DataTreeIterator * it)
+{
+		mitk::DataTreeIterator* _it=it->clone();
+    mitk::BoundingBox::Pointer m_BoundingBox=mitk::BoundingBox::New();
+    mitk::BoundingBox::PointsContainer::Pointer pointscontainer=mitk::BoundingBox::PointsContainer::New();
+    
+		float nullpoint[]={0,0,0};
+    mitk::BoundingBox::PointType p(nullpoint);
+
+    mitk::BoundingBox::PointIdentifier pointid=0;
+    
+    while (_it->hasNext()) {
+			_it->next();
+			mitk::DataTreeNode::Pointer node = _it->get();
+			if (node->GetData() != NULL && node->GetData()->GetGeometry() != NULL	) {
+				mitk::BoundingBox::ConstPointer nextBoundingBox = node->GetData()->GetGeometry()->GetBoundingBox();
+				const mitk::BoundingBox::PointsContainer * nextPoints = nextBoundingBox->GetPoints();
+				mitk::BoundingBox::PointsContainer::ConstIterator pointsIt = nextPoints->Begin();
+
+				while (pointsIt != nextPoints->End() ) {
+					pointscontainer->InsertElement( pointid++, pointsIt->Value());
+					pointsIt++;
+				}
+			}
+		}
+
+	mitk::BoundingBox::Pointer result = mitk::BoundingBox::New();
+	result->SetPoints(pointscontainer);
+	result->ComputeBoundingBox();
+
+	delete _it;
+	return result;
 }
--- Core/DataStructures/mitkDataTree.h
+++ Core/DataStructures/mitkDataTree.h
@@ -1,105 +1,67 @@
 #ifndef DATATREE_H_HEADER_INCLUDED_C1C7797C
-
 #define DATATREE_H_HEADER_INCLUDED_C1C7797C
 
-
-
 #include "mitkCommon.h"
-
-
-
 #include "DataTreeNode.h"
-
-
-
+//#include "DataTreeIterator.h"
+#include "Geometry3D.h"
 #include <Tree/LinkedTree.h>
 
-
-
 namespace mitk {
-
 //##ModelId=3EA93EC901B8
-
 typedef ::LinkedTree<mitk::DataTreeNode::Pointer> DataTreeBase;
 //##ModelId=3EA9438F0346
 typedef DataTreeBase::TreeChangeListener DataTreeBaseTreeChangeListener;
 
-
-
-//##ModelId=3E38F35101A0
-
-class DataTree : public itk::Object, public DataTreeBase, public DataTreeBaseTreeChangeListener
-
-{
-
-   public:
-
-	/** Standard class typedefs. */
-
-    //##ModelId=3E38FFC80202
-
-	typedef DataTree                 Self;
-
-    //##ModelId=3E38FFC80220
-
-	typedef itk::Object              Superclass;
-
-    //##ModelId=3E38FFC8023E
-
-	typedef itk::SmartPointer<Self>  Pointer;
-
-    //##ModelId=3E38FFC80266
-
-	typedef itk::SmartPointer<const Self>  ConstPointer;
-
-
-
-	/** Method for creation through the object factory. */
-
-	itkNewMacro(Self);
-
-
-
-	/** Run-time type information (and related methods). */
-
-	itkTypeMacro(DataTree,itk::Object);
-
-
-
-    //##ModelId=3E3FE0430148
-
-	TreeIterator<mitk::DataTreeNode::Pointer>* GetNext( const char* propertyName, const mitk::BaseProperty* property,  TreeIterator<mitk::DataTreeNode::Pointer>* startPosition = NULL );
-
-
-
- protected:
-
-    //##ModelId=3E38F46A0190
-
-    DataTree();
-
-
-
-    //##ModelId=3E38F46A01AE
-
-    virtual ~DataTree();
-
-
-
-    //##ModelId=3EA6ADB7029F
-
-	void treeChanged( TreeIterator<DataTreeNode::Pointer>& changedTreePosition );	
-
-
-
-};
-
 //##ModelId=3E394E99028E
 typedef	::TreeIterator<DataTreeNode::Pointer> DataTreeIterator;
 
+
+//##ModelId=3E38F35101A0
+class DataTree : public itk::Object, public DataTreeBase, public DataTreeBaseTreeChangeListener
+{
+
+	public:
+
+
+
+	/** Standard class typedefs. */
+    //##ModelId=3E38FFC80202
+	typedef DataTree                 Self;
+    //##ModelId=3E38FFC80220
+	typedef itk::Object              Superclass;
+    //##ModelId=3E38FFC8023E
+	typedef itk::SmartPointer<Self>  Pointer;
+    //##ModelId=3E38FFC80266
+	typedef itk::SmartPointer<const Self>  ConstPointer;
+
+	/** Method for creation through the object factory. */
+	itkNewMacro(Self);
+
+	/** Run-time type information (and related methods). */
+	itkTypeMacro(DataTree,itk::Object);
+
+    //##ModelId=3E3FE0430148
+	TreeIterator<mitk::DataTreeNode::Pointer>* GetNext( const char* propertyName, const mitk::BaseProperty* property,  TreeIterator<mitk::DataTreeNode::Pointer>* startPosition = NULL );
+
+	/*!
+		\brief compute the bounding box of data tree structure
+		@param it an iterator of a data tree structure
+		*/
+	static mitk::BoundingBox::Pointer ComputeBoundingBox(mitk::DataTreeIterator * it);
+
+ protected:
+    //##ModelId=3E38F46A0190
+    DataTree();
+
+    //##ModelId=3E38F46A01AE
+    virtual ~DataTree();
+
+    //##ModelId=3EA6ADB7029F
+	void treeChanged( TreeIterator<DataTreeNode::Pointer>& changedTreePosition );	
+
+};
+
 } // namespace mitk
-
 #endif /* DATATREE_H_HEADER_INCLUDED_C1C7797C */
-
-
 

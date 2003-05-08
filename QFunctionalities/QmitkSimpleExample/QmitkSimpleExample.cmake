9bb6c490ca909e749e63fa2822745c1280528301
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2003-05-08 09:16:26 +0000

    build application with cmake

--- QFunctionalities/QmitkSimpleExample/QmitkSimpleExample.cmake
+++ QFunctionalities/QmitkSimpleExample/QmitkSimpleExample.cmake
@@ -2,7 +2,7 @@ INCLUDE_DIRECTORIES(../../Framework/QmitkApplicationBase ../../AppModule/QmitkSi
 
 # an example of a functionality
 
-SET( APPMOD_H ${APPMOD_H} ../../AppModule/QmitkSimpleExample/QmitkSimpleExampleFunctionality.h )
+SET( APPMOD_MOC_H ${APPMOD_MOC_H} ../../AppModule/QmitkSimpleExample/QmitkSimpleExampleFunctionality.h )
 SET( APPMOD_CPP ${APPMOD_CPP} ../../AppModule/QmitkSimpleExample/QmitkSimpleExampleFunctionality.cpp )
 
 SET (APPMOD_FORMS ${APPMOD_FORMS} ../../AppModule/QmitkSimpleExample/QmitkSimpleExampleControls.ui)

3d8f980414dd8823ee9041d891d4450585e55af2
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-06-17 15:37:45 +0000

    Removed some lines for debug-purposes.

--- Core/Rendering/mitkOpenGLRenderer.cpp
+++ Core/Rendering/mitkOpenGLRenderer.cpp
@@ -19,8 +19,8 @@
 mitk::OpenGLRenderer::OpenGLRenderer() : m_VtkMapperPresent(NULL)
 {
     m_CameraController=NULL;
-//    m_CameraController = VtkInteractorCameraController::New();
-//    m_CameraController->AddRenderer(this);
+    m_CameraController = VtkInteractorCameraController::New();
+    m_CameraController->AddRenderer(this);
 }
 
 //##ModelId=3E3D28AB0018
@@ -236,8 +236,6 @@ void mitk::OpenGLRenderer::Render()
 		
 		mitk::DataTreeNode::Pointer node = it->get();
 		mitk::Mapper::Pointer mapper = node->GetMapper(m_MapperID);
-if(dynamic_cast<mitk::Image*>(node->GetData())==NULL)
-{
 		if(mapper.IsNotNull())
 		{
 			GLMapper2D* mapper2d=dynamic_cast<GLMapper2D*>(mapper.GetPointer());
@@ -245,7 +243,6 @@ if(dynamic_cast<mitk::Image*>(node->GetData())==NULL)
 				mapper2d->Paint(this);
 			}
 		}
-}
 	}
 	
 	delete it;

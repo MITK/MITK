c7df1907f43f062c8c6884988812fbc2117d1bbc
siwei <siwei@nowhere>
2003-07-03 14:32:10 +0000

    DICOM/Live3D File Reader

---
+++ Core/Algorithms/mitkDICOMFileReader.cpp
@@ -0,0 +1,133 @@
+#include "mitkDICOMFileReader.h"
+#include <itkImageFileReader.h>
+#ifdef MITK_DICOM_ENABLED
+extern "C"
+{
+#include "ipDicom.h"
+}
+#endif
+
+void mitk::DICOMFileReader::GenerateOutputInformation()
+{
+    mitk::Image::Pointer output = this->GetOutput();
+
+    if ((output->IsInitialized()) && (this->GetMTime() <= m_ReadHeaderTime.GetMTime()))
+        return;
+
+    itkDebugMacro(<<"Reading file for GenerateOutputInformation()" << m_FileName);
+
+    // Check to see if we can read the file given the name or prefix
+    //
+    if ( m_FileName == "" )
+    {
+        throw itk::ImageFileReaderException(__FILE__, __LINE__, "One of FileName or FilePrefix must be non-empty");
+    }
+
+    if( m_FileName != "")
+    {
+		//**************
+//		std::string outputfile_name;
+		ipUInt4_t header_size;
+		ipUInt4_t image_size,slices;
+		ipUInt1_t *header = NULL;
+		unsigned char *image = NULL;
+		ipBool_t color;
+        ipBool_t encrypt;
+        void * data;
+		ipUInt4_t  len;
+		outputfile_name = m_FileName;
+		dicomGetHeaderAndImage( const_cast<char *>(m_FileName.c_str()),
+                          &header, &header_size,
+                          &image, &image_size );
+
+        if( !image )
+        exit(-1);
+		pic = _dicomToPic( header, header_size,
+                     image, image_size, color );
+		//pic->dim--;
+		
+	/*{
+		//ipInt4_t slices;
+
+		ipInt4FromLE( data, &slices );
+		memcpy( &slices, data, 4 );
+		std::cout<<"Image Dimensioin"<<pic->dim;
+		pic->n[pic->dim] = pic->n[pic->dim-1];
+		pic->n[pic->dim-1] = slices;
+        //pic->dim++;
+	}*/
+		outputfile_name = "out.pic";
+		ipPicPut( const_cast<char *>(outputfile_name.c_str()),pic );
+		
+		/////////////
+
+      //ipPicDescriptor* pic_header=ipPicGetHeader(const_cast<char *>(outputfile_name.c_str()), NULL);
+
+      //pic_header=ipPicGetTags(const_cast<char *>(outputfile_name.c_str()), pic_header);
+
+        if( pic == NULL)
+        {
+            itk::ImageFileReaderException e(__FILE__, __LINE__);
+            itk::OStringStream msg;
+            msg << " Could not read file "
+                << outputfile_name.c_str();
+            e.SetDescription(msg.str().c_str());
+            throw e;
+            return;
+        }
+
+        output->Initialize(pic);
+    }
+
+    m_ReadHeaderTime.Modified();
+}
+
+//##ModelId=3E187255016C
+void mitk::DICOMFileReader::GenerateData()
+{
+    mitk::Image::Pointer output = this->GetOutput();
+
+    // Check to see if we can read the file given the name or prefix
+    //
+    if ( m_FileName == "" && m_FilePrefix == "" )
+    {
+        throw itk::ImageFileReaderException(__FILE__, __LINE__, "One of FileName or FilePrefix must be non-empty");
+    }
+
+    if( m_FileName != "")
+    {
+        
+    //left to right handed conversion
+		if(pic->dim==3)
+        {
+            ipPicDescriptor* slice=ipPicCopyHeader(pic, NULL);
+            slice->dim=2;
+            int size=_ipPicSize(slice);
+            slice->data=malloc(size);
+            unsigned char *p_first=(unsigned char *)pic->data;
+            unsigned char *p_last=(unsigned char *)pic->data;
+            p_last+=size*(pic->n[2]-1);
+
+            int i, smid=pic->n[2]/2;
+            for(i=0; i<smid;++i,p_last-=size, p_first+=size)
+            {
+                memcpy(slice->data, p_last, size);
+                memcpy(p_last, p_first, size);
+                memcpy(p_first, slice->data, size);
+            }
+            ipPicFree(slice);
+        }
+        output->SetPicVolume(pic);
+    }
+}
+
+//##ModelId=3E1874D202D0
+mitk::DICOMFileReader::DICOMFileReader()
+    : m_FileName(""), m_FilePrefix(""), m_FilePattern("")
+{
+}
+
+//##ModelId=3E1874E50179
+mitk::DICOMFileReader::~DICOMFileReader()
+{
+}
---
+++ Core/Algorithms/mitkDICOMFileReader.h
@@ -0,0 +1,67 @@
+#ifndef MITKDICOMFILEREADER_H_HEADER_INCLUDED_C1F48A22
+#define MITKDICOMFILEREADER_H_HEADER_INCLUDED_C1F48A22
+
+#include "mitkCommon.h"
+#include "FileReader.h"
+#include "ImageSource.h"
+
+namespace mitk {
+//##Documentation
+//## @brief Reader to read files in DICOM-format
+//## @ingroup Process
+class DICOMFileReader : public ImageSource, public FileReader
+{
+public:
+    mitkClassMacro(DICOMFileReader, FileReader);
+
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+
+    itkSetStringMacro(FileName);
+    itkGetStringMacro(FileName);
+
+    itkSetStringMacro(FilePrefix);
+    itkGetStringMacro(FilePrefix);
+
+    itkSetStringMacro(FilePattern);
+    itkGetStringMacro(FilePattern);
+
+protected:
+    //##ModelId=3E187255016C
+    virtual void GenerateData();
+
+    //##ModelId=3E1878400265
+    virtual void GenerateOutputInformation();
+
+    //##ModelId=3E1874D202D0
+    DICOMFileReader();
+
+    //##ModelId=3E1874E50179
+    ~DICOMFileReader();
+
+    //##ModelId=3E19EA120063
+    //##Description 
+    //## @brief Time when Header was last read
+    itk::TimeStamp m_ReadHeaderTime;
+
+    //##ModelId=3EA6ADB80138
+    int m_StartFileIndex;
+  protected:
+    //##ModelId=3E186FC80253
+    std::string m_FileName;
+
+    //##ModelId=3E1873B3030A
+    std::string m_FilePrefix;
+
+    //##ModelId=3E1873B30346
+    std::string m_FilePattern;
+private:
+	std::string outputfile_name;
+	ipPicDescriptor *pic;
+
+};
+
+} // namespace mitk
+
+#endif /* MITKDICOMFILEREADER_H_HEADER_INCLUDED_C1F48A22 */
+

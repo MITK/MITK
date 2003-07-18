a63a726a3d0a81378c02b64f5672a61568f3108a
mark <mark@nowhere>
2003-07-18 11:24:18 +0000

    itk-Filter zum Anschluss der mitk-Pipeline and die itk Pipeline. Datentyp und Dimension des Bildes muessen bekannt sein.

--- Core/Algorithms/mitkImageToItk.h
+++ Core/Algorithms/mitkImageToItk.h
@@ -2,19 +2,22 @@
 #define IMAGETOITK_H_HEADER_INCLUDED_C1C2FCD2
 
 #include <itkImage.h>
+#include <itkImageSource.h>
 #include "mitkImage.h"
 
 namespace mitk
 {
 	
-template <class TPixel, unsigned int VImageDimension=2>
-class ITK_EXPORT ImageToItk : public itk::ImageSource<TPixel,VImageDimension>
+template <typename TPixel, unsigned int VImageDimension=2>
+class ITK_EXPORT ImageToItk : public itk::ImageSource< itk::Image<TPixel,VImageDimension> >
 {
 protected:
-//	mitk::Image::Pointer m_MitkImage;
+	mitk::Image::Pointer m_MitkImage;
 	mitk::ImageDataItem::Pointer m_ImageDataItem;
+  
 public:
-	mitkClassMacro(ImageToItk, itk::Image);
+  typedef itk::Image<TPixel,VImageDimension> TOutputImage;
+	mitkClassMacro(ImageToItk, itk::ImageSource<TOutputImage >);
 
 	/** Method for creation through the object factory. */
 	itkNewMacro(Self);  
@@ -26,115 +29,19 @@ public:
 	typedef mitk::Image                    InputImageType;
 	typedef InputImageType::Pointer        InputImagePointer;
 	typedef InputImageType::ConstPointer   InputImageConstPointer;
-	typedef SlicedData::RegionType         InputImageRegionType; 
+	typedef SlicedData::RegionType         InputImageRegionType;
+  typedef typename TOutputImage::SizeType         SizeType;
+  typedef typename TOutputImage::IndexType        IndexType;
+  typedef typename TOutputImage::RegionType       RegionType;
+  typedef typename TOutputImage::PixelType        PixelType;
+  
+//	/** Set/Get the image input of this process object.  */
+//	virtual void SetInput( const InputImageType *image);
+//	virtual void SetInput( unsigned int, const InputImageType * image);
+//	const InputImageType * GetInput(void);
+//	const InputImageType * GetInput(unsigned int idx);
 
-	/** Set/Get the image input of this process object.  */
-	virtual void SetInput( const InputImageType *image);
-	virtual void SetInput( unsigned int, const InputImageType * image);
-	const InputImageType * GetInput(void);
-	const InputImageType * GetInput(unsigned int idx);
-
-	/** set the @a mitk::Image that shall be accessed from itk via this itk-wrapper-class.
-	* @param n channel number to be accessed
-	*/
-	void SetMitkImage(mitk::Image *image, int n = 0)
-	{
-//		m_MitkImage = image;
-
-		if(image.IsNull())
-			itkExceptionMacro( << "image is null" );
-
-		if(image->GetDimension()!=VImageDimension)
-			itkExceptionMacro( << "image has dimension " << image->GetDimension() << " instead of " << VImageDimension );
-
-		if(typeid(TPixel) != *image->GetType().GetTypeId())
-			itkExceptionMacro( << "image has wrong pixel type " );
-
-		itkDebugMacro("ImageToItk::SetImageToItk ...");
-		
-		SizeType  size;
-		double origin[ VImageDimension ];
-		double spacing[ VImageDimension ];
-		
-		for (int i=0; i < VImageDimension; ++i) 
-		{
-			size[i]    = image->GetDimension(i);
-			spacing[i] = image->GetGeometry()->GetSpacing()[i];
-		}
-		
-		mitk::Point3D mitkorigin(0,0,0);
-		image->GetGeometry()->UnitsToMM(mitkorigin, mitkorigin);
-		origin[0] = mitkorigin.x;    // X coordinate
-		origin[1] = mitkorigin.y;    // Y coordinate
-		origin[2] = mitkorigin.z;    // Z coordinate
-		
-		IndexType start;
-		start.Fill( 0 );
-		
-		RegionType region;
-		region.SetIndex( start );
-		region.SetSize(  size  );
-		
-		unsigned long noBytes = size[0];
-		for (int i=1; i<VImageDimension; ++i) 
-		{
-			noBytes = noBytes * size[i];
-		}
-		
-		if (m_CopyMemFlag)
-		{
-			itkDebugMacro("copyMem ...");
-			
-			this->SetRegions( region );
-			this->SetOrigin( origin );
-			this->SetSpacing( spacing );
-			
-			this->Allocate();
-			memcpy( (TPixel *) this->GetBufferPointer(),
-				image->GetData(), sizeof(TPixel)*noBytes);
-			
-		}
-		else 
-		{
-			m_ImageDataItem = image->GetChannelData(n);
-
-			itkDebugMacro("do not copyMem ...");
-			
-			typedef itk::ImportImageContainer< unsigned long, TPixel >   ImportContainerType;
-			ImportContainerType::Pointer import;
-			
-			import = ImportContainerType::New();
-			import->Initialize();
-			
-			itkDebugMacro( << "size of container = " << import->Size() );
-			import->SetImportPointer( (TPixel*) m_ImageDataItem->GetData(),	noBytes, false);
-			
-			this->SetRegions( region );
-			this->SetOrigin( origin );
-			this->SetSpacing( spacing );
-			this->SetPixelContainer(import);
-			//this->Update();
-			itkDebugMacro( << "size of container = " << import->Size() );
-		}
-		
-		itkDebugMacro( << "ImageToItk::SetMitkImage OK!" );
-	}
-	
-	void SetCopyMemFlagOn()
-	{
-		m_CopyMemFlag = true;
-	}
-	
-	void SetCopyMemFlagOff()
-	{
-		m_CopyMemFlag = false;
-	}
-	
-	void SetCopyMemFlag(bool flag)
-	{
-		m_CopyMemFlag = flag;
-	}
-	
+	void SetMitkImage(mitk::Image::Pointer image, int n = 0);
 	
 protected:
 	ImageToItk();
@@ -143,26 +50,22 @@ protected:
 
 	void PrintSelf(std::ostream& os, itk::Indent indent) const;
 	
-	/** What is the input requested region that is required to produce the
-	* output requested region? The base assumption for image processing
-	* filters is that the input requested region can be set to match the
-	* output requested region.  If a filter requires more input (for instance
-	* a filter that uses neighborhoods needs more input than output to avoid
-	* introducing artificial boundary conditions) or less input (for instance 
-	* a magnify filter) will have to override this method.  In doing so, it
-	* should call its superclass' implementation as its first step. Note that
-	* this imaging filters operate differently than the classes to this
-	* point in the class hierachy.  Up till now, the base assumption has been
-	* that the largest possible region will be requested of the input.
-	*
-	* \sa ProcessObject::GenerateInputRequestedRegion(),
-	*     ImageSource::GenerateInputRequestedRegion() */
-	virtual void GenerateInputRequestedRegion();
+
+  virtual void GenerateData();
+
+  virtual void GenerateOutputInformation();
+  
+  void SetCopyMemFlagOn();
+
+  void SetCopyMemFlagOff();
+
+  void SetCopyMemFlag(bool flag);
 
 private:
 	bool m_CopyMemFlag;
-		
-	ImageToItk(const Self&); //purposely not implemented
+  int m_Channel;
+  
+	//ImageToItk(const Self&); //purposely not implemented
 	void operator=(const Self&); //purposely not implemented
 
 };
--- Core/Algorithms/mitkImageToItk.txx
+++ Core/Algorithms/mitkImageToItk.txx
@@ -27,7 +27,7 @@ namespace mitk
 
 template<class TPixel, unsigned int VImageDimension>
 ImageToItk<TPixel, VImageDimension>
-::ImageToItk() : itk::Image<TPixel, VImageDimension>(), m_CopyMemFlag(false)
+::ImageToItk() : itk::ImageSource< itk::Image<TPixel, VImageDimension> >(), m_CopyMemFlag(false)
 {
 
 }
@@ -38,6 +38,155 @@ ImageToItk<TPixel, VImageDimension>
 ::~ImageToItk()
 {
 
+}
+
+/** set the @a mitk::Image that shall be accessed from itk via this itk-wrapper-class.
+* @param n channel number to be accessed
+*/
+template<class TPixel, unsigned int VImageDimension>
+void ImageToItk<TPixel, VImageDimension>
+::SetMitkImage(mitk::Image::Pointer image, int n = 0)
+{
+
+  std::cout << "ImageToItk::SetMitkImage() ... " << std::endl;
+
+	if(image == NULL)
+			itkExceptionMacro( << "image is null" );
+		if(image->GetDimension()!=VImageDimension)
+			itkExceptionMacro( << "image has dimension " << image->GetDimension() << " instead of " << VImageDimension );
+
+
+   if(typeid(TPixel) != *image->GetPixelType().GetTypeId())
+			itkExceptionMacro( << "image has wrong pixel type " );
+
+		itkDebugMacro("ImageToItk::SetImageToItk ...");
+
+  std::cout << "Hallo..  " << std::endl;    
+ 		m_MitkImage = image;
+
+   m_Channel = n;
+
+
+   this->Modified();
+   
+
+		itkDebugMacro( << "ImageToItk::SetMitkImage OK!" );
+  std::cout << "ImageToItk::SetMitkImage() OK! " << std::endl;
+  
+}
+
+template<class TPixel, unsigned int VImageDimension>
+void ImageToItk<TPixel, VImageDimension>
+::SetCopyMemFlagOn()
+{
+		m_CopyMemFlag = true;
+}
+
+template<class TPixel, unsigned int VImageDimension>
+void ImageToItk<TPixel, VImageDimension>
+::SetCopyMemFlagOff()
+{
+		m_CopyMemFlag = false;
+}
+
+template<class TPixel, unsigned int VImageDimension>
+void ImageToItk<TPixel, VImageDimension>
+::SetCopyMemFlag(bool flag)
+	{
+		m_CopyMemFlag = flag;
+	}
+
+
+template<class TPixel, unsigned int VImageDimension>
+void ImageToItk<TPixel, VImageDimension>
+::GenerateData()
+{
+
+  std::cout << "ImageToItk<TPixel, VImageDimension>::GenerateData() ... " << std::endl;
+
+
+  // Allocate output
+  typename OutputImageType::Pointer output = this->GetOutput();
+
+
+	unsigned long noBytes = m_MitkImage->GetDimension(0);
+	for (int i=1; i<VImageDimension; ++i)
+	{
+		noBytes = noBytes * m_MitkImage->GetDimension(i);
+	}
+
+	if (m_CopyMemFlag)
+	{
+			itkDebugMacro("copyMem ...");
+
+
+		output->Allocate();
+    
+    // hier wird momentan wohl nur der erste Channel verwendet??!!
+		memcpy( (PixelType *) output->GetBufferPointer(), m_MitkImage->GetData(), sizeof(PixelType)*noBytes);
+
+	}
+	else
+	{
+		m_ImageDataItem = m_MitkImage->GetChannelData( m_Channel );
+
+		itkDebugMacro("do not copyMem ...");
+		typedef itk::ImportImageContainer< unsigned long, TPixel >   ImportContainerType;
+		ImportContainerType::Pointer import;
+
+		import = ImportContainerType::New();
+		import->Initialize();
+
+    itkDebugMacro( << "size of container = " << import->Size() );
+		import->SetImportPointer( (TPixel*) m_ImageDataItem->GetData(),	noBytes, false);
+
+		output->SetPixelContainer(import);
+		//output->Update();
+		itkDebugMacro( << "size of container = " << import->Size() );
+		}
+    
+  std::cout << "ImageToItk<TPixel, VImageDimension>::GenerateData() OK! " << std::endl;
+
+  
+}
+
+template<class TPixel, unsigned int VImageDimension>
+void ImageToItk<TPixel, VImageDimension>
+::GenerateOutputInformation()
+{
+  std::cout << "ImageToItk<TPixel, VImageDimension>::GenerateOutputInformation() ... " << std::endl;
+
+ typename OutputImageType::Pointer output = this->GetOutput();
+ 
+	SizeType  size;
+	double origin[ VImageDimension ];
+	double spacing[ VImageDimension ];
+
+	for (int i=0; i < VImageDimension; ++i)
+	{
+		size[i]    = m_MitkImage->GetDimension(i);
+		spacing[i] = m_MitkImage->GetGeometry()->GetSpacing()[i];
+	}
+
+	mitk::Point3D mitkorigin(0,0,0);
+	m_MitkImage->GetGeometry()->UnitsToMM(mitkorigin, mitkorigin);
+	origin[0] = mitkorigin.x;    // X coordinate
+	origin[1] = mitkorigin.y;    // Y coordinate
+	origin[2] = mitkorigin.z;    // Z coordinate
+
+	IndexType start;
+	start.Fill( 0 );
+
+	RegionType region;
+	region.SetIndex( start );
+	region.SetSize(  size  );
+
+	output->SetRegions( region );
+	output->SetOrigin( origin );
+  output->SetSpacing( spacing );
+
+  std::cout << "ImageToItk<TPixel, VImageDimension>::GenerateOutputInformation() OK! " << std::endl;
+  
 }
 
 template<class TPixel, unsigned int VImageDimension>

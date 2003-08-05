0dfe9455bd1dd33caee256d94411948d4d3b76f6
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2003-08-05 16:50:04 +0000

    corrected mistake in writing

--- Core/Interactions/mitkEventDescription.h
+++ Core/Interactions/mitkEventDescription.h
@@ -9,7 +9,7 @@ namespace mitk {
 
 //##ModelId=3E5A3910009B
 //##Documentation
-//## @brief adds additional Information (Name and StateID) to an Event
+//## @brief adds additional Information (Name and EventID) to an Event
 //## @ingroup Interaction
 //## A name and an ID is added to the information of an event, so the event can
 //## be processed futher on.

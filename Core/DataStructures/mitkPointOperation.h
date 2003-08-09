0b4d2af9edc36863213abf922020954e80998e64
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2003-08-09 16:59:06 +0000

    fixed/added and wrote docu

--- Core/DataStructures/mitkPointOperation.h
+++ Core/DataStructures/mitkPointOperation.h
@@ -14,26 +14,27 @@ namespace mitk {
 //##
 //## @ingroup Undo
 //##
-//## Tores everything for Adding, Moving and Deleting a Point.
-//##
-//## @params 
-//## operationType is the type of that operation (see Operation.h; e.g. move or add; Information for StateMachine::ExecuteOperation())
-//## point is the information of the point to add. or for the point to move into (change)
+//## Stores everything for Adding, Moving and Deleting a Point.
 class PointOperation : public mitk::Operation
 {
   public:
     //##ModelId=3F0189F003A2
 	//##Documentation
-
-    PointOperation(OperationType operationType, 
+	//##@brief Operation, that handles all aktions on one Point.
+	//##
+	//## @params
+	//## operationType is the type of that operation (see Operation.h; e.g. move or add; Information for StateMachine::ExecuteOperation());
+	//## point is the information of the point to add or is the information to change a point into; index is e.g. the position in a
+	//## list which describes the element to change
+    PointOperation(OperationType operationType,
 					ITKPoint3D point, int index);
-    
+
     //##ModelId=3F0189F003B2
     virtual ~PointOperation();
-	
+
     //##ModelId=3F0189F003B4
 	ITKPoint3D GetPoint();
-	
+
     //##ModelId=3F0189F003B5
 	int GetIndex();
 

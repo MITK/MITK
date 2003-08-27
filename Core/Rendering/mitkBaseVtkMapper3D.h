3a95b2d8598cb56a66956530fc69fdff8931cde7
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-08-27 14:56:55 +0000

    reformatted

--- Core/Rendering/mitkBaseVtkMapper3D.h
+++ Core/Rendering/mitkBaseVtkMapper3D.h
@@ -21,63 +21,61 @@ namespace mitk {
 class BaseVtkMapper3D : public Mapper3D
 {
 public:
-	mitkClassMacro(Mapper,BaseVtkMapper3D);
+  mitkClassMacro(Mapper,BaseVtkMapper3D);
 
-    //##ModelId=3E32C93D0000
-    virtual vtkProp* GetProp();
-    //##ModelId=3EF180C70065
-    //##Documentation
-    //## @brief Apply color and opacity read from the PropertyList
-    virtual void ApplyProperties(vtkActor* actor, mitk::BaseRenderer* renderer);
-    //##ModelId=3EF19F3000A5
-    //##Documentation
-    //## @brief Called by BaseRenderer when an update is required
-    virtual void Update(mitk::BaseRenderer* renderer) = 0;
+  //##ModelId=3E32C93D0000
+  virtual vtkProp* GetProp();
+  //##ModelId=3EF180C70065
+  //##Documentation
+  //## @brief Apply color and opacity read from the PropertyList
+  virtual void ApplyProperties(vtkActor* actor, mitk::BaseRenderer* renderer);
+  //##ModelId=3EF19F3000A5
+  //##Documentation
+  //## @brief Called by BaseRenderer when an update is required
+  virtual void Update(mitk::BaseRenderer* renderer) = 0;
 
 
-
-  protected:
-    //##ModelId=3E3424950213
-    BaseVtkMapper3D();
-  
-    //##ModelId=3E3424950231
-    virtual ~BaseVtkMapper3D();
-
-  public:
-
-    itkGetObjectMacro(Geometry,Geometry3D);
-    itkSetObjectMacro(Geometry,Geometry3D);
-
-    itkGetConstMacro(SliceNr,int);
-    itkSetMacro(SliceNr,int);
-
-    itkGetConstMacro(TimeNr,int);
-    itkSetMacro(TimeNr,int);
-
-    itkGetConstMacro(ChannelNr,int);
-    itkSetMacro(ChannelNr,int);
 
 protected:
-    //##ModelId=3EDD039F0117
-    Geometry3D::Pointer m_Geometry;
-    //##ModelId=3ED91D050038
-    LevelWindow m_LevelWindow;
+  //##ModelId=3E3424950213
+  BaseVtkMapper3D();
 
-    //##ModelId=3E3AE0DC023D
-    int m_SliceNr;
+  //##ModelId=3E3424950231
+  virtual ~BaseVtkMapper3D();
 
-    //##ModelId=3E33ECF203A1
-    int m_TimeNr;
+public:
 
-    //##ModelId=3E33ECF203B5
-    int m_ChannelNr;
+  itkGetObjectMacro(Geometry,Geometry3D);
+  itkSetObjectMacro(Geometry,Geometry3D);
 
-    //##ModelId=3ED91D050046
-	vtkProp3D* m_Prop3D;
+  itkGetConstMacro(SliceNr,int);
+  itkSetMacro(SliceNr,int);
+
+  itkGetConstMacro(TimeNr,int);
+  itkSetMacro(TimeNr,int);
+
+  itkGetConstMacro(ChannelNr,int);
+  itkSetMacro(ChannelNr,int);
+
+protected:
+  //##ModelId=3EDD039F0117
+  Geometry3D::Pointer m_Geometry;
+  //##ModelId=3ED91D050038
+  LevelWindow m_LevelWindow;
+
+  //##ModelId=3E3AE0DC023D
+  int m_SliceNr;
+
+  //##ModelId=3E33ECF203A1
+  int m_TimeNr;
+
+  //##ModelId=3E33ECF203B5
+  int m_ChannelNr;
+
+  //##ModelId=3ED91D050046
+  vtkProp3D* m_Prop3D;
 };
 
 } // namespace mitk
-
-
 
 #endif /* BASEVTKMAPPER3D_H_HEADER_INCLUDED_C1CD0F0A */

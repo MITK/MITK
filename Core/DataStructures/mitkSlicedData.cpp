465130b177e258d7b15e4e188637347e4fb55974
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2003-09-26 22:01:00 +0000

    corrected SetLargestPossibleRegion

--- Core/DataStructures/mitkSlicedData.cpp
+++ Core/DataStructures/mitkSlicedData.cpp
@@ -3,165 +3,169 @@
 //##ModelId=3E141028018A
 void mitk::SlicedData::UpdateOutputInformation()
 {
-	if (this->GetSource())
-	{
-		this->GetSource()->UpdateOutputInformation();
-	}
-	// If we don't have a source, then let's make our Image
-	// span our buffer
-	else
-	{
-		m_UseLargestPossibleRegion = true;
-	}
+  if (this->GetSource())
+  {
+    this->GetSource()->UpdateOutputInformation();
+  }
+  // If we don't have a source, then let's make our Image
+  // span our buffer
+  else
+  {
+    m_UseLargestPossibleRegion = true;
+  }
 
-	// Now we should know what our largest possible region is. If our 
-	// requested region was not set yet, (or has been set to something 
-	// invalid - with no data in it ) then set it to the largest possible
-	// region.
-	if ( ! m_RequestedRegionInitialized)
-	{
-		this->SetRequestedRegionToLargestPossibleRegion();
-		m_RequestedRegionInitialized = true;
-	}
+  // Now we should know what our largest possible region is. If our 
+  // requested region was not set yet, (or has been set to something 
+  // invalid - with no data in it ) then set it to the largest possible
+  // region.
+  if ( ! m_RequestedRegionInitialized)
+  {
+    this->SetRequestedRegionToLargestPossibleRegion();
+    m_RequestedRegionInitialized = true;
+  }
 
-	m_LastRequestedRegionWasOutsideOfTheBufferedRegion = 0;
+  m_LastRequestedRegionWasOutsideOfTheBufferedRegion = 0;
 }
 
 //##ModelId=3E14102C029E
 void mitk::SlicedData::SetRequestedRegionToLargestPossibleRegion()
 {
-	m_UseLargestPossibleRegion = true;
+  m_UseLargestPossibleRegion = true;
+  m_RequestedRegion.SetIndex((SlicedData::RegionType::IndexType &)GetGeometry()->GetLargestPossibleRegion().GetIndex());
+  m_RequestedRegion.SetSize((SlicedData::RegionType::SizeType &)GetGeometry()->GetLargestPossibleRegion().GetSize());
+  m_RequestedRegion.SetIndex(4, 0);
+  m_RequestedRegion.SetSize(4, m_NumberOfChannels);
 }
 
 //##ModelId=3E14104300AC
 bool mitk::SlicedData::RequestedRegionIsOutsideOfTheBufferedRegion()
 {
-	if(VerifyRequestedRegion()==false) return true;
+  if(VerifyRequestedRegion()==false) return true;
 
-	// Is the requested region within the currently buffered data?
-	// SlicedData and subclasses store entire volumes or slices. The
-	// methods IsVolumeSet() and IsSliceSet are provided to check, 
-	// a volume or slice, respectively, is available. Thus, these
-	// methods used here.
-	const IndexType &requestedRegionIndex = m_RequestedRegion.GetIndex();
+  // Is the requested region within the currently buffered data?
+  // SlicedData and subclasses store entire volumes or slices. The
+  // methods IsVolumeSet() and IsSliceSet are provided to check, 
+  // a volume or slice, respectively, is available. Thus, these
+  // methods used here.
+  const IndexType &requestedRegionIndex = m_RequestedRegion.GetIndex();
 
-	const SizeType& requestedRegionSize = m_RequestedRegion.GetSize();
-	const Geometry3D::SizeType& largestPossibleRegionSize
-		= GetGeometry()->GetLargestPossibleRegion().GetSize();
+  const SizeType& requestedRegionSize = m_RequestedRegion.GetSize();
+  const Geometry3D::SizeType& largestPossibleRegionSize
+    = GetGeometry()->GetLargestPossibleRegion().GetSize();
 
-	// are whole channels requested?
-	int c, cEnd;
-	c=requestedRegionIndex[4];
-	cEnd=c+static_cast<long>(requestedRegionSize[4]);
-	if(requestedRegionSize[3] == largestPossibleRegionSize[3])
-	{
-		for (; c< cEnd; ++c)
-			if(IsChannelSet(c)==false) return true;
-		return false;
-	}
+  // are whole channels requested?
+  int c, cEnd;
+  c=requestedRegionIndex[4];
+  cEnd=c+static_cast<long>(requestedRegionSize[4]);
+  if(requestedRegionSize[3] == largestPossibleRegionSize[3])
+  {
+    for (; c< cEnd; ++c)
+      if(IsChannelSet(c)==false) return true;
+    return false;
+  }
 
-	// are whole volumes requested?
-	int t, tEnd;
-	t=requestedRegionIndex[3];
-	tEnd=t+static_cast<long>(requestedRegionSize[3]);
-	if(requestedRegionSize[2] == largestPossibleRegionSize[2])
-	{
-		for (; c< cEnd; ++c)
-			for (; t< tEnd; ++t)
-				if(IsVolumeSet(t, c)==false) return true;
-		return false;
-	}
+  // are whole volumes requested?
+  int t, tEnd;
+  t=requestedRegionIndex[3];
+  tEnd=t+static_cast<long>(requestedRegionSize[3]);
+  if(requestedRegionSize[2] == largestPossibleRegionSize[2])
+  {
+    for (; c< cEnd; ++c)
+      for (; t< tEnd; ++t)
+        if(IsVolumeSet(t, c)==false) return true;
+    return false;
+  }
 
-	// ok, only slices are requested. Check if they are available.
-	int s, sEnd;
-	s=requestedRegionIndex[2];
-	sEnd=s+static_cast<long>(requestedRegionSize[2]);
-	for (; c< cEnd; ++c)
-		for (; t< tEnd; ++t)
-			for (; s< sEnd; ++s)
-				if(IsSliceSet(s, t, c)==false) return true;
+  // ok, only slices are requested. Check if they are available.
+  int s, sEnd;
+  s=requestedRegionIndex[2];
+  sEnd=s+static_cast<long>(requestedRegionSize[2]);
+  for (; c< cEnd; ++c)
+    for (; t< tEnd; ++t)
+      for (; s< sEnd; ++s)
+        if(IsSliceSet(s, t, c)==false) return true;
 
-	return false;
+  return false;
 }
 
 //##ModelId=3E14105B00F7
 bool mitk::SlicedData::VerifyRequestedRegion()
 {
-    if(m_Geometry3D.IsNull()) return false;
+  if(m_Geometry3D.IsNull()) return false;
 
-	unsigned int i;
+  unsigned int i;
 
-	// Is the requested region within the LargestPossibleRegion?
-	// Note that the test is indeed against the largest possible region
-	// rather than the buffered region; see DataObject::VerifyRequestedRegion.
-	const IndexType &requestedRegionIndex = m_RequestedRegion.GetIndex();
-	const Geometry3D::IndexType &largestPossibleRegionIndex
-		= GetGeometry()->GetLargestPossibleRegion().GetIndex();
+  // Is the requested region within the LargestPossibleRegion?
+  // Note that the test is indeed against the largest possible region
+  // rather than the buffered region; see DataObject::VerifyRequestedRegion.
+  const IndexType &requestedRegionIndex = m_RequestedRegion.GetIndex();
+  const Geometry3D::IndexType &largestPossibleRegionIndex
+    = GetGeometry()->GetLargestPossibleRegion().GetIndex();
 
-	const SizeType& requestedRegionSize = m_RequestedRegion.GetSize();
-	const Geometry3D::SizeType& largestPossibleRegionSize
-		= GetGeometry()->GetLargestPossibleRegion().GetSize();
+  const SizeType& requestedRegionSize = m_RequestedRegion.GetSize();
+  const Geometry3D::SizeType& largestPossibleRegionSize
+    = GetGeometry()->GetLargestPossibleRegion().GetSize();
 
 #if _MSC_VER < 1300
-	for (i=0; i< GImageDimension; ++i)
+  for (i=0; i< GImageDimension; ++i)
 #else
-	for (i=0; i< Geometry3D::GImageDimension; ++i)
+  for (i=0; i< Geometry3D::GImageDimension; ++i)
 #endif
-	{
-		if ( (requestedRegionIndex[i] < largestPossibleRegionIndex[i]) ||
-			((requestedRegionIndex[i] + static_cast<long>(requestedRegionSize[i]))
-				> (largestPossibleRegionIndex[i]+static_cast<long>(largestPossibleRegionSize[i]))))
-		{
-			return false;
-		}
-	}
-	if ((i==4) && 
-		((requestedRegionIndex[i] < 0) ||
-		((requestedRegionIndex[i] + static_cast<long>(requestedRegionSize[i]))
-			> m_NumberOfChannels))
-		)
-	{
-		return false;
-	}
+  {
+    if ( (requestedRegionIndex[i] < largestPossibleRegionIndex[i]) ||
+      ((requestedRegionIndex[i] + static_cast<long>(requestedRegionSize[i]))
+    > (largestPossibleRegionIndex[i]+static_cast<long>(largestPossibleRegionSize[i]))))
+    {
+      return false;
+    }
+  }
+  if ((i==4) && 
+    ((requestedRegionIndex[i] < 0) ||
+    ((requestedRegionIndex[i] + static_cast<long>(requestedRegionSize[i]))
+    > m_NumberOfChannels))
+    )
+  {
+    return false;
+  }
 
-	return true;
+  return true;
 }
 
 //##ModelId=3E1410760114
 void mitk::SlicedData::SetRequestedRegion(itk::DataObject *data)
 {
-	m_UseLargestPossibleRegion=false;
+  m_UseLargestPossibleRegion=false;
 
-	mitk::SlicedData *slicedData;
+  mitk::SlicedData *slicedData;
 
-	slicedData = dynamic_cast<mitk::SlicedData*>(data);
+  slicedData = dynamic_cast<mitk::SlicedData*>(data);
 
-	if (slicedData)
-	{
-		m_RequestedRegion = slicedData->GetRequestedRegion();
-		m_RequestedRegionInitialized = true;
-	}
-	else
-	{
-		// pointer could not be cast back down
-		itkExceptionMacro( << "mitk::SlicedData::SetRequestedRegion(DataObject*) cannot cast " << typeid(data).name() << " to " << typeid(SlicedData*).name() );
-	}
+  if (slicedData)
+  {
+    m_RequestedRegion = slicedData->GetRequestedRegion();
+    m_RequestedRegionInitialized = true;
+  }
+  else
+  {
+    // pointer could not be cast back down
+    itkExceptionMacro( << "mitk::SlicedData::SetRequestedRegion(DataObject*) cannot cast " << typeid(data).name() << " to " << typeid(SlicedData*).name() );
+  }
 }
 
 void mitk::SlicedData::SetRequestedRegion(SlicedData::RegionType *region)
 {
-	m_UseLargestPossibleRegion=false;
+  m_UseLargestPossibleRegion=false;
 
-	if(region!=NULL)
-	{
-		m_RequestedRegion = *region;
-		m_RequestedRegionInitialized = true;
-	}
-	else
-	{
-		// pointer could not be cast back down
-		itkExceptionMacro( << "mitk::SlicedData::SetRequestedRegion(SlicedData::RegionType*) cannot cast " << typeid(region).name() << " to " << typeid(SlicedData*).name() );
-	}
+  if(region!=NULL)
+  {
+    m_RequestedRegion = *region;
+    m_RequestedRegionInitialized = true;
+  }
+  else
+  {
+    // pointer could not be cast back down
+    itkExceptionMacro( << "mitk::SlicedData::SetRequestedRegion(SlicedData::RegionType*) cannot cast " << typeid(region).name() << " to " << typeid(SlicedData*).name() );
+  }
 }
 
 
@@ -179,18 +183,18 @@ mitk::SlicedData::~SlicedData()
 //##ModelId=3E34513B016D
 void mitk::SlicedData::CopyInformation(const itk::DataObject *data)
 {
-	const mitk::SlicedData *slicedData;
+  const mitk::SlicedData *slicedData;
 
-	slicedData = dynamic_cast<const mitk::SlicedData*>(data);
+  slicedData = dynamic_cast<const mitk::SlicedData*>(data);
 
-	if (slicedData)
-	{
-        m_Geometry3D = new Geometry3D(*slicedData->GetGeometry().GetPointer());
-    }
-	else
-	{
-		// pointer could not be cast back down
-		itkExceptionMacro( << "mitk::SlicedData::CopyInformation(const DataObject *data) cannot cast " << typeid(data).name() << " to " << typeid(SlicedData*).name() );
-	}
+  if (slicedData)
+  {
+    m_Geometry3D = new Geometry3D(*slicedData->GetGeometry().GetPointer());
+  }
+  else
+  {
+    // pointer could not be cast back down
+    itkExceptionMacro( << "mitk::SlicedData::CopyInformation(const DataObject *data) cannot cast " << typeid(data).name() << " to " << typeid(SlicedData*).name() );
+  }
 }
 
--- Core/DataStructures/mitkSlicedData.h
+++ Core/DataStructures/mitkSlicedData.h
@@ -26,132 +26,132 @@ class SlicedData : public BaseData
 {
 protected:
 #if !(_MSC_VER < 1300)
-    //##ModelId=3E144ED20061
-	static const int VmitkImageDimension=5;
+  //##ModelId=3E144ED20061
+  static const int VmitkImageDimension=5;
 #endif
 public:
-	mitkClassMacro(SlicedData, BaseData);
+  mitkClassMacro(SlicedData, BaseData);
 
-	/** Region typedef support. A region is used to specify a subset of a @a SlicedData. */
-    //##ModelId=3E143BF1002F
-	typedef itk::ImageRegion<VmitkImageDimension>  RegionType;
+  /** Region typedef support. A region is used to specify a subset of a @a SlicedData. */
+  //##ModelId=3E143BF1002F
+  typedef itk::ImageRegion<VmitkImageDimension>  RegionType;
 
-	/** Index typedef support. An index is used to access pixel values. */
-    //##ModelId=3E144ED10331
-	typedef itk::Index<VmitkImageDimension>  IndexType;
-    //##ModelId=3E144ED10346
-	typedef IndexType::IndexValueType  IndexValueType;
-	  
-	/** Offset typedef support. An offset represent relative position
-	* between indices. */
-    //##ModelId=3E144ED1035A
-	typedef itk::Offset<VmitkImageDimension>  OffsetType;
-    //##ModelId=3E144ED1036D
-	typedef OffsetType::OffsetValueType OffsetValueType;
+  /** Index typedef support. An index is used to access pixel values. */
+  //##ModelId=3E144ED10331
+  typedef itk::Index<VmitkImageDimension>  IndexType;
+  //##ModelId=3E144ED10346
+  typedef IndexType::IndexValueType  IndexValueType;
 
-	/** Size typedef support. A size is used to define region bounds. */
-    //##ModelId=3E144ED10377
-	typedef itk::Size<VmitkImageDimension>  SizeType;
-    //##ModelId=3E144ED10382
-	typedef SizeType::SizeValueType SizeValueType;
-    
-	//##ModelId=3E141028018A
-    //##Documentation
-    //## Update the information for this DataObject so that it can be used as
-    //## an output of a ProcessObject.  This method is used in the pipeline
-    //## mechanism to propagate information and initialize the meta data
-    //## associated with a itk::DataObject.  Any implementation of this method
-    //## in a derived class of itk::DataObject is assumed to call its source's
-    //## ProcessObject::UpdateOutputInformation() which determines modified
-    //## times, LargestPossibleRegions, and any extra meta data like spacing,
-    //## origin, etc.
-	virtual void UpdateOutputInformation();
+  /** Offset typedef support. An offset represent relative position
+  * between indices. */
+  //##ModelId=3E144ED1035A
+  typedef itk::Offset<VmitkImageDimension>  OffsetType;
+  //##ModelId=3E144ED1036D
+  typedef OffsetType::OffsetValueType OffsetValueType;
 
-	//##ModelId=3E14102C029E
-    //##Documentation
-    //## Set the RequestedRegion to the LargestPossibleRegion.  This forces a
-    //## filter to produce all of the output in one execution (i.e. not
-    //## streaming) on the next call to Update().
-	virtual void SetRequestedRegionToLargestPossibleRegion();
+  /** Size typedef support. A size is used to define region bounds. */
+  //##ModelId=3E144ED10377
+  typedef itk::Size<VmitkImageDimension>  SizeType;
+  //##ModelId=3E144ED10382
+  typedef SizeType::SizeValueType SizeValueType;
 
-	//##ModelId=3E14104300AC
-    //##Documentation
-    //## Determine whether the RequestedRegion is outside of the
-    //## BufferedRegion. This method returns true if the RequestedRegion is
-    //## outside the BufferedRegion (true if at least one pixel is outside).
-    //## This is used by the pipeline mechanism to determine whether a filter
-    //## needs to re-execute in order to satisfy the current request.  If the
-    //## current RequestedRegion is already inside the BufferedRegion from the
-    //## previous execution (and the current filter is up to date), then a
-    //## given filter does not need to re-execute
-	virtual bool RequestedRegionIsOutsideOfTheBufferedRegion();
+  //##ModelId=3E141028018A
+  //##Documentation
+  //## Update the information for this DataObject so that it can be used as
+  //## an output of a ProcessObject.  This method is used in the pipeline
+  //## mechanism to propagate information and initialize the meta data
+  //## associated with a itk::DataObject.  Any implementation of this method
+  //## in a derived class of itk::DataObject is assumed to call its source's
+  //## ProcessObject::UpdateOutputInformation() which determines modified
+  //## times, LargestPossibleRegions, and any extra meta data like spacing,
+  //## origin, etc.
+  virtual void UpdateOutputInformation();
 
-	//##ModelId=3E14105B00F7
-    //##Documentation
-    //## @brief Verify that the RequestedRegion is within the
-    //## LargestPossibleRegion.  
-    //## 
-    //## Verify that the RequestedRegion is within the LargestPossibleRegion.
-    //## If the RequestedRegion is not within the LargestPossibleRegion,
-    //## then the filter cannot possibly satisfy the request. This method
-    //## returns true if the request can be satisfied (even if it will be
-    //## necessary to process the entire LargestPossibleRegion) and
-    //## returns false otherwise.  This method is used by
-    //## PropagateRequestedRegion().  PropagateRequestedRegion() throws a
-    //## InvalidRequestedRegionError exception if the requested region is
-    //## not within the LargestPossibleRegion.
-	virtual bool VerifyRequestedRegion();
+  //##ModelId=3E14102C029E
+  //##Documentation
+  //## Set the RequestedRegion to the LargestPossibleRegion.  This forces a
+  //## filter to produce all of the output in one execution (i.e. not
+  //## streaming) on the next call to Update().
+  virtual void SetRequestedRegionToLargestPossibleRegion();
 
-	//##ModelId=3E1410760114
-    //##Documentation
-    //## Set the requested region from this data object to match the requested
-    //## region of the data object passed in as a parameter.  This method is
-    //## implemented in the concrete subclasses of DataObject.
-	virtual void SetRequestedRegion(itk::DataObject *data);
+  //##ModelId=3E14104300AC
+  //##Documentation
+  //## Determine whether the RequestedRegion is outside of the
+  //## BufferedRegion. This method returns true if the RequestedRegion is
+  //## outside the BufferedRegion (true if at least one pixel is outside).
+  //## This is used by the pipeline mechanism to determine whether a filter
+  //## needs to re-execute in order to satisfy the current request.  If the
+  //## current RequestedRegion is already inside the BufferedRegion from the
+  //## previous execution (and the current filter is up to date), then a
+  //## given filter does not need to re-execute
+  virtual bool RequestedRegionIsOutsideOfTheBufferedRegion();
 
-    //##Documentation
-    //## Set the requested region from this data object to match the requested
-    //## region of the data object passed in as a parameter.  This method is
-    //## implemented in the concrete subclasses of DataObject.
-	virtual void SetRequestedRegion(SlicedData::RegionType *region);
+  //##ModelId=3E14105B00F7
+  //##Documentation
+  //## @brief Verify that the RequestedRegion is within the
+  //## LargestPossibleRegion.  
+  //## 
+  //## Verify that the RequestedRegion is within the LargestPossibleRegion.
+  //## If the RequestedRegion is not within the LargestPossibleRegion,
+  //## then the filter cannot possibly satisfy the request. This method
+  //## returns true if the request can be satisfied (even if it will be
+  //## necessary to process the entire LargestPossibleRegion) and
+  //## returns false otherwise.  This method is used by
+  //## PropagateRequestedRegion().  PropagateRequestedRegion() throws a
+  //## InvalidRequestedRegionError exception if the requested region is
+  //## not within the LargestPossibleRegion.
+  virtual bool VerifyRequestedRegion();
 
-    //##ModelId=3E144ED20089
-    //##Documentation
-    //## Get the region object that defines the size and starting index
-    //## for the region of the image requested (i.e., the region of the
-    //## image to be operated on by a filter).
-    virtual const RegionType& GetRequestedRegion() const
-    {
-		return m_RequestedRegion;
-	}
-    //##ModelId=3E14713503B7
-    virtual bool IsSliceSet(int s = 0, int t = 0, int n = 0) const = 0;
-    //##ModelId=3E147163027C
-    virtual bool IsVolumeSet(int t = 0, int n = 0) const = 0;
-    //##ModelId=3E1550BD0272
-    virtual bool IsChannelSet(int n = 0) const = 0;
-    //##ModelId=3E34513B016D
-    virtual void CopyInformation(const itk::DataObject *data);
+  //##ModelId=3E1410760114
+  //##Documentation
+  //## Set the requested region from this data object to match the requested
+  //## region of the data object passed in as a parameter.  This method is
+  //## implemented in the concrete subclasses of DataObject.
+  virtual void SetRequestedRegion(itk::DataObject *data);
 
-    //##Documentation
-    //## @brief Get the number of channels
-	itkGetConstMacro(NumberOfChannels, unsigned int); 
+  //##Documentation
+  //## Set the requested region from this data object to match the requested
+  //## region of the data object passed in as a parameter.  This method is
+  //## implemented in the concrete subclasses of DataObject.
+  virtual void SetRequestedRegion(SlicedData::RegionType *region);
+
+  //##ModelId=3E144ED20089
+  //##Documentation
+  //## Get the region object that defines the size and starting index
+  //## for the region of the image requested (i.e., the region of the
+  //## image to be operated on by a filter).
+  virtual const RegionType& GetRequestedRegion() const
+  {
+    return m_RequestedRegion;
+  }
+  //##ModelId=3E14713503B7
+  virtual bool IsSliceSet(int s = 0, int t = 0, int n = 0) const = 0;
+  //##ModelId=3E147163027C
+  virtual bool IsVolumeSet(int t = 0, int n = 0) const = 0;
+  //##ModelId=3E1550BD0272
+  virtual bool IsChannelSet(int n = 0) const = 0;
+  //##ModelId=3E34513B016D
+  virtual void CopyInformation(const itk::DataObject *data);
+
+  //##Documentation
+  //## @brief Get the number of channels
+  itkGetConstMacro(NumberOfChannels, unsigned int); 
 protected:
-    //##ModelId=3E19EA3300BA
-    SlicedData();
-    //##ModelId=3E19EA3300CE
-    virtual ~SlicedData();
+  //##ModelId=3E19EA3300BA
+  SlicedData();
+  //##ModelId=3E19EA3300CE
+  virtual ~SlicedData();
 
-	//##ModelId=3E143BF10135
-	RegionType          m_RequestedRegion;
-    //##ModelId=3E143BF10149
-	RegionType          m_BufferedRegion;
-    //##ModelId=3E144ED20075
-	bool                m_UseLargestPossibleRegion;
+  //##ModelId=3E143BF10135
+  RegionType          m_RequestedRegion;
+  //##ModelId=3E143BF10149
+  RegionType          m_BufferedRegion;
+  //##ModelId=3E144ED20075
+  bool                m_UseLargestPossibleRegion;
 
-    //##ModelId=3E19EA1200A9
-	unsigned int m_NumberOfChannels;
-  private:
+  //##ModelId=3E19EA1200A9
+  unsigned int m_NumberOfChannels;
+private:
 
 };
 

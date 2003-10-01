9422076d44ac128182d3762d4b391919141aacb8
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2003-10-01 10:52:36 +0000

    changed name from FocusManager to mitkFocusManager and added InvokeEvent(FocusEvent)

---
+++ Core/Controllers/mitkFocusManager.cpp
@@ -0,0 +1,129 @@
+#include "mitkFocusManager.h"
+
+//##ModelId=3EDCAF790194
+mitk::FocusManager::FocusManager()
+{
+	m_Loop = true;//default
+	m_FocusList.clear();
+	m_FocElement = NULL;
+}
+
+mitk::FocusManager::~FocusManager()
+{
+}
+
+//##ModelId=3EDCAF7901C3
+bool mitk::FocusManager::AddElement(FocusElement *element)
+{
+  // Try find  
+  mitk::FocusManager::FocusListIterator position = std::find(m_FocusList.begin(),m_FocusList.end(),element);
+	if (position != m_FocusList.end())
+		return false;
+	
+  m_FocusList.push_back(element);
+	if (m_FocElement ==NULL)
+    m_FocElement = element;
+	return true;
+}
+
+//##ModelId=3EDCAF790230
+bool mitk::FocusManager::RemoveElement(mitk::FocusManager::FocusElement *element)
+{
+	// Try find  
+	mitk::FocusManager::FocusListIterator position = std::find(m_FocusList.begin(),m_FocusList.end(),element);
+	if (position == m_FocusList.end())
+		return false;
+	position = m_FocusList.erase(position);
+	//first delete the one on the position, and store the one afterewards into position
+	if (position == m_FocusList.end())//deleded was the last in row, then take the one before
+		m_FocElement = m_FocusList.back();
+	else
+		m_FocElement = *position;//m_FocElement is equal to the next one in row
+	return true;
+}
+
+//##ModelId=3EDCAF7902BD
+mitk::FocusManager::FocusElement const* mitk::FocusManager::GetFocused() const 
+{
+	return m_FocElement;
+}
+
+//##ModelId=3EDCAF7902FB
+bool mitk::FocusManager::SetFocused(FocusElement *element)
+{
+  if (m_FocElement == element)
+    return true;
+  FocusListIterator position = std::find(m_FocusList.begin(),m_FocusList.end(),element);
+	if (position == m_FocusList.end())//not found
+		return false;
+
+	m_FocElement = *position;
+  ((const itk::Object*)this)->InvokeEvent(FocusEvent());
+	return true;
+}
+
+//##ModelId=3EDCAF790378
+bool mitk::FocusManager::IsLast()
+{
+	return (m_FocElement == m_FocusList.back());
+}
+
+//##ModelId=3EDCAF7903A7
+bool mitk::FocusManager::IsFirst()
+{
+	return (m_FocElement == m_FocusList.front());
+}
+
+//##ModelId=3EDCAF7903D6
+mitk::FocusManager::FocusElement const* mitk::FocusManager::GetFirst() const
+{
+	return m_FocusList.front();
+}
+
+//##ModelId=3EDCAF7A002D
+mitk::FocusManager::FocusElement const* mitk::FocusManager::GetLast() const
+{
+	return m_FocusList.back();
+}
+
+//##ModelId=3EDCAF7A007B
+bool mitk::FocusManager::GoToNext()
+{
+	//find the m_FocElement
+	FocusListIterator position = std::find(m_FocusList.begin(),m_FocusList.end(),m_FocElement);
+	if (position == m_FocusList.end())//not found
+		return false;
+	else if (*position == m_FocusList.back())//last in row
+	{
+		if (m_Loop)
+		{
+			m_FocElement = *(m_FocusList.begin());
+			return true;
+		}
+		else
+		{
+			return false;//last in row and loop == false, so GoToNext == false
+		}
+	}
+	else //not last in row
+	{
+		m_FocElement = *(++position);//increase position and set m_FocElement
+		return true;
+	}
+	return false;
+}
+
+//##ModelId=3EDCAF7A00AA
+//##Documentation
+//## returns an iterator, that points to the 
+//## beginning of the list
+mitk::FocusManager::FocusListIterator mitk::FocusManager::GetIter() 
+{
+	return m_FocusList.begin();
+}
+
+//##ModelId=3EDCAF7A00D8
+void mitk::FocusManager::SetLoop(bool loop)
+{
+	m_Loop = loop;
+}
---
+++ Core/Controllers/mitkFocusManager.h
@@ -0,0 +1,134 @@
+#ifndef MITKFOCUSMANAGER_H_HEADER_INCLUDED_C135A197
+#define MITKFOCUSMANAGER_H_HEADER_INCLUDED_C135A197
+
+#include "mitkCommon.h"
+#include "BaseRenderer.h"
+#include <vector>
+#include <itkEventObject.h>
+
+namespace mitk {
+
+//##ModelId=3EDCAF46031B
+//##Documentation
+//## @brief manages a list of BaseRenderer.
+//## @ingroup Interaction
+//## 
+//## A focuspointer can be set and read.
+//## GoToNext can be used to switch through the list.
+//## if the switch m_Loop is set to true, GetNext loops through the list; after
+//## the last it goes to the first.
+//## if it is not set, it returnes NULL if it steps behind the last Widget.
+  class FocusManager : public itk::Object
+{
+  public:
+  mitkClassMacro(FocusManager, itk::LightObject);
+
+  //##ModelId=3F01770802B7
+	typedef mitk::BaseRenderer FocusElement;
+	//##ModelId=3F01770802C7
+	typedef std::vector<FocusElement*> FocusElementList;
+	//##ModelId=3F01770802D7
+	typedef std::vector<FocusElement*>::iterator FocusListIterator;
+	
+	//##ModelId=3EDCAF790194
+	//##Documentation
+  //## Constructor
+  FocusManager();
+
+  //##Documentation
+  //## Destructor
+  ~FocusManager();
+
+  //##ModelId=3EDCAF7901C3
+  //##Documentation
+  //## Adds the widget into the set of managed Widgets after the focused
+  //## widget and sets the focus to the added one
+  bool AddElement(FocusElement *element);
+
+  //##ModelId=3EDCAF790230
+	//##Documentation
+  //## removes the given widget from the list.
+	//## true if found and removed, else false
+	//## afterwards the focused widget is the one behind the deleted
+	//## or if the deleted was the last, then the one before the deleted
+	//## that way you can delete sequentialy from the back on or from front to back
+	bool RemoveElement(FocusElement *element);
+
+  //##ModelId=3EDCAF7902BD
+  //##Documentation
+  //## returns the focused Widget
+  FocusElement const* GetFocused() const;
+
+  //##ModelId=3EDCAF7902FB
+  //##Documentation
+  //## searches the given Widget in List;
+  //## if found, sets the focus to this widget and returns true
+  bool SetFocused(FocusElement *element);
+
+  //##ModelId=3EDCAF790378
+  //##Documentation
+  //## returns, if this focused widget points behind the end of the List
+  bool IsLast();
+
+  //##ModelId=3EDCAF7903A7
+  //##Documentation
+  //## returns true, if the focused widget is the first in the list
+  bool IsFirst();
+
+  //##ModelId=3EDCAF7903D6
+  //##Documentation
+  //## returns the first widget in list
+  FocusElement const* GetFirst() const;
+
+  //##ModelId=3EDCAF7A002D
+  //##Documentation
+  //## returns the last widget in list
+  FocusElement const* GetLast() const;
+
+	//##ModelId=3EDCAF7A007B
+  //##Documentation
+  //## sets the focus to the next in list
+  //## loops the list, if switch loop is true
+	//## returns true if successful, else false
+  bool GoToNext();
+    
+  //##ModelId=3EDCAF7A00AA
+	//##Documentation
+  //## returns an iterator, that points to the 
+	//## beginning of the list
+	//## no changes are made to the current focused element
+	FocusListIterator GetIter();
+
+  //##ModelId=3EDCAF7A00D8
+	//##Documentation
+  //## Sets the LoopMode.
+	//## if set to true-> the one after the last is the first
+	void SetLoop(bool loop);
+
+private:
+  //##ModelId=3F0185C802C7
+	//##Documentation
+  //## stores the Widgets
+	FocusElementList m_FocusList;
+
+  //##ModelId=3F0185C802D7
+	//##Documentation
+  //## holds the focused Widget
+	FocusElement *m_FocElement;
+
+  //##ModelId=3EDCAF790155
+	//##Documentation
+  //## switch which sets the LoopMode.
+	//## if true, then the next after the last one is the first
+	bool m_Loop;
+};
+
+//##Documentation
+//## @brief connect to this Event to get noticed when the focus changes
+itkEventMacro( FocusEvent , itk::AnyEvent );
+
+} // namespace mitk
+
+
+
+#endif /* MITKFOCUSMANAGER_H_HEADER_INCLUDED_C135A197 */

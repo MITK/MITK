1c9ced1a8c21cb27e3834a4a4f8571aaec6219ee
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2003-11-03 12:23:29 +0000

    added mitkInteractor which is equal to old Roi. Then added raw mitkAfineInteractor. Converted the code to the newe structure.

---
+++ Core/Interactions/mitkInteractor.cpp
@@ -0,0 +1,16 @@
+#include "mitkInteractor.h"
+
+mitk::Interactor::Interactor(std::string type, DataTreeNode* dataTreeNode)
+: mitk::StateMachine(type), m_DataTreeNode(dataTreeNode)
+{
+  if (m_DataTreeNode.IsNotNull())
+    m_DataTreeNode->SetInteractor(this);
+}
+
+
+bool ExecuteSideEffect(int, mitk::StateEvent const* , int , int )
+{
+/*subclasses MUST override this Method! not abstract, because of itkNewMakro can't instantiate a abstract class!*/
+return false;
+}
+
---
+++ Core/Interactions/mitkInteractor.h
@@ -0,0 +1,47 @@
+#ifndef INTERACTOR_H_HEADER_INCLUDED
+#define INTERACTOR_H_HEADER_INCLUDED
+
+#include "mitkCommon.h"
+#include "mitkStateMachine.h"
+#include <mitkDataTreeNode.h>
+#include <string>
+
+namespace mitk {
+
+//##Documentation
+//## @brief Interface for an Interactor, that is held by a DataTreeNode and holds its node
+//## @ingroup Interaction
+class Interactor : public StateMachine
+{
+public:
+
+  mitkClassMacro(Interactor,StateMachine);
+
+  itkNewMacro(Self);
+
+  //##Documentation
+  //## @brief Executes Side-Effects
+  virtual bool ExecuteSideEffect(int sideEffectId, mitk::StateEvent const* stateEvent, int objectEventId, int groupEventId);
+
+protected:
+  //##Documentation
+  //## @brief Constructor
+  //##
+  //## @params dataTreeNode is the node, this Interactor is connected to
+  //## type is the type of StateMachine like declared in the XML-Configure-File
+  //##
+  //## Interactor connects itself to the DataTreeNode-Interactor-pointer through call of SetInteractor(this)
+  Interactor(std::string type, DataTreeNode* dataTreeNode);
+  //##Documentation
+  //## @brief Default Constructor
+  Interactor(){};
+
+  ~Interactor(){};
+
+  //##Documentation
+  //## @brief Pointer to the data, this object handles the Interaction for
+  DataTreeNode::Pointer m_DataTreeNode;
+};
+
+}//namespace mitk
+#endif /* INTERACTOR_H_HEADER_INCLUDED */

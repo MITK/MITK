6e1643234f7798795113bb3a346289ac6e526cf4
mark <mark@nowhere>
2003-11-13 19:14:41 +0000

    added Point3d property

--- Core/DataStructures/mitkProperties.h
+++ Core/DataStructures/mitkProperties.h
@@ -2,9 +2,17 @@
 #define MITKPROPERTIES_H_HEADER_INCLUDED
 
 #include "mitkGenericProperty.h"
+#include "vecmath.h"
 
 namespace mitk {
    typedef GenericProperty<int> IntProperty;
    template class GenericProperty<int>;
+
+   typedef GenericProperty<Point3d> Point3dProperty;
+   template class GenericProperty<Point3d>;
+
+//   typedef GenericProperty<int> IntProperty;
+//   template class GenericProperty<int>;
+            
 }  // namespace mitk
 #endif /* MITKPROPERTIES_H_HEADER_INCLUDED */

43e8f2f6ef90a4ba5a18de0d9bec7ddeb356fbbb
boettger <boettger@nowhere>
2003-12-03 08:38:44 +0000

    contour class for freehand drawing...

---
+++ Core/DataStructures/mitkContour.cpp
@@ -0,0 +1,133 @@
+#include "mitkContour.h"
+
+#include <mitkPointOperation.h>
+#include <mitkOperationActor.h>
+
+//#include <qapplication.h>//@todo because of quickimplementation UpdateAllWidgets
+//#include <qwidgetlist.h>//@todo because of quickimplementation UpdateAllWidgets
+
+#include <mitkInteractionConst.h>
+
+
+//inline void UpdateAllWidgets()//@todo global quickimplementation. is to be changed into Modified Data...
+//{
+//    QWidgetList *list = QApplication::allWidgets();
+//    QWidgetListIt it( *list );         // iterate over the widgets
+//    QWidget * w;
+//    while ( (w=it.current()) != 0 ) {  // for each widget...
+//        ++it;
+//        w->update();
+//    }
+//    delete list;                      // delete the list, not the widgets
+//
+//}
+
+//##ModelId=3F0177E901BD
+mitk::Contour::Contour() :
+m_ContourPath (PathType::New()),
+m_CurrentWindow ( NULL ),
+m_BoundingBox (BoundingBoxType::New()),
+m_Vertices ( BoundingBoxType::PointsContainer::New() )  
+{
+  m_Geometry3D->Initialize(1);
+}
+
+//##ModelId=3F0177E901BE
+mitk::Contour::~Contour()
+{}
+
+void mitk::Contour::AddVertex(mitk::ITKPoint3D newPoint)
+{
+  BoundingBoxType::PointType p;
+  p.CastFrom(newPoint);
+  m_Vertices->InsertElement(m_Vertices->Size(), p);
+
+  ContinuousIndexType idx;
+  idx.CastFrom(newPoint);
+  m_ContourPath->AddVertex(idx);
+}
+
+//##ModelId=3F0177E901BF
+void mitk::Contour::ExecuteOperation(Operation* op)
+{
+  mitk::PointOperation * pointOp = dynamic_cast<mitk::PointOperation*>( op );
+  if ( pointOp != NULL )
+  {
+
+    switch (op->GetOperationType())
+    {
+    case mitk::OpADD:
+      {
+        m_ContourPath = PathType::New();
+        m_ContourPath->Initialize();
+      }
+
+    case mitk::OpMOVE:
+      {
+        //std::cout << "add vertex to contour" << std::endl;
+        AddVertex(pointOp->GetPoint());
+        UpdateOutputInformation();
+        if (m_CurrentWindow != NULL) {
+          m_CurrentWindow->Update();
+        }
+      }
+    }
+  }
+}
+
+//##ModelId=3F0177E901EE
+void mitk::Contour::UpdateOutputInformation()
+{
+  float mitkBounds[6];
+  if (m_Vertices->Size() == 0)  {
+    mitkBounds[0] = 0.0;
+    mitkBounds[1] = 0.0;
+    mitkBounds[2] = 0.0;
+    mitkBounds[3] = 0.0;
+    mitkBounds[4] = 0.0;
+    mitkBounds[5] = 0.0;
+  }
+  else
+  {
+    m_BoundingBox->ComputeBoundingBox();
+    BoundingBoxType::BoundsArrayType tmp = m_BoundingBox->GetBounds();
+    mitkBounds[0] = tmp[0];
+    mitkBounds[1] = tmp[1];
+    mitkBounds[2] = tmp[2];
+    mitkBounds[3] = tmp[3];
+    mitkBounds[4] = tmp[4];
+    mitkBounds[5] = tmp[5];
+  }
+  m_Geometry3D->SetBoundingBox(mitkBounds);
+}
+
+//##ModelId=3F0177E901FB
+void mitk::Contour::SetRequestedRegionToLargestPossibleRegion()
+{
+}
+//##ModelId=3F0177E901FD
+bool mitk::Contour::RequestedRegionIsOutsideOfTheBufferedRegion()
+{
+    return false;
+}
+//##ModelId=3F0177E901FF
+bool mitk::Contour::VerifyRequestedRegion()
+{
+    return false;
+}
+//##ModelId=3F0177E9020B
+void mitk::Contour::SetRequestedRegion(itk::DataObject*)
+{
+}
+
+mitk::Contour::PathType::Pointer 
+mitk::Contour::GetContourPath()
+{
+  return m_ContourPath;
+}
+
+void 
+mitk::Contour::SetCurrentWindow(mitk::RenderWindow* rw)
+{
+  m_CurrentWindow = rw;
+}
---
+++ Core/DataStructures/mitkContour.h
@@ -0,0 +1,74 @@
+#ifndef _MITK_CONTOUR_H_
+#define _MITK_CONTOUR_H_
+
+#include "mitkCommon.h"
+#include "mitkBaseData.h"
+#include "mitkRenderWindow.h"
+
+#include <itkPolyLineParametricPath.h>
+
+namespace mitk {
+
+class Contour : public BaseData
+{
+public:
+  mitkClassMacro(Contour, BaseData);
+
+  itkNewMacro(Self);
+
+  typedef itk::PolyLineParametricPath<3>                        PathType;
+  typedef itk::PolyLineParametricPath<3>                        PathType;
+  typedef PathType::Pointer                                     PathPointer;
+  typedef PathType::ContinuousIndexType                         ContinuousIndexType;
+  typedef PathType::InputType                                   InputType;
+  typedef PathType::OutputType                                  OutputType;
+  typedef PathType::OffsetType                                  OffsetType;
+  typedef itk::BoundingBox<unsigned long, 3, ScalarType>        BoundingBoxType;
+
+
+  //##ModelId=3F0177E901BF
+	void ExecuteOperation(Operation* operation);
+
+
+  void AddVertex(mitk::ITKPoint3D newPoint);
+
+  PathType::Pointer GetContourPath();
+
+
+  void SetCurrentWindow(mitk::RenderWindow* rw);
+
+	//virtual methods, that need to be implemented
+  //##ModelId=3F0177E901EE
+	virtual void UpdateOutputInformation();
+  //##ModelId=3F0177E901FB
+	virtual void SetRequestedRegionToLargestPossibleRegion();
+  //##ModelId=3F0177E901FD
+	virtual bool RequestedRegionIsOutsideOfTheBufferedRegion();
+  //##ModelId=3F0177E901FF
+	virtual bool VerifyRequestedRegion();
+  //##ModelId=3F0177E9020B
+	virtual void SetRequestedRegion(itk::DataObject *data);
+
+protected:
+  //##ModelId=3F0177E901BD
+	Contour();
+
+  //##ModelId=3F0177E901BE
+	virtual ~Contour();
+
+private:
+
+  PathType::Pointer m_ContourPath;
+
+  mitk::RenderWindow* m_CurrentWindow;
+
+  BoundingBoxType::Pointer m_BoundingBox;
+  BoundingBoxType::PointsContainer::Pointer m_Vertices;
+
+};
+
+} // namespace mitk
+
+
+
+#endif //_MITK_CONTOUR_H_

97a1f22b321fee8b890d371a6b97dee7c4c6d626
andre <andre@nowhere>
2004-01-17 22:58:18 +0000

    big endian fixes

--- Utilities/ipPic/ipEndian.c
+++ Utilities/ipPic/ipEndian.c
@@ -1,49 +1,3 @@
-/*****************************************************************************
-
- Copyright (c) 1993-2000,  Div. Medical and Biological Informatics, 
- Deutsches Krebsforschungszentrum, Heidelberg, Germany
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without 
- modification, are permitted provided that the following conditions are met:
-
- - Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
- - Redistributions in binary form must reproduce the above copyright notice, 
-   this list of conditions and the following disclaimer in the documentation 
-   and/or other materials provided with the distribution.
-
- - All advertising materials mentioning features or use of this software must 
-   display the following acknowledgement: 
-          
-     "This product includes software developed by the Div. Medical and 
-      Biological Informatics, Deutsches Krebsforschungszentrum, Heidelberg, 
-      Germany."
-
- - Neither the name of the Deutsches Krebsforschungszentrum nor the names of 
-   its contributors may be used to endorse or promote products derived from 
-   this software without specific prior written permission. 
-
-   THIS SOFTWARE IS PROVIDED BY THE DIVISION MEDICAL AND BIOLOGICAL
-   INFORMATICS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-   IN NO EVENT SHALL THE DIVISION MEDICAL AND BIOLOGICAL INFORMATICS,
-   THE DEUTSCHES KREBSFORSCHUNGSZENTRUM OR CONTRIBUTORS BE LIABLE FOR 
-   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
-   GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
-   IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN 
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-
- Send comments and/or bug reports to:
-   mbi-software@dkfz-heidelberg.de
-
-*****************************************************************************/
-
 
 /*
  * $RCSfile$
@@ -53,16 +7,13 @@
  *   between different machine representations
  *
  * $Log$
- * Revision 1.9  2002/11/13 17:52:59  ivo
- * new ipPic added.
+ * Revision 1.10  2004/01/17 22:58:18  andre
+ * big endian fixes
  *
- * Revision 1.7  2000/05/04 12:52:34  ivo
- * inserted BSD style license
+ * Revision 1.6  2004/01/17 22:56:18  uid9277
+ * big endian fixes
  *
- * Revision 1.6  2000/05/04 12:35:57  ivo
- * some doxygen comments.
- *
- * Revision 1.5  1999/11/27  23:59:45  andre
+ * Revision 1.5  1999/11/27 23:59:45  andre
  * *** empty log message ***
  *
  * Revision 1.4  1999/11/27  19:15:08  andre
@@ -148,9 +99,12 @@ void _ipCpCvtEndian( void *source, void *destination, unsigned long int len, uns
   if( !destination )
     return;
 
-  for( i=0; i<len; i+=bytes )
-    for( i2=0; i2<bytes; i2++ )
-      {
-        ((char *)destination)[i+i2] = ((char *)source)[i+bytes-i2-1];
-      }
+  if( bytes == 1 )
+    memcpy( destination, source, len );
+  else
+    for( i=0; i<len; i+=bytes )
+      for( i2=0; i2<bytes; i2++ )
+        {
+          ((char *)destination)[i+i2] = ((char *)source)[i+bytes-i2-1];
+        }
 }
--- Utilities/ipPic/ipPicGetMem.c
+++ Utilities/ipPic/ipPicGetMem.c
@@ -1,59 +1,8 @@
-/*****************************************************************************
 
- Copyright (c) 1993-2000,  Div. Medical and Biological Informatics, 
- Deutsches Krebsforschungszentrum, Heidelberg, Germany
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without 
- modification, are permitted provided that the following conditions are met:
-
- - Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
- - Redistributions in binary form must reproduce the above copyright notice, 
-   this list of conditions and the following disclaimer in the documentation 
-   and/or other materials provided with the distribution.
-
- - All advertising materials mentioning features or use of this software must 
-   display the following acknowledgement: 
-          
-     "This product includes software developed by the Div. Medical and 
-      Biological Informatics, Deutsches Krebsforschungszentrum, Heidelberg, 
-      Germany."
-
- - Neither the name of the Deutsches Krebsforschungszentrum nor the names of 
-   its contributors may be used to endorse or promote products derived from 
-   this software without specific prior written permission. 
-
-   THIS SOFTWARE IS PROVIDED BY THE DIVISION MEDICAL AND BIOLOGICAL
-   INFORMATICS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-   IN NO EVENT SHALL THE DIVISION MEDICAL AND BIOLOGICAL INFORMATICS,
-   THE DEUTSCHES KREBSFORSCHUNGSZENTRUM OR CONTRIBUTORS BE LIABLE FOR 
-   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
-   GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
-   IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN 
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-
- Send comments and/or bug reports to:
-   mbi-software@dkfz-heidelberg.de
-
-*****************************************************************************/
-
-
-/** @file 
- * creates a PicStructure from memory block
- */
-#ifndef lint
-  static char *what = { "@(#)ipPicGetMem\t\tDKFZ (Dept. MBI)\t"__DATE__"\t$Revision$" };
-#endif
-
-#include "ipPic.h"
-/** creates a PicStructure from memory block
+/*
+ *--------------------------------------------------------------------
+ * DESCRIPTION
+ *   creates a PicStructure from memory block
  *
  * FUNCTION DECLARATION
  *  
@@ -64,9 +13,17 @@
  * AUTHOR & DATE
  *  Harald Evers   15.7.98
  *
+ * UPDATES
+ *
+ *--------------------------------------------------------------------
+ *  COPYRIGHT (c) 1998 by DKFZ (Dept. MBI) Heidelberg, FRG
  */
 
+#ifndef lint
+  static char *what = { "@(#)ipPicGetMem\t\tDKFZ (Dept. MBI)\t"__DATE__"\t$Revision$" };
+#endif
 
+#include "ipPic.h"
 
 ipPicDescriptor *ipPicGetMem( ipUInt1_t *mem_pic )
 {
@@ -74,6 +31,9 @@ ipPicDescriptor *ipPicGetMem( ipUInt1_t *mem_pic )
   ipUInt4_t        len;
   ipUInt4_t        to_read;
   ipUInt1_t       *mem_ptr = mem_pic;
+
+  if( !mem_pic )
+    return( NULL );
   
   pic = ipPicNew();
   ipPicClear( pic );
@@ -85,21 +45,31 @@ ipPicDescriptor *ipPicGetMem( ipUInt1_t *mem_pic )
       return( NULL );
     }
 
-  memcpy( &len,  mem_ptr, sizeof(ipUInt4_t) );
+  /*memcpy( &len,  mem_ptr, sizeof(ipUInt4_t) );*/
+  ipCpFromLE( mem_ptr, &len, sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
   mem_ptr += sizeof(ipUInt4_t);
-  memcpy( &(pic->type),  mem_ptr, sizeof(ipUInt4_t) );
+
+  /*memcpy( &(pic->type),  mem_ptr, sizeof(ipUInt4_t) );*/
+  ipCpFromLE( mem_ptr, &(pic->type), sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
   mem_ptr += sizeof(ipUInt4_t);
-  memcpy( &(pic->bpe),  mem_ptr, sizeof(ipUInt4_t) );
+
+  /*memcpy( &(pic->bpe),  mem_ptr, sizeof(ipUInt4_t) );*/
+  ipCpFromLE( mem_ptr, &(pic->bpe), sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
   mem_ptr += sizeof(ipUInt4_t);
-  memcpy( &(pic->dim),  mem_ptr, sizeof(ipUInt4_t) );
+
+  /*memcpy( &(pic->dim),  mem_ptr, sizeof(ipUInt4_t) );*/
+  ipCpFromLE( mem_ptr, &(pic->dim), sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
   mem_ptr += sizeof(ipUInt4_t);
-  memcpy( &(pic->n),  mem_ptr, pic->dim * sizeof(ipUInt4_t) );
+
+  /*memcpy( &(pic->n),  mem_ptr, pic->dim * sizeof(ipUInt4_t) );*/
+  ipCpFromLE( mem_ptr, &(pic->n), pic->dim*sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
   mem_ptr += pic->dim*sizeof(ipUInt4_t);
 
   to_read = len -        3 * sizeof(ipUInt4_t)
                 - pic->dim * sizeof(ipUInt4_t);
   pic->info->tags_head = _ipPicReadTagsMem( pic->info->tags_head, 
-					    to_read, &mem_ptr );
+					    to_read, &mem_ptr,
+                                            ipPicEncryptionType(pic) );
 
   pic->info->write_protect = ipFalse;
   pic->data = malloc( _ipPicSize(pic) );
@@ -108,14 +78,15 @@ ipPicDescriptor *ipPicGetMem( ipUInt1_t *mem_pic )
     pic->info->pixel_start_in_file = ftell( infile );
   */
   
-  memcpy( pic->data,  mem_ptr, pic->bpe/8*_ipPicElements(pic) );
+  /*memcpy( pic->data,  mem_ptr, pic->bpe/8*_ipPicElements(pic) );*/
+  ipCpFromLE( mem_ptr, pic->data, _ipPicSize(pic), pic->bpe/8 );
 
   return( pic );
 }
 
 _ipPicTagsElement_t *
 _ipPicReadTagsMem( _ipPicTagsElement_t *head, ipUInt4_t bytes_to_read, 
-		   ipUInt1_t **mem_ptr )
+		   ipUInt1_t **mem_ptr, char encryption_type )
 {
   while( bytes_to_read > 0 )
     {
@@ -128,16 +99,24 @@ _ipPicReadTagsMem( _ipPicTagsElement_t *head, ipUInt4_t bytes_to_read,
       tsv->tag[_ipPicTAGLEN] = '\0';
       *mem_ptr += _ipPicTAGLEN;
 
-      memcpy( &len,  *mem_ptr, sizeof(ipUInt4_t) );
+      /*memcpy( &len,  *mem_ptr, sizeof(ipUInt4_t) );*/
+      ipCpFromLE( *mem_ptr, &len, sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
       *mem_ptr += sizeof(ipUInt4_t);
 
-      memcpy( &(tsv->type), *mem_ptr, sizeof(ipUInt4_t) );
+      /*memcpy( &(tsv->type), *mem_ptr, sizeof(ipUInt4_t) );*/
+      ipCpFromLE( *mem_ptr, &(tsv->type), sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
       *mem_ptr += sizeof(ipUInt4_t);
-      memcpy( &(tsv->bpe), *mem_ptr, sizeof(ipUInt4_t) );
+
+      /*memcpy( &(tsv->bpe), *mem_ptr, sizeof(ipUInt4_t) );*/
+      ipCpFromLE( *mem_ptr, &(tsv->bpe), sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
       *mem_ptr += sizeof(ipUInt4_t);
-      memcpy( &(tsv->dim), *mem_ptr, sizeof(ipUInt4_t) );
+
+      /*memcpy( &(tsv->dim), *mem_ptr, sizeof(ipUInt4_t) );*/
+      ipCpFromLE( *mem_ptr, &(tsv->dim), sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
       *mem_ptr += sizeof(ipUInt4_t);
-      memcpy( &(tsv->n), *mem_ptr, tsv->dim*sizeof(ipUInt4_t) );
+
+      /*memcpy( &(tsv->n), *mem_ptr, tsv->dim*sizeof(ipUInt4_t) );*/
+      ipCpFromLE( *mem_ptr, &(tsv->n), tsv->dim*sizeof(ipUInt4_t), sizeof(ipUInt4_t) );
       *mem_ptr += tsv->dim*sizeof(ipUInt4_t);
 
       if( tsv->type == ipPicTSV )
@@ -146,7 +125,8 @@ _ipPicReadTagsMem( _ipPicTagsElement_t *head, ipUInt4_t bytes_to_read,
           tsv->value = _ipPicReadTagsMem( tsv->value,
 					  len -        3 * sizeof(ipUInt4_t)
   					      - tsv->dim * sizeof(ipUInt4_t),
-					  mem_ptr );
+					  mem_ptr,
+                                          encryption_type );
         }
       else
         {
@@ -159,19 +139,31 @@ _ipPicReadTagsMem( _ipPicTagsElement_t *head, ipUInt4_t bytes_to_read,
             tsv->bpe = 8;
 
           if( tsv->type == ipPicASCII )
-            tsv->value = malloc( elements+1 * tsv->bpe / 8 );
+            tsv->value = malloc( (elements+1) * (tsv->bpe / 8) );
           else
             tsv->value = malloc( elements * tsv->bpe / 8 );
 
-	  memcpy( tsv->value,  *mem_ptr, tsv->bpe/8*_ipPicTSVElements(tsv) );
+	  /*memcpy( tsv->value,  *mem_ptr, tsv->bpe/8*_ipPicTSVElements(tsv) );*/
+          ipCpFromLE( *mem_ptr, tsv->value, _ipPicTSVSize(tsv), tsv->bpe/8 );
 	  *mem_ptr += tsv->bpe/8*_ipPicTSVElements(tsv);
 	  
           if( tsv->type == ipPicASCII )
             ((char *)(tsv->value))[elements] = '\0';
 
-          /*
-	    if( encryption_type == 'e' ..
-	  */
+         if( encryption_type == 'e'
+             && ( tsv->type == ipPicASCII
+                  || tsv->type == ipPicNonUniform ) )
+           {
+             int i;
+         
+             for( i = 0; i < elements * tsv->bpe/8; i++ )
+               {
+                 if( i % 2 )
+                   ((ipUInt1_t *)tsv->value)[i] = (255 - ((ipUInt1_t *)tsv->value)[i]) ^ i;
+                 else
+                   ((ipUInt1_t *)tsv->value)[i] = ((ipUInt1_t *)tsv->value)[i] ^ i;
+               }
+           }
         }
 
       head = _ipPicInsertTag( head, tsv );

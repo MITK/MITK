cbec688a3e16d22aa470cf4c5473d2440159277b
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2004-01-22 15:29:48 +0000

    Operation that holds all necessary information about an operation on a line

---
+++ Core/DataStructures/mitkLineOperation.cpp
@@ -0,0 +1,25 @@
+#include "mitkLineOperation.h"
+
+mitk::LineOperation::LineOperation(OperationType operationType,
+									int cellId, int pIdA, int pIdB)
+: mitk::Operation(operationType), m_CellId(cellId), m_PIdA(pIdA), m_PIdB(pIdB)
+{}
+
+mitk::LineOperation::~LineOperation()
+{
+}
+
+int mitk::LineOperation::GetCellId()
+{
+	return m_CellId;
+}
+
+int mitk::LineOperation::GetPIdA()
+{
+	return m_PIdA;
+}
+
+int mitk::LineOperation::GetPIdB()
+{
+	return m_PIdB;
+}
---
+++ Core/DataStructures/mitkLineOperation.h
@@ -0,0 +1,42 @@
+#ifndef MITKLINEOPERATION
+#define MITKLINEOPERATION
+
+#include "mitkCommon.h"
+#include "mitkOperation.h"
+#include "mitkVector.h"
+
+
+namespace mitk {
+
+//##Documentation
+//## @brief Operation, that holds everything necessary for an operation on a line.
+//##
+//## @ingroup Undo
+//##
+//## Stores everything for de-/ selecting, inserting , moving and removing a line.
+class LineOperation : public mitk::Operation
+{
+  public:
+	//##Documentation
+	//##@brief constructor.
+	//##
+	//## @params
+	//## operationType is the type of that operation (see mitkOperation.h; e.g. move or add; Information for StateMachine::ExecuteOperation());
+	//## point is the information of the point to add or is the information to change a point into; index is e.g. the position in a
+	//## list which describes the element to change
+    LineOperation(OperationType operationType,
+					int cellId, int pIdA, int pIdB);
+
+    virtual ~LineOperation();
+
+	int GetCellId();
+	int GetPIdA();
+  int GetPIdB();
+
+  private:
+  int m_CellId;
+	int m_PIdA;
+  int m_PIdB;
+};
+}//namespace mitk
+#endif /* MITKLINEOPERATION*/

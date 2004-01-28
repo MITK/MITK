4ece87feacd09165fd539ccc3112789ca12ea5fb
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2004-01-28 15:30:06 +0000

    mitk::KeyEvent no longer inherits QKeyEvent

--- Core/Interactions/mitkEventTypedefs.h
+++ Core/Interactions/mitkEventTypedefs.h
@@ -4,21 +4,26 @@
 #include <qgl.h>
 #include <assert.h>
 #include <qptrlist.h> 
-
+#include <string>
 namespace mitk {
     
 typedef ::QMouseEvent MouseEvent;
 typedef ::QWheelEvent WheelEvent;
-class KeyEvent : public ::QKeyEvent
-{
+
+class KeyEvent {
 
     int m_X;
     int m_Y;
     int m_GlobalX;
     int m_GlobalY;
-public:
-    KeyEvent ( Type type, int key, int ascii, int state, const QString & text = QString::null, bool autorep = FALSE, ushort count = 1, int x=0, int y=0, int globalX=0, int globalY=0 ) :
-        ::QKeyEvent(type, key, ascii, state, text, autorep, count), m_X(x), m_Y(y), m_GlobalX(globalX), m_GlobalY(globalY) 
+    int m_State;
+    int m_Ascii;
+    int m_Type;
+    int m_Key;
+    std::string m_Text;
+  public:
+    KeyEvent ( int type, int key, int ascii, int state, const std::string & text = "", bool autorep = FALSE, ushort count = 1, int x=0, int y=0, int globalX=0, int globalY=0 )  :
+ m_X(x), m_Y(y), m_GlobalX(globalX), m_GlobalY(globalY) , m_State(state),m_Ascii(ascii),m_Text(text),m_Type(type),m_Key(key)
     {
     };
     ~KeyEvent()
@@ -40,6 +45,26 @@ public:
     { 
         return m_GlobalY;
     }
+
+    bool isAccepted() const {
+      return true;
+    }
+    int state() const {
+      return m_State;
+    }
+    int ascii() const {
+      return m_Ascii;
+    }
+    const char* text() const {
+      return m_Text.c_str();
+    }
+     int type() const {
+      return m_Type;
+    }
+      int key() const {
+      return m_Key;
+    }
+    
 };
 
 #define mitkEnum Qt

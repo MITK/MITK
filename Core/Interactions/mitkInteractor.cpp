1df9f57e4403dfe48dfe39cc2c898b77681cce6c
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2004-02-04 21:06:25 +0000

    inserted Mode of Interactor

--- Core/Interactions/mitkInteractor.cpp
+++ Core/Interactions/mitkInteractor.cpp
@@ -2,8 +2,20 @@
 #include <mitkDataTreeNode.h>
 
 mitk::Interactor::Interactor(const char * type, DataTreeNode* dataTreeNode)
-: mitk::StateMachine(type), m_DataTreeNode(dataTreeNode)
+: mitk::StateMachine(type), m_DataTreeNode(dataTreeNode), m_Mode(SMMode::SELECTED)
 {
   if (m_DataTreeNode != NULL)
     m_DataTreeNode->SetInteractor(this);
 }
+
+void mitk::Interactor::SetMode(int mode)
+{
+  if ((-1<mode)&&(mode<3))//if mode in range from 0 to 2
+    m_Mode = mode;
+}
+
+const int mitk::Interactor::GetMode() const
+{
+  return m_Mode;
+}
+
--- Core/Interactions/mitkInteractor.h
+++ Core/Interactions/mitkInteractor.h
@@ -16,10 +16,28 @@ class DataTreeNode;
 //##
 //## The Interactor is held with a SmartPointer by a DataTreeNode 
 //## and holds its Node with a Pointer. That way Smartpointer doesn't build a circle.
+//## Different Modes: In order to work with hierarchical StateMachines and not to send Events to all StateMachines, a StateMachine can be 
+//## in three different modes: 
+//## DESELECTED: this statemachine doesn't wait for an event
+//## SELECTED: this statemachine just has handled an event and waits for the next one
+//## SUBSELECTED: a hierarchical lower statemachine has just handled an event so this statemachine wants to pass the event to this machine
+//## Guidelines for the modevalues: Selected if the coresponding data is selected, deselected if deselect of data, subselect if submachine is selected
+//## in moving the machine is selected. After a new insert the machine is selected, since the data is also selected
 //## @ingroup Interaction
 class Interactor : public StateMachine
 {
 public:
+
+  //##Documentation
+  //##@brief Enumeration of the different modes an Interactor can be into. 
+  //## See class documentation for further details
+  enum SMMode
+  {
+    DESELECTED = 0,
+    SELECTED,
+    SUBSELECTED
+  };
+
   mitkClassMacro(Interactor,StateMachine);
 
   //##Documentation
@@ -36,6 +54,15 @@ public:
   ~Interactor(){}
 
   //##Documentation
+  //## @brief Set the Mode of the Interactor to Selected, Subselected or Deselected
+  //## use enum SMMode for parameter
+  void SetMode(int mode);
+
+  //##Documentation
+  //## @brief Get the Mode of the Interactor. Use enum SMMode for return parameter
+  const int GetMode() const;
+
+  //##Documentation
   //## @brief Executes Side-Effects
   virtual bool ExecuteSideEffect(int sideEffectId, mitk::StateEvent const* stateEvent, int objectEventId, int groupEventId) = 0;
 
@@ -43,6 +70,10 @@ protected:
   //##Documentation
   //## @brief Pointer to the data, this object handles the Interaction for
   DataTreeNode* m_DataTreeNode;
+
+  //##Documentation
+  //## @brief Mode of Selection
+  int m_Mode;
 
 };
 

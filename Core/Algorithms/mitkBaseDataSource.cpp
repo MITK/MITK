abd3d652d2565297868e381b0554677cf1a2f7c7
max <max@nowhere>
2004-02-06 14:43:45 +0000

    Added documentation. Made MakeOutput(idx) function pure virtual.

--- Core/Algorithms/mitkBaseDataSource.cpp
+++ Core/Algorithms/mitkBaseDataSource.cpp
@@ -3,37 +3,25 @@
 
 mitk::BaseDataSource::BaseDataSource()
 {
-    // Create the output.
-    //OutputType::Pointer output = static_cast<OutputType*> ( this->MakeOutput( 0 ).GetPointer() );
+    OutputType::Pointer output = static_cast<OutputType*> ( this->MakeOutput( 0 ).GetPointer() );
     Superclass::SetNumberOfRequiredOutputs( 1 );
-    //Superclass::SetNthOutput( 0, output.GetPointer() );
+    Superclass::SetNthOutput( 0, output.GetPointer() );
 }
+
+
 
 mitk::BaseDataSource::~BaseDataSource()
 {
 }
 
 
-itk::DataObject::Pointer mitk::BaseDataSource::MakeOutput ( unsigned int idx )
-{
-    //this doesn't work, since we can not instantiate a base data
-    //return OutputType::New().getPointer();
-    return NULL;
-}
 
-
-
-
-/**
- * Allows to set the output of the base data source. According to the itk
- * this method is outdated and should not be used. Instead GraftOutput(...)
- * should be used.
- * @param output the intended output of the vessel graph source
- */
 void mitk::BaseDataSource::SetOutput( OutputType* output )
 {
     this->SetNthOutput( 0, output );
 }
+
+
 
 void mitk::BaseDataSource::SetOutput( unsigned int idx, OutputType* output )
 {
@@ -42,10 +30,6 @@ void mitk::BaseDataSource::SetOutput( unsigned int idx, OutputType* output )
 
 
 
-/**
- * Returns the output with index 0 of the base data source
- * @returns the output
- */
 mitk::BaseDataSource::OutputType* mitk::BaseDataSource::GetOutput()
 {
     if ( this->GetNumberOfOutputs() < 1 )
@@ -56,11 +40,7 @@ mitk::BaseDataSource::OutputType* mitk::BaseDataSource::GetOutput()
 }
 
 
-/**
- * Returns the n'th output of the base data source
- * @param idx the index of the wanted output
- * @returns the output with index idx.
- */
+
 mitk::BaseDataSource::OutputType* mitk::BaseDataSource::GetOutput ( unsigned int idx )
 {
     return static_cast<OutputType*> ( Superclass::GetOutput( idx ) );
--- Core/Algorithms/mitkBaseDataSource.h
+++ Core/Algorithms/mitkBaseDataSource.h
@@ -7,24 +7,34 @@
 namespace mitk
 {
 
+/**
+ * @brief Superclass of all classes generating base datas (instances of class
+ * mitk::BaseData) as output. 
+ * @ingroup Process
+ * In itk and vtk the generated result of a ProcessObject is only guaranteed
+ * to be up-to-date, when Update() of the ProcessObject or the generated
+ * DataObject is called immediately before access of the data stored in the
+ * DataObject. This is also true for subclasses of mitk::BaseProcess and thus
+ * for mitk::BaseDataSource.
+ */
 class BaseDataSource : public BaseProcess
 {
 public:
     mitkClassMacro( BaseDataSource, BaseProcess );
-
-    itkNewMacro( Self );
 
     typedef mitk::BaseData OutputType;
 
     typedef OutputType::Pointer OutputTypePointer;
     
     /**
-     * Allocates a new output object and returns it. Currently the
-     * index idx is not evaluated.
+     * Allocates a new output object and returns it. This function
+     * is pure virtual because mitk::BaseData contains pure virtual functions
+     * and can not directly be instantiated. This must be done by subclasses, which
+     * know which subclasses of mitk::BaseData they want to instantiate.
      * @param idx the index of the output for which an object should be created
      * @returns the new object
      */
-    virtual itk::DataObject::Pointer MakeOutput ( unsigned int idx );
+    virtual itk::DataObject::Pointer MakeOutput ( unsigned int idx ) = 0;
 
     /**
      * Allows to set the output of the base data source. 

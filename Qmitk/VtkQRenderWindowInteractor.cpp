1489b4cad3d6f3506c589099ff009289625cfd55
mvetter <mvetter@nowhere>
2004-02-17 18:31:27 +0000

    rename mitkVtkQRenderWindowInteractor.h and mitkVtkQRenderWindowInteractor.cpp to VtkQRenderWindowInteractor.h and VtkQRenderWindowInteractor.cpp

---
+++ Qmitk/VtkQRenderWindowInteractor.cpp
@@ -0,0 +1,336 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "VtkQRenderWindowInteractor.h"
+
+#include "vtkInteractorStyle.h"
+#include "vtkActor.h"
+#include "vtkObjectFactory.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkCommand.h"
+#include "vtkRenderWindow.h"
+#include "vtkVersion.h"
+
+//assume vtk-version 4.0, if VTK_VERSION is undefined
+#ifndef VTK_VERSION
+#define VTK_MAJOR_VERSION 4
+#define VTK_MINOR_VERSION 0
+#endif
+
+//----------------------------------------------------------------------------
+//##ModelId=3E6D600F00E1
+VtkQRenderWindowInteractor* VtkQRenderWindowInteractor::New()
+{
+    // First try to create the object from the vtkObjectFactory
+    vtkObject* ret = vtkObjectFactory::CreateInstance("VtkQRenderWindowInteractor");
+    if(ret)
+    {
+        return (VtkQRenderWindowInteractor*)ret;
+    }
+    // If the factory was unable to create the object, then create it here.
+    return new VtkQRenderWindowInteractor;
+}
+
+//##ModelId=3E6D600F00CC
+void (*VtkQRenderWindowInteractor::ClassExitMethod)(void *) = (void (*)(void *))NULL;
+//##ModelId=3E6D600F00E0
+void *VtkQRenderWindowInteractor::ClassExitMethodArg = (void *)NULL;
+//##ModelId=3E6D600F00D6
+void (*VtkQRenderWindowInteractor::ClassExitMethodArgDelete)(void *) = (void (*)(void *))NULL;
+
+//// Construct object so that light follows camera motion.
+//##ModelId=3E6D600F0164
+//##ModelId=3E6D600F0177
+VtkQRenderWindowInteractor::VtkQRenderWindowInteractor() : m_MitkRenderer(NULL)
+{
+    //static int timerId           = 1;
+    //this->WindowId           = 0;
+    //this->TimerId            = timerId++;
+    //this->InstallMessageProc = 1;
+    this->MouseInWindow = 0;
+}
+
+//##ModelId=3E6D600F016C
+VtkQRenderWindowInteractor::~VtkQRenderWindowInteractor()
+{
+    
+}
+
+// Begin processing keyboard strokes.
+//##ModelId=3E6D600F00F5
+void VtkQRenderWindowInteractor::Initialize()
+{
+    int *size;
+    
+    // make sure we have a RenderWindow and camera
+   if (this->Initialized)
+    {
+        return;
+    }
+    this->Initialized = 1;
+if (GetRenderWindow() != NULL) {    
+    size = GetRenderWindow()->GetSize();
+    
+    this->Enable();
+    this->Size[0] = size[0];
+    this->Size[1] = size[1];}
+
+}
+
+//##ModelId=3E6D600F00F7
+void VtkQRenderWindowInteractor::Enable()
+{
+    if (this->GetEnabled())
+    {
+        return;
+    }
+    if (this->InstallMessageProc)
+    {
+        //// add our callback
+        //ren = (vtkWin32OpenGLRenderWindow *)(this->RenderWindow);
+        //this->OldProc = (WNDPROC)GetWindowLong(this->WindowId,GWL_WNDPROC);
+        //tmp=(vtkWin32OpenGLRenderWindow *)GetWindowLong(this->WindowId,4);
+        //// watch for odd conditions
+        //if (tmp != ren)
+        //  {
+        //  // OK someone else has a hold on our event handler
+        //  // so lets have them handle this stuff
+        //  // well send a USER message to the other
+        //  // event handler so that it can properly
+        //  // call this event handler if required
+        //  CallWindowProc(this->OldProc,this->WindowId,WM_USER+12,24,(LONG)vtkHandleMessage);
+        //  }
+        //else
+        //  {
+        //  SetWindowLong(this->WindowId,GWL_WNDPROC,(LONG)vtkHandleMessage);
+        //  }
+        // in case the size of the window has changed while we were away
+        int *size;
+        size = GetRenderWindow()->GetSize();
+        this->Size[0] = size[0];
+        this->Size[1] = size[1];
+    }
+    this->Enabled = 1;
+    this->Modified();
+}
+
+
+//##ModelId=3E6D600F00FF
+void VtkQRenderWindowInteractor::Disable()
+{
+    if (!this->GetEnabled())
+    {
+        return;
+    }
+    
+    if (this->InstallMessageProc && this->GetEnabled())
+    {
+        //// we need to release any hold we have on a windows event loop
+        //vtkWin32OpenGLRenderWindow *ren;
+        //ren = (vtkWin32OpenGLRenderWindow *)(this->RenderWindow);
+        //tmp = (vtkWin32OpenGLRenderWindow *)GetWindowLong(this->WindowId,4);
+        //// watch for odd conditions
+        //if ((tmp != ren) && (ren != NULL))
+        //  {
+        //  // OK someone else has a hold on our event handler
+        //  // so lets have them handle this stuff
+        //  // well send a USER message to the other
+        //  // event handler so that it can properly
+        //  // call this event handler if required
+        //  CallWindowProc(this->OldProc,this->WindowId,WM_USER+14,28,(LONG)this->OldProc);
+        //  }
+        //else
+        //  {
+        //  SetWindowLong(this->WindowId,GWL_WNDPROC,(LONG)this->OldProc);
+        //  }
+    }
+    this->Enabled = 0;
+    this->Modified();
+}
+
+//##ModelId=3E6D600F0113
+void VtkQRenderWindowInteractor::TerminateApp(void)
+{
+    
+}
+
+//##ModelId=3E6D600F0115
+int VtkQRenderWindowInteractor::CreateTimer(int timertype)
+{
+    if (timertype==VTKI_TIMER_FIRST)
+    {
+        return TimerId=startTimer(10);
+    }
+    return 1;
+}
+
+//##ModelId=3E6D600F011D
+int VtkQRenderWindowInteractor::DestroyTimer(void)
+{
+    killTimer(TimerId);
+    return 1;
+}
+
+
+//##ModelId=3E6D600F014F
+void VtkQRenderWindowInteractor::SetSize(int w, int h)
+{
+    Resize(w, h);
+}
+
+//##ModelId=3E6D600F0158
+void VtkQRenderWindowInteractor::SetSize(int size[2])
+{
+    Resize(size[0], size[1]);
+}
+
+//##ModelId=3E6D600F016D
+void VtkQRenderWindowInteractor::Resize(int w, int h)
+{
+    UpdateSize(w, h);
+    if(InteractorStyle==NULL) return;
+    if(InteractorStyle->IsA("vtkInteractorStyle"))
+    {
+        vtkInteractorStyle* vtkinteractorstyle=(vtkInteractorStyle*)InteractorStyle;
+        SetEventSize(w, h);
+        SetEventPosition(0, Size[1] - 0 - 1);
+        if (this->GetEnabled())
+        {
+#if ((VTK_MAJOR_VERSION>4) || ((VTK_MAJOR_VERSION==4) && (VTK_MINOR_VERSION>=2)))
+            InvokeEvent(vtkCommand::ConfigureEvent,NULL);
+#else
+            vtkinteractorstyle->OnConfigure(w,h);
+#endif
+        }
+    }
+}
+
+// Specify the default function to be called when an interactor needs to exit.
+// This callback is overridden by an instance ExitMethod that is defined.
+//##ModelId=3E6D600F013A
+void
+VtkQRenderWindowInteractor::SetClassExitMethod(void (*f)(void *),void *arg)
+{
+    if ( f != VtkQRenderWindowInteractor::ClassExitMethod
+        || arg != VtkQRenderWindowInteractor::ClassExitMethodArg)
+    {
+        // delete the current arg if there is a delete method
+        if ((VtkQRenderWindowInteractor::ClassExitMethodArg)
+            && (VtkQRenderWindowInteractor::ClassExitMethodArgDelete))
+        {
+            (*VtkQRenderWindowInteractor::ClassExitMethodArgDelete)
+                (VtkQRenderWindowInteractor::ClassExitMethodArg);
+        }
+        VtkQRenderWindowInteractor::ClassExitMethod = f;
+        VtkQRenderWindowInteractor::ClassExitMethodArg = arg;
+        
+        // no call to this->Modified() since this is a class member function
+    }
+}
+
+
+// Set the arg delete method.  This is used to free user memory.
+//##ModelId=3E6D600F0144
+void
+VtkQRenderWindowInteractor::SetClassExitMethodArgDelete(void (*f)(void *))
+{
+    if (f != VtkQRenderWindowInteractor::ClassExitMethodArgDelete)
+    {
+        VtkQRenderWindowInteractor::ClassExitMethodArgDelete = f;
+        
+        // no call to this->Modified() since this is a class member function
+    }
+}
+
+//##ModelId=3E6D600F00ED
+void VtkQRenderWindowInteractor::PrintSelf(ostream& os, vtkIndent indent)
+{
+    vtkRenderWindowInteractor::PrintSelf(os,indent);
+    os << indent << "InstallMessageProc: " << this->InstallMessageProc << endl;
+}
+
+//##ModelId=3E6D600F0147
+void VtkQRenderWindowInteractor::ExitCallback()
+{
+    if (this->HasObserver(vtkCommand::ExitEvent))
+    {
+        this->InvokeEvent(vtkCommand::ExitEvent,NULL);
+    }
+    else if (this->ClassExitMethod)
+    {
+        (*this->ClassExitMethod)(this->ClassExitMethodArg);
+    }
+    else
+    {
+        this->TerminateApp();
+    }
+}
+
+
+//##ModelId=3E6D600F011F
+void VtkQRenderWindowInteractor::MousePressEvent(mitk::MouseEvent *me)
+{
+}
+
+//##ModelId=3E6D600F0128
+void VtkQRenderWindowInteractor::MouseReleaseEvent(mitk::MouseEvent *me)
+{
+}
+
+//##ModelId=3E6D600F0130
+void VtkQRenderWindowInteractor::MouseMoveEvent(mitk::MouseEvent *me)
+{
+}
+
+//##ModelId=3E6D600F0133
+void VtkQRenderWindowInteractor::KeyPressEvent(mitk::KeyEvent *ke)
+{
+}
+
+//##ModelId=3E6E4BB2027B
+void VtkQRenderWindowInteractor::timerEvent ( QTimerEvent * )
+{
+    if (!this->GetEnabled())
+    {
+        return;
+    }
+#if ((VTK_MAJOR_VERSION>4) || ((VTK_MAJOR_VERSION==4) && (VTK_MINOR_VERSION>=2)))
+    InvokeEvent(vtkCommand::TimerEvent,NULL);
+#else
+    this->InteractorStyle->OnTimer();
+#endif
+}
+
+//##
+VTK_CREATE_CREATE_FUNCTION(VtkQRenderWindowInteractor);
+
+VtkQRenderWindowInteractorFactory*  VtkQRenderWindowInteractorFactory::m_Factory = NULL;
+
+//##
+VtkQRenderWindowInteractorFactory::VtkQRenderWindowInteractorFactory( )
+: vtkObjectFactory() {
+    
+  if ( !m_Factory ) {
+
+    vtkObjectFactory::RegisterFactory( this );
+    RegisterOverride( "vtkRenderWindowInteractor", "VtkQRenderWindowInteractor", "RenderWindowInteractor for mitk and QT", 1, vtkObjectFactoryCreateVtkQRenderWindowInteractor );
+  }  
+  
+  m_Factory = this;
+}
+
+//##
+const char* VtkQRenderWindowInteractorFactory::GetVTKSourceVersion()
+{
+  return VTK_SOURCE_VERSION;
+}
+
+//##
+const char* VtkQRenderWindowInteractorFactory::GetDescription()
+{
+  return "Factory for VtkQRenderWindowInteractor";
+}
+
+VtkQRenderWindowInteractorFactory vtkQRenderWindowInteractorFactory;
---
+++ Qmitk/VtkQRenderWindowInteractor.h
@@ -0,0 +1,168 @@
+#ifndef MITKVTKQRENDERWINDOWINTERACTOR_H_HEADER_INCLUDED_C1C53722
+#define MITKVTKQRENDERWINDOWINTERACTOR_H_HEADER_INCLUDED_C1C53722
+
+//## #include <stdlib.h>
+#include "mitkVtkRenderWindowInteractor.h"
+#include <qobject.h>
+#include "vtkObjectFactory.h"
+
+//##ModelId=3E6D600E0193
+//##Documentation
+//## @brief vtkRenderWindowInteractor implementation for Qt/mitk
+//## For using the VtkQRenderWindowInteractor it is necessary to add the VtkQRenderWindowInteractor.cpp to your project.
+//## The VtkQRenderWindowInteractorFactory will install the VtkQRenderWindowInteractor in the vtkObjectFactory automatically.
+//## (Bridge-pattern)
+//## @ingroup NavigationControl
+class VtkQRenderWindowInteractor : public mitk::VtkRenderWindowInteractor, public QObject
+{
+public:
+   //##ModelId=3E6D600F00E1
+  //##Documentation
+  //## Construct object so that light follows camera motion.
+  static VtkQRenderWindowInteractor *New();
+
+    //##ModelId=3E6D600F00EA
+  vtkTypeMacro(VtkQRenderWindowInteractor,vtkRenderWindowInteractor);
+    //##ModelId=3E6D600F00ED
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  itkTypeMacro(VtkQRenderWindowInteractor,vtkRenderWindowInteractor);
+
+  itkSetObjectMacro(MitkRenderer, mitk::BaseRenderer);
+  itkGetObjectMacro(MitkRenderer, mitk::BaseRenderer);
+
+  //##ModelId=3E6D600F00F5
+  //##Documentation
+  //## Initialize the event handler
+  virtual void Initialize();
+
+  //##ModelId=3E6D600F00F7
+  //##Documentation
+  //## Enable/Disable interactions.  By default interactors are enabled when
+  //## initialized.  Initialize() must be called prior to enabling/disabling
+  //## interaction. These methods are used when a window/widget is being
+  //## shared by multiple renderers and interactors.  This allows a "modal"
+  //## display where one interactor is active when its data is to be displayed
+  //## and all other interactors associated with the widget are disabled
+  //## when their data is not displayed.
+  virtual void Enable();
+    //##ModelId=3E6D600F00FF
+  virtual void Disable();
+
+  //##ModelId=3E6D600F0101
+  //##Documentation
+  //## By default the interactor installs a MessageProc callback which
+  //## intercepts windows messages to the window and controls interactions.
+  //## MFC or BCB programs can prevent this and instead directly route any mouse/key
+  //## messages into the event bindings by setting InstallMessgeProc to false.
+  vtkSetMacro(InstallMessageProc,int);
+    //##ModelId=3E6D600F0108
+  vtkGetMacro(InstallMessageProc,int);
+    //##ModelId=3E6D600F010B
+  vtkBooleanMacro(InstallMessageProc,int);
+
+  //##ModelId=3E6D600F0113
+  //##Documentation
+  //## Win32 specific application terminate, calls ClassExitMethod then
+  //## calls PostQuitMessage(0) to terminate the application. An application can Specify
+  //## ExitMethod for alternative behavior (i.e. suppression of keyboard exit)
+  void TerminateApp(void);
+
+  //##ModelId=3E6D600F0115
+  //##Documentation
+  //## Win32 timer methods
+  int CreateTimer(int timertype);
+    //##ModelId=3E6D600F011D
+  int DestroyTimer(void);
+
+  //##ModelId=3E6D600F011F
+  //##Documentation
+  //## Various methods that a Qt window can redirect to this class to be 
+  //## handled.
+  virtual void MousePressEvent(mitk::MouseEvent*);
+    //##ModelId=3E6D600F0128
+  virtual void MouseReleaseEvent(mitk::MouseEvent*);
+    //##ModelId=3E6D600F0130
+  virtual void MouseMoveEvent(mitk::MouseEvent*);
+    //##ModelId=3E6D600F0133
+  virtual void KeyPressEvent(mitk::KeyEvent*);
+  //ETX
+
+  //##ModelId=3E6D600F013A
+  //##Documentation
+  //## Methods to set the default exit method for the class. This method is
+  //## only used if no instance level ExitMethod has been defined.  It is
+  //## provided as a means to control how an interactor is exited given
+  //## the various language bindings (tcl, Win32, etc.).
+  static void SetClassExitMethod(void (*f)(void *), void *arg);
+    //##ModelId=3E6D600F0144
+  static void SetClassExitMethodArgDelete(void (*f)(void *));
+
+  //##ModelId=3E6D600F0147
+  //##Documentation
+  //## These methods correspond to the the Exit, User and Pick
+  //## callbacks. They allow for the Style to invoke them.
+  virtual void ExitCallback();
+
+    //##ModelId=3E6D600F014F
+  virtual void SetSize(int w, int h);
+    //##ModelId=3E6D600F0158
+  virtual void SetSize(int size[2]);
+
+protected:
+    //##ModelId=3E6D600F0164
+  VtkQRenderWindowInteractor();
+    //##ModelId=3E6D600F016C
+  ~VtkQRenderWindowInteractor();
+
+  //HWND    WindowId;
+    //##ModelId=3E6E4BB20203
+  int    TimerId;
+  //WNDPROC OldProc;
+    //##ModelId=3E6D600F00B8
+  int     InstallMessageProc;
+
+    //##ModelId=3E6D600F00B9
+  mitk::BaseRenderer::Pointer m_MitkRenderer;
+
+    //##ModelId=3E6D600F00C2
+  int     MouseInWindow;
+
+    //##ModelId=3E6D600F016D
+  virtual void Resize(int w, int h);
+
+  //BTX
+  //##ModelId=3E6D600F00CC
+  //##Documentation
+  //## Class variables so an exit method can be defined for this class
+  //## (used to set different exit methods for various language bindings,
+  //## i.e. tcl, java, Win32)
+  static void (*ClassExitMethod)(void *);
+    //##ModelId=3E6D600F00D6
+  static void (*ClassExitMethodArgDelete)(void *);
+    //##ModelId=3E6D600F00E0
+  static void *ClassExitMethodArg;
+  //ETX
+
+    //##ModelId=3E6E4BB2027B
+  virtual void timerEvent ( QTimerEvent * );
+
+private:
+    //##ModelId=3E6D600F0177
+  VtkQRenderWindowInteractor(const VtkQRenderWindowInteractor&);  //## Not implemented.
+    //##ModelId=3E6D600F0179
+  void operator=(const VtkQRenderWindowInteractor&);  //## Not implemented.
+};
+
+//## Factory for VtkQRenderWindowInteractor
+class VtkQRenderWindowInteractorFactory : public vtkObjectFactory 
+{
+  static VtkQRenderWindowInteractorFactory* m_Factory;
+public:
+  VtkQRenderWindowInteractorFactory( );
+  virtual const char* GetVTKSourceVersion();
+  virtual const char* GetDescription();
+};
+
+#endif /* MITKVTKQRENDERWINDOWINTERACTOR_H_HEADER_INCLUDED_C1C53722 */
+

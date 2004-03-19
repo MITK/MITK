e60ea037db1cc07bd468577df0db88fc19090e7e
Jochen Neuhaus <j.neuhaus@dkfz-heidelberg.de>
2004-03-19 16:31:59 +0000

    changed calculation of region of interest

--- Core/Algorithms/mitkBoundingObjectCutter.cpp
+++ Core/Algorithms/mitkBoundingObjectCutter.cpp
@@ -61,27 +61,71 @@ void mitk::BoundingObjectCutter::GenerateData()
     outputImage = NULL; 
     return; // return and do nothing in case of failure
   }
-  std::cout << "itk Spacing:  " << itkImage->GetSpacing()[0] << ", " << itkImage->GetSpacing()[1] << ", " << itkImage->GetSpacing()[2] << ".\n";
-  std::cout << "mitk Spacing: " << inputImageMitk->GetSlicedGeometry()->GetSpacing()[0] << ", " << inputImageMitk->GetSlicedGeometry()->GetSpacing()[1] << ", " << inputImageMitk->GetSlicedGeometry()->GetSpacing()[2] << ".\n";
+  //std::cout << "itk Spacing:  " << itkImage->GetSpacing()[0] << ", " << itkImage->GetSpacing()[1] << ", " << itkImage->GetSpacing()[2] << ".\n";
+  //std::cout << "mitk Spacing: " << inputImageMitk->GetSlicedGeometry()->GetSpacing()[0] << ", " << inputImageMitk->GetSlicedGeometry()->GetSpacing()[1] << ", " << inputImageMitk->GetSlicedGeometry()->GetSpacing()[2] << ".\n";
 
   // calculate region of interest
   m_BoundingObject->UpdateOutputInformation();
   mitk::BoundingBox::Pointer boundingBox = const_cast<mitk::BoundingBox*>(m_BoundingObject->GetGeometry()->GetBoundingBox());
-  const mitk::BoundingBox::PointType minPoint = boundingBox->GetMinimum();
-  const mitk::BoundingBox::PointType maxPoint = boundingBox->GetMaximum();
-  float* posPoint = m_BoundingObject->GetGeometry()->GetVtkTransform()->GetPosition();
-  mitk::ITKPoint3D position;
-  position[0] = posPoint[0] + minPoint[0] + 0.5;  // + 0.5 because TransformPhys... just truncates the result and does not round correctly
-  position[1] = posPoint[1] + minPoint[1] + 0.5;
-  position[2] = posPoint[2] + minPoint[2] + 0.5;
   
+  /* get BoundingBox min max and center in world coordinates */
+
+  mitk::BoundingBox::PointType minPoint = boundingBox->GetMinimum();
+  mitk::ScalarType min[3];
+  min[0] = minPoint[0];   min[1] = minPoint[1];   min[2] = minPoint[2];
+  m_BoundingObject->GetGeometry()->GetVtkTransform()->TransformPoint(min, min);
+  mitk::ITKPoint3D globalMinPoint;
+  mitk::ITKPoint3D globalMaxPoint;
+  globalMinPoint[0] = min[0];   globalMinPoint[1] = min[1];   globalMinPoint[2] = min[2];  
+  globalMaxPoint[0] = min[0];   globalMaxPoint[1] = min[1];   globalMaxPoint[2] = min[2];
+  /* create all 8 points of the bounding box */
+  mitk::BoundingBox::PointsContainerPointer points = mitk::BoundingBox::PointsContainer::New();
+  mitk::ITKPoint3D p;
+  p = boundingBox->GetMinimum();
+  points->InsertElement(0, p);
+  p[0] = -p[0];
+  points->InsertElement(1, p);
+  p = boundingBox->GetMinimum();
+  p[1] = -p[1];    
+  points->InsertElement(2, p);
+  p = boundingBox->GetMinimum();
+  p[2] = -p[2];    
+  points->InsertElement(3, p);
+  p = boundingBox->GetMaximum();
+  points->InsertElement(4, p);
+  p[0] = -p[0];
+  points->InsertElement(5, p);
+  p = boundingBox->GetMaximum();
+  p[1] = -p[1];    
+  points->InsertElement(6, p);
+  p = boundingBox->GetMaximum();
+  p[2] = -p[2];    
+  points->InsertElement(7, p);
+  mitk::BoundingBox::PointsContainerConstIterator pointsIterator = points->Begin();
+  mitk::BoundingBox::PointsContainerConstIterator pointsIteratorEnd = points->End();
+  while (pointsIterator != pointsIteratorEnd) // for each vertex of the bounding box
+  {
+    minPoint = pointsIterator->Value();
+    min[0] = minPoint[0];   min[1] = minPoint[1];   min[2] = minPoint[2];
+    m_BoundingObject->GetGeometry()->GetVtkTransform()->TransformPoint(min, min);  // transform vertex point to world coordinates
+
+    globalMinPoint[0] = (min[0] < globalMinPoint[0]) ? min[0] : globalMinPoint[0];  // check if world point
+    globalMinPoint[1] = (min[1] < globalMinPoint[1]) ? min[1] : globalMinPoint[1];  // has a lower or a
+    globalMinPoint[2] = (min[2] < globalMinPoint[2]) ? min[2] : globalMinPoint[2];  // higher value as
+    globalMaxPoint[0] = (min[0] > globalMaxPoint[0]) ? min[0] : globalMaxPoint[0];  // the last known highest
+    globalMaxPoint[1] = (min[1] > globalMaxPoint[1]) ? min[1] : globalMaxPoint[1];  // value
+    globalMaxPoint[2] = (min[2] > globalMaxPoint[2]) ? min[2] : globalMaxPoint[2];  // in each axis
+    pointsIterator++;
+  }
+
+  /* calculate regíon of interest in pixel values */
   ItkImageType::IndexType start;
-  itkImage->TransformPhysicalPointToIndex(position, start);
+  itkImage->TransformPhysicalPointToIndex(globalMinPoint, start);
   
-  ItkImageType::SizeType size;
-  size[0] = static_cast<ItkImageType::SizeType::SizeValueType>((maxPoint[0] - minPoint[0])/ itkImage->GetSpacing()[0]); // number of pixels along X axis
-  size[1] = static_cast<ItkImageType::SizeType::SizeValueType>((maxPoint[1] - minPoint[1])/ itkImage->GetSpacing()[1]); // number of pixels along Y axis
-  size[2] = static_cast<ItkImageType::SizeType::SizeValueType>((maxPoint[2] - minPoint[2])/ itkImage->GetSpacing()[2]); // number of pixels along Z axis
+  ItkImageType::SizeType size;  
+  size[0] = static_cast<ItkImageType::SizeType::SizeValueType>((globalMaxPoint[0] - globalMinPoint[0])/ itkImage->GetSpacing()[0]); // number of pixels along X axis
+  size[1] = static_cast<ItkImageType::SizeType::SizeValueType>((globalMaxPoint[1] - globalMinPoint[1])/ itkImage->GetSpacing()[1]); // number of pixels along Y axis
+  size[2] = static_cast<ItkImageType::SizeType::SizeValueType>((globalMaxPoint[2] - globalMinPoint[2])/ itkImage->GetSpacing()[2]); // number of pixels along Z axis
   ItkRegionType regionOfInterest;
   regionOfInterest.SetSize(size);
   regionOfInterest.SetIndex(start);
@@ -104,10 +148,13 @@ void mitk::BoundingObjectCutter::GenerateData()
   }
 
   ItkImageIteratorType imageIterator(itkImageCut, itkImageCut->GetRequestedRegion());
-  mitk::ITKPoint3D p;
+//  mitk::ITKPoint3D p;
   bool inside = false;
   m_OutsidePixelCount = 0;
   m_InsidePixelCount = 0;
+  //m_UseInsideValue = true;  // just for testing
+  //m_InsideValue = 30000;  // just for testing
+  //m_OutsideValue = -30000;  // just for testing
   if (GetUseInsideValue()) // use a fixed value for each inside pixel(create a binary mask of the bounding object)
     for (imageIterator.GoToBegin(); !imageIterator.IsAtEnd(); ++imageIterator)
     {          

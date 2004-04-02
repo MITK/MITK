45e2bdb1bbb64cf53bc660cdb66c35feec7383d2
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2004-04-02 11:10:08 +0000

    adding overloaded constructors for having a PointType

--- Core/DataStructures/mitkPointOperation.h
+++ Core/DataStructures/mitkPointOperation.h
@@ -1,9 +1,10 @@
-#ifndef MITKPOINTOPERATION
-#define MITKPOINTOPERATION
+#ifndef MITKPOINTOPERATION_H
+#define MITKPOINTOPERATION_H
 
 #include "mitkCommon.h"
 #include "mitkOperation.h"
 #include "mitkVector.h"
+
 
 
 namespace mitk {
@@ -18,7 +19,7 @@ namespace mitk {
 class PointOperation : public mitk::Operation
 {
   public:
-    //##ModelId=3F0189F003A2
+  //##ModelId=3F0189F003A2
 	//##Documentation
 	//##@brief Operation, that handles all aktions on one Point.
 	//##
@@ -26,24 +27,34 @@ class PointOperation : public mitk::Operation
 	//## operationType is the type of that operation (see mitkOperation.h; e.g. move or add; Information for StateMachine::ExecuteOperation());
 	//## point is the information of the point to add or is the information to change a point into; index is e.g. the position in a
 	//## list which describes the element to change
-    PointOperation(OperationType operationType,
-					ITKPoint3D point, int index);
+  PointOperation(OperationType operationType,	ITKPoint3D point);
+  PointOperation(OperationType operationType,	ITKPoint3D point, int index);
+  PointOperation(OperationType operationType,	ITKPoint3D point, int index, PointSpecificationType type);
 
-    //##ModelId=3F0189F003B2
-    virtual ~PointOperation();
+  //##ModelId=3F0189F003B2
+  virtual ~PointOperation();
 
-    //##ModelId=3F0189F003B4
+  //##ModelId=3F0189F003B4
 	ITKPoint3D GetPoint();
 
-    //##ModelId=3F0189F003B5
+  //##ModelId=3F0189F003B5
 	int GetIndex();
 
+  PointSpecificationType GetPointType();
+
   private:
-    //##ModelId=3F0189F00393
+  //##ModelId=3F0189F00393
 	ITKPoint3D m_Point;
-    //##ModelId=3F0189F003A1
+
+  //##ModelId=3F0189F003A1
+  //##Documentation
+  //##@brief to declare an index where to store the point in data
 	int m_Index;
+  
+  //##Documentation
+  //##@brief to describe the type of the point. See enum PointSpecification for different types
+  PointSpecificationType m_Type;
 };
 }//namespace mitk
-#endif /* MITKPOINTOPERATION*/
+#endif /* MITKPOINTOPERATION_H*/
 

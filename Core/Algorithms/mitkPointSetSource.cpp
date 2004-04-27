99c2923bfbc7c24a25b937e777c523132e3610df
max <max@nowhere>
2004-04-27 16:46:40 +0000

    Initial import of an mitkPointSetSource

---
+++ Core/Algorithms/mitkPointSetSource.cpp
@@ -0,0 +1,67 @@
+#include "mitkPointSetSource.h"
+
+
+mitk::PointSetSource::PointSetSource()
+{
+    // Create the output.
+    OutputType::Pointer output = dynamic_cast<OutputType*> ( this->MakeOutput( 0 ).GetPointer() );
+    assert (output.IsNotNull());
+    this->SetNumberOfOutputs( 1 );
+    this->SetOutput(0, output.GetPointer());
+}
+
+
+
+
+mitk::PointSetSource::~PointSetSource()
+{
+}
+
+
+
+
+itk::DataObject::Pointer mitk::PointSetSource::MakeOutput ( unsigned int idx )
+{
+    return OutputType::New().GetPointer();
+}
+
+
+
+
+void mitk::PointSetSource::SetOutput( OutputType* output )
+{
+    this->ProcessObject::SetNthOutput( 0, output );
+}
+
+
+
+
+void mitk::PointSetSource::SetOutput( unsigned int idx, OutputType* output )
+{
+    this->ProcessObject::SetNthOutput(idx, output);    
+}
+
+
+
+
+mitk::PointSetSource::OutputType* mitk::PointSetSource::GetOutput()
+{
+    if ( this->GetNumberOfOutputs() < 1 )
+    {
+        return 0;
+    }
+    else
+    {
+        return dynamic_cast<OutputType*> ( this->GetOutput( 0 ) );
+    }
+}
+
+
+
+
+mitk::PointSetSource::OutputType* mitk::PointSetSource::GetOutput ( unsigned int idx )
+{
+    return dynamic_cast<OutputType*> ( this->ProcessObject::GetOutput( idx ) );
+}
+
+

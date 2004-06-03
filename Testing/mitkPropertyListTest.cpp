e995a8b8cd74ebc8fc5109f7ce9511223a1ffacd
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2004-06-03 16:13:15 +0000

    extended test

--- Testing/mitkPropertyListTest.cpp
+++ Testing/mitkPropertyListTest.cpp
@@ -1,18 +1,39 @@
 #include "mitkPropertyList.h"
+#include "mitkProperties.h"
 
 #include <fstream>
 int mitkPropertyListTest(int argc, char* argv[])
 {
   mitk::PropertyList::Pointer propList;
 
-  std::cout << "Testing New(): ";
+  std::cout << "Testing mitk::PropertyList::New(): ";
   propList = mitk::PropertyList::New();
   if (propList.IsNull()) {
     std::cout<<"[FAILED]"<<std::endl;
     return EXIT_FAILURE;
   }
   std::cout<<"[PASSED]"<<std::endl;
+  
+  mitk::BoolProperty::Pointer boolProp = new mitk::BoolProperty(false);
 
+  std::cout << "Testing SetProperty(): ";
+  bool propChanged = propList->SetProperty("test",boolProp); 
+  if (!propChanged) {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  boolProp->SetValue(true);
+  propChanged = propList->SetProperty("test",boolProp); 
+  if (!propChanged) {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  propChanged = propList->SetProperty("test",boolProp); 
+  if (propChanged) {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  
   std::cout<<"[TEST DONE]"<<std::endl;
   return EXIT_SUCCESS;
 }

7f4d8863a9ed47c599d7d10611f5874baf3ed820
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2004-06-07 14:02:30 +0000

    adapted to new interface

--- Testing/mitkPropertyListTest.cpp
+++ Testing/mitkPropertyListTest.cpp
@@ -5,35 +5,76 @@
 int mitkPropertyListTest(int argc, char* argv[])
 {
   mitk::PropertyList::Pointer propList;
-
   std::cout << "Testing mitk::PropertyList::New(): ";
   propList = mitk::PropertyList::New();
   if (propList.IsNull()) {
     std::cout<<"[FAILED]"<<std::endl;
     return EXIT_FAILURE;
   }
+  else {
+  std::cout<<"[PASSED]"<<std::endl;
+  } 
+  mitk::BoolProperty::Pointer boolProp = new mitk::BoolProperty(false);
+  mitk::BoolProperty::Pointer boolProp2 = new mitk::BoolProperty(false);
+  std::cout << "Testing BoolProperty ==: ";
+  if (! (*boolProp2 == *boolProp) ) {
+    
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+    } 
+  std::cout<<"[PASSED]"<<std::endl;
+  unsigned long tBefore,tAfter; 
+  
+  std::cout << "Testing SetProperty() with new key value: ";
+  tBefore = propList->GetMTime();
+  propList->SetProperty("test",boolProp); 
+  tAfter = propList->GetMTime();
+  if (! ( tAfter > tBefore) ) {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
   std::cout<<"[PASSED]"<<std::endl;
   
-  mitk::BoolProperty::Pointer boolProp = new mitk::BoolProperty(false);
-
-  std::cout << "Testing SetProperty(): ";
-  bool propChanged = propList->SetProperty("test",boolProp); 
-  if (!propChanged) {
+  std::cout << "Testing SetProperty() with new property value: ";
+  tBefore = propList->GetMTime();
+  propList->SetProperty("test",new mitk::BoolProperty(true)); 
+  tAfter = propList->GetMTime();
+  if (!  (tAfter > tBefore) ) {
     std::cout<<"[FAILED]"<<std::endl;
     return EXIT_FAILURE;
   }
-  boolProp->SetValue(true);
-  propChanged = propList->SetProperty("test",boolProp); 
-  if (!propChanged) {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
-  propChanged = propList->SetProperty("test",boolProp); 
-  if (propChanged) {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
+  std::cout<<"[PASSED]"<<std::endl;
   
+  std::cout << "Testing SetProperty() with unchanged property value: ";
+  tBefore = propList->GetMTime();
+  propList->SetProperty("test",new mitk::BoolProperty(true)); 
+  tAfter = propList->GetMTime();
+  if ( tBefore == tAfter ) {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+  
+  std::cout << "Testing MTime correctness when changing property value: ";
+  boolProp = new mitk::BoolProperty(true);
+  propList->SetProperty("test",boolProp); 
+  std::cout << "prop->MTIme() before: >" << boolProp->GetMTime() << "< ";
+  std::cout << (tBefore = propList->GetMTime());
+  boolProp->SetValue(true);
+  std::cout << "prop->MTIme() after: >" << boolProp->GetMTime() << "< ";
+  std::cout << (tAfter = propList->GetMTime());
+  boolProp->SetValue(false);
+  std::cout << "prop->MTIme() afterAll: >" << boolProp->GetMTime() << "< ";
+  unsigned long tAfterAll = propList->GetMTime();
+  
+  
+  if (tBefore != tAfter || tAfterAll <= tAfter) {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+  
+
   std::cout<<"[TEST DONE]"<<std::endl;
   return EXIT_SUCCESS;
 }

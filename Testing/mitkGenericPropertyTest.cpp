6d3c7f5da782e97d173ebbba568b3785b1d47414
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2004-06-08 13:25:30 +0000

    added mitkGenericPropertyTest

---
+++ Testing/mitkGenericPropertyTest.cpp
@@ -0,0 +1,39 @@
+#include "mitkGenericProperty.h"
+
+#include <fstream>
+int mitkGenericPropertyTest(int argc, char* argv[])
+{
+  std::cout << "Testing mitk::GenericProperty<int>:";
+  mitk::GenericProperty<int>::Pointer prop = new mitk::GenericProperty<int>();
+  mitk::GenericProperty<int>::Pointer prop2 = new mitk::GenericProperty<int>(5);
+  mitk::GenericProperty<int>::Pointer prop3 = new mitk::GenericProperty<int>(7);
+  unsigned long tBefore = prop->GetMTime();
+  prop->SetValue(5);
+  unsigned long tAfter = prop->GetMTime();
+  prop->SetValue(5);
+  unsigned long tAfterAll = prop->GetMTime();
+  
+  if (prop->GetValue() != 5 || prop->GetValueAsString() != "5") {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+    
+  std::cout << "Testing equality operation: ";
+  
+  if ( (! (*prop == *prop2)) || (*prop2 == *prop3) ) {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+  std::cout << "Testing MTime correctness when changing property value: ";
+  
+  if (tBefore >= tAfter || tAfterAll != tAfter) {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+  
+
+  std::cout<<"[TEST DONE]"<<std::endl;
+  return EXIT_SUCCESS;
+}

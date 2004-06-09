90812b34a9867f9ec1d80fb6d55eab6efbf0c123
boettger <boettger@nowhere>
2004-06-09 15:04:03 +0000

    added test code

--- Testing/mitkContourTest.cpp
+++ Testing/mitkContourTest.cpp
@@ -1,4 +1,5 @@
 #include "mitkContour.h"
+#include "mitkCommon.h"
 
 #include <fstream>
 int mitkContourTest(int argc, char* argv[])
@@ -13,6 +14,27 @@ int mitkContourTest(int argc, char* argv[])
   else {
   std::cout<<"[PASSED]"<<std::endl;
   } 
+
+  std::cout << "Testing mitk::Contour::AddVertex(): ";
+  mitk::ITKPoint3D p;
+  p.Fill(0);
+  contour->AddVertex(p);
+  p.Fill(1);
+  contour->AddVertex(p);
+  p.Fill(2);
+  contour->AddVertex(p);
+  
+  if (contour->GetNumberOfPoints() != 3)   
+    {
+      std::cout<<"[FAILED]"<<std::endl;
+      return EXIT_FAILURE;
+    }
+  else 
+    {
+    std::cout<<"[PASSED]"<<std::endl;
+    } 
+
+
   
 
   std::cout<<"[TEST DONE]"<<std::endl;

13c8fbfa9eff7053d2ad6ad5d8b4ec0588ee8a69
boettger <boettger@nowhere>
2004-06-17 09:15:42 +0000

    added new navigator and method GetPos()/Setpos() to SliderNavigator

--- Qmitk/QmitkTwoButtonNavigator.ui.h
+++ Qmitk/QmitkTwoButtonNavigator.ui.h
@@ -6,58 +6,66 @@
 ** init() function in place of a constructor, and a destroy() function in
 ** place of a destructor.
 *****************************************************************************/
-
-
-void QmitkSliderNavigator::Refetch()
+void QmitkTwoButtonNavigator::Refetch()
 {
   if(!m_InRefetch)
   {
     m_InRefetch=true;
     
-    slider->setMinValue(0);
-    slider->setMaxValue(m_Stepper->GetSteps()-1);
-    slider->setValue(m_Stepper->GetPos());
-    
-    spinBox->setMinValue(m_Stepper->ConvertPosToUnit(0));
-    spinBox->setMaxValue(m_Stepper->ConvertPosToUnit(m_Stepper->GetSteps()));
-    spinBox->setValue(m_Stepper->ConvertPosToUnit(m_Stepper->GetPos()));
-    
+    m_MinValue = 0;
+    m_MaxValue = m_Stepper->GetSteps()-1;
+    m_SliceNumber = m_Stepper->GetPos();
+       
     m_InRefetch=false;
   }
 }
 
 
-void QmitkSliderNavigator::SetStepper( mitk::Stepper * stepper)
+void QmitkTwoButtonNavigator::SetStepper( mitk::Stepper * stepper)
 {
     m_Stepper = stepper;
     m_InRefetch = (m_Stepper==NULL); // this avoids trying to use m_Stepper until it is set to something != NULL (additionally to the avoiding recursions during refetching)
 }
 
 
-void QmitkSliderNavigator::slider_valueChanged( int )
+/*void QmitkTwoButtonNavigator::slider_valueChanged( int )
 {
   if(!m_InRefetch)
     m_Stepper->SetPos(slider->value());
 }
+*/
 
-void QmitkSliderNavigator::init()
+void QmitkTwoButtonNavigator::init()
 {
   m_InRefetch = true; // this avoids trying to use m_Stepper until it is set to something != NULL (additionally to the avoiding recursions during refetching)
 }
 
 
-void QmitkSliderNavigator::spinBox_valueChanged( int )
+/*void QmitkSliderNavigator::spinBox_valueChanged( int )
 {
   if(!m_InRefetch)
     m_Stepper->SetPos(m_Stepper->ConvertPosToUnit(spinBox->value()));
 }
+*/
 
-int QmitkSliderNavigator::GetPos()
-{
-    return m_Stepper->GetPos();
-}
 
-void QmitkSliderNavigator::SetPos(int val)
-{
-    if(!m_InRefetch)    m_Stepper->SetPos( val );
-}
+void QmitkTwoButtonNavigator::prevButton_clicked()
+  {
+  if(!m_InRefetch) 
+    {
+    if (m_SliceNumber > m_MinValue) m_SliceNumber--;
+    m_Stepper->SetPos( m_SliceNumber );
+    m_SliceNumberLabel->setText( QString("%1").arg(m_SliceNumber) );
+    }
+  }
+
+
+void QmitkTwoButtonNavigator::nextButton_clicked()
+  {
+  if(!m_InRefetch) 
+    {
+    if (m_SliceNumber < m_MaxValue) m_SliceNumber++;
+    m_Stepper->SetPos( m_SliceNumber );
+    m_SliceNumberLabel->setText( QString("%1").arg( m_SliceNumber ) );
+    }
+  }

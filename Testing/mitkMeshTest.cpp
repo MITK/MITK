0c82483d437661842e1ccdaceba9450087f7a1e8
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2004-06-18 09:17:11 +0000

    test of mitkMesh

---
+++ Testing/mitkMeshTest.cpp
@@ -0,0 +1,60 @@
+#include <mitkMesh.h>
+#include <mitkVector.h>
+#include <mitkPointOperation.h>
+#include <mitkInteractionConst.h>
+#include <fstream>
+
+int mitkMeshTest(int argc, char* argv[])
+{
+  //Create mesh
+  mitk::Mesh::Pointer mesh;
+  mesh = mitk::Mesh::New();
+
+  //try to get the itkmesh
+  mitk::Mesh::DataType::Pointer itkdata = NULL;
+  itkdata = mesh->GetMesh();
+  if (itkdata.IsNull())
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //fresh mesh has to be empty!
+  if (mesh->GetSize() != 0)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //create an operation and add a point.
+  int position = 0;
+  mitk::ITKPoint3D itkPoint;
+  itkPoint.Fill(1);
+  mitk::PointOperation* doOp = new mitk::PointOperation(mitk::OpINSERT, itkPoint, position);
+	mesh->ExecuteOperation(doOp);
+
+  //now check new condition!
+  if ( (mesh->GetSize()!=1) ||
+      (!mesh->IndexExists(position)))
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  mitk::ITKPoint3D tempPoint;
+  tempPoint.Fill(0);
+  tempPoint = mesh->GetItkPoint(position);
+  if (tempPoint != itkPoint)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+
+
+
+  //well done!!! Passed!
+  std::cout<<"[PASSED]"<<std::endl;
+
+  std::cout<<"[TEST DONE]"<<std::endl;
+  return EXIT_SUCCESS;
+}

9f0838ec7f73c2d7c2029ea1db31313bac1697f6
stephanie <stephanie@nowhere>
2004-06-25 12:56:20 +0000

    can now load the contents of the lightbox. adds a new Image whenever the lightbox is filled. preliminary implementation

--- QApplications/MainApp/ChiliPlugin/QcMITKSamplePlugin.cpp
+++ QApplications/MainApp/ChiliPlugin/QcMITKSamplePlugin.cpp
@@ -1,7 +1,11 @@
 #include <chili/task.h>
 #include <chili/plugin.xpm>
+#include <chili/qclightbox.h>
 #include <qlayout.h>
 #include <qstring.h>
+#include <mitkDataTree.h>
+#include <QmitkStdMultiWidget.h>
+//#include <mitkLightBoxImageReader.h>
 #include "SampleApp.h"
 #include "QcMITKSamplePlugin.h"
 
@@ -12,11 +16,26 @@ QcMITKSamplePlugin::QcMITKSamplePlugin( QWidget *parent )
 {
   task = new QcTask( xpm(), parent, name() );
   QGridLayout* layout = new QGridLayout(task,0,0,0,-1,"plugin_area");
-  SampleApp *ap; 
+   
   ap = new SampleApp(task,"sample",0);
 
   ap->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
   layout->addWidget(ap,0,0,0);
+
+  itkGenericOutputMacro(<<"hallo");
+
+  //funktioniert nicht:
+  //QcLightbox* lightbox;
+  //lightbox=lightboxManager()->getActiveLightbox();
+  //if(lightbox==NULL)
+  //{
+  //  itkGenericOutputMacro(<<"lightbox is null");
+  //}
+  //else
+  //{
+  //  itkGenericOutputMacro(<<"lightbox is not null");
+  //}
+  //connect(lightbox,SIGNAL(lightbox->lightboxActivated(QcLightbox*)),this,SLOT(selectSerie (QcLightbox*)));
 }
 
 QString 
@@ -41,3 +60,87 @@ create( QWidget *parent )
 {
 return new QcMITKSamplePlugin( parent );  
 }
+
+void 	QcMITKSamplePlugin::lightboxFilled (QcLightbox* lightbox)
+{
+  itkGenericOutputMacro(<<"lightbox filled");
+  selectSerie(lightbox);
+}
+
+void QcMITKSamplePlugin::selectSerie (QcLightbox* lightbox)
+{
+  itkGenericOutputMacro(<<"selectSerie");
+
+  ipPicDescriptor* pic;
+    
+  if(lightbox->getFrames()==0)
+    return;
+
+  mitk::Image::Pointer image = mitk::Image::New();
+  
+  //Variante 1
+  pic=lightbox->fetchVolume();
+
+  image->Initialize(pic);
+  image->SetPicVolume(pic);
+  //Variante 2
+//  mitk::LightBoxImageReader::Pointer reader=mitk::LightBoxImageReader::New();
+  //reader->SetLightBox(lightbox);
+  //image=reader->GetOutput();
+
+  mitk::DataTree::Pointer tree = ap->GetTree();
+  
+  mitk::DataTreeIterator* it = tree->inorderIterator();
+
+  mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+
+  node->SetData(image);
+
+  it->add(node);
+
+
+
+  ap->getMultiWidget()->texturizePlaneSubTree( tree->inorderIterator());
+  ap->getMultiWidget()->updateMitkWidgets();
+  ap->getMultiWidget()->fit();
+
+
+}
+
+/*
+  
+  ipPicDescriptor* pic;
+  
+  QcLightbox* lightbox;
+  
+  mitk::LightBoxImageReader::Pointer reader=mitk::LightBoxImageReader::New();
+  reader->SetLightBox(
+
+
+  connect(lightbox,lightboxReloadPics(lightbox),this,SLOT(
+  
+  pic= von der qclightbox
+  
+  mitk::Image::Pointer image = mitk::Image::New();
+  
+  image->Initialize(pic);
+  image->SetPicVolume(pic);
+
+  mitk::DataTree::Pointer tree = ap->GetTree();
+  
+  mitk::DataTreeIterator* it = tree->inorderIterator();
+
+  mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+
+  node->SetData(image);
+
+  it->add(node);
+
+
+
+  ap->getMultiWidget()->texturizePlaneSubTree( tree->inorderIterator());
+  ap->getMultiWidget()->updateMitkWidgets();
+  ap->getMultiWidget()->fit();
+
+
+*/
--- QApplications/MainApp/ChiliPlugin/QcMITKSamplePlugin.h
+++ QApplications/MainApp/ChiliPlugin/QcMITKSamplePlugin.h
@@ -3,6 +3,7 @@
 
 #include <chili/plugin.h>
 class QcTask;
+class SampleApp;
 
 class QcEXPORT QcMITKSamplePlugin: public QcPlugin {
 
@@ -15,6 +16,14 @@ public:
 
     QString name(); 
     const char ** xpm();
+
+    virtual void 	lightboxFilled (QcLightbox* lightbox);
+
+    SampleApp *ap;
+
+public slots :
+    
+    void selectSerie (QcLightbox* lightbox);
 
 private:
 

c208af9c83982ae6fe6aacb5a076eac7d00506c4
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2004-08-13 18:35:43 +0000

    ENH: debug message more meaningful

--- Core/Interactions/mitkStateMachine.cpp
+++ Core/Interactions/mitkStateMachine.cpp
@@ -39,7 +39,7 @@ mitk::StateMachine::StateMachine(const char * type) : m_CurrentState(NULL)
     m_Type = type;
 	  m_CurrentState = mitk::StateMachineFactory::GetStartState(type);
     if (m_CurrentState == NULL)
-      itkWarningMacro("Error from "<<this->GetNameOfClass()<<"; Message: did not recieve a pointer for start-state");
+      itkWarningMacro("Error: no start-state defined in state-machine definition "<<type<<".");
   }
   //else
   //  itkWarningMacro("Error from "<<this->GetNameOfClass()<<"; Message: Type of StateMachine is NULL!");

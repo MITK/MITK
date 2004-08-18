927a18067e17789f238510c1856cb8bae8780255
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2004-08-18 20:41:54 +0000

    ADD: class for writing result images into a Chili lightbox. Compiles, but will not work yet.

---
+++ Core/IO/mitkLightBoxResultImageWriter.cpp
@@ -0,0 +1,194 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkLightBoxResultImageWriter.h"
+
+#include <chili/plugin.h>
+#include <chili/qclightbox.h>
+#include <isg.h>
+#include <ipDicom.h>
+
+#include <mitkImage.h>
+#include <mitkImageSliceSelector.h>
+
+#include <itkRootTreeIterator.h>
+#include <itkImageFileReader.h>
+
+mitk::LightBoxResultImageWriter::LightBoxResultImageWriter()
+{
+  m_ImageTypeName = "Result";
+}
+
+mitk::LightBoxResultImageWriter::~LightBoxResultImageWriter()
+{
+}
+
+const mitk::Image *mitk::LightBoxResultImageWriter::GetInput(void)
+{
+  return static_cast< const mitk::Image * >(this->ProcessObject::GetInput(0));		
+}
+
+void mitk::LightBoxResultImageWriter::SetInput(const mitk::Image *image)
+{
+  this->ProcessObject::SetNthInput( 0, const_cast< mitk::Image * >( image ) );	
+}
+
+void mitk::LightBoxResultImageWriter::SetInputByNode(const mitk::DataTreeNode *node)
+{
+  if(node==NULL)
+    return;
+
+  SetInput(dynamic_cast<mitk::Image*>(node->GetData()));
+  
+  node->GetLevelWindow(m_LevelWindow, NULL, "levelWindow");
+  node->GetLevelWindow(m_LevelWindow, NULL);
+  const char * name;
+  if(node->GetStringProperty("name", name, NULL))
+    SetImageTypeName(name);
+}
+
+const mitk::Image *mitk::LightBoxResultImageWriter::GetSourceImage(void)
+{
+  return static_cast< const mitk::Image * >(this->ProcessObject::GetInput(1));		
+}
+
+void mitk::LightBoxResultImageWriter::SetSourceImage(const mitk::Image *source)
+{
+  this->ProcessObject::SetNthInput( 1, const_cast< mitk::Image * >( source ) );	
+}
+
+bool mitk::LightBoxResultImageWriter::SetSourceByTreeSearch(mitk::DataTreeIteratorBase* iterator)
+{
+  if(iterator==NULL)
+    return false;
+
+  const mitk::Image* image = GetInput();
+
+  if(image == NULL)
+    return false;
+
+
+  mitk::DataTreeIteratorClone it=new itk::RootTreeIterator<DataTreeBase>(iterator->GetTree(), iterator->GetNode());
+  bool LoadedFromChili;
+  while(!it->IsAtEnd())
+  {
+    if(it->Get()->GetBoolProperty("LoadedFromChili", LoadedFromChili) && LoadedFromChili)
+    {
+      mitk::Image::Pointer sourcecandidate=dynamic_cast<mitk::Image*>(it->Get()->GetData());
+      if(sourcecandidate.IsNotNull())
+      {
+        if(image->GetDimension()==sourcecandidate->GetDimension())
+        {
+          int i, dim=image->GetDimension();
+          for(i=0; i<dim;++i)
+            if(image->GetDimension(i)!=sourcecandidate->GetDimension(i))
+              break;
+          if(i==dim)
+          {
+            SetSourceImage(sourcecandidate);
+            return true;
+          }
+        }
+      }
+      ++it;
+    }
+    return false;
+  }
+}
+
+void mitk::LightBoxResultImageWriter::SetLightBox(QcLightbox* lightbox)
+{
+  if(lightbox!=m_LightBox)
+  {
+    m_LightBox=lightbox;
+    Modified();    
+  }
+}
+
+QcLightbox* mitk::LightBoxResultImageWriter::GetLightBox() const
+{
+  return m_LightBox;
+}
+
+
+void mitk::LightBoxResultImageWriter::GenerateData()
+{
+  itkGenericOutputMacro(<<"GenerateData ");
+  const Image* image = GetInput();
+  const Image* sourceimage = GetSourceImage();
+  if((image==NULL) || (sourceimage==NULL) || (m_LightBox==NULL))
+  {
+    itk::ImageFileReaderException e(__FILE__, __LINE__);
+    itk::OStringStream msg;
+    if(image==NULL)
+      msg << "image not set. ";
+    if(sourceimage==NULL)
+      msg << "source-image not set. ";
+    if(m_LightBox==NULL)
+      msg << "lightbox not set. ";
+    e.SetDescription(msg.str().c_str());
+    throw e;
+    return;
+  }
+
+  ImageSliceSelector::Pointer resultslice = ImageSliceSelector::New();
+  ImageSliceSelector::Pointer sourceslice = ImageSliceSelector::New();
+
+  resultslice->SetInput(image);
+  sourceslice->SetInput(sourceimage);
+
+  ipPicDescriptor *cur, *prev=NULL;
+  LevelWindow levelwindow;
+  int s, t;
+  int smax, tmax;
+  smax = image->GetDimension(2);
+  tmax = image->GetDimension(3);
+  for(s=0;s<smax;++s)
+  {
+    resultslice->SetSliceNr(s);
+    sourceslice->SetSliceNr(s);
+    for(t=0;t<tmax;++t)
+    {
+      resultslice->SetTimeNr(t);
+      sourceslice->SetTimeNr(t);
+      resultslice->Update();
+      sourceslice->Update();
+
+      cur=ipPicClone(resultslice->GetOutput()->GetPic());
+      ipPicDelTag( cur, "SOURCE HEADER" );
+
+      QcPlugin::addTags( cur, sourceslice->GetOutput()->GetPic(), prev==NULL );
+
+      //interSliceGeometry_t isg;
+      //isg.unit_t = ipPicUtilMillimeter;
+
+      //QcPlugin::addDicomHeader( cur, isg );
+      QcPlugin::addIcon( cur, true );
+
+      QcPlugin::addLevelWindow( cur, m_LevelWindow.GetMin(), m_LevelWindow.GetMax());
+
+      QcPlugin::changeImageType( cur, const_cast<char*>(m_ImageTypeName.c_str()) ); 
+      if( prev )
+        QcPlugin::changeSeries( cur, prev );
+      m_LightBox->addImage( cur );
+
+      prev = cur;
+    }
+  }
+}
---
+++ Core/IO/mitkLightBoxResultImageWriter.h
@@ -0,0 +1,105 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef LIGHTBOXRESULTIMAGEWRITER_H_HEADER_INCLUDED_C1F48A22
+#define LIGHTBOXRESULTIMAGEWRITER_H_HEADER_INCLUDED_C1F48A22
+
+#define NO_pVersion
+
+#include "mitkBaseProcess.h"
+#include "mitkCommon.h"
+#include "mitkDataTree.h"
+
+class QcLightbox;
+
+namespace mitk {
+
+class Image;
+
+/**
+* @brief Writer for result images into a Chili lightbox. The result image
+* must be of the same size and geometry as an @em source image (set of 
+* slices), which is already in the Chili database and has to be provided 
+* to the writer.
+* @ingroup Process
+*/
+class LightBoxResultImageWriter : public BaseProcess
+{
+public:
+  /** Standard class typedefs. */
+  mitkClassMacro(LightBoxResultImageWriter, BaseProcess);
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  virtual void SetInput(const mitk::Image *image);
+  const mitk::Image *GetInput(void);
+
+  //##Documentation
+  //## @brief Sets the input to node->GetData() and additionally 
+  //## m_LevelWindow and m_ImageTypeName from the node properties
+  virtual void SetInputByNode(const mitk::DataTreeNode *node);
+
+  const mitk::Image *GetSourceImage(void);
+  virtual void SetSourceImage(const mitk::Image *source); 
+  bool SetSourceByTreeSearch(mitk::DataTreeIteratorBase* iterator);
+
+  void SetLightBox(QcLightbox* lightbox);
+  QcLightbox* GetLightBox() const;
+
+  itkSetStringMacro( ImageTypeName );
+  itkGetStringMacro( ImageTypeName );
+
+  //##Documentation
+  //## @brief Get the level-window, which will be written in the
+  //## lightbox
+  const LevelWindow& GetLevelWindow() const
+  {
+    return m_LevelWindow;
+  }
+
+  //##Documentation
+  //## @brief Set the level-window, which will be written in the
+  //## lightbox
+  void SetLevelWindow(const LevelWindow& levelwindow)
+  {
+    if(m_LevelWindow!=levelwindow)
+    {
+      m_LevelWindow = levelwindow;
+      Modified();
+    }
+  }
+
+protected:
+  virtual void GenerateData();
+
+  LightBoxResultImageWriter();
+
+  ~LightBoxResultImageWriter();
+
+  QcLightbox* m_LightBox;
+
+  LevelWindow m_LevelWindow;
+
+  std::string m_ImageTypeName;
+};
+
+} // namespace mitk
+
+#endif /* LIGHTBOXRESULTIMAGEWRITER_H_HEADER_INCLUDED_C1F48A22 */

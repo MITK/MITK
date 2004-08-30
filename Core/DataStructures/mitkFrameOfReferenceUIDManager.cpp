629b0b72884d57f13cd1c37491b5f58beee1038d
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2004-08-30 18:21:40 +0000

    ADD: manager for mapping internal reference ID to DICOM referenceUID

---
+++ Core/DataStructures/mitkFrameOfReferenceUIDManager.cpp
@@ -0,0 +1,58 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkFrameOfReferenceUIDManager.h"
+
+std::vector<std::string> mitk::FrameOfReferenceUIDManager::m_FrameOfReferenceUIDs;
+
+mitk::FrameOfReferenceUIDManager::FrameOfReferenceUIDManager()
+{
+  if(m_FrameOfReferenceUIDs.size()==0)
+    m_FrameOfReferenceUIDs.push_back("");
+}
+
+mitk::FrameOfReferenceUIDManager::~FrameOfReferenceUIDManager()
+{
+  m_FrameOfReferenceUIDs.clear();
+}
+
+unsigned int mitk::FrameOfReferenceUIDManager::AddFrameOfReferenceUID(const char* forUID)
+{
+  std::string forUIDstring=forUID;
+  int i, size;
+  size=m_FrameOfReferenceUIDs.size();
+  std::vector<std::string>::iterator it;
+  for(i=0, it=m_FrameOfReferenceUIDs.begin(); i<size; ++i,++it)
+  {
+    if(*it==forUIDstring)
+      return i;
+  }
+itkGenericOutputMacro(<<"adding uid "<<forUIDstring<< " as id "<<i);
+  m_FrameOfReferenceUIDs.push_back(forUIDstring);
+  return i;
+}
+
+const char* mitk::FrameOfReferenceUIDManager::GetFrameOfReferenceUID(unsigned int geometryFrameOfReferenceID)
+{
+  if(geometryFrameOfReferenceID >= m_FrameOfReferenceUIDs.size())
+    return NULL;
+  return m_FrameOfReferenceUIDs[geometryFrameOfReferenceID].c_str();
+}
+
+static mitk::FrameOfReferenceUIDManager AddStandardFOR;
---
+++ Core/DataStructures/mitkFrameOfReferenceUIDManager.h
@@ -0,0 +1,49 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef FRAMEOFREFERENCEUIDMANAGER_H_HEADER_INCLUDED_C1EBD0AD
+#define FRAMEOFREFERENCEUIDMANAGER_H_HEADER_INCLUDED_C1EBD0AD
+
+#include "mitkCommon.h"
+#include <vector>
+#include <string>
+
+namespace mitk {
+
+//##Documentation
+//## @brief Manager to map the internal FrameOfReferenceID of
+//## the Geometry3D (Geometry3D::m_FrameOfReferenceID) to a 
+//## DICOM FrameOfReferenceUID
+//##
+class FrameOfReferenceUIDManager
+{
+public:
+  typedef std::string FrameOfReferenceUIDType;
+private:
+  static std::vector<FrameOfReferenceUIDType> m_FrameOfReferenceUIDs;
+public:
+  static unsigned int AddFrameOfReferenceUID(const char* forUID);
+  static const char* GetFrameOfReferenceUID(unsigned int geometryFrameOfReferenceID);
+  FrameOfReferenceUIDManager();
+  ~FrameOfReferenceUIDManager();
+};
+
+} // namespace mitk
+
+#endif /* GEOMETRY3D_H_HEADER_INCLUDED_C1EBD0AD */

2b3d038ef70ef6ca5c67bad24672566a3b98465b
max <max@nowhere>
2004-10-15 10:01:03 +0000

    Added some convenience functions for accessing the data tree by type and or properties

--- Qmitk/QmitkApplicationBase/QmitkCommonFunctionality.h
+++ Qmitk/QmitkApplicationBase/QmitkCommonFunctionality.h
@@ -445,7 +445,7 @@ static mitk::DataTreeNode::Pointer FileOpen()
     {
       std::string selectedItemsName = itksys::SystemTools::GetFilenameWithoutExtension(name);
       selectedItemsName += ".stl";
-      QString fileName = QFileDialog::getSaveFileName(QString(selectedItemsName),"Surface Data(*.stl *.vtk)");
+      QString fileName = QFileDialog::getSaveFileName(QString(selectedItemsName.c_str()),"Surface Data(*.stl *.vtk)");
         if (fileName != NULL ) 
         {
           if(fileName.endsWith(".stl")==true)
@@ -458,7 +458,8 @@ static mitk::DataTreeNode::Pointer FileOpen()
           }
           else 
           {
-            if (fileName.endsWith(".vtk")==false) fileName += ".vtk";
+            if (fileName.endsWith(".vtk")==false) 
+                fileName += ".vtk";
             mitk::SurfaceVtkWriter<vtkPolyDataWriter>::Pointer writer=mitk::SurfaceVtkWriter<vtkPolyDataWriter>::New();
             writer->SetInput( surface );
             writer->SetFileName(fileName.latin1());
@@ -505,6 +506,207 @@ static mitk::DataTreeNode::Pointer FileOpen()
         return NULL;
       return static_cast<mitk::Image*>(it->Get()->GetData());
     }
+
+    /**
+     * Searches for the first node in the data tree, which holds a given type. 
+     * @param it an iterator pointing to the position in the data tree, where
+     *          the search should start
+     * @returns the first node in the data tree which is of the type given by
+     *          the template parameter T, or NULL otherwise.
+     */
+    template <typename T>
+    static mitk::DataTreeNode* GetFirstNodeByType( mitk::DataTreeIteratorClone it )
+    {
+        if ( it.GetPointer() == NULL )
+        {
+            return NULL;
+        }
+
+        mitk::DataTreeIteratorClone iteratorClone = it;
+        while ( !iteratorClone->IsAtEnd() )
+        {
+            mitk::DataTreeNode::Pointer node = iteratorClone->Get();
+            if ( node->GetData() != NULL )
+            {
+                // access the original data
+                T* data = dynamic_cast<T*>( node->GetData() );
+
+                // enquiry whether img is NULL
+                if ( data != NULL )
+                {
+                    return node.GetPointer();
+                }
+            }
+            ++iteratorClone;
+        }
+        return NULL;
+    }
+
+    /**
+     * Searches for the data object in the data tree, which matches a given type. 
+     * @param it an iterator pointing to the position in the data tree, where
+     *          the search should start
+     * @returns the first data oobject in the data tree which is of the type given by
+     *          the template parameter T, or NULL otherwise.
+     */
+    template <typename T>
+    static T* GetFirstDataByType( mitk::DataTreeIteratorClone it )
+    {
+        mitk::DataTreeNode* node = GetFirstNodeByType<T>(it);
+        if ( node == NULL )
+        {
+            return NULL;
+        }
+        else
+        {
+            return dynamic_cast<T*>( node->GetData() );
+        }
+    }
+
+    /**
+     * Searches for the first node in the data tree, which holds a given type 
+     * and matches a given property key and value. This may be used to search
+     * e.g. for a node holding an image with a given name in the data tree.
+     * @param it an iterator pointing to the position in the data tree, where
+     *          the search should start
+     * @param propertyKey the name of the property we want to compare with
+     * @param property the value of the property we want to search for in the data tree
+     * @returns the first node in the data tree which is of the type given by
+     *          the template parameter T, and matches propertyKey and property, or NULL otherwise.
+     */
+    template <typename T>
+    static mitk::DataTreeNode* GetFirstNodeByTypeAndProperty( mitk::DataTreeIteratorClone it, std::string propertyKey, mitk::BaseProperty* property )
+    {
+        if ( it.GetPointer() == NULL )
+        {
+            return NULL;
+        }
+
+        mitk::DataTreeIteratorClone iteratorClone = it;
+        while ( !iteratorClone->IsAtEnd() )
+        {
+            mitk::DataTreeNode::Pointer node = iteratorClone->Get();
+            if ( node->GetData() != NULL )
+            {
+                // access the original data
+                T* data = dynamic_cast<T*>( node->GetData() );
+
+                // enquiry whether img is NULL
+                if ( data != NULL )
+                {
+                    // check, if the data has the given property...
+                    mitk::BaseProperty::Pointer tmp = node->GetPropertyList()->GetProperty( propertyKey.c_str() );
+                    if ( (*property) == *(tmp) )
+                        return node.GetPointer();
+                }
+            }
+            ++iteratorClone;
+        }
+        return NULL;
+    }
+
+    /**
+     * Searches for the first data object in the data tree, which is of a given type 
+     * and whose node matches a given property key and value. This may be used to search
+     * e.g. for an image with a given name in the data tree.
+     * @param it an iterator pointing to the position in the data tree, where
+     *          the search should start
+     * @param propertyKey the name of the property we want to compare with
+     * @param property the value of the property we want to search for in the data tree
+     * @returns the first data object in the data tree which is of the type given by
+     *          the template parameter T, and matches propertyKey and property, or NULL otherwise.
+     */
+    template <typename T>
+    static T* GetFirstDataByTypeAndProperty( mitk::DataTreeIteratorClone it, std::string propertyKey, mitk::BaseProperty* property )
+    {
+        mitk::DataTreeNode* node = GetFirstNodeByTypeAndProperty<T>( it, propertyKey, property );
+        if ( node == NULL )
+        {
+            return NULL;
+        }
+        else
+        {
+            return dynamic_cast<T*>( node->GetData() );
+        }
+    }
+
+    /**
+     * Searches for the first node in the data tree, which matches a given 
+     * property key and value. This may be used to search e.g. for a node holding
+     * a data object with a given name in the data tree.
+     * @param it an iterator pointing to the position in the data tree, where
+     *          the search should start
+     * @param propertyKey the name of the property we want to compare with
+     * @param property the value of the property we want to search for in the data tree
+     * @returns the first node in the data tree which matches propertyKey and property, or NULL otherwise.
+     */
+    static mitk::DataTreeNode* GetFirstNodeByProperty( mitk::DataTreeIteratorClone it, std::string propertyKey, mitk::BaseProperty* property )
+    {
+        mitk::DataTreeIteratorClone pos = dynamic_cast<mitk::DataTree*>( it->GetTree() )->GetNext( propertyKey.c_str(), property, it.GetPointer() );
+        if ( ! pos->IsAtEnd() )
+        {
+            return pos->Get();
+        }
+        else
+        {
+            return NULL;
+        }
+        
+    }
+
+    /**
+     * Searches for the first data object in the data tree, whose node matches a given 
+     * property key and value. This may be used to search e.g. for a node holding
+     * a data object with a given name in the data tree.
+     * @param it an iterator pointing to the position in the data tree, where
+     *          the search should start
+     * @param propertyKey the name of the property we want to compare with
+     * @param property the value of the property we want to search for in the data tree
+     * @returns the first data object in the data tree whose node matches propertyKey and property, or NULL otherwise.
+     */
+    static mitk::BaseData* GetFirstDataByProperty( mitk::DataTreeIteratorClone it, std::string propertyKey, mitk::BaseProperty* property )
+    {
+        mitk::DataTreeNode* node = GetFirstNodeByProperty( it, propertyKey, property );
+        if ( node == NULL )
+        {
+            return NULL;
+        }
+        else
+        {
+            return node->GetData();
+        }
+    }
+
+    /**
+     * Searches for the node in the data tree which holds a given mitk::BaseData 
+     * @param it an iterator pointing to the position in the data tree, where
+     *          the search should start
+     * @param data the data object, for which the node in the tree should be searched.
+     * @returns the node holding data, or NULL otherwise.
+     */
+    static mitk::DataTreeNode* GetNodeForData( mitk::DataTreeIteratorClone it, mitk::BaseData* data )
+    {
+        if ( it.GetPointer() == NULL )
+        {
+            return NULL;
+        }
+
+        mitk::DataTreeIteratorClone iteratorClone = it;
+        while ( !iteratorClone->IsAtEnd() )
+        {
+            mitk::DataTreeNode::Pointer node = iteratorClone->Get();
+            if ( node.IsNotNull() )
+            {
+                if ( node->GetData() == data )
+                    return node.GetPointer();
+            }
+            ++iteratorClone;
+        }
+        return NULL;
+    }
+
+
+
 
 
 };

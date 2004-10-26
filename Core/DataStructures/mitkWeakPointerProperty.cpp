1a69e5aed5c45ce0c3654bfcce4dd0f8502fd690
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2004-10-26 15:05:50 +0000

    ENH: parallel to SmartPointerProperty

---
+++ Core/DataStructures/mitkWeakPointerProperty.cpp
@@ -0,0 +1,57 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkWeakPointerProperty.h"
+
+//##ModelId=3ED94B4203C1
+bool mitk::WeakPointerProperty::operator==(const BaseProperty& property) const
+{
+    const Self *other = dynamic_cast<const Self*>(&property);
+
+    if(other==NULL) return false;
+
+    return other->m_WeakPointer==m_WeakPointer;
+}
+
+//##ModelId=3ED94B7500F2
+mitk::WeakPointerProperty::WeakPointerProperty(itk::Object* pointer) : m_WeakPointer(pointer)
+{
+    Modified();
+}
+
+//##ModelId=3ED94B750111
+mitk::WeakPointerProperty::~WeakPointerProperty()
+{
+}
+
+//##ModelId=3ED952AD02B7
+itk::Object::Pointer mitk::WeakPointerProperty::GetWeakPointer() const
+{
+    return m_WeakPointer;
+}
+
+//##ModelId=3ED952AD02F6
+void mitk::WeakPointerProperty::SetWeakPointer(itk::Object* pointer)
+{
+    if(m_WeakPointer.GetPointer() != pointer)
+    {
+        m_WeakPointer = pointer;
+        Modified();
+    }
+}
---
+++ Core/DataStructures/mitkWeakPointerProperty.h
@@ -0,0 +1,59 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef MITKWEAKPOINTERPROPERTY_H_HEADER_INCLUDED_C126B791
+#define MITKWEAKPOINTERPROPERTY_H_HEADER_INCLUDED_C126B791
+
+#include "mitkCommon.h"
+#include "mitkBaseProperty.h"
+
+namespace mitk {
+
+//##ModelId=3ED94AC90382
+//##Documentation
+//## @brief Property containing a smart-pointer
+//## @ingroup DataTree
+class WeakPointerProperty : public BaseProperty
+{
+  public:
+    mitkClassMacro(WeakPointerProperty, BaseProperty);
+
+    //##ModelId=3ED94B4203C1
+    virtual bool operator==(const BaseProperty& property) const;
+    //##ModelId=3ED94B7500F2
+    WeakPointerProperty(itk::Object* pointer);
+
+    //##ModelId=3ED94B750111
+    virtual ~WeakPointerProperty();
+
+    //##ModelId=3ED952AD02B7
+    itk::Object::Pointer GetWeakPointer() const;
+
+    //##ModelId=3ED952AD02F6
+    void SetWeakPointer(itk::Object* pointer);
+  protected:
+    //##ModelId=3ED95309021C
+    itk::WeakPointer<itk::Object> m_WeakPointer;
+};
+
+} // namespace mitk
+
+
+
+#endif /* MITKWEAKPOINTERPROPERTY_H_HEADER_INCLUDED_C126B791 */

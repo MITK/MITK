97d33bb391ceb8bfaf860c7bcf256c1c3c15c632
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-01-05 09:52:18 +0000

    ADD: interactor for contours; currently it can only create a contour

---
+++ Core/Interactions/mitkContourInteractor.cpp
@@ -0,0 +1,163 @@
+#include "mitkContourInteractor.h"
+
+#include <mitkContour.h>
+
+//#include "ipSegmentation.h"
+//#include "mitkDataTreeNode.h"
+#include <mitkProperties.h>
+#include <mitkStringProperty.h>
+#include <mitkPointOperation.h>
+#include <mitkAction.h>
+#include <mitkPositionEvent.h>
+#include <mitkInteractionConst.h>
+#include <mitkPositionEvent.h>
+#include <mitkOpenGLRenderer.h>
+
+#include <vtkRenderer.h>
+
+mitk::ContourInteractor::ContourInteractor(const char * type, mitk::DataTreeNode* dataTreeNode)
+  : mitk::Interactor(type, dataTreeNode), m_Started(false)
+{
+  assert(m_DataTreeNode != NULL);
+
+  m_DataTreeNode->SetProperty("layer", new mitk::IntProperty(100) );
+  m_DataTreeNode->SetProperty("name", new mitk::StringProperty("InteractiveFeedbackData") );
+  m_DataTreeNode->SetOpacity(1);
+  m_DataTreeNode->SetColor(0.4,0.9,0.0);
+  m_DataTreeNode->SetProperty( "Width", new mitk::FloatProperty(2.0) );
+  m_Started = false;
+}
+
+mitk::ContourInteractor::~ContourInteractor()
+{
+
+}
+
+//mitk::Contour::Pointer ContourInteractor::ExtractContour(ipPicDescriptor* pic)
+//{
+//  int idx;
+//  int size = _ipPicElements (pic);
+//  for (idx = 0; idx < size; idx++)
+//    if ( ((ipUInt1_t*) pic->data)[idx]> 0) break;
+//
+//  int sizePoints;	// size of the _points buffer (number of coordinate pairs that fit in)
+//  int numPoints;		// number of coordinate pairs stored in _points buffer
+//  float *points = 0;
+//
+//  points = ipSegmentationGetContour8N( pic, idx, numPoints, sizePoints, points );
+//
+//  mitk::Contour::Pointer contour = mitk::Contour::New(); 
+//  contour->Initialize();
+//  mitk::Point3D pointInMM, pointInUnits;
+//  mitk::Point3D itkPoint;
+//  for (int pointIdx = 0; pointIdx < numPoints; pointIdx++)
+//  {
+//    pointInUnits[0] = points[2*pointIdx];
+//    pointInUnits[1] = points[2*pointIdx+1];
+//    pointInUnits[2] = m_ZCoord;
+//    m_SelectedImageGeometry->UnitsToMM(CorrectPointCoordinates(pointInUnits),pointInMM);
+//    contour->AddVertex(pointInMM);
+//  }
+//  return contour;
+//}
+
+bool mitk::ContourInteractor::ExecuteAction(mitk::Action* action, mitk::StateEvent const* stateEvent)
+{
+  mitk::Point3D eventPoint;
+
+
+  const mitk::PositionEvent* posEvent = dynamic_cast<const mitk::PositionEvent*>(stateEvent->GetEvent());
+
+  if(posEvent==NULL)
+  {
+    const mitk::DisplayPositionEvent* displayPosEvent = dynamic_cast<const mitk::DisplayPositionEvent*>(stateEvent->GetEvent());
+    mitk::OpenGLRenderer* sender = (mitk::OpenGLRenderer*) stateEvent->GetEvent()->GetSender();
+
+    if((displayPosEvent == NULL) || (sender == NULL))
+      return false;
+
+    eventPoint[0] = displayPosEvent->GetDisplayPosition()[0];
+    eventPoint[1] = displayPosEvent->GetDisplayPosition()[1];
+    eventPoint[2] = 0;
+
+    sender->GetVtkRenderer()->SetDisplayPoint(eventPoint.GetDataPointer());
+    sender->GetVtkRenderer()->DisplayToWorld();
+    float *vtkwp = sender->GetVtkRenderer()->GetWorldPoint();
+    vtk2itk(vtkwp, eventPoint);
+  }
+  else
+  {
+    eventPoint = posEvent->GetWorldPosition();
+  }
+
+
+  bool ok = false;
+  switch (action->GetActionId())
+  {
+  case mitk::AcNEWPOINT:
+    {
+      Press(eventPoint);
+      ok = true;
+      m_Started = true;
+      break;
+    }
+  case mitk::AcINITMOVEMENT:
+    {
+      if (m_Started)
+      {
+        Move(eventPoint);
+        ok = true;
+        break;
+      }
+    }
+  case mitk::AcMOVEPOINT:
+    {
+      if (m_Started)
+      {
+        Move(eventPoint);
+        ok = true;
+        break;
+      }
+    }
+  case mitk::AcFINISHMOVEMENT:
+    {
+      if (m_Started)
+      {
+        Release(eventPoint);
+        ok = true;
+        m_Started = false;
+      }
+      break;
+    }
+  default:
+    ok = false;
+    break;
+  }
+  return ok;
+}
+
+void mitk::ContourInteractor::Press(mitk::Point3D& point)
+{
+  mitk::Contour* contour = dynamic_cast<mitk::Contour*>(m_DataTreeNode->GetData());
+  assert(contour!=NULL);
+
+  if (!m_Positive)
+    m_DataTreeNode->SetColor(1.0,0.0,0.0);
+
+  contour->Initialize();
+  contour->AddVertex( point );
+}
+
+void mitk::ContourInteractor::Move(mitk::Point3D& point)
+{
+  mitk::Contour* contour = dynamic_cast<mitk::Contour*>(m_DataTreeNode->GetData());
+  assert(contour!=NULL);
+
+  contour->AddVertex( point );
+//  m_Parent->UpdateWidgets();
+}
+
+void mitk::ContourInteractor::Release(mitk::Point3D& point)
+{
+//vermutlich  m_Parent->UpdateWidgets();
+}
---
+++ Core/Interactions/mitkContourInteractor.h
@@ -0,0 +1,46 @@
+#ifndef __ContourInteractor_H
+#define __ContourInteractor_H
+
+#include "mitkCommon.h"
+#include <mitkInteractor.h>
+#include <mitkVector.h>
+
+namespace mitk {
+
+//##Documentation
+//## @brief Interactor for the creation of an mitk::Contour
+//## @ingroup Interaction
+class ContourInteractor : public mitk::Interactor
+{
+public:
+  mitkClassMacro(ContourInteractor, Interactor);
+
+  ContourInteractor(const char * type, DataTreeNode* dataTreeNode);
+  virtual ~ContourInteractor();
+
+protected:
+  virtual bool ExecuteAction(Action* action, mitk::StateEvent const* stateEvent);
+
+  /**
+  * entry method for any interaction. Method is called if user 
+  * presses the left mouse button down.
+  */
+  virtual void Press (mitk::Point3D& op);
+
+  /**
+  * this method is finally called after user release the left mouse button
+  */
+  virtual void Release (mitk::Point3D& op);
+
+  /**
+  * method is called when the user moves the mouse with left mouse button down
+  */
+  virtual void Move (mitk::Point3D& op);
+
+protected:
+  bool m_Positive;
+  bool m_Started;
+};
+
+}
+#endif //__ContourInteractor_H

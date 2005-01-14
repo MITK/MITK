d6af9707b5c15b6080a230ec101dc489796255cc
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-01-14 13:34:05 +0000

    FIX: bug #25: BaseRenderer::Resize has to tell m_CameraController the new size

--- Core/Rendering/mitkBaseRenderer.cpp
+++ Core/Rendering/mitkBaseRenderer.cpp
@@ -105,6 +105,9 @@ void mitk::BaseRenderer::Resize(int w, int h)
   m_Size[0] = w;
   m_Size[1] = h;
 
+  if(m_CameraController) 	 
+    m_CameraController->Resize(w, h);
+
   GetDisplayGeometry()->SetSizeInDisplayUnits(w, h);
   //@FIXME: die nächste Zeile ist nur da, weil der Anpassungsvorgang in SetSizeInDisplayUnits leider noch nicht richtig funktioniert.
   GetDisplayGeometry()->Fit();

1435d83947337501b4c57de79bf4da878dadd954
liqun <liqun@nowhere>
2005-01-21 14:56:52 +0000

    initial import

---
+++ Core/Rendering/mitkPointDataVtkMapper3D.cpp
@@ -0,0 +1,56 @@
+#include "mitkPointDataVtkMapper3D.h"
+
+#include <vtkActor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkSphereSource.h>
+#include <vtkProperty.h>
+
+vtkProp* mitk::PointDataVtkMapper3D::GetProp()
+{
+    return m_PointActor;
+}
+
+mitk::PointDataVtkMapper3D::PointDataVtkMapper3D()
+{
+	int i;
+    float x[6][3] = {{105,0,0},{-105,0,0},{0,105,0},{0,-105,0},{0,0,105},{0,0,-105}};
+	vtkIdType pts[3][2] = {{0,1},{2,3},{4,5}};
+	m_PointActor = vtkActor::New();
+	m_PointMapper = vtkPolyDataMapper::New();
+
+	m_PolyData = vtkPolyData::New();
+	m_Points = vtkPoints::New();
+	m_CellArray = vtkCellArray::New();
+
+	for (i=0;i<6;i++) m_Points->InsertPoint(i,x[i]);
+	for (i=0;i<3;i++) m_CellArray->InsertNextCell(2,pts[i]);
+
+	m_PolyData->SetPoints(m_Points);
+	m_PolyData->SetLines(m_CellArray);
+
+	m_PointMapper->SetInput(m_PolyData);
+	m_PointActor->SetMapper( m_PointMapper );
+	m_PointActor->GetProperty()->SetColor(0.0,0.0,1.0);
+}
+
+mitk::PointDataVtkMapper3D::~PointDataVtkMapper3D()
+{
+	m_Points->Delete();
+	m_CellArray->Delete();
+	m_PolyData->Delete();
+	m_PointMapper->Delete();
+	m_PointActor->Delete();
+}
+
+void mitk::PointDataVtkMapper3D::GenerateData()
+{
+    // Get the input data object ( a mitk::PointData )
+    mitk::PointData::Pointer input = dynamic_cast< mitk::PointData* >( this->GetData() );
+
+	mitk::Point3D point = input->GetPoint3D();
+//	std::cout<<point[0]<<" "<<point[1]<<" "<<point[2]<<std::endl;
+
+	m_PointActor->SetPosition( point[0], point[1], point[2] );
+
+	m_PolyData->Update();
+}
---
+++ Core/Rendering/mitkPointDataVtkMapper3D.h
@@ -0,0 +1,65 @@
+#ifndef _MITK_POINT_DATA_VTK_MAPPER_3D__H_
+#define _MITK_POINT_DATA_VTK_MAPPER_3D__H_
+
+#include <mitkBaseVtkMapper3D.h>
+#include "mitkPointData.h"
+#include "vtkCellArray.h"
+#include "vtkFloatArray.h"
+#include "vtkPointData.h"
+#include "vtkPoints.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderer.h"
+
+
+class vtkActor;
+class vtkPolyDataMapper;
+class vtkSphereSource;
+class vtkPolyData;
+class vtkPoints;
+class vtkCellArray;
+
+namespace mitk
+{
+
+class PointDataVtkMapper3D : public BaseVtkMapper3D
+{
+public:
+
+    mitkClassMacro( PointDataVtkMapper3D, BaseVtkMapper3D );
+
+    itkNewMacro( Self );
+
+    itkSetMacro( Radius, float);
+
+    itkGetMacro( Radius, float);
+
+    virtual vtkProp* GetProp();
+
+protected:
+
+    PointDataVtkMapper3D();
+
+    virtual ~PointDataVtkMapper3D();
+
+    virtual void GenerateData();
+
+    vtkActor* m_PointActor;
+
+	vtkPolyDataMapper* m_PointMapper;
+
+	vtkPolyData* m_PolyData;
+	vtkPoints* m_Points;
+	vtkCellArray* m_CellArray;
+
+	float m_Radius;
+};
+
+
+} //namespace mitk
+
+
+#endif
+

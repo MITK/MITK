d564c1b88d48020bc58e80e30029bd39f55ca2bb
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-01-24 14:03:52 +0000

    ADD: source for mitk::Mesh

---
+++ Core/Algorithms/mitkMeshSource.cpp
@@ -0,0 +1,87 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkMeshSource.h"
+
+
+mitk::MeshSource::MeshSource()
+{
+    // Create the output.
+    OutputType::Pointer output = dynamic_cast<OutputType*> ( this->MakeOutput( 0 ).GetPointer() );
+    assert (output.IsNotNull());
+    this->SetNumberOfRequiredInputs(0);
+    this->SetNumberOfOutputs( 1 );
+    this->SetOutput(0, output.GetPointer());  
+}
+
+
+
+
+mitk::MeshSource::~MeshSource()
+{
+}
+
+
+
+
+itk::DataObject::Pointer mitk::MeshSource::MakeOutput ( unsigned int idx )
+{
+    return OutputType::New().GetPointer();
+}
+
+
+
+
+void mitk::MeshSource::SetOutput( OutputType* output )
+{
+    this->ProcessObject::SetNthOutput( 0, output );
+}
+
+
+
+
+void mitk::MeshSource::SetOutput( unsigned int idx, OutputType* output )
+{
+    this->ProcessObject::SetNthOutput(idx, output);    
+}
+
+
+
+
+mitk::MeshSource::OutputType* mitk::MeshSource::GetOutput()
+{
+    if ( this->GetNumberOfOutputs() < 1 )
+    {
+        return 0;
+    }
+    else
+    {
+        return dynamic_cast<OutputType*> ( this->GetOutput( 0 ) );
+    }
+}
+
+
+
+
+mitk::MeshSource::OutputType* mitk::MeshSource::GetOutput ( unsigned int idx )
+{
+    return dynamic_cast<OutputType*> ( this->ProcessObject::GetOutput( idx ) );
+}
+
+
---
+++ Core/Algorithms/mitkMeshSource.h
@@ -0,0 +1,90 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef _MITKMESHSOURCE_H
+#define _MITKMESHSOURCE_H
+
+#include "itkProcessObject.h"
+#include "mitkMesh.h"
+
+namespace mitk
+{
+/**
+ * @brief Superclass of all classes generating point sets (instances of class
+ * mitk::Mesh) as output. 
+ * @ingroup Process
+ * In itk and vtk the generated result of a ProcessObject is only guaranteed
+ * to be up-to-date, when Update() of the ProcessObject or the generated
+ * DataObject is called immediately before access of the data stored in the
+ * DataObject. 
+ */
+class MeshSource : public itk::ProcessObject
+{
+public:
+    mitkClassMacro( MeshSource, itk::ProcessObject );
+
+    itkNewMacro( Self );
+
+    typedef mitk::Mesh OutputType;
+
+    typedef OutputType::Pointer OutputTypePointer;
+    
+    /**
+     * Allocates a new output object and returns it. Currently the
+     * index idx is not evaluated.
+     * @param idx the index of the output for which an object should be created
+     * @returns the new object
+     */
+    virtual itk::DataObject::Pointer MakeOutput ( unsigned int idx );
+
+    /**
+     * Allows to set the output of the point set source. 
+     * @param output the intended output of the point set source
+     */
+    virtual void SetOutput( OutputType* output );
+    
+    /**
+     * Allows to set the n-th output of the point set source. 
+     * @param output the intended output of the point set source
+     */
+    virtual void SetOutput( unsigned int idx, OutputType* output );
+    
+    /**
+     * Returns the output with index 0 of the point set source
+     * @returns the output
+     */
+    virtual OutputType* GetOutput();
+    
+    /**
+     * Returns the n'th output of the point set source
+     * @param idx the index of the wanted output
+     * @returns the output with index idx.
+     */
+    virtual OutputType* GetOutput ( unsigned int idx );
+    
+protected:
+
+    MeshSource();
+    
+    virtual ~MeshSource();
+
+};
+
+}
+#endif // #define _MITKMESHSOURCE_H

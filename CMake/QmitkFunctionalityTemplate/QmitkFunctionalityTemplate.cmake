f7f5e8c55389a4d78bae3682d12edb256a6eb3b6
matthias <matthias@nowhere>
2005-01-25 15:36:17 +0000

    Initial revision

---
+++ CMake/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.cmake
@@ -0,0 +1,18 @@
+INCLUDE_DIRECTORIES(../../Framework/QmitkApplicationBase ../../Functionalities/QmitkFunctionalityTemplate)
+
+
+
+# Functionality Template
+
+
+
+SET( APPMOD_H ${APPMOD_H} ../../Functionalities/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.h )
+
+SET( APPMOD_MOC_H ${APPMOD_MOC_H} ../../Functionalities/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.h )
+
+SET( APPMOD_CPP ${APPMOD_CPP} ../../Functionalities/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.cpp )
+
+
+
+SET (APPMOD_FORMS ${APPMOD_FORMS} ../../Functionalities/QmitkFunctionalityTemplate/QmitkFunctionalityTemplateControls.ui)
+
---
+++ CMake/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.cpp
@@ -0,0 +1,103 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "QmitkFunctionalityTemplate.h"
+#include "QmitkFunctionalityTemplateControls.h"
+
+#include <qaction.h>
+#include "icon.xpm"
+
+// for slice-navigation
+#include <mitkEventMapper.h>
+#include <mitkGlobalInteraction.h>
+#include <mitkBaseRenderer.h>
+#include "QmitkRenderWindow.h"
+#include "QmitkSelectableGLWidget.h"
+#include "QmitkStdMultiWidget.h"
+#include <QmitkStepperAdapter.h>
+#include "qpushbutton.h"
+
+// for stereo setting
+#include <mitkOpenGLRenderer.h>
+#include <mitkVtkRenderWindow.h>
+#include <vtkRenderWindow.h>
+
+// for zoom/pan
+#include <mitkDisplayCoordinateOperation.h>
+#include <mitkDisplayVectorInteractor.h>
+#include <mitkDisplayInteractor.h>
+#include <mitkInteractionConst.h>
+
+
+QmitkFunctionalityTemplate::QmitkFunctionalityTemplate(QObject *parent, const char *name, QmitkStdMultiWidget *mitkStdMultiWidget, mitk::DataTreeIteratorBase* it) : QmitkFunctionality(parent, name, it) ,
+controls(NULL), multiWidget(mitkStdMultiWidget)
+{
+    setAvailability(true);
+}
+
+
+QmitkFunctionalityTemplate::~QmitkFunctionalityTemplate()
+{
+
+}
+
+
+QWidget * QmitkFunctionalityTemplate::createMainWidget(QWidget *parent)
+{
+		return NULL;
+}
+
+QWidget * QmitkFunctionalityTemplate::createControlWidget(QWidget *parent)
+{
+    if (controls == NULL)
+    {
+        controls = new QmitkFunctionalityTemplateControls(parent);        
+    }
+    return controls;
+}
+
+
+void QmitkFunctionalityTemplate::createConnections()
+{
+  if ( controls ) 
+  {
+    //connect( ... );
+  }
+}
+
+
+QAction * QmitkFunctionalityTemplate::createAction(QActionGroup *parent)
+{
+    QAction* action;
+    action = new QAction( tr( "Functionality Template" ), QPixmap((const char**)icon_xpm), tr( "&Functionality Template" ), CTRL + Key_G, parent, "functionality template" );
+    return action;
+}
+
+
+void QmitkFunctionalityTemplate::treeChanged()
+{
+
+}
+
+
+void QmitkFunctionalityTemplate::activated()
+{
+  QmitkFunctionality::activated();
+  
+}

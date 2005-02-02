76bdecbff5278ad66c4853728956553f30a354c7
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-02-02 18:41:01 +0000

    CHG: changed standard eps (relaxed to system*100)

--- Core/DataStructures/mitkVector.cpp
+++ Core/DataStructures/mitkVector.cpp
@@ -18,6 +18,6 @@ PURPOSE.  See the above copyright notices for more information.
 
 #include "mitkVector.h"
 
-const mitk::ScalarType mitk::eps     = vnl_math::float_eps;
+const mitk::ScalarType mitk::eps     = vnl_math::float_eps*100;
 const mitk::ScalarType mitk::sqrteps = vnl_math::float_sqrteps;
 

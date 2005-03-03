2f41c0b0242288a7dd7b9cf3141fb30204e95e63
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2005-03-03 14:33:33 +0000

    FIX: line endings

--- CMake/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.cmake
+++ CMake/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.cmake
@@ -1,18 +1,9 @@
 INCLUDE_DIRECTORIES(../../Framework/QmitkApplicationBase ../../Functionalities/QmitkFunctionalityTemplate)
 
-
-
 # FunctionalityTemplate
 
-
-
 SET( APPMOD_H ${APPMOD_H} ../../Functionalities/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.h )
-
 SET( APPMOD_MOC_H ${APPMOD_MOC_H} ../../Functionalities/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.h )
-
 SET( APPMOD_CPP ${APPMOD_CPP} ../../Functionalities/QmitkFunctionalityTemplate/QmitkFunctionalityTemplate.cpp )
 
-
-
 SET (APPMOD_FORMS ${APPMOD_FORMS} ../../Functionalities/QmitkFunctionalityTemplate/QmitkFunctionalityTemplateControls.ui)
-

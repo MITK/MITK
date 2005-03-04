be1120d8a446a2fdebfd4711ad997ec6eab9de3f
boettger <boettger@nowhere>
2005-03-04 14:27:02 +0000

    ENH: added initial version of two more tests

---
+++ Testing/mitkAutoCropImageFilterTest.cpp
@@ -0,0 +1,49 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkAutoCropImageFilter.h"
+
+
+int mitkAutoCropImageFilterTest(int argc, char* argv[])
+{
+  mitk::AutoCropImageFilter::Pointer filter;
+  std::cout << "Testing mitk::AutoCropImageFilter::New(): ";
+  filter = mitk::AutoCropImageFilter::New();
+  if (filter.IsNull()) 
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  else {
+  std::cout<<"[PASSED]"<<std::endl;
+  } 
+
+  //std::cout << "Testing mitk::SurfaceToImageFilter::*TESTED_METHOD_DESCRIPTION: ";
+  //// METHOD_TEST_CODE
+  //if (filter.IsNull()) {
+  //  std::cout<<"[FAILED]"<<std::endl;
+  //  return EXIT_FAILURE;
+  //}
+  //else {
+  //std::cout<<"[PASSED]"<<std::endl;
+  //} 
+
+  std::cout<<"[TEST DONE]"<<std::endl;
+  return EXIT_SUCCESS;
+}
---
+++ Testing/mitkSurfaceToImageFilterTest.cpp
@@ -0,0 +1,50 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkSurfaceToImageFilter.h"
+
+#include <fstream>
+
+int mitkSurfaceToImageFilterTest(int argc, char* argv[])
+{
+  mitk::SurfaceToImageFilter::Pointer filter;
+  std::cout << "Testing mitk::Surface::New(): ";
+  filter = mitk::SurfaceToImageFilter::New();
+  if (filter.IsNull()) 
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  else {
+  std::cout<<"[PASSED]"<<std::endl;
+  } 
+
+  //std::cout << "Testing mitk::SurfaceToImageFilter::*TESTED_METHOD_DESCRIPTION: ";
+  //// METHOD_TEST_CODE
+  //if (filter.IsNull()) {
+  //  std::cout<<"[FAILED]"<<std::endl;
+  //  return EXIT_FAILURE;
+  //}
+  //else {
+  //std::cout<<"[PASSED]"<<std::endl;
+  //} 
+
+  std::cout<<"[TEST DONE]"<<std::endl;
+  return EXIT_SUCCESS;
+}

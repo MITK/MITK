a56a84eced166731ebbaf85fb95a3099127b59cb
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-03-07 16:03:07 +0000

    FIX: workaround for inverse-access of ITK 2.0/2.1.

--- Core/DataStructures/mitkGeometry3D.cpp
+++ Core/DataStructures/mitkGeometry3D.cpp
@@ -312,7 +312,11 @@ void mitk::Geometry3D::BackTransform(const mitk::Point3D &in, mitk::Point3D& out
     temp[j] = in[j] - offset[j];
   }
 
+#if (ITK_VERSION_MAJOR == 1 && ITK_VERSION_MINOR <= 8)
   const TransformType::MatrixType& inverse = m_IndexToWorldTransform->GetInverse();
+#else
+  const TransformType::MatrixType& inverse = m_IndexToWorldTransform->GetInverseMatrix();
+#endif
   for (i = 0; i < 3; i++) 
   {
     out[i] = 0.0;
@@ -325,7 +329,12 @@ void mitk::Geometry3D::BackTransform(const mitk::Point3D &in, mitk::Point3D& out
 
 void mitk::Geometry3D::BackTransform(const mitk::Point3D &at, const mitk::Vector3D &in, mitk::Vector3D& out) const
 {
-  out = m_IndexToWorldTransform->GetInverse() * in;
+#if (ITK_VERSION_MAJOR == 1 && ITK_VERSION_MINOR <= 8)
+  const TransformType::MatrixType& inverse = m_IndexToWorldTransform->GetInverse();
+#else
+  const TransformType::MatrixType& inverse = m_IndexToWorldTransform->GetInverseMatrix();
+#endif
+  out = inverse * in;
 }
 
 const mitk::Vector3D mitk::Geometry3D::GetXAxis()

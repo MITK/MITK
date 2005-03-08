9e3f99c4b0e04ba61720999ca93f01d7a7ca5648
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2005-03-08 13:26:42 +0000

    FIX: data type "BYTE" changed to "glByte"

--- Core/Controllers/mitkMovieGenerator.cpp
+++ Core/Controllers/mitkMovieGenerator.cpp
@@ -11,7 +11,7 @@ bool mitk::MovieGenerator::WriteMovie()
     if (!ok) return false;
     int imgSize = 3 * m_width * m_height;
     printf( "Video size = %i x %i\n", m_width, m_height );
-    BYTE *data = new BYTE[imgSize];
+    GLbyte *data = new GLbyte[imgSize];
     for (int i=0; i<m_stepper->GetSteps(); i++) {
       if (m_renderer) m_renderer->MakeCurrent();
       glReadPixels( 0, 0, m_width, m_height, GL_BGR_EXT, GL_UNSIGNED_BYTE, (void*)data );

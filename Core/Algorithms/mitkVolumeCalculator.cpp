9e2a0c2b5b47c364942fef86973e89243f5f3cab
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2005-03-23 16:59:11 +0000

    fixed indentation

--- Core/Algorithms/mitkVolumeCalculator.cpp
+++ Core/Algorithms/mitkVolumeCalculator.cpp
@@ -1,19 +1,19 @@
 /*=========================================================================
-
+ 
 Program:   Medical Imaging & Interaction Toolkit
 Module:    $RCSfile$
 Language:  C++
 Date:      $Date$
 Version:   $Revision$
-
+ 
 Copyright (c) German Cancer Research Center, Division of Medical and
 Biological Informatics. All rights reserved.
 See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
-
+ 
 This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
-
+ 
 =========================================================================*/
 
 #include "mitkVolumeCalculator.h"
@@ -21,32 +21,30 @@ PURPOSE.  See the above copyright notices for more information.
 #include <itkImageRegionConstIterator.h>
 
 mitk::VolumeCalculator::VolumeCalculator() : m_Image(NULL), m_Threshold(0)
-{
-}
+{}
 
 mitk::VolumeCalculator::~VolumeCalculator()
-{
-}
+{}
 
 void mitk::VolumeCalculator::ComputeVolume()
 {
   AccessByItk(m_Image,InternalCompute);
 }
 
-template < typename TPixel, unsigned int VImageDimension > 
-void mitk::VolumeCalculator::InternalCompute(itk::Image< TPixel, VImageDimension >* itkImage) 
+template < typename TPixel, unsigned int VImageDimension >
+void mitk::VolumeCalculator::InternalCompute(itk::Image< TPixel, VImageDimension >* itkImage)
 {
-    itk::ImageRegionConstIterator<itk::Image < TPixel, VImageDimension > > 
-        imageIt(itkImage, itkImage->GetLargestPossibleRegion() );
-	unsigned int count = 0; 
-	
-	for (imageIt.GoToBegin(); !imageIt.IsAtEnd(); ++imageIt)
-	{
-		if ( imageIt.Get() > m_Threshold )
-		{
-			count++;
-		}
-	}
-	mitk::Vector3D spacing = m_Image->GetSlicedGeometry()->GetSpacing();
-        m_Volume = count / 1000.0 * spacing[0] * spacing[1] * spacing[2];
+  itk::ImageRegionConstIterator<itk::Image < TPixel, VImageDimension > >
+  imageIt(itkImage, itkImage->GetLargestPossibleRegion() );
+  unsigned int count = 0;
+
+  for (imageIt.GoToBegin(); !imageIt.IsAtEnd(); ++imageIt)
+  {
+    if ( imageIt.Get() > m_Threshold )
+    {
+      count++;
+    }
+  }
+  mitk::Vector3D spacing = m_Image->GetSlicedGeometry()->GetSpacing();
+  m_Volume = count / 1000.0 * spacing[0] * spacing[1] * spacing[2];
 }
--- Core/Algorithms/mitkVolumeCalculator.h
+++ Core/Algorithms/mitkVolumeCalculator.h
@@ -1,19 +1,19 @@
 /*=========================================================================
-
+ 
 Program:   Medical Imaging & Interaction Toolkit
 Module:    $RCSfile$
 Language:  C++
 Date:      $Date$
 Version:   $Revision$
-
+ 
 Copyright (c) German Cancer Research Center, Division of Medical and
 Biological Informatics. All rights reserved.
 See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
-
+ 
 This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
-
+ 
 =========================================================================*/
 
 #ifndef VOLUME_CALCULATOR_H_HEADER_INCLUDED
@@ -26,35 +26,36 @@ PURPOSE.  See the above copyright notices for more information.
 #include "mitkCommon.h"
 #include "mitkImage.h"
 
-namespace mitk {
-
-//##Documentation
-//## @brief Provides an easy way to calculate the volume of a mitk::Image
-//##
-class VolumeCalculator : public itk::Object
+namespace mitk
 {
-public:
-	mitkClassMacro(VolumeCalculator,itk::Object);
 
-	itkNewMacro(Self);  
-	itkSetMacro(Image,mitk::Image::ConstPointer);
-	itkSetMacro(Threshold,int);
-	// TODO: calculate if needed in GetHistogram()
-	void ComputeVolume();
-	itkGetMacro(Volume,float);
-	
-protected:
-	VolumeCalculator();
+  //##Documentation
+  //## @brief Provides an easy way to calculate the volume of a mitk::Image
+  //##
+  class VolumeCalculator : public itk::Object
+  {
+  public:
+    mitkClassMacro(VolumeCalculator,itk::Object);
 
-	virtual ~VolumeCalculator();
-	
-	template < typename TPixel, unsigned int VImageDimension > 
-        void InternalCompute(itk::Image< TPixel, VImageDimension >* itkImage);
-        
-	mitk::Image::ConstPointer m_Image;
-	int m_Threshold;
-	float m_Volume;
-};
+    itkNewMacro(Self);
+    itkSetMacro(Image,mitk::Image::ConstPointer);
+    itkSetMacro(Threshold,int);
+    // TODO: calculate if needed in GetHistogram()
+    void ComputeVolume();
+    itkGetMacro(Volume,float);
+
+  protected:
+    VolumeCalculator();
+
+    virtual ~VolumeCalculator();
+
+    template < typename TPixel, unsigned int VImageDimension >
+    void InternalCompute(itk::Image< TPixel, VImageDimension >* itkImage);
+
+    mitk::Image::ConstPointer m_Image;
+    int m_Threshold;
+    float m_Volume;
+  };
 
 } // namespace mitk
 

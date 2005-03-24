82682c43292bb33745eacb9ac4f085b90183e4b6
robert <robert@nowhere>
2005-03-24 16:31:52 +0000

    no message

---
+++ Build/Tools/FilterFactoryItemGenerator/start.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+public class start {
+	
+	String Title, mitkPath, xmlPath, inputClass, inputDim, outputClass, outputDim;
+
+	
+	public start(String Title, String mitkPath, String xmlPath, 
+			String inputClass, String inputDim, String outputClass, 
+			String outputDim)
+	{
+		this.Title = Title;
+		this.mitkPath = mitkPath;
+		this.xmlPath = xmlPath;
+		this.inputClass = inputClass;
+		this.inputDim = inputDim;
+		this.outputClass = outputClass;
+		this.outputDim = outputDim;
+	}
+	
+	public void run()
+	throws IOException
+	{
+		// copy template and replace title
+		CopyTemplate ct = new CopyTemplate(Title, mitkPath);
+		ct.run();
+		
+		// update various files
+		UpdateFiles uq = new UpdateFiles(Title, mitkPath, xmlPath, inputClass, 
+				inputDim, outputClass, outputDim);
+		uq.run();
+	}
+}

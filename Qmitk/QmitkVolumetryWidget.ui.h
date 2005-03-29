9779a5d761f63d7a751a3e470f702bced7dac9ab
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2005-03-29 13:42:45 +0000

    ENH: time series button

--- Qmitk/QmitkVolumetryWidget.ui.h
+++ Qmitk/QmitkVolumetryWidget.ui.h
@@ -1,19 +1,19 @@
 /*=========================================================================
-
+ 
 Program:   Medical Imaging & Interaction Toolkit
 Module:    $RCSfile$
 Language:  C++
 Date:      $Date$
 Version:   $Revision$
-
+ 
 Copyright (c) German Cancer Research Center, Division of Medical and
 Biological Informatics. All rights reserved.
 See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
-
+ 
 This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
-
+ 
 =========================================================================*/
 /****************************************************************************
 ** ui.h extension file, included from the uic-generated form implementation.
@@ -30,17 +30,54 @@ PURPOSE.  See the above copyright notices for more information.
 
 void QmitkVolumetryWidget::SetDataTreeNode(mitk::DataTreeNode* node)
 {
-  std::cout << "Signal received!!!" << std::endl;
   m_Node = node;
   std::string name;
   if (node->GetName(name))
   {
     m_TreeNodeNameLabel->setText( name.c_str() );
   }
+  m_CalcButton->setEnabled(false);
+  m_TimeSeriesButton->setEnabled(false);
+
+  if (m_Node)
+  {
+    mitk::Image* image = dynamic_cast<mitk::Image*>(m_Node->GetData());
+    if (image)
+    {
+      if (image->GetDimension() == 4)
+      {
+        m_TimeSeriesButton->setEnabled(true);
+      }
+      else
+      {
+        m_CalcButton->setEnabled(true);
+      }
+    }
+  }
 }
 
-
 void QmitkVolumetryWidget::CalculateVolume()
+{
+  if (m_Node)
+  {
+    mitk::Image* image = dynamic_cast<mitk::Image*>(m_Node->GetData());
+    if (image)
+    {
+      std::cout << "Dimension:" << image->GetDimension() << std::endl;
+      std::cout << "Dimension[3]:" << image->GetDimension(3) << std::endl;
+      mitk::VolumeCalculator::Pointer volCalc = mitk::VolumeCalculator::New();
+      volCalc->SetImage(image);
+      volCalc->SetThreshold(m_ThresholdSlider->value());
+      volCalc->ComputeVolume();
+      std::stringstream vs;
+      vs << volCalc->GetVolume() << " ml";
+      m_Result->setText(vs.str().c_str() );
+      std::cout << "Volume:" << vs.str() << std::endl;
+    }
+  }
+}
+
+void QmitkVolumetryWidget::CalculateTimeSeries()
 {
   if (m_Node)
   {
@@ -51,10 +88,15 @@ void QmitkVolumetryWidget::CalculateVolume()
       volCalc->SetImage(image);
       volCalc->SetThreshold(m_ThresholdSlider->value());
       volCalc->ComputeVolume();
+      std::vector<float> volumes = volCalc->GetVolumes();
       std::stringstream vs;
-      vs << volCalc->GetVolume() << " ml";
-      m_Result->setText(vs.str().c_str() );
-      std::cout << "Volume:" << vs.str() << std::endl;
+      int timeStep = 0;
+      for (std::vector<float>::iterator it = volumes.begin(); it != volumes.end(); it++)
+      {
+        vs << timeStep++ << "," << *it << std::endl;
+        // m_Result->setText(vs.str().c_str() );
+      }
+      std::cout << "Result:" << std::endl << vs.str() << std::endl;
     }
   }
 }

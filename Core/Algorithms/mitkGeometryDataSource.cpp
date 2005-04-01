093b61d51403d0123455fb2f194a41a3d9514a9e
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-04-01 16:01:41 +0000

    ADD: base-class fpr GeometryData classes

---
+++ Core/Algorithms/mitkGeometryDataSource.cpp
@@ -0,0 +1,93 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkGeometryDataSource.h"
+#include "mitkGeometryData.h"
+
+mitk::GeometryDataSource::GeometryDataSource()
+{
+  // Create the output. We use static_cast<> here because we know the default
+  // output must be of type TOutputImage
+  mitk::GeometryData::Pointer output
+    = static_cast<mitk::GeometryData*>(this->MakeOutput(0).GetPointer());
+  output->Initialize();
+
+  Superclass::SetNumberOfRequiredOutputs(1);
+  Superclass::SetNthOutput(0, output.GetPointer());
+}
+
+mitk::GeometryDataSource::~GeometryDataSource()
+{
+}
+
+mitk::GeometryDataSource::DataObjectPointer mitk::GeometryDataSource::MakeOutput(unsigned int idx)
+{
+  return static_cast<itk::DataObject*>(mitk::GeometryData::New().GetPointer());
+}
+
+mitk::GeometryData* mitk::GeometryDataSource::GetOutput()
+{
+  if (this->GetNumberOfOutputs() < 1)
+  {
+    return 0;
+  }
+
+  return static_cast<mitk::GeometryData*>
+    (this->BaseProcess::GetOutput(0));
+}
+
+mitk::GeometryData* mitk::GeometryDataSource::GetOutput(unsigned int idx)
+{
+  return static_cast<mitk::GeometryData*>
+    (this->ProcessObject::GetOutput(idx));
+}
+
+void mitk::GeometryDataSource::SetOutput(mitk::GeometryData* output)
+{
+  itkWarningMacro(<< "SetOutput(): This method is slated to be removed from ITK.  Please use GraftOutput() in possible combination with DisconnectPipeline() instead." );
+  BaseProcess::SetNthOutput(0, output);
+}
+
+void mitk::GeometryDataSource::GraftOutput(mitk::GeometryData* graft)
+{
+  this->GraftNthOutput(0, graft);
+}
+
+//##ModelId=3EF56C440197
+void mitk::GeometryDataSource::GraftNthOutput(unsigned int idx, mitk::GeometryData *graft)
+{
+  if (idx < this->GetNumberOfOutputs())
+  {
+    mitk::GeometryData * output = this->GetOutput(idx);
+
+    if (output && graft)
+    {
+      // grab a handle to the bulk data of the specified data object
+      //      output->SetPixelContainer( graft->GetPixelContainer() ); @FIXME!!!!
+
+      // copy the region ivars of the specified data object
+      output->SetRequestedRegion( graft );//graft->GetRequestedRegion() );
+      //      output->SetLargestPossibleRegion( graft->GetLargestPossibleRegion() ); @FIXME!!!!
+      //      output->SetBufferedRegion( graft->GetBufferedRegion() ); @FIXME!!!!
+
+      // copy the meta-information
+      output->CopyInformation( graft );
+    }
+  }
+}
---
+++ Core/Algorithms/mitkGeometryDataSource.h
@@ -0,0 +1,70 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef MITKGEOMETRYDATASOURCE_H_HEADER_INCLUDED_C10B4740
+#define MITKGEOMETRYDATASOURCE_H_HEADER_INCLUDED_C10B4740
+
+#include "mitkBaseProcess.h"
+#include "mitkGeometryDataSource.h"
+
+namespace mitk {
+
+class GeometryData;
+
+//##Documentation
+//## @brief Superclass of all classes generating GeometryData (instances of class
+//## GeometryData) as output. 
+//## @ingroup Process
+//## In itk and vtk the generated result of a ProcessObject is only guaranteed
+//## to be up-to-date, when Update() of the ProcessObject or the generated
+//## DataObject is called immediately before access of the data stored in the
+//## DataObject. This is also true for subclasses of mitk::BaseProcess and thus
+//## for mitk::GeometryDataSource.
+class GeometryDataSource : public BaseProcess
+{
+public:
+  mitkClassMacro(GeometryDataSource, BaseProcess);
+
+  itkNewMacro(Self);  
+
+  typedef itk::DataObject::Pointer DataObjectPointer;
+
+  virtual DataObjectPointer MakeOutput(unsigned int idx);
+
+  void SetOutput(mitk::GeometryData* output);
+
+  mitk::GeometryData* GetOutput();
+
+  mitk::GeometryData* GetOutput(unsigned int idx);
+
+  virtual void GraftOutput(mitk::GeometryData* graft);
+
+  virtual void GraftNthOutput(unsigned int idx, mitk::GeometryData *graft);
+
+protected:
+  GeometryDataSource();
+
+  virtual ~GeometryDataSource();
+};
+
+} // namespace mitk
+
+
+
+#endif /* MITKGEOMETRYDATASOURCE_H_HEADER_INCLUDED_C10B4740 */

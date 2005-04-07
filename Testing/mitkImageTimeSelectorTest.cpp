61dc029c0822bc45db576720baf35471616fb1bd
boettger <boettger@nowhere>
2005-04-07 13:48:53 +0000

    ENH: adding failing test for timeselector

---
+++ Testing/mitkImageTimeSelectorTest.cpp
@@ -0,0 +1,100 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkImage.h"
+#include "mitkDataTreeNodeFactory.h"
+#include "mitkImageTimeSelector.h"
+#include <itksys/SystemTools.hxx>
+
+#include <fstream>
+int mitkImageTimeSelectorTest(int argc, char* argv[])
+{
+
+  std::cout << "Loading file: ";
+  if(argc==0)
+  {
+    std::cout<<"no file specified [FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  mitk::Image::Pointer image = NULL;
+  mitk::DataTreeNodeFactory::Pointer factory = mitk::DataTreeNodeFactory::New();
+  try
+  {
+    factory->SetFileName( argv[1] );
+    factory->Update();
+
+    if(factory->GetNumberOfOutputs()<1)
+    {
+      std::cout<<"file could not be loaded [FAILED]"<<std::endl;
+      return EXIT_FAILURE;
+    }
+    mitk::DataTreeNode::Pointer node = factory->GetOutput( 0 );
+    image = dynamic_cast<mitk::Image*>(node->GetData());
+    if(image.IsNull())
+    {
+      std::cout<<"file not an image - test will not be applied [PASSED]"<<std::endl;
+      std::cout<<"[TEST DONE]"<<std::endl;
+      return EXIT_SUCCESS;
+    }
+  }
+  catch ( itk::ExceptionObject & ex )
+  {
+    std::cout << "Exception: " << ex << "[FAILED]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if( image->IsVolumeSet(0) == false)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+
+
+  //Take a time step
+  mitk::ImageTimeSelector::Pointer timeSelector = mitk::ImageTimeSelector::New();
+  timeSelector->SetInput(image);
+	timeSelector->SetTimeNr( 0 );
+  timeSelector->Update();
+  mitk::Image::Pointer result = timeSelector->GetOutput();
+
+  std::cout << "Testing IsInitialized(): ";
+  if(result->IsInitialized()==false)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+
+  std::cout << "Testing if Volume is set ";
+  //result->DisconnectPipeline();
+  //timeSelector = NULL;
+
+  if( result->IsVolumeSet(0) == false)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+
+
+
+  std::cout<<"[TEST DONE]"<<std::endl;
+  return EXIT_SUCCESS;
+}

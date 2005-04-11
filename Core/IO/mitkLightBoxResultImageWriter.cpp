7818829b74b77bcda407ebbd31d8bd23be6f8e8f
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-04-11 01:56:33 +0000

    ENH: removed obsolete header

--- Core/IO/mitkLightBoxResultImageWriter.cpp
+++ Core/IO/mitkLightBoxResultImageWriter.cpp
@@ -23,7 +23,6 @@ PURPOSE.  See the above copyright notices for more information.
 #include <chili/plugin.h>
 #include <chili/qclightbox.h>
 #include <chili/isg.h>
-#include <ipDicom/ipDicom.h>
 #endif
 
 #include <mitkImage.h>

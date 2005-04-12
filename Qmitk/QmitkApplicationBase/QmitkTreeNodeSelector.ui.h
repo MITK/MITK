f09e9104df70df8772f5bfd9f6a85b95973be1dd
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2005-04-12 12:59:29 +0000

    ENH: new requested method GetSelectedNode

--- Qmitk/QmitkApplicationBase/QmitkTreeNodeSelector.ui.h
+++ Qmitk/QmitkApplicationBase/QmitkTreeNodeSelector.ui.h
@@ -10,7 +10,7 @@
 ** destructor.
 *****************************************************************************/
 
-/** 
+/**
   \class QmitkTreeNodeSelector. A simple ComboXBox for selecting an image node
 */
 
@@ -25,22 +25,26 @@ void QmitkTreeNodeSelector::SetDataTreeNodeIterator( mitk::DataTreeIteratorClone
 void QmitkTreeNodeSelector::UpdateContent()
 {
   // iteriere ueber Baum und wende filter funktion an
-  CommonFunctionality::DataTreeNodeVector images = CommonFunctionality::FilterNodes(m_DataTreeIteratorClone,m_FilterFunction);    
+  CommonFunctionality::DataTreeNodeVector images = CommonFunctionality::FilterNodes(m_DataTreeIteratorClone,m_FilterFunction);
   QString currentText = m_ComboBox->currentText();
   while (m_ComboBox->count()) { m_ComboBox->removeItem(0); }
-  m_TreeNodes.clear(); 
-  for (CommonFunctionality::DataTreeNodeVector::iterator it = images.begin(); it != images.end() ; it++ ) {
+  m_TreeNodes.clear();
+  for (CommonFunctionality::DataTreeNodeVector::iterator it = images.begin(); it != images.end() ; it++ )
+  {
     std::string name;
-    if ((*it)->GetName(name)) {
+    if ((*it)->GetName(name))
+    {
       m_ComboBox->insertItem( name.c_str() );
       m_TreeNodes[name] =*it;
-      if (currentText.find(name.c_str()) != -1 ) {
+      if (currentText.find(name.c_str()) != -1 )
+      {
         m_ComboBox->setCurrentText(currentText);
       }
     }
   }
   QString newText = m_ComboBox->currentText();
-  if (currentText != newText) {
+  if (currentText != newText)
+  {
     TreeNodeSelected(newText);
   }
 }
@@ -48,17 +52,29 @@ void QmitkTreeNodeSelector::UpdateContent()
 
 bool QmitkTreeNodeSelector::IsImageNode( mitk::DataTreeNode * node )
 {
-    return node && node->GetData() && dynamic_cast<mitk::Image*>(node->GetData());
+  return node && node->GetData() && dynamic_cast<mitk::Image*>(node->GetData());
 }
 
 void QmitkTreeNodeSelector::init()
 {
-   m_FilterFunction = QmitkTreeNodeSelector::IsImageNode;
+  m_FilterFunction = QmitkTreeNodeSelector::IsImageNode;
 }
 
 
 void QmitkTreeNodeSelector::TreeNodeSelected( const QString &name )
 {
-   emit Activated(m_TreeNodes[name.ascii()]);
+  emit Activated(m_TreeNodes[name.ascii()]);
+}
+
+mitk::DataTreeNode * QmitkTreeNodeSelector::GetSelectedNode()
+{
+  if (m_ComboBox && !m_ComboBox->currentText().isEmpty())
+  {
+    return m_TreeNodes[m_ComboBox->currentText().ascii()];
+  }
+  else
+  {
+    return NULL;
+  }
 }
 

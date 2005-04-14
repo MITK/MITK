c9356c6d24a0567889993a5fb4d0a2e6b9bba9be
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2005-04-14 14:47:31 +0000

    CHG: SetDataTreeNode now initializes Canvas

--- Qmitk/QmitkTransferFunctionWidget.ui.h
+++ Qmitk/QmitkTransferFunctionWidget.ui.h
@@ -13,34 +13,45 @@
 
 void QmitkTransferFunctionWidget::SetDataTreeNode( mitk::DataTreeNode* node)
 {
-    if (mitk::TransferFunctionProperty::Pointer tranferFunctionProp = dynamic_cast<mitk::TransferFunctionProperty*>(node->GetProperty("TransferFunction").GetPointer())) {
-        
-    }
-      
-}
-
-
-void QmitkTransferFunctionWidget::PropertyChange( QListViewItem * PropertyItem )
-{
-  std::cout << "Property Selection in TransferFunctionWidget Changed" << std::endl;
-  if (NodeViewPropertyItem* nvpi = dynamic_cast<NodeViewPropertyItem*>(PropertyItem)) {
-    mitk::TransferFunctionProperty* tfp = dynamic_cast<mitk::TransferFunctionProperty*>(nvpi->GetProperty().GetPointer()); 
-    if (tfp) {
-      mitk::TransferFunction* tf = dynamic_cast<mitk::TransferFunction*>(tfp->GetValue().GetPointer());   
-      if (tf) {
+  if (mitk::TransferFunctionProperty::Pointer tfp = dynamic_cast<mitk::TransferFunctionProperty*>(node->GetProperty("TransferFunction").GetPointer()))
+  {
+    if (tfp)
+    {
+      mitk::TransferFunction* tf = dynamic_cast<mitk::TransferFunction*>(tfp->GetValue().GetPointer());
+      if (tf)
+      {
         std::cout << "TF access" << std::endl;
         m_TransferFunctionCanvas->SetTransferFunction(tf);
-	UpdateMinMaxLabels();
+        UpdateMinMaxLabels();
       }
     }
   }
 }
 
+void QmitkTransferFunctionWidget::PropertyChange( QListViewItem * PropertyItem )
+{
+  std::cout << "Property Selection in TransferFunctionWidget Changed" << std::endl;
+  if (NodeViewPropertyItem* nvpi = dynamic_cast<NodeViewPropertyItem*>(PropertyItem))
+  {
+    mitk::TransferFunctionProperty* tfp = dynamic_cast<mitk::TransferFunctionProperty*>(nvpi->GetProperty().GetPointer());
+    if (tfp)
+    {
+      mitk::TransferFunction* tf = dynamic_cast<mitk::TransferFunction*>(tfp->GetValue().GetPointer());
+      if (tf)
+      {
+        std::cout << "TF access" << std::endl;
+        m_TransferFunctionCanvas->SetTransferFunction(tf);
+        UpdateMinMaxLabels();
+      }
+    }
+  }
+}
 
 void QmitkTransferFunctionWidget::UpdateMinMaxLabels()
 {
-    if (m_TransferFunctionCanvas->GetTransferFunction().IsNotNull()) {
-      m_MinLabel->setText(QString::number(m_TransferFunctionCanvas->GetTransferFunction()->GetMin()));
-      m_MaxLabel->setText(QString::number(m_TransferFunctionCanvas->GetTransferFunction()->GetMax()));
-    }
+  if (m_TransferFunctionCanvas->GetTransferFunction().IsNotNull())
+  {
+    m_MinLabel->setText(QString::number(m_TransferFunctionCanvas->GetTransferFunction()->GetMin()));
+    m_MaxLabel->setText(QString::number(m_TransferFunctionCanvas->GetTransferFunction()->GetMax()));
+  }
 }

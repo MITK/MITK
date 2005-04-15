7aa76c2796d07bf8e8b38b1327390b7688f97bb9
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2005-04-15 09:10:53 +0000

    CHG/ENH: use the new PropertyListView

--- QFunctionalities/QmitkDataManager/QmitkDataManagerControls.ui.h
+++ QFunctionalities/QmitkDataManager/QmitkDataManagerControls.ui.h
@@ -1,19 +1,19 @@
 /*=========================================================================
-
+ 
 Program:   Medical Imaging & Interaction Toolkit
 Module:    $RCSfile$
 Language:  C++
 Date:      $Date$
 Version:   $Revision$
-
+ 
 Copyright (c) German Cancer Research Center, Division of Medical and
 Biological Informatics. All rights reserved.
 See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
-
+ 
 This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
-
+ 
 =========================================================================*/
 
 
@@ -45,21 +45,24 @@ PURPOSE.  See the above copyright notices for more information.
 #include "mitkChiliPlugin.h"
 #include "mitkLightBoxResultImageWriter.h"
 
-void QmitkDataManagerControls::init() { 
+void QmitkDataManagerControls::init()
+{
   m_DataTreeIterator = NULL;
-  while (m_DataTreeView->columns() > 0 ) {
- m_DataTreeView->removeColumn(0);
+  while (m_DataTreeView->columns() > 0 )
+  {
+    m_DataTreeView->removeColumn(0);
   }
   m_DataTreeView->addColumn( "Name" );
   m_DataTreeView->addColumn( "NodeType" );
   m_DataTreeView->addColumn( "RefCount");
   m_DataTreeView->setSortColumn(20);
   mitk::GlobalInteraction* globalInteraction = dynamic_cast<mitk::GlobalInteraction*> (mitk::EventMapper::GetGlobalStateMachine());
-  if (globalInteraction) {
+  if (globalInteraction)
+  {
     mitk::FocusManager* fm = globalInteraction->GetFocusManager();
     QmitkFocusChangeCommand::Pointer fcc = QmitkFocusChangeCommand::New();
-    fcc->SetQmitkDataManagerControls(this); 
-    mitk::FocusEvent fe; 
+    fcc->SetQmitkDataManagerControls(this);
+    mitk::FocusEvent fe;
 
     fm->AddObserver(fe,fcc);
   }
@@ -67,57 +70,66 @@ void QmitkDataManagerControls::init() {
     m_SaveToLightBox->hide();
 }
 
-void QmitkDataManagerControls::destroy() 
+void QmitkDataManagerControls::destroy()
 {
-
 }
 
-// init the combobox with all   
-void QmitkDataManagerControls::UpdateRendererCombo() {
-  mitk::RenderWindow* focusedRenderWindow = NULL;  
+// init the combobox with all
+void QmitkDataManagerControls::UpdateRendererCombo()
+{
+  mitk::RenderWindow* focusedRenderWindow = NULL;
 
-  mitk::GlobalInteraction* globalInteraction = 
+  mitk::GlobalInteraction* globalInteraction =
     dynamic_cast<mitk::GlobalInteraction*> (mitk::EventMapper::GetGlobalStateMachine());
-  if (globalInteraction) {
+  if (globalInteraction)
+  {
     mitk::FocusManager* fm = globalInteraction->GetFocusManager();
     mitk::BaseRenderer::ConstPointer br = fm->GetFocused();
-    if (br.IsNotNull()) {
+    if (br.IsNotNull())
+    {
       focusedRenderWindow = br->GetRenderWindow();
     }
   }
 
- m_RenderWindowCombo->clear(); 
+  m_RenderWindowCombo->clear();
   const mitk::RenderWindow::RenderWindowSet rws = mitk::RenderWindow::GetInstances();
-  for (mitk::RenderWindow::RenderWindowSet::const_iterator iter = rws.begin();iter != rws.end();iter++) {
-    if ((*iter)->GetName()) {
+  for (mitk::RenderWindow::RenderWindowSet::const_iterator iter = rws.begin();iter != rws.end();iter++)
+  {
+    if ((*iter)->GetName())
+    {
       m_RenderWindowCombo->insertItem(QString((*iter)->GetName()),0);
-//        if ((*iter) == focusedRenderWindow) {
-//          m_RenderWindowCombo->setCurrentItem(0);
-//        }
+      //        if ((*iter) == focusedRenderWindow) {
+      //          m_RenderWindowCombo->setCurrentItem(0);
+      //        }
     }
- 
+
   }
-  if (focusedRenderWindow) {
-    m_RenderWindowCombo->setCurrentText(focusedRenderWindow->GetName()); 
+  if (focusedRenderWindow)
+  {
+    m_RenderWindowCombo->setCurrentText(focusedRenderWindow->GetName());
     // m_RenderWindowCombo->activated(focusedRenderWindow->name());
-  } else {
+  }
+  else
+  {
     m_RenderWindowCombo->setCurrentText("no focused window");
   }
-  m_RendererPropertiesView->SetRenderWindow(m_RenderWindowCombo->currentText()); 
+  // TODO: new interface
+  //  m_RendererPropertiesView->SetRenderWindow(m_RenderWindowCombo->currentText());
 }
 void QmitkDataManagerControls::SetDataTreeIterator(mitk::DataTreeIteratorBase* it)
 {
-    if (it == NULL) return;
-  m_GlobalNodePropertiesView->SetDataTreeNode(NULL);
-  m_RendererPropertiesView->SetDataTreeNode(NULL);
+  if (it == NULL) return;
+  m_NodePropertiesView->SetPropertyList(NULL);
+  m_RendererPropertiesView->SetPropertyList(NULL);
 
-    while (m_DataTreeView->firstChild()) {
-     delete m_DataTreeView->firstChild();
-     }
-    m_DataTreeIterator = it;
-    mitk::DataTreeIteratorClone tempIt = m_DataTreeIterator;
+  while (m_DataTreeView->firstChild())
+  {
+    delete m_DataTreeView->firstChild();
+  }
+  m_DataTreeIterator = it;
+  mitk::DataTreeIteratorClone tempIt = m_DataTreeIterator;
 
-  if (!tempIt->IsAtEnd()) 
+  if (!tempIt->IsAtEnd())
   {
     QmitkDataTreeViewItem * rootItem = new QmitkDataTreeViewItem(m_DataTreeView, "Loaded Data", "root", tempIt.GetPointer());
     ++tempIt;
@@ -128,28 +140,32 @@ void QmitkDataManagerControls::SetDataTreeIterator(mitk::DataTreeIteratorBase* i
 void QmitkDataManagerControls::RemoveButtonClicked()
 {
   QmitkDataTreeViewItem *selected = dynamic_cast<QmitkDataTreeViewItem*>(m_DataTreeView->selectedItem());
-  if (selected == NULL) {
-  } else {
+  if (selected == NULL)
+  {}
+  else
+  {
     mitk::DataTreeIteratorClone selectedIterator = selected->GetDataTreeIterator();
     assert(selectedIterator.IsNotNull());
     delete selected;
     selectedIterator->Remove();
     mitk::RenderWindow::UpdateAllInstances();
-  } 
+  }
 }
 
 
 void QmitkDataManagerControls::SaveButton_clicked()
 {
   QmitkDataTreeViewItem *selected = dynamic_cast<QmitkDataTreeViewItem*>(m_DataTreeView->selectedItem());
-  if (selected != NULL) {
+  if (selected != NULL)
+  {
     std::string selectedItemsName = std::string(selected->text(0).ascii());
-  
+
     mitk::DataTreeIteratorBase* selectedIterator = selected->GetDataTreeIterator();
     if (selectedIterator != NULL)
     {
       mitk::DataTreeNode* node = selectedIterator->Get();
-      if (node != NULL ){
+      if (node != NULL )
+      {
         mitk::BaseData::Pointer data=node->GetData();
 
         if (data.IsNotNull())
@@ -166,7 +182,7 @@ void QmitkDataManagerControls::SaveButton_clicked()
             selectedItemsName = itksys::SystemTools::GetFilenameWithoutExtension(selectedItemsName);
             selectedItemsName += ".mps";
             QString fileName = QFileDialog::getSaveFileName(QString(selectedItemsName.c_str()),"MITK Point-Sets (*.mps)");
-            if (fileName != NULL ) 
+            if (fileName != NULL )
             {
               mitk::PointSetWriter::Pointer writer = mitk::PointSetWriter::New();
               writer->SetInput( pointset );
@@ -181,7 +197,9 @@ void QmitkDataManagerControls::SaveButton_clicked()
           }
         }
       }
-    } else {
+    }
+    else
+    {
       std::cout << "logical error" << std::endl;
     }
   }
@@ -193,9 +211,9 @@ void QmitkDataManagerControls::SaveLightBox_clicked()
   if(mitk::ChiliPlugin::IsPlugin()==false)
   {
     QMessageBox::critical( this, "Save Selected to LightBox",
-      "Saving to LightBox not possible:\n"
-      "This is not a plugin!! ... or the plugin is not correctly initialized!", 
-      QMessageBox::Cancel|QMessageBox::Default|QMessageBox::Escape,QMessageBox::NoButton );
+                           "Saving to LightBox not possible:\n"
+                           "This is not a plugin!! ... or the plugin is not correctly initialized!",
+                           QMessageBox::Cancel|QMessageBox::Default|QMessageBox::Escape,QMessageBox::NoButton );
     return;
   }
 
@@ -222,8 +240,8 @@ void QmitkDataManagerControls::SaveLightBox_clicked()
             if(lbwriter->SetSourceByTreeSearch(selectedIterator)==false)
             {
               QMessageBox::critical( this, "Save Selected to LightBox",
-                "Saving to LightBox not possible:\n"
-                "Unable to find parent image that came from Chili.", QMessageBox::Cancel|QMessageBox::Default|QMessageBox::Escape,QMessageBox::NoButton );
+                                     "Saving to LightBox not possible:\n"
+                                     "Unable to find parent image that came from Chili.", QMessageBox::Cancel|QMessageBox::Default|QMessageBox::Escape,QMessageBox::NoButton );
               return;
             }
             lbwriter->SetLightBoxToCurrentLightBox();
@@ -239,7 +257,7 @@ void QmitkDataManagerControls::SaveLightBox_clicked()
 void QmitkDataManagerControls::m_ReInitButton_clicked()
 {
   QmitkDataTreeViewItem *selected = dynamic_cast<QmitkDataTreeViewItem*>(m_DataTreeView->selectedItem());
-  if (selected != NULL) 
+  if (selected != NULL)
   {
     mitk::DataTreeIteratorClone selectedIterator = selected->GetDataTreeIterator();
     assert(selectedIterator.IsNotNull());
@@ -250,14 +268,32 @@ void QmitkDataManagerControls::m_ReInitButton_clicked()
       emit InitializeStandardViews( basedata );
       mitk::RenderWindow::UpdateAllInstances();
     }
-  } 
+  }
 }
 
 
 void QmitkDataManagerControls::TreeSelectionChanged( QListViewItem * item )
 {
- QmitkDataTreeViewItem* dtvi = dynamic_cast<QmitkDataTreeViewItem*>(item);
+  QmitkDataTreeViewItem* dtvi = dynamic_cast<QmitkDataTreeViewItem*>(item);
   assert(dtvi != NULL);
   assert(dtvi->GetDataTreeNode().IsNotNull());
-  m_TransferFunctionWidget->SetDataTreeNode(dtvi->GetDataTreeNode());
+  m_TransferFunctionWidget->SetDataTreeNode(dtvi->GetDataTreeNode());  m_NodePropertiesView->SetPropertyList(dtvi->GetDataTreeNode()->GetPropertyList());
+
+  mitk::RenderWindow* focusedRenderWindow = NULL;
+
+  mitk::GlobalInteraction* globalInteraction =
+    dynamic_cast<mitk::GlobalInteraction*> (mitk::EventMapper::GetGlobalStateMachine());
+  if (globalInteraction)
+  {
+    mitk::FocusManager* fm = globalInteraction->GetFocusManager();
+    mitk::BaseRenderer::ConstPointer br = fm->GetFocused();
+    if (br.IsNotNull())
+    {
+      focusedRenderWindow = br->GetRenderWindow();
+    }
+  }
+  if (focusedRenderWindow)
+  {
+    m_RendererPropertiesView->SetPropertyList(dtvi->GetDataTreeNode()->GetPropertyList(focusedRenderWindow->GetRenderer()));
+  }
 }

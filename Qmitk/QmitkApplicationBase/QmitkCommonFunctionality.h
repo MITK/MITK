2fcff1587c23270d3dc9e99c12530bb2738f91f1
liqun <liqun@nowhere>
2005-04-18 15:19:39 +0000

    added SaveBaseData(), Save(Un)DirectedVesselGraph().

--- Qmitk/QmitkApplicationBase/QmitkCommonFunctionality.h
+++ Qmitk/QmitkApplicationBase/QmitkCommonFunctionality.h
@@ -43,6 +43,13 @@ PURPOSE.  See the above copyright notices for more information.
 #include <mitkImageCast.h>
 #include <mitkDataTree.h>
 #include <mitkPicFileReader.h>
+#include "mitkPointSetWriter.h"
+#include "itkImage.h"
+
+#ifdef MBI_INTERNAL
+  #include "mitkVesselGraphFileWriter.h"
+#endif
+
 
 #include <qfiledialog.h>
 #include "ipPic/ipPic.h"
@@ -568,6 +575,113 @@ static QString SaveImage(mitk::Image* image)
           }
         }
     }
+#ifdef MBI_INTERNAL
+  /**
+   * Saves the given directed vessel graph to a file. If no name is provided, the
+   * user is prompted for a file name.
+   */
+  static void SaveDirectedVesselGraph( mitk::DirectedVesselGraphData* graph, std::string name = "" )
+  {
+    if(graph == NULL)
+    {
+      std::cout << "Warning in file " << __FILE__<< " line " << __LINE__ <<": vessel graph is NULL!" << std::endl;
+      return;
+    }
+    QString fileName = name.c_str();
+    if (fileName == "")
+    {
+        fileName = QFileDialog::getSaveFileName(QString("VesselGraph.dvg"),"MITK VesselGraph (*.dvg)");
+    }
+    if (fileName != NULL ) 
+    {
+      mitk::VesselGraphFileWriter<Directed>::Pointer writer = mitk::VesselGraphFileWriter<Directed>::New();
+      writer->SetInput( graph );
+      writer->SetFileName( fileName.ascii() );
+      writer->Update();
+    }
+  }
+
+  /**
+   * Saves the given undirected vessel graph to a file. If no name is provided, the
+   * user is prompted for a file name.
+   */
+  static void SaveUndirectedVesselGraph( mitk::UndirectedVesselGraphData* graph, std::string name = "" )
+  {
+    if(graph == NULL)
+    {
+      std::cout << "Warning in file " << __FILE__<< " line " << __LINE__ <<": vessel graph is NULL!" << std::endl;
+      return;
+    }
+    QString fileName = name.c_str();
+    if (fileName == "")
+    {
+        fileName = QFileDialog::getSaveFileName(QString("VesselGraph.uvg"),"MITK VesselGraph (*.uvg)");
+    }
+    if (fileName != NULL ) 
+    {
+      mitk::VesselGraphFileWriter<Undirected>::Pointer writer = mitk::VesselGraphFileWriter<Undirected>::New();
+      writer->SetInput( graph );
+      writer->SetFileName( fileName.ascii() );
+      writer->Update();
+    }
+  }
+#endif
+
+  /**
+   * Saves the given mitk::BaseData to a file. The user is prompted to
+   * enter a file name. Currently only mitk::Image, mitk::Surface, mitk::PointSet and
+   * mitk::VesselGraphData are supported. This function is deprecated
+   * until the save-problem is solved by means of a Save-Factory or any
+   * other "nice" mechanism
+   */
+    static void SaveBaseData( mitk::BaseData* data, std::string name = "" )
+  {
+    if (data != NULL)
+    {
+      mitk::Image::Pointer image = dynamic_cast<mitk::Image*>(data);
+      if(image.IsNotNull())
+      {
+        typedef itk::Image<int,3> ImageType;
+        CommonFunctionality::SaveImage< ImageType >(image);
+      }
+
+      mitk::PointSet::Pointer pointset = dynamic_cast<mitk::PointSet*>(data);
+      if(pointset.IsNotNull())
+      {
+        name = itksys::SystemTools::GetFilenameWithoutExtension(name);
+        name += ".mps";
+        QString fileName = QFileDialog::getSaveFileName(QString(name.c_str()),"MITK Point-Sets (*.mps)");
+        if (fileName != NULL )
+        {
+          mitk::PointSetWriter::Pointer writer = mitk::PointSetWriter::New();
+          writer->SetInput( pointset );
+          writer->SetFileName( fileName.ascii() );
+          writer->Update();
+        }
+      }
+
+      mitk::Surface::Pointer surface = dynamic_cast<mitk::Surface*>(data);
+      if(surface.IsNotNull())
+      {
+        CommonFunctionality::SaveSurface(surface, "SurfaceModel.stl");
+      }
+
+    #ifdef MBI_INTERNAL
+      mitk::UndirectedVesselGraphData::Pointer uvg = dynamic_cast<mitk::UndirectedVesselGraphData*>(data);
+      if (uvg.IsNotNull())
+      {
+        CommonFunctionality::SaveUndirectedVesselGraph(uvg);
+      }
+
+      mitk::DirectedVesselGraphData::Pointer dvg = dynamic_cast<mitk::DirectedVesselGraphData*>(data);
+      if (dvg.IsNotNull())
+      {
+        CommonFunctionality::SaveDirectedVesselGraph(dvg);
+      }
+    #endif
+    }
+  }
+
 
 	static mitk::DataTreeIteratorBase* GetIteratorToFirstImage(mitk::DataTreeIteratorBase* dataTreeIterator)
 	{

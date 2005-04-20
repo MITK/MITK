6d0e35d49967073f7a7ddbb7336ba1881894fcbb
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2005-04-20 09:34:29 +0000

    ENH: reformatted

--- Core/Interactions/mitkEventDescription.h
+++ Core/Interactions/mitkEventDescription.h
@@ -26,32 +26,31 @@ PURPOSE.  See the above copyright notices for more information.
 
 namespace mitk {
 
-//##ModelId=3E5A3910009B
-//##Documentation
-//## @brief adds additional Information (Name and EventID) to an Event
-//## @ingroup Interaction
-//## A name and an ID is added to the information of an event, so the event can
-//## be processed futher on.
-class EventDescription : public Event
-{
+  //##ModelId=3E5A3910009B
+  //##Documentation
+  //## @brief adds additional Information (Name and EventID) to an Event
+  //## @ingroup Interaction
+  //## A name and an ID is added to the information of an event, so the event can
+  //## be processed futher on.
+  class EventDescription : public Event
+  {
   public:
     //##ModelId=3E5B30A30095
-	EventDescription(int type, int button, int buttonState,int key, std::string name, int id);
+    EventDescription(int type, int button, int buttonState,int key, std::string name, int id);
 
     //##ModelId=3E5B3103030A
-	std::string GetName() const;
+    std::string GetName() const;
 
     //##ModelId=3E5B3132027C
     int GetId() const;
 
   private:
     //##ModelId=3E5B2F66020C
-	std::string m_Name;
+    std::string m_Name;
 
     //##ModelId=3E5B2F660220
     int m_Id;
-
-};
+  };
 
 } // namespace mitk
 

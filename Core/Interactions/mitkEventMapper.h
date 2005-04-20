bf77b736d0c19ba51e11ef31eb587f4252b82cf0
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2005-04-20 09:36:12 +0000

    ENH: reformatted; changed two includes in .h into class declarations and moved to .cpp

--- Core/Interactions/mitkEventMapper.h
+++ Core/Interactions/mitkEventMapper.h
@@ -23,164 +23,167 @@ PURPOSE.  See the above copyright notices for more information.
 #include "mitkEvent.h"
 #include "mitkCommon.h"
 #include "mitkEventDescription.h"
-#include "mitkStateEvent.h"
-#include "mitkStateMachine.h"
 #include <vtkXMLParser.h> 
 #include <vector>
 #include <string>
 #include <map>
 
 namespace mitk {
-struct ltstr
-{
-  bool operator()(const char* s1, const char* s2) const
+  struct ltstr
   {
-    return strcmp(s1, s2) < 0;
-  }
-};
-//for friendship wor to set the stateevent after calculating
-class GlobalInteraction;
+    bool operator()(const char* s1, const char* s2) const
+    {
+      return strcmp(s1, s2) < 0;
+    }
+  };
+  //for friendship wor to set the stateevent after calculating
+  class GlobalInteraction;
+  class StateMachine;
+  class StateEvent;
 
-//##ModelId=3E5A390401F2
-//##Documentation
-//## @brief Maps an Event to its description 
-//## @ingroup Interaction
-//## 
-//## EventMapping:
-//## This class mapps the Events, usually given by the OS or here by QT, to a MITK internal EventId.
-//## It loads all information from the xml-file (possible, understandable Events with the mitkEventID).
-//## If an event appears, the method MapEvent is called with the event params.
-//## This Method looks up the event params, and tries to find an mitkEventId to it.
-//## If yes, then sends the event and the found ID to the globalStateMachine, which handles all 
-//## further operations of that event.
-//## For Undo-Mechanism a statechanging StateMachine::HandleEvent is connected to an ObjectEventID and an GroupEventId.
-//## That way a fine an raw Undo is possible (fine for ObjectID by ObjectID, raw for GroupID for GroupID)
-//## Here the ObjectEventID gets increased, 
-//## not the GroupEventId(must get increased by a StateMachine, that has the information when a new Group of operation starts)
-class EventMapper : public vtkXMLParser
-{
+  //##ModelId=3E5A390401F2
+  //##Documentation
+  //## @brief Maps an Event to its description 
+  //## @ingroup Interaction
+  //## 
+  //## EventMapping:
+  //## This class mapps the Events, usually given by the OS or here by QT, to a MITK internal EventId.
+  //## It loads all information from the xml-file (possible, understandable Events with the mitkEventID).
+  //## If an event appears, the method MapEvent is called with the event params.
+  //## This Method looks up the event params, and tries to find an mitkEventId to it.
+  //## If yes, then sends the event and the found ID to the globalStateMachine, which handles all 
+  //## further operations of that event.
+  //## For Undo-Mechanism a statechanging StateMachine::HandleEvent is connected to an ObjectEventID and an GroupEventId.
+  //## That way a fine an raw Undo is possible (fine for ObjectID by ObjectID, raw for GroupID for GroupID)
+  //## Here the ObjectEventID gets increased, 
+  //## not the GroupEventId(must get increased by a StateMachine, that has the information when a new Group of operation starts)
+  class EventMapper : public vtkXMLParser
+  {
   public:
-  //##ModelId=3F02F896006D
-  EventMapper();
-  //##ModelId=3F02F896008C
-  ~EventMapper();
+    //##ModelId=3F02F896006D
+    EventMapper();
+    //##ModelId=3F02F896008C
+    ~EventMapper();
 
-	//##ModelId=3F0177080269
-	typedef std::vector<mitk::EventDescription> EventDescriptionVec;
-	//##ModelId=3F0177080288
-	typedef std::vector<mitk::EventDescription>::iterator EventDescriptionVecIter;
+    //##ModelId=3F0177080269
+    typedef std::vector<mitk::EventDescription> EventDescriptionVec;
+    //##ModelId=3F0177080288
+    typedef std::vector<mitk::EventDescription>::iterator EventDescriptionVecIter;
 
-  typedef std::map<const char*, int, ltstr> ConstMap;
-  typedef std::map<const char*, int, ltstr>::iterator ConstMapIter;
+    typedef std::map<const char*, int, ltstr> ConstMap;
+    typedef std::map<const char*, int, ltstr>::iterator ConstMapIter;
 
-  //##ModelId=3E5B349600CB
-  //##Documentation
-	//## set the global StateMachine. If temporaryly changed,
-	//## then copy the old statemachine with GetStateMachine()
-  static void SetGlobalStateMachine(StateMachine* stateMachine);
+    //##ModelId=3E5B349600CB
+    //##Documentation
+    //## set the global StateMachine. If temporaryly changed,
+    //## then copy the old statemachine with GetStateMachine()
+    static void SetGlobalStateMachine(StateMachine* stateMachine);
 
-  //##ModelId=3EDCA746026F
-	static StateMachine* GetGlobalStateMachine();
+    //##ModelId=3EDCA746026F
+    static StateMachine* GetGlobalStateMachine();
 
-  //##ModelId=3E5B34CF0041
-	//##Documentation
-	//## searches the Event in m_EventDescription
-	//## and if included transmitts the event to globalInteraction
-  static bool MapEvent(Event* event);
+    //##ModelId=3E5B34CF0041
+    //##Documentation
+    //## searches the Event in m_EventDescription
+    //## and if included transmitts the event to globalInteraction
+    static bool MapEvent(Event* event);
 
-  //##Documentation
-	//## maps the Event in m_EventDescription with the ID
-	//## and if found returns the Id,
-  //## if not found it returns -1
-  static StateEvent* RefreshStateEvent(StateEvent* stateEvent);
+    //##Documentation
+    //## maps the Event in m_EventDescription with the ID
+    //## and if found returns the Id,
+    //## if not found it returns -1
+    static StateEvent* RefreshStateEvent(StateEvent* stateEvent);
 
-  //##ModelId=3E5B35140072
-	//##Documentation
-	//## loads an XML-File containing Events into m_EventDescriptions
-	//## also involved: EventMapper::startEvent(...)
-	static bool LoadBehavior(std::string fileName);
+    //##ModelId=3E5B35140072
+    //##Documentation
+    //## loads an XML-File containing Events into m_EventDescriptions
+    //## also involved: EventMapper::startEvent(...)
+    static bool LoadBehavior(std::string fileName);
 
-  //##Documentation
-  //## Try to load standard behavior file "StateMachine.xml"
-  //##
-  //## Search strategy:
-  //## \li try environment variable "MITKCONF" (path to "StateMachine.xml")
-  //## \li try "./StateMachine.xml"
-  //## \li try via source directory (using MITKROOT from cmake-created 
-  //## mitkConfig.h) "MITKROOT/Interactions/mitkBaseInteraction/StateMachine.xml"
-  static bool LoadStandardBehavior();
+    //##Documentation
+    //## Try to load standard behavior file "StateMachine.xml"
+    //##
+    //## Search strategy:
+    //## \li try environment variable "MITKCONF" (path to "StateMachine.xml")
+    //## \li try "./StateMachine.xml"
+    //## \li try via source directory (using MITKROOT from cmake-created 
+    //## mitkConfig.h) "MITKROOT/Interactions/mitkBaseInteraction/StateMachine.xml"
+    static bool LoadStandardBehavior();
 
-  //##ModelId=3E788FC00308
-	//##Documentation
-	//## reads a Tag from an XML-file
-	//## adds Events to m_EventDescription
+    //##ModelId=3E788FC00308
+    //##Documentation
+    //## reads a Tag from an XML-file
+    //## adds Events to m_EventDescription
 
-	//##ModelId=3E7B20EE01F5
-  std::string GetStyleName();
+    //##ModelId=3E7B20EE01F5
+    std::string GetStyleName();
 
-  //friendship because of SetStateEvent for computing WorldCoordinates
-  friend class mitk::GlobalInteraction;
+    //friendship because of SetStateEvent for computing WorldCoordinates
+    friend class mitk::GlobalInteraction;
 
   protected:
-  //##Documentation
-	//##@brief method only for GlobalInteraction to change the Event (from DiplayPositionEvent to PositionEvent)
-  static void SetStateEvent(Event* event);
+    //##Documentation
+    //##@brief method only for GlobalInteraction to change the Event (from DiplayPositionEvent to PositionEvent)
+    static void SetStateEvent(Event* event);
 
   private:
 
-  //##
-  void  StartElement (const char *elementName, const char **atts);
+    //##Documentation
+    //## @brief method used in XLM-Reading; gets called when a start-tag is read
+    void  StartElement (const char *elementName, const char **atts);
 
-  //##
-  std::string ReadXMLStringAttribut( std::string name, const char** atts);
+    //##Documentation
+    //## @brief reads an XML-String-Attribute
+    std::string ReadXMLStringAttribut( std::string name, const char** atts);
 
-  //##
-  int ReadXMLIntegerAttribut( std::string name, const char** atts );
-  //##Documentation
-  //## @brief converts the strings given by the XML-Behaviour-File to int
-  inline const int convertConstString2ConstInt(std::string input);
+    //##Documentation
+    //## @brief reads an XML-Integer-Attribute
+    int ReadXMLIntegerAttribut( std::string name, const char** atts );
 
-  //##Documentation
-  //## @brief maps the strings to int for convertion from XML-Behaviour-File
-  ConstMap m_EventConstMap;
+    //##Documentation
+    //## @brief converts the strings given by the XML-Behaviour-File to int
+    inline const int convertConstString2ConstInt(std::string input);
 
-  //##ModelId=3E5B33F303CA
-  //##Documentation
-  //## @brief stores the information for the connection between QT-Events and the internal EventId.
-  //## gets this information from xml-File
-	static EventDescriptionVec m_EventDescriptions;
+    //##Documentation
+    //## @brief maps the strings to int for convertion from XML-Behaviour-File
+    ConstMap m_EventConstMap;
 
-  //##ModelId=3E5B343701F1
-  static StateMachine *m_GlobalStateMachine;
+    //##ModelId=3E5B33F303CA
+    //##Documentation
+    //## @brief stores the information for the connection between QT-Events and the internal EventId.
+    //## gets this information from xml-File
+    static EventDescriptionVec m_EventDescriptions;
 
-	//##ModelId=3E956E3A036B
-  static StateEvent m_StateEvent;
+    //##ModelId=3E5B343701F1
+    static StateMachine *m_GlobalStateMachine;
 
-	//##ModelId=3E7B1EB800CC
-  //##Documentation
-  //## @brief stores the name of the Event-Style loaded
-  static std::string m_StyleName;
+    //##ModelId=3E956E3A036B
+    static StateEvent m_StateEvent;
 
-  //##ModelId=3E788FC000E5
-	static const std::string STYLE;
-  //##ModelId=3E788FC0025C
-	static const std::string NAME;
-  //##ModelId=3E788FC002AA
-	static const std::string ID;
-  //##ModelId=3E785B1B00FD
-	static const std::string TYPE;
-  //##ModelId=3E785B1B015B
-	static const std::string BUTTON;
-  //##ModelId=3E8B08FA01AA
-  static const std::string BUTTONSTATE;
-  //##ModelId=3E785B1B01A9
-	static const std::string KEY;
-  //##
-  static const std::string EVENTS;
+    //##ModelId=3E7B1EB800CC
+    //##Documentation
+    //## @brief stores the name of the Event-Style loaded
+    static std::string m_StyleName;
 
-  //##
-  static const std::string EVENT;
-};
+    //##ModelId=3E788FC000E5
+    static const std::string STYLE;
+    //##ModelId=3E788FC0025C
+    static const std::string NAME;
+    //##ModelId=3E788FC002AA
+    static const std::string ID;
+    //##ModelId=3E785B1B00FD
+    static const std::string TYPE;
+    //##ModelId=3E785B1B015B
+    static const std::string BUTTON;
+    //##ModelId=3E8B08FA01AA
+    static const std::string BUTTONSTATE;
+    //##ModelId=3E785B1B01A9
+    static const std::string KEY;
+    //##
+    static const std::string EVENTS;
+    //##
+    static const std::string EVENT;
+  };
 } // namespace mitk
 
 #endif /* EVENTMAPPER_H_HEADER_INCLUDED_C187864A */

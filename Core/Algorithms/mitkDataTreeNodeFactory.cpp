ef3154ef66c9333e7463fc8c1e621cc471d443e7
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-06-16 14:00:28 +0000

    FIX: set ImageGeometryOn after reinitializing the geometry when using the ITK reader

--- Core/Algorithms/mitkDataTreeNodeFactory.cpp
+++ Core/Algorithms/mitkDataTreeNodeFactory.cpp
@@ -726,6 +726,7 @@ void mitk::DataTreeNodeFactory::ReadFileTypeITKImageIOFactory()
   image->SetVolume( buffer );
   image->GetSlicedGeometry()->SetSpacing( spacing );
   image->GetTimeSlicedGeometry()->InitializeEvenlyTimed(image->GetSlicedGeometry(), image->GetDimension(3));
+  image->GetGeometry()->ImageGeometryOn();
   free( buffer );
   buffer = NULL;
   std::cout << "number of image components: "<< image->GetPixelType().GetNumberOfComponents() << std::endl;

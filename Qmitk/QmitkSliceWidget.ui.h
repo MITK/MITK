135f42b95e37217f7a0eb7268fd22a4243caf14e
andreas <andreas@nowhere>
2005-06-20 12:39:03 +0000

    ENH: connected time.event of navigation controller to renderer

--- Qmitk/QmitkSliceWidget.ui.h
+++ Qmitk/QmitkSliceWidget.ui.h
@@ -45,6 +45,7 @@ void QmitkSliceWidget::init()
   m_SliceNavigator = new mitk::SliceNavigationController(NULL);
   m_SliceNavigator->SetViewDirection(mitk::SliceNavigationController::Transversal);
   m_SliceNavigator->ConnectGeometrySliceEvent(m_Renderer.GetPointer());
+  m_SliceNavigator->ConnectGeometryTimeEvent(m_Renderer.GetPointer());
   m_SliceNavigator->ConnectRepaint(m_RenderWindow);
   new QmitkStepperAdapter(m_NavigatorWidget, m_SliceNavigator->GetSlice(), "navigation");  
   

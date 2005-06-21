4da8f5b8a4eac4388b5c43a0d63f8eb43fa5dab9
max <max@nowhere>
2005-06-21 13:55:31 +0000

    initial import. not really useful yet.

---
+++ Core/Algorithms/mitkImageToLookupTableFilter.h
@@ -0,0 +1,54 @@
+#ifndef _MITK_IMAGE_TO_LOOKUP_TABLE_FILTER__H_
+#define _MITK_IMAGE_TO_LOOKUP_TABLE_FILTER__H_
+
+#include <mitkLookupTableSource.h>
+#include <mitkImage.h>
+
+namespace mitk
+{
+    
+/**
+ * Creates a mitk::LookupTable from an image. This may be used to 
+ * create a default coloring scheme for vector images...
+ */
+class ImageToLookupTableFilter : public LookupTableSource
+{
+public:
+        
+    mitkClassMacro( ImageToLookupTableFilter, LookupTableSource );    
+
+    itkNewMacro( Self );
+    
+    // typedefs for setting an image as input
+	typedef mitk::Image                    InputImageType;
+	typedef InputImageType::Pointer        InputImagePointer;
+	typedef InputImageType::ConstPointer   InputImageConstPointer;
+    
+    #if ((VTK_MAJOR_VERSION > 4) || ((VTK_MAJOR_VERSION==4) && (VTK_MINOR_VERSION>=4) ))
+      typedef double vtkScalarType;
+    #else
+      typedef float vtkScalarType;
+    #endif
+    
+	virtual void SetInput( const InputImageType *image);
+    
+	virtual void SetInput( unsigned int, const InputImageType * image);
+    
+	const InputImageType * GetInput(void);
+    
+	const InputImageType * GetInput(unsigned int idx);
+            
+protected:   
+        
+    virtual void GenerateData();
+
+    ImageToLookupTableFilter();
+    
+    virtual ~ImageToLookupTableFilter();
+    
+};
+
+    
+} //end of namespace mitk
+
+#endif

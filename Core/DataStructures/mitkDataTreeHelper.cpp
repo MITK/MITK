fdf1d3c6e30f36d2be0c8f545bd2b7c4c4db3e25
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2005-06-22 09:25:52 +0000

    ENH: new method GetNamedChild

--- Core/DataStructures/mitkDataTreeHelper.cpp
+++ Core/DataStructures/mitkDataTreeHelper.cpp
@@ -1,27 +1,27 @@
 /*=========================================================================
-
+ 
 Program:   Medical Imaging & Interaction Toolkit
 Module:    $RCSfile$
 Language:  C++
 Date:      $Date$
 Version:   $Revision$
-
+ 
 Copyright (c) German Cancer Research Center, Division of Medical and
 Biological Informatics. All rights reserved.
 See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
-
+ 
 This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
-
+ 
 =========================================================================*/
 
 #include "mitkDataTreeHelper.h"
 
 mitk::DataTreeIteratorClone mitk::DataTreeHelper::FindIteratorToNode(
-    mitk::DataTreeBase* tree, 
-    const mitk::DataTreeNode* node, 
-    const mitk::DataTreeIteratorBase* startPosition )
+  mitk::DataTreeBase* tree,
+  const mitk::DataTreeNode* node,
+  const mitk::DataTreeIteratorBase* startPosition )
 {
   DataTreeIteratorClone pos;
 
@@ -38,4 +38,22 @@ mitk::DataTreeIteratorClone mitk::DataTreeHelper::FindIteratorToNode(
   }
   return pos;
 
+}
+mitk::DataTreeIteratorClone mitk::DataTreeHelper::GetNamedChild(mitk::DataTreeIteratorBase* iterator, std::string name)
+{
+  mitk::DataTreeChildIterator childIterator(*iterator);
+  while (! childIterator.IsAtEnd() )
+  {
+    mitk::DataTreeNode* node = childIterator.Get();
+    std::string nodeName;
+    if (node && node->GetName(nodeName))
+    {
+      if (nodeName == name)
+      {
+        return DataTreeIteratorClone(childIterator);
+      }
+    }
+    ++childIterator;
+  }
+  return DataTreeIteratorClone(NULL);
 }
--- Core/DataStructures/mitkDataTreeHelper.h
+++ Core/DataStructures/mitkDataTreeHelper.h
@@ -1,19 +1,19 @@
 /*=========================================================================
-
+ 
 Program:   Medical Imaging & Interaction Toolkit
 Module:    $RCSfile$
 Language:  C++
 Date:      $Date$
 Version:   $Revision$
-
+ 
 Copyright (c) German Cancer Research Center, Division of Medical and
 Biological Informatics. All rights reserved.
 See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
-
+ 
 This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
-
+ 
 =========================================================================*/
 
 
@@ -28,70 +28,74 @@ PURPOSE.  See the above copyright notices for more information.
 #include "mitkLevelWindowProperty.h"
 #include "mitkDataTreeNodeFactory.h"
 
-namespace mitk 
+namespace mitk
 {
 
-//##Documentation
-//## @brief Convenience methods for common tasks concerning the data tree
-//## @ingroup DataTree
-namespace DataTreeHelper
-{
   //##Documentation
-  //## @brief Search a data tree for (the first) iterator to a given node
-  DataTreeIteratorClone FindIteratorToNode(mitk::DataTreeBase* tree, const DataTreeNode* node, const DataTreeIteratorBase* startPosition = NULL );
-
-  //##Documentation
-  //## @brief Add itk-image to data tree
-  //##
-  //## The itk-image is converted into an mitk::Image.
-  //## @param itkImage the itk-image to be added
-  //## @param iterator the iterator used for adding the node, containing the image
-  //## @param name string with the name of the image, stored in the property "name"
-  //## @param replaceByName if @a true, the tree is searched starting at 
-  //##   @a iterator for a node called @a name, which data is replaced with 
-  //##   @a itkImage
-  template < typename TImageType >
-  mitk::DataTreeNode::Pointer 
-  AddItkImageToDataTree(TImageType* itkImage, mitk::DataTreeIteratorBase* iterator, const char* name=NULL, bool replaceByName=false)
+  //## @brief Convenience methods for common tasks concerning the data tree
+  //## @ingroup DataTree
+  namespace DataTreeHelper
   {
-    itkImage->Update();
+    //##Documentation
+    //## @brief Search a data tree for (the first) iterator to a given node
+    DataTreeIteratorClone FindIteratorToNode(mitk::DataTreeBase* tree, const DataTreeNode* node, const DataTreeIteratorBase* startPosition = NULL );
 
-    mitk::Image::Pointer mitkImage = mitk::Image::New();
-    mitk::CastToMitkImage(itkImage, mitkImage);
-
-    mitk::DataTreeNode::Pointer node = NULL;
-    bool done=false;
-    if((replaceByName) && (name != NULL))
+    //##Documentation
+    //## @brief Add itk-image to data tree
+    //##
+    //## The itk-image is converted into an mitk::Image.
+    //## @param itkImage the itk-image to be added
+    //## @param iterator the iterator used for adding the node, containing the image
+    //## @param name string with the name of the image, stored in the property "name"
+    //## @param replaceByName if @a true, the tree is searched starting at
+    //##   @a iterator for a node called @a name, which data is replaced with
+    //##   @a itkImage
+    template < typename TImageType >
+    mitk::DataTreeNode::Pointer
+    AddItkImageToDataTree(TImageType* itkImage, mitk::DataTreeIteratorBase* iterator, const char* name=NULL, bool replaceByName=false)
     {
-      mitk::DataTreeIteratorClone it=iterator;
-      mitk::DataTreeIteratorClone itByName = ((mitk::DataTree *) it->GetTree())->GetNext("name", new mitk::StringProperty( name ));
+      itkImage->Update();
 
-      if( (itByName->IsAtEnd() == false) && (itByName->Get().IsNotNull()) )
+      mitk::Image::Pointer mitkImage = mitk::Image::New();
+      mitk::CastToMitkImage(itkImage, mitkImage);
+
+      mitk::DataTreeNode::Pointer node = NULL;
+      bool done=false;
+      if((replaceByName) && (name != NULL))
       {
-        node = itByName->Get();
-        node->SetData(mitkImage);
-        done = true;
+        mitk::DataTreeIteratorClone it=iterator;
+        mitk::DataTreeIteratorClone itByName = ((mitk::DataTree *) it->GetTree())->GetNext("name", new mitk::StringProperty( name ));
+
+        if( (itByName->IsAtEnd() == false) && (itByName->Get().IsNotNull()) )
+        {
+          node = itByName->Get();
+          node->SetData(mitkImage);
+          done = true;
+        }
       }
-    }
-    if(done==false)
-    {
-      node=mitk::DataTreeNode::New();
-      node->SetData(mitkImage);
-      if(name!=NULL)
-        node->SetProperty("name", new mitk::StringProperty(name));
-      mitk::DataTreeNodeFactory::SetDefaultImageProperties(node);
-      iterator->Add(node);
-    }
+      if(done==false)
+      {
+        node=mitk::DataTreeNode::New();
+        node->SetData(mitkImage);
+        if(name!=NULL)
+          node->SetProperty("name", new mitk::StringProperty(name));
+        mitk::DataTreeNodeFactory::SetDefaultImageProperties(node);
+        iterator->Add(node);
+      }
 
-    mitk::LevelWindowProperty::Pointer levWinProp = new mitk::LevelWindowProperty();
-    mitk::LevelWindow levelwindow;
-    levelwindow.SetAuto( mitkImage );
-    levWinProp->SetLevelWindow( levelwindow );
-    node->SetProperty( "levelwindow", levWinProp );
+      mitk::LevelWindowProperty::Pointer levWinProp = new mitk::LevelWindowProperty();
+      mitk::LevelWindow levelwindow;
+      levelwindow.SetAuto( mitkImage );
+      levWinProp->SetLevelWindow( levelwindow );
+      node->SetProperty( "levelwindow", levWinProp );
 
-    return node;
-  }
-};
+      return node;
+    }
+    /** Get child node which has a specified name
+     * @returns child node of iterator which has the given name
+    */
+    DataTreeIteratorClone GetNamedChild(mitk::DataTreeIteratorBase* iterator, std::string name);
+  };
 
 } // namespace mitk
 

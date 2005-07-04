42d5233945004739e642c8f2327294b8bd392f54
kgrunewa <kgrunewa@nowhere>
2005-07-04 12:05:40 +0000

    FIX: data tree nodes have now correct names

--- QApplications/MainApp/ChiliPlugin/QcMITKSamplePlugin.cpp
+++ QApplications/MainApp/ChiliPlugin/QcMITKSamplePlugin.cpp
@@ -24,6 +24,9 @@
 #include <ipFunc.h>
 #include <ipDicom/ipDicom.h>
 
+#include "QmitkCommonFunctionality.h"
+
+
 
 QcPlugin* QcMITKSamplePlugin::s_PluginInstance = NULL;
 
@@ -103,7 +106,19 @@ void QcMITKSamplePlugin::selectSerie (QcLightbox* lightbox)
   activated=true;
   if(lightbox->getFrames()==0)
     return;
- 
+  
+  QObject * parent = this->parent();
+  QWidget * parentWidget = (QWidget *)parent;
+
+  if (parentWidget->isVisible()) {
+    itkGenericOutputMacro(<<"plugin active");
+  } else
+  {
+    itkGenericOutputMacro(<<"plugin not active");
+  }
+  
+
+  
   mitk::Image::Pointer image = mitk::Image::New();
   
   mitk::LightBoxImageReader::Pointer reader=mitk::LightBoxImageReader::New();
@@ -120,7 +135,7 @@ void QcMITKSamplePlugin::selectSerie (QcLightbox* lightbox)
     mitk::DataTreePreOrderIterator it(tree);
     mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
     node->SetData(image);
-    it.Add(node);
+    
     mitk::LevelWindowProperty::Pointer levWinProp = new mitk::LevelWindowProperty();
     mitk::LevelWindow levelwindow;
     levelwindow.SetAuto( reader->GetOutput() );
@@ -135,16 +150,33 @@ void QcMITKSamplePlugin::selectSerie (QcLightbox* lightbox)
     ipPicTSV_t* description= ipPicQueryTag(pic, "SERIES DESCRIPTION");
     ipPicTSV_t* patientName= ipPicQueryTag(pic, "PATIENT NAME");
 
-    if (description)
+    int laenge = patientName->n[0];
+    char* patName = new char[laenge+1];
+    strncpy(patName, (char*)(patientName->value), laenge);
+    patName[laenge] = 0;
+    //char a;
+    //while (*patName != 0) 
+    //{
+    //  a = *patName;
+    //  patName++;
+    //}
+
+    std::string test((char*)patientName->value );
+    int lenght = test.length();
+    
+   if (description)
       node->SetProperty("name", new mitk::StringProperty( (char*)uid->value ));
     else if (uid)
       node->SetProperty("name", new mitk::StringProperty( (char*)description->value ));
 
     else if (patientName)
-      node->SetProperty("name", new mitk::StringProperty( (char*)patientName->value ));
+      //node->SetProperty("name", new mitk::StringProperty( (char*)patientName->value ));
+      //node->SetProperty("name", new mitk::StringProperty( test.c_str() ));
+      node->SetProperty("name", new mitk::StringProperty( patName ));
     else 
       node->SetProperty("name", new mitk::StringProperty( lightbox->name() ));
 
+    bool notAlreadyInDataTree = true;
 
     ipPicTSV_t* tsv = ipPicQueryTag(pic, "SOURCE HEADER");
     void* data;
@@ -155,25 +187,54 @@ void QcMITKSamplePlugin::selectSerie (QcLightbox* lightbox)
       //0x0008, 0x103e : Series Description - ist der in der unter "Beschreibung" stehende name (_bh/_bh_P)
       // aber: wahrscheinlich unsicher, da vom Benutzer eingegeben
       //eventuell brauchbar: 0x0018, 0x0024 : Sequence Name
-      dicomFindElement((unsigned char*) tsv->value, 0x0018, 0x1030, &data, &len);
-      char * tmp = new char[len+1];
-      strncpy(tmp, (char*)data, len);
-      tmp[len]=0;
-      node->SetProperty("protocol", new mitk::StringProperty( tmp ));
-      delete [] tmp;
+      char * tmp;
+      if (dicomFindElement((unsigned char*) tsv->value, 0x0018, 0x1030, &data, &len))
+      {
+        tmp = new char[len+1];
+        strncpy(tmp, (char*)data, len);
+        tmp[len]=0;
+        node->SetProperty("protocol", new mitk::StringProperty( tmp ));
+        delete [] tmp;
+      }
 
-      dicomFindElement((unsigned char*) tsv->value, 0x0008, 0x103e, &data, &len);
-      tmp = new char[len+1];
-      strncpy(tmp, (char*)data, len);
-      tmp[len]=0;
-      node->SetProperty("series description", new mitk::StringProperty( tmp ));
-      delete [] tmp;
+      if (dicomFindElement((unsigned char*) tsv->value, 0x0020, 0x000e, &data, &len))
+      {
+        dicomFindElement((unsigned char*) tsv->value, 0x0020, 0x000e, &data, &len);
+        tmp = new char[len+1];
+        strncpy(tmp, (char*)data, len);
+        tmp[len]=0;
+        node->SetProperty("series uid", new mitk::StringProperty( tmp ));
+
+        mitk::DataTreeNode* existingNode = CommonFunctionality::GetFirstNodeByProperty(it,"series uid", new mitk::StringProperty( tmp ));
+
+        if ( existingNode )
+        {
+          notAlreadyInDataTree = false;
+        }
+
+        delete [] tmp;
+      }      
+
+      
+      if ( dicomFindElement((unsigned char*) tsv->value, 0x0008, 0x103e, &data, &len) )
+      {
+        tmp = new char[len+1];
+        strncpy(tmp, (char*)data, len);
+        tmp[len]=0;
+        node->SetProperty("series description", new mitk::StringProperty( tmp ));
+        delete [] tmp;
+      }
     }
         //dicomFindElement((unsigned char*) tsv->value, 0x0020, 0x0013, &data, &len);
         //sscanf( (char *) data, "%d", &imageNumber );
         //info.imageNumber=imageNumber;
+    
 
-
+    if (notAlreadyInDataTree)
+    {
+      it.Add(node);
+    }
+    
     if((ap->GetMultiWidget()->GetRenderWindow1()->GetRenderer()->GetMTime()==initTime) && (ap->GetStandardViewsInitialized()==false))
     {
       ap->SetStandardViewsInitialized(ap->GetMultiWidget()->InitializeStandardViews(&it));

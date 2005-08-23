8de9523720c0e189be715b601e4607acd13add8b
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-08-23 13:23:58 +0000

    ENH: calculation of boundingbox of all contained boundingboxes (not yet tested) ENH: changes according to general availability of TimeSlicedGeometry in BaseData ENH: removed obsolete VerifyRequestedRegion

--- Core/DataStructures/mitkBoundingObjectGroup.cpp
+++ Core/DataStructures/mitkBoundingObjectGroup.cpp
@@ -18,12 +18,14 @@ PURPOSE.  See the above copyright notices for more information.
 
 
 #include "mitkBoundingObjectGroup.h"
+#include "mitkBaseProcess.h"
 #include <vtkLinearTransform.h>
 
 mitk::BoundingObjectGroup::BoundingObjectGroup()
 :  m_Counter(0), m_CSGMode(Difference) //m_CSGMode(Union) m_CSGMode(Intersection)
 {
-  m_Geometry3D->Initialize();
+  GetTimeSlicedGeometry()->Initialize(1);
+  GetGeometry(0)->SetIndexToWorldTransform(GetTimeSlicedGeometry()->GetIndexToWorldTransform());
   m_BoundingObjects = mitk::BoundingObjectGroup::BoundingObjectContainer::New();
   SetVtkPolyData(NULL);
 }
@@ -32,121 +34,76 @@ mitk::BoundingObjectGroup::~BoundingObjectGroup()
 {
 } 
 
-bool mitk::BoundingObjectGroup::VerifyRequestedRegion()
-{
-  assert(m_Geometry3D.IsNotNull());
-  return true; //Superclass::VerifyRequestedRegion();
-}
-
 void mitk::BoundingObjectGroup::UpdateOutputInformation()
 {  
-  ScalarType bounds[6]={0,1,0,1,0,1};  // {xmin,x_max, ymin,y_max,zmin,z_max};
-  
+  if ( this->GetSource() )
+  {
+    this->GetSource()->UpdateOutputInformation();
+  }
+
   // calculate global bounding box
   mitk::BoundingObjectGroup::BoundingObjectContainer::ConstIterator boundingObjectsIterator = m_BoundingObjects->Begin();
   const mitk::BoundingObjectGroup::BoundingObjectContainer::ConstIterator boundingObjectsIteratorEnd = m_BoundingObjects->End();
 
   if(boundingObjectsIterator == boundingObjectsIteratorEnd) // if there is no BoundingObject, the bounding box is zero
   {
-    bounds[0] = bounds[1] = bounds[2] = bounds[3] = bounds[4] = bounds[5] = 0.0;
+    mitk::BoundingBox::BoundsArrayType boundsArray;
+    boundsArray.Fill(0);
+    GetTimeSlicedGeometry()->Initialize(1);
+    GetGeometry()->SetBounds(boundsArray);
+    GetTimeSlicedGeometry()->UpdateInformation();
     return; 
   }
-  mitk::BoundingBox::Pointer boundingBox;
-  mitk::BoundingBox::PointType minPoint;
-  mitk::BoundingBox::PointType globalMinPoint;  
-  mitk::BoundingBox::PointType globalMaxPoint;
 
-  /* Initialize globalMin/MaxPoints */
-  boundingObjectsIterator.Value()->UpdateOutputInformation();
-  boundingBox = const_cast<mitk::BoundingBox*>(boundingObjectsIterator.Value()->GetGeometry()->GetBoundingBox());
-  
-  minPoint = boundingBox->GetMinimum();
-  mitk::ScalarType min[3];
-  min[0] = minPoint[0];   min[1] = minPoint[1];   min[2] = minPoint[2];
-  boundingObjectsIterator.Value()->GetGeometry()->GetVtkTransform()->TransformPoint(min, min);
-  globalMinPoint[0] = min[0];   globalMinPoint[1] = min[1];   globalMinPoint[2] = min[2];  
-  globalMaxPoint[0] = min[0];   globalMaxPoint[1] = min[1];   globalMaxPoint[2] = min[2];
-  //boundingObjectsIterator++;  
-  while (boundingObjectsIterator != boundingObjectsIteratorEnd) // calculate a bounding box that includes all BoundingObjects
-  {  
-    boundingObjectsIterator.Value()->UpdateOutputInformation();    
-    boundingBox = const_cast<mitk::BoundingBox*>(boundingObjectsIterator.Value()->GetGeometry()->GetBoundingBox());
+  // initialize container
+  mitk::BoundingBox::PointsContainer::Pointer pointscontainer=mitk::BoundingBox::PointsContainer::New();
 
-    /* create all 8 points of the bounding box */
-    mitk::BoundingBox::PointsContainerPointer points = mitk::BoundingBox::PointsContainer::New();
-    mitk::Point3D p;
-    p = boundingBox->GetMinimum();
-    points->InsertElement(0, p);
-    p[0] = -p[0];
-    points->InsertElement(1, p);
-    p = boundingBox->GetMinimum();
-    p[1] = -p[1];    
-    points->InsertElement(2, p);
-    p = boundingBox->GetMinimum();
-    p[2] = -p[2];    
-    points->InsertElement(3, p);
-    p = boundingBox->GetMaximum();
-    points->InsertElement(4, p);
-    p[0] = -p[0];
-    points->InsertElement(5, p);
-    p = boundingBox->GetMaximum();
-    p[1] = -p[1];    
-    points->InsertElement(6, p);
-    p = boundingBox->GetMaximum();
-    p[2] = -p[2];    
-    points->InsertElement(7, p);
-    mitk::BoundingBox::PointsContainerConstIterator pointsIterator = points->Begin();
-    mitk::BoundingBox::PointsContainerConstIterator pointsIteratorEnd = points->End();
-    while (pointsIterator != pointsIteratorEnd) // for each vertex of the bounding box
+  mitk::BoundingBox::PointIdentifier pointid=0;
+  mitk::Point3D point;
+
+  mitk::AffineTransform3D* transform = GetTimeSlicedGeometry()->GetIndexToWorldTransform();
+
+  // calculate a bounding box that includes all BoundingObjects
+  // \todo probably we should do this additionally for each time-step
+  while (boundingObjectsIterator != boundingObjectsIteratorEnd)
+  {
+    const Geometry3D* geometry = boundingObjectsIterator.Value()->GetUpdatedTimeSlicedGeometry();
+    unsigned char i;
+    for(i=0; i<8; ++i)
     {
-      minPoint = pointsIterator->Value();
-      min[0] = minPoint[0];   min[1] = minPoint[1];   min[2] = minPoint[2];
-      boundingObjectsIterator.Value()->GetGeometry()->GetVtkTransform()->TransformPoint(min, min);  // transform vertex point to world coordinates
-
-      globalMinPoint[0] = (min[0] < globalMinPoint[0]) ? min[0] : globalMinPoint[0];  // check if world point
-      globalMinPoint[1] = (min[1] < globalMinPoint[1]) ? min[1] : globalMinPoint[1];  // has a lower or a
-      globalMinPoint[2] = (min[2] < globalMinPoint[2]) ? min[2] : globalMinPoint[2];  // higher value as
-      globalMaxPoint[0] = (min[0] > globalMaxPoint[0]) ? min[0] : globalMaxPoint[0];  // the last known highest
-      globalMaxPoint[1] = (min[1] > globalMaxPoint[1]) ? min[1] : globalMaxPoint[1];  // value
-      globalMaxPoint[2] = (min[2] > globalMaxPoint[2]) ? min[2] : globalMaxPoint[2];  // in each axis
-      pointsIterator++;
+      point = transform->BackTransformPoint(geometry->GetCornerPoint(i));
+      if(point[0]*point[0]+point[1]*point[1]+point[2]*point[2] < mitk::large)
+        pointscontainer->InsertElement( pointid++, point);
+      else
+      {
+        itkGenericOutputMacro( << "Unrealistically distant corner point encountered. Ignored. BoundingObject: " << boundingObjectsIterator.Value() );
+      }
     }
-    boundingObjectsIterator++;
+    ++boundingObjectsIterator;
   }
+
+  mitk::BoundingBox::Pointer boundingBox = mitk::BoundingBox::New();
+  boundingBox->SetPoints(pointscontainer);
+  boundingBox->ComputeBoundingBox();
 
   /* BoundingBox is centered around the center of all sub bounding objects */
-  Vector3D centerPoint;
-  centerPoint[0] = (globalMinPoint[0] + globalMaxPoint[0])/ 2.0;
-  centerPoint[1] = (globalMinPoint[1] + globalMaxPoint[1])/ 2.0;
-  centerPoint[2] = (globalMinPoint[2] + globalMaxPoint[2])/ 2.0;
+  Point3D center = boundingBox->GetCenter();
 
-  bounds[0] = globalMinPoint[0] - centerPoint[0]; // x Min
-  bounds[1] = globalMaxPoint[0] - centerPoint[0]; // x Max
-  bounds[2] = globalMinPoint[1] - centerPoint[1]; // y Min
-  bounds[3] = globalMaxPoint[1] - centerPoint[1]; // y Max
-  bounds[4] = globalMinPoint[2] - centerPoint[2]; // z Min
-  bounds[5] = globalMaxPoint[2] - centerPoint[2]; // z Max
+  Point3D minimum, maximum;
+  minimum.Fill(0); maximum.Fill(0);
+  minimum += boundingBox->GetMinimum() - center;
+  maximum += boundingBox->GetMaximum() - center;
 
-  mitk::TimeSlicedGeometry::Pointer timeGeometry = const_cast< mitk::TimeSlicedGeometry *>(this->GetTimeSlicedGeometry());
-  mitk::Geometry3D::Pointer geometry3d = timeGeometry->GetGeometry3D(0);
-  unsigned int timesteps = timeGeometry->GetTimeSteps();
-  if(geometry3d.IsNull())
-  {
-    assert(timesteps==0);
-    geometry3d = mitk::Geometry3D::New();
-    timesteps = 1;
-  }
-  else
-  {
-    assert(timesteps>0);
-  }
+  boundingBox->SetMinimum(minimum);
+  boundingBox->SetMaximum(maximum);
 
+  Geometry3D* geometry3d = GetGeometry(0);
+  geometry3d->SetIndexToWorldTransform(transform);
+  geometry3d->SetBounds(boundingBox->GetBounds());
   /* the objects position is the center of all sub bounding objects */
-  geometry3d->SetBounds(bounds);
-  geometry3d->Translate(centerPoint);
-  geometry3d->TransferItkToVtkTransform();
+  geometry3d->SetOrigin(center);
 
-  timeGeometry->InitializeEvenlyTimed(geometry3d, timesteps);
+  GetTimeSlicedGeometry()->InitializeEvenlyTimed(geometry3d, GetTimeSlicedGeometry()->GetTimeSteps());
 }
 
 void mitk::BoundingObjectGroup::AddBoundingObject(mitk::BoundingObject::Pointer boundingObject)

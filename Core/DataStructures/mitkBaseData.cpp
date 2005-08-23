3156a92f613705370b69d66fff78a9455ce6c133
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-08-23 09:02:17 +0000

    FIX: missing ()

--- Core/DataStructures/mitkBaseData.cpp
+++ Core/DataStructures/mitkBaseData.cpp
@@ -114,7 +114,7 @@ void mitk::BaseData::UnRegister() const
     if((m_Unregistering==false) && (m_SmartSourcePointer.IsNotNull()))
     {
       m_Unregistering=true;
-      if((GetSource==NULL) || (m_SmartSourcePointer->GetExternalReferenceCount()==0))
+      if((this->GetSource()==NULL) || (this->m_SmartSourcePointer->GetExternalReferenceCount()==0))
         m_SmartSourcePointer=NULL; // now the reference count is zero and this object has been destroyed; thus nothing may be done after this line!!
       else
         m_Unregistering=false;

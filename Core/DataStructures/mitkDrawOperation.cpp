1a82b153826e5d1f0a6043a2787f78e0c69cba71
matthias <matthias@nowhere>
2005-09-14 11:25:46 +0000

    ENH: initial version of mitkDrawOperation

---
+++ Core/DataStructures/mitkDrawOperation.cpp
@@ -0,0 +1,65 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include <mitkDrawOperation.h>
+
+
+mitk::DrawOperation::DrawOperation(OperationType operationType,	Point3D point, Point3D last_point, int draw_state, int radius)
+: Operation(operationType), m_Point(point), m_LastPoint(last_point), m_DrawState(draw_state), m_Radius(radius)
+{}
+
+mitk::DrawOperation::DrawOperation(OperationType operationType,	SeedsImage::Pointer seedsImage, SeedsImage::Pointer lastSeedsImage)
+: Operation(operationType), m_SeedsImage(seedsImage), m_LastSeedsImage(lastSeedsImage)
+{}
+
+
+mitk::DrawOperation::~DrawOperation()
+{
+}
+
+
+mitk::Point3D mitk::DrawOperation::GetPoint()
+{
+	return m_Point;
+}
+
+mitk::Point3D mitk::DrawOperation::GetLastPoint()
+{
+	return m_LastPoint;
+}
+
+int mitk::DrawOperation::GetDrawState()
+{
+  return m_DrawState;
+}
+
+int mitk::DrawOperation::GetRadius()
+{
+  return m_Radius;
+}
+
+mitk::SeedsImage::Pointer mitk::DrawOperation::GetSeedsImage()
+{
+  return m_SeedsImage;
+}
+
+mitk::SeedsImage::Pointer mitk::DrawOperation::GetLastSeedsImage()
+{
+  return m_LastSeedsImage;
+}
---
+++ Core/DataStructures/mitkDrawOperation.h
@@ -0,0 +1,88 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef MITKDRAWOPERATION_H
+#define MITKDRAWOPERATION_H
+
+#include <mitkCommon.h>
+#include <mitkOperation.h>
+#include <mitkVector.h>
+#include <mitkImage.h>
+#include <mitkSeedsImage.h>
+
+namespace mitk {
+
+//##ModelId=3F0189F0016F
+//##Documentation
+//## @brief Operation, that handles all aktions on Seeds.
+//##
+//## @ingroup Undo
+//##
+//## Stores everything for Adding and Deleting Seeds.
+class DrawOperation : public Operation
+{
+  public:
+  //##ModelId=3F0189F003A2
+	//##Documentation
+	//##@brief Operation, that handles all aktions on Seeds.
+	//##
+	//## @param operationType is the type of that operation (see mitkOperation.h; e.g. move or add; Information for StateMachine::ExecuteOperation());
+  //## @param point is the information of the seed point to add to the seeds image
+  //## @param last_point is the information of the point added before
+  //## @param draw_state represents the seeds type e.g foreground or background seeds
+  //## @param radius is the radius of seeds 
+  DrawOperation(OperationType operationType,	Point3D point, Point3D last_point, int draw_state, int radius);
+ 
+	//##Documentation
+	//##@brief Operation, that handles all aktions on Seeds.
+	//##
+	//## @param operationType is the type of that operation (see mitkOperation.h; e.g. move or add; Information for StateMachine::ExecuteOperation());
+  //## @param seedsImage is the current image of seeds
+  //## @param lastSeedsImage is the image before a seed point is set
+  DrawOperation(OperationType operationType,	SeedsImage::Pointer seedsImage, SeedsImage::Pointer lastSeedsImage);
+
+
+  virtual ~DrawOperation();
+
+	Point3D GetPoint();
+
+	Point3D GetLastPoint();
+
+	int GetDrawState();
+
+	int GetRadius();
+
+  SeedsImage::Pointer DrawOperation::GetSeedsImage();
+
+  SeedsImage::Pointer DrawOperation::GetLastSeedsImage();
+
+  private:
+	Point3D m_Point;
+	Point3D m_LastPoint;
+
+	int m_DrawState;
+  int m_Radius;
+
+  SeedsImage::Pointer m_SeedsImage;
+  SeedsImage::Pointer m_LastSeedsImage;
+
+};
+}//namespace mitk
+#endif /* MITKDRAWOPERATION_H*/
+

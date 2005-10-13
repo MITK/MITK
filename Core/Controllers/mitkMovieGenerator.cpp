985125d814ce10a97704761f0e8d380e3f43ed7f
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2005-10-13 12:58:56 +0000

    ENH: reformatted

--- Core/Controllers/mitkMovieGenerator.cpp
+++ Core/Controllers/mitkMovieGenerator.cpp
@@ -1,24 +1,42 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
 #include "mitkMovieGenerator.h"
 #include <mitkRenderingManager.h>
 #include <GL/gl.h>
 
-#ifdef WIN32    
-	#include "mitkMovieGeneratorWin32.h"
+#ifdef WIN32
+#include "mitkMovieGeneratorWin32.h"
 #endif
 #ifndef GL_BGR
-	#define GL_BGR GL_BGR_EXT
+#define GL_BGR GL_BGR_EXT
 #endif
-
 
 mitk::MovieGenerator::Pointer mitk::MovieGenerator::New() 
 {
   Pointer smartPtr;
   MovieGenerator *rawPtr = ::itk::ObjectFactory<MovieGenerator>::Create();
   if(rawPtr == NULL) {
-    #ifdef WIN32
-      mitk::MovieGenerator::Pointer wp = static_cast<mitk::MovieGenerator*>(MovieGeneratorWin32::New());
-      return wp;
-    #endif
+#ifdef WIN32
+    mitk::MovieGenerator::Pointer wp = static_cast<mitk::MovieGenerator*>(MovieGeneratorWin32::New());
+    return wp;
+#endif
   }
   smartPtr = rawPtr;
   if(rawPtr != NULL) rawPtr->UnRegister();
@@ -28,26 +46,28 @@ mitk::MovieGenerator::Pointer mitk::MovieGenerator::New()
 
 bool mitk::MovieGenerator::WriteMovie()
 {  
-	bool ok = false;  
-	if (m_stepper) {    
-  	if (m_renderer) m_renderer->MakeCurrent();      
-		m_stepper->First();
+  bool ok = false;
+  if (m_stepper)
+  {
+    if (m_renderer) m_renderer->MakeCurrent();
+    m_stepper->First();
     RenderingManager::GetInstance()->ForceImmediateUpdate(m_renderer->GetRenderWindow());
 
-		ok = InitGenerator();    
-		if (!ok) return false;   
-		int imgSize = 3 * m_width * m_height;    
-		printf( "Video size = %i x %i\n", m_width, m_height );    
-		GLbyte *data = new GLbyte[imgSize];    
-		for (unsigned int i=0; i<m_stepper->GetSteps(); i++) {      
-			if (m_renderer) m_renderer->MakeCurrent();      
+    ok = InitGenerator();
+    if (!ok) return false;
+    int imgSize = 3 * m_width * m_height;
+    printf( "Video size = %i x %i\n", m_width, m_height );
+    GLbyte *data = new GLbyte[imgSize];
+    for (unsigned int i=0; i<m_stepper->GetSteps(); i++)
+    {
+      if (m_renderer) m_renderer->MakeCurrent();
       RenderingManager::GetInstance()->ForceImmediateUpdate(m_renderer->GetRenderWindow());
-			glReadPixels( 0, 0, m_width, m_height, GL_BGR, GL_UNSIGNED_BYTE, (void*)data );      
-			AddFrame( data );      
-			m_stepper->Next();    
-		}    
-		ok = TerminateGenerator();    
-		delete[] data;  
-	}  
-	return ok;
+      glReadPixels( 0, 0, m_width, m_height, GL_BGR, GL_UNSIGNED_BYTE, (void*)data );
+      AddFrame( data );
+      m_stepper->Next();
+    }
+    ok = TerminateGenerator();
+    delete[] data;
+  }
+  return ok;
 }
--- Core/Controllers/mitkMovieGenerator.h
+++ Core/Controllers/mitkMovieGenerator.h
@@ -1,71 +1,88 @@
-#ifndef	MOVIEGENERATOR_H_HEADER_INCLUDED
+/*=========================================================================
 
-#define	MOVIEGENERATOR_H_HEADER_INCLUDED
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef  MOVIEGENERATOR_H_HEADER_INCLUDED
+
+#define  MOVIEGENERATOR_H_HEADER_INCLUDED
 
 
 #include "mitkCommon.h"
 #include "mitkStepper.h"
 #include "mitkBaseRenderer.h"
 
-namespace mitk {  
-  
-class	MovieGenerator : public	itk::LightObject  { 
+namespace mitk {
 
-  public:    
-    
-    mitkClassMacro(MovieGenerator, itk::LightObject);    
-    
-    // delivers Win32 or Linux-versions of MovieGenerator    
-    static Pointer New(void);    
-    
-    //!	stepper	used to	control	movie	generation    
-    virtual void SetStepper( Stepper *stepper ) { m_stepper = stepper; }    
-    
-    //!	renderer to record    
-    virtual void SetRenderer(	BaseRenderer *renderer ) { m_renderer	=	renderer;	}    
-    
-    //!	filename under which movie is	saved    
-    virtual void SetFileName(	const	char *fileName ) { strcpy( m_fileName, fileName	); }    
-    
-    //!	uses given stepper and filename	to create a movie from the active OpenGL context    
-    virtual bool WriteMovie();
+class  MovieGenerator : public  itk::LightObject
+{
+public:
 
-  protected:
+  mitkClassMacro(MovieGenerator, itk::LightObject);
 
-    //!	default	constructor
-    MovieGenerator() { 
-      m_fileName[0]	=	0;
-      m_stepper	=	0;
-      m_renderer = 0;
-      m_width	=	0;
-      m_height = 0;
-    }
+  // delivers Win32 or Linux-versions of MovieGenerator
+  static Pointer New(void);    
 
-    //!	called directly	before the first frame is	added, determines	movie	size from	renderer
-    virtual	bool InitGenerator() = 0;
+  //!  stepper  used to  control  movie  generation
+  virtual void SetStepper( Stepper *stepper ) { m_stepper = stepper; }
 
-    //!	used to	add	a	frame
-    virtual	bool AddFrame( void	*data	)	=	0;
+  //!  renderer to record    
+  virtual void SetRenderer(  BaseRenderer *renderer ) { m_renderer  =  renderer;  }
 
-    //!	called after the last	frame	is added
-    virtual	bool TerminateGenerator()	=	0;
+  //!  filename under which movie is  saved    
+  virtual void SetFileName(  const  char *fileName ) { strcpy( m_fileName, fileName  ); }
+
+  //!  uses given stepper and filename  to create a movie from the active OpenGL context    
+  virtual bool WriteMovie();
+
+protected:
+
+  //!  default  constructor
+  MovieGenerator() { 
+    m_fileName[0]  =  0;
+    m_stepper  =  0;
+    m_renderer = 0;
+    m_width  =  0;
+    m_height = 0;
+  }
+
+  //!  called directly  before the first frame is  added, determines  movie  size from  renderer
+  virtual bool InitGenerator() = 0;
+
+  //!  used to  add  a  frame
+  virtual  bool AddFrame( void  *data  )  =  0;
+
+  //!  called after the last  frame  is added
+  virtual  bool TerminateGenerator()  =  0;
 
 
-    //!	stores the movie filename
-    char				 m_fileName[1024];
+  //!  stores the movie filename
+  char          m_fileName[1024];
 
-    //!	stores the used	stepper
-    Stepper			 *m_stepper;
+  //!  stores the used  stepper
+  Stepper       *m_stepper;
 
-    //! stores the uses renderer
-    BaseRenderer *m_renderer;
+  //! stores the uses renderer
+  BaseRenderer *m_renderer;
 
-    //!	InitGenerator()	stores movie size	in those variables
-    int					 m_width,	m_height;  
-
+  //!  InitGenerator()  stores movie size  in those variables
+  int           m_width, m_height;
 };
 
 } //namespace mitk
 
-#endif /*	MOVIEGENERATOR_H_HEADER_INCLUDED */
-
+#endif /*  MOVIEGENERATOR_H_HEADER_INCLUDED */

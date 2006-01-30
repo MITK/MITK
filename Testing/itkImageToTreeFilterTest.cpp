c4e577a80a2315c9380163821490844f1c06d7f3
nico <nico@nowhere>
2006-01-30 15:22:10 +0000

    test are running atomic

--- Testing/itkImageToTreeFilterTest.cpp
+++ Testing/itkImageToTreeFilterTest.cpp
@@ -15,80 +15,43 @@ PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
 
+#include <list>
 #include <itkImage.h>
 #include <itkITTFilterContext.h>
 #include <itkImageToTreeFilter.h>
 
 #include <mitkVesselTreeData.h>
 
+// image type
+typedef unsigned char   PixelType;
+const unsigned int      Dimension = 3;
+typedef itk::Image<PixelType, Dimension>            ImageType;
+typedef ImageType::Pointer                          ImagePointer;
+typedef ImageType::PointType                        PointType;
 
+// tree type
+typedef mitk::VesselTreeData                        OutputTreeType;
+typedef OutputTreeType::Pointer                     OutputTreePointer;
 
-// namespace itk
-// {
-//   class  ITTFilterContextTest: public CppUnit::TestFixture
-//   {
-//   private:
-//     FilterContextPointer                                m_FilterContext;
-//     PointType                                           point1;
-//     PointType                                           point2;
-//   public:
-//     void setUp()
-//     {
-//       m_FilterContext = FilterContextType::New();
-//
-//       // init some test data
-//       testPoint1.Fill(0);
-//       testPoint2.Fill(1);
-//
-//       PointQueueType* pointQueue = m_FilterContext->GetStartPointQueue();
-//       std::cout << "Pushing points to filter context...\n";
-//       pointQueue1->push(testPoint1);
-//       pointQueue1->push(testPoint2);
-//     }
-//
-//     void tearDown()
-//     {
-//       m_FilterContext::Delete();
-//     }
-//
-//     void testPointQueue()
-//     {
-//       PointQueueType* pointQueue = m_FilterContext->GetStartPointQueue();
-//
-//       CPPUNIT_ASSERT( pointQueue->front() == point1 );
-//       pointQueue.pop();
-//       CPPUNIT_ASSERT( pointQueue->front() == point2 );
-//     }
-//   };
-// }
+//   typedef itk::ImageFileReader<ImageType>         ImageReaderType;
+//   typedef ImageReaderType::Pointer                ImageReaderPointer;
 
-int itkImageToTreeFilterTest(int, char* argv[] )
+// test classes
+typedef itk::ImageToTreeFilter<ImageType, OutputTreeType>
+                                                    ImageToTreeFilterType;
+typedef ImageToTreeFilterType::Pointer              ImageToTreeFilterPointer;
+typedef itk::ITTFilterContext<ImageType, OutputTreeType>
+                                                    FilterContextType;
+typedef FilterContextType::Pointer                  FilterContextPointer;
+typedef FilterContextType::PointQueueType           PointQueueType;
+
+typedef std::list<int>                              ResultListType;
+
+/******************************************************************
+ * TEST 1: Saving and loading data objects to the filter context
+ *****************************************************************/
+int testFilterContext(char* argv[])
 {
-  // image type
-  typedef unsigned char   PixelType;
-  const unsigned int      Dimension = 3;
-  typedef itk::Image<PixelType, Dimension>            ImageType;
-  typedef ImageType::Pointer                          ImagePointer;
-  typedef ImageType::PointType                        PointType;
-
-  // tree type
-  typedef mitk::VesselTreeData                        OutputTreeType;
-  typedef OutputTreeType::Pointer                     OutputTreePointer;
-
-
-
-  //   typedef itk::ImageFileReader<ImageType>         ImageReaderType;
-  //   typedef ImageReaderType::Pointer                ImageReaderPointer;
-
-  // test classes
-  typedef itk::ImageToTreeFilter<ImageType, OutputTreeType>
-  ImageToTreeFilterType;
-  typedef ImageToTreeFilterType::Pointer              ImageToTreeFilterPointer;
-  typedef itk::ITTFilterContext<ImageType, OutputTreeType>
-  FilterContextType;
-  typedef FilterContextType::Pointer                  FilterContextPointer;
-  typedef FilterContextType::PointQueueType           PointQueueType;
-
   // init some test data
   PointType testPoint1;
   testPoint1.Fill(0);
@@ -96,19 +59,7 @@ int itkImageToTreeFilterTest(int, char* argv[] )
   PointType testPoint2;
   testPoint2.Fill(1);
 
-  // TODO: read files with mitk and convert to itk
-  // read in first file
-  //   ImageReaderPointer reader = ImageReaderType::New();
-  //   reader->SetFileName(argv[1]);
-  //   reader->Update();
-  //
-  //   ImagePointer testImage = reader->GetOutput();
-  //   ImagePointer testImage2;
-
-  /******************************************************************
-   * TEST 1: Saving and loading data objects to the filter context
-  *****************************************************************/
-  std::cout << "Testing ITTFilterContext for storage of objects...\n";
+  std::cout << " *** Testing ITTFilterContext for storage of objects ***\n";
   FilterContextPointer filterContext = FilterContextType::New();
 
   PointQueueType* pointQueue1 = filterContext->GetStartPointQueue();
@@ -136,31 +87,76 @@ int itkImageToTreeFilterTest(int, char* argv[] )
   else
   {
     std::cout << "Points not in queue.\n";
-    std::cout << "[TEST FAILED]\n";
+    std::cout << " *** [TEST FAILED] ***\n";
     return EXIT_FAILURE;
   }
-  std::cout << "[TEST PASSED]\n";
+  std::cout << " *** [TEST PASSED] ***\n";
+}
 
-  /****************************************************************
-   * TEST 2: Initialising the filter
-   ****************************************************************/
-  std::cout << "Testing initialization of filter...\n";
+/****************************************************************
+ * TEST 2: Initialising the filter
+ ****************************************************************/
+int testInitFilter(char* argv[])
+{
+  // init some test data
+  PointType testPoint1;
+  testPoint1.Fill(0);
+
+  std::cout << " *** Testing initialization of filter ***\n";
   ImageToTreeFilterPointer testFilter = ImageToTreeFilterType::New();
   testFilter->SetStartPoint(&testPoint1);
 
   // start point should be the first point in the filter
   FilterContextPointer testFilterContext = testFilter->GetFilterContext();
-  PointQueueType* testQueue = filterContext->GetStartPointQueue();
+  PointQueueType* testQueue = testFilterContext->GetStartPointQueue();
   if(testQueue->front() != testPoint1)
   {
     std::cout << "Startpoint not in queue.\n";
-    std::cout << "[TEST FAILED]\n";
+    std::cout << " *** [TEST FAILED] ***\n";
     return EXIT_FAILURE;
   }
-  std::cout << "[TEST PASSED]\n";
+  std::cout << " *** [TEST PASSED] ***\n";
+}
+
+int testTubeSegmentDetector(char* argv[] )
+{
+  return EXIT_FAILURE;
+}
 
 
-  std::cout << "[ALL TESTS DONE]\n";
+int itkImageToTreeFilterTest(int, char* argv[] )
+{
+  ResultListType resultList;
+  int failedCount = 0;
+  int testCount;
+  float failRatio;
+
+  // run all tests
+  resultList.push_back(testFilterContext(argv));
+  resultList.push_back(testInitFilter(argv));
+//   resultList.push_back(testTubeSegmentDetector(argv));
+
+  std::cout << " *** [ALL TESTS DONE] ***\n";
+
+  testCount = resultList.size();
+
+  while (resultList.size() > 0)
+  {
+    int value = resultList.front();
+    if(value == EXIT_FAILURE) { failedCount++; }
+    resultList.pop_front();
+  }
+
+  failRatio = (float) failedCount / (float) testCount;
+
+  std::cout << "Result: ";
+  std::cout << failedCount;
+  std::cout << "/";
+  std::cout << testCount;
+  std::cout << " Tests failed (";
+  std::cout << failRatio;
+  std::cout << " %)\n";
+
   return EXIT_SUCCESS;
 }
 

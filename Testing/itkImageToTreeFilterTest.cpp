e60f892e822388a4931325065199c4616e267d8c
nico <nico@nowhere>
2006-01-30 10:08:31 +0000

    - changed template params to input and output

--- Testing/itkImageToTreeFilterTest.cpp
+++ Testing/itkImageToTreeFilterTest.cpp
@@ -19,24 +19,41 @@ PURPOSE.  See the above copyright notices for more information.
 #include <itkITTFilterContext.h>
 #include <itkImageToTreeFilter.h>
 
+#include <mitkVesselTreeData.h>
+
 int itkImageToTreeFilterTest(int, char* argv[] )
 {
+  // image type
   typedef unsigned char   PixelType;
   const unsigned int      Dimension = 3;
+  typedef itk::Image<PixelType, Dimension>            ImageType;
+  typedef ImageType::Pointer                          ImagePointer;
+  typedef ImageType::PointType                        PointType;
 
-  typedef itk::Image<PixelType, Dimension>        ImageType;
-  typedef ImageType::Pointer                      ImagePointer;
-  typedef ImageType::PointType                    PointType;
+  // tree type
+  typedef mitk::VesselTreeData                        OutputTreeType;
+  typedef OutputTreeType::Pointer                     OutputTreePointer;
+
+
+
 //   typedef itk::ImageFileReader<ImageType>         ImageReaderType;
 //   typedef ImageReaderType::Pointer                ImageReaderPointer;
 
   // test classes
-  typedef itk::ImageToTreeFilter<ImageType>       ImageToTreeFilterType;
-  typedef ImageToTreeFilterType::Pointer          ImageToTreeFilterPointer;
-  typedef itk::ITTFilterContext<ImageType, PointType>
-                                                  FilterContextType;
-  typedef FilterContextType::Pointer              FilterContextPointer;
-  typedef FilterContextType::PointQueueType       PointQueueType;
+  typedef itk::ImageToTreeFilter<ImageType, OutputTreeType>
+                                                      ImageToTreeFilterType;
+  typedef ImageToTreeFilterType::Pointer              ImageToTreeFilterPointer;
+  typedef itk::ITTFilterContext<ImageType, OutputTreeType>
+                                                      FilterContextType;
+  typedef FilterContextType::Pointer                  FilterContextPointer;
+  typedef FilterContextType::PointQueueType           PointQueueType;
+
+  // init some test data
+  PointType testPoint1;
+  testPoint1.Fill(0);
+
+  PointType testPoint2;
+  testPoint1.Fill(1);
 
   // TODO: read files with mitk and convert to itk
   // read in first file
@@ -53,18 +70,53 @@ int itkImageToTreeFilterTest(int, char* argv[] )
   std::cout << "Testing ITTFilterContext for storage of objects...\n";
   FilterContextPointer filterContext = FilterContextType::New();
 
-  PointType testPoint1;
-  testPoint1.Fill(0);
+  PointQueueType* pointQueue1 = filterContext->GetStartPointQueue();
+  std::cout << "Pushing points to filter context...\n";
+  pointQueue1->push(testPoint1);
+  pointQueue1->push(testPoint2);
 
+  std::cout << "Reading points from filter context...\n";
+  PointQueueType* pointQueue2 = filterContext->GetStartPointQueue();
+  if (pointQueue2->front() == testPoint1)
+  {
+    pointQueue2->pop();
+  }
+  else
+  {
+    std::cout << "Points not in queue.\n";
+    std::cout << "[TEST FAILED]\n";
+    return EXIT_FAILURE;
+  }
+
+  if (pointQueue2->front() == testPoint2)
+  {
+    pointQueue2->pop();
+  }
+  else
+  {
+    std::cout << "Points not in queue.\n";
+    std::cout << "[TEST FAILED]\n";
+    return EXIT_FAILURE;
+  }
 
   std::cout << "[TEST PASSED]\n";
 
   /****************************************************************
    * TEST 2: Initialising the filter
    ****************************************************************/
+  std::cout << "Testing initialization of filter...\n";
   ImageToTreeFilterPointer testFilter = ImageToTreeFilterType::New();
+  testFilter->SetStartPoint(&testPoint1);
 
-
+  // start point should be the first point in the filter
+  FilterContextPointer testFilterContext = testFilter->GetFilterContext();
+  PointQueueType* testQueue = filterContext->GetStartPointQueue();
+  if(testQueue->front() != testPoint1)
+  {
+    std::cout << "Startpoint not in queue.\n";
+    std::cout << "[TEST FAILED]\n";
+    return EXIT_FAILURE;
+  }
 
   std::cout << "[TEST DONE]\n";
   return EXIT_SUCCESS;

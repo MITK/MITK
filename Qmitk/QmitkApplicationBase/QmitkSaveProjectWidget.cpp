f0e3b16a947f665be9edc59e96e22f668ebd23e1
matthias <matthias@nowhere>
2006-02-02 10:37:54 +0000

    ENH: initial version of QmitkSaveProjectWidget

---
+++ Qmitk/QmitkApplicationBase/QmitkSaveProjectWidget.cpp
@@ -0,0 +1,231 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$ 
+ 
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "QmitkSaveProjectWidget.h"
+#include <QmitkDataTreeListView.h>
+
+#include <mitkXMLWriter.h>
+
+#include <qlayout.h>
+#include <qfiledialog.h>
+#include <qpushbutton.h>
+#include <qcheckbox.h>
+#include <qlineedit.h>
+#include <qtoolbutton.h>
+
+//#include <qstatusbar.h>
+
+QmitkSaveProjectWidget::QmitkSaveProjectWidget(mitk::DataTree::Pointer dataTree, QWidget* parent, const char* name)
+:QWidget(parent,name), m_DataTree(dataTree)
+{ 
+  PrepareDataTreeFilter();
+  SetLayout();  
+  AddConnections();
+}
+
+void QmitkSaveProjectWidget::PrepareDataTreeFilter()
+{
+  //mitk::DataTreeFilter::PropertyList visible_props;
+  visible_props.push_back("name");
+  //mitk::DataTreeFilter::PropertyList editable_props;
+  editable_props.push_back("name");
+  
+  tree_filter = mitk::DataTreeFilter::New(m_DataTree);
+  tree_filter->SetFilter(&mitk::IsDataTreeNode);
+  //tree_filter->SetFilter(&mitk::IsGoodDataTreeNode);  // filters all nodes without a name
+  tree_filter->SetVisibleProperties(visible_props);
+  tree_filter->SetEditableProperties(editable_props);
+          
+}
+
+
+void QmitkSaveProjectWidget::SetLayout()
+{
+  QVBoxLayout* vertical = new QVBoxLayout(this, QBoxLayout::TopToBottom);
+  QHBoxLayout* horizontal = new QHBoxLayout();
+
+  sourceCheckBox = new QCheckBox("Save Source Files", this);
+  sourceCheckBox->setChecked(true);
+  vertical->addWidget(sourceCheckBox);
+
+  folderChooseButton = new QToolButton(this);
+  folderChooseButton->setMinimumWidth(20);
+  folderChooseButton->setMinimumHeight(20);
+  folderChooseButton->setText("...");
+  folderChooseButton->setTextLabel("Choose a folder to store the source files");
+  horizontal->addWidget(folderChooseButton);
+
+  folderLineEdit = new QLineEdit(this);
+  horizontal->addWidget(folderLineEdit);
+
+  vertical->insertLayout(1, horizontal);
+
+  treelistview = new QmitkDataTreeListView(tree_filter, this);
+  //treelistview->setStretchedColumn(2); 
+  vertical->addWidget( treelistview );
+
+  saveButton = new QPushButton("Save", this);
+  saveButton->setMaximumWidth(80);
+  vertical->addWidget(saveButton);
+}
+
+
+void QmitkSaveProjectWidget::AddConnections()
+{
+  QObject::connect(saveButton, SIGNAL(clicked()), this, SLOT(Save()));
+  QObject::connect(folderChooseButton, SIGNAL(clicked()), this, SLOT(ChooseSourceFolder()));
+  QObject::connect(sourceCheckBox, SIGNAL(stateChanged(int)), this, SLOT(SetEditable(int)));
+}
+
+
+void QmitkSaveProjectWidget::Save()
+{
+  // save dialog
+  QString filename = QFileDialog::getSaveFileName( QString::null, "XML Project description (*.xml)", this,
+                                                   "Save Project File", "Choose a filename to save under");
+
+
+  if ( !filename.isEmpty() ) {
+      TryToExportTree(filename);
+      std::cout<<filename.ascii()<<std::endl;
+      this->close();
+  } //else {
+      //statusBar()->message( "Saving aborted", 2000 );
+  //}
+//std::cout<<filename.ascii()<<std::endl;
+
+}
+
+
+void QmitkSaveProjectWidget::ChooseSourceFolder()
+{
+  // folder dialog
+  QString filename = QFileDialog::getExistingDirectory(QString::null, this, "get existing directory",
+                                                       "Choose the source directory", FALSE );
+  if ( !filename.isEmpty() )
+      folderLineEdit->setText(filename);
+      //std::cout<<filename.ascii()<<std::endl;
+}
+
+
+void QmitkSaveProjectWidget::SetEditable(int state)
+{
+  if(state == QButton::On){
+    folderChooseButton->setDisabled(false);
+    folderLineEdit->setDisabled(false);
+    editable_props.push_back("name");
+    tree_filter->SetEditableProperties(editable_props);
+  }
+  if(state == QButton::Off){
+    editable_props.clear();
+    tree_filter->SetEditableProperties(editable_props);
+    folderChooseButton->setDisabled(true);
+    folderLineEdit->setDisabled(true);
+  }
+}
+
+
+void QmitkSaveProjectWidget::WriteSelectedItems(const mitk::DataTreeFilter::ItemList* items,
+                                          mitk::XMLWriter& xmlWriter)
+{
+  mitk::DataTreeFilter::ConstItemIterator itemiter( items->Begin() ); 
+  mitk::DataTreeFilter::ConstItemIterator itemiterend( items->End() ); 
+
+  while ( itemiter != itemiterend ) // for all items
+  {
+    // check, whether item is selected
+    if ( itemiter->IsSelected() )
+    {
+      xmlWriter.BeginNode(mitk::DataTree::XML_TAG_TREE_NODE);
+
+      // write node via XMLWriter
+      const mitk::DataTreeNode* node = itemiter->GetNode();
+
+      if (node) // could be, that node is NULL
+      {
+        // sets the source file name
+        mitk::BaseProperty::Pointer m_Property = node->GetProperty("name");
+        std::string sourceFileName;
+        if (m_Property){
+          sourceFileName = m_Property->GetValueAsString();
+          if (sourceFileName != "") 
+            sourceFileName = ReplaceWhiteSpacesAndRemoveFileExtension(sourceFileName);
+        }
+        xmlWriter.SetSourceFileName(sourceFileName.c_str());
+        //
+      }      
+      const_cast<mitk::DataTreeNode*>(node)->WriteXML( xmlWriter );
+    }
+    // write children
+    if ( itemiter->HasChildren() )
+    {
+      WriteSelectedItems( itemiter->GetChildren(), xmlWriter );
+    }
+
+    // close XML node
+    if (itemiter->IsSelected())
+    {
+      xmlWriter.EndNode();
+    }
+
+    ++itemiter; // next item
+  }
+}
+
+void QmitkSaveProjectWidget::TryToExportTree(QString filename) 
+{
+  const mitk::DataTreeFilter::ItemList* items;
+  
+  items = tree_filter->GetItems();
+  
+  //std::ofstream os(filename);
+  //mitk::XMLWriter xmlWriter( os );
+  mitk::XMLWriter xmlWriter(filename.ascii());
+
+  xmlWriter.SetSaveSourceFiles(sourceCheckBox->isChecked());
+  if(folderLineEdit->isEnabled())
+    xmlWriter.SetSubFolder(folderLineEdit->text());
+  xmlWriter.BeginNode(mitk::DataTree::XML_NODE_NAME);
+  WriteSelectedItems(items, xmlWriter);
+  xmlWriter.EndNode();
+}
+
+QmitkSaveProjectWidget::~QmitkSaveProjectWidget() 
+{
+  delete sourceCheckBox;
+  delete folderChooseButton;
+  delete folderLineEdit;
+  delete saveButton;
+  delete treelistview;
+  
+  tree_filter = 0;
+  m_DataTree = 0;
+}
+
+std::string QmitkSaveProjectWidget::ReplaceWhiteSpacesAndRemoveFileExtension(std::string string) 
+{
+  int pos = string.find(" ");
+  while (pos != string.npos){
+    string.replace(pos, 1, "_");
+    pos = string.find(" ");
+  }
+  
+  //int e0 = string.find(".");
+  //return string.substr(0, e0).c_str();
+  return string;
+}
+    
---
+++ Qmitk/QmitkApplicationBase/QmitkSaveProjectWidget.h
@@ -0,0 +1,88 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$ 
+ 
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include <qwidget.h>
+
+#include <mitkProperties.h>
+#include <mitkDataTree.h>
+#include <mitkDataTreeFilter.h>
+
+namespace mitk {
+  class XMLWriter;
+}
+
+class QmitkDataTreeListView;
+class QPushButton;
+class QCheckBox;
+class QLineEdit;
+class QToolButton;
+
+class QmitkSaveProjectWidget : public QWidget
+{
+  Q_OBJECT
+
+  public:
+
+    QmitkSaveProjectWidget(mitk::DataTree::Pointer dataTree, QWidget* parent, const char* name = 0);
+
+    virtual ~QmitkSaveProjectWidget();
+    //void QmitkSaveProjectWidget::WriteSelectedItems(const mitk::DataTreeFilter::ItemList*, mitk::XMLWriter&);   
+ 
+  public slots:
+    
+    // opens the save dialog and calls the function to write the XML file
+    void Save();
+
+    // opens a dialog to set the directory of the source files
+    void ChooseSourceFolder();
+
+    void SetEditable(int state);
+    
+  protected:
+    void PrepareDataTreeFilter();
+
+    // sets the layout of the QmitkSaveProjectWidget
+    void SetLayout();
+    
+    void AddConnections();
+
+    // starts the xmlWriter
+    void TryToExportTree(QString);
+    
+    // writes the selected nodes of the datatree to the XML file
+    void WriteSelectedItems(const mitk::DataTreeFilter::ItemList*, mitk::XMLWriter&);
+
+  private:
+    // replaces white spaces with "_"
+    std::string ReplaceWhiteSpacesAndRemoveFileExtension(std::string);
+
+    mitk::DataTree::Pointer m_DataTree;
+
+    mitk::DataTreeFilter::Pointer tree_filter;
+    
+    mitk::DataTreeFilter::PropertyList visible_props;
+    mitk::DataTreeFilter::PropertyList editable_props;
+
+    QmitkDataTreeListView * treelistview;
+    QToolButton * folderChooseButton;
+    QLineEdit * folderLineEdit;
+    QPushButton * saveButton;
+    QCheckBox * sourceCheckBox;
+
+
+};
+

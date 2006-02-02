ce85f294b2d789f312478f62a5be72b70924605d
matthias <matthias@nowhere>
2006-02-02 10:19:15 +0000

    FIX: fixed the path property to get the complete path

--- Core/Algorithms/mitkDataTreeNodeFactory.cpp
+++ Core/Algorithms/mitkDataTreeNodeFactory.cpp
@@ -232,7 +232,9 @@ void mitk::DataTreeNodeFactory::GenerateData()
   {
     mitk::DataTreeNode::Pointer node = this->GetOutput(i);
     // set path without filename as string property
-    mitk::StringProperty::Pointer pathProp = new mitk::StringProperty( itksys::SystemTools::GetFilenamePath( m_FileName ) );
+    std::string path = itksys::SystemTools::GetFilenamePath( m_FileName );
+    path += "/";
+    mitk::StringProperty::Pointer pathProp = new mitk::StringProperty( path );
     node->SetProperty( "path", pathProp );
     // set filename without path as string property
     mitk::StringProperty::Pointer nameProp = dynamic_cast<mitk::StringProperty*>(node->GetProperty("name").GetPointer());

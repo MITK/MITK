9de462ff61ee6706e318d52af9dfae34eb491fb5
matthias <matthias@nowhere>
2006-04-03 13:14:24 +0000

    ENH: initial version

---
+++ Core/IO/mitkObjFileReader.cpp
@@ -0,0 +1,78 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "mitkObjFileReader.h"
+#include <mitkSurface.h>
+#include <vtkOBJReader.h>
+
+
+mitk::ObjFileReader::ObjFileReader()
+    : m_FileName("")
+{
+}
+
+mitk::ObjFileReader::~ObjFileReader()
+{
+}
+
+void mitk::ObjFileReader::GenerateData()
+{
+  mitk::Surface::Pointer output = this->GetOutput();
+
+  if( m_FileName != "")
+  {
+    std::cout << "Loading " << m_FileName << " as obj..." << std::endl;
+
+    vtkOBJReader *reader = vtkOBJReader::New();
+    reader->SetFileName( m_FileName.c_str() );
+    reader->Update();
+
+    if ( reader->GetOutput() != NULL )
+      output->SetVtkPolyData( reader->GetOutput() );
+
+    reader->Delete();
+
+    std::cout << "...finished!" << std::endl;
+  }
+}
+
+bool mitk::ObjFileReader::CanReadFile(const std::string filename, const std::string filePrefix, const std::string filePattern) 
+{
+  // First check the extension
+  //std::string filename = file;
+  if(  filename == "" )
+    {
+      //std::cout<<"No filename specified."<<std::endl;
+    return false;
+    }
+
+  bool extensionFound = false;
+  std::string::size_type ObjPos = filename.rfind(".obj");
+  if ((ObjPos != std::string::npos)
+      && (ObjPos == filename.length() - 4))
+    {
+    extensionFound = true;
+    }
+
+  if( !extensionFound )
+    {
+      //std::cout<<"The filename extension is not recognized."<<std::endl;
+    return false;
+    }
+
+  return true;
+}
---
+++ Core/IO/mitkObjFileReader.h
@@ -0,0 +1,68 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef ObjFileReader_H_HEADER_INCLUDED
+#define ObjFileReader_H_HEADER_INCLUDED
+
+#include "mitkCommon.h"
+#include "mitkSurfaceSource.h"
+
+namespace mitk {
+//##Documentation
+//## @brief Reader to read files in Obj-format
+//## @ingroup IO
+class ObjFileReader : public SurfaceSource
+{
+public:
+    mitkClassMacro(ObjFileReader, SurfaceSource);
+
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+
+    itkSetMacro(FileName, std::string);
+    itkSetStringMacro(FileName);
+    itkGetMacro(FileName, std::string);
+    itkGetStringMacro(FileName);
+
+    itkSetMacro(FilePrefix, std::string);
+    itkSetStringMacro(FilePrefix);
+    itkGetMacro(FilePrefix, std::string);
+    itkGetStringMacro(FilePrefix);
+
+    itkSetMacro(FilePattern, std::string);
+    itkSetStringMacro(FilePattern);
+    itkGetMacro(FilePattern, std::string);
+    itkGetStringMacro(FilePattern);
+
+    static bool CanReadFile(const std::string filename, const std::string filePrefix, const std::string filePattern);
+
+protected:
+    virtual void GenerateData();
+
+    ObjFileReader();
+
+    ~ObjFileReader();
+
+    std::string m_FileName, m_FilePrefix, m_FilePattern;
+
+};
+
+} // namespace mitk
+
+#endif /* ObjFileReader_H_HEADER_INCLUDED */

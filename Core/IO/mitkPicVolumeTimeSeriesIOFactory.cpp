0ef6dc65fcb91df528825a3e13674467e69787a8
matthias <matthias@nowhere>
2006-04-05 12:27:36 +0000

    ENH: initial version

---
+++ Core/IO/mitkPicVolumeTimeSeriesIOFactory.cpp
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "mitkPicVolumeTimeSeriesIOFactory.h"
+#include "mitkIOAdapter.h"
+#include "mitkPicVolumeTimeSeriesReader.h"
+
+#include "itkVersion.h"
+
+  
+namespace mitk
+{
+PicVolumeTimeSeriesIOFactory::PicVolumeTimeSeriesIOFactory()
+{
+  this->RegisterOverride("mitkIOAdapter",
+                         "mitkPicVolumeTimeSeriesReader",
+                         "mitk Pic Image IO",
+                         1,
+                         itk::CreateObjectFunction<IOAdapter<PicVolumeTimeSeriesReader> >::New());
+}
+  
+PicVolumeTimeSeriesIOFactory::~PicVolumeTimeSeriesIOFactory()
+{
+}
+
+const char* PicVolumeTimeSeriesIOFactory::GetITKSourceVersion() const
+{
+  return ITK_SOURCE_VERSION;
+}
+
+const char* PicVolumeTimeSeriesIOFactory::GetDescription() const
+{
+  return "PicVolumeTimeSeries IO Factory, allows the loading of DKFZ Pic images";
+}
+
+} // end namespace mitk

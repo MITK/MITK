2c1b365d99354cd582fdb017b5379861e033face
matthias <matthias@nowhere>
2006-04-06 14:17:51 +0000

    ENH: initial version

---
+++ Core/IO/mitkVtkImageIOFactory.cpp
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "mitkVtkImageIOFactory.h"
+#include "mitkIOAdapter.h"
+#include "mitkVtkImageReader.h"
+
+#include "itkVersion.h"
+
+  
+namespace mitk
+{
+VtkImageIOFactory::VtkImageIOFactory()
+{
+  this->RegisterOverride("mitkIOAdapter",
+                         "mitkVtkImageReader",
+                         "mitk Vtk Image IO",
+                         1,
+                         itk::CreateObjectFunction<IOAdapter<VtkImageReader> >::New());
+}
+  
+VtkImageIOFactory::~VtkImageIOFactory()
+{
+}
+
+const char* VtkImageIOFactory::GetITKSourceVersion() const
+{
+  return ITK_SOURCE_VERSION;
+}
+
+const char* VtkImageIOFactory::GetDescription() const
+{
+  return "VtkImage IO Factory, allows the loading of pvtk files";
+}
+
+} // end namespace mitk

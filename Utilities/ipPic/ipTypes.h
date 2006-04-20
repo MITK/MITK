5e4fa3ac21d47e6dc722b22981a2648b633514e5
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2006-04-20 12:56:00 +0000

    FIX/CHG/ENH: added PPC-Mac definitions

--- Utilities/ipPic/ipTypes.h
+++ Utilities/ipPic/ipTypes.h
@@ -58,7 +58,7 @@
 #ifndef _ipTypes_h
 #define _ipTypes_h
 
-#if defined (__mips) || (_IBMR2) || (sun) || (__i386__) || (__convex__) || (__alpha) || (__hppa) || (WIN32) || (_WIN32) || (__PPC__) || (__x86_64__)
+#if defined (__mips) || (_IBMR2) || (sun) || (__i386__) || (__convex__) || (__alpha) || (__hppa) || (WIN32) || (_WIN32) || defined (__PPC__) || defined (__x86_64__) || defined (__APPLE__)
   typedef    signed char  ipInt1_t;
   typedef   signed short  ipInt2_t;
   typedef     signed int  ipInt4_t;

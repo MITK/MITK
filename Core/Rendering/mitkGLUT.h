172731c0ec2aa6c4382a38f59fa08f5637e9d7d6
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2006-04-20 13:49:29 +0000

    initial revision

---
+++ Core/Rendering/mitkGLUT.h
@@ -0,0 +1,30 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef MITKGLUT_H_HEADER_INCLUDED
+#define MITKGLUT_H_HEADER_INCLUDED
+
+#ifdef __APPLE__
+#include <glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#endif /* MITKGLUT_H_HEADER_INCLUDED */
+

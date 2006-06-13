d4f92cb7a1229436854bbaef9a3b47695c11a207
Daniel Maleike <d.maleike@dkfz-heidelberg.de>
2006-06-13 16:22:48 +0000

    remove debug code

--- Core/DataStructures/mitkPropertyManager.cpp
+++ Core/DataStructures/mitkPropertyManager.cpp
@@ -44,8 +44,7 @@ mitk::BaseProperty::Pointer mitk::PropertyManager::CreateDefaultProperty(std::st
   }
   else
   {
-    std::cout << "Warning: non-existing default property requested: "
-    << name << std::endl;
+    //std::cout << "Warning: non-existing default property requested: " << name << std::endl;
   }
   return newProperty;
 }

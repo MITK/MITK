427010d103e1424130c968027d4b353de8403c7c
max <max@nowhere>
2006-06-23 14:47:04 +0000

    Renamed vtkGradientBackground to mitkGradientBackground and moved it to an appropriate directory

---
+++ Core/Rendering/mitkGradientBackground.cpp
@@ -0,0 +1,239 @@
+#include "mitkGradientBackground.h"
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkMapper.h>
+#include <vtkActor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkPolyData.h>
+#include <vtkCamera.h>
+#include <vtkLookupTable.h>
+#include <vtkCellArray.h>
+#include <vtkUnsignedIntArray.h>
+#include <vtkPoints.h>
+#include <vtkPointData.h>
+#include <vtkObjectFactory.h>
+#include <vtkRendererCollection.h>
+
+mitk::GradientBackground::GradientBackground()
+{
+  m_RenderWindow = NULL;
+  m_Renderer = vtkRenderer::New();
+  m_Actor = vtkActor::New();
+  m_Mapper = vtkPolyDataMapper::New();
+  m_Lut = vtkLookupTable::New();
+  m_Plane = vtkPolyData::New();
+  m_Layer = 1;
+
+  vtkPoints* points = vtkPoints::New( ); 
+  points->InsertPoint(0,-10,0,0);
+  points->InsertPoint(1,-10,1,0);
+  points->InsertPoint(2,10,1,0);
+  points->InsertPoint(3,10,0,0);
+
+  vtkCellArray* cellArray = vtkCellArray::New();
+  cellArray->InsertNextCell(4);
+  cellArray->InsertCellPoint(0);
+  cellArray->InsertCellPoint(1);
+  cellArray->InsertCellPoint(2);
+  cellArray->InsertCellPoint(3);
+
+  vtkUnsignedIntArray* data = vtkUnsignedIntArray::New();  
+  data->InsertTuple1(0,1);
+  data->InsertTuple1(1,0);
+  data->InsertTuple1(2,0);
+  data->InsertTuple1(3,1);
+
+  m_Plane->SetPoints( points );
+  m_Plane->SetPolys( cellArray );
+  m_Plane->GetPointData()->SetScalars( data );
+
+  m_Lut->SetNumberOfColors( 2 );
+  m_Lut->Build();
+  m_Lut->SetTableValue( m_Lut->GetIndex(0), 1, 1, 1 );
+  m_Lut->SetTableValue( m_Lut->GetIndex(1), 0, 0, 0 );
+
+  m_Mapper->SetInput( m_Plane );
+  m_Mapper->SetLookupTable( m_Lut );
+
+  m_Actor->SetMapper( m_Mapper );
+
+  m_Renderer->AddActor( m_Actor );
+  m_Renderer->InteractiveOff();
+  m_Renderer->GetActiveCamera()->ParallelProjectionOn();
+  m_Renderer->ResetCamera();
+  m_Renderer->GetActiveCamera()->SetParallelScale(0.5);
+  m_Renderer->SetLayer( m_Layer );
+}
+
+mitk::GradientBackground::~GradientBackground()
+{
+  if ( m_RenderWindow != NULL )
+    if ( this->IsEnabled() )
+      this->Disable();
+  
+  if ( m_Plane != NULL )
+    m_Plane->Delete();
+  
+  if( m_Lut != NULL )
+    m_Lut->Delete();
+
+  if ( m_Mapper != NULL )
+    m_Mapper->Delete();
+  
+  if ( m_Actor!=NULL )
+    m_Actor->Delete();
+  
+  if ( m_Renderer != NULL )
+    m_Renderer->Delete();
+}
+
+/**
+ * Sets the renderwindow, in which the gradient background
+ * will be shown. Make sure, you have called this function
+ * before calling Enable()
+ */
+void mitk::GradientBackground::SetVtkRenderWindow( vtkRenderWindow* renderWindow )
+{
+  m_RenderWindow = renderWindow;
+}
+
+/**
+ * Returns the vtkRenderWindow, which is used
+ * for displaying the gradient background
+ */
+vtkRenderWindow* mitk::GradientBackground::GetVtkRenderWindow()
+{
+  return m_RenderWindow;
+}
+
+/**
+ * Returns the renderer responsible for
+ * rendering the color gradient into the
+ * vtkRenderWindow
+ */
+vtkRenderer* mitk::GradientBackground::GetVtkRenderer()
+{
+  return m_Renderer;
+}
+
+/**
+ * Returns the actor associated with the color gradient
+ */
+vtkActor* mitk::GradientBackground::GetActor()
+{
+  return m_Actor;
+}
+
+/**
+ * Returns the mapper associated with the color
+ * gradient.
+ */
+vtkPolyDataMapper* mitk::GradientBackground::GetMapper()
+{
+  return m_Mapper;
+}
+
+
+/**
+ * Sets the gradient colors. The gradient
+ * will smoothly fade from color1 to color2
+ */
+void mitk::GradientBackground::SetGradientColors( vtkFloatingPointType r1, vtkFloatingPointType g1, vtkFloatingPointType b1, vtkFloatingPointType r2, vtkFloatingPointType g2, vtkFloatingPointType b2 )
+{
+  m_Lut->SetTableValue( m_Lut->GetIndex(0), r1, g1, b1 );
+  m_Lut->SetTableValue( m_Lut->GetIndex(1), r2, g2, b2 );
+}
+
+
+void mitk::GradientBackground::SetUpperColor(vtkFloatingPointType r, vtkFloatingPointType g, vtkFloatingPointType b )
+{
+  m_Lut->SetTableValue( m_Lut->GetIndex(0), r, g, b );
+}
+
+
+void mitk::GradientBackground::SetLowerColor(vtkFloatingPointType r, vtkFloatingPointType g, vtkFloatingPointType b )
+{
+  m_Lut->SetTableValue( m_Lut->GetIndex(1), r, g, b );
+}
+
+
+/**
+ * The layer, which will be used for rendering into
+ * the renderwindow may be set using this function.
+ * The default layer is one.
+ */
+void mitk::GradientBackground::SetLayer( int layer )
+{
+  m_Layer = layer;
+  m_Renderer->SetLayer( m_Layer );
+}
+
+/**
+ * Returns the layer, in which the color gradient
+ * will be drawn
+ */
+int mitk::GradientBackground::GetLayer()
+{
+  return m_Layer;
+}
+
+/**
+ * Enables drawing of the color gradient background.
+ * If you want to disable it, call the Disable() function.
+ */
+void mitk::GradientBackground::Enable()
+{
+  if ( m_RenderWindow == NULL )
+    return;
+  if ( m_Renderer == NULL )
+    return;
+
+  m_RenderWindow->SetNumberOfLayers( 2 );
+  m_RenderWindow->AddRenderer( m_Renderer );
+}
+
+/**
+ * Disables drawing of the color gradient background.
+ * If you want to enable it, call the Enable() function.
+ */
+void mitk::GradientBackground::Disable()
+{
+  if ( this->IsEnabled() )
+    m_RenderWindow->RemoveRenderer( m_Renderer );
+}
+
+
+
+/**
+ * Checks, if the gradient background is currently
+ * enabled (visible)
+ */
+bool mitk::GradientBackground::IsEnabled()
+{
+    if ( m_RenderWindow == NULL )
+        return false;
+    else
+        return ( m_RenderWindow->GetRenderers()->IsItemPresent( m_Renderer ) != 0 );    
+}
+
+
+void mitk::GradientBackground::SetRequestedRegionToLargestPossibleRegion()
+{
+    //nothing to do
+}
+
+bool mitk::GradientBackground::RequestedRegionIsOutsideOfTheBufferedRegion()
+{
+    return false;    
+}
+
+bool mitk::GradientBackground::VerifyRequestedRegion()
+{
+    return true;
+}
+
+void mitk::GradientBackground::SetRequestedRegion(itk::DataObject*)
+{
+    //nothing to do
+}
+
---
+++ Core/Rendering/mitkGradientBackground.h
@@ -0,0 +1,158 @@
+#ifndef _vtk_Gradient_Background_h_
+#define _vtk_Gradient_Background_h_
+
+
+#include <vtkSystemIncludes.h>
+#include <mitkBaseData.h>
+
+class vtkRenderWindow;
+class vtkRenderer;
+class vtkMapper;
+class vtkActor;
+class vtkPolyDataMapper;
+class vtkLookupTable;
+class vtkPolyData;
+
+namespace mitk
+{
+
+/**
+ * Displays a color gradient in the background
+ * of a vtkRenderWindow.
+ * The gradient ist faked by displaying a non-interactable
+ * smoothly shaded plane in a separate layer behind the
+ * scene. After setting the renderwindow, the gradient may be 
+ * activated by calling Enable()
+ */
+class GradientBackground : public BaseData
+{
+public:
+
+  mitkClassMacro( GradientBackground, BaseData );
+  
+  itkNewMacro( Self );
+
+  /**
+   * Sets the renderwindow, in which the gradient background
+   * will be shown. Make sure, you have called this function
+   * before calling Enable()
+   */
+  virtual void SetVtkRenderWindow( vtkRenderWindow* renderWindow );
+
+  /**
+   * Returns the vtkRenderWindow, which is used
+   * for displaying the gradient background
+   */
+  virtual vtkRenderWindow* GetVtkRenderWindow();
+
+  /**
+   * Returns the renderer responsible for
+   * rendering the color gradient into the
+   * vtkRenderWindow
+   */
+  virtual vtkRenderer* GetVtkRenderer();
+
+  /**
+   * Returns the actor associated with the color gradient
+   */
+  virtual vtkActor* GetActor();
+
+  /**
+   * Returns the mapper associated with the color
+   * gradient.
+   */
+  virtual vtkPolyDataMapper* GetMapper();
+
+  /**
+   * Sets the gradient colors. The gradient
+   * will smoothly fade from color1 to color2
+   */
+  virtual void SetGradientColors( vtkFloatingPointType r1, vtkFloatingPointType g1, vtkFloatingPointType b1, vtkFloatingPointType r2, vtkFloatingPointType g2, vtkFloatingPointType b2);
+  virtual void SetUpperColor(vtkFloatingPointType r, vtkFloatingPointType g, vtkFloatingPointType b );
+  virtual void SetLowerColor(vtkFloatingPointType r, vtkFloatingPointType g, vtkFloatingPointType b );
+
+  /**
+   * The layer, which will be used for rendering into
+   * the renderwindow may be set using this function.
+   * The default layer is one.
+   */
+  virtual void SetLayer( int layer );
+
+  /**
+   * Returns the layer, in which the color gradient
+   * will be drawn
+   */
+  virtual int GetLayer();
+
+  /**
+   * Enables drawing of the color gradient background.
+   * If you want to disable it, call the Disable() function.
+   */
+  virtual void Enable();
+
+  /**
+   * Disables drawing of the color gradient background.
+   * If you want to enable it, call the Enable() function.
+   */
+  virtual void Disable();
+  
+  /**
+   * Checks, if the gradient background is currently
+   * enabled (visible)
+   */
+  virtual bool IsEnabled();
+  
+  /**
+   * Empty implementation, since the GradientBackground doesn't
+   * support the requested region concept
+   */
+  virtual void SetRequestedRegionToLargestPossibleRegion();
+  
+  /**
+   * Empty implementation, since the GradientBackground doesn't
+   * support the requested region concept
+   */
+  virtual bool RequestedRegionIsOutsideOfTheBufferedRegion();
+  
+  /**
+   * Empty implementation, since the GradientBackground doesn't
+   * support the requested region concept
+   */
+  virtual bool VerifyRequestedRegion();
+  
+  /**
+   * Empty implementation, since the GradientBackground doesn't
+   * support the requested region concept
+   */
+  virtual void SetRequestedRegion(itk::DataObject*);
+  
+protected:
+
+  /**
+   * Constructor
+   */
+  GradientBackground();
+
+  /**
+   * Destructor
+   */
+  ~GradientBackground();
+
+  vtkRenderWindow* m_RenderWindow;
+
+  vtkRenderer* m_Renderer;
+
+  vtkActor* m_Actor;
+
+  vtkPolyDataMapper* m_Mapper;
+
+  vtkLookupTable* m_Lut;
+
+  vtkPolyData* m_Plane;
+
+  int m_Layer; 
+};
+
+} //end of namespace mitk
+#endif
+

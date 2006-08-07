040b90cec80a2f131dbabee176766e56e1717c52
nico <nico@nowhere>
2006-08-07 15:52:02 +0000

    ADD: doxygen doc

--- Core/DataStructures/mitkItkBaseDataAdapter.h
+++ Core/DataStructures/mitkItkBaseDataAdapter.h
@@ -1,3 +1,21 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #include "mitkBaseData.h"
 
 #include <itkDataObject.h>
@@ -5,7 +23,11 @@
 namespace mitk {
 
 /**
- * \brief Small class to use itk::DataObjects in the mitk::DataTree.
+ * \brief Small class to store itk::DataObjects in the mitk::DataTree.
+ *
+ * This class can used to store any itk::DataObject in the data tree.
+ * Please be aware that these objects cannot be visualized via mappers.
+ * The user of this class must care himself for type safety.
  **/
 class ItkBaseDataAdapter : public BaseData
 {
@@ -13,9 +35,12 @@ public:
   mitkClassMacro(ItkBaseDataAdapter, BaseData);
   itkNewMacro(Self);
 
+  /** Typedef for the data object */
   typedef itk::DataObject DataType;
 
+  /** Returns the data object. **/
   itkGetObjectMacro(ItkDataObject, DataType);
+  /** Sets the data object. **/
   itkSetObjectMacro(ItkDataObject, DataType);
 
   virtual void SetRequestedRegionToLargestPossibleRegion() {}

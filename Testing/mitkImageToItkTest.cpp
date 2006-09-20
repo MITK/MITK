50c329c168a4056ffb820fc859507707c3198898
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2006-09-20 14:17:11 +0000

    ENH: improved testing for 4D images

--- Testing/mitkImageToItkTest.cpp
+++ Testing/mitkImageToItkTest.cpp
@@ -54,7 +54,91 @@ int compareGeometries(mitk::Geometry3D* geometry1, mitk::Geometry3D* geometry2)
   }
   return EXIT_SUCCESS;
 }
-  
+
+template <class ImageType>
+int testBackCasting(mitk::Image* imgMem, typename ImageType::Pointer & itkImage)
+{
+  int result;
+
+  if(itkImage.IsNull())
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  int *p = (int*)itkImage->GetBufferPointer();
+  if(p==NULL)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+
+  std::cout << "Testing mitk::CastToMitkImage: " << std::flush;
+  mitk::Image::Pointer mitkImage = mitk::Image::New();
+  mitk::CastToMitkImage( itkImage, mitkImage );
+  std::cout<<"[PASSED]"<<std::endl;
+
+  result = compareGeometries(imgMem->GetGeometry(), mitkImage->GetGeometry());
+  if(result != EXIT_SUCCESS)
+    return result;
+
+  std::cout << "Testing whether data after mitk::CastToMitkImage is available: " << std::flush;
+  if(mitkImage->IsChannelSet()==false)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+
+  std::cout << "Testing mitk::ImportItkImage: " << std::flush;
+  mitkImage = mitk::ImportItkImage(itkImage);
+  std::cout<<"[PASSED]"<<std::endl;
+
+  result = compareGeometries(imgMem->GetGeometry(), mitkImage->GetGeometry());
+  if(result != EXIT_SUCCESS)
+    return result;
+
+  std::cout << "Testing whether data after mitk::ImportItkImage is available: " << std::flush;
+  if(mitkImage->IsChannelSet()==false)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+
+  return EXIT_SUCCESS;
+}
+
+
+template <unsigned int dim>
+int testImageToItkAndBack(mitk::Image* imgMem)
+{
+  int result;
+
+  int *p = (int*)imgMem->GetData();
+  if(p==NULL)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout<<"[PASSED]"<<std::endl;
+
+  std::cout << "Testing for dimension " << dim << ": " << std::flush;
+  std::cout << "Testing mitk::ImageToItk: " << std::flush;
+  typedef itk::Image<int,dim> ImageType;
+
+  mitk::ImageToItk<ImageType>::Pointer toItkFilter = mitk::ImageToItk<ImageType>::New();
+  toItkFilter->SetInput(imgMem);
+  toItkFilter->Update();
+  ImageType::Pointer itkImage = toItkFilter->GetOutput();
+
+  result = testBackCasting<ImageType>(imgMem, itkImage);
+  if(result != EXIT_SUCCESS)
+    return result;
+
+  return EXIT_SUCCESS;
+}
+
 int mitkImageToItkTest(int /*argc*/, char* /*argv*/[])
 {
   int result;
@@ -89,34 +173,23 @@ int mitkImageToItkTest(int /*argc*/, char* /*argv*/[])
 
   std::cout << "Testing Initialize(const mitk::PixelType& type, int sDim, const mitk::PlaneGeometry& geometry) and GetData(): ";
   imgMem->Initialize(mitk::PixelType(typeid(int)), 40, *planegeometry); //XXXXXXXXXXXXXXXXXXXXXCHANGE!
-  int *p = (int*)imgMem->GetData();
-  if(p==NULL)
-  {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
-  std::cout<<"[PASSED]"<<std::endl;
+
+  result = testImageToItkAndBack<3>(imgMem);
+  if(result != EXIT_SUCCESS)
+    return result;
 
   std::cout << "Testing mitk::CastToItkImage: " << std::flush;
   typedef itk::Image<float,3> ImageType;
   ImageType::Pointer itkImage;
   mitk::CastToItkImage( imgMem, itkImage );
-  std::cout<<"[PASSED]"<<std::endl;
 
-  std::cout << "Testing mitk::CastToMitkImage: " << std::flush;
-  mitk::Image::Pointer mitkImage = mitk::Image::New();
-  mitk::CastToMitkImage( itkImage, mitkImage );
-  std::cout<<"[PASSED]"<<std::endl;
-
-  result = compareGeometries(imgMem->GetGeometry(), mitkImage->GetGeometry());
+  result = testBackCasting<ImageType>(imgMem, itkImage);
   if(result != EXIT_SUCCESS)
     return result;
 
-  std::cout << "Testing mitk::ImportItkImage: " << std::flush;
-  mitkImage = mitk::ImportItkImage(itkImage);
-  std::cout<<"[PASSED]"<<std::endl;
-
-  result = compareGeometries(imgMem->GetGeometry(), mitkImage->GetGeometry());
+  std::cout << "Testing Initialize(const mitk::PixelType& type, int sDim, const mitk::PlaneGeometry& geometry) and GetData(): ";
+  imgMem->Initialize(mitk::PixelType(typeid(int)), 40, *planegeometry, false, 1, 6);
+  result = testImageToItkAndBack<4>(imgMem);
   if(result != EXIT_SUCCESS)
     return result;
 

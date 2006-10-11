ad87aaad817f433a34a7fc8951430bddb324c662
Matthias Baumhauer <m.baumhauer@dkfz-heidelberg.de>
2006-10-11 09:39:11 +0000

    FIX: avoid getting videos with bottom up; documentation added

--- Core/Controllers/mitkMovieGeneratorOpenCV.cpp
+++ Core/Controllers/mitkMovieGeneratorOpenCV.cpp
@@ -1,5 +1,6 @@
 #include "mitkMovieGeneratorOpenCV.h"
 #include <GL/gl.h>
+#include <iostream>
 
 
 mitk::MovieGeneratorOpenCV::MovieGeneratorOpenCV()
@@ -10,8 +11,8 @@ mitk::MovieGeneratorOpenCV::MovieGeneratorOpenCV()
 
 void mitk::MovieGeneratorOpenCV::SetFileName( const char *fileName )
 {
-  m_sFile = fileName;
 
+  m_sFile = fileName;
 }
 
 bool mitk::MovieGeneratorOpenCV::InitGenerator()
@@ -22,12 +23,30 @@ bool mitk::MovieGeneratorOpenCV::InitGenerator()
   m_width = viewport[2];  m_width -= m_width % 4;
   m_height = viewport[3];  m_height -= m_height % 4;
   
-  m_currentFrame = cvCreateImage(cvSize(m_width,m_height),8,3);
-  m_aviWriter    = cvCreateVideoWriter(m_sFile,-1,m_dwRate,cvSize(m_width,m_height));
+  m_currentFrame = cvCreateImage(cvSize(m_width,m_height),8,3); // creating image with widget size, 8 bit per pixel and 3 channel r,g,b
+  m_currentFrame->origin = 1; // avoid building a video with bottom up
+  
+  /*
+  m_sFile = Name of the output video file. 
+  -1 = 4-character code of codec used to compress the frames. For example, CV_FOURCC('P','I','M','1') is MPEG-1 codec, 
+  CV_FOURCC('M','J','P','G') is motion-jpeg codec etc. Under Win32 it is possible to pass -1 in order to choose compression 
+  method and additional compression parameters from dialog. 
+  m_dwRate = Framerate of the created video stream. 
+  frame_size Size of video frames. 
+  1 = If it is not zero, the encoder will expect and encode color frames, otherwise it will work with grayscale frames 
+  (the flag is currently supported on Windows only).*/
+  #ifdef WIN32
+    m_aviWriter = cvCreateVideoWriter(m_sFile,-1,m_dwRate,cvSize(m_width,m_height),1); //initializing video writer
+  #else
+    m_aviWriter = cvCreateVideoWriter(m_sFile,CV_FOURCC('X','V','I','D'),m_dwRate,cvSize(m_width,m_height)); //initializing video writer
+  #endif
   
   if(!m_aviWriter)
+  {
+    std::cout << "errors initializing video writer...correct video file path? on linux: ffmpeg must be included in OpenCV.";
     return false;
-  
+  }
+
   return true;
 }
 
@@ -36,14 +55,14 @@ bool mitk::MovieGeneratorOpenCV::AddFrame( void *data )
 {
   cvSetImageData(m_currentFrame,data,m_width*3);
   cvWriteFrame(m_aviWriter,m_currentFrame);
-
   return true;
 }
 
 
 bool mitk::MovieGeneratorOpenCV::TerminateGenerator()
 {
-  if (m_aviWriter) {
+  if (m_aviWriter) 
+  {
     cvReleaseVideoWriter(&m_aviWriter);
   }
   return true;

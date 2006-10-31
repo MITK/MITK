1f9eb27800b66f617678a648745b7bae9507709a
hasselberg <hasselberg@nowhere>
2006-10-31 16:06:05 +0000

    NEW: Test for closedspline (Bug#218)

---
+++ Testing/mitkClosedSplineTest.cpp
@@ -0,0 +1,63 @@
+#include <mitkClosedSpline.h>
+
+//#include <mitkVector.h>
+#include <mitkPointOperation.h>
+#include <mitkInteractionConst.h>
+#include <mitkRingSpline.h>
+#include <fstream>
+
+int mitkClosedSplineTest(int /*argc*/, char* /*argv*/[])
+{  //Create ClosedSpline
+  mitk::RingSpline::Pointer cs = mitk::RingSpline::New();
+  //mitk::ClosedSpline::Pointer cs = mitk::ClosedSpline::New();
+
+  //try to get the itkClosedSpline
+  std::cout << "Create a RingSpline and try to get the itkClosedSpline";
+  mitk::RingSpline::DataType::Pointer itkdata = NULL;
+  //mitk::ClosedSpline::DataType::Pointer itkdata = NULL;
+  itkdata = cs->GetPointSet();
+  if (itkdata.IsNull())
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //fresh ClosedSpline has to be empty!
+  std::cout << "Is the ClosedSpline empty?";
+  if (cs->GetSize() != 0)
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //create an operation and add a point.
+  int position;
+  mitk::Point3D point;
+  for (int i = 1; i <6; ++i)
+  {
+    position = i-1;
+    point.Fill( (i%2)?i:i*5 );
+    mitk::PointOperation* doOp = new mitk::PointOperation(mitk::OpINSERT, point, position);
+    cs->ExecuteOperation(doOp);
+    
+    //now check new condition!
+    if ( (cs->GetSize()!= i) )
+    {
+      std::cout<<"[FAILED] while: "<<cs->GetSize()<<std::endl;
+      return EXIT_FAILURE;
+    }
+  }
+
+  if (!cs->SplineExist())
+  {
+    std::cout<<"Spline no exist: [FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+
+
+  //well done!!! Passed!
+  std::cout<<"[PASSED]"<<std::endl;
+
+  std::cout<<"[TEST DONE]"<<std::endl;
+  return EXIT_SUCCESS;
+}

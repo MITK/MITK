fcbabcf6d42d9cb68529899ff16b9aa4cf0fdf5b
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2006-11-13 11:56:29 +0000

    FIX: works now

--- QApplications/MainApp/ChiliPlugin/mitkChiliPluginFactory.cpp
+++ QApplications/MainApp/ChiliPlugin/mitkChiliPluginFactory.cpp
@@ -19,26 +19,82 @@ PURPOSE.  See the above copyright notices for more information.
 #include "mitkChiliPluginFactory.h"
 #include "itkCreateObjectFunction.h"
 #include "itkVersion.h"
-  
+
+#include "mitkChiliPlugin.h"
+#include "mitkChiliPluginImpl.h"
+#include "mitkLightBoxImageReader.h"
+#include "mitkLightBoxImageReaderImpl.h"
+#include "mitkLightBoxResultImageWriter.h"
+#include "mitkLightBoxResultImageWriterImpl.h"
+
 namespace mitk
 {
-ChiliPluginFactory::ChiliPluginFactory()
+
+template <class T>
+class CreateChiliObjectFunction : public itk::CreateObjectFunctionBase
 {
-  this->RegisterOverride("mitk::ChiliPlugin",
+public:
+  /** Standard class typedefs. */
+  typedef CreateChiliObjectFunction  Self;
+  typedef itk::SmartPointer<Self>    Pointer;
+    
+  /** Methods from itk:LightObject. */
+  itkFactorylessNewMacro(Self);
+  LightObject::Pointer CreateObject() { typename T::Pointer p = T::New(); 
+    p->Register();
+    return p.GetPointer(); 
+  }
+
+protected:
+  CreateChiliObjectFunction() {}
+  ~CreateChiliObjectFunction() {}
+  
+private:
+  CreateChiliObjectFunction(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+};
+
+
+
+  
+  
+  
+  
+  
+  ChiliPluginFactory::ChiliPluginFactory()
+{
+  std::cout << "registering ChiliPluginFactory" << std::endl;
+  /*this->RegisterOverride("mitk::ChiliPlugin",
                          "mitk::ChiliPluginImpl",
                          "MITK Chili Plugin",
                          1,
-                         CreateObjectFunction<mitk::ChiliPluginImpl>::New());
+                         itk::CreateObjectFunction<mitk::ChiliPluginImpl>::New());
   this->RegisterOverride("mitk::LightBoxImageReader",
                          "mitk::LightBoxImageReaderImpl",
                          "MITK Chili Plugin Lightbox Reader",
                          1,
-                         CreateObjectFunction<mitk::LightBoxImageReaderImpl>::New());
+                         itk::CreateObjectFunction<mitk::LightBoxImageReaderImpl>::New());
   this->RegisterOverride("mitk::LightBoxResultImageWriter",
                          "mitk::LightBoxResultImageWriterImpl",
                          "MITK Chili Plugin Lightbox Writer",
                          1,
-                         CreateObjectFunction<mitk::LightBoxResultImageWriterImpl>::New());
+                         mitk::CreateChiliObjectFunction<mitk::LightBoxResultImageWriterImpl>::New());*/
+  this->RegisterOverride(typeid(mitk::ChiliPlugin).name(),
+                         typeid(mitk::ChiliPluginImpl).name(),
+                         "MITK Chili Plugin",
+                         1,
+                         mitk::CreateChiliObjectFunction<mitk::ChiliPluginImpl>::New());
+  this->RegisterOverride(typeid(mitk::LightBoxImageReader).name(),
+                         typeid(mitk::LightBoxImageReaderImpl).name(),
+                         "MITK Chili Plugin Lightbox Reader).name()",
+                         1,
+                         mitk::CreateChiliObjectFunction<mitk::LightBoxImageReaderImpl>::New());
+  this->RegisterOverride(typeid(mitk::LightBoxResultImageWriter).name(),
+                         typeid(mitk::LightBoxResultImageWriterImpl).name(),
+                         "MITK Chili Plugin Lightbox Writer",
+                         1,
+                         mitk::CreateChiliObjectFunction<mitk::LightBoxResultImageWriterImpl>::New());
+  ObjectFactoryBase::RegisterFactory(this);
 }
   
 ChiliPluginFactory::~ChiliPluginFactory()
@@ -56,4 +112,6 @@ const char* ChiliPluginFactory::GetDescription() const
 }
 
 } // end namespace mitk
+
+mitk::ChiliPluginFactory::Pointer dummyFactory = mitk::ChiliPluginFactory::New();
 
--- QApplications/MainApp/ChiliPlugin/mitkChiliPluginFactory.h
+++ QApplications/MainApp/ChiliPlugin/mitkChiliPluginFactory.h
@@ -19,12 +19,14 @@ PURPOSE.  See the above copyright notices for more information.
 #ifndef MITK_CHILIPLUGINFACTORY_H_INCLUDED
 #define MITK_CHILIPLUGINFACTORY_H_INCLUDED
 
+#include <mitkCommon.h>
 #include <itkObjectFactoryBase.h>
 
 namespace mitk {
 
-  class ChiliPluginFactory : public ObjectFactoryBase {
-    
+  class ChiliPluginFactory : public itk::ObjectFactoryBase {
+
+    public:
     mitkClassMacro(mitk::ChiliPluginFactory,itk::ObjectFactoryBase)
 
       /** Class methods used to interface with the registered factories. */
@@ -37,8 +39,12 @@ namespace mitk {
     /** Register one factory of this type  */
     static void RegisterOneFactory()
     {
-      ChiliPluginFactory::Pointer chiliPluginFactory = ChiliPluginFactory::New();
-      ObjectFactoryBase::RegisterFactory(chiliPluginFactory);
+      static bool IsRegistered = false;
+      if (!IsRegistered) {
+        ChiliPluginFactory::Pointer chiliPluginFactory = ChiliPluginFactory::New();
+        ObjectFactoryBase::RegisterFactory(chiliPluginFactory);
+        IsRegistered = true;
+      }
     }
     protected:
     ChiliPluginFactory();
@@ -48,8 +54,7 @@ namespace mitk {
     ChiliPluginFactory(const Self&); //purposely not implemented
     void operator=(const Self&); //purposely not implemented
 
-
-  }
+  };
 }
 
 #endif

2087f6ffa19320920d4ac728b487283ba3d69187
Matthias Baumhauer <m.baumhauer@dkfz-heidelberg.de>
2006-11-25 17:13:10 +0000

    - initial version: simple class for showing a video in the background of a renderwindow.

---
+++ Qmitk/QmitkVideoBackground.cpp
@@ -0,0 +1,162 @@
+#include "QmitkVideoBackground.h"
+
+#include "mitkVtkLayerController.h"
+#include "mitkVtkRenderWindow.h"
+#include "mitkRenderWindow.h"
+#include "mitkVideoSource.h"
+
+#include "qtimer.h"
+
+#include "vtkSystemIncludes.h"
+#include "vtkRenderer.h"
+#include "vtkMapper.h"
+#include "vtkObjectFactory.h"
+#include "vtkImageActor.h"
+#include "vtkRenderWindow.h"
+#include "vtkImageImport.h"
+#include "vtkCommand.h"
+#include "vtkCamera.h"
+
+
+// VTK CALLBACK for automatic resize of the video
+class vtkVideoSizeCallback : public vtkCommand
+{
+public:
+  static vtkVideoSizeCallback *New(){ return new vtkVideoSizeCallback; }
+   
+  virtual void Execute(vtkObject *caller, unsigned long, void*)
+  {
+    std::cout << "renderwindow modified" << std::endl;
+  }
+};
+
+QmitkVideoBackground::QmitkVideoBackground()
+{ 
+  m_ImageWidth  = 720;
+  m_ImageHeight = 576;
+  m_TimerDelay  = 50;
+  ResetVideoBackground();
+}
+void QmitkVideoBackground::ResetVideoBackground()
+{
+  m_RenderWindow = NULL;
+  m_QTimer = new QTimer(this, "VideoTimer");
+  connect( m_QTimer, SIGNAL(timeout()), SLOT(UpdateVideo()) );
+  
+  m_Actor = vtkImageActor::New();
+  m_VideoRenderer = vtkRenderer::New();
+
+  m_VtkImageImport = vtkImageImport::New();
+  m_VtkImageImport->SetDataScalarTypeToUnsignedChar();
+  m_VtkImageImport->SetNumberOfScalarComponents(3);
+  m_VtkImageImport->SetWholeExtent(0,m_ImageWidth-1,0,m_ImageHeight-1,0,1-1);
+  m_VtkImageImport->SetDataExtentToWholeExtent();  
+}
+
+QmitkVideoBackground::~QmitkVideoBackground()
+{
+  if ( m_RenderWindow != NULL )
+    if ( this->IsEnabled() )
+      this->Disable();
+  if ( m_Actor!=NULL )
+    m_Actor->Delete();
+  if ( m_VideoRenderer != NULL )
+    m_VideoRenderer->Delete();
+  if ( m_VtkImageImport != NULL)
+    m_VtkImageImport->Delete();
+}
+
+
+
+/**
+ * Sets the renderwindow, in which the Video background
+ * will be shown. Make sure, you have called this function
+ * before calling Enable()
+ */
+void QmitkVideoBackground::SetRenderWindow( mitk::RenderWindow* renderWindow )
+{
+  m_RenderWindow = renderWindow;
+
+  vtkVideoSizeCallback * videoCallback = vtkVideoSizeCallback::New();
+  m_RenderWindow->GetVtkRenderWindow()->AddObserver(vtkCommand::ModifiedEvent,videoCallback);
+}
+/**
+ * Enables drawing of the color Video background.
+ * If you want to disable it, call the Disable() function.
+ */
+void QmitkVideoBackground::Enable()
+{
+  m_VideoRenderer = vtkRenderer::New();
+   
+  UpdateVideo();
+  m_Actor->SetInput(m_VtkImageImport->GetOutput());
+ 
+  m_VideoRenderer->AddActor2D(m_Actor);
+  m_VideoRenderer->ResetCamera();
+  m_VideoRenderer->InteractiveOff();
+  m_VideoRenderer->GetActiveCamera()->ParallelProjectionOn();
+  m_VideoRenderer->GetActiveCamera()->SetParallelScale(m_ImageHeight/2);
+ 
+  m_RenderWindow->GetVtkLayerController()->InsertBackgroundRenderer(m_VideoRenderer,true);
+  m_QTimer->start(m_TimerDelay);
+  
+}
+
+/**
+ * Disables drawing of the color Video background.
+ * If you want to enable it, call the Enable() function.
+ */
+void QmitkVideoBackground::Disable()
+{
+  if ( this->IsEnabled() )
+  {
+    m_RenderWindow->GetVtkLayerController()->RemoveRenderer(m_VideoRenderer);
+    m_QTimer->stop();
+  }
+}
+/**
+ * Checks, if the Video background is currently
+ * enabled (visible)
+ */
+bool QmitkVideoBackground::IsEnabled()
+{
+  if ( m_RenderWindow->GetVtkLayerController()->IsRendererInserted(m_VideoRenderer) )
+      return true;
+  else
+      return false;    
+}
+
+void QmitkVideoBackground::UpdateVideo()
+{  
+  unsigned char *src = 0;
+  src = mitk::VideoSource::GetInstance()->GetVideoTexture();
+  if(src)
+  {
+    m_VtkImageImport->SetImportVoidPointer(src);
+    m_VtkImageImport->Modified();
+    m_VtkImageImport->Update();
+    m_RenderWindow->GetVtkRenderWindow()->Render();
+  } 
+}
+
+ // how to superimpose a logo
+ // vtkCamera*          camera2        = vtkCamera::New();
+ // vtkImageMapper   *   mapper2D      = vtkImageMapper::New();
+ // vtkPNGReader      *  pngReader   = vtkPNGReader::New();
+ // vtkActor2D      *    actor2D     = vtkActor2D::New();
+ // pngReader->SetFileName("D:\\mbilogo.png");
+ // pngReader->Update();
+ // mapper2D->SetInput(pngReader->GetOutput());
+ // double range[2];
+ // pngReader->GetOutput()->GetScalarRange(range);
+ // mapper2D->SetColorWindow(range[1] - range[0] );
+ // mapper2D->SetColorLevel((range[0]+range[1])/2);
+ // actor2D->SetPosition(0,0);
+ ///* actor2D->SetWidth(300);
+ // actor2D->SetHeight(200);*/
+ // actor2D->SetMapper(mapper2D);
+ // actor2D->SetLayerNumber(99);
+ // m_RenderWindow->GetVtkLayerController()->GetSceneRenderer()->AddActor2D(actor2D);
+  /*renderer2->SetActiveCamera(camera);
+  renderer2->ResetCamera();
+  m_RenderWindow->GetVtkLayerController()->InsertForegroundRenderer(renderer2,true);*/
---
+++ Qmitk/QmitkVideoBackground.h
@@ -0,0 +1,78 @@
+#ifndef _Qmitk_Video_Background_h_
+#define _Qmitk_Video_Background_h_
+
+// Q-Timer
+#include "qobject.h"
+class QTimer;
+class QTime;
+
+class vtkRenderer;
+class vtkRenderWindow;
+class vtkImageActor;
+class vtkImageImport;
+class vtkActor2D;
+class vtkVideoSizeCallback;
+
+
+namespace mitk {
+class RenderWindow;
+}
+/**
+ * Displays a video in the background
+ * of a vtkRenderWindow.
+ */
+class QmitkVideoBackground : public QObject
+{
+  Q_OBJECT
+
+public:
+   QmitkVideoBackground();
+   ~QmitkVideoBackground();
+
+  mitk::RenderWindow* GetRenderWindow();
+  void SetRenderWindow(mitk::RenderWindow* renderWindow);
+
+  void Enable();
+  void Disable();
+
+  void SetImageDimensions(int x, int y)
+  {
+    m_ImageWidth  = x;
+    m_ImageHeight = y;
+    ResetVideoBackground();
+  }
+
+  void SetTimerDelay(int ms)
+  {
+    m_TimerDelay = ms;
+    ResetVideoBackground();
+  }
+
+  
+  /**
+   * Checks, if the Video background is currently
+   * enabled (visible)
+   */
+   bool IsEnabled();
+  
+  public slots:
+    void UpdateVideo();
+  
+protected:
+  void ResetVideoBackground();
+
+  mitk::RenderWindow*         m_RenderWindow;
+  vtkRenderer*                m_VideoRenderer;
+  vtkImageActor*              m_Actor;
+  vtkVideoSizeCallback*       m_VideoCallback;
+  vtkImageImport*             m_VtkImageImport;
+ 
+  //for framegrabbing
+  QTimer*                     m_QTimer;
+
+  int m_ImageWidth, m_ImageHeight, m_TimerDelay;
+};
+
+
+#endif
+

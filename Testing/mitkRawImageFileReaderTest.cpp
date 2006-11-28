fccaa3dde3243fb98454c3f7ddd82fbda1d55a57
Daniel Maleike <d.maleike@dkfz-heidelberg.de>
2006-11-28 16:56:00 +0000

    Basic testing (just instantiation)

---
+++ Testing/mitkRawImageFileReaderTest.cpp
@@ -0,0 +1,32 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkRawImageFileReader.h"
+
+int mitkRawImageFileReaderTest(int argc, char* argv[])
+{
+  // instantiation
+  mitk::RawImageFileReader::Pointer reader = mitk::RawImageFileReader::New();
+
+  // freeing
+  reader = NULL;
+
+  std::cout<<"[TEST DONE]"<<std::endl;
+  return EXIT_SUCCESS;
+}

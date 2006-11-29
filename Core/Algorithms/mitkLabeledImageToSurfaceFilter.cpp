5c6a3b48b4723b7f8eb741156b4dfa83253b8f97
max <max@nowhere>
2006-11-29 09:14:38 +0000

    ENH: Initial version of surface extraction for labeled images.

---
+++ Core/Algorithms/mitkLabeledImageToSurfaceFilter.cpp
@@ -0,0 +1,348 @@
+#include <mitkLabeledImageToSurfaceFilter.h>
+
+#include <vtkImageChangeInformation.h>
+#include <vtkImageThreshold.h>
+#include <vtkImageGaussianSmooth.h>
+#include <vtkImageMarchingCubes.h>
+#include <vtkPolyData.h>
+#include <vtkSmoothPolyDataFilter.h>
+#include <vtkDecimatePro.h>
+#if (VTK_MAJOR_VERSION < 5)
+#include <vtkDecimate.h>
+#endif
+#include <vtkLinearTransform.h>
+#include <vtkMatrix4x4.h>
+
+
+
+#include <mitkImageAccessByItk.h>
+#include <itkImageRegionIterator.h>
+#include <itkNumericTraits.h>
+
+
+
+mitk::LabeledImageToSurfaceFilter::LabeledImageToSurfaceFilter()
+{
+  m_GaussianStandardDeviation = 1.5;
+  m_GenerateAllLabels = true;
+  m_Label = 1;
+  m_BackgroundLabel = 0;
+}
+
+mitk::LabeledImageToSurfaceFilter::~LabeledImageToSurfaceFilter()
+{
+  
+}
+
+
+
+void mitk::LabeledImageToSurfaceFilter::GenerateData()
+{
+  mitk::Image* image =  ( mitk::Image* )GetInput();
+  mitk::Image::RegionType outputRegion = image->GetRequestedRegion();
+
+  //
+  // check which labels are available in the image
+  //
+  m_AvailableLabels = this->GetAvailableLabels();
+  m_IdxToLabels.clear();
+  
+  //
+  // if we don't want to generate surfaces for all labels
+  // we have to remove all labales except m_Label and m_BackgroundLabel 
+  // from the list of available labels
+  //
+  if ( ! m_GenerateAllLabels )
+  {
+    LabelMapType tmp;
+    LabelMapType::iterator it;
+    it = m_AvailableLabels.find( m_Label );
+    if ( it != m_AvailableLabels.end() )
+      tmp[m_Label] = it->second;
+    else
+      tmp[m_Label] = 0;
+    
+    it = m_AvailableLabels.find( m_BackgroundLabel );
+    if ( it != m_AvailableLabels.end() )
+      tmp[m_BackgroundLabel] = it->second;
+    else
+      tmp[m_BackgroundLabel] = 0;
+    
+    m_AvailableLabels = tmp;
+  }
+  
+  //
+  // check for the number of labels: if the whole image is filled, no
+  // background is available and thus the numberOfOutpus is equal to the 
+  // number of available labels in the image (which is a special case). 
+  // If we have background voxels, the number of ouputs is one less than
+  // then number of available labels.
+  //
+  unsigned int numberOfOutputs = 0;
+  if ( m_AvailableLabels.find( m_BackgroundLabel ) == m_AvailableLabels.end() )
+    numberOfOutputs = m_AvailableLabels.size();
+  else
+    numberOfOutputs = m_AvailableLabels.size() - 1;
+  if ( numberOfOutputs == 0 )
+  {
+    itkWarningMacro("Number of outputs == 0");
+    return;     
+  }
+  
+  //
+  // set the number of outputs to the number of labels used.
+  // initialize the output surfaces accordingly (incl. timesteps)
+  //
+  this->SetNumberOfOutputs( numberOfOutputs );
+  this->SetNumberOfRequiredOutputs( numberOfOutputs );
+  for ( unsigned int i = 0 ; i < numberOfOutputs; ++i )
+  {
+    if ( ! this->GetOutput( i ) )
+    {
+      mitk::Surface::Pointer output = static_cast<mitk::Surface*>( this->MakeOutput(0).GetPointer() );
+      assert ( output.IsNotNull() );
+      output->Initialize( 1 ); //TODO: set number of timesteps
+      this->SetNthOutput( i, output.GetPointer() );
+    }
+  }
+  
+  //
+  // traverse the known labels and create surfaces for them.
+  //
+  unsigned int currentOutputIndex = 0;
+  for ( LabelMapType::iterator it = m_AvailableLabels.begin() ; it != m_AvailableLabels.end() ; ++it )
+  {
+    if ( it->first == m_BackgroundLabel )
+      continue;
+    if ( it->second == 0 )
+      continue;
+    assert ( currentOutputIndex < numberOfOutputs );
+    mitk::Surface::Pointer surface = this->GetOutput( currentOutputIndex );
+    assert( surface.IsNotNull() );
+    int tstart=outputRegion.GetIndex(3);
+    int tmax=tstart+outputRegion.GetSize(3); //GetSize()==1 - will aber 0 haben, wenn nicht zeitaufgeloet
+    int t;
+    for( t=tstart; t < tmax; ++t)
+    {
+      vtkImageData *vtkimagedata =  image->GetVtkImageData( t );
+      CreateSurface( t,vtkimagedata,surface.GetPointer(), it->first );
+    }
+    m_IdxToLabels[ currentOutputIndex ] = it->first;
+    currentOutputIndex++;
+  }
+}
+
+void mitk::LabeledImageToSurfaceFilter::CreateSurface( int time, vtkImageData *vtkimage, mitk::Surface * surface, mitk::LabeledImageToSurfaceFilter::LabelType label )
+{
+  vtkImageChangeInformation *indexCoordinatesImageFilter = vtkImageChangeInformation::New();
+  indexCoordinatesImageFilter->SetInput( vtkimage );
+  indexCoordinatesImageFilter->SetOutputOrigin( 0.0,0.0,0.0 );
+
+  vtkImageThreshold* threshold = vtkImageThreshold::New();
+  threshold->SetInput( indexCoordinatesImageFilter->GetOutput() );
+  indexCoordinatesImageFilter->Delete();
+  threshold->SetInValue( 100 );
+  threshold->SetOutValue( 0 );
+  threshold->ThresholdBetween( label, label ); 
+  threshold->SetOutputScalarTypeToUnsignedChar();
+  threshold->ReleaseDataFlagOn();
+
+  vtkImageGaussianSmooth *gaussian = vtkImageGaussianSmooth::New();
+  gaussian->SetInput( threshold->GetOutput() ); 
+  threshold->Delete();
+  gaussian->SetDimensionality( 3  );
+  gaussian->SetRadiusFactor( 0.49 );
+  gaussian->SetStandardDeviation( 1.5 );
+  gaussian->ReleaseDataFlagOn();
+  gaussian->UpdateInformation();
+  gaussian->Update();
+  
+  //MarchingCube -->create Surface
+  vtkImageMarchingCubes *mc = vtkImageMarchingCubes::New();
+  mc->SetInput( gaussian->GetOutput() );//RC++
+  gaussian->Delete();
+  mc->ReleaseDataFlagOn();
+  mc->SetValue( 0, 50 );
+
+  vtkPolyData *polydata;
+  polydata = mc->GetOutput();
+  polydata->Register( NULL );//RC++
+  mc->Delete();
+
+  if (m_Smooth)
+  {
+    vtkSmoothPolyDataFilter *smoother = vtkSmoothPolyDataFilter::New();
+    //read poly1 (poly1 can be the original polygon, or the decimated polygon)
+    smoother->SetInput(polydata);//RC++
+    smoother->SetNumberOfIterations( m_SmoothIteration );
+    smoother->SetRelaxationFactor( m_SmoothRelaxation );
+    smoother->SetFeatureAngle( 60 );
+    smoother->FeatureEdgeSmoothingOff();
+    smoother->BoundarySmoothingOff();
+    smoother->SetConvergence( 0 );
+    smoother->ReleaseDataFlagOn();
+
+    polydata->Delete();//RC--
+    polydata = smoother->GetOutput();
+    polydata->Register(NULL);//RC++
+    smoother->Delete();
+  }
+
+#if (VTK_MAJOR_VERSION >= 5)
+  if (m_Decimate == Decimate )
+  {
+    std::cerr << "vtkDecimate not available for VTK 5.0 and above.";
+    std::cerr << " Using vtkDecimatePro instead." << std::endl;
+    m_Decimate = DecimatePro;
+  }
+#endif
+
+  //decimate = to reduce number of polygons
+  if(m_Decimate==DecimatePro)
+  {
+    vtkDecimatePro *decimate = vtkDecimatePro::New();
+    decimate->SplittingOff();
+    decimate->SetErrorIsAbsolute(5);
+    decimate->SetFeatureAngle(30);
+    decimate->PreserveTopologyOn();
+    decimate->BoundaryVertexDeletionOff();
+    decimate->SetDegree(10); //std-value is 25!
+
+    decimate->SetInput(polydata);//RC++
+    decimate->SetTargetReduction(m_TargetReduction);
+    decimate->SetMaximumError(0.002);
+    decimate->ReleaseDataFlagOn();
+
+    polydata->Delete();//RC--
+    polydata = decimate->GetOutput();
+    polydata->Register(NULL);//RC++
+    decimate->Delete();
+  }
+#if (VTK_MAJOR_VERSION < 5)
+  else if (m_Decimate==Decimate)
+  {
+    vtkDecimate *decimate = vtkDecimate::New();
+    decimate->SetInput( polydata );
+    decimate->PreserveTopologyOn();
+    decimate->BoundaryVertexDeletionOff();
+    decimate->SetTargetReduction( m_TargetReduction );
+    decimate->ReleaseDataFlagOn();
+    polydata->Delete();//RC--
+    polydata = decimate->GetOutput();
+    polydata->Register(NULL);//RC++
+    decimate->Delete();
+  }
+#endif
+
+  polydata->Update();
+
+  polydata->SetSource(NULL);
+
+  if(polydata->GetNumberOfPoints() > 0)
+  {
+    mitk::Vector3D spacing = GetInput()->GetGeometry(time)->GetSpacing();
+
+    vtkPoints * points = polydata->GetPoints();
+    vtkMatrix4x4 *vtkmatrix = vtkMatrix4x4::New();
+    GetInput()->GetGeometry(time)->GetVtkTransform()->GetMatrix(vtkmatrix);
+    double (*matrix)[4] = vtkmatrix->Element;
+
+    unsigned int i,j;
+    for(i=0;i<3;++i)
+      for(j=0;j<3;++j)
+        matrix[i][j]/=spacing[j];
+
+    unsigned int n = points->GetNumberOfPoints();
+    vtkFloatingPointType point[3];
+
+    for (i = 0; i < n; i++)
+    {
+      points->GetPoint(i, point);
+      mitkVtkLinearTransformPoint(matrix,point,point);
+      points->SetPoint(i, point);
+    }
+    vtkmatrix->Delete();
+  }
+
+  surface->SetVtkPolyData(polydata, time);
+  polydata->UnRegister(NULL);
+}
+
+template < typename TPixel, unsigned int VImageDimension >
+    void GetAvailableLabelsInternal( itk::Image<TPixel, VImageDimension>* image, mitk::LabeledImageToSurfaceFilter::LabelMapType& availableLabels )
+{
+  typedef itk::Image<TPixel, VImageDimension> ImageType;
+  typedef itk::ImageRegionIterator< ImageType > ImageRegionIteratorType;
+  availableLabels.clear();
+  ImageRegionIteratorType it( image, image->GetRequestedRegion() );
+  it.GoToBegin();
+  mitk::LabeledImageToSurfaceFilter::LabelMapType::iterator labelIt;
+  while( ! it.IsAtEnd() )
+  {
+    labelIt = availableLabels.find( ( mitk::LabeledImageToSurfaceFilter::LabelType ) ( it.Get() ) );
+    if ( labelIt == availableLabels.end() )
+      availableLabels[ ( mitk::LabeledImageToSurfaceFilter::LabelType ) ( it.Get() ) ] = 1;
+    else
+      ( labelIt->second )++;
+    ++it;
+  }
+}
+
+InstantiateAccessFunctionForFixedDimension_1(GetAvailableLabelsInternal, 3, mitk::LabeledImageToSurfaceFilter::LabelMapType&);
+
+
+mitk::LabeledImageToSurfaceFilter::LabelMapType mitk::LabeledImageToSurfaceFilter::GetAvailableLabels()
+{
+  mitk::Image::Pointer image =  ( mitk::Image* )GetInput();
+  LabelMapType availableLabels;
+  AccessFixedDimensionByItk_1( image, GetAvailableLabelsInternal, 3, availableLabels );
+  return availableLabels;
+}
+
+
+void mitk::LabeledImageToSurfaceFilter::CreateSurface(int, vtkImageData*, mitk::Surface*, const ScalarType)
+{
+  itkWarningMacro( "This function should never be called!" );
+  assert(false);  
+}
+
+mitk::LabeledImageToSurfaceFilter::LabelType mitk::LabeledImageToSurfaceFilter::GetLabelForNthOutput( const unsigned int& idx )
+{
+  IdxToLabelMapType::iterator it = m_IdxToLabels.find( idx );
+  if ( it != m_IdxToLabels.end() )
+  {
+    return it->second;
+  }
+  else
+  {
+    itkWarningMacro( "Unknown index encountered: " << idx << ". There are " << this->GetNumberOfOutputs() << " outputs available." );
+    return itk::NumericTraits<LabelType>::max();
+  }
+}
+
+mitk::ScalarType mitk::LabeledImageToSurfaceFilter::GetVolumeForNthOutput( const unsigned int& i )
+{
+  return GetVolumeForLabel( GetLabelForNthOutput( i ) );
+}
+
+mitk::ScalarType mitk::LabeledImageToSurfaceFilter::GetVolumeForLabel( const mitk::LabeledImageToSurfaceFilter::LabelType& label )
+{
+  // get the image spacing 
+  mitk::Image* image =  ( mitk::Image* )GetInput();
+  const float* spacing = image->GetSlicedGeometry()->GetFloatSpacing();
+  
+  // get the number of voxels encountered for the given label,
+  // calculate the volume and return it.
+  LabelMapType::iterator it = m_AvailableLabels.find( label );
+  if ( it != m_AvailableLabels.end() )
+  {
+    return it->second * ( spacing[0] * spacing[1] * spacing[2] / 1000.0 );
+  }
+  else
+  {
+    itkWarningMacro( "Unknown label encountered: " << label );
+    return 0.0;
+  }
+}
+
+
---
+++ Testing/mitkLabeledImageToSurfaceFilterTest.cpp
@@ -0,0 +1,328 @@
+#include "mitkLabeledImageToSurfaceFilter.h"
+#include <itksys/SystemTools.hxx>
+#include "mitkDataTreeNodeFactory.h"
+#include <cmath>
+
+bool equals(const mitk::ScalarType& val1, const mitk::ScalarType& val2, mitk::ScalarType epsilon = mitk::eps )
+{
+  return ( std::fabs(val1 - val2) <= epsilon );
+}
+
+int mitkLabeledImageToSurfaceFilterTest(int argc, char** argv)
+{
+  if(argc==0)
+  {
+    std::cout<<"no file specified [FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  std::string path = argv[1];
+  itksys::SystemTools::ConvertToUnixSlashes(path);
+  std::string fileIn = path + "/BallBinary30x30x30.pic.gz";
+
+  
+  fileIn = itksys::SystemTools::ConvertToOutputPath(fileIn.c_str());
+  std::cout<<"Eingabe Datei: "<<fileIn<<std::endl;
+  mitk::Image::Pointer image = NULL;
+  mitk::DataTreeNodeFactory::Pointer factory = mitk::DataTreeNodeFactory::New();
+  try
+  {
+    std::cout << "Loading file: ";
+    factory->SetFileName( fileIn.c_str() );
+    factory->Update();
+
+    if(factory->GetNumberOfOutputs()<1)
+    {
+      std::cout<<"file could not be loaded [FAILED]"<<std::endl;
+      return EXIT_FAILURE;
+    }
+    mitk::DataTreeNode::Pointer node = factory->GetOutput( 0 );
+    image = dynamic_cast<mitk::Image*>(node->GetData());
+    if(image.IsNull())
+    {
+      std::cout<<"file not an image - test will not be applied [PASSED]"<<std::endl;
+      std::cout<<"[TEST DONE]"<<std::endl;
+      return EXIT_SUCCESS;
+    }
+  }
+  catch ( itk::ExceptionObject & ex )
+  {
+    std::cout << "Exception: " << ex << "[FAILED]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+
+  std::cout << "Testing instantiation: " ;
+  mitk::LabeledImageToSurfaceFilter::Pointer filter = mitk::LabeledImageToSurfaceFilter::New();
+  if (filter.IsNull())
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  else {
+  std::cout<<"[PASSED]"<<std::endl;
+  }
+
+  std::cout << "Create surface with default settings: ";
+  filter->SetInput(image);
+  filter->Update();
+
+  if ( filter->GetNumberOfOutputs() != 1 )
+  {
+     std::cout<<"Wrong number of outputs, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput() == NULL )
+  {
+     std::cout<<"Output is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput()->GetVtkPolyData() == NULL )
+  {
+     std::cout<<"PolyData of surface is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  std::cout << "Testing index to label conversion: ";
+  if ( filter->GetLabelForNthOutput( 0 ) != 257 )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }  
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  std::cout << "Testing volume for label calculation: ";
+  if ( ! equals ( filter->GetVolumeForLabel( 257 ) , 14.328 ) )
+  {
+     std::cout<<filter->GetVolumeForLabel( 257 )<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }  
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+    
+  std::cout << "Testing volume for index calculation: ";
+  if ( ! equals ( filter->GetVolumeForNthOutput( 0 ) , 14.328 ) )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }  
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+ 
+  std::cout << "Create surface using optimised settings: ";
+  filter->GenerateAllLabelsOn();
+  filter->SetGaussianStandardDeviation( 1.5 ); 
+  filter->SetSmooth(true); // smooth wireframe
+  filter->SetDecimate( mitk::ImageToSurfaceFilter::DecimatePro );
+  filter->SetTargetReduction( 0.8 );
+  if( filter->GetNumberOfOutputs() != 1 )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  std::cout << "Create surface for label 257: ";
+  filter->GenerateAllLabelsOff();
+  filter->SetLabel(257);
+  filter->Update();
+  if ( filter->GetNumberOfOutputs() != 1 )
+  {
+     std::cout<<"Wrong number of outputs, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput() == NULL )
+  {
+     std::cout<<"Output is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput()->GetVtkPolyData() == NULL )
+  {
+     std::cout<<"PolyData of surface is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  std::cout << "Testing volume for label calculation: ";
+  if ( ! equals ( filter->GetVolumeForLabel( 257 ) , 14.328 ) )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }  
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+    
+  std::cout << "Testing volume for index calculation: ";
+  if ( ! equals ( filter->GetVolumeForNthOutput( 0 ) , 14.328 ) )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }  
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  std::cout << "Create surface for multiple labels: ";
+  filter->GenerateAllLabelsOn();
+  filter->SetBackgroundLabel(32000);
+  filter->Update();
+  if ( filter->GetNumberOfOutputs() != 2 )
+  {
+     std::cout<<"Wrong number of outputs, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput(0) == NULL )
+  {
+     std::cout<<"Output 0 is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput(0)->GetVtkPolyData() == NULL )
+  {
+     std::cout<<"PolyData of output 0 is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput(1) == NULL )
+  {
+     std::cout<<"Output 1 is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput(1)->GetVtkPolyData() == NULL )
+  {
+     std::cout<<"PolyData of output 1 is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  std::cout << "Testing volume for label calculation: ";
+  if ( ! equals ( filter->GetVolumeForLabel( 257 ) , 14.328 ) )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( ! equals ( filter->GetVolumeForLabel( 0 ), 12.672 ) )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+    
+  std::cout << "Testing volume for index calculation: ";
+  if ( ! equals( filter->GetVolumeForNthOutput( 1 ), 14.328 ) )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }  
+  else if ( ! equals ( filter->GetVolumeForNthOutput( 0 ), 12.672 ) )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  std::cout << "Create surface for background (label 0): ";
+  filter->GenerateAllLabelsOff();
+  filter->SetLabel(0);
+  filter->SetBackgroundLabel(257);
+  filter->Update();
+  if ( filter->GetNumberOfOutputs() != 1 )
+  {
+     std::cout<<"Wrong number of outputs, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput() == NULL )
+  {
+     std::cout<<"Output is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else if ( filter->GetOutput()->GetVtkPolyData() == NULL )
+  {
+     std::cout<<"PolyData of surface is NULL, [FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  std::cout << "Testing index to label conversion: ";
+  if ( filter->GetLabelForNthOutput( 0 ) != 0 )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }  
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  std::cout << "Testing volume for label calculation: ";
+  if ( ! equals ( filter->GetVolumeForLabel( filter->GetLabel() ) , 12.672 ) )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }  
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+    
+  std::cout << "Testing volume for index calculation: ";
+  if ( ! equals( filter->GetVolumeForNthOutput( 0 ), 12.672 ) )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }  
+  else 
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  std::cout << "Create surface for invalid label: ";
+  filter->GenerateAllLabelsOff();
+  filter->SetLabel(1);
+  filter->Update();
+  if ( filter->GetNumberOfOutputs() != 0 )
+  {
+     std::cout<<"[FAILED]"<<std::endl;
+     return EXIT_FAILURE;
+  }
+  else
+  {
+     std::cout<<"[PASSED]"<<std::endl;
+  }
+  
+  
+
+  std::cout<<"[TEST DONE]"<<std::endl;
+  return EXIT_SUCCESS;
+  
+}

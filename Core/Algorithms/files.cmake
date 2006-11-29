884b6f58a173a4e0020b5030f9c2ce116dba749d
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2006-11-29 12:29:38 +0000

    *** empty log message ***

--- Core/Algorithms/files.cmake
+++ Core/Algorithms/files.cmake
@@ -1,48 +1,44 @@
-SET(ALGORITHMS_CPP 
-mitkAngleCorrectByPointFilter.cpp
-mitkAutoCropImageFilter.cpp
-mitkBaseDataSource.cpp
-mitkBaseProcess.cpp
-mitkBoundingObjectCutter.cpp
-mitkCallbackFromGUIThread.cpp
-mitkContourSetToPointSetFilter.cpp
-# mitkCoonsPatchFilter.cpp
-mitkCylindricToCartesianFilter.cpp
-mitkDataTreeNodeFactory.cpp
-mitkDataTreeNodeSource.cpp
-# mitkDICOMFileIOFactory.cpp
-# mitkDICOMFileReader.cpp
-mitkDopplerToStrainRateFilter.cpp
-mitkGeometry2DDataToSurfaceFilter.cpp
-mitkGeometryClipImageFilter.cpp
-mitkGeometryDataSource.cpp
-mitkHistogramGenerator.cpp
-mitkImageCastPart1.cpp
-mitkImageCastPart2.cpp
-mitkImageChannelSelector.cpp
-mitkImageSliceSelector.cpp
-mitkImageSource.cpp
-mitkImageTimeSelector.cpp
-mitkImageToImageFilter.cpp
-mitkImageToLookupTableFilter.cpp
-mitkImageToSurfaceFilter.cpp
-mitkInterpolateLinesFilter.cpp
-# mitkItkProcessObjectAdapter.cpp
-mitkLookupTableSource.cpp
-mitkManualSegmentationToSurfaceFilter.cpp
-mitkMaskImageFilter.cpp
-mitkMeshSource.cpp
-mitkPadImageFilter.cpp
-mitkPlaneCutFilter.cpp
-mitkPlaneFit.cpp
-mitkPlanesPerpendicularToLinesFilter.cpp
-mitkPointSetSource.cpp
-mitkPolygonToRingFilter.cpp
-mitkProbeFilter.cpp
-mitkSubImageSelector.cpp
-mitkSurfaceSource.cpp
-mitkSurfaceToImageFilter.cpp
-mitkUIDGenerator.cpp
-mitkVolumeCalculator.cpp
+SET(CPP_FILES
+  mitkAngleCorrectByPointFilter.cpp
+  mitkAutoCropImageFilter.cpp
+  mitkBaseDataSource.cpp
+  mitkBaseProcess.cpp
+  mitkBoundingObjectCutter.cpp
+  mitkCallbackFromGUIThread.cpp
+  mitkContourSetToPointSetFilter.cpp
+  mitkCylindricToCartesianFilter.cpp
+  mitkDataTreeNodeFactory.cpp
+  mitkDataTreeNodeSource.cpp
+  mitkDopplerToStrainRateFilter.cpp
+  mitkGeometry2DDataToSurfaceFilter.cpp
+  mitkGeometryClipImageFilter.cpp
+  mitkGeometryDataSource.cpp
+  mitkHistogramGenerator.cpp
+  mitkImageCastPart1.cpp
+  mitkImageCastPart2.cpp
+  mitkImageChannelSelector.cpp
+  mitkImageSliceSelector.cpp
+  mitkImageSource.cpp
+  mitkImageTimeSelector.cpp
+  mitkImageToImageFilter.cpp
+  mitkImageToLookupTableFilter.cpp
+  mitkImageToSurfaceFilter.cpp
+  mitkInterpolateLinesFilter.cpp
+  #mitkItkProcessObjectAdapter.cpp
+  mitkLookupTableSource.cpp
+  mitkManualSegmentationToSurfaceFilter.cpp
+  mitkMaskImageFilter.cpp
+  mitkMeshSource.cpp
+  mitkPadImageFilter.cpp
+  mitkPlaneCutFilter.cpp
+  mitkPlaneFit.cpp
+  mitkPlanesPerpendicularToLinesFilter.cpp
+  mitkPointSetSource.cpp
+  mitkPolygonToRingFilter.cpp
+  mitkProbeFilter.cpp
+  mitkSubImageSelector.cpp
+  mitkSurfaceSource.cpp
+  mitkSurfaceToImageFilter.cpp
+  mitkUIDGenerator.cpp
+  mitkVolumeCalculator.cpp
 )
-

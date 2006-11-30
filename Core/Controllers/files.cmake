ecb90d2d5d9db873b0ce3d5e5b8d060104476bf3
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2006-11-30 15:26:39 +0000

    *** empty log message ***

--- Core/Controllers/files.cmake
+++ Core/Controllers/files.cmake
@@ -1,26 +1,26 @@
-SET(CONTROLLERS_CPP
-mitkBaseController.cpp
-mitkCameraController.cpp
-mitkCameraRotationController.cpp
-mitkFocusManager.cpp
-mitkLimitedLinearUndo.cpp
-mitkMovieGenerator.cpp
-mitkMultiStepper.cpp
-mitkOperationEvent.cpp
-mitkRenderingManager.cpp
-mitkReportGenerator.cpp
-mitkSliceNavigationController.cpp
-mitkSlicesCoordinator.cpp
-mitkSlicesRotator.cpp
-mitkStatusBar.cpp
-mitkStepper.cpp
-mitkUndoController.cpp
-mitkUndoModel.cpp
-mitkVerboseLimitedLinearUndo.cpp
-mitkVtkInteractorCameraController.cpp
-mitkVtkLayerController.cpp
-# VtkQRenderWindowInteractor.cpp
+SET(CPP_FILES
+  mitkBaseController.cpp
+  mitkCameraController.cpp
+  mitkCameraRotationController.cpp
+  mitkFocusManager.cpp
+  mitkLimitedLinearUndo.cpp
+  mitkMovieGenerator.cpp
+  mitkMultiStepper.cpp
+  mitkOperationEvent.cpp
+  mitkRenderingManager.cpp
+  mitkReportGenerator.cpp
+  mitkSliceNavigationController.cpp
+  mitkSlicesCoordinator.cpp
+  mitkSlicesRotator.cpp
+  mitkStatusBar.cpp
+  mitkStepper.cpp
+  mitkUndoController.cpp
+  mitkUndoModel.cpp
+  mitkVerboseLimitedLinearUndo.cpp
+  mitkVtkInteractorCameraController.cpp
+  mitkVtkLayerController.cpp
+  # VtkQRenderWindowInteractor.cpp
 )
 IF(WIN32)
-SET(CONTROLLERS_CPP ${CONTROLLERS_CPP} mitkMovieGeneratorWin32.cpp)
+  SET(CONTROLLERS_CPP ${CONTROLLERS_CPP} mitkMovieGeneratorWin32.cpp)
 ENDIF(WIN32)
--- Core/Rendering/files.cmake
+++ Core/Rendering/files.cmake
@@ -1,46 +1,46 @@
-SET(RENDERING_CPP
-mitkARMapper2D.cpp
-mitkBaseRenderer.cpp
-mitkBaseVtkMapper2D.cpp
-mitkBaseVtkMapper3D.cpp
-mitkContourMapper2D.cpp
-mitkContourSetMapper2D.cpp
-mitkContourSetVtkMapper3D.cpp
-mitkContourVtkMapper3D.cpp
-mitkGeometry2DDataMapper2D.cpp
-mitkGeometry2DDataVtkMapper3D.cpp
-mitkGLMapper2D.cpp
-mitkGradientBackground.cpp
-mitkImageMapper2D.cpp
-mitkLineMapper2D.cpp
-# mitkLineVtkMapper3D.cpp
-mitkMapper2D.cpp
-mitkMapper3D.cpp
-mitkMapper.cpp
-mitkMapperFactory.cpp
-mitkMeshMapper2D.cpp
-mitkMeshVtkMapper3D.cpp
-mitkNativeRenderWindowInteractor.cpp
-mitkOpenGLRenderer.cpp
-mitkPointDataVtkMapper3D.cpp
-mitkPointSetMapper2D.cpp
-mitkPointSetVtkMapper3D.cpp
-mitkPolyDataGLMapper2D.cpp
-mitkRenderWindow.cpp
-mitkSplineVtkMapper3D.cpp
-mitkSurfaceMapper2D.cpp
-mitkSurfaceStencilVtkMapper3D.cpp
-mitkSurfaceVtkMapper3D.cpp
-# mitkVectorImageMapper2D.cpp
-# mitkVectorImageVtkGlyphMapper3D.cpp
-mitkVolumeDataVtkMapper3D.cpp
-mitkVtkARRenderWindow.cpp
-mitkVtkRenderWindow.cpp
-mitkVtkStencilOpenGLActor.cpp
-mitkVtkStencilRenderWindow.cpp
+SET(CPP_FILES
+  mitkARMapper2D.cpp
+  mitkBaseRenderer.cpp
+  mitkBaseVtkMapper2D.cpp
+  mitkBaseVtkMapper3D.cpp
+  mitkContourMapper2D.cpp
+  mitkContourSetMapper2D.cpp
+  mitkContourSetVtkMapper3D.cpp
+  mitkContourVtkMapper3D.cpp
+  mitkGeometry2DDataMapper2D.cpp
+  mitkGeometry2DDataVtkMapper3D.cpp
+  mitkGLMapper2D.cpp
+  mitkGradientBackground.cpp
+  mitkImageMapper2D.cpp
+  mitkLineMapper2D.cpp
+  # mitkLineVtkMapper3D.cpp
+  mitkMapper2D.cpp
+  mitkMapper3D.cpp
+  mitkMapper.cpp
+  mitkMapperFactory.cpp
+  mitkMeshMapper2D.cpp
+  mitkMeshVtkMapper3D.cpp
+  mitkNativeRenderWindowInteractor.cpp
+  mitkOpenGLRenderer.cpp
+  mitkPointDataVtkMapper3D.cpp
+  mitkPointSetMapper2D.cpp
+  mitkPointSetVtkMapper3D.cpp
+  mitkPolyDataGLMapper2D.cpp
+  mitkRenderWindow.cpp
+  mitkSplineVtkMapper3D.cpp
+  mitkSurfaceMapper2D.cpp
+  mitkSurfaceStencilVtkMapper3D.cpp
+  mitkSurfaceVtkMapper3D.cpp
+  # mitkVectorImageMapper2D.cpp
+  # mitkVectorImageVtkGlyphMapper3D.cpp
+  mitkVolumeDataVtkMapper3D.cpp
+  mitkVtkARRenderWindow.cpp
+  mitkVtkRenderWindow.cpp
+  mitkVtkStencilOpenGLActor.cpp
+  mitkVtkStencilRenderWindow.cpp
 )
 IF(WIN32)
-  SET(RENDERING_CPP ${RENDERING_CPP}
+  SET(CPP_FILES ${CPP_FILES}
     mitkVtkWin32ARRenderWindow.cpp
     mitkVtkWin32StencilRenderWindow.cpp
     mitkWin32ARMapper2D.cpp

f45adbf0a4e1502707380bbc424e4268d359d516
Matthias Baumhauer <m.baumhauer@dkfz-heidelberg.de>
2006-12-01 10:22:58 +0000

    initial version, class for simple displaying any 2D image as a backround image of any widget

---
+++ Core/Rendering/mitkImageBackground2D.cpp
@@ -0,0 +1,248 @@
+#include "mitkImageBackground2D.h"
+// MITK includes
+#include "mitkVtkLayerController.h"
+#include "mitkVtkRenderWindow.h"
+#include "mitkRenderWindow.h"
+
+// VTK includes
+#include "vtkSystemIncludes.h"
+#include "vtkRenderer.h"
+#include "vtkMapper.h"
+#include "vtkObjectFactory.h"
+#include "vtkImageActor.h"
+#include "vtkRenderWindow.h"
+#include "vtkImageImport.h"
+#include "vtkCommand.h"
+#include "vtkCamera.h"
+
+#include <iostream>
+
+// VTK CALLBACK for automatic resize of the video
+class vtkVideoSizeCallback : public vtkCommand
+{
+public:
+  static vtkVideoSizeCallback *New(){ return new vtkVideoSizeCallback; }
+  
+  vtkRenderer * m_ImageRenderer;
+  int m_ImageWidth, m_ImageHeight;
+
+  void SetVtkVideoRenderer(vtkRenderer* r)
+  {
+    m_ImageRenderer = r;
+  }
+  void SetVideoDimensions(int x, int y)
+  {
+    m_ImageWidth = x; m_ImageHeight = y;
+  }
+
+  virtual void Execute(vtkObject *caller, unsigned long, void*)
+  {
+    vtkRenderWindow * RenderWindow = reinterpret_cast<vtkRenderWindow*>(caller);
+    //m_ImageRenderer->ResetCameraClippingRange();
+  }
+};
+
+mitk::ImageBackground2D::ImageBackground2D()
+{
+  m_ImageWidth              = 720;
+  m_ImageHeight             = 576;
+  m_ImageScalarComponents   = 3;
+  m_ParallelScale           = 0;
+  m_RenderWindow = NULL;
+  
+  m_Actor             = vtkImageActor::New();
+  m_ImageRenderer     = vtkRenderer::New();
+  m_VtkImageImport    = vtkImageImport::New();
+  
+  m_ImageData         = 0;
+
+  InitVtkImageImport();
+}
+
+void mitk::ImageBackground2D::InitVtkImageImport()
+{ 
+  m_VtkImageImport->SetDataScalarTypeToUnsignedChar();
+  m_VtkImageImport->SetNumberOfScalarComponents(m_ImageScalarComponents);
+  m_VtkImageImport->SetWholeExtent(0,m_ImageWidth-1,0,m_ImageHeight-1,0,1-1);
+  m_VtkImageImport->SetDataExtentToWholeExtent();  
+}
+
+mitk::ImageBackground2D::~ImageBackground2D()
+{
+  if ( m_RenderWindow != NULL )
+    if ( this->IsEnabled() )
+      this->Disable();
+  if ( m_Actor!=NULL )
+    m_Actor->Delete();
+  if ( m_ImageRenderer != NULL )
+    m_ImageRenderer->Delete();
+  if ( m_VtkImageImport != NULL)
+    m_VtkImageImport->Delete();
+}
+/**
+ * Sets the renderwindow, in which the Video background
+ * will be shown. Make sure, you have called this function
+ * before calling Enable()
+ */
+void mitk::ImageBackground2D::SetRenderWindow( const mitk::RenderWindow* renderWindow )
+{
+  m_RenderWindow = renderWindow;
+
+  /*m_SizeCallback = vtkVideoSizeCallback::New();
+  m_RenderWindow->GetVtkRenderWindow()->AddObserver(vtkCommand::ModifiedEvent,m_SizeCallback);*/
+  
+}
+
+void mitk::ImageBackground2D::SetParallelScale(int scale)
+{
+  m_ParallelScale = scale;
+  m_ImageRenderer->GetActiveCamera()->SetParallelScale(m_ParallelScale);
+}
+int mitk::ImageBackground2D::GetParallelScale()
+{
+  return m_ImageRenderer->GetActiveCamera()->GetParallelScale();
+}
+/**
+ * Enables drawing of the color Video background.
+ * If you want to disable it, call the Disable() function.
+ */
+void mitk::ImageBackground2D::Enable()
+{
+  m_ImageRenderer = vtkRenderer::New();
+  
+  unsigned char * c = 0;
+  Update(c);
+  m_Actor->SetInput(m_VtkImageImport->GetOutput());
+ 
+  m_ImageRenderer->AddActor2D(m_Actor);
+  m_ImageRenderer->ResetCamera();
+  m_ImageRenderer->InteractiveOff();
+  m_ImageRenderer->GetActiveCamera()->ParallelProjectionOn();
+  if(m_ParallelScale == 0)
+    m_ImageRenderer->GetActiveCamera()->SetParallelScale(m_ImageHeight/2);
+ 
+  //m_SizeCallback->SetVtkVideoRenderer(m_ImageRenderer);
+  //m_SizeCallback->SetVideoDimensions(m_ImageWidth, m_ImageHeight);
+  m_RenderWindow->GetVtkLayerController()->InsertBackgroundRenderer(m_ImageRenderer,true); 
+}
+
+/**
+ * Disables drawing of the color Video background.
+ * If you want to enable it, call the Enable() function.
+ */
+void mitk::ImageBackground2D::Disable()
+{
+  if ( this->IsEnabled() )
+    m_RenderWindow->GetVtkLayerController()->RemoveRenderer(m_ImageRenderer);    
+}
+/**
+ * Checks, if the Video background is currently
+ * enabled (visible)
+ */
+bool mitk::ImageBackground2D::IsEnabled()
+{
+  if ( m_RenderWindow->GetVtkLayerController()->IsRendererInserted(m_ImageRenderer) )
+      return true;
+  else
+      return false;    
+}
+
+void mitk::ImageBackground2D::Update(unsigned char * datapointer)
+{
+  if(!IsEnabled())
+    return;
+
+  if(!datapointer)
+  {// no valid image pointer provided -> initialize black image
+    m_ImageData = new unsigned char[m_ImageHeight*m_ImageWidth*m_ImageScalarComponents];
+    unsigned char* b = m_ImageData;
+    for ( int textCounter = 0; textCounter < (m_ImageHeight*m_ImageWidth*m_ImageScalarComponents); textCounter++ )
+      *b++ = 0;
+  }
+  
+  m_VtkImageImport->SetImportVoidPointer(m_ImageData);
+  m_VtkImageImport->Modified();
+  m_VtkImageImport->Update();
+  m_RenderWindow->GetVtkRenderWindow()->Render();
+   
+}
+
+#ifdef OPEN_CV
+
+void mitk::ImageBackground2D::Update(IplImage * image)
+{
+  if(!image || !IsEnabled())
+    return;
+  
+  
+  if(image->roi == NULL)
+  {
+    if(image->height != m_ImageHeight || image->width != m_ImageWidth || image->nChannels != m_ImageScalarComponents)
+    { // image parameters changed, update data array 
+      m_ImageHeight           = image->height;
+      m_ImageWidth            = image->width;
+      m_ImageScalarComponents = image->nChannels;
+      
+      if(m_ImageData != 0)
+      {  
+        delete m_ImageData;
+        m_ImageData = 0;
+      }
+    }
+    if(m_ImageData == 0)
+      m_ImageData = new unsigned char[m_ImageWidth*m_ImageHeight*m_ImageScalarComponents];
+
+  }
+  else
+  {
+    if(image->roi->height != m_ImageHeight|| image->roi->width != m_ImageWidth || image->nChannels != m_ImageScalarComponents)
+    {
+
+    }
+  }
+
+  InitVtkImageImport();
+
+  int column, row;
+  unsigned char* tex = m_ImageData;
+  char* data = image->imageData;
+  
+  if(image->nChannels == 1 && image->origin == 1)
+  { // flip image
+    unsigned char g;
+    for (column = 0; column < m_ImageHeight; column++)
+    {
+      for (row = 0; row < m_ImageWidth; row++)
+      {
+            g   = *data++;
+        *tex++  = g;
+      }
+    }
+  }
+  else if(image->nChannels == 3 && image->origin == 1)
+  { // flip image, change color channels
+    unsigned char r, g, b;
+    for (column = 0; column < m_ImageHeight; column++)
+    {
+      for (row = 0; row < m_ImageWidth; row++)
+      {   //change r with b
+          b = *data++;
+          g = *data++;
+          r = *data++;
+          *tex++ = r;
+          *tex++ = g;
+          *tex++ = b;
+      }
+    }
+  }
+  else
+    std::cout << "opencv image not supported in mitkimagebackround2d.cpp" << std::endl;
+
+  m_VtkImageImport->SetImportVoidPointer(m_ImageData);
+  m_VtkImageImport->Modified();
+  m_VtkImageImport->Update();
+  m_RenderWindow->GetVtkRenderWindow()->Render();
+}
+#endif
+
+ 
---
+++ Core/Rendering/mitkImageBackground2D.h
@@ -0,0 +1,94 @@
+#ifndef _mitk_Image_Background2D_h_
+#define _mitk_Image_Background2D_h_
+
+#include "mitkVideoSource.h"
+
+#include "mitkConfig.h"
+
+#ifdef OPEN_CV
+  #include "cv.h"
+#endif
+
+class vtkRenderer;
+class vtkRenderWindow;
+class vtkImageActor;
+class vtkImageImport;
+class vtkVideoSizeCallback;
+
+namespace mitk 
+{
+
+  class RenderWindow;
+
+  /**
+  * Displays a image (currently unsigned char) in the background
+  * of a vtkRenderWindow.
+  * Caution: As the data is not being copied, a user is responsible for a valid 
+  * pointer to the image. Also the image dimensions needs to be set correctly before enabling the 
+  * background.
+  */
+  class ImageBackground2D : public itk::Object
+  {
+    public:
+     mitkClassMacro( ImageBackground2D, itk::Object );
+     itkNewMacro( Self );
+
+    ////##Documentation
+    ////## @brief returns the mitkRenderWindow in which the video is displayed.
+    mitk::RenderWindow* GetRenderWindow();
+    ////##Documentation
+    ////## @brief sets the mitkRenderWindow in which the video is displayed.
+    ////## must be initialized before enabling the background.
+    void SetRenderWindow(const mitk::RenderWindow* renderWindow);
+    ////##Documentation
+    ////## @brief sets the width,height and number of scalar components of the image.
+    ////## must be correctly initialized before enabling the background.
+    void SetImageDimensions(int x, int y, int nrOfScalarComponents)
+    {
+      m_ImageWidth  = x;
+      m_ImageHeight = y;
+      m_ImageScalarComponents = nrOfScalarComponents;
+      InitVtkImageImport();
+    }
+
+    void SetParallelScale(int scale);
+    int  GetParallelScale();
+    
+    void Update(unsigned char * dataPointer);
+    
+    #ifdef OPEN_CV
+      void Update(IplImage* image);
+    #endif
+
+    ////##Documentation
+    ////## @brief visualizes the video. Requires image dimensions and an active mitkvideosource to be set.
+    void Enable();
+    ////##Documentation
+    ////## @brief disables visualization of the video.
+    void Disable();
+    ////##Documentation
+    ////## @brief Checks, if the Video background is currently enabled (visible).
+    bool IsEnabled();
+     
+  protected:
+    ImageBackground2D();
+    ~ImageBackground2D();
+
+    void InitVtkImageImport();
+
+    const mitk::RenderWindow*   m_RenderWindow;
+    vtkRenderer*                m_ImageRenderer;
+    vtkImageActor*              m_Actor;
+    vtkImageImport*             m_VtkImageImport;
+
+     // Adjust Image Size
+     vtkVideoSizeCallback *     m_SizeCallback;
+
+    unsigned char *             m_ImageData;
+     
+    int m_ImageWidth, m_ImageHeight, m_ImageScalarComponents, m_ParallelScale;
+  };
+} // namespace mitk
+
+#endif
+

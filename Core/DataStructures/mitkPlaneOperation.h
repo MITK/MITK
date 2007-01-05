404aeb37efdc6bc56567c467fad41c2f33a1cd42
Mathias Seitel <m.seitel@dkfz-heidelberg.de>
2007-01-05 15:47:32 +0000

    ADD: Operation to define a plane (by origin, normal), e.g. for slice re-orientation

---
+++ Core/DataStructures/mitkPlaneOperation.h
@@ -0,0 +1,54 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef MITKPlaneOperation_H
+#define MITKPlaneOperation_H
+
+#include "mitkCommon.h"
+#include "mitkPointOperation.h"
+#include "mitkVector.h"
+
+
+
+namespace mitk {
+
+/**
+ * @brief Operation for setting a plane (defined by its origin and normal)
+ *
+ * @ingroup Undo
+ */
+class PlaneOperation : public PointOperation
+{
+public:
+  PlaneOperation( OperationType operationType, Point3D point, Vector3D normal );
+
+  virtual ~PlaneOperation();
+
+  Vector3D GetNormal();
+
+private:
+  Vector3D m_Normal;
+
+};
+
+} //namespace mitk
+
+
+#endif /* MITKPlaneOperation_H */
+

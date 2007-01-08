605f5c2954fe10b1ce302d60b0dd05436b9b5a02
Daniel Maleike <d.maleike@dkfz-heidelberg.de>
2007-01-08 16:33:56 +0000

    ADD for creation of PointSets by the DataTreeNodeFactory

---
+++ Core/IO/mitkPointSetIOFactory.cpp
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "mitkPointSetIOFactory.h"
+#include "mitkIOAdapter.h"
+#include "mitkPointSetReader.h"
+
+#include "itkVersion.h"
+
+  
+namespace mitk
+{
+PointSetIOFactory::PointSetIOFactory()
+{
+  this->RegisterOverride("mitkIOAdapter",
+                         "mitkPointSetReader",
+                         "mitk PointSet IO",
+                         1,
+                         itk::CreateObjectFunction<IOAdapter<PointSetReader> >::New());
+}
+  
+PointSetIOFactory::~PointSetIOFactory()
+{
+}
+
+const char* PointSetIOFactory::GetITKSourceVersion() const
+{
+  return ITK_SOURCE_VERSION;
+}
+
+const char* PointSetIOFactory::GetDescription() const
+{
+  return "PointSet IO Factory, allows the loading of MITK pointsets";
+}
+
+} // end namespace mitk
---
+++ Core/IO/mitkPointSetIOFactory.h
@@ -0,0 +1,73 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __mitkPointSetIOFactory_h
+#define __mitkPointSetIOFactory_h
+
+#ifdef _MSC_VER
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkObjectFactoryBase.h"
+#include "mitkBaseData.h"
+
+namespace mitk
+{
+//##Documentation
+//## @brief Create instances of PointSetReader objects using an object factory.
+//##
+//## @ingroup IO
+class PointSetIOFactory : public itk::ObjectFactoryBase
+{
+public:  
+  /** Standard class typedefs. */
+  typedef PointSetIOFactory   Self;
+  typedef itk::ObjectFactoryBase  Superclass;
+  typedef itk::SmartPointer<Self>  Pointer;
+  typedef itk::SmartPointer<const Self>  ConstPointer;
+  
+  /** Class methods used to interface with the registered factories. */
+  virtual const char* GetITKSourceVersion(void) const;
+  virtual const char* GetDescription(void) const;
+    
+  /** Method for class instantiation. */
+  itkFactorylessNewMacro(Self);
+  static PointSetIOFactory* FactoryNew() { return new PointSetIOFactory;}
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(PointSetIOFactory, ObjectFactoryBase);
+
+  /** Register one factory of this type  */
+  static void RegisterOneFactory(void)
+  {
+    PointSetIOFactory::Pointer PointSetIOFactory = PointSetIOFactory::New();
+    ObjectFactoryBase::RegisterFactory(PointSetIOFactory);
+  }
+  
+protected:
+  PointSetIOFactory();
+  ~PointSetIOFactory();
+
+private:
+  PointSetIOFactory(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+};
+  
+  
+} // end namespace mitk
+
+#endif

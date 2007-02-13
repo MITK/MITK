2632653fdba8c4cb428363e9e92246ea0b0aebb2
Jochen Neuhaus <j.neuhaus@dkfz-heidelberg.de>
2007-02-13 12:58:42 +0000

    COMP Initial version: Property that is just a tag to combine several datatree nodes to a common group (needed for QmitkReportGenerator)

---
+++ Core/DataStructures/mitkGroupTagProperty.cpp
@@ -0,0 +1,56 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "mitkGroupTagProperty.h"
+
+
+mitk::GroupTagProperty::GroupTagProperty()
+ : mitk::BaseProperty()
+{
+}
+
+mitk::GroupTagProperty::~GroupTagProperty()
+{
+}
+
+
+/*!
+  Should be implemented by subclasses to indicate whether they can accept the parameter 
+  as the right-hand-side argument of an assignment. This test will most probably include
+  some dynamic_cast.
+ */
+bool mitk::GroupTagProperty::Assignable(const BaseProperty& other) const
+{
+  try
+  {
+    dynamic_cast<const Self&>(other); // dear compiler, please don't optimize this away!
+    return true; 
+  }
+  catch (std::bad_cast)
+  {
+  }
+  return false;
+}
+
+bool mitk::GroupTagProperty::operator==(const BaseProperty& property) const
+{
+    const Self *other = dynamic_cast<const Self*>(&property);
+
+    return (other!=NULL); // if other property is also a GroupTagProperty, then it is equal to us, because tags have no value themselves
+}
---
+++ Core/DataStructures/mitkGroupTagProperty.h
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef GROUPTAGPROPERTY_H_HEADER_INCLUDED_C1F4DF54
+#define GROUPTAGPROPERTY_H_HEADER_INCLUDED_C1F4DF54
+
+#include <mitkBaseProperty.h>
+
+namespace mitk {
+
+/*! @brief Property class that has no value. 
+    
+  @ingroup DataTree
+
+    The GroupTag property is used to tag a datatree node to show, that it is member of a 
+    group of datatree nodes. This can be used to build groups of datatreenodes without the
+    need to contain them in a specific hiearchic order in the datatree
+*/
+class GroupTagProperty : public BaseProperty
+{
+  public:
+    GroupTagProperty();
+    virtual ~GroupTagProperty();
+
+    mitkClassMacro(GroupTagProperty, BaseProperty);
+
+    virtual bool operator==(const BaseProperty& property) const;
+    virtual bool Assignable(const BaseProperty& other) const;
+};
+
+} // namespace mitk
+
+
+
+#endif /* GROUPTAGPROPERTY_H_HEADER_INCLUDED_C1F4DF54 */

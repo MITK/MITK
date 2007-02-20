b5b3021247c6029a375319b9cadb4905ab605dd7
hasselberg <hasselberg@nowhere>
2007-02-20 14:10:56 +0000

    COMP: undo fix

--- Core/Interactions/mitkPointSetInteractor.cpp
+++ Core/Interactions/mitkPointSetInteractor.cpp
@@ -128,7 +128,7 @@ void mitk::PointSetInteractor::UnselectAll()
     int position = it->Index();
     PointSet::PointDataType pointData;
     itkPointSet->GetPointData(position, &pointData);
-    if ( pointData && pointData.selected )//then declare an operation which unselects this point; UndoOperation as well!
+    if ( pointData.selected )//then declare an operation which unselects this point; UndoOperation as well!
     {
       mitk::Point3D noPoint;
       noPoint.Fill(0);

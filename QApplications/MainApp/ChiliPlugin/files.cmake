441bb7614891f7014ce41e53679f6ca208b278ec
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2007-03-22 13:42:19 +0000

    CHG: naming scheme, better ipPic and ipFunc handling for chili plugin builds

---
+++ QApplications/MainApp/ChiliPlugin/files.cmake
@@ -0,0 +1,24 @@
+SET(CPP_FILES
+  mitkChiliPluginFactory.cpp
+  mitkChiliPluginImpl.cpp
+  # mitkConference/mitkChiliConferenceKitFactory.cpp
+  mitkConference/mitkConferenceKit.cpp
+  mitkConference/mitkConferenceKitFactory.cpp
+  mitkConference/mitkConferenceToken.cpp
+  mitkConference/QmitkChili3ConferenceKit.cpp
+  mitkConference/QmitkChili3ConferenceKitFactory.cpp
+  # mitkConference/QmitkQChili3Conference.cpp
+  mitkConference/TextLoggerConferenceKit.cpp
+  mitkConference/TextLoggerConferenceKitFactory.cpp
+  mitkLightBoxImageReaderImpl.cpp
+  mitkLightBoxResultImageWriterImpl.cpp
+  QcMITKSamplePlugin.cpp
+  QcMITKTask.cpp
+  QmitkEventCollector.cpp
+  ToolBar.cpp
+)
+SET(MOC_H_FILES
+  QcMITKSamplePlugin.h
+  QmitkEventCollector.h
+  ToolBar.h
+)

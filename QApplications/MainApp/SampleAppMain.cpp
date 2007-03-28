7110a309b4e3e27065cb3af9aedecadf9d7b2587
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2007-03-28 14:26:28 +0000

    MERGE: last changes in CVS

--- QApplications/MainApp/SampleAppMain.cpp
+++ QApplications/MainApp/SampleAppMain.cpp
@@ -1,7 +1,7 @@
 /*=========================================================================
 
 Program:   Medical Imaging & Interaction Toolkit
-Module:    $RCSfile$
+Module:    $RCSfile: SampleAppMain.cpp,v $
 Language:  C++
 Date:      $Date$
 Version:   $Revision$
@@ -19,7 +19,9 @@ PURPOSE.  See the above copyright notices for more information.
 #include "SampleApp.h"
 #include <qapplication.h>
 #include "itkTextOutput.h"
+#include <itksys/SystemTools.hxx>
 #include <stdexcept>
+
 /*
 * BUG: ATI-grafics-cards are slow in building up display lists in case of displaying transparent surfaces.
 * Activating GlobalImmediateModeRendering in vtk solves this problem.
@@ -27,16 +29,22 @@ PURPOSE.  See the above copyright notices for more information.
 //#include <vtkMapper.h>
 
 #include <QmitkFunctionalityTesting.h>
-
-
+#include <sstream>
 
 int main(int argc, char* argv[])
 {
   try
   {
+    //
+    // determine application name without path and extension
+    //
+    std::string executableName = itksys::SystemTools::GetFilenameWithoutExtension( itksys::SystemTools::GetFilenameName(argv[0] ) );
+    std::stringstream caption;
+    caption << executableName << ", Builddate " << __DATE__ << ", " << __TIME__ << ".";
     itk::OutputWindow::SetInstance(itk::TextOutput::New().GetPointer());
     QApplication a( argc, argv );
     SampleApp mainWindow(NULL, "mainwindow");
+    mainWindow.setCaption( caption.str().c_str() );
     a.setMainWidget(&mainWindow);
 #ifdef USEDARKPALETTE
     std::cout << "changing palette ..." << std::endl;
@@ -49,10 +57,11 @@ int main(int argc, char* argv[])
     * Activating GlobalImmediateModeRendering in vtk solves this problem. Also add #include vtkMapper above
     */
     //vtkMapper::GlobalImmediateModeRenderingOn();
-
     mainWindow.showMaximized();
-    if(strcmp(argv[argc-1], "-testing")==0)
+    if(strcmp(argv[argc-1], "-testing")==0) {
+      std::cout.setf(std::ios_base::unitbuf);
       return StartQmitkFunctionalityTesting(mainWindow.GetFctMediator());
+    }
     else
     {
       return a.exec();

06f3cbe4478a47b732df64e364350212b226dd57
Daniel Maleike <d.maleike@dkfz-heidelberg.de>
2007-05-25 09:28:25 +0000

    DOC add a comment about CastToItkImage

--- Core/Algorithms/mitkImageAccessByItk.h
+++ Core/Algorithms/mitkImageAccessByItk.h
@@ -602,7 +602,7 @@ namespace mitk
   void _CastToItkImage2Access( itk::Image<TPixel, VImageDimension>* itkInputImage, itk::SmartPointer<ItkOutputImageType>& itkOutputImage);
 #endif //DOXYGEN_SKIP
   //##Documentation
-  //## @brief Cast an mitk::Image to an itk::Image with a specific type.
+  //## @brief Cast an mitk::Image to an itk::Image with a specific type. You don't have to initialize the itk::Image<..>::Pointer.
   //## @ingroup Adaptor
   template <typename ItkOutputImageType> void CastToItkImage(const mitk::Image * mitkImage, itk::SmartPointer<ItkOutputImageType>& itkOutputImage);
 }

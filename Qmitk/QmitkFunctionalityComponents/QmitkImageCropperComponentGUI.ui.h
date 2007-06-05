7364225bb306868d831bad4fac4a68aaada16a6d
Urte Rietdorf <u.rietdorf@dkfz-heidelberg.de>
2007-06-05 09:04:22 +0000

    NEW: The ImageCropper as component

---
+++ Qmitk/QmitkFunctionalityComponents/QmitkImageCropperComponentGUI.ui.h
@@ -0,0 +1,69 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you want to add, delete, or rename functions or slots, use
+** Qt Designer to update this file, preserving your code.
+**
+** You should not define a constructor or destructor in this file.
+** Instead, write your code in functions called init() and destroy().
+** These will automatically be called by the form's constructor and
+** destructor.
+*****************************************************************************/
+
+#include "QmitkTreeNodeSelector.h"
+
+
+
+void QmitkImageCropperComponentGUI::SetDataTreeIterator( mitk::DataTreeIteratorBase * it )
+{
+   m_DataTreeIterator = it;
+//TODO   m_TreeNodeSelector->SetDataTreeNodeIterator(it);
+   //m_VolumetryWidget->SetDataTreeNodeIterator(it);
+}
+
+
+
+
+QmitkDataTreeComboBox * QmitkImageCropperComponentGUI::GetTreeNodeSelector()
+{
+ return m_TreeNodeSelector;
+}
+
+
+QGroupBox* QmitkImageCropperComponentGUI::GetShowImageCropperGroupBox()
+{
+  return m_ShowThresholdGroupBox;
+}
+
+QGroupBox* QmitkImageCropperComponentGUI::GetImageCropperFinderGroupBox()
+{
+  return m_ThresholdFinder;
+}
+
+QGroupBox* QmitkImageCropperComponentGUI::GetSelectDataGroupBox()
+{
+  return m_ThresholdSelectDataGroupBox;
+}
+
+QGroupBox* QmitkImageCropperComponentGUI::GetImageCropperValueContent()
+{
+  return m_ThresholdValueContent;
+}
+
+QGroupBox* QmitkImageCropperComponentGUI::GetImageContent()
+{
+  return m_ImageContent;
+}
+
+
+
+QGroupBox* QmitkImageCropperComponentGUI::GetContainerContent()
+{
+  return m_ContainerContent;
+}
+
+
+QPushButton * QmitkImageCropperComponentGUI::GetCropImageButton()
+{
+  return m_CropImageButton;
+}

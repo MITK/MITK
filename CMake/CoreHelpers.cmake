945dff469c02102a67c97cc5d06d997c05981a2a
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2007-06-15 13:37:59 +0000

    DOC: improved MITK_MULTIPLEX_PICTYPE

--- CMake/CoreHelpers.cmake
+++ CMake/CoreHelpers.cmake
@@ -61,11 +61,17 @@ MACRO(CHECK_AND_SET flag sourcelist )
   ENDIF(R)
 ENDMACRO(CHECK_AND_SET)
 
-# file should be named mitkMyAlgo-TYPE.cpp
+#
+# MITK_MULTIPLEX_PICTYPE: generate separated source files for different
+# data types to reduce memory consumption of compiler during template 
+# instantiation
+#
+# Param "file" should be named like mitkMyAlgo-TYPE.cpp
 # in the file, every occurence of @TYPE@ is replaced by the
 # datatype. For each datatype, a new file mitkMyAlgo-datatype.cpp 
-# is generated and added to FILES_CPP
-
+# is generated and added to CPP_FILES. Use this macro in files.cmake after
+# creating the CPP_FILES variable.
+#
 MACRO(MITK_MULTIPLEX_PICTYPE file)
   SET(TYPES "double;float;int;unsigned int;short;unsigned short;char;unsigned char")
   FOREACH(TYPE ${TYPES})

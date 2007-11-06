3c2fbdc5a94511165ea733b2ec3b321626233924
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2007-11-06 21:13:22 +0000

    ENH: initial revision of test template

---
+++ Testing/mitkTestTemplate.cpp
@@ -0,0 +1,49 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision: 7837 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include <iostream>
+#include "mitkClassName.h"
+
+/**
+ *  Simple example for a test for the (non-existent) class "ClassName".
+ *  
+ *  argc and argv are the command line parameters which were passed to 
+ *  the ADD_TEST command in the CMakeLists.txt file. For the automatic
+ *  tests, argv is either empty for the simple tests or contains the filename
+ *  of a test image for the image tests (see CMakeLists.txt).
+ */
+int mitkClassNameTest(int /* argc */, char** /*argv*/)
+{
+  // always start with this!
+  MITK_TEST_BEGIN()
+
+  // let's create an object of our class  
+  mitk::ClassName::Pointer myClassName = mitk::ClassName::New();
+  
+  // first test: did this work?
+  // using MITK_TEST_CONDITION_REQUIRED makes the test stop after failure, since
+  // it makes no sense to continue without an object.
+  MITK_TEST_CONDITION_REQUIRED(myClassName.IsNotNull(),"Testing instantiation") 
+
+  // write your own tests here and use the macros from mitkTestingMacros.h !!!
+  // do not write to std::cout and do not return from this function yourself!
+  
+  // always end with this!
+  MITK_TEST_END()
+}
+

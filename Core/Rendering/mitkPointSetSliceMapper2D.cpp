9d39dbaf901319312c8218ee58b4518273aac01d
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2007-11-13 14:45:26 +0000

    ADD: initial commit

---
+++ Core/Rendering/mitkPointSetSliceMapper2D.cpp
@@ -0,0 +1,535 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision: 11618 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include <mitkGL.h>
+#include "mitkPointSetSliceMapper2D.h"
+
+#include "mitkBaseRenderer.h"
+#include "mitkPlaneGeometry.h"
+#include "mitkUnstructuredGrid.h"
+#include "mitkTransferFunction.h"
+#include "mitkTransferFunctionProperty.h"
+#include "mitkColorProperty.h"
+#include "mitkVtkScalarModeProperty.h"
+#include "mitkProperties.h"
+#include "mitkAbstractTransformGeometry.h"
+#include "mitkBaseVtkMapper3D.h"
+
+#include "vtkPointSetSlicer.h"
+
+#include <vtkPointSet.h>
+#include <vtkPointSetSource.h>
+#include <vtkPolyData.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkPlane.h>
+#include <vtkPoints.h>
+#include <vtkCellArray.h>
+#include <vtkLookupTable.h>
+#include <vtkPointData.h>
+#include <vtkCellData.h>
+#include <vtkDataArray.h>
+#include <vtkLinearTransform.h>
+#include <vtkActor.h>
+#include <vtkVolume.h>
+#include <vtkAssembly.h>
+#include <vtkVolumeProperty.h>
+#include <vtkAbstractMapper3D.h>
+#include <vtkMapper.h>
+#include <vtkAbstractVolumeMapper.h>
+#include <vtkScalarsToColors.h>
+#include <vtkPiecewiseFunction.h>
+#include <vtkColorTransferFunction.h>
+
+#include <itkProcessObject.h>
+
+void mitk::PointSetSliceMapper2D::GenerateData()
+{
+  mitk::DataTreeNode::ConstPointer node = this->GetDataTreeNode();
+  if ( node.IsNull() )
+    return;
+
+
+  if (!node->GetProperty(m_ScalarMode, "scalar mode"))
+  {
+    m_ScalarMode = new mitk::VtkScalarModeProperty(0);
+  }
+
+  if (!node->GetProperty(m_ScalarVisibility, "scalar visibility"))
+  {
+    m_ScalarVisibility = new mitk::BoolProperty(true);
+  }
+  
+  if (!node->GetProperty(m_Outline, "outline polygons"))
+  {
+    m_Outline = new mitk::BoolProperty(false);
+  }
+  
+  if (!node->GetProperty(m_Color, "color"))
+  {
+    m_Color = new mitk::ColorProperty(1.0f, 1.0f, 1.0f);
+  }
+  
+  if (!node->GetProperty(m_LineWidth, "linewidth"))
+  {
+    m_LineWidth = new mitk::IntProperty(1);
+  }
+
+}
+
+void mitk::PointSetSliceMapper2D::GenerateData( mitk::BaseRenderer* renderer )
+{
+  mitk::BaseData::Pointer input = const_cast<mitk::BaseData*>( this->GetData() );
+  assert( input );
+
+  input->Update();
+  
+  if (m_VtkPointSet) m_VtkPointSet->UnRegister(0);
+  m_VtkPointSet = this->GetVtkPointSet(renderer);
+  assert(m_VtkPointSet);
+  m_VtkPointSet->Register(0);
+    
+  mitk::DataTreeNode::ConstPointer node = this->GetDataTreeNode();
+  mitk::TransferFunctionProperty::Pointer transferFuncProp;
+  node->GetProperty(transferFuncProp, "TransferFunction", renderer);
+  if (transferFuncProp.IsNotNull())
+  {
+    mitk::TransferFunction::Pointer tf = transferFuncProp->GetValue();
+    if (m_ScalarsToColors) m_ScalarsToColors->UnRegister(0);
+    m_ScalarsToColors = static_cast<vtkScalarsToColors*>(tf->GetColorTransferFunction());
+    m_ScalarsToColors->Register(0);
+    
+    if (m_ScalarsToOpacity) m_ScalarsToOpacity->UnRegister(0);
+    m_ScalarsToOpacity = tf->GetScalarOpacityFunction();
+    m_ScalarsToOpacity->Register(0);
+  }
+  else
+  {
+    if (m_ScalarsToColors) m_ScalarsToColors->UnRegister(0);
+    m_ScalarsToColors = this->GetVtkLUT(renderer);
+    assert(m_ScalarsToColors);
+    m_ScalarsToColors->Register(0);
+    
+    float opacity;
+    node->GetOpacity(opacity, renderer);
+    if (m_ScalarsToOpacity) m_ScalarsToOpacity->UnRegister(0);
+    m_ScalarsToOpacity = vtkPiecewiseFunction::New();
+    double range[2];
+    m_VtkPointSet->GetScalarRange(range);
+    m_ScalarsToOpacity->AddSegment(range[0], opacity, range[1], opacity);
+  }
+}
+
+void mitk::PointSetSliceMapper2D::Paint( mitk::BaseRenderer* renderer )
+{
+  if ( IsVisible( renderer ) == false )
+    return ;
+
+  vtkLinearTransform * vtktransform = GetDataTreeNode()->GetVtkTransform();
+  vtkLinearTransform * inversetransform = vtktransform->GetLinearInverse();
+
+  Geometry2D::ConstPointer worldGeometry = renderer->GetCurrentWorldGeometry2D();
+  PlaneGeometry::ConstPointer worldPlaneGeometry = dynamic_cast<const PlaneGeometry*>( worldGeometry.GetPointer() );
+
+  Point3D point;
+  Vector3D normal;
+
+  if(worldPlaneGeometry.IsNotNull())
+  {
+    // set up vtkPlane according to worldGeometry
+    point=worldPlaneGeometry->GetOrigin();
+    normal=worldPlaneGeometry->GetNormal(); normal.Normalize();
+    m_Plane->SetTransform((vtkAbstractTransform*)NULL);
+  }
+  else
+  {
+    //@FIXME: does not work correctly. Does m_Plane->SetTransform really transforms a "plane plane" into a "curved plane"?
+    return;
+    AbstractTransformGeometry::ConstPointer worldAbstractGeometry = dynamic_cast<const AbstractTransformGeometry*>(renderer->GetCurrentWorldGeometry2D());
+    if(worldAbstractGeometry.IsNotNull())
+    {
+      // set up vtkPlane according to worldGeometry
+      point=const_cast<mitk::BoundingBox*>(worldAbstractGeometry->GetParametricBoundingBox())->GetMinimum();
+      FillVector3D(normal, 0, 0, 1);
+      m_Plane->SetTransform(worldAbstractGeometry->GetVtkAbstractTransform()->GetInverse());
+    }
+    else
+      return;
+  }
+
+  vtkFloatingPointType vp[ 3 ], vnormal[ 3 ];
+
+  vnl2vtk(point.Get_vnl_vector(), vp);
+  vnl2vtk(normal.Get_vnl_vector(), vnormal);
+
+  //normally, we would need to transform the surface and cut the transformed surface with the cutter.
+  //This might be quite slow. Thus, the idea is, to perform an inverse transform of the plane instead.
+  //@todo It probably does not work for scaling operations yet:scaling operations have to be
+  //dealed with after the cut is performed by scaling the contour.
+  inversetransform->TransformPoint( vp, vp );
+  inversetransform->TransformNormalAtPoint( vp, vnormal, vnormal );
+
+  m_Plane->SetOrigin( vp );
+  m_Plane->SetNormal( vnormal );
+
+  // set data into cutter
+  m_Slicer->SetInput( m_VtkPointSet );
+  //    m_Cutter->GenerateCutScalarsOff();
+  //    m_Cutter->SetSortByToSortByCell();
+
+  // calculate the cut
+  m_Slicer->Update();
+
+  // fetch geometry
+  mitk::DisplayGeometry::Pointer displayGeometry = renderer->GetDisplayGeometry();
+  assert( displayGeometry );
+  //  float toGL=displayGeometry->GetSizeInDisplayUnits()[1];
+
+  //apply color and opacity read from the PropertyList
+  ApplyProperties( renderer );
+
+  // traverse the cut contour
+  vtkPolyData * contour = m_Slicer->GetOutput();
+
+  vtkPoints *vpoints = contour->GetPoints();
+  vtkCellArray *vlines = contour->GetLines();
+  vtkCellArray *vpolys = contour->GetPolys();
+  vtkPointData *vpointdata = contour->GetPointData();
+  vtkDataArray* vscalars = vpointdata->GetScalars();
+
+  vtkCellData *vcelldata = contour->GetCellData();
+  vtkDataArray* vcellscalars = vcelldata->GetScalars();
+
+  int numberOfLines = contour->GetNumberOfLines();
+  int numberOfPolys = contour->GetNumberOfPolys();
+
+  Point3D p;
+  Point2D p2d, last, first;
+
+  vlines->InitTraversal();
+  vpolys->InitTraversal();
+  
+  for (int i = 0;i < numberOfLines;++i )
+  {
+    vtkIdType *cell(0);
+    vtkIdType cellSize(0);
+
+    vlines->GetNextCell( cellSize, cell );
+
+    if (m_ScalarVisibility->GetValue())
+    {
+      if ( m_ScalarMode->GetVtkScalarMode() == VTK_SCALAR_MODE_DEFAULT ||
+           m_ScalarMode->GetVtkScalarMode() == VTK_SCALAR_MODE_USE_CELL_DATA )
+      {  // color each cell according to cell data
+        double scalar = vcellscalars->GetComponent( i, 0 );
+        double rgb[3] = { 1.0f, 1.0f, 1.0f };
+        m_ScalarsToColors->GetColor(scalar, rgb);
+        float rgba[4] = {(float)rgb[0], (float)rgb[1], (float)rgb[2], 1.0f };
+        rgba[3] = (float)m_ScalarsToOpacity->GetValue(scalar);
+        glColor4fv( rgba );
+      }
+      else if ( m_ScalarMode->GetVtkScalarMode() == VTK_SCALAR_MODE_USE_POINT_DATA )
+      {
+        double scalar = vscalars->GetComponent( i, 0 );
+        double rgb[3] = { 1.0f, 1.0f, 1.0f };
+        m_ScalarsToColors->GetColor(scalar, rgb);
+        float rgba[4] = {(float)rgb[0], (float)rgb[1], (float)rgb[2], 1.0f };
+        rgba[3] = (float)m_ScalarsToOpacity->GetValue(scalar);
+        glColor4fv( rgba );
+      }
+    }
+
+    glBegin ( GL_LINE_LOOP );
+    for ( int j = 0;j < cellSize;++j )
+    {
+      vpoints->GetPoint( cell[ j ], vp );
+      //take transformation via vtktransform into account
+      vtktransform->TransformPoint( vp, vp );
+
+      vtk2itk( vp, p );
+
+      //convert 3D point (in mm) to 2D point on slice (also in mm)
+      worldGeometry->Map( p, p2d );
+
+      //convert point (until now mm and in worldcoordinates) to display coordinates (units )
+      displayGeometry->WorldToDisplay( p2d, p2d );
+
+      //convert display coordinates ( (0,0) is top-left ) in GL coordinates ( (0,0) is bottom-left )
+      //p2d[1]=toGL-p2d[1];
+
+      //add the current vertex to the line
+      glVertex2f( p2d[0], p2d[1] );
+    }
+    glEnd ();
+
+  }
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  
+  for (int i = 0;i < numberOfPolys;++i )
+  {
+    vtkIdType *cell(0);
+    vtkIdType cellSize(0);
+
+    vpolys->GetNextCell( cellSize, cell );
+
+    if (m_ScalarVisibility->GetValue())
+    {
+      if ( m_ScalarMode->GetVtkScalarMode() == VTK_SCALAR_MODE_DEFAULT ||
+            m_ScalarMode->GetVtkScalarMode() == VTK_SCALAR_MODE_USE_CELL_DATA )
+      {  // color each cell according to cell data
+        double scalar = vcellscalars->GetComponent( i, 0 );
+        double rgb[3] = { 1.0f, 1.0f, 1.0f };
+        m_ScalarsToColors->GetColor(scalar, rgb);
+        float rgba[4] = {(float)rgb[0], (float)rgb[1], (float)rgb[2], 1.0f };
+        rgba[3] = (float)m_ScalarsToOpacity->GetValue(scalar);
+        glColor4fv( rgba );
+      }
+      else if (m_ScalarMode->GetVtkScalarMode() == VTK_SCALAR_MODE_USE_POINT_DATA )
+      {
+        double scalar = vscalars->GetComponent( i, 0 );
+        double rgb[3] = { 1.0f, 1.0f, 1.0f };
+        m_ScalarsToColors->GetColor(scalar, rgb);
+        float rgba[4] = {(float)rgb[0], (float)rgb[1], (float)rgb[2], 1.0f };
+        rgba[3] = (float)m_ScalarsToOpacity->GetValue(scalar);
+        glColor4fv( rgba );
+      }
+    }
+
+    //Point2D points[cellSize];
+    glBegin( GL_POLYGON );
+    for (int j = 0; j < cellSize; ++j)
+    {
+      vpoints->GetPoint( cell[ j ], vp );
+      //take transformation via vtktransform into account
+      vtktransform->TransformPoint( vp, vp );
+
+      vtk2itk( vp, p );
+
+      //convert 3D point (in mm) to 2D point on slice (also in mm)
+      worldGeometry->Map( p, p2d );
+
+      //convert point (until now mm and in worldcoordinates) to display coordinates (units )
+      displayGeometry->WorldToDisplay( p2d, p2d );
+
+      //convert display coordinates ( (0,0) is top-left ) in GL coordinates ( (0,0) is bottom-left )
+      //p2d[1]=toGL-p2d[1];
+
+      //add the current vertex to the line
+      glVertex2f( p2d[0], p2d[1] );
+    }
+    glEnd();
+  }
+  glDisable(GL_BLEND);
+  
+  if (m_Outline->GetValue())
+  {
+    vpolys->InitTraversal();
+    mitk::Color  color = m_Color->GetColor();
+    glColor4f(color[0], color[1], color[2], 1.0f);
+    glLineWidth((float)m_LineWidth->GetValue());
+    for (int i = 0;i < numberOfPolys;++i )
+    {
+      vtkIdType *cell(0);
+      vtkIdType cellSize(0);
+
+      vpolys->GetNextCell( cellSize, cell );
+
+      //Point2D points[cellSize];
+      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+      glBegin( GL_POLYGON );
+      //glPolygonOffset(1.0, 1.0);
+      for (int j = 0; j < cellSize; ++j)
+      {
+        vpoints->GetPoint( cell[ j ], vp );
+        //take transformation via vtktransform into account
+        vtktransform->TransformPoint( vp, vp );
+
+        vtk2itk( vp, p );
+
+        //convert 3D point (in mm) to 2D point on slice (also in mm)
+        worldGeometry->Map( p, p2d );
+
+        //convert point (until now mm and in worldcoordinates) to display coordinates (units )
+        displayGeometry->WorldToDisplay( p2d, p2d );
+
+        //convert display coordinates ( (0,0) is top-left ) in GL coordinates ( (0,0) is bottom-left )
+        //p2d[1]=toGL-p2d[1];
+
+        //add the current vertex to the line
+        glVertex2f( p2d[0], p2d[1] );
+      }
+      glEnd();
+      glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+  }
+}
+
+
+vtkAbstractMapper3D* 
+mitk::PointSetSliceMapper2D
+::GetVtkAbstractMapper3D(mitk::BaseRenderer * renderer)
+{
+  //std::cout << "GETVTKABSTRACTMAPPER3D\n";
+  mitk::DataTreeNode::ConstPointer node = this->GetDataTreeNode();
+  if ( node.IsNull() )
+    return 0;
+
+  mitk::BaseVtkMapper3D::Pointer mitkMapper = dynamic_cast< mitk::BaseVtkMapper3D* > ( node->GetMapper( 2 ) );
+  if ( mitkMapper.IsNull() )
+  {
+    return 0;
+  }
+
+  mitkMapper->Update(renderer);
+
+  vtkActor* actor = dynamic_cast<vtkActor*>( mitkMapper->GetProp() );
+  if (actor)
+  {
+    return dynamic_cast<vtkAbstractMapper3D*>( actor->GetMapper() );
+  }
+  vtkVolume* volume = dynamic_cast<vtkVolume*>( mitkMapper->GetProp() );
+  if (volume)
+  {
+    return dynamic_cast<vtkAbstractMapper3D*>( volume->GetMapper() );
+  }
+
+  return 0;
+}
+
+
+
+vtkPointSet*
+mitk::PointSetSliceMapper2D
+::GetVtkPointSet(mitk::BaseRenderer* renderer)
+{
+  //std::cout << "GETVTKPOINTSET\n";
+  vtkAbstractMapper3D * abstractMapper = GetVtkAbstractMapper3D(renderer);
+  if ( abstractMapper == 0 )
+  {
+    // try to get data from the node
+    mitk::DataTreeNode::ConstPointer node = this->GetDataTreeNode();
+    if ( node.IsNull() )
+      return 0;
+    mitk::BaseData::Pointer data = node->GetData();
+    mitk::UnstructuredGrid::Pointer grid = dynamic_cast<mitk::UnstructuredGrid*>(data.GetPointer());
+    if (!grid.IsNull())
+      return static_cast<vtkPointSet*>(grid->GetVtkUnstructuredGrid());
+
+      return 0;
+  }
+  else
+  {
+    vtkMapper* mapper = dynamic_cast<vtkMapper*>(abstractMapper);
+    if (mapper)
+    {
+      return dynamic_cast<vtkPointSet*>(mapper->GetInput());
+    }
+    vtkAbstractVolumeMapper* volMapper = dynamic_cast<vtkAbstractVolumeMapper*>(abstractMapper);
+    if (volMapper)
+    {
+      return dynamic_cast<vtkPointSet*>(volMapper->GetDataSetInput());
+    }
+  }
+
+  return 0;
+}
+
+
+
+vtkScalarsToColors* mitk::PointSetSliceMapper2D::GetVtkLUT(mitk::BaseRenderer* renderer)
+{
+  //std::cout << "GETVTKLUT\n";
+  vtkMapper * mapper = dynamic_cast<vtkMapper*>(GetVtkAbstractMapper3D(renderer));
+  if (mapper)
+    return mapper->GetLookupTable();
+  else
+  {
+    mitk::DataTreeNode::ConstPointer node = this->GetDataTreeNode();
+    if ( node.IsNull() )
+      return 0;
+
+    mitk::BaseVtkMapper3D::Pointer mitkMapper = dynamic_cast< mitk::BaseVtkMapper3D* > ( node->GetMapper( 2 ) );
+    if ( mitkMapper.IsNull() )
+    {
+      //std::cout << "mitkMapper is null\n";
+      return 0;
+    }
+
+    mitkMapper->Update(renderer);
+
+    vtkVolume* volume = dynamic_cast<vtkVolume*>( mitkMapper->GetProp() );
+    if (volume)
+    {
+      //std::cout << "found volume prop\n";
+      return static_cast<vtkScalarsToColors*>(volume->GetProperty()->GetRGBTransferFunction());
+    }
+    
+    vtkAssembly* assembly = dynamic_cast<vtkAssembly*>(mitkMapper->GetProp());
+    if (assembly)
+    {
+      //std::cout << "found assembly prop\n";
+      mitk::TransferFunctionProperty::Pointer transferFuncProp;
+      node->GetProperty(transferFuncProp, "TransferFunction", 0);
+      if (transferFuncProp.IsNotNull())
+      {
+        std::cout << "return colortransferfunction\n";
+        return static_cast<vtkScalarsToColors*>(transferFuncProp->GetValue()->GetColorTransferFunction());
+      }
+    }
+    return 0;
+  }
+}
+
+
+bool mitk::PointSetSliceMapper2D::IsConvertibleToVtkPointSet(mitk::BaseRenderer * renderer)
+{
+    return ( GetVtkPointSet(renderer) != 0 );
+}
+
+mitk::PointSetSliceMapper2D::PointSetSliceMapper2D()
+{
+    m_Plane = vtkPlane::New();
+    m_Slicer = vtkPointSetSlicer::New();
+
+    m_Slicer->SetSlicePlane( m_Plane );
+    
+    m_ScalarsToColors = 0;
+    m_ScalarsToOpacity = 0;
+    m_VtkPointSet = 0;
+
+    //m_LUT = vtkLookupTable::New();
+    //m_LUT->SetTableRange( 0, 255 );
+    //m_LUT->SetNumberOfColors( 255 );
+    //m_LUT->SetRampToLinear ();
+    //m_LUT->Build();
+}
+
+
+
+mitk::PointSetSliceMapper2D::~PointSetSliceMapper2D()
+{
+  m_Slicer->Delete();
+  m_Plane->Delete();
+  
+  m_ScalarsToOpacity->UnRegister(0);
+  m_ScalarsToColors->UnRegister(0);
+  m_VtkPointSet->UnRegister(0);
+}
+
---
+++ Core/Rendering/mitkPointSetSliceMapper2D.h
@@ -0,0 +1,113 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision: 3393 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef MitkPointSetSliceLMapper2D_H
+#define MitkPointSetSliceLMapper2D_H
+
+#include "mitkCommon.h"
+#include "mitkGLMapper2D.h"
+#include "mitkTransferFunction.h"
+#include "mitkProperties.h"
+#include "mitkColorProperty.h"
+#include "mitkVtkScalarModeProperty.h"
+
+class vtkPointSetSlicer;
+class vtkPlane;
+class vtkLookupTable;
+class vtkPointSet;
+class vtkScalarsToColors;
+class vtkPiecewiseFunction;
+class vtkAbstractMapper3D;
+
+namespace mitk
+{
+
+class BaseRenderer;
+
+/**
+ * @brief OpenGL-based mapper to display a 2d cut through a poly data
+ * OpenGL-based mapper to display a 2D cut through a poly data. The result is
+ * normally a line. This class can be added to any data object, which is 
+ * rendered in 3D via a vtkPolyData.
+ */
+class PointSetSliceMapper2D : public GLMapper2D
+{
+public:
+
+    mitkClassMacro( PointSetSliceMapper2D, GLMapper2D );
+
+    itkNewMacro( Self );
+
+    /**
+     * Renders a cut through a pointset by cutting trough the n-cells,
+     * producing (n-1)-cells.
+     * @param renderer the render to render in.
+     */
+    virtual void Paint( mitk::BaseRenderer* renderer );
+
+protected:
+
+    PointSetSliceMapper2D();
+
+    virtual ~PointSetSliceMapper2D();
+
+    virtual void GenerateData();
+    virtual void GenerateData(BaseRenderer*);
+    
+    /**
+     * Determines, if the associated BaseData is mapped three-dimensionally (mapper-slot id 2)
+     * with a class convertable to vtkAbstractMapper3D().
+     * @returns NULL if it is not convertable or the appropriate Mapper otherwise
+     */
+    virtual vtkAbstractMapper3D* GetVtkAbstractMapper3D(BaseRenderer* renderer);
+
+    /**
+     * Determines the pointset object to be cut.
+     * returns the pointset if possible, otherwise NULL.
+     */
+    virtual vtkPointSet* GetVtkPointSet(BaseRenderer* renderer );
+
+    /**
+     * Determines the LookupTable used by the associated vtkMapper.
+     * returns the LUT if possible, otherwise NULL.
+     */
+    virtual vtkScalarsToColors* GetVtkLUT(BaseRenderer* renderer );
+
+    /**
+     * Checks if this mapper can be used to generate cuts through the associated 
+     * base data.
+     * @return true if yes or false if not.
+     */
+    virtual bool IsConvertibleToVtkPointSet(BaseRenderer* renderer);
+
+    vtkPlane* m_Plane;
+    vtkPointSetSlicer* m_Slicer;
+    vtkPointSet* m_VtkPointSet;
+    vtkScalarsToColors* m_ScalarsToColors;
+    vtkPiecewiseFunction* m_ScalarsToOpacity;
+    
+    mitk::ColorProperty::Pointer m_Color;
+    mitk::IntProperty::Pointer m_LineWidth;
+    mitk::BoolProperty::Pointer m_Outline;
+    mitk::BoolProperty::Pointer m_ScalarVisibility;
+    mitk::VtkScalarModeProperty::Pointer m_ScalarMode;
+};
+
+} // namespace mitk
+#endif /* MitkPointSetSliceLMapper2D_H */

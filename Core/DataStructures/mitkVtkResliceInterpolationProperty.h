4d74eccd2492037eebc347cf1922a7c81a361760
Mathias Seitel <m.seitel@dkfz-heidelberg.de>
2007-11-30 07:59:03 +0000

    COMP: Added missing classes... (see bug #522)

---
+++ Core/DataStructures/mitkVtkResliceInterpolationProperty.h
@@ -0,0 +1,75 @@
+#ifndef _MITK_VTK_RESLICE_INTERPOLATION_PROPERTY__H_
+#define _MITK_VTK_RESLICE_INTERPOLATION_PROPERTY__H_
+
+#include "mitkEnumerationProperty.h"
+
+#include <vtkImageReslice.h>
+
+namespace mitk
+{
+
+/**
+ * Encapsulates the enumeration for reslice interpolation. Valid values are 
+ * (VTK constant/Id/string representation):
+ * VTK_RESLICE_NEAREST, VTK_RESLICE_LINEAR, VTK_RESLICE_CUBIC
+ * Default is VTK_RESLICE_NEAREST
+ */
+class VtkResliceInterpolationProperty : public EnumerationProperty
+{
+public:
+
+  mitkClassMacro( VtkResliceInterpolationProperty, EnumerationProperty );
+
+  /** Sets reslice interpolation mode to default (VTK_RESLICE_NEAREST).
+   */
+  VtkResliceInterpolationProperty( );
+  
+  /**
+   * Constructor. Sets reslice interpolation to the given value.
+   */
+  VtkResliceInterpolationProperty( const IdType& value );
+  
+  /**
+   * Constructor. Sets reslice interpolation to the given value.
+   */
+  VtkResliceInterpolationProperty( const std::string& value );
+  
+  /**
+   * Returns the current interpolation value as defined by VTK constants.
+   */
+  virtual int GetInterpolation();
+
+  /**
+   * Sets the interpolation type to VTK_RESLICE_NEAREST.
+   */
+  virtual void SetInterpolationToNearest();
+  
+  /**
+   * Sets the interpolation type to VTK_RESLICE_LINEAR.
+   */
+  virtual void SetInterpolationToLinear();
+  
+  /**
+   * Sets the interpolation type to VTK_RESLICE_CUBIC.
+   */
+  virtual void SetInterpolationToCubic();
+  
+protected:
+
+  /**
+   * this function is overridden as protected, so that the user may not add
+   * additional invalid interpolation types.
+   */
+  virtual bool AddEnum( const std::string& name, const IdType& id );
+
+  /**
+   * Adds the enumeration types as defined by vtk to the list of known 
+   * enumeration values.
+   */
+  virtual void AddInterpolationTypes();
+};
+
+} // end of namespace mitk
+
+#endif
+

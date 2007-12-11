9b64172e6ea292391d1dd71e4e4199fdbdc6f315
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2007-12-11 14:30:28 +0000

    FIX (#691): removed ITK 2.2 #ifdef branch and now obsolete comment about buggy ITK

--- Core/DataStructures/mitkItkMatrixHack.h
+++ Core/DataStructures/mitkItkMatrixHack.h
@@ -28,7 +28,6 @@ namespace mitk {
 //##
 //## Usage: static_cast object of type itk::MatrixOffsetTransformBase 
 //## (or derived from this) to this and call MatrixChanged().
-//## Primary reason for this class: ITK 2.2.0 has a bug: 
 //## itk::MatrixOffsetTransformBase::SetParameters does not set 
 //## m_MatrixMTime thus m_InverseMatrixMTime is the same 
 //## as m_MatrixMTime and the inverse is not recalculated.
@@ -40,11 +39,7 @@ class ItkMatrixHack : public TTransformType
 public:
   void MatrixChanged()
   {
-#if (ITK_VERSION_MAJOR == 2 && ITK_VERSION_MINOR < 2)
-    this->m_MatrixMTime.Modified();
-#else
     this->SetVarMatrix(this->GetMatrix());
-#endif
   }
 };
 

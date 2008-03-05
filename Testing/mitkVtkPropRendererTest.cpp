adb70ac702e9eac692734dce99a7176935911e7f
Christoph Bergmeir <c.bergmeir@dkfz-heidelberg.de>
2008-03-05 13:03:11 +0000

    ENH: Added a check that makes a screenshot, adds all pixel values and checks this to a predefined value

--- Testing/mitkVtkPropRendererTest.cpp
+++ Testing/mitkVtkPropRendererTest.cpp
@@ -22,11 +22,11 @@ PURPOSE.  See the above copyright notices for more information.
 #include "vtkRenderer.h"
 #include "vtkRenderWindowInteractor.h"
 #include "vtkProperty.h"
+#include "vtkUnsignedCharArray.h"
 
 #include <mitkPicFileReader.h>
 #include <mitkImage.h>
 #include <mitkDataTree.h>
-//#include <mitkImageMapper2D.h>
 #include <mitkLevelWindow.h>
 #include <mitkLevelWindowProperty.h>
 
@@ -53,6 +53,11 @@ int mitkVtkPropRendererTest(int argc, char* argv[])
     std::cout<<"[TEST DONE]"<<std::endl;
     return EXIT_SUCCESS;
   }
+
+  static long int sum_orig_Pic3D_pic_gz = 14685408;
+
+  std::string argv_str(argv[1]);
+
   ipPicGetTags(argv[1], picheader);
 
   //Read pic-Image from file
@@ -123,14 +128,6 @@ int mitkVtkPropRendererTest(int argc, char* argv[])
   propRenderer->SetData(&it);
   std::cout<<"[PASSED]"<<std::endl;
 
-  //std::cout << "Testing if an mitk::ImageMapper2D was created: ";
-  //if(dynamic_cast<mitk::ImageMapper2D*>(node->GetMapper(1))==NULL)
-  //{
-  //  std::cout<<"[FAILED]"<<std::endl;
-  //  return EXIT_FAILURE;
-  //}
-  //std::cout<<"[PASSED]"<<std::endl;
-
   std::cout << "Creating vtkMitkRenderProp and connecting it to the VtkPropRenderer: ";
   vtkMitkRenderProp* renderProp = vtkMitkRenderProp::New();
   renderProp->SetPropRenderer(propRenderer);
@@ -141,18 +138,70 @@ int mitkVtkPropRendererTest(int argc, char* argv[])
   mitk::VtkLayerController::GetInstance(renderWindow)->InsertForegroundRenderer(sphereRenderer,true);
   std::cout<<"[PASSED]"<<std::endl;
 
+  // mouse interaction for debugging
+  //vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+  //iren->SetRenderWindow(renderWindow);
+
+  std::cout << "Setting and getting size of renderWindow: ";
+  renderWindow->SetSize(400, 400);
+  int *size = renderWindow->GetSize();
+  std::cout<<"[PASSED]"<<std::endl;
+
   std::cout << "Do the rendering: ";
   renderWindow->Render();
   std::cout<<"[PASSED]"<<std::endl;
 
-  // mouse interaction for debugging
-  //vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
-  //iren->SetRenderWindow(renderWindow);
   //iren->Start();
-  
+
+  std::cout << "Testing to pick a world position: ";
+  mitk::Point2D p;
+  mitk::Point3D p_mm;
+  p[0] = 10;
+  p[1] = 10;
+  propRenderer->PickWorldPoint(p, p_mm);
+  std::cout << "returned world position: " << p_mm << "\n";
+
+  std::cout << "Creating vtkUnsignedCharArray: ";
+  vtkUnsignedCharArray *vtkImage = vtkUnsignedCharArray::New();
+  std::cout<<"[PASSED]"<<std::endl;
+
+  cout << "Reading image from renderWindow" << std::endl;
+  renderWindow->GetRGBACharPixelData(0, 0, size[0]-1, size[1]-1, 0, vtkImage);
+  cout << "Read " << size[0]*size[1] << " data points\n";
+
+  cout << "Computing sum of all RGBA values..\n";
+  long int sum_now = 0;
+  for(int i=0;i<size[0]*size[1];i++) 
+    sum_now += vtkImage->GetValue(i);
+  cout << "Sum of all RGBA values: " << sum_now << "\n";
+
+  //std::string Pic3d_pic_gz_str("Pic3D.pic.gz");
+  //std::cout << "pic3d " << Pic3d_pic_gz_str << "\n";
+  //std::cout << "argv " << argv_str << "\n";
+  //std::cout << "find " << (int) argv_str.find("Pic3D.pic.gz") << "\n";
+  //std::cout << "size " << argv_str.size() << "\n";
+
+  if(argv_str.size() - ((int) argv_str.find("Pic3D.pic.gz")) == 12)
+  {
+    std::cout << "Input image is Pic3D.pic.gz\n";    
+    std::cout << "Sum should be: " << sum_orig_Pic3D_pic_gz << "\n";
+    if(sum_orig_Pic3D_pic_gz!=sum_now)
+    {
+      std::cout<<"[FAILED]"<<std::endl;
+      return EXIT_FAILURE;
+    }
+    std::cout<<"[PASSED]"<<std::endl;
+  }
+  else
+  {
+    std::cout<<"Unknown image, comparison test skipped"<<std::endl;
+  }
+
   renderWindow->Delete();
   tree = NULL; // As the tree has been registered explicitely, destroy it again.
 
   std::cout<<"[TEST DONE]"<<std::endl;
   return EXIT_SUCCESS;
 }
+
+

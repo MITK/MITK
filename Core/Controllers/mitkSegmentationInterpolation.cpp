d3084844bde6776831a9f8396bb4d2a43c18dde6
Daniel Maleike <d.maleike@dkfz-heidelberg.de>
2008-03-26 12:23:32 +0000

    COMP: related to bug #1269. Havn't understood it completely yet, but this fixes the crash.

--- Core/Controllers/mitkSegmentationInterpolation.cpp
+++ Core/Controllers/mitkSegmentationInterpolation.cpp
@@ -389,11 +389,17 @@ mitk::Image::Pointer mitk::SegmentationInterpolation::Interpolate( unsigned int 
     lowerMITKSlice = extractor->GetOutput();
     lowerMITKSlice->DisconnectPipeline(); // otherwise the next output of the filter will overwrite this pointer, too
     
+    extractor= ExtractImageFilter::New();
+    extractor->SetInput( m_Segmentation );
+    extractor->SetSliceDimension( sliceDimension );
     extractor->SetSliceIndex( sliceIndex );
     extractor->Update();
     resultImage = extractor->GetOutput();
     resultImage->DisconnectPipeline();
     
+    extractor= ExtractImageFilter::New();
+    extractor->SetInput( m_Segmentation );
+    extractor->SetSliceDimension( sliceDimension );
     extractor->SetSliceIndex( upperBound );
     extractor->Update();
     upperMITKSlice = extractor->GetOutput();

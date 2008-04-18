9a19a6f1832b9b69359b915a11b1d9685a916d3e
Daniel Maleike <d.maleike@dkfz-heidelberg.de>
2008-04-18 15:15:46 +0000

    COMP: missing files... ups

---
+++ Core/Interactions/mitkFeedbackContourTool.h
@@ -0,0 +1,110 @@
+/*=========================================================================
+ 
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile: mitk.cpp,v $
+Language:  C++
+Date:      $Date$
+Version:   $Revision: 1.0 $
+ 
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+ 
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+ 
+=========================================================================*/
+
+#ifndef mitkFeedbackContourTool_h_Included
+#define mitkFeedbackContourTool_h_Included
+
+#include "mitkCommon.h"
+#include "mitkSegTool2D.h"
+#include "mitkContour.h"
+#include "mitkImage.h"
+
+#include "mitkDataTreeNode.h"
+
+#include "mitkImageCast.h"
+
+namespace mitk
+{
+
+/**
+  \brief Base class for tools that use a contour for feedback
+
+  \sa Tool
+  \sa Contour
+
+  \ingroup Interaction
+  \ingroup Reliver
+
+  Implements helper methods, that might be of use to all kind of 2D segmentation tools that use a contour for user feedback. 
+   - Providing a feedback contour that might be added or removed from the visible scene (SetFeedbackContourVisible).
+   - Filling of a contour into a 2D slice
+
+  \warning Only to be instantiated by mitk::ToolManager.
+
+  $Author: nolden $
+*/
+class FeedbackContourTool : public SegTool2D
+{
+  public:
+    
+    mitkClassMacro(FeedbackContourTool, SegTool2D);
+
+  protected:
+
+    FeedbackContourTool(); // purposely hidden
+    FeedbackContourTool(const char*); // purposely hidden
+    virtual ~FeedbackContourTool();
+
+    Contour* GetFeedbackContour();
+    void SetFeedbackContour(Contour&);
+    void SetFeedbackContourVisible(bool);
+
+    /// Provide values from 0.0 (black) to 1.0 (full color)
+    void SetFeedbackContourColor( float r, float g, float b );
+    void SetFeedbackContourColorDefault();
+
+    /**
+      \brief Projects a contour onto an image point by point. Converts from world to index coordinates.
+
+      \param correctionForIpSegmentation adds 0.5 to x and y index coordinates (difference between ipSegmentation and MITK contours)
+    */
+    Contour::Pointer ProjectContourTo2DSlice(Image* slice, Contour* contourIn3D, bool correctionForIpSegmentation = false, bool constrainToInside = true);
+
+    /**
+      \brief Projects a slice index coordinates of a contour back into world coordinates.
+      
+      \param correctionForIpSegmentation substracts 0.5 to x and y index coordinates (difference between ipSegmentation and MITK contours)
+    */
+    Contour::Pointer BackProjectContourFrom2DSlice(Image* slice, Contour* contourIn2D, bool correctionForIpSegmentation = false);
+
+
+    /**
+      \brief Paint a filled contour (e.g. of an ipSegmentation pixel type) into a mitk::Image (or arbitraty pixel type).
+      Will not copy the whole filledContourSlice, but only set those pixels in originalSlice to overwritevalue, where the corresponding pixel
+      in filledContourSlice is non-zero.
+    */
+    template<typename TPixel, unsigned int VImageDimension>
+    void ItkCopyFilledContourToSlice( itk::Image<TPixel,VImageDimension>* originalSlice, const Image* filledContourSlice, int overwritevalue = 1 );
+
+    /**
+      \brief Fill a contour in a 2D slice with a specified pixel value.
+    */
+    void FillContourInSlice( Contour* projectedContour, Image* sliceImage, int paintingPixelValue = 1 );
+
+  private:
+
+    Contour::Pointer      m_FeedbackContour;
+    DataTreeNode::Pointer m_FeedbackContourNode;
+    bool                  m_FeedbackContourVisible;
+
+};
+
+} // namespace
+
+#endif
+

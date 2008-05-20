925b0186a1c83a34f18aaf6004bbea6b08d26f2d
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2008-05-20 15:56:28 +0000

    CHG (#1397): perform application tests only with selected images

--- QApplications/MainApp/ApplicationTesting.cmake
+++ QApplications/MainApp/ApplicationTesting.cmake
@@ -1,38 +1,43 @@
-# tests without data
+# tests without data or empty nodes
+SET(datatypes Image Surface PointSet Node) 
+
 IF(MITK_APPLICATION_TESTING)
   ADD_TEST(MainAppWithoutData ${EXECUTABLE_OUTPUT_PATH}/MainApp -noMITKOptions -testing)
-  ADD_TEST(MainApp_EmptyImage ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmptyImage -noMITKOptions -testing)
-  ADD_TEST(MainApp_EmptySurface ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmptySurface -noMITKOptions -testing)
-  ADD_TEST(MainApp_EmptyPointSet ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmptyPointSet -noMITKOptions -testing)
-  ADD_TEST(MainApp_EmptyNode ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmptyNode -noMITKOptions -testing)
+  FOREACH(datatype ${datatypes})
+    ADD_TEST(MainApp_Empty${datatype} ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmpty${datatype} -noMITKOptions -testing)
+  ENDFOREACH(datatype)
 ENDIF(MITK_APPLICATION_TESTING)
 IF(MITK_FUNCTIONALITY_TESTING)
   FOREACH(FUNC_NAME ${FUNC_LIBRARIES})
     ADD_TEST(${FUNC_NAME}_WithoutData ${EXECUTABLE_OUTPUT_PATH}/MainApp -noMITKOptions -testing ${FUNC_NAME})
-    ADD_TEST(${FUNC_NAME}_EmptyImage ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmptyImage -noMITKOptions -testing ${FUNC_NAME})
-    ADD_TEST(${FUNC_NAME}_EmptySurface ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmptySurface -noMITKOptions -testing ${FUNC_NAME})
-    ADD_TEST(${FUNC_NAME}_EmptyPointSet ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmptyPointSet -noMITKOptions -testing ${FUNC_NAME})
-    ADD_TEST(${FUNC_NAME}_EmptyNode ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmptyNode -noMITKOptions -testing ${FUNC_NAME})
+    FOREACH(datatype ${datatypes})
+      ADD_TEST(${FUNC_NAME}_Empty${datatype} ${EXECUTABLE_OUTPUT_PATH}/MainApp -testEmpty${datatype} -noMITKOptions -testing ${FUNC_NAME})
+    ENDFOREACH(datatype)
   ENDFOREACH(FUNC_NAME)
 ENDIF(MITK_FUNCTIONALITY_TESTING)
 
-# now test with all data files found
-FILE(GLOB testdatalist ${MITK_SOURCE_DIR}/Testing/Data/*.*)
-LIST(REMOVE_ITEM testdatalist "${MITK_SOURCE_DIR}/Testing/Data/.svn")
-FOREACH(testdataitem ${testdatalist})
-
-  GET_FILENAME_COMPONENT(testdataname ${testdataitem} NAME)
+# now test with some data files 
+SET(testdatadir ${MITK_SOURCE_DIR}/Testing/Data/)
+SET(testdatalist 
+  binary.stl
+  lungs.vtk
+  Pic2DplusT.pic.gz
+  Pic3D.pic.gz
+  Png2D-bw.png
+  US4DCyl.pic.gz
+)
+FOREACH(testdataname ${testdatalist})
 
   IF(MITK_FUNCTIONALITY_TESTING)
     FOREACH(FUNC_NAME ${FUNC_LIBRARIES})
-    ADD_TEST(${FUNC_NAME}_${testdataname} ${EXECUTABLE_OUTPUT_PATH}/MainApp ${testdataitem} -noMITKOptions -testing ${FUNC_NAME})
+    ADD_TEST(${FUNC_NAME}_${testdataname} ${EXECUTABLE_OUTPUT_PATH}/MainApp ${testdatadir}/${testdataname} -noMITKOptions -testing ${FUNC_NAME})
     ENDFOREACH(FUNC_NAME)
   ENDIF(MITK_FUNCTIONALITY_TESTING)
 
   IF(MITK_APPLICATION_TESTING)
-    ADD_TEST(MainApp_${testdataname} ${EXECUTABLE_OUTPUT_PATH}/MainApp ${testdataitem} -noMITKOptions -testing)
+    ADD_TEST(MainApp_${testdataname} ${EXECUTABLE_OUTPUT_PATH}/MainApp ${testdatadir}/${testdataname} -noMITKOptions -testing)
   ENDIF(MITK_APPLICATION_TESTING)
 
-ENDFOREACH(testdataitem)
+ENDFOREACH(testdataname)
 
 

080abeac2290769418b8afa944e14e3ad7496b46
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2008-06-09 16:10:01 +0000

    FIX (#1417): use CHILI 3.14 SDK workaround for i18n to fix qt button labels in MITK CHILI plugin

--- QApplications/MainApp/ChiliPlugin/QcMITKTask.cpp
+++ QApplications/MainApp/ChiliPlugin/QcMITKTask.cpp
@@ -4,6 +4,10 @@ QcMITKTask::QcMITKTask(const char** xpm, QWidget* parent, const char* name) :
             QcTask(xpm, parent, name), m_Visible(true)
 {
   isImageTask( true );
+  // this is only available in the CHILI SDK 3.14.1 or later. Unfortunately there is
+  // a problem with the VERSION_CODE_MACRO (see bug 1418). If you get compilation errors here,
+  // please comment this line.
+  this->setLocalizable(false);
 }
 
 

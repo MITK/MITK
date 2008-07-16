68e26854b7f1137762d82dbf8447c68bc87fda00
Johannes Kast <j.kast@dkfz-heidelberg.de>
2008-07-16 13:58:24 +0000

    initial Version of the DataTreeNode Test

---
+++ Testing/mitkDataTreeNodeTest.cpp
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date: 2008-02-25 17:27:17 +0100 (Mo, 25 Feb 2008) $
+Version:   $Revision: 7837 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "mitkDataTreeNode.h"
+
+#include "mitkTestingMacros.h"
+
+#include <iostream>
+
+/**
+ *  Simple example for a test for the (non-existent) class "DataTreeNode".
+ *  
+ *  argc and argv are the command line parameters which were passed to 
+ *  the ADD_TEST command in the CMakeLists.txt file. For the automatic
+ *  tests, argv is either empty for the simple tests or contains the filename
+ *  of a test image for the image tests (see CMakeLists.txt).
+ */
+int mitkDataTreeNodeTest(int /* argc */, char* /*argv*/[])
+{
+  // always start with this!
+  MITK_TEST_BEGIN("DataTreeNode")
+
+  // let's create an object of our class  
+  mitk::DataTreeNode::Pointer myDataTreeNode = mitk::DataTreeNode::New();
+  
+  // first test: did this work?
+  // using MITK_TEST_CONDITION_REQUIRED makes the test stop after failure, since
+  // it makes no sense to continue without an object.
+  MITK_TEST_CONDITION_REQUIRED(myDataTreeNode.IsNotNull(),"Testing instantiation") 
+
+  // write your own tests here and use the macros from mitkTestingMacros.h !!!
+  // do not write to std::cout and do not return from this function yourself!
+  
+  // always end with this!
+  MITK_TEST_END()
+}
+

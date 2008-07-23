7dfb109a3d4c8979450fe67f26d0a702473ed969
Anne Sauer <a.sauer@dkfz-heidelberg.de>
2008-07-23 15:40:48 +0000

    COMP: Compile mistake fixed

--- Testing/mitkMaterialPropertyTest.cpp
+++ Testing/mitkMaterialPropertyTest.cpp
@@ -1,28 +1,29 @@
 /*=========================================================================
 
-Program:   Medical Imaging & Interaction Toolkit
-Language:  C++
-Date:      $Date$
-Version:   $Revision$
+ Program:   Medical Imaging & Interaction Toolkit
+ Language:  C++
+ Date:      $Date$
+ Version:   $Revision$
 
-Copyright (c) German Cancer Research Center, Division of Medical and
-Biological Informatics. All rights reserved.
-See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+ Copyright (c) German Cancer Research Center, Division of Medical and
+ Biological Informatics. All rights reserved.
+ See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
 
-This software is distributed WITHOUT ANY WARRANTY; without even
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the above copyright notices for more information.
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
 
-=========================================================================*/
+ =========================================================================*/
 #include "mitkMaterialPropertyTest.h"
 
 #include "mitkMaterialProperty.h"
-
+#include "mitkBaseProperty.h"
+#include "mitkBaseRenderer.h"
 #include "mitkTestingMacros.h"
+#include "mitkDataTreeNode.h"
+#include <mitkVtkPropRenderer.h>
 
 #include <iostream>
-
-
 
 /**
  *  Simple example for a test for the (non-existent) class "ClassName".
@@ -32,33 +33,115 @@ PURPOSE.  See the above copyright notices for more information.
  *  tests, argv is either empty for the simple tests or contains the filename
  *  of a test image for the image tests (see CMakeLists.txt).
  */
-int TestMaterialPropertyForData()
-{
-  //MITK_TEST_CONDITION_REQUIRED((*prop == *prop2),"Testing equality operator (operator==)");
-}
 
+  int
+  mitkMaterialPropertyTest(int /* argc */, char* /*argv*/[])
+  {
+    // always start with this!
+    MITK_TEST_BEGIN("MaterialProperty")
 
+    mitk::MaterialProperty::Pointer myMP;
 
+          {
+            //static Pointer New(mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0)
+            mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+            mitk::BaseRenderer::Pointer renderer = new mitk::VtkPropRenderer();
+            myMP = mitk::MaterialProperty::New(node, renderer);
+            MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+            MITK_TEST_CONDITION( node == myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+            MITK_TEST_CONDITION( renderer == myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
+          }
 
+       /* {
+         //static Pointer New( Color color, vtkFloatingPointType opacity = 1.0f, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0)
+         mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+         mitk::BaseRenderer::Pointer renderer = new mitk::VtkPropRenderer();
+         mitk::Color color;
+         color.Set(0, 0, 0);
+         vtkFloatingPointType opacity = 1.0f;
+         myMP = mitk::MaterialProperty::New(color, opacity, node, renderer);
+         MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+         MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+         MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
+         MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
+         MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
+         }
 
+        {
+         //static Pointer New( vtkFloatingPointType red, vtkFloatingPointType green, vtkFloatingPointType blue, vtkFloatingPointType opacity = 1.0f, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0)
+         mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+         mitk::BaseRenderer::Pointer renderer = new mitk::VtkPropRenderer();
+         Color color;
+         color.Set(0, 0, 0);
+         vtkFloatingPointType opacity = 1.0f;
+         vtkFloatingPointType rgb = 0;
 
+         myMP = mitk::MaterialProperty::New(rgb, rgb, rgb, opacity, node, renderer);
+         MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+         MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+         MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
+         MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
+         MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
+         }*/
 
-int mitkMaterialPropertyTest(int /* argc */, char* /*argv*/[])
-{
-  // always start with this!
-  MITK_TEST_BEGIN("MaterialProperty")
+         /*{
+         //static Pointer New( vtkFloatingPointType red, vtkFloatingPointType green, vtkFloatingPointType blue, vtkFloatingPointType colorCoefficient,  vtkFloatingPointType specularCoefficient, vtkFloatingPointType specularPower, vtkFloatingPointType opacity, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0 )
+         mitk::DataTreeNode::Pointer node = DataTreeNode::New();
+         mitk::BaseRenderer::Pointer renderer = BaseRenderer::New();
+         Color color;
+         color.Set(0, 0, 0);
+         vtkFloatingPointType opacity = 1.0f;
+         vtkFloatingPointType rgb = 0;
+         vtkFloatingPointType colorCoefficient = 0;
+         vtkFloatingPointType specularCoefficient = 0;
+         vtkFloatingPointType specularPower = 0;
 
-  // let's create an object of our class
-  mitk::MaterialProperty::Pointer myMP= mitk::MaterialProperty::New();
+         myMP = MaterialProperty::New(rgb, rgb, rgb, colorCoefficient, specularCoefficient, specularPower, opacity, node, renderer);
+         MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+         MITK_TEST_CONDITION( node==myMp->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+         MITK_TEST_CONDITION( renderer==myMp->GetRenderer(), "Testing if a Renderer object was set correctly" )
+         MITK_TEST_CONDITION( color==myMp->GetColor(), "Testing if a Color object was set correctly" )
+         MITK_TEST_CONDITION( opacity==myMp->GetOpacity(), "Testing if a Opacity object was set correctly" )
+         MITK_TEST_CONDITION( specularCoefficient==myMp->GetSpecularCoefficient(), "Testing if a Coefficient object was set correctly" )
+         MITK_TEST_CONDITION( specularPower==myMp->GetSpecularPower(), "Testing if a SpecularPower object was set correctly" )
+         MITK_TEST_CONDITION( colorCoefficient==myMp->GetColorCoefficient(), "Testing if a colorCoefficient object was set correctly" )
 
-  // first test: did this work?
-  // using MITK_TEST_CONDITION_REQUIRED makes the test stop after failure, since
-  // it makes no sense to continue without an object.
-  MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+         }
 
-  // write your own tests here and use the macros from mitkTestingMacros.h !!!
-  // do not write to std::cout and do not return from this function yourself!
+         {
+         //static Pointer New( Color color, vtkFloatingPointType colorCoefficient, vtkFloatingPointType specularCoefficient, vtkFloatingPointType specularPower, vtkFloatingPointType opacity, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0 )
+         mitk::DataTreeNode::Pointer node = DataTreeNode::New();
+         mitk::BaseRenderer::Pointer renderer = BaseRenderer::New();
+         Color color;
+         color.Set(0, 0, 0);
+         vtkFloatingPointType opacity = 1.0f;
+         vtkFloatingPointType rgb = 0;
+         vtkFloatingPointType colorCoefficient = 0;
+         vtkFloatingPointType specularCoefficient = 0;
+         vtkFloatingPointType specularPower = 0;
 
-  // always end with this!
-  MITK_TEST_END()
-}
+         myMP = MaterialProperty::New(rgb, rgb, rgb, colorCoefficient,
+         specularCoefficient, specularPower, opacity, node, renderer);
+         MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+         MITK_TEST_CONDITION( node==myMp->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+         MITK_TEST_CONDITION( renderer==myMp->GetRenderer(), "Testing if a Renderer object was set correctly" )
+         MITK_TEST_CONDITION( color==myMp->GetColor(), "Testing if a Color object was set correctly" )
+         MITK_TEST_CONDITION( opacity==myMp->GetOpacity(), "Testing if a Opacity object was set correctly" )
+         MITK_TEST_CONDITION( specularCoefficient==myMp->GetSpecularCoefficient(), "Testing if a Coefficient object was set correctly" )
+         MITK_TEST_CONDITION( specularPower==myMp->GetSpecularPower(), "Testing if a SpecularPower object was set correctly" )
+
+         }*/
+
+        // let's create an object of our class
+        //mitk::MaterialProperty::Pointer myMP= mitk::MaterialProperty::New();
+
+        // first test: did this work?
+        // using MITK_TEST_CONDITION_REQUIRED makes the test stop after failure, since
+        // it makes no sense to continue without an object.
+
+        // write your own tests here and use the macros from mitkTestingMacros.h !!!
+        // do not write to std::cout and do not return from this function yourself!
+
+        // always end with this!
+MITK_TEST_END      ()
+    }
--- Testing/mitkMaterialPropertyTest.h
+++ Testing/mitkMaterialPropertyTest.h
@@ -1,19 +1,19 @@
 /*=========================================================================
 
-Program:   Medical Imaging & Interaction Toolkit
-Language:  C++
-Date:      $Date$
-Version:   $Revision$
+ Program:   Medical Imaging & Interaction Toolkit
+ Language:  C++
+ Date:      $Date$
+ Version:   $Revision$
 
-Copyright (c) German Cancer Research Center, Division of Medical and
-Biological Informatics. All rights reserved.
-See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+ Copyright (c) German Cancer Research Center, Division of Medical and
+ Biological Informatics. All rights reserved.
+ See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
 
-This software is distributed WITHOUT ANY WARRANTY; without even
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the above copyright notices for more information.
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
 
-=========================================================================*/
+ =========================================================================*/
 
 #ifndef MITKMATERIALPROPERTYTEST_H_
 #define MITKMATERIALPROPERTYTEST_H_
@@ -22,7 +22,6 @@ class mitkMaterialPropertyTest
 {
 public:
   mitkMaterialPropertyTest();
-  int TestMaterialPropertyForData();
   virtual
   ~mitkMaterialPropertyTest();
 };

6ab27a01c7c1517ef375e1aec7d690349678ff4a
Anne Sauer <a.sauer@dkfz-heidelberg.de>
2008-07-23 13:28:29 +0000

    Add mitkMaterialproperty Test

---
+++ Testing/mitkMaterialPropertyTest.cpp
@@ -0,0 +1,64 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "mitkMaterialPropertyTest.h"
+
+#include "mitkMaterialProperty.h"
+
+#include "mitkTestingMacros.h"
+
+#include <iostream>
+
+
+
+/**
+ *  Simple example for a test for the (non-existent) class "ClassName".
+ *
+ *  argc and argv are the command line parameters which were passed to
+ *  the ADD_TEST command in the CMakeLists.txt file. For the automatic
+ *  tests, argv is either empty for the simple tests or contains the filename
+ *  of a test image for the image tests (see CMakeLists.txt).
+ */
+int TestMaterialPropertyForData()
+{
+  //MITK_TEST_CONDITION_REQUIRED((*prop == *prop2),"Testing equality operator (operator==)");
+}
+
+
+
+
+
+
+
+int mitkMaterialPropertyTest(int /* argc */, char* /*argv*/[])
+{
+  // always start with this!
+  MITK_TEST_BEGIN("MaterialProperty")
+
+  // let's create an object of our class
+  mitk::MaterialProperty::Pointer myMP= mitk::MaterialProperty::New();
+
+  // first test: did this work?
+  // using MITK_TEST_CONDITION_REQUIRED makes the test stop after failure, since
+  // it makes no sense to continue without an object.
+  MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+
+  // write your own tests here and use the macros from mitkTestingMacros.h !!!
+  // do not write to std::cout and do not return from this function yourself!
+
+  // always end with this!
+  MITK_TEST_END()
+}
---
+++ Testing/mitkMaterialPropertyTest.h
@@ -0,0 +1,30 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef MITKMATERIALPROPERTYTEST_H_
+#define MITKMATERIALPROPERTYTEST_H_
+
+class mitkMaterialPropertyTest
+{
+public:
+  mitkMaterialPropertyTest();
+  int TestMaterialPropertyForData();
+  virtual
+  ~mitkMaterialPropertyTest();
+};
+
+#endif /* MITKMATERIALPROPERTYTEST_H_ */

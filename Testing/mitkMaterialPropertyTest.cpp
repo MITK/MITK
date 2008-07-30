1ccfbd5c1bfaaa5358212bd42b85a08caf4c17b8
Anne Sauer <a.sauer@dkfz-heidelberg.de>
2008-07-30 15:37:31 +0000

    ENH:One test added

--- Testing/mitkMaterialPropertyTest.cpp
+++ Testing/mitkMaterialPropertyTest.cpp
@@ -22,7 +22,6 @@
 #include "mitkDataTreeNode.h"
 #include <mitkVtkPropRenderer.h>
 
-
 #include <iostream>
 
 /**
@@ -45,92 +44,111 @@ mitkMaterialPropertyTest(int /* argc */, char* /*argv*/[])
         {
           //static Pointer New(mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0)
           mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
-          mitk::BaseRenderer *renderer=NULL;
+          mitk::BaseRenderer *renderer = NULL;
           myMP = mitk::MaterialProperty::New(node, renderer);
           MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
           MITK_TEST_CONDITION( node == myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
           MITK_TEST_CONDITION( renderer == myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
         }
 
-       {
-       //static Pointer New( Color color, vtkFloatingPointType opacity = 1.0f, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0)
-       mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
-       mitk::BaseRenderer *renderer=NULL;
-       mitk::Color color;
-       color.Set(0, 0, 0);
-       vtkFloatingPointType opacity = 1.0f;
-       myMP = mitk::MaterialProperty::New(color, opacity, node, renderer);
-       MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
-       MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
-       MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
-       MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
-       MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
-       }
+        {
+          //static Pointer New( Color color, vtkFloatingPointType opacity = 1.0f, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0)
+          mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+          mitk::BaseRenderer *renderer = NULL;
+          mitk::Color color;
+          color.Set(0, 0, 0);
+          vtkFloatingPointType opacity = 1.0f;
+          myMP = mitk::MaterialProperty::New(color, opacity, node, renderer);
+          MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+          MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+          MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
+          MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
+          MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
+        }
 
-      {
-       //static Pointer New( vtkFloatingPointType red, vtkFloatingPointType green, vtkFloatingPointType blue, vtkFloatingPointType opacity = 1.0f, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0)
-       mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
-       mitk::BaseRenderer *renderer=NULL;
-       mitk::MaterialProperty::Color color;
-       color.Set(0, 0, 0);
-       vtkFloatingPointType opacity = 1.0f;
-       vtkFloatingPointType rgb = 0;
+        {
+          //static Pointer New( vtkFloatingPointType red, vtkFloatingPointType green, vtkFloatingPointType blue, vtkFloatingPointType opacity = 1.0f, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0)
+          mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+          mitk::BaseRenderer *renderer = NULL;
+          mitk::MaterialProperty::Color color;
+          color.Set(0, 0, 0);
+          vtkFloatingPointType opacity = 1.0f;
+          vtkFloatingPointType rgb = 0;
 
-       myMP = mitk::MaterialProperty::New(rgb, rgb, rgb, opacity, node, renderer);
-       MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
-       MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
-       MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
-       MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
-       MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
-       }
+          myMP = mitk::MaterialProperty::New(rgb, rgb, rgb, opacity, node,
+              renderer);
+          MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+          MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+          MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
+          MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
+          MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
+        }
 
-      {
-       //static Pointer New( vtkFloatingPointType red, vtkFloatingPointType green, vtkFloatingPointType blue, vtkFloatingPointType colorCoefficient,  vtkFloatingPointType specularCoefficient, vtkFloatingPointType specularPower, vtkFloatingPointType opacity, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0 )
-       mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
-       mitk::BaseRenderer *renderer=NULL;
-       mitk::MaterialProperty::Color color;
-       color.Set(0, 0, 0);
-       vtkFloatingPointType opacity = 1.0f;
-       vtkFloatingPointType rgb = 0;
-       vtkFloatingPointType colorCoefficient = 0;
-       vtkFloatingPointType specularCoefficient = 0;
-       vtkFloatingPointType specularPower = 0;
+        {
+          //static Pointer New( vtkFloatingPointType red, vtkFloatingPointType green, vtkFloatingPointType blue, vtkFloatingPointType colorCoefficient,  vtkFloatingPointType specularCoefficient, vtkFloatingPointType specularPower, vtkFloatingPointType opacity, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0 )
+          mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+          mitk::BaseRenderer *renderer = NULL;
+          mitk::MaterialProperty::Color color;
+          color.Set(0, 0, 0);
+          vtkFloatingPointType opacity = 1.0f;
+          vtkFloatingPointType rgb = 0;
+          vtkFloatingPointType colorCoefficient = 0;
+          vtkFloatingPointType specularCoefficient = 0;
+          vtkFloatingPointType specularPower = 0;
 
-       myMP = mitk::MaterialProperty::New(rgb, rgb, rgb, colorCoefficient, specularCoefficient, specularPower, opacity, node, renderer);
-       MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
-       MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
-       MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
-       MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
-       MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
-       MITK_TEST_CONDITION( specularCoefficient==myMP->GetSpecularCoefficient(), "Testing if a Coefficient object was set correctly" )
-       MITK_TEST_CONDITION( specularPower==myMP->GetSpecularPower(), "Testing if a SpecularPower object was set correctly" )
-       MITK_TEST_CONDITION( colorCoefficient==myMP->GetColorCoefficient(), "Testing if a colorCoefficient object was set correctly" )
+          myMP = mitk::MaterialProperty::New(rgb, rgb, rgb, colorCoefficient,
+              specularCoefficient, specularPower, opacity, node, renderer);
+          MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+          MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+          MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
+          MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
+          MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
+          MITK_TEST_CONDITION( specularCoefficient==myMP->GetSpecularCoefficient(), "Testing if a Coefficient object was set correctly" )
+          MITK_TEST_CONDITION( specularPower==myMP->GetSpecularPower(), "Testing if a SpecularPower object was set correctly" )
+          MITK_TEST_CONDITION( colorCoefficient==myMP->GetColorCoefficient(), "Testing if a colorCoefficient object was set correctly" )
 
-       }
+        }
+mitk::MaterialProperty::ConstPointer reference = NULL;
+        {
+          //static Pointer New( Color color, vtkFloatingPointType colorCoefficient, vtkFloatingPointType specularCoefficient, vtkFloatingPointType specularPower, vtkFloatingPointType opacity, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0 )
+          mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+          mitk::BaseRenderer *renderer = NULL;
+          mitk::MaterialProperty::Color color;
+          color.Set(0, 0, 0);
+          vtkFloatingPointType opacity = 1.0f;
+          vtkFloatingPointType rgb = 0;
+          vtkFloatingPointType colorCoefficient = 0;
+          vtkFloatingPointType specularCoefficient = 0;
+          vtkFloatingPointType specularPower = 0;
 
-      {
-       //static Pointer New( Color color, vtkFloatingPointType colorCoefficient, vtkFloatingPointType specularCoefficient, vtkFloatingPointType specularPower, vtkFloatingPointType opacity, mitk::DataTreeNode* node = 0, mitk::BaseRenderer* renderer = 0 )
-       mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
-       mitk::BaseRenderer *renderer=NULL;
-       mitk::MaterialProperty::Color color;
-       color.Set(0, 0, 0);
-       vtkFloatingPointType opacity = 1.0f;
-       vtkFloatingPointType rgb = 0;
-       vtkFloatingPointType colorCoefficient = 0;
-       vtkFloatingPointType specularCoefficient = 0;
-       vtkFloatingPointType specularPower = 0;
+          myMP = mitk::MaterialProperty::New(rgb, rgb, rgb, colorCoefficient,
+              specularCoefficient, specularPower, opacity, node, renderer);
+          reference = myMP;
+          MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+          MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+          MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
+          MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
+          MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
+          MITK_TEST_CONDITION( specularCoefficient==myMP->GetSpecularCoefficient(), "Testing if a Coefficient object was set correctly" )
+          MITK_TEST_CONDITION( specularPower==myMP->GetSpecularPower(), "Testing if a SpecularPower object was set correctly" )
 
-       myMP = mitk::MaterialProperty::New(rgb, rgb, rgb, colorCoefficient,
-       specularCoefficient, specularPower, opacity, node, renderer);
-       MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
-       MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
-       MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
-       MITK_TEST_CONDITION( color==myMP->GetColor(), "Testing if a Color object was set correctly" )
-       MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
-       MITK_TEST_CONDITION( specularCoefficient==myMP->GetSpecularCoefficient(), "Testing if a Coefficient object was set correctly" )
-       MITK_TEST_CONDITION( specularPower==myMP->GetSpecularPower(), "Testing if a SpecularPower object was set correctly" )
+        }
 
-       }
+        {
+          //static Pointer New( const MaterialProperty& property, vtkFloatingPointType red, vtkFloatingPointType green, vtkFloatingPointType blue, vtkFloatingPointType opacity = 1.0, std::string name = "" )
+          mitk::DataTreeNode::Pointer node = mitk::DataTreeNode::New();
+          mitk::BaseRenderer *renderer = NULL;
+          vtkFloatingPointType opacity = 1.0f;
+          vtkFloatingPointType rgb = 0;
+          std::string name = "Hans Wurst";
+
+          myMP = mitk::MaterialProperty::New(*reference.GetPointer(), rgb, rgb, rgb, opacity, name);
+          MITK_TEST_CONDITION_REQUIRED(myMP.IsNotNull(),"Testing instantiation")
+          MITK_TEST_CONDITION( node==myMP->GetDataTreeNode(), "Testing if a DataTreeNode object was set correctly" )
+          MITK_TEST_CONDITION( renderer==myMP->GetRenderer(), "Testing if a Renderer object was set correctly" )
+          MITK_TEST_CONDITION( opacity==myMP->GetOpacity(), "Testing if a Opacity object was set correctly" )
+          //MITK_TEST_CONDITION( name.compare(myMP->GetName(),0,9), "Testing if a Name object was set correctly" )
+        }
 
       // first test: did this work?
       // using MITK_TEST_CONDITION_REQUIRED makes the test stop after failure, since

51d11bee6accf2c8cabcc530cbb5ba295f143db4
Ivo Wolf <i.wolf@dkfz-heidelberg.de>
2008-08-07 16:59:58 +0000

    FIX (#1539): Improved mitk::SurfaceToImageMapperTest to test a case that did not work prior to the fix of bug #1537.

--- Testing/mitkSurfaceToImageFilterTest.cpp
+++ Testing/mitkSurfaceToImageFilterTest.cpp
@@ -17,32 +17,81 @@ PURPOSE.  See the above copyright notices for more information.
 
 
 #include "mitkSurfaceToImageFilter.h"
+#include "mitkDataTreeNodeFactory.h"
+#include "mitkPicFileWriter.h"
 
 #include <fstream>
 
-int mitkSurfaceToImageFilterTest(int /*argc*/, char* /*argv*/[])
+int mitkSurfaceToImageFilterTest(int argc, char* argv[])
 {
-  mitk::SurfaceToImageFilter::Pointer filter;
-  std::cout << "Testing mitk::Surface::New(): ";
-  filter = mitk::SurfaceToImageFilter::New();
-  if (filter.IsNull()) 
+  mitk::SurfaceToImageFilter::Pointer s2iFilter;
+  std::cout << "Testing mitk::Surface::New(): " << std::flush;
+  s2iFilter = mitk::SurfaceToImageFilter::New();
+  if (s2iFilter.IsNull()) 
   {
     std::cout<<"[FAILED]"<<std::endl;
     return EXIT_FAILURE;
   }
-  else {
-  std::cout<<"[PASSED]"<<std::endl;
+  else 
+  {
+    std::cout<<"[PASSED]"<<std::endl;
   } 
 
-  //std::cout << "Testing mitk::SurfaceToImageFilter::*TESTED_METHOD_DESCRIPTION: ";
-  //// METHOD_TEST_CODE
-  //if (filter.IsNull()) {
-  //  std::cout<<"[FAILED]"<<std::endl;
-  //  return EXIT_FAILURE;
-  //}
-  //else {
-  //std::cout<<"[PASSED]"<<std::endl;
-  //} 
+  std::cout << "Loading file: " << std::flush;
+  if(argc==0)
+  {
+    std::cout<<"no file specified [FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  mitk::Surface::Pointer surface = NULL;
+  mitk::DataTreeNodeFactory::Pointer factory = mitk::DataTreeNodeFactory::New();
+  try
+  {
+    std::cout<<argv[1]<<std::endl;
+    factory->SetFileName( argv[1] );
+    factory->Update();
+
+    if(factory->GetNumberOfOutputs()<1)
+    {
+      std::cout<<"file could not be loaded [FAILED]"<<std::endl;
+      return EXIT_FAILURE;
+    }
+    mitk::DataTreeNode::Pointer node = factory->GetOutput( 0 );
+    surface = dynamic_cast<mitk::Surface*>(node->GetData());
+    if(surface.IsNull())
+    {
+      std::cout<<"file not a surface - test will not be applied [PASSED]"<<std::endl;
+      std::cout<<"[TEST DONE]"<<std::endl;
+      return EXIT_SUCCESS;
+    }
+  }
+  catch ( itk::ExceptionObject & ex )
+  {
+    std::cout << "Exception: " << ex << "[FAILED]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "Testing creation of mitk::Image with same Geometry as Surface: " << std::flush;
+  mitk::Image::Pointer image = mitk::Image::New();
+  surface->UpdateOutputInformation(); //should not be necessary, bug #1536
+  image->Initialize(typeid(unsigned char), *surface->GetGeometry());
+
+  std::cout << "Testing mitk::SurfaceToImageFilter::SetInput(): " << std::flush;
+  s2iFilter->SetInput(surface);
+  std::cout<<"[PASSED]"<<std::endl;
+
+  std::cout << "Testing mitk::SurfaceToImageFilter::SetImage(): " << std::flush;
+  s2iFilter->SetImage(image);
+  std::cout<<"[PASSED]"<<std::endl;
+
+  std::cout << "Testing mitk::SurfaceToImageFilter::Update(): " << std::flush;
+  s2iFilter->Update();
+  std::cout<<"[PASSED]"<<std::endl;
+
+  mitk::PicFileWriter::Pointer picWriter = mitk::PicFileWriter::New();
+  picWriter->SetInput(s2iFilter->GetOutput());
+  picWriter->SetFileName("SurfaceToImageFilterTestOutput.pic");
+  picWriter->Write();
 
   std::cout<<"[TEST DONE]"<<std::endl;
   return EXIT_SUCCESS;

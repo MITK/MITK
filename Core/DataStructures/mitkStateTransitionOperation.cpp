9d486b54b9d5c0213357ec452861dfdc350584d1
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2008-08-20 13:14:45 +0000

    DOC: removing obsolete ModelIDs originally needed by RationalRose

--- Core/DataStructures/mitkStateTransitionOperation.cpp
+++ Core/DataStructures/mitkStateTransitionOperation.cpp
@@ -18,12 +18,10 @@ PURPOSE.  See the above copyright notices for more information.
 
 #include "mitkStateTransitionOperation.h"
 
-//##ModelId=3E78311303E7
 mitk::StateTransitionOperation::StateTransitionOperation(OperationType operationType, State* state)
 : mitk::Operation(operationType), m_State(state)
 {}
 
-//##ModelId=3E78307601D8
 mitk::State* mitk::StateTransitionOperation::GetState()
 {
 	return m_State;
--- Core/DataStructures/mitkStateTransitionOperation.h
+++ Core/DataStructures/mitkStateTransitionOperation.h
@@ -24,25 +24,21 @@ PURPOSE.  See the above copyright notices for more information.
 #include "mitkState.h"
 
 namespace mitk {
-//##ModelId=3E782FC201BB
-//##Documentation
-//## @brief operation, that changes the stateMachine and sets a state according to a
-//## transition to an other state.
-//##
-//## @ingroup Undo
-class MITK_CORE_EXPORT StateTransitionOperation : public Operation
-{
+  //##Documentation
+  //## @brief operation, that changes the stateMachine and sets a state according to a
+  //## transition to an other state.
+  //##
+  //## @ingroup Undo
+  class MITK_CORE_EXPORT StateTransitionOperation : public Operation
+  {
   public:
-	  //##ModelId=3E78311303E7
-      StateTransitionOperation(OperationType operationType, State* state);
+    StateTransitionOperation(OperationType operationType, State* state);
 
-	  //##ModelId=3E78307601D8
-	  State* GetState();
+    State* GetState();
 
   private:
-    //##ModelId=3E78305B00D5
     State* m_State;
 
-};
+  };
 }//namespace mitk
 #endif /* STATETRANSITIONOPERATION_H_HEADER_INCLUDED_C1695462 */

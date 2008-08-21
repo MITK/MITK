e7720ea2759e7289738d5b3a26bfb064f34b3379
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2008-08-21 14:54:50 +0000

    STYLE: Adapted the test to new TestMacros to clear up the file. Same behavior. Done as preparation for changes made for #869.

--- Testing/mitkInteractorTest.cpp
+++ Testing/mitkInteractorTest.cpp
@@ -20,45 +20,30 @@ PURPOSE.  See the above copyright notices for more information.
 #include <mitkEvent.h>
 #include <mitkStateEvent.h>
 #include <mitkInteractionConst.h>
+#include <iostream>
+#include "mitkTestingMacros.h"
 
-
-#include <fstream>
 int mitkInteractorTest(int /*argc*/, char* /*argv*/[])
 {
+  MITK_TEST_BEGIN("Interactor")
+
   //create interactor; use pattern InteractorTestPattern
   mitk::Interactor::Pointer interactor = mitk::Interactor::New("InteractorTestPattern", NULL);
 
   //load interactor
-  std::cout << "Testing to initialize the interactor: ";
-  if (interactor.IsNull())
-  {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
-  std::cout<<"[PASSED]"<<std::endl;
+  std::cout << "Testing to initialize the interactor";
+  MITK_TEST_CONDITION_REQUIRED(interactor.IsNotNull(),"Testing to initialize the interactor")
   
   std::cout<<"The pattern of the interactor is called: "<<interactor->GetType()<<std::endl;
-
-  std::cout << "Testing to send an event to go from State 0 to State 1: ";
+  
   //creating an event to go from state 0 to state 1:
   //<!-- middle MouseButton -->
   //<event NAME="middle MouseBN" ID="4" TYPE="Type_MouseButtonPress" BUTTON="BS_MidButton" BUTTONSTATE="0x0000" KEY="Key_none" />
   mitk::Event *event = new mitk::Event(NULL, mitk::Type_MouseButtonPress, mitk::BS_MidButton, mitk::BS_NoButton, mitk::Key_none);
   mitk::StateEvent *stateEvent = new mitk::StateEvent(4, event);
-  if (!interactor->HandleEvent(stateEvent))
-  {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
-  std::cout<<"[PASSED]"<<std::endl;
-
-  std::cout << "Testing right Mode and thus right action behavior: ";
-  if (interactor->GetMode() != mitk::Interactor::SMSELECTED)
-  {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
-  std::cout<<"[PASSED]"<<std::endl;
+  MITK_TEST_CONDITION_REQUIRED(interactor->HandleEvent(stateEvent),"Testing to send an event to go from State 0 to State 1")
+  
+  MITK_TEST_CONDITION_REQUIRED(interactor->GetMode() == mitk::Interactor::SMSELECTED,"Testing right Mode and thus right action behavior")
   
   //statemachine should have called transition "initmove", executed action with he id 9 and currentState should be 1 = "move"
   delete event;
@@ -68,21 +53,9 @@ int mitkInteractorTest(int /*argc*/, char* /*argv*/[])
   //<event NAME="middleBN+MouseMove" ID="533" TYPE="Type_MouseMove" BUTTON="BS_NoButton" BUTTONSTATE="0x0004" KEY="Key_none" />
   event = new mitk::Event(NULL, mitk::Type_MouseMove, mitk::BS_NoButton, mitk::BS_MidButton, mitk::Key_none);
   stateEvent->Set(533, event);
-  std::cout << "Staying in State 1: ";
-  if (!interactor->HandleEvent(stateEvent))
-  {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
-  std::cout<<"[PASSED]"<<std::endl;
+  MITK_TEST_CONDITION_REQUIRED(interactor->HandleEvent(stateEvent),"Staying in State 1")
 
-  std::cout << "Testing for the same Mode: ";
-  if (interactor->GetMode() != mitk::Interactor::SMSELECTED)
-  {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
-  std::cout<<"[PASSED]"<<std::endl;
+  MITK_TEST_CONDITION_REQUIRED(interactor->GetMode() == mitk::Interactor::SMSELECTED,"Testing for the same Mode")
   delete event;
 
   //create a new event
@@ -90,38 +63,18 @@ int mitkInteractorTest(int /*argc*/, char* /*argv*/[])
   //<event NAME="MiddleMouseRelease" ID="506" TYPE="Type_MouseButtonRelease" BUTTON="BS_MidButton" BUTTONSTATE="0x0004" KEY="Key_none" />
   event = new mitk::Event(NULL, mitk::Type_MouseButtonRelease, mitk::BS_MidButton, mitk::BS_MidButton, mitk::Key_none);
   stateEvent->Set(506, event);
-  std::cout << "Testing to call CalculateJurisdiction with next event: ";
   //because the statemachine is waiting for this event, 0.5 should be returned
   float returnvalue = interactor->CalculateJurisdiction(stateEvent);
-  if ( returnvalue != 0.5)
-  {
-    std::cout<<"[FAILED]"<<std::endl;
-    std::cout<<"CalculateJurisdiction returned: "<<returnvalue<<std::endl;
-    return EXIT_FAILURE;
-  }
-  std::cout<<"[PASSED]"<<std::endl;
+  MITK_TEST_CONDITION_REQUIRED(returnvalue == 0.5,"Testing to call CalculateJurisdiction with next event")
   std::cout<<"CalculateJurisdiction returned: "<<returnvalue<<std::endl;
 
-  std::cout << "Testing to send next event leading back to startState0: ";
   //The transition "finish" should call no action an lead into state 0 = startState 
-  if (!interactor->HandleEvent(stateEvent))
-  {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
-  std::cout<<"[PASSED]"<<std::endl;
+  MITK_TEST_CONDITION_REQUIRED(interactor->HandleEvent(stateEvent),"Testing to send next event leading back to startState0")
 
-  std::cout << "Testing right Mode and thus right action behavior: ";
-  if (interactor->GetMode() != mitk::Interactor::SMDESELECTED)
-  {
-    std::cout<<"[FAILED]"<<std::endl;
-    return EXIT_FAILURE;
-  }
-  std::cout<<"[PASSED]"<<std::endl;
+  MITK_TEST_CONDITION_REQUIRED(interactor->GetMode() == mitk::Interactor::SMDESELECTED,"Testing right Mode and thus right action behavior")
   delete event;
 
   delete stateEvent;
 
-  std::cout<<"[TEST DONE]"<<std::endl;
-  return EXIT_SUCCESS;
+  MITK_TEST_END()
 }

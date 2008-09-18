0cf1c542f1c64b82ac94c197abe6c295555a2dd0
Alexander Seitel <a.seitel@dkfz-heidelberg.de>
2008-09-18 10:10:18 +0000

    FIX (#1611): Added test for PointSetWriter

---
+++ Testing/mitkPointSetWriterTest.cpp
@@ -0,0 +1,75 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision: 7837 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "mitkPointSet.h"
+#include "mitkPointSetWriter.h"
+
+#include "mitkTestingMacros.h"
+
+#include <iostream>
+#include <time.h>
+
+/**
+ *  Simple example for a test for the (non-existent) class "ClassName".
+ *  
+ *  argc and argv are the command line parameters which were passed to 
+ *  the ADD_TEST command in the CMakeLists.txt file. For the automatic
+ *  tests, argv is either empty for the simple tests or contains the filename
+ *  of a test image for the image tests (see CMakeLists.txt).
+ */
+int mitkPointSetWriterTest(int /* argc */, char* /*argv*/[])
+{
+  // always start with this!
+  MITK_TEST_BEGIN("PointSetWriter")
+
+  // let's create an object of our class  
+  mitk::PointSetWriter::Pointer myPointSetWriter = mitk::PointSetWriter::New();
+  
+  // first test: did this work?
+  // using MITK_TEST_CONDITION_REQUIRED makes the test stop after failure, since
+  // it makes no sense to continue without an object.
+  MITK_TEST_CONDITION_REQUIRED(myPointSetWriter.IsNotNull(),"Testing instantiation") 
+
+  // write your own tests here and use the macros from mitkTestingMacros.h !!!
+  // do not write to std::cout and do not return from this function yourself!
+
+  // create pointSet
+  srand(time(NULL));
+  mitk::PointSet::Pointer pointSet = mitk::PointSet::New();
+  int numberOfPoints = rand()%100;
+  for (int i=0; i<=numberOfPoints+1;i++)
+  {
+    mitk::Point3D point;
+    point[0] = rand()%1000;
+    point[1] = rand()%1000;
+    point[2] = rand()%1000;
+    pointSet->SetPoint(i,point);
+  }
+
+  MITK_TEST_CONDITION_REQUIRED(pointSet.IsNotNull(),"PointSet creation")
+
+  // test for exception handling
+  MITK_TEST_FOR_EXCEPTION_BEGIN(std::exception)
+  myPointSetWriter->SetInput(pointSet);
+  myPointSetWriter->SetFileName("/tmp/not-existing");
+  myPointSetWriter->Update();
+  MITK_TEST_FOR_EXCEPTION_END(std::exception)
+  
+  // always end with this!
+  MITK_TEST_END()
+}
+

98f6f898dc0fd03e1473ae49bf03b37c1bae6e8a
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2008-11-06 19:59:47 +0000

    ENH (#1356): svn cleanup

--- Bundles/org.mitk.gui.qt.datamanager/src/mitkQtStdViewsDll.h
+++ Bundles/org.mitk.gui.qt.datamanager/src/mitkQtStdViewsDll.h
@@ -1,41 +1,41 @@
 /*=========================================================================
- 
+
 Program:   openCherry Platform
 Language:  C++
 Date:      $Date$
 Version:   $Revision$
- 
+
 Copyright (c) German Cancer Research Center, Division of Medical and
 Biological Informatics. All rights reserved.
 See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
- 
+
 This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
- 
+
 =========================================================================*/
 
-#ifndef MITKQTDATAMANAGERDLL_H_
-#define MITKQTDATAMANAGERDLL_H_
+#ifndef MITKQTSTDVIEWSDLL_H_
+#define MITKQTSTDVIEWSDLL_H_
 
 //
-// The following block is the standard way of creating macros which make exporting 
+// The following block is the standard way of creating macros which make exporting
 // from a DLL simpler. All files within this DLL are compiled with the org_mitk_gui_qt_application_EXPORTS
 // symbol defined on the command line. this symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see 
+// that uses this DLL. This way any other project whose source files include this file see
 // MITK_QT_APP functions as being imported from a DLL, wheras this DLL sees symbols
 // defined with this macro as being exported.
 //
 #if defined(_WIN32) && !defined(CHERRY_STATIC)
-  #if defined(org_mitk_gui_qt_datamanager_EXPORTS)
-    #define MITK_QT_DATAMANAGER __declspec(dllexport)
+  #if defined(org_mitk_gui_qt_stdviews_EXPORTS)
+    #define MITK_QT_STDVIEWS __declspec(dllexport)
   #else
-    #define MITK_QT_DATAMANAGER __declspec(dllimport)  
+    #define MITK_QT_STDVIEWS __declspec(dllimport)
   #endif
 #endif
 
-#if !defined(MITK_QT_DATAMANAGER)
-  #define MITK_QT_DATAMANAGER
+#if !defined(MITK_QT_STDVIEWS)
+  #define MITK_QT_STDVIEWS
 #endif
 
-#endif /*MITKQTDATAMANAGERDLL_H_*/
+#endif /*MITKQTSTDVIEWSDLL_H_*/

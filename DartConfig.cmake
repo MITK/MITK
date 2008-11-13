a669cdad681fe6b4f2e7b4dcb4ec4cd001f46749
Daniel Maleike <d.maleike@dkfz-heidelberg.de>
2008-11-13 13:23:32 +0000

    FIX (#1731): Allow my.cdash for external dart clients

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -8,27 +8,27 @@ SET (DROP_LOCATION "MITK-public")
 SET (NIGHTLY_START_TIME "21:00:00 CET")	
 SET (DART_TESTING_TIMEOUT 900)
 
-IF(FALSE)
-
 # configure dart for submission to kitware
 IF(USE_KITWARE_DARTBOARD)
-	#
-	# Dart server to submit results (used by client)
-	#
-	SET (DROP_METHOD http)
-	SET (DROP_SITE "public.kitware.com")
-	SET (DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
-	SET (DROP_SITE_USER "ftpuser")
-	SET (DROP_SITE_PASSWORD "public")
-	SET (TRIGGER_SITE "http://public.kitware.com/cgi-bin/Submit-MITK-TestingResults.pl")
-	SET (ROLLUP_URL "http://${DROP_SITE}/cgi-bin/MITK-rollup-dashboard.sh")
-        SET (NIGHTLY_START_TIME "21:00:00 EST")	
-	#
-        # SET (CVS_WEB_URL "http://mbi-srv/docs/programming/cvs/viewcvs/cgi/viewcvs.cgi/ip%2b%2b/")
-	# SET (DOXYGEN_URL "http://mbi-srv.inet.dkfz-heidelberg.de/docs/image_processing/mitk/doxygen/" )
+#
+# Dart server to submit results (used by client)
+#
+    SET(DROP_METHOD "http")
+    SET(DROP_SITE "my.cdash.org")
+    SET(DROP_LOCATION "/submit.php?project=MITK")
+
+    SET(CTEST_DROP_METHOD "http")
+    SET(CTEST_DROP_SITE "my.cdash.org")
+    SET(CTEST_DROP_LOCATION "/submit.php?project=MITK")
+    SET(CTEST_DROP_SITE_CDASH TRUE)
+
+    SET(CTEST_NIGHTLY_START_TIME "21:00 CET")
+    SET (NIGHTLY_START_TIME "21:00:00 CET")	
+#
+#    SET (CVS_WEB_URL "http://mbi-srv/docs/programming/cvs/viewcvs/cgi/viewcvs.cgi/ip%2b%2b/")
+#    SET (DOXYGEN_URL "http://mbi-srv.inet.dkfz-heidelberg.de/docs/image_processing/mitk/doxygen/" )
         
 ENDIF(USE_KITWARE_DARTBOARD)
-ENDIF(FALSE)
 
 IF(EXISTS ${MBI_DART_WORKDIR}/notes.txt)
   MAKE_DIRECTORY(${MBI_DART_WORKDIR}/bin/Testing/Notes)

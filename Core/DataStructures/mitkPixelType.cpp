649c2cd7a2f3bec09e4530993892150d2fa0b721
Klaus Fritzsche <k.fritzsche@dkfz-heidelberg.de>
2008-12-04 14:36:18 +0000

    ENH (#1691): enabling itk::vector as pixel type

--- Core/DataStructures/mitkPixelType.cpp
+++ Core/DataStructures/mitkPixelType.cpp
@@ -24,6 +24,47 @@ PURPOSE.  See the above copyright notices for more information.
 #include "itkDiffusionTensor3D.h"
 #include "itkConfidenceDiffusionTensor3D.h"
 
+#define HUNDRED_VECS(HUN)   \
+  TEN_VECS(HUN)                \
+  TEN_VECS(HUN+10)             \
+  TEN_VECS(HUN+20)             \
+  TEN_VECS(HUN+30)             \
+  TEN_VECS(HUN+40)             \
+  TEN_VECS(HUN+50)             \
+  TEN_VECS(HUN+60)             \
+  TEN_VECS(HUN+70)             \
+  TEN_VECS(HUN+80)             \
+  TEN_VECS(HUN+90)             \
+
+#define TEN_VECS(TEN)   \
+  if(false){}               \
+  N_VEC(TEN+ 1)                \
+  N_VEC(TEN+ 2)             \
+  N_VEC(TEN+ 3)             \
+  N_VEC(TEN+ 4)             \
+  N_VEC(TEN+ 5)             \
+  N_VEC(TEN+ 6)             \
+  N_VEC(TEN+ 7)             \
+  N_VEC(TEN+ 8)             \
+  N_VEC(TEN+ 9)             \
+  N_VEC(TEN+10)             \
+
+#define N_VEC(N_DIRS)       \
+  _N_VEC(N_DIRS,double)     \
+  _N_VEC(N_DIRS,float)      \
+
+#define _N_VEC(N_DIRS,PIXTYPE)                                                          \
+  else if ( *m_TypeId == typeid( itk::Vector<PIXTYPE,N_DIRS> ))  \
+  {                                                                                     \
+  found = true;                                                                      \
+  m_TypeId = & typeid( PIXTYPE );                                                    \
+  m_NumberOfComponents *= N_DIRS;                                                \
+  m_Type = ipPicFloat;                                                               \
+  m_Bpe = sizeof(PIXTYPE) * 8 * m_NumberOfComponents;                                \
+  m_ItkTypeId = &typeid( itk::Vector<PIXTYPE,N_DIRS> );     \
+  }                                                                                     \
+
+
 mitk::PixelType::PixelType() : m_TypeId( NULL ), m_Type( ipPicUnknown ), m_Bpe( 0 ), m_NumberOfComponents( 1 )
 {
 
@@ -115,7 +156,7 @@ bool mitk::PixelType::operator!=(const std::type_info& typeId) const
     SET_ITK_TYPE_ID(SCALAR, 1, TYPE ) else                                            \
                                                                                       \
     SET_ITK_TYPE_ID(RGB, 3, itk::RGBPixel<TYPE> ) else                                \
-    SET_ITK_TYPE_ID(DIFFUSIONTENSOR3D, 6, itk::DiffusionTensor3D<TYPE> ) else                                \
+    SET_ITK_TYPE_ID(DIFFUSIONTENSOR3D, 6, itk::DiffusionTensor3D<TYPE> ) else         \
     SET_ITK_TYPE_ID(VECTOR, 3, Vector3Type ) else                                     \
     SET_ITK_TYPE_ID(COVARIANTVECTOR, 3, CovariantVector3Type ) else                   \
     SET_ITK_TYPE_ID(POINT, 3, Point3Type ) else                                       \
@@ -146,39 +187,7 @@ void mitk::PixelType::Initialize( const std::type_info& aTypeId, int numberOfCom
    SET_TYPE(unsigned short, ipPicUInt)
    SET_TYPE(char, ipPicInt)
    SET_TYPE(unsigned char, ipPicUInt)
-   if ( *m_TypeId == typeid( itk::Vector<double,3> ) )
-   {
-      m_TypeId = & typeid( double );
-      m_NumberOfComponents *= 3;
-      m_Type = ipPicFloat;
-      m_Bpe = sizeof(double) * 8 * m_NumberOfComponents;
-      m_ItkTypeId = &typeid( itk::Vector<double,3> );
-   }
-   else if ( *m_TypeId == typeid( itk::Vector<float,3> ) )
-   {
-      m_TypeId = & typeid( float );
-      m_NumberOfComponents *= 3;
-      m_Type = ipPicFloat;
-      m_Bpe = sizeof(float) * 8 * m_NumberOfComponents;
-      m_ItkTypeId = &typeid( itk::Vector<float,3> );
-   }
-   else if ( *m_TypeId == typeid( itk::Vector<double,2> ) )
-   {
-      m_TypeId = & typeid( double );
-      m_NumberOfComponents *= 2;
-      m_Type = ipPicFloat;
-      m_Bpe = sizeof(double) * 8 * m_NumberOfComponents;
-      m_ItkTypeId = &typeid( itk::Vector<double,2> );
-   }
-   else if ( *m_TypeId == typeid( itk::Vector<float,2> ) )
-   {
-     m_TypeId = & typeid( float );
-     m_NumberOfComponents *= 2;
-     m_Type = ipPicFloat;
-     m_Bpe = sizeof(float) * 8 * m_NumberOfComponents;
-     m_ItkTypeId = &typeid( itk::Vector<float,2> );
-   }
-   else if ( *m_TypeId == typeid( itk::DiffusionTensor3D<float> ) )
+   if ( *m_TypeId == typeid( itk::DiffusionTensor3D<float> ) )
    {
      m_TypeId = & typeid( float );
      m_NumberOfComponents *= 6;
@@ -212,18 +221,47 @@ void mitk::PixelType::Initialize( const std::type_info& aTypeId, int numberOfCom
    }
    else if ( *m_TypeId == typeid( itk::RGBPixel<unsigned char> ) )
    {
-      m_Type = ipPicUInt;
-      m_NumberOfComponents = 3;
-      m_Bpe = sizeof(unsigned char) * 8 * m_NumberOfComponents;
-      m_ItkTypeId = &typeid( itk::RGBPixel<unsigned char> );
+     m_Type = ipPicUInt;
+     m_NumberOfComponents = 3;
+     m_Bpe = sizeof(unsigned char) * 8 * m_NumberOfComponents;
+     m_ItkTypeId = &typeid( itk::RGBPixel<unsigned char> );
    }
    else if ( *m_TypeId == typeid( itk::RGBAPixel<unsigned char> ) )
    {
-      itkExceptionMacro( "Pixel type currently not supported." );
+     itkExceptionMacro( "Pixel type currently not supported." );
    }
    else
    {
-      itkExceptionMacro( "Pixel type currently not supported." );
+     bool found = false;
+     
+     // the following lines allow for fixed-size vector images up to a certain size limit
+     // (commented out for shorter compile times)
+     //HUNDRED_VECS(000)
+     //HUNDRED_VECS(100)
+     //HUNDRED_VECS(200)
+     //HUNDRED_VECS(300)
+     //HUNDRED_VECS(400)
+     //HUNDRED_VECS(500)
+     //HUNDRED_VECS(600)
+     //HUNDRED_VECS(700)
+
+     // allow for fixed-size vectors of specific length
+     // (inspired by itkPointshell.cpp, precompiled q-ball configs)
+     TEN_VECS(0)
+     N_VEC(11)
+     N_VEC(12)
+     N_VEC(42)
+     N_VEC(92)
+     N_VEC(162)
+     N_VEC(252)
+     N_VEC(362)
+     N_VEC(492)
+     N_VEC(642)
+     N_VEC(812)
+     N_VEC(1002)
+
+     if(!found)
+       itkExceptionMacro( "Pixel type currently not supported." );
    }
    m_BitsPerComponent = m_Bpe / m_NumberOfComponents;
 }

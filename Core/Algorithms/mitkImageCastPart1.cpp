64f09f6b5709752833c9c59b8656eaa3cbe3430c
Klaus Fritzsche <k.fritzsche@dkfz-heidelberg.de>
2008-12-04 14:26:41 +0000

    ENH (#1691): enabling itk::vector as pixel type

--- Core/Algorithms/mitkImageCastPart1.cpp
+++ Core/Algorithms/mitkImageCastPart1.cpp
@@ -98,4 +98,59 @@ template void MITK_CORE_EXPORT _CastToItkImage2Access(itkImageDTIF3*,  itk::Smar
 template void MITK_CORE_EXPORT _CastToItkImage2Access(itkImageDTID3*,  itk::SmartPointer<itkImageDTID3>&);
 template void MITK_CORE_EXPORT _CastToItkImage2Access(itkImageCDTIF3*,  itk::SmartPointer<itkImageCDTIF3>&);
 template void MITK_CORE_EXPORT _CastToItkImage2Access(itkImageCDTID3*,  itk::SmartPointer<itkImageCDTID3>&);
+
+#define CAST_HUNDRED_VECS(HUN)   \
+  CAST_TEN_VECS(HUN)              \
+  CAST_TEN_VECS(HUN+10)              \
+  CAST_TEN_VECS(HUN+20)              \
+  CAST_TEN_VECS(HUN+30)              \
+  CAST_TEN_VECS(HUN+40)              \
+  CAST_TEN_VECS(HUN+50)              \
+  CAST_TEN_VECS(HUN+60)              \
+  CAST_TEN_VECS(HUN+70)              \
+  CAST_TEN_VECS(HUN+80)              \
+  CAST_TEN_VECS(HUN+90)              \
+
+#define CAST_TEN_VECS(TEN)   \
+  CAST_N_VEC(TEN+ 1)              \
+  CAST_N_VEC(TEN+ 2)              \
+  CAST_N_VEC(TEN+ 3)              \
+  CAST_N_VEC(TEN+ 4)              \
+  CAST_N_VEC(TEN+ 5)              \
+  CAST_N_VEC(TEN+ 6)              \
+  CAST_N_VEC(TEN+ 7)              \
+  CAST_N_VEC(TEN+ 8)              \
+  CAST_N_VEC(TEN+ 9)              \
+  CAST_N_VEC(TEN+10)              \
+
+#define CAST_N_VEC(N_DIRS)                                                                   \
+  _CAST_N_VEC(N_DIRS,double)                                                                 \
+  _CAST_N_VEC(N_DIRS,float)                                                                  \
+
+#define _CAST_N_VEC(N_DIRS,PIXTYPE)                                                                         \
+  template void MITK_CORE_EXPORT _CastToItkImage2Access(itk::Image<itk::Vector<PIXTYPE,N_DIRS>, 2> *,  itk::SmartPointer<itk::Image<itk::Vector<PIXTYPE,N_DIRS>, 2> >&);  \
+  template void MITK_CORE_EXPORT _CastToItkImage2Access(itk::Image<itk::Vector<PIXTYPE,N_DIRS>, 3> *,  itk::SmartPointer<itk::Image<itk::Vector<PIXTYPE,N_DIRS>, 3> >&);  \
+
+// the following lines allow for fixed-size vector images up to a certain size limit
+// (commented out for shorter compile times)
+//CAST_HUNDRED_VECS(0)
+//CAST_HUNDRED_VECS(100)
+//CAST_HUNDRED_VECS(200)
+//CAST_HUNDRED_VECS(300)
+
+// allow for fixed-size vectors of specific length
+// (inspired by itkPointshell.cpp, precompiled q-ball configs)
+CAST_TEN_VECS(0)
+CAST_N_VEC(11)
+CAST_N_VEC(12)
+CAST_N_VEC(42)
+CAST_N_VEC(92)
+CAST_N_VEC(162)
+CAST_N_VEC(252)
+CAST_N_VEC(362)
+CAST_N_VEC(492)
+CAST_N_VEC(642)
+CAST_N_VEC(812)
+CAST_N_VEC(1002)
+
 }
--- Core/Algorithms/mitkImageCastPart2.cpp
+++ Core/Algorithms/mitkImageCastPart2.cpp
@@ -24,6 +24,7 @@ PURPOSE.  See the above copyright notices for more information.
 
 namespace mitk 
 {
+
 #ifndef DOXYGEN_SKIP
   template <typename ItkOutputImageType> void CastToItkImage(const mitk::Image * mitkImage, itk::SmartPointer<ItkOutputImageType>& itkOutputImage)
   {
@@ -67,27 +68,27 @@ template void MITK_CORE_EXPORT CastToItkImage(const mitk::Image *, itk::SmartPoi
 template void MITK_CORE_EXPORT CastToItkImage(const mitk::Image *, itk::SmartPointer<itkImageUS2>&);
 template void MITK_CORE_EXPORT CastToItkImage(const mitk::Image *, itk::SmartPointer<itkImageSC2>&);
 template void MITK_CORE_EXPORT CastToItkImage(const mitk::Image *, itk::SmartPointer<itkImageUC2>&);
-template <> void CastToItkImage<itkImageRGBUC2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageRGBUC2>& itkOutputImage)
+template <> void MITK_CORE_EXPORT CastToItkImage<itkImageRGBUC2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageRGBUC2>& itkOutputImage)
 {
   typedef itkImageRGBUC2 ItkOutputImageType;
   AccessFixedTypeByItk_1(mitkImage, _CastToItkImage2Access, itk::RGBPixel<unsigned char>, ::itk::GetImageDimension<ItkOutputImageType>::ImageDimension, itkOutputImage);
 }
-template <> void CastToItkImage<itkImageDTIF2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageDTIF2>& itkOutputImage)
+template <> void MITK_CORE_EXPORT CastToItkImage<itkImageDTIF2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageDTIF2>& itkOutputImage)
 {
   typedef itkImageDTIF2 ItkOutputImageType;
   AccessFixedTypeByItk_1(mitkImage, _CastToItkImage2Access, itk::DiffusionTensor3D<float>, ::itk::GetImageDimension<ItkOutputImageType>::ImageDimension, itkOutputImage);
 }
-template <> void CastToItkImage<itkImageDTID2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageDTID2>& itkOutputImage)
+template <> void MITK_CORE_EXPORT CastToItkImage<itkImageDTID2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageDTID2>& itkOutputImage)
 {
   typedef itkImageDTID2 ItkOutputImageType;
   AccessFixedTypeByItk_1(mitkImage, _CastToItkImage2Access, itk::DiffusionTensor3D<double>, ::itk::GetImageDimension<ItkOutputImageType>::ImageDimension, itkOutputImage);
 }
-template <> void CastToItkImage<itkImageCDTIF2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageCDTIF2>& itkOutputImage)
+template <> void MITK_CORE_EXPORT CastToItkImage<itkImageCDTIF2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageCDTIF2>& itkOutputImage)
 {
   typedef itkImageCDTIF2 ItkOutputImageType;
   AccessFixedTypeByItk_1(mitkImage, _CastToItkImage2Access, itk::ConfidenceDiffusionTensor3D<float>, ::itk::GetImageDimension<ItkOutputImageType>::ImageDimension, itkOutputImage);
 }
-template <> void CastToItkImage<itkImageCDTID2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageCDTID2>& itkOutputImage)
+template <> void MITK_CORE_EXPORT CastToItkImage<itkImageCDTID2>(const mitk::Image * mitkImage, itk::SmartPointer<itkImageCDTID2>& itkOutputImage)
 {
   typedef itkImageCDTID2 ItkOutputImageType;
   AccessFixedTypeByItk_1(mitkImage, _CastToItkImage2Access, itk::ConfidenceDiffusionTensor3D<double>, ::itk::GetImageDimension<ItkOutputImageType>::ImageDimension, itkOutputImage);
@@ -127,6 +128,73 @@ template <> void MITK_CORE_EXPORT CastToItkImage<itkImageCDTID3>(const mitk::Ima
   AccessFixedTypeByItk_1(mitkImage, _CastToItkImage2Access, itk::ConfidenceDiffusionTensor3D<double>, ::itk::GetImageDimension<ItkOutputImageType>::ImageDimension, itkOutputImage);
 }
 
+#define TYPE_VECS(HUN)        \
+  TYPE_TEN_VECS(HUN)        \
+  TYPE_TEN_VECS(HUN + 10)        \
+  TYPE_TEN_VECS(HUN + 20)        \
+  TYPE_TEN_VECS(HUN + 30)        \
+  TYPE_TEN_VECS(HUN + 40)        \
+  TYPE_TEN_VECS(HUN + 50)        \
+  TYPE_TEN_VECS(HUN + 60)        \
+  TYPE_TEN_VECS(HUN + 70)        \
+  TYPE_TEN_VECS(HUN + 80)        \
+  TYPE_TEN_VECS(HUN + 90)        \
+  
+#define TYPE_TEN_VECS(HUN)        \
+  TYPE_N_VEC(HUN +  1)         \
+  TYPE_N_VEC(HUN +  2)        \
+  TYPE_N_VEC(HUN +  3)        \
+  TYPE_N_VEC(HUN +  4)        \
+  TYPE_N_VEC(HUN +  5)        \
+  TYPE_N_VEC(HUN +  6)        \
+  TYPE_N_VEC(HUN +  7)        \
+  TYPE_N_VEC(HUN +  8)        \
+  TYPE_N_VEC(HUN +  9)        \
+  TYPE_N_VEC(HUN + 10)        \
+
+#define TYPE_N_VEC(N_DIRS)                                                                   \
+  _TYPE_N_VEC(N_DIRS,double)                                                                 \
+  _TYPE_N_VEC(N_DIRS,float)                                                                  \
+
+#define _TYPE_N_VEC(N_DIRS,PIXTYPE)                                                                                                                                                                \
+  template <> void MITK_CORE_EXPORT CastToItkImage<itk::Image<itk::Vector<PIXTYPE,N_DIRS>, 2> >(const mitk::Image * mitkImage, itk::SmartPointer<itk::Image<itk::Vector<PIXTYPE,N_DIRS>, 2> >& itkOutputImage)                                       \
+{                                                                                                                                                                                                \
+  typedef itk::Vector<PIXTYPE,N_DIRS> VECTORTYPE;                                                                                                                           \
+  typedef itk::Image<VECTORTYPE, 2>  ItkOutputImageType2;                                                                                                                                               \
+  AccessFixedTypeByItk_1(mitkImage, _CastToItkImage2Access, VECTORTYPE, ::itk::GetImageDimension<ItkOutputImageType2>::ImageDimension, itkOutputImage);                                               \
+}                                                                                                                                                                                                \
+  template <> void MITK_CORE_EXPORT CastToItkImage<itk::Image<itk::Vector<PIXTYPE,N_DIRS>, 3> >(const mitk::Image * mitkImage, itk::SmartPointer<itk::Image<itk::Vector<PIXTYPE,N_DIRS>, 3> >& itkOutputImage)                                       \
+{                                                                                                                                                                                                \
+  typedef itk::Vector<PIXTYPE,N_DIRS> VECTORTYPE;                                                                                                                           \
+  typedef itk::Image<VECTORTYPE, 3>  ItkOutputImageType3;                                                                                                                                               \
+  AccessFixedTypeByItk_1(mitkImage, _CastToItkImage2Access, VECTORTYPE, ::itk::GetImageDimension<ItkOutputImageType3>::ImageDimension, itkOutputImage);                                               \
+}                                                                                                                                                                                                \
+
+// the following lines allow for fixed-size vector images up to a certain size limit
+// (commented out for shorter compile times)
+//TYPE_VECS(000)
+//TYPE_VECS(100)
+//TYPE_VECS(200)
+//TYPE_VECS(300)
+//TYPE_VECS(400)
+//TYPE_VECS(500)
+//TYPE_VECS(600)
+//TYPE_VECS(700)
+
+// allow for fixed-size vectors of specific length
+// (inspired by itkPointshell.cpp, precompiled q-ball configs)
+TYPE_TEN_VECS(0)
+TYPE_N_VEC(11)
+TYPE_N_VEC(12)
+TYPE_N_VEC(42)
+TYPE_N_VEC(92)
+TYPE_N_VEC(162)
+TYPE_N_VEC(252)
+TYPE_N_VEC(362)
+TYPE_N_VEC(492)
+TYPE_N_VEC(642)
+TYPE_N_VEC(812)
+TYPE_N_VEC(1002)
 
 #ifndef DOXYGEN_SKIP
 //// Extension for RGB (and maybe also for vector types)

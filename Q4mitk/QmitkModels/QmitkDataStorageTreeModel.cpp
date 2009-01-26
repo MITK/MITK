e6e3e54d37f31e3bc4a3d7b68e522306148edabc
Klaus Fritzsche <k.fritzsche@dkfz-heidelberg.de>
2009-01-26 11:54:47 +0000

    ADD (#581): QT4 model

---
+++ Q4mitk/QmitkModels/QmitkDataStorageTreeModel.cpp
@@ -0,0 +1,186 @@
+/*=========================================================================
+
+ Program:   MITK
+ Language:  C++
+ Date:      $Date: 2008-08-13 16:56:36 +0200 (Mi, 13 Aug 2008) $
+ Version:   $Revision: 14972 $
+
+ Copyright (c) German Cancer Research Center, Division of Medical and
+ Biological Informatics. All rights reserved.
+ See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+
+ =========================================================================*/
+
+#include "QmitkDataStorageTreeModel.h"
+
+#include <mitkStringProperty.h>
+#include "mitkNodePredicateFirstLevel.h"
+#include "mitkNodePredicateAND.h"
+
+QmitkDataStorageTreeModel::QmitkDataStorageTreeModel(mitk::NodePredicateBase* pred, QObject* parent)
+ : QAbstractItemModel(parent), m_NodePredicate(pred), m_ViewMode(DSVW_Hirarchy)
+{
+  SetDataStorage(mitk::DataStorage::GetInstance());
+}
+
+QmitkDataStorageTreeModel::~QmitkDataStorageTreeModel()
+{
+  if (m_NodePredicate) delete m_NodePredicate;
+}
+
+void QmitkDataStorageTreeModel::SetDataStorage(mitk::DataStorage::Pointer dataStorage)
+{
+  m_DataStorage = dataStorage;
+
+  this->reset();
+}
+
+Qt::ItemFlags QmitkDataStorageTreeModel::flags(const QModelIndex& /*index*/) const
+{
+  return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+  //ItemIsSelectable = 1,
+  //ItemIsEditable = 2,
+  //ItemIsDragEnabled = 4,
+  //ItemIsDropEnabled = 8,
+  //ItemIsUserCheckable = 16,
+  //ItemIsEnabled = 32,
+  //ItemIsTristate = 64
+}
+
+QVariant QmitkDataStorageTreeModel::data(const QModelIndex& index, int role) const
+{
+	if (!index.isValid() && index.internalPointer())
+		return QVariant();
+
+	mitk::DataTreeNode::Pointer node = static_cast<mitk::DataTreeNode*>(index.internalPointer());
+
+	switch (role)
+	{
+	case Qt::DisplayRole:
+		{
+			std::string name = node->GetName();
+			return QVariant(QString::fromStdString(name));
+		}
+		break;
+	case QmitkDataTreeNodeRole:
+		{
+			return QVariant::fromValue(node);
+		}
+		break;
+	default:
+		return QVariant();
+	}
+return 0;
+}
+
+QVariant QmitkDataStorageTreeModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+  return QVariant("Scene Data");
+}
+
+int QmitkDataStorageTreeModel::rowCount(const QModelIndex& parent) const
+{
+
+	if(!parent.isValid())
+	{
+		if(m_NodePredicate)
+		{
+			mitk::NodePredicateFirstLevel p1(m_DataStorage);
+			mitk::NodePredicateAND p2(p1,*m_NodePredicate);
+			return m_DataStorage->GetSubset(p2)->Size();
+		}
+		else
+		{
+			mitk::NodePredicateFirstLevel p1(m_DataStorage);
+			int num  = m_DataStorage->GetSubset(p1)->Size();
+			return m_DataStorage->GetSubset(p1)->Size();
+		}
+	}
+	else
+	{
+		mitk::DataTreeNode::Pointer node = static_cast<mitk::DataTreeNode*>(parent.internalPointer());
+		return m_DataStorage->GetDerivations(node, m_NodePredicate, true)->Size();
+	}
+}
+
+QModelIndex QmitkDataStorageTreeModel::parent ( const QModelIndex & index ) const
+{
+	if (index.isValid())
+	{
+		mitk::DataTreeNode::Pointer node = 
+			data(index, QmitkDataTreeNodeRole).value<mitk::DataTreeNode::Pointer>();
+			
+		if (m_DataStorage->GetSources(node, m_NodePredicate, true)->Size())
+			return createIndex(0, 0, m_DataStorage->GetSources(node, m_NodePredicate, true)->GetElement(0));
+		else
+			return QModelIndex();
+	}
+
+	return QModelIndex();
+}
+
+QModelIndex QmitkDataStorageTreeModel::index ( int row, int column, const QModelIndex & parent ) const
+{
+	if (!hasIndex(row, column, parent))
+		return QModelIndex();
+	
+	if (parent.isValid())
+	{
+		mitk::DataTreeNode::Pointer node = 
+			static_cast<mitk::DataTreeNode*>(parent.internalPointer());
+		if (m_DataStorage->GetDerivations(node, m_NodePredicate, true)->GetElement(row))
+			return createIndex(row, column, m_DataStorage->GetDerivations(node, m_NodePredicate, true)->GetElement(row));
+		else
+			return QModelIndex();
+	}
+	else
+	{
+		if(m_NodePredicate)
+		{
+			mitk::NodePredicateFirstLevel p1(m_DataStorage);
+			mitk::NodePredicateAND p2(p1,*m_NodePredicate);
+			if (m_DataStorage->GetSubset(p2)->GetElement(row))
+				return createIndex(row, column, m_DataStorage->GetSubset(p2)->GetElement(row));
+		}
+		else
+		{
+			mitk::NodePredicateFirstLevel p1(m_DataStorage);
+			if (m_DataStorage->GetSubset(p1)->Size() > row)
+			{
+				int num = m_DataStorage->GetSubset(p1)->Size();
+				return createIndex(row, column, m_DataStorage->GetSubset(p1)->GetElement(row));
+			}
+		}
+
+		return QModelIndex();
+	}
+}
+
+bool QmitkDataStorageTreeModel::hasChildren ( const QModelIndex & parent ) const
+{
+	if (parent.isValid())
+	{
+		mitk::DataTreeNode::Pointer node = static_cast<mitk::DataTreeNode*>(parent.internalPointer());
+		return m_DataStorage->GetDerivations(node, m_NodePredicate, true)->Size() > 0;
+	}
+	if(m_NodePredicate)
+	{
+		mitk::NodePredicateFirstLevel p1(m_DataStorage);
+		mitk::NodePredicateAND p2(p1,*m_NodePredicate);
+		return m_DataStorage->GetSubset(p2)->Size();
+	}
+	else
+	{
+		mitk::NodePredicateFirstLevel p1(m_DataStorage);
+		return m_DataStorage->GetSubset(p1)->Size();
+	}
+}
+
+int QmitkDataStorageTreeModel::columnCount ( const QModelIndex & parent ) const
+{
+	return 1;
+}
---
+++ Q4mitk/QmitkModels/QmitkDataStorageTreeModel.h
@@ -0,0 +1,67 @@
+/*=========================================================================
+
+ Program:   MITK
+ Language:  C++
+ Date:      $Date: 2008-08-13 16:56:36 +0200 (Mi, 13 Aug 2008) $
+ Version:   $Revision: 14972 $
+
+ Copyright (c) German Cancer Research Center, Division of Medical and
+ Biological Informatics. All rights reserved.
+ See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+
+ =========================================================================*/
+
+
+#ifndef QMITKDATASTORAGETREEMODEL_H_
+#define QMITKDATASTORAGETREEMODEL_H_
+
+#include <QAbstractListModel>
+
+#include <mitkDataStorage.h>
+#include <mitkNodePredicateBase.h>
+
+#include <QmitkEnums.h>
+#include <QmitkCustomVariants.h>
+
+class QMITK_EXPORT QmitkDataStorageTreeModel : public QAbstractItemModel
+{
+public:
+
+  enum DataStorageViewMode
+  {
+    DSVW_DatatypeBased,
+	DSVW_Hirarchy
+  };
+
+  /**
+   * The NodePredicate is owned by the model
+   */
+  QmitkDataStorageTreeModel(mitk::NodePredicateBase* pred = 0, QObject* parent = 0);
+  ~QmitkDataStorageTreeModel();
+
+  void SetDataStorage(mitk::DataStorage::Pointer dataStorage);
+
+  int columnCount ( const QModelIndex & parent = QModelIndex() ) const;
+  QVariant data ( const QModelIndex & index, int role = Qt::DisplayRole ) const;
+  QModelIndex parent ( const QModelIndex & index ) const;
+  QModelIndex index ( int row, int column, const QModelIndex & parent = QModelIndex() ) const;
+  int rowCount ( const QModelIndex & parent = QModelIndex() ) const;
+  bool hasChildren ( const QModelIndex & parent = QModelIndex() ) const;
+
+  Qt::ItemFlags flags(const QModelIndex& index) const;
+  
+  QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+  
+private:
+
+  mitk::DataStorage::Pointer m_DataStorage;
+  mitk::DataStorage::SetOfObjects::ConstPointer m_DataNodes;
+  mitk::NodePredicateBase* m_NodePredicate;
+  DataStorageViewMode m_ViewMode;
+};
+
+#endif /* QMITKDATASTORAGETREEMODEL_H_ */

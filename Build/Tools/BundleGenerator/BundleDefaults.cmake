f6407cd911e893fcb9dc3405f5b46b69b4f32685
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2009-02-06 16:27:26 +0000

    ENH (#1763): added MITK BundleGenerator

---
+++ Build/Tools/BundleGenerator/BundleDefaults.cmake
@@ -0,0 +1,26 @@
+SET(BUNDLE_NAMESPACE "")
+SET(DEFAULT_REQUIRED_BUNDLES org.opencherry.osgi org.mitk.gui.qt.common)
+SET(DEFAULT_PLUGIN_ID "org.mitk.gui.qt.<your-name>")
+SET(DEFAULT_PLUGIN_VERSION "0.1")
+SET(DEFAULT_PLUGIN_VENDOR "DKFZ, Medical and Biological Informatics")
+SET(DEFAULT_VIEW_BASE_CLASS "QmitkFunctionality")
+SET(DEFAULT_VIEW_BASE_CLASS_H "QmitkFunctionality.h")
+SET(DEFAULT_VIEW_ID "org.mitk.view.<your-id>")
+SET(PROJECT_STATIC_VAR "MITK_STATIC")
+SET(PLUGIN_COPYRIGHT "/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$ 
+ 
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/")

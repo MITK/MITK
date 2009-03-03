2dd62e9b7cae14d34cf036ad06243f44ddc49728
Alfred Franz <a.franz@dkfz-heidelberg.de>
2009-03-03 15:04:25 +0000

    DOC: controlled and corrected the comments with respect to the IGT release

--- Core/IGT/IGTTrackingDevices/mitkClaronInterfaceStub.h
+++ Core/IGT/IGTTrackingDevices/mitkClaronInterfaceStub.h
@@ -1,3 +1,21 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #ifndef MITKCLARONINTERFACE_H_HEADER_INCLUDED_
 #define MITKCLARONINTERFACE_H_HEADER_INCLUDED_
 
@@ -9,7 +27,13 @@ typedef int mtHandle;
 namespace mitk
 {
   typedef int claronToolHandle;
-
+  
+  /** Documentation:
+  *   @brief An object of this class represents the interface to the MicronTracker. Normally the methods of this class
+  *          are calling the c-functions which are provided by the MTC-library. But in this case the MicronTracker is
+  *			 not installed and so we only have stubs here. These stubs send error messages when you try to call the
+  *			 methods.
+  */
   class ClaronInterface
   {
   public:
@@ -63,7 +87,7 @@ namespace mitk
     * \brief This is only a stub, please switch the cmake variable USE_MICRON_TRACKER to on if you want to use the Microntracker.
     */
     void GrabFrame();
-  
+
     /**
     * @return Returns wether the MicronTracker is installed (means wether the C-Make-Variable "MITK_USE_MICRON_TRACKER" is set),
     *         so returns false in this case.

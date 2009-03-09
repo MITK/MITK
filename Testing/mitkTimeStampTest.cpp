9ac3af40366d0885eac69b9183d247a55e5fbecb
Markus Engel <m.engel@dkfz-heidelberg.de>
2009-03-09 14:40:10 +0000

    ENH (#1888): added documentation for the test of mitkTimeStamp

--- Testing/mitkTimeStampTest.cpp
+++ Testing/mitkTimeStampTest.cpp
@@ -47,8 +47,10 @@ int mitkTimeStampTest(int /* argc */, char* /*argv*/[])
   itk::Object::Pointer tester = itk::Object::New();
   itk::Object::Pointer tester2 = itk::Object::New();
 
+  //start-tracking sets the reference-time, timestamps are relative to this time
   mitk::TimeStamp::GetInstance()->StartTracking(tester);
 
+  //sleeps for 20 ms
   #if defined (WIN32) || defined (_WIN32)
   Sleep(20);
   #else
@@ -57,21 +59,27 @@ int mitkTimeStampTest(int /* argc */, char* /*argv*/[])
 
   double time_elapsed;
 
+  //gets time elapsed since start
   time_elapsed = mitk::TimeStamp::GetInstance()->GetElapsed();
 
-  MITK_TEST_CONDITION_REQUIRED((time_elapsed-20) > -10 , "Testing if elapsed time is correct (+-30)");
-  MITK_TEST_CONDITION_REQUIRED((time_elapsed-20) < 10 , "Testing if elapsed time is correct (+-30)");
+  //elapsed time is not allowed to be too far from 20 ms (+-10ms)-> debugging "destroys" the correct value of course
+  MITK_TEST_CONDITION_REQUIRED((time_elapsed-20) > -10 , "Testing if elapsed time is correct (-10)");
+  MITK_TEST_CONDITION_REQUIRED((time_elapsed-20) < 10 , "Testing if elapsed time is correct (+10)");
 
+  //second "device" starts tracking
   mitk::TimeStamp::GetInstance()->StartTracking(tester2);
+  //first device stops
   mitk::TimeStamp::GetInstance()->StopTracking(tester);
 
   time_elapsed = mitk::TimeStamp::GetInstance()->GetElapsed();
 
+  //timestamp still has to be valid (tester2 still tracking), and has to be larger than 20ms
   MITK_TEST_CONDITION_REQUIRED( time_elapsed > 20 , "testing if second device is still keeping the TimeStamp \"alive\"");
 
   mitk::TimeStamp::GetInstance()->StopTracking(tester2);
   time_elapsed = mitk::TimeStamp::GetInstance()->GetElapsed();
 
+  //when all devices have stopped, -1 has to be returned
   MITK_TEST_CONDITION_REQUIRED( time_elapsed == -1 , "testing if -1 is returned after all devices have stopped");
 
   // always end with this!

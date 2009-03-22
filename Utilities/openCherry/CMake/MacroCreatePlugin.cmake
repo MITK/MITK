4553f2d6bfd8f64c0936234acf122df3781a5109
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2009-03-22 02:10:47 +0000

    COMP (#1971): fixed Windows specific linker errors when building external projects

--- Utilities/openCherry/CMake/MacroCreatePlugin.cmake
+++ Utilities/openCherry/CMake/MacroCreatePlugin.cmake
@@ -1,12 +1,12 @@
 # MACRO_CREATE_PLUGIN()
-# 
+#
 # Creates the current plugin.
 # This macro should be called from the plugins CMakeLists.txt file. The plugin
 # library is build in a subdirectory of PLUGIN_OUTPUT_DIR, which is derived from
-# the variable PLUGIN_OUTPUT_BASE_DIR coming from COLLECT_PLUGINS(). 
+# the variable PLUGIN_OUTPUT_BASE_DIR coming from COLLECT_PLUGINS().
 # The target name is available as PLUGIN_TARGET
 # to add additional libraries in your CMakeLists.txt. Include paths and link
-# libraries are set depending on the value of the Required-Bundles header 
+# libraries are set depending on the value of the Required-Bundles header
 # in your plugins MANIFEST.MF file.
 #
 # Available variables:
@@ -22,43 +22,43 @@ MACRO(MACRO_CREATE_PLUGIN)
   IF(MSVC80)
     ADD_DEFINITIONS(-D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_WARNINGS)
   ENDIF(MSVC80)
-  
-  #_MACRO_CREATE_PLUGIN_NAME(PLUGIN_NAME 
+
+  #_MACRO_CREATE_PLUGIN_NAME(PLUGIN_NAME
   #                          INPUT ${CMAKE_CURRENT_SOURCE_DIR}
   #                          BASEDIR ${PLUGINS_SOURCE_BASE_DIR})
-  
+
   MACRO_PARSE_MANIFEST(${CMAKE_CURRENT_SOURCE_DIR}/META-INF/MANIFEST.MF)
-  
-  
+
+
   MESSAGE(STATUS "Creating plugin ${BUNDLE-SYMBOLICNAME}")
-  
+
   STRING(REPLACE . _ PLUGIN_TARGET ${BUNDLE-SYMBOLICNAME})
-  
+
   IF(NOT PLUGIN_CPP_FILES)
     MESSAGE(STATUS "Using files.cmake for plugin ${BUNDLE-SYMBOLICNAME}")
     INCLUDE(files.cmake)
-    
+
     SET(PLUGIN_CPP_FILES ${CPP_FILES})
     SET(PLUGIN_MOC_H_FILES ${MOC_H_FILES})
     SET(PLUGIN_UI_FILES ${UI_FILES})
     SET(PLUGIN_RESOURCE_FILES ${RESOURCE_FILES})
     SET(PLUGIN_RES_FILES ${RES_FILES})
     SET(PLUGIN_H_FILES ${H_FILES})
-    
+
   ELSE(NOT PLUGIN_CPP_FILES)
     MESSAGE(STATUS "Using PLUGIN_CPP_FILES for plugin ${BUNDLE-SYMBOLICNAME}")
   ENDIF(NOT PLUGIN_CPP_FILES)
-  
+
   SET(PLUGIN_OUTPUT_DIR ${PLUGINS_OUTPUT_BASE_DIR}/${BUNDLE-SYMBOLICNAME})
-  
+
   FILE(MAKE_DIRECTORY ${PLUGIN_OUTPUT_DIR})
-  
+
   # Copy the META-INF directory into the binary output dir
   SET (LIBRARY_OUTPUT_PATH ${PLUGIN_OUTPUT_DIR}/bin)
   SOURCE_GROUP("Plugin META Files" FILES ${CMAKE_CURRENT_SOURCE_DIR}/META-INF/MANIFEST.MF)
   SET (PLUGIN_META_FILES ${PLUGIN_META_FILES} ${CMAKE_CURRENT_SOURCE_DIR}/META-INF/MANIFEST.MF)
   CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/META-INF/MANIFEST.MF ${PLUGIN_OUTPUT_DIR}/META-INF/MANIFEST.MF COPYONLY)
-  
+
   # Copy the plugin.xml into the binary output dir
   SET(PLUGINXML ${CMAKE_CURRENT_SOURCE_DIR}/plugin.xml)
   IF(EXISTS ${PLUGINXML})
@@ -66,7 +66,7 @@ MACRO(MACRO_CREATE_PLUGIN)
     SET (PLUGIN_META_FILES ${PLUGIN_META_FILES} ${CMAKE_CURRENT_SOURCE_DIR}/plugin.xml)
     CONFIGURE_FILE(${PLUGINXML} ${PLUGIN_OUTPUT_DIR}/plugin.xml COPYONLY)
   ENDIF(EXISTS ${PLUGINXML})
-  
+
   # Copy the resource files into the binary output dir
   FOREACH(_resource ${PLUGIN_RESOURCE_FILES})
     CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/${_resource}" "${PLUGIN_OUTPUT_DIR}/${_resource}" COPYONLY)
@@ -79,7 +79,7 @@ MACRO(MACRO_CREATE_PLUGIN)
   IF(PLUGIN_MOC_H_FILES)
     # this is a workaround for Visual Studio. The relative include paths in the generated
     # moc files can get very long and can't be resolved by the MSVC compiler. See bug #1961
-    # for details. 
+    # for details.
     FOREACH(moc_h_file ${PLUGIN_MOC_H_FILES})
       QT4_WRAP_CPP(PLUGIN_GENERATED_FILES ${moc_h_file} OPTIONS -f${moc_h_file})
     ENDFOREACH(moc_h_file)
@@ -87,20 +87,20 @@ MACRO(MACRO_CREATE_PLUGIN)
   IF(PLUGIN_RES_FILES)
     QT4_ADD_RESOURCES(PLUGIN_GENERATED_FILES ${PLUGIN_RES_FILES})
   ENDIF(PLUGIN_RES_FILES)
- 
+
 
   INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/src ${CMAKE_CURRENT_BINARY_DIR})
   _MACRO_SETUP_PLUGIN_DEPENDENCIES(_linklibs PLUGIN_NAME ${BUNDLE-SYMBOLICNAME})
-  
-  MACRO_ORGANIZE_SOURCES(INPUT ${PLUGIN_CPP_FILES} 
-                         ${PLUGIN_UI_FILES} 
+
+  MACRO_ORGANIZE_SOURCES(INPUT ${PLUGIN_CPP_FILES}
+                         ${PLUGIN_UI_FILES}
                          ${PLUGIN_RES_FILES}
                          ${PLUGIN_H_FILES}
                          GENERATED ${PLUGIN_GENERATED_FILES})
-                         
+
   SET(_all_target_files
-      ${PLUGIN_CPP_FILES} 
-      ${PLUGIN_UI_FILES} 
+      ${PLUGIN_CPP_FILES}
+      ${PLUGIN_UI_FILES}
       ${PLUGIN_RES_FILES}
       ${PLUGIN_H_FILES}
       ${PLUGIN_GENERATED_FILES}
@@ -109,10 +109,10 @@ MACRO(MACRO_CREATE_PLUGIN)
       ${CORRESPONDING__H_FILES}
       ${CORRESPONDING__TXX_FILES}
       )
-  
+
   #INCLUDE_DIRECTORIES(PLUGIN_OUTPUT_DIR)
   ADD_LIBRARY(${PLUGIN_TARGET} SHARED ${_all_target_files})
-  
+
   # we need to explicitly state the debug versions of the libraries
   # we are linking to in the TARGET_LINK_LIBRARIES command.
   # Although we set CMAKE_DEBUG_POSTFIX to d, CMake automatically
@@ -121,16 +121,12 @@ MACRO(MACRO_CREATE_PLUGIN)
   # External projects using this macro would therefore alway link
   # to lib1, instead of lib1d in debug configurations
   SET(_debug_linklibs "")
-  FOREACH(_linklib ${_linklibs})
+  FOREACH(_linklib ${PLUGIN_LINK_LIBRARIES})
     SET(_debug_linklibs ${_debug_linklibs} optimized "${_linklib}" debug "${_linklib}${CMAKE_DEBUG_POSTFIX}")
   ENDFOREACH(_linklib)
   #MESSAGE(STATUS "${PLUGIN_TARGET} deps: ${_linklibs}")
-  #IF(BUILDING_OPENCHERRY)
-  #  TARGET_LINK_LIBRARIES(${PLUGIN_TARGET} ${_linklibs})
-  #ELSE(BUILDING_OPENCHERRY)
-    TARGET_LINK_LIBRARIES(${PLUGIN_TARGET} ${_debug_linklibs})
-  #ENDIF(BUILDING_OPENCHERRY)
-    
+  TARGET_LINK_LIBRARIES(${PLUGIN_TARGET} ${_debug_linklibs})
+
   #SET_TARGET_PROPERTIES(${PLUGIN_TARGET} PROPERTIES PREFIX lib IMPORT_PREFIX lib)
   SET_TARGET_PROPERTIES(${PLUGIN_TARGET} PROPERTIES PREFIX lib)
 
@@ -139,15 +135,14 @@ ENDMACRO(MACRO_CREATE_PLUGIN)
 
 # Macro to set specific Qt options, calls CREATE_PLUGIN at the end
 MACRO(MACRO_CREATE_QT_PLUGIN)
-  
+
   IF(QT4_FOUND)
     INCLUDE_DIRECTORIES(${QT_INCLUDES})
     ADD_DEFINITIONS(${QT_DEFINITIONS})
-  
+
     MACRO_CREATE_PLUGIN()
-  
+
     TARGET_LINK_LIBRARIES(${PLUGIN_TARGET} ${QT_LIBRARIES})
   ENDIF(QT4_FOUND)
-  
-ENDMACRO(MACRO_CREATE_QT_PLUGIN)
 
+ENDMACRO(MACRO_CREATE_QT_PLUGIN)
--- Utilities/openCherry/CMake/cherryPluginHelpers.cmake
+++ Utilities/openCherry/CMake/cherryPluginHelpers.cmake
@@ -7,7 +7,7 @@
 # the plugin-name is computed by removing basedir from dir and
 # replacing slashes with points
 #
-# Example: BASEDIR=/root 
+# Example: BASEDIR=/root
 #          INPUT=/root/org/opencherry/ui.qt
 #          output_name=org.opencherry.ui.qt
 #
@@ -17,9 +17,9 @@ MACRO(_MACRO_CREATE_PLUGIN_NAME output_name)
 
   STRING(REPLACE "${_CREATE_NAME_BASEDIR}/" "" _name_with_slashes ${_CREATE_NAME_INPUT})
   STRING(REPLACE "/" "." _name_with_points ${_name_with_slashes})
-  
+
   SET(${output_name} ${_name_with_points})
-  
+
 ENDMACRO(_MACRO_CREATE_PLUGIN_NAME)
 
 
@@ -28,15 +28,15 @@ ENDMACRO(_MACRO_CREATE_PLUGIN_NAME)
 # pluginname: the unique plugin id (i.e. org.opencherry.osgi)
 #
 MACRO(_MACRO_FIND_PLUGIN_SRC_DIR pluginpath pluginname)
-  
+
   #FOREACH(plugindir ${OPENCHERRY_PLUGIN_SOURCE_DIRS})
   #  IF(EXISTS ${plugindir}/${ARGV1})
   #    SET(${pluginpath} ${plugindir}/${ARGV1})
   #  ENDIF(EXISTS ${plugindir}/${ARGV1})
   #ENDFOREACH(plugindir ${OPENCHERRY_PLUGIN_SOURCE_DIRS})
-  
+
   SET(${pluginpath} "${${ARGV1}_SRC_DIR}")
-  
+
   #MESSAGE(STATUS "Found plugin src dir: ${${pluginpath}}")
 
 ENDMACRO(_MACRO_FIND_PLUGIN_SRC_DIR)
@@ -46,14 +46,14 @@ ENDMACRO(_MACRO_FIND_PLUGIN_SRC_DIR)
 # pluginname: the unique plugin id (i.e. org.opencherry.osgi)
 #
 MACRO(_MACRO_FIND_PLUGIN_BIN_DIR pluginpath pluginname)
-  
+
   #FOREACH(plugindir ${OPENCHERRY_PLUGIN_BINARY_DIRS})
   #  IF(EXISTS ${plugindir}/${ARGV1})
   #    SET(${pluginpath} ${plugindir}/${ARGV1})
   #  ENDIF(EXISTS ${plugindir}/${ARGV1})
   #ENDFOREACH(plugindir ${OPENCHERRY_PLUGIN_BINARY_DIRS})
   SET(${pluginpath} "${${ARGV1}_BIN_DIR}")
-  
+
   #MESSAGE(STATUS "Found plugin bin dir: ${${pluginpath}}")
 
 ENDMACRO(_MACRO_FIND_PLUGIN_BIN_DIR)
@@ -66,7 +66,7 @@ MACRO(_MACRO_REQUIRED_BUNDLES_LIST output_list input_string)
   IF(_reqb_input)
     STRING(REPLACE "," ";" _reqb_dependencies ${_reqb_input})
   ENDIF(_reqb_input)
-  
+
   FOREACH(_dep ${_reqb_dependencies})
     # remove leading and trailing whitespace
     STRING(REGEX MATCH "[^ ]+" _trimeddep ${_dep})
@@ -82,43 +82,43 @@ ENDMACRO(_MACRO_REQUIRED_BUNDLES_LIST output_list input_string)
 MACRO(_MACRO_SETUP_PLUGIN_DEPENDENCIES _explicit_libs)
 
   MACRO_PARSE_ARGUMENTS(_FIND_DEPS "PLUGIN_NAME" "" ${ARGN})
-  
+
   SET(_plugin_src_dir )
   _MACRO_FIND_PLUGIN_SRC_DIR(_plugin_src_dir ${_FIND_DEPS_PLUGIN_NAME})
-  
+
   MACRO_PARSE_MANIFEST(${_plugin_src_dir}/META-INF/MANIFEST.MF)
-  
+
   SET(_plugin_dependencies )
   _MACRO_REQUIRED_BUNDLES_LIST(_plugin_dependencies "${REQUIRE-BUNDLE}")
-  
+
   SET(_plugin_depends_all )
   FOREACH(_dep ${_plugin_dependencies})
-  
+
     STRING(REPLACE . _ _dep_target ${_dep})
     SET(${_explicit_libs} ${${_explicit_libs}} ${_dep_target})
-    
+
     # build a list of all explicit and implicit dependencies
     LIST(FIND _plugin_depends_all ${_dep} _depall_index)
     IF(_depall_index EQUAL -1)
       LIST(APPEND _plugin_depends_all ${_dep})
       SET(_added_dep 1)
     ENDIF(_depall_index EQUAL -1)
-    
+
     WHILE(_added_dep)
       SET(_added_dep 0)
       FOREACH(_depdep ${_plugin_depends_all})
-        
+
         SET(_plugin_src_dir )
         _MACRO_FIND_PLUGIN_SRC_DIR(_plugin_src_dir ${_depdep})
-        
+
         IF(NOT _plugin_src_dir)
           MESSAGE(SEND_ERROR "EMPTY DIR found for dep: \"${_depdep}\"")
         ENDIF(NOT _plugin_src_dir)
-  
+
         MACRO_PARSE_MANIFEST("${_plugin_src_dir}/META-INF/MANIFEST.MF")
         SET(_plugin_dependencies )
         _MACRO_REQUIRED_BUNDLES_LIST(_plugin_dependencies "${REQUIRE-BUNDLE}")
-        
+
         FOREACH(_depdepdep ${_plugin_dependencies})
           LIST(FIND _plugin_depends_all ${_depdepdep} _depdepdep_index)
           IF(_depdepdep_index EQUAL -1)
@@ -126,43 +126,46 @@ MACRO(_MACRO_SETUP_PLUGIN_DEPENDENCIES _explicit_libs)
             SET(_added_dep 1)
           ENDIF(_depdepdep_index EQUAL -1)
         ENDFOREACH(_depdepdep ${_plugin_dependencies})
-        
+
       ENDFOREACH(_depdep ${_plugin_depends_all})
-      
+
     ENDWHILE(_added_dep)
-  
+
   ENDFOREACH(_dep ${_plugin_dependencies})
-  
+
   # iterator over all dependencies
   SET(_plugins_turned_off )
+  SET(PLUGIN_LINK_LIBRARIES )
   FOREACH(_dep ${_plugin_depends_all})
-  
+
     # collect the ids of plugins we will have to build
     IF(NOT BUILD_${_dep})
       LIST(APPEND _plugins_turned_off ${_dep})
     ENDIF(NOT BUILD_${_dep})
-    
+
     # set include and link directories
     SET(_dep_src_dir )
     SET(_dep_bin_dir )
     _MACRO_FIND_PLUGIN_SRC_DIR(_dep_src_dir ${_dep})
     _MACRO_FIND_PLUGIN_BIN_DIR(_dep_bin_dir ${_dep})
-  
+
     LINK_DIRECTORIES(${_dep_bin_dir}/bin)
     INCLUDE_DIRECTORIES(${_dep_src_dir}/src)
-    
+
     IF(EXISTS ${_dep_src_dir}/includes.cmake)
       INCLUDE(${_dep_src_dir}/includes.cmake)
       FOREACH(_added_incldir ${ADDITIONAL_INCLUDE_DIRECTORIES})
         INCLUDE_DIRECTORIES(${_dep_src_dir}/${_added_incldir})
       ENDFOREACH(_added_incldir ${ADDITIONAL_INCLUDE_DIRECTORIES})
     ENDIF(EXISTS ${_dep_src_dir}/includes.cmake)
-    
+
+    STRING(REPLACE . _ _dep_target ${_dep})
+    LIST(APPEND PLUGIN_LINK_LIBRARIES ${_dep_target})
+
   ENDFOREACH(_dep ${_plugin_depends_all})
   
   IF(_plugins_turned_off)
-    MESSAGE(SEND_ERROR "Unmet dependencies: The plugin ${_FIND_DEPS_PLUGIN_NAME} depends on the following plugins:\n${_plugins_turned_off}.\nSwitch them on in order to build ${_FIND_DEPS_PLUGIN_NAME}.") 
+    MESSAGE(SEND_ERROR "Unmet dependencies: The plugin ${_FIND_DEPS_PLUGIN_NAME} depends on the following plugins:\n${_plugins_turned_off}.\nSwitch them on in order to build ${_FIND_DEPS_PLUGIN_NAME}.")
   ENDIF(_plugins_turned_off)
-  
-ENDMACRO(_MACRO_SETUP_PLUGIN_DEPENDENCIES)
 
+ENDMACRO(_MACRO_SETUP_PLUGIN_DEPENDENCIES)

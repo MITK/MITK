6d284a073d67201572fcec21550970123b197969
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2009-04-14 15:51:34 +0000

    STYLE: added newline at end of file

--- Utilities/openCherry/Bundles/org.opencherry.core.runtime/src/cherryIAdaptable.cpp
+++ Utilities/openCherry/Bundles/org.opencherry.core.runtime/src/cherryIAdaptable.cpp
@@ -1,18 +1,18 @@
 /*=========================================================================
- 
+
 Program:   openCherry Platform
 Language:  C++
 Date:      $Date$
 Version:   $Revision: 16014 $
- 
+
 Copyright (c) German Cancer Research Center, Division of Medical and
 Biological Informatics. All rights reserved.
 See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
- 
+
 This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
- 
+
 =========================================================================*/
 
 #include "cherryIAdaptable.h"

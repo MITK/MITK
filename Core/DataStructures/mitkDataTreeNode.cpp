4348aeee6398ff6c0305e76a5837774f612a9404
Ingmar Wegner <i.wegner@dkfz-heidelberg.de>
2009-04-22 14:42:13 +0000

    COMP (#1682): missed changing interface of template GetPropertyValue

--- Core/DataStructures/mitkDataTreeNode.cpp
+++ Core/DataStructures/mitkDataTreeNode.cpp
@@ -694,10 +694,10 @@ bool mitk::DataTreeNode::GetPropertyValue(const char* propertyKey, T & value, mi
   return false;
 }
 
-template bool mitk::DataTreeNode::GetPropertyValue<double>(char const*, double&, mitk::BaseRenderer*, bool*) const;
-template bool mitk::DataTreeNode::GetPropertyValue<float>(char const*, float&, mitk::BaseRenderer*, bool*) const;
-template bool mitk::DataTreeNode::GetPropertyValue<int>(char const*, int&, mitk::BaseRenderer*, bool*) const;
-template bool mitk::DataTreeNode::GetPropertyValue<bool>(char const*, bool&, mitk::BaseRenderer*, bool*) const;
+template bool mitk::DataTreeNode::GetPropertyValue<double>(char const*, double&, mitk::BaseRenderer*) const;
+template bool mitk::DataTreeNode::GetPropertyValue<float>(char const*, float&, mitk::BaseRenderer*) const;
+template bool mitk::DataTreeNode::GetPropertyValue<int>(char const*, int&, mitk::BaseRenderer*) const;
+template bool mitk::DataTreeNode::GetPropertyValue<bool>(char const*, bool&, mitk::BaseRenderer*) const;
 
 #endif
 

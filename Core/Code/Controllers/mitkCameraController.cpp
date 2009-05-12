b1c90c966a61f087f88de216d5a66ba22c8ad008
Jochen Neuhaus <j.neuhaus@dkfz-heidelberg.de>
2009-05-12 20:32:53 +0000

    CHG: remove DataTree dependency

--- Core/Code/Controllers/mitkCameraController.cpp
+++ Core/Code/Controllers/mitkCameraController.cpp
@@ -32,123 +32,132 @@ mitk::CameraController::CameraController(const char * type) : BaseController(typ
 mitk::CameraController::~CameraController()
 {}
 
+
 void mitk::CameraController::Resize(int, int)
 {}
+
 
 void mitk::CameraController::MousePressEvent(mitk::MouseEvent*)
 {}
 
+
 void mitk::CameraController::MouseReleaseEvent(mitk::MouseEvent*)
 {}
+
 
 void mitk::CameraController::MouseMoveEvent(mitk::MouseEvent*)
 {}
 
+
 void mitk::CameraController::KeyPressEvent(mitk::KeyEvent*)
 {}
+
+
 void mitk::CameraController::SetViewToAnterior()
 {
   this->SetStandardView(ANTERIOR);
 }
+
+
 void mitk::CameraController::SetViewToPosterior()
 {
   this->SetStandardView(POSTERIOR);
 }
+
+
 void mitk::CameraController::SetViewToSinister()
 {
   this->SetStandardView(SINISTER);
 }
+
+
 void mitk::CameraController::SetViewToDexter()
 {
   this->SetStandardView(DEXTER);
 }
+
+
 void mitk::CameraController::SetViewToCranial()
 {
   this->SetStandardView(CRANIAL);
 }
+
+
 void mitk::CameraController::SetViewToCaudal()
 {
   this->SetStandardView(CAUDAL);
 }
 
+
 void mitk::CameraController::SetStandardView( mitk::CameraController::StandardView view )
 {
   const mitk::VtkPropRenderer* glRenderer = dynamic_cast<const mitk::VtkPropRenderer*>(m_Renderer);
-  if (glRenderer)
+  if (glRenderer == NULL)
+    return;
+  vtkRenderer* vtkRenderer = glRenderer->GetVtkRenderer();
+  assert (vtkRenderer);
+
+  mitk::BoundingBox::Pointer bb;
+  mitk::DataStorage* ds = m_Renderer->GetDataStorage();
+  if (ds != NULL)
+    bb = ds->ComputeBoundingBox();
+  else
+    return;
+
+  mitk::Point3D middle = bb->GetCenter();
+  vtkRenderer->GetActiveCamera()->SetFocalPoint(middle[0], middle[1], middle[2]);
+  switch(view)
   {
-    vtkRenderer* vtkRenderer = glRenderer->GetVtkRenderer();
-
-    mitk::BoundingBox::Pointer bb;
-    mitk::DataStorage* ds = m_Renderer->GetDataStorage();
-    if (ds != NULL)
-    {
-      bb = ds->ComputeBoundingBox();
-    }
-    else
-    {
-      mitk::DataTree*  tree = dynamic_cast<mitk::DataTree*>(m_Renderer->GetData()->GetTree());
-      bb = tree->ComputeBoundingBox(m_Renderer->GetData());
-    }
-
-    mitk::Point3D middle =bb->GetCenter();
-    vtkRenderer->GetActiveCamera()->SetFocalPoint(middle[0],middle[1],middle[2]);
-    switch(view)
-    {
-    case ANTERIOR:
-    case POSTERIOR:
-    case SINISTER:
-    case DEXTER:
-      vtkRenderer->GetActiveCamera()->SetViewUp(0,0,1);
-      break;
-    case CRANIAL:
-    case CAUDAL:
-      vtkRenderer->GetActiveCamera()->SetViewUp(0,-1,0);
-    }
-
-    switch(view)
-    {
-    case ANTERIOR:
-      vtkRenderer->GetActiveCamera()->SetPosition(middle[0],-100000,middle[2]);
-      break;
-    case POSTERIOR:
-      vtkRenderer->GetActiveCamera()->SetPosition(middle[0],+100000,middle[2]);
-      break;
-    case SINISTER:
-      vtkRenderer->GetActiveCamera()->SetPosition(+100000,middle[1],middle[2]);
-      break;
-    case DEXTER:
-      vtkRenderer->GetActiveCamera()->SetPosition(-100000,middle[1],middle[2]);
-      break;
-    case CRANIAL:
-      vtkRenderer->GetActiveCamera()->SetPosition(middle[0],middle[1],100000);
-      break;
-    case CAUDAL:
-      vtkRenderer->GetActiveCamera()->SetPosition(middle[0],middle[1],-100000);
-      break;
-    }
-    vtkRenderer->ResetCamera();
-    
-    double *cameraPosition = vtkRenderer->GetActiveCamera()->GetPosition();
-    
-    switch(view)
-    {
-    case ANTERIOR:
-    case POSTERIOR:
-      vtkRenderer->GetActiveCamera()->SetPosition(cameraPosition[0],cameraPosition[1] / m_ZoomFactor,cameraPosition[2]);
-      break;
-
-    case SINISTER:
-    case DEXTER:
-      vtkRenderer->GetActiveCamera()->SetPosition(cameraPosition[0] / m_ZoomFactor,cameraPosition[1],cameraPosition[2]);
-      break;
-
-    case CRANIAL:
-    case CAUDAL:
-      vtkRenderer->GetActiveCamera()->SetPosition(cameraPosition[0],cameraPosition[1],cameraPosition[2] / m_ZoomFactor);
-      break;
-    }
-
-    vtkRenderer->ResetCameraClippingRange();
-    mitk::RenderingManager::GetInstance()->RequestUpdateAll();
+  case ANTERIOR:
+  case POSTERIOR:
+  case SINISTER:
+  case DEXTER:
+    vtkRenderer->GetActiveCamera()->SetViewUp(0,0,1);
+    break;
+  case CRANIAL:
+  case CAUDAL:
+    vtkRenderer->GetActiveCamera()->SetViewUp(0,-1,0);
   }
+  switch(view)
+  {
+  case ANTERIOR:
+    vtkRenderer->GetActiveCamera()->SetPosition(middle[0],-100000,middle[2]);
+    break;
+  case POSTERIOR:
+    vtkRenderer->GetActiveCamera()->SetPosition(middle[0],+100000,middle[2]);
+    break;
+  case SINISTER:
+    vtkRenderer->GetActiveCamera()->SetPosition(+100000,middle[1],middle[2]);
+    break;
+  case DEXTER:
+    vtkRenderer->GetActiveCamera()->SetPosition(-100000,middle[1],middle[2]);
+    break;
+  case CRANIAL:
+    vtkRenderer->GetActiveCamera()->SetPosition(middle[0],middle[1],100000);
+    break;
+  case CAUDAL:
+    vtkRenderer->GetActiveCamera()->SetPosition(middle[0],middle[1],-100000);
+    break;
+  }
+  vtkRenderer->ResetCamera();
+  double *cameraPosition = vtkRenderer->GetActiveCamera()->GetPosition();
+  switch(view)
+  {
+  case ANTERIOR:
+  case POSTERIOR:
+    vtkRenderer->GetActiveCamera()->SetPosition(cameraPosition[0],cameraPosition[1] / m_ZoomFactor,cameraPosition[2]);
+    break;
+
+  case SINISTER:
+  case DEXTER:
+    vtkRenderer->GetActiveCamera()->SetPosition(cameraPosition[0] / m_ZoomFactor,cameraPosition[1],cameraPosition[2]);
+    break;
+
+  case CRANIAL:
+  case CAUDAL:
+    vtkRenderer->GetActiveCamera()->SetPosition(cameraPosition[0],cameraPosition[1],cameraPosition[2] / m_ZoomFactor);
+    break;
+  }
+  vtkRenderer->ResetCameraClippingRange();
+  mitk::RenderingManager::GetInstance()->RequestUpdateAll();
 }

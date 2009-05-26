2d72db595f00953b85341965623a06807d1ded30
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2009-05-26 11:37:29 +0000

    ENH: Simplify the internal _MACRO_SETUP_PLUGIN_DEPENDENCIES macro by using CMake 2.6 features. This should speed up the configuration process.

--- openCherry/CMake/cherryPluginHelpers.cmake
+++ openCherry/CMake/cherryPluginHelpers.cmake
@@ -20,7 +20,7 @@ MACRO(_MACRO_CREATE_PLUGIN_NAME output_name)
 
   SET(${output_name} ${_name_with_points})
 
-ENDMACRO(_MACRO_CREATE_PLUGIN_NAME)
+ENDMACRO()
 
 
 # sets pluginpath to the absolut path of the plugins source directory
@@ -39,7 +39,7 @@ MACRO(_MACRO_FIND_PLUGIN_SRC_DIR pluginpath pluginname)
 
   #MESSAGE(STATUS "Found plugin src dir: ${${pluginpath}}")
 
-ENDMACRO(_MACRO_FIND_PLUGIN_SRC_DIR)
+ENDMACRO()
 
 # sets pluginpath to the absolut path of the plugins binary output directory
 #
@@ -56,11 +56,13 @@ MACRO(_MACRO_FIND_PLUGIN_BIN_DIR pluginpath pluginname)
 
   #MESSAGE(STATUS "Found plugin bin dir: ${${pluginpath}}")
 
-ENDMACRO(_MACRO_FIND_PLUGIN_BIN_DIR)
+ENDMACRO()
 
-MACRO(_MACRO_REQUIRED_BUNDLES_LIST output_list input_string)
+MACRO(_MACRO_REQUIRED_BUNDLES_LIST output_list input_file)
 
-  SET(_reqb_input ${input_string})
+  SET(_file "${input_file}")
+  FILE(STRINGS "${_file}" _mfline LENGTH_MINIMUM 1 REGEX " *Require-Bundle *: *[^ ]+")
+  STRING(REGEX REPLACE "^[^:]*[:](.*)" "\\1" _reqb_input "${_mfline}")
 
   SET(_reqb_dependencies )
   IF(_reqb_input)
@@ -69,10 +71,10 @@ MACRO(_MACRO_REQUIRED_BUNDLES_LIST output_list input_string)
 
   FOREACH(_dep ${_reqb_dependencies})
     # remove leading and trailing whitespace
-    STRING(REGEX MATCH "[^ ]+" _trimeddep ${_dep})
+    STRING(STRIP ${_dep} _trimeddep)
     LIST(APPEND ${output_list} ${_trimeddep})
   ENDFOREACH(_dep ${_reqb_dependencies})
-ENDMACRO(_MACRO_REQUIRED_BUNDLES_LIST output_list input_string)
+ENDMACRO()
 
 # _MACRO_SETUP_PLUGIN_DEPENDENCIES(
 #      _explicit_libs
@@ -86,87 +88,75 @@ MACRO(_MACRO_SETUP_PLUGIN_DEPENDENCIES _explicit_libs)
   SET(_plugin_src_dir )
   _MACRO_FIND_PLUGIN_SRC_DIR(_plugin_src_dir ${_FIND_DEPS_PLUGIN_NAME})
 
-  MACRO_PARSE_MANIFEST(${_plugin_src_dir}/META-INF/MANIFEST.MF)
-
   SET(_plugin_dependencies )
-  _MACRO_REQUIRED_BUNDLES_LIST(_plugin_dependencies "${REQUIRE-BUNDLE}")
-
-  SET(_plugin_depends_all )
+  _MACRO_REQUIRED_BUNDLES_LIST(_plugin_dependencies "${_plugin_src_dir}/META-INF/MANIFEST.MF")
+  
   FOREACH(_dep ${_plugin_dependencies})
-
     STRING(REPLACE . _ _dep_target ${_dep})
     SET(${_explicit_libs} ${${_explicit_libs}} ${_dep_target})
+  ENDFOREACH()
 
-    # build a list of all explicit and implicit dependencies
-    LIST(FIND _plugin_depends_all ${_dep} _depall_index)
-    IF(_depall_index EQUAL -1)
-      LIST(APPEND _plugin_depends_all ${_dep})
-      SET(_added_dep 1)
-    ENDIF(_depall_index EQUAL -1)
-
-    WHILE(_added_dep)
-      SET(_added_dep 0)
-      FOREACH(_depdep ${_plugin_depends_all})
+  SET(_plugin_old_dependencies "")
+  SET(_dep_error 0)
+  WHILE(NOT "${_plugin_dependencies}" STREQUAL "${_plugin_old_dependencies}")
+    SET(_plugin_old_dependencies ${_plugin_dependencies})
+    
+    FOREACH(_dep ${_plugin_dependencies})
 
         SET(_plugin_src_dir )
-        _MACRO_FIND_PLUGIN_SRC_DIR(_plugin_src_dir ${_depdep})
+        _MACRO_FIND_PLUGIN_SRC_DIR(_plugin_src_dir ${_dep})
 
         IF(NOT _plugin_src_dir)
-          MESSAGE(SEND_ERROR "EMPTY DIR found for dep: \"${_depdep}\"")
-        ENDIF(NOT _plugin_src_dir)
+          MESSAGE(SEND_ERROR "Plug-in dependency \"${_dep}\" does not seem to exist.")
+          SET(_dep_error 1)
+          BREAK()
+        ENDIF()
 
-        MACRO_PARSE_MANIFEST("${_plugin_src_dir}/META-INF/MANIFEST.MF")
-        SET(_plugin_dependencies )
-        _MACRO_REQUIRED_BUNDLES_LIST(_plugin_dependencies "${REQUIRE-BUNDLE}")
+        _MACRO_REQUIRED_BUNDLES_LIST(_plugin_dependencies "${_plugin_src_dir}/META-INF/MANIFEST.MF")
 
-        FOREACH(_depdepdep ${_plugin_dependencies})
-          LIST(FIND _plugin_depends_all ${_depdepdep} _depdepdep_index)
-          IF(_depdepdep_index EQUAL -1)
-            LIST(APPEND _plugin_depends_all ${_depdepdep})
-            SET(_added_dep 1)
-          ENDIF(_depdepdep_index EQUAL -1)
-        ENDFOREACH(_depdepdep ${_plugin_dependencies})
+    ENDFOREACH()
+    
+    LIST(REMOVE_DUPLICATES _plugin_dependencies)
+    LIST(SORT _plugin_dependencies)
+    
+  ENDWHILE()
 
-      ENDFOREACH(_depdep ${_plugin_depends_all})
-
-    ENDWHILE(_added_dep)
-
-  ENDFOREACH(_dep ${_plugin_dependencies})
-
-  # iterator over all dependencies
-  SET(_plugins_turned_off )
-  SET(PLUGIN_LINK_LIBRARIES )
-  FOREACH(_dep ${_plugin_depends_all})
-
-    # collect the ids of plugins we will have to build
-    IF(NOT _BUILD_${_dep})
-      LIST(APPEND _plugins_turned_off ${_dep})
-    ENDIF()
-
-    # set include and link directories
-    SET(_dep_src_dir )
-    SET(_dep_bin_dir )
-    _MACRO_FIND_PLUGIN_SRC_DIR(_dep_src_dir ${_dep})
-    _MACRO_FIND_PLUGIN_BIN_DIR(_dep_bin_dir ${_dep})
-
-    LINK_DIRECTORIES(${_dep_bin_dir}/bin)
-    INCLUDE_DIRECTORIES(${_dep_src_dir}/src)
-
-    IF(EXISTS ${_dep_src_dir}/includes.cmake)
-      INCLUDE(${_dep_src_dir}/includes.cmake)
-      FOREACH(_added_incldir ${ADDITIONAL_INCLUDE_DIRECTORIES})
-        INCLUDE_DIRECTORIES(${_dep_src_dir}/${_added_incldir})
-      ENDFOREACH(_added_incldir ${ADDITIONAL_INCLUDE_DIRECTORIES})
-    ENDIF(EXISTS ${_dep_src_dir}/includes.cmake)
-
-    STRING(REPLACE . _ _dep_target ${_dep})
-    LIST(APPEND PLUGIN_LINK_LIBRARIES ${_dep_target})
-
-  ENDFOREACH(_dep ${_plugin_depends_all})
-  
-  IF(_plugins_turned_off)
-    MESSAGE(SEND_ERROR "Unmet dependencies: The plugin ${_FIND_DEPS_PLUGIN_NAME} depends on the following plugins:\n${_plugins_turned_off}.\nSwitch them on in order to build ${_FIND_DEPS_PLUGIN_NAME}.")
-  ENDIF(_plugins_turned_off)
+  IF(NOT _dep_error)
+      # iterator over all dependencies
+      SET(_plugins_turned_off )
+      SET(PLUGIN_LINK_LIBRARIES )
+      FOREACH(_dep ${_plugin_dependencies})
+    
+        # collect the ids of plugins we will have to build
+        IF(NOT _BUILD_${_dep})
+          LIST(APPEND _plugins_turned_off ${_dep})
+        ENDIF()
+    
+        # set include and link directories
+        SET(_dep_src_dir )
+        SET(_dep_bin_dir )
+        _MACRO_FIND_PLUGIN_SRC_DIR(_dep_src_dir ${_dep})
+        _MACRO_FIND_PLUGIN_BIN_DIR(_dep_bin_dir ${_dep})
+    
+        LINK_DIRECTORIES(${_dep_bin_dir}/bin)
+        INCLUDE_DIRECTORIES(${_dep_src_dir}/src)
+    
+        IF(EXISTS ${_dep_src_dir}/includes.cmake)
+          INCLUDE(${_dep_src_dir}/includes.cmake)
+          FOREACH(_added_incldir ${ADDITIONAL_INCLUDE_DIRECTORIES})
+            INCLUDE_DIRECTORIES(${_dep_src_dir}/${_added_incldir})
+          ENDFOREACH(_added_incldir ${ADDITIONAL_INCLUDE_DIRECTORIES})
+        ENDIF(EXISTS ${_dep_src_dir}/includes.cmake)
+    
+        STRING(REPLACE . _ _dep_target ${_dep})
+        LIST(APPEND PLUGIN_LINK_LIBRARIES ${_dep_target})
+    
+      ENDFOREACH(_dep ${_plugin_dependencies})
+      
+      IF(_plugins_turned_off)
+        MESSAGE(SEND_ERROR "Unmet dependencies: The plugin ${_FIND_DEPS_PLUGIN_NAME} depends on the following plugins:\n${_plugins_turned_off}.\nSwitch them on in order to build ${_FIND_DEPS_PLUGIN_NAME}.")
+      ENDIF(_plugins_turned_off)
+  ENDIF()
 
 ENDMACRO(_MACRO_SETUP_PLUGIN_DEPENDENCIES)
 

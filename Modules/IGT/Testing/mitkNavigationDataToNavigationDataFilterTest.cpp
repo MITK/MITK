4a051457e00d0506cfc62bc9f9130c9bb92889fa
Jochen Neuhaus <j.neuhaus@dkfz-heidelberg.de>
2009-06-03 15:37:37 +0000

    ENHENH (#1777): add unit test for mitk::NavigationDataToNavigationDataFilter.  This test could not be not compiled yet, because a IGT-Testdriver is missing at the moment.

---
+++ Modules/IGT/Testing/mitkNavigationDataToNavigationDataFilterTest.cpp
@@ -0,0 +1,73 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date: 2009-05-13 14:52:01 +0200 (Mi, 13. Mai 2009) $
+Version:   $Revision: 17230 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "mitkNavigationDataToNavigationDataFilter.h"
+#include "mitkNavigationData.h"
+
+#include "mitkTestingMacros.h"
+
+
+/**Documentation
+ *  test for the class "NavigationDataToNavigationDataFilter".
+ */
+int mitkNavigationDataToNavigationDataFilterTest(int /* argc */, char* /*argv*/[])
+{
+  MITK_TEST_BEGIN("NavigationDataToNavigationDataFilter")
+
+  // let's create an object of our class  
+  mitk::NavigationDataToNavigationDataFilter::Pointer myFilter = mitk::NavigationDataToNavigationDataFilter::New();
+  
+  MITK_TEST_CONDITION_REQUIRED(myFilter.IsNotNull(),"Testing instantiation");
+
+  /* create helper objects: navigation data with position as origin, zero quaternion, zero error and data valid */
+  mitk::NavigationData::PositionType initialPos;
+  mitk::FillVector3D(initialPos, 1.0, 2.0, 3.0);
+  mitk::NavigationData::OrientationType initialOri(0.1, 0.2, 0.3, 0.4);
+  mitk::ScalarType initialError(22.22);
+  bool initialValid(true);  
+  mitk::NavigationData::Pointer nd0 = mitk::NavigationData::New();
+  nd0->SetPosition(initialPos);
+  nd0->SetOrientation(initialOri);
+  nd0->SetPositionAccuracy(initialError);
+  nd0->SetDataValid(initialValid);
+
+  
+  MITK_TEST_CONDITION(myFilter->GetOutput() == NULL, "testing GetOutput()");
+
+  myFilter->SetInput(nd0);
+  MITK_TEST_CONDITION(myFilter->GetInput() == nd0, "testing Set-/GetInput()");
+  MITK_TEST_CONDITION(myFilter->GetInput(0) == nd0, "testing Set-/GetInput(0)");
+  MITK_TEST_CONDITION(myFilter->GetOutput() != NULL, "testing GetOutput() after SetInput()");
+  MITK_TEST_CONDITION(myFilter->GetOutput(0) != NULL, "testing GetOutput() after SetInput()");
+  MITK_TEST_CONDITION(myFilter->GetOutput(0) != nd0, "testing GetOutput() different object than input");
+
+  mitk::NavigationData::Pointer nd1 = mitk::NavigationData::New();
+  nd1->Graft(nd0);
+  nd1->SetDataValid(false);
+  myFilter->SetInput(1, nd1);
+  MITK_TEST_CONDITION(myFilter->GetInput(1) == nd1, "testing Set-/GetInput(1)");
+  MITK_TEST_CONDITION(myFilter->GetInput(0) == nd0, "testing Set-/GetInput(0) again");
+  MITK_TEST_CONDITION(myFilter->GetOutput(1) != NULL, "testing GetOutput() after SetInput()");
+  MITK_TEST_CONDITION(myFilter->GetOutput(0) != myFilter->GetOutput(1), "testing GetOutput(0) different object than GetOutput(1)");
+
+  myFilter->SetInput(10, nd1);
+  MITK_TEST_CONDITION(myFilter->GetNumberOfInput() == 11, "testing SetInput(10) produces 11 outputs");
+  MITK_TEST_CONDITION(myFilter->GetInput(10) == nd1, "testing Set-/GetInput(10)");
+
+  // always end with this!
+  MITK_TEST_END();
+}

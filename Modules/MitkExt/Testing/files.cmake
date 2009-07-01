0f007908f3205f1f3e663ad5edaf398fed3657bd
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2009-07-01 17:37:15 +0000

    FIX (#2172): make some MitkExt unit tests run again

---
+++ Modules/MitkExt/Testing/files.cmake
@@ -0,0 +1,37 @@
+SET(MODULE_TESTS 
+  # itkGraphCutSegmentationFilterTest.cpp
+  # itkImageToTreeFilterTest.cpp
+  # itkThreadPoolTest.cpp
+  mitkAutoCropImageFilterTest.cpp
+  # mitkClosedSplineTest.cpp
+  mitkCompressedImageContainerTest.cpp
+  mitkContourMapper2DTest.cpp
+  mitkContourTest.cpp
+  mitkCylindricToCartesianFilterTest.cpp
+  # mitkDataTreeFilterTest.cpp
+  # mitkDataTreeTest.cpp
+  mitkDemonsRegistrationTest.cpp
+  mitkExternalToolsTest.cpp
+  mitkExtractImageFilterTest.cpp
+  # mitkGeometryTest.cpp
+  mitkHistogramMatchingTest.cpp
+  mitkLabeledImageToSurfaceFilterTest.cpp
+  mitkManualSegmentationToSurfaceFilterTest.cpp
+  mitkMeshTest.cpp
+  mitkOrganTypePropertyTest.cpp
+  mitkOverwriteSliceImageFilterTest.cpp
+  mitkPipelineSmartPointerCorrectnessTest.cpp
+  mitkPlaneFitTest.cpp
+  mitkPointLocatorTest.cpp
+  mitkReportGeneratorTest.cpp
+  # mitkRequestedRegionTest.cpp
+  mitkRigidRegistrationPresetTest.cpp
+  # mitkRingSplineTest.cpp
+  mitkSegmentationInterpolationTest.cpp
+  mitkSurfaceToImageFilterTest.cpp
+  mitkSymmetricForcesDemonsRegistrationTest.cpp
+  # mitkTestTemplate.cpp
+  # mitkToolManagerTest.cpp
+  mitkUnstructuredGridVtkWriterTest.cpp
+  # vtkPolyDataToPovRayMesh2Test.cpp
+)

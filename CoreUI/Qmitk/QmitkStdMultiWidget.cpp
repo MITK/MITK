94a87ecb8270c74262889db16f0d0c3d6ebd460d
Diana Wald <d.wald@dkfz-heidelberg.de>
2009-07-09 11:51:00 +0000

    STYLE: change initialization order in the constructor

--- CoreUI/Qmitk/QmitkStdMultiWidget.cpp
+++ CoreUI/Qmitk/QmitkStdMultiWidget.cpp
@@ -41,14 +41,14 @@ PURPOSE.  See the above copyright notices for more information.
 
 QmitkStdMultiWidget::QmitkStdMultiWidget(QWidget* parent, Qt::WindowFlags f)
 : QWidget(parent, f), 
-m_PlaneNode1(NULL), 
-m_PlaneNode2(NULL), 
-m_PlaneNode3(NULL), 
-m_Node(NULL),
 mitkWidget1(NULL),
 mitkWidget2(NULL),
 mitkWidget3(NULL),
-mitkWidget4(NULL)
+mitkWidget4(NULL),
+m_PlaneNode1(NULL), 
+m_PlaneNode2(NULL), 
+m_PlaneNode3(NULL), 
+m_Node(NULL)
 {
   /*******************************/
   //Create Widget manually

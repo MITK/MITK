d64638fad56492270e376ecf9f204f35140383fa
Xin Wang <xin.wang@dkfz-heidelberg.de>
2009-07-15 12:11:56 +0000

    COMP (#1868): added movie maker plugin, no compiling errors.

---
+++ Modules/Bundles/org.mitk.gui.qt.moviemaker/manifest.cpp
@@ -0,0 +1,54 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision: 17495 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include <Poco/ClassLibrary.h>
+
+#include <cherryIViewPart.h>
+#include "src/internal/QmitkMovieMaker.h"
+
+
+POCO_BEGIN_NAMED_MANIFEST(cherryIViewPart, cherry::IViewPart)
+  POCO_EXPORT_CLASS(::QmitkMovieMaker)
+POCO_END_MANIFEST
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Module:    $RCSfile$
+Language:  C++
+Date:      $Date$
+Version:   $Revision: 17495 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include <Poco/ClassLibrary.h>
+
+#include <cherryIViewPart.h>
+#include "src/internal/QmitkMovieMaker.h"
+
+
+POCO_BEGIN_NAMED_MANIFEST(cherryIViewPart, cherry::IViewPart)
+  POCO_EXPORT_CLASS(::QmitkMovieMaker)
+POCO_END_MANIFEST

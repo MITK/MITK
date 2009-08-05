c240a53ed530a2bb2bfca71958a81fcf23c28899
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2009-08-05 16:59:28 +0000

    COMP: added files.cmake for tests.

---
+++ Core/Code/Testing/files.cmake
@@ -0,0 +1,75 @@
+
+# tests with no extra command line parameter
+SET(MODULE_TESTS
+  mitkPointSetWriterTest.cpp
+  mitkMaterialPropertyTest.cpp
+  mitkDataTreeNodeTest.cpp
+  mitkActionTest.cpp
+  mitkDataStorageTest.cpp
+  mitkEnumerationPropertyTest.cpp
+  mitkEventTest.cpp
+  mitkFocusManagerTest.cpp
+  mitkGenericPropertyTest.cpp
+  mitkGeometry3DTest.cpp
+  mitkGeometryDataToSurfaceFilterTest.cpp
+  mitkGlobalInteractionTest.cpp
+  mitkImageDataItemTest.cpp
+  #mitkImageMapper2DTest.cpp
+  mitkImageTest.cpp
+  #mitkImageToItkTest.cpp
+  mitkInteractorTest.cpp
+  mitkITKThreadingTest.cpp
+  # mitkLevelWindowManagerTest.cpp
+  mitkLevelWindowTest.cpp
+  mitkMessageTest.cpp
+  #mitkPipelineSmartPointerCorrectnessTest.cpp
+  mitkPlaneGeometryTest.cpp
+  mitkPointSetFileIOTest.cpp
+  mitkPointSetTest.cpp
+  mitkPropertyListTest.cpp
+  #mitkRegistrationBaseTest.cpp
+  #mitkSegmentationInterpolationTest.cpp
+  mitkSlicedGeometry3DTest.cpp
+  mitkSliceNavigationControllerTest.cpp
+  mitkStateMachineTest.cpp
+  mitkStateMachineFactoryTest.cpp
+  #mitkStateTest.cpp
+  mitkSurfaceTest.cpp
+  mitkSurfaceToSurfaceFilterTest.cpp
+  mitkTimeSlicedGeometryTest.cpp
+  mitkTransitionTest.cpp
+  mitkUndoControllerTest.cpp
+  mitkVtkWidgetRenderingTest.cpp
+  mitkTransferFunctionTest.cpp
+  #mitkAbstractTransformGeometryTest.cpp
+  #mitkPicFileIOTest.cpp
+  #mitkStepperTest.cpp
+  
+)
+
+# test with image filename as an extra command line parameter
+SET(MODULE_IMAGE_TESTS
+  mitkSurfaceVtkWriterTest.cpp
+  mitkPicFileWriterTest.cpp
+  mitkImageWriterTest.cpp
+  #mitkImageSliceSelectorTest.cpp
+  mitkImageTimeSelectorTest.cpp
+  mitkPicFileReaderTest.cpp
+  # mitkVtkPropRendererTest.cpp
+)
+
+# list of images for which the tests are run
+SET(MODULE_TESTIMAGES
+  US4DCyl.pic.gz
+  Pic3D.pic.gz
+  Pic2DplusT.pic.gz
+  BallBinary30x30x30.pic.gz
+  binary.stl
+  ball.stl
+)
+
+SET(MODULE_CUSTOM_TESTS 
+    #mitkLabeledImageToSurfaceFilterTest.cpp
+    #mitkExternalToolsTest.cpp
+)
+

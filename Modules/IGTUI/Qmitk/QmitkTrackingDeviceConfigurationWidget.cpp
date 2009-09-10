2b1ea63eeab478ea56275ed8d7b9bb060e6224f7
Alfred Franz <a.franz@dkfz-heidelberg.de>
2009-09-10 11:47:20 +0000

    ENH (#2361): Further work on TrackingDeviceConfigurationWidget ... UI is working now, functionality will be added later.

--- Modules/IGTUI/Qmitk/QmitkTrackingDeviceConfigurationWidget.cpp
+++ Modules/IGTUI/Qmitk/QmitkTrackingDeviceConfigurationWidget.cpp
@@ -17,12 +17,18 @@ PURPOSE.  See the above copyright notices for more information.
 =========================================================================*/
 
 #include "QmitkTrackingDeviceConfigurationWidget.h"
+#include "mitkClaronTrackingDevice.h"
+#include "mitkNDITrackingDevice.h"
+#include "mitkSerialCommunication.h"
 
-
+const std::string QmitkTrackingDeviceConfigurationWidget::VIEW_ID = "org.mitk.views.trackingdeviceconfigurationwidget";
 
 QmitkTrackingDeviceConfigurationWidget::QmitkTrackingDeviceConfigurationWidget(QWidget* parent, Qt::WindowFlags f)
   : QWidget(parent, f)
 {
+  m_Controls = NULL;
+  this->CreateQtPartControl(this);
+  this->CreateConnections();
 }
 
 
@@ -32,10 +38,85 @@ QmitkTrackingDeviceConfigurationWidget::~QmitkTrackingDeviceConfigurationWidget(
 
 void QmitkTrackingDeviceConfigurationWidget::CreateQtPartControl(QWidget *parent)
 {
+  
   if (!m_Controls)
   {
   // create GUI widgets
   m_Controls = new Ui::QmitkTrackingDeviceConfigurationWidgetControls;
   m_Controls->setupUi(parent);
   }
+
+  //create connections
+  CreateConnections();
+}
+
+void QmitkTrackingDeviceConfigurationWidget::CreateConnections()
+{
+  if ( m_Controls )
+  {
+    connect( (QObject*)(m_Controls->m_trackingDeviceChooser), SIGNAL(currentIndexChanged(int)), this, SLOT(TrackingDeviceChanged()) );
+  }
+}
+
+void QmitkTrackingDeviceConfigurationWidget::TrackingDeviceChanged()
+{
+  //show the correspondig widget
+  m_Controls->m_TrackingSystemWidget->setCurrentIndex(m_Controls->m_trackingDeviceChooser->currentIndex());
+  
+  //the new trackingdevice is not configurated yet
+  m_TrackingDeviceConfigurated = false;
+  
+  if (m_Controls->m_trackingDeviceChooser->currentIndex()==0)//NDI Polaris
+      {
+      m_Controls->m_outputTextPolaris->setHtml("<body bgcolor=black><span style=\"color:#ffffff;\"><u>output:</u><br> NDI Polaris selected");
+      //m_Controls->m_outputTextPolaris->toPlainText().append(
+      /* => später zu finished verschieben!
+      mitk::NDITrackingDevice::Pointer tempTrackingDevice = mitk::NDITrackingDevice::New();
+      tempTrackingDevice->SetType(mitk::NDIPolaris);
+      
+      switch (m_Controls->m_comPortSpinBoxPolaris->value())
+        {
+        case 1: tempTrackingDevice->SetPortNumber(mitk::SerialCommunication::COM1); break;
+        case 2: tempTrackingDevice->SetPortNumber(mitk::SerialCommunication::COM2); break;
+        case 3: tempTrackingDevice->SetPortNumber(mitk::SerialCommunication::COM3); break;
+        case 4: tempTrackingDevice->SetPortNumber(mitk::SerialCommunication::COM4); break;
+        case 5: tempTrackingDevice->SetPortNumber(mitk::SerialCommunication::COM5); break;
+        case 6: tempTrackingDevice->SetPortNumber(mitk::SerialCommunication::COM6); break;
+        case 7: tempTrackingDevice->SetPortNumber(mitk::SerialCommunication::COM7); break;
+        case 8: tempTrackingDevice->SetPortNumber(mitk::SerialCommunication::COM8); break;
+        }
+      
+      m_TrackingDevice = tempTrackingDevice;
+      */
+      } 
+  else if (m_Controls->m_trackingDeviceChooser->currentIndex()==1)//NDI Aurora
+      {
+        m_Controls->m_outputTextAurora->setHtml("<body bgcolor=black><span style=\"color:#ffffff;\"><u>output:</u><br> NDI Aurora selected");
+      }
+  else if (m_Controls->m_trackingDeviceChooser->currentIndex()==2)//ClaronTechnology MicronTracker 2
+      {
+        m_Controls->m_outputTextMicronTracker->setHtml("<body bgcolor=black><span style=\"color:#ffffff;\"><u>output:</u><br> MicronTracker selected");
+      }
+}
+
+void QmitkTrackingDeviceConfigurationWidget::EnableUserReset(bool enable)
+{
+  if (enable) m_Controls->m_resetButton->setVisible(true);
+  else m_Controls->m_resetButton->setVisible(false);
+}
+
+void QmitkTrackingDeviceConfigurationWidget::TestConnection()
+{
+if (m_Controls->m_trackingDeviceChooser->currentIndex()==0)//NDI Polaris
+      {
+      }
+else if (m_Controls->m_trackingDeviceChooser->currentIndex()==1)//NDI Aurora
+      {
+      }
+else if (m_Controls->m_trackingDeviceChooser->currentIndex()==2)//ClaronTechnology MicronTracker 2
+      {
+      mitk::ClaronTrackingDevice::Pointer tempTrackingDevice = mitk::ClaronTrackingDevice::New();
+      m_Controls->m_outputTextMicronTracker->setHtml(m_Controls->m_outputTextMicronTracker->toHtml().append("<br>Testing connect..."));
+      }
+
 }
--- Modules/IGTUI/Qmitk/QmitkTrackingDeviceConfigurationWidget.h
+++ Modules/IGTUI/Qmitk/QmitkTrackingDeviceConfigurationWidget.h
@@ -22,19 +22,82 @@ PURPOSE.  See the above copyright notices for more information.
 #include <QWidget>
 #include "MitkIGTUIExports.h"
 #include "ui_QmitkTrackingDeviceConfigurationWidgetControls.h"
+#include "mitkTrackingDevice.h"
 
+ /** Documentation:
+  *   \brief An object of this class offers a UI to configurate
+  *          a tracking device. If the user finished the configuration process and
+  *          a fully configurated tracking device is availiabe the object sends a
+  *          signal "TrackingDeviceConfigurationFinished()". You can then get the
+  *          tracking device by calling the method GetTrackingDevice().
+  *          
+  *          Once the tracking device is configurated there are two ways to reset
+  *          the UI to allow the user for configuring a new device. The method Reset()
+  *          can be called and there is also a button "reset" which can be pressed by
+  *          the user. In both cases a signal "TrackingDeviceConfigurationReseted()"
+  *          is sent and you may wait for a new configurated tracking device.
+  *
+  *          The possibility to reset the configuration by the user can also be switched
+  *          of by calling the method EnableUserReset(boolean enable).
+  *
+  *   \ingroup IGTUI
+  */
 class MitkIGTUI_EXPORT QmitkTrackingDeviceConfigurationWidget : public QWidget
 {
   Q_OBJECT
 
   public:
+    static const std::string VIEW_ID;
+
     QmitkTrackingDeviceConfigurationWidget(QWidget* parent = 0, Qt::WindowFlags f = 0);
     ~QmitkTrackingDeviceConfigurationWidget();
+    
+    /* @return Returns the current configurated tracking device. If the user didn't finished the
+     *         configuration process NULL is returned.
+     */ 
+    mitk::TrackingDevice::Pointer GetTrackingDevice();
+    
+    /* @brief Resets the UI to allow the user for configurating a new tracking device.
+     */
+    void Reset();
 
-     virtual void CreateQtPartControl(QWidget *parent);
-  
+    /* @brief Sets our unsets the possibility to reset the UI and start
+     *        a new configuration by the user. Concretely this means the
+     *        button "reset" is shown or not.
+     */
+    void EnableUserReset(bool enable);
+
+  signals:
+
+    /* @brief This signal is sent if the user has finished the configuration of the tracking device.
+     *        The device is now availiable if the method GetTrackingDevice() is called. The tracking
+     *        device you'll get is completly configurated but no tools are added yet.
+     */
+    void TrackingDeviceConfigurationFinished();
+
+    /* @brief This signal is sent if the UI was reseted and the user is required to configurate 
+     *        a new tracking device.
+     */
+    void TrackingDeviceConfigurationReseted();
+
   protected:
+    
+    /// \brief Creation of the connections
+    virtual void CreateConnections();
+
+    virtual void CreateQtPartControl(QWidget *parent);
+
     Ui::QmitkTrackingDeviceConfigurationWidgetControls* m_Controls;
+
+    mitk::TrackingDevice::Pointer m_TrackingDevice;
+    bool m_TrackingDeviceConfigurated;
+
+  protected slots:
+    /* @brief This method is called when the user changes the selection of the trackingdevice (m_trackingDeviceChooser).
+              It then sets the correct widget for the selected tracking device.*/
+    void TrackingDeviceChanged();
+
+    void TestConnection();
 
 };
 #endif

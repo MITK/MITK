5b4e7e9004e474f6e9473cf9e35d3e83bb50cb99
Daniel Maleike <d.maleike@dkfz-heidelberg.de>
2009-10-09 11:38:25 +0000

    FIX (#2183): ask user for a filename for loading/saving

--- Modules/Bundles/org.mitk.gui.qt.sceneserialization/src/internal/QmitkSceneSerializationView.cpp
+++ Modules/Bundles/org.mitk.gui.qt.sceneserialization/src/internal/QmitkSceneSerializationView.cpp
@@ -24,7 +24,13 @@ PURPOSE.  See the above copyright notices for more information.
 #include "QmitkDataStorageTableModel.h"
 
 #include "mitkSceneIO.h"
+#include "mitkNodePredicateNOT.h"
+#include "mitkNodePredicateProperty.h"
+#include "mitkProperties.h"
+#include "mitkProgressBar.h"
 
+#include <QFileDialog>
+#include <QMessageBox>
 
 const std::string QmitkSceneSerializationView::VIEW_ID = "org.mitk.views.sceneserialization";
 
@@ -85,62 +91,131 @@ void QmitkSceneSerializationView::Deactivated()
 
 void QmitkSceneSerializationView::SerializeSelected()
 {
-  mitk::SceneIO::Pointer sceneIO = mitk::SceneIO::New();
-
-  /*  TODO respect user selection in list view
-   *  intermediate step: create new data storage, add all selected nodes
-   *  OR: create a predicate ORing all selected nodes
-   */
-  mitk::DataStorage::Pointer storage = this->GetDefaultDataStorage();
-
-  /*  TODO ask the user for a file name */
-  sceneIO->SaveScene( storage, "scene.zip" );
-
-  mitk::SceneIO::FailedBaseDataListType::ConstPointer failedNodes = sceneIO->GetFailedNodes();
-  if (failedNodes.IsNotNull())
+  QString sceneFilename;
+  try
   {
-    std::cout << "The following nodes could not be serialized:" << std::endl;
-    for ( mitk::SceneIO::FailedBaseDataListType::const_iterator iter = failedNodes->begin();
-          iter != failedNodes->end();
-          ++iter )
+    // first ask user for a filename 
+    bool niceFilenameFound(false);
+    while (!niceFilenameFound)
     {
-      std::cout << " - ";
-      if ( mitk::BaseData* data =(*iter)->GetData() )
+      sceneFilename = QFileDialog::getSaveFileName( m_Parent, "Load MITK scene", QString::null, "MITK scene files (*.mitk)", NULL );
+
+      if (sceneFilename.isEmpty()) return;
+
+      if ( sceneFilename.right(5) != ".mitk" ) sceneFilename += ".mitk";
+
+      std::ifstream exists( sceneFilename.toLocal8Bit().constData() );
+      if (exists)
       {
-        std::cout << data->GetNameOfClass();
+        int result = QMessageBox::question(NULL,
+                                           "Overwrite file?", 
+                                           sceneFilename + " already exists. \n\nDo you want to OVERWRITE this file?",
+                                           QMessageBox::Yes,
+                                           QMessageBox::No,
+                                           QMessageBox::Cancel);
+
+        if (result == QMessageBox::Cancel) return;
+
+        niceFilenameFound = QMessageBox::Yes == result;
       }
       else
       {
-        std::cout << "(NULL)";
+        niceFilenameFound = true;
+      }
+    }
+
+    // really save now
+    mitk::SceneIO::Pointer sceneIO = mitk::SceneIO::New();
+
+    /*  TODO respect user selection in list view
+     *  intermediate step: create new data storage, add all selected nodes
+     *  OR: create a predicate ORing all selected nodes
+     */
+    mitk::DataStorage::Pointer storage = this->GetDefaultDataStorage();
+
+    mitk::ProgressBar::GetInstance()->AddStepsToDo(2);
+    if ( !sceneIO->SaveScene( storage, sceneFilename.toLocal8Bit().constData() ) )
+    {
+      QMessageBox::information(NULL,
+                            "Scene saving", 
+                            "Scene could not be written completely. Please check the log.", 
+                            QMessageBox::Ok);
+
+    }
+    mitk::ProgressBar::GetInstance()->Progress(2);
+
+    mitk::SceneIO::FailedBaseDataListType::ConstPointer failedNodes = sceneIO->GetFailedNodes();
+    if (failedNodes.IsNotNull())
+    {
+      std::stringstream ss;
+      ss << "The following nodes could not be serialized:" << std::endl;
+      for ( mitk::SceneIO::FailedBaseDataListType::const_iterator iter = failedNodes->begin();
+            iter != failedNodes->end();
+            ++iter )
+      {
+        ss << " - ";
+        if ( mitk::BaseData* data =(*iter)->GetData() )
+        {
+          ss << data->GetNameOfClass();
+        }
+        else
+        {
+          ss << "(NULL)";
+        }
+
+        ss << " contained in node '" << (*iter)->GetName() << "'" << std::endl;
       }
 
-      std::cout << " contained in node '" << (*iter)->GetName() << "'" << std::endl;
+      LOG_WARN << ss.str();
+    }
+
+    mitk::PropertyList::ConstPointer failedProperties = sceneIO->GetFailedProperties();
+    if (failedProperties.IsNotNull())
+    {
+      std::stringstream ss;
+      ss << "The following properties could not be serialized:" << std::endl;
+      const mitk::PropertyList::PropertyMap* propmap = failedProperties->GetMap();
+      for ( mitk::PropertyList::PropertyMap::const_iterator iter = propmap->begin();
+            iter != propmap->end();
+            ++iter )
+      {
+        ss << " - " << iter->second.first->GetNameOfClass() << " associated to key '" << iter->first << "'" << std::endl;
+      }
+
+      LOG_WARN << ss.str();
     }
   }
-
-  mitk::PropertyList::ConstPointer failedProperties = sceneIO->GetFailedProperties();
-  if (failedProperties.IsNotNull())
+  catch (std::exception& e)
   {
-    std::cout << "The following properties could not be serialized:" << std::endl;
-    const mitk::PropertyList::PropertyMap* propmap = failedProperties->GetMap();
-    for ( mitk::PropertyList::PropertyMap::const_iterator iter = propmap->begin();
-          iter != propmap->end();
-          ++iter )
-    {
-      std::cout << " - " << iter->second.first->GetNameOfClass() << " associated to key '" << iter->first << "'" << std::endl;
-    }
+    LOG_ERROR << "Exception caught during scene saving: " << e.what();
   }
 
 }
 
 void QmitkSceneSerializationView::LoadSceneFile()
 {
+  /* ask the user for a file name */
+  QString sceneFilename = QFileDialog::getOpenFileName( m_Parent, "Load MITK scene", QDir::currentPath(), "MITK scene files (*.mitk)", NULL );
+
+  if (sceneFilename.isEmpty()) return;
+
   mitk::SceneIO::Pointer sceneIO = mitk::SceneIO::New();
 
   mitk::DataStorage::Pointer storage = this->GetDefaultDataStorage();
 
-  /*  TODO ask the user for a file name */
-  mitk::DataStorage::Pointer newStorage = sceneIO->LoadScene( "scene.zip", storage );
+  bool clearDataStorageFirst(false);
+  mitk::ProgressBar::GetInstance()->AddStepsToDo(2);
+  mitk::DataStorage::Pointer newStorage = sceneIO->LoadScene( sceneFilename.toLocal8Bit().constData(), storage, clearDataStorageFirst );
+  mitk::ProgressBar::GetInstance()->Progress(2);
+
+  // perform reset on multiwidget
+  /* get all nodes that have not set "includeInBoundingBox" to false */
+  mitk::NodePredicateNOT::Pointer pred = mitk::NodePredicateNOT::New(mitk::NodePredicateProperty::New("includeInBoundingBox", mitk::BoolProperty::New(false)));
+  mitk::DataStorage::SetOfObjects::ConstPointer rs = storage->GetSubset(pred);
+  /* calculate bounding geometry of these nodes */
+  mitk::TimeSlicedGeometry::Pointer bounds = storage->ComputeBoundingGeometry3D(rs);
+  /* initialize the views to the bounding geometry */
+  mitk::RenderingManager::GetInstance()->InitializeViews(bounds);
 
   assert( newStorage.GetPointer() == NULL || newStorage.GetPointer() == storage.GetPointer() );
 }

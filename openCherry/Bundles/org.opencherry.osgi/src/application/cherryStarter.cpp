ce96b26bce303cfee961d842e8721007c7b8e290
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2009-10-19 18:41:37 +0000

    FIX (#2447): removing "global" try/catch block to be able to see the call stack if an exception is thrown

--- openCherry/Bundles/org.opencherry.osgi/src/application/cherryStarter.cpp
+++ openCherry/Bundles/org.opencherry.osgi/src/application/cherryStarter.cpp
@@ -41,120 +41,114 @@ int Starter::Run(int& argc, char** argv,
   InternalPlatform* platform = InternalPlatform::GetInstance();
 
   int returnCode = 0;
-  try
+
+  // startup the internal platform
+  platform->Initialize(argc, argv, config);
+  platform->Launch();
+
+  bool consoleLog = platform->ConsoleLog();
+
+  // run the application
+  IExtensionPointService::Pointer service =
+      platform->GetExtensionPointService();
+  if (service == 0)
   {
-    // startup the internal platform
-    platform->Initialize(argc, argv, config);
-    platform->Launch();
+    platform->GetLogger()->log(
+        Poco::Message(
+            "Starter",
+            "The extension point service could not be retrieved. This usually indicates that the openCherry OSGi plug-in could not be loaded.",
+            Poco::Message::PRIO_FATAL));
+    std::unexpected();
+    returnCode = 1;
+  }
+  else
+  {
+    IConfigurationElement::vector extensions(
+        service->GetConfigurationElementsFor(Starter::XP_APPLICATIONS));
+    IConfigurationElement::vector::iterator iter;
 
-    bool consoleLog = platform->ConsoleLog();
-
-    // run the application
-    IExtensionPointService::Pointer service =
-        platform->GetExtensionPointService();
-    if (service == 0)
+    for (iter = extensions.begin(); iter != extensions.end();)
     {
-      platform->GetLogger()->log(
-          Poco::Message(
-              "Starter",
-              "The extension point service could not be retrieved. This usually indicates that the openCherry OSGi plug-in could not be loaded.",
-              Poco::Message::PRIO_FATAL));
-      std::unexpected();
+      if ((*iter)->GetName() != "application")
+        iter = extensions.erase(iter);
+      else
+        ++iter;
+    }
+
+    std::string argApplication = Platform::GetConfiguration().getString(
+        Platform::ARG_APPLICATION, "");
+
+    IApplication* app = 0;
+    if (extensions.size() == 0)
+    {
+      CHERRY_FATAL
+          << "No extensions configured into extension-point 'org.opencherry.core.runtime.applications' found. Aborting.\n";
+      returnCode = 0;
+    }
+    else if (extensions.size() == 1)
+    {
+      if (!argApplication.empty())
+        CHERRY_INFO(consoleLog)
+            << "One 'org.opencherry.core.runtime.applications' extension found, ignoring "
+            << Platform::ARG_APPLICATION << " argument.\n";
+      std::vector<IConfigurationElement::Pointer> runs(
+          extensions[0]->GetChildren("run"));
+      app = runs.front()->CreateExecutableExtension<IApplication> ("class");
+    }
+    else
+    {
+      if (argApplication.empty())
+      {
+        CHERRY_WARN << "You must provide an application id via \""
+            << Platform::ARG_APPLICATION << "=<id>\"";
+        CHERRY_INFO << "Possible application ids are:";
+        for (iter = extensions.begin(); iter != extensions.end(); ++iter)
+        {
+          std::string appid;
+          if ((*iter)->GetAttribute("id", appid) && !appid.empty())
+          {
+            CHERRY_INFO << appid;
+          }
+        }
+        returnCode = 0;
+      }
+      else
+      {
+        for (iter = extensions.begin(); iter != extensions.end(); ++iter)
+        {
+          CHERRY_INFO(consoleLog) << "Checking applications extension from: "
+              << (*iter)->GetContributor() << std::endl;
+
+          std::string appid;
+          if ((*iter)->GetAttribute("id", appid))
+          {
+            CHERRY_INFO(consoleLog) << "Found id: " << appid << std::endl;
+            if (appid.size() > 0 && appid == argApplication)
+            {
+              std::vector<IConfigurationElement::Pointer> runs(
+                  (*iter)->GetChildren("run"));
+              app = runs.front()->CreateExecutableExtension<IApplication> (
+                  "class");
+              break;
+            }
+          }
+          else
+            throw CoreException("missing attribute", "id");
+        }
+      }
+    }
+
+    if (app == 0)
+    {
+      CHERRY_ERROR
+          << "Could not create executable application extension for id: "
+          << argApplication << std::endl;
       returnCode = 1;
     }
     else
     {
-      IConfigurationElement::vector extensions(
-          service->GetConfigurationElementsFor(Starter::XP_APPLICATIONS));
-      IConfigurationElement::vector::iterator iter;
-
-      for (iter = extensions.begin(); iter != extensions.end();)
-      {
-        if ((*iter)->GetName() != "application")
-          iter = extensions.erase(iter);
-        else
-          ++iter;
-      }
-
-      std::string argApplication = Platform::GetConfiguration().getString(
-          Platform::ARG_APPLICATION, "");
-
-      IApplication* app = 0;
-      if (extensions.size() == 0)
-      {
-        CHERRY_FATAL
-            << "No extensions configured into extension-point 'org.opencherry.core.runtime.applications' found. Aborting.\n";
-        returnCode = 0;
-      }
-      else if (extensions.size() == 1)
-      {
-        if (!argApplication.empty())
-          CHERRY_INFO(consoleLog)
-              << "One 'org.opencherry.core.runtime.applications' extension found, ignoring "
-              << Platform::ARG_APPLICATION << " argument.\n";
-        std::vector<IConfigurationElement::Pointer> runs(
-            extensions[0]->GetChildren("run"));
-        app = runs.front()->CreateExecutableExtension<IApplication> ("class");
-      }
-      else
-      {
-        if (argApplication.empty())
-        {
-          CHERRY_WARN << "You must provide an application id via \""
-              << Platform::ARG_APPLICATION << "=<id>\"";
-          CHERRY_INFO << "Possible application ids are:";
-          for (iter = extensions.begin(); iter != extensions.end(); ++iter)
-          {
-            std::string appid;
-            if ((*iter)->GetAttribute("id", appid) && !appid.empty())
-            {
-              CHERRY_INFO << appid;
-            }
-          }
-          returnCode = 0;
-        }
-        else
-        {
-          for (iter = extensions.begin(); iter != extensions.end(); ++iter)
-          {
-            CHERRY_INFO(consoleLog) << "Checking applications extension from: "
-                << (*iter)->GetContributor() << std::endl;
-
-            std::string appid;
-            if ((*iter)->GetAttribute("id", appid))
-            {
-              CHERRY_INFO(consoleLog) << "Found id: " << appid << std::endl;
-              if (appid.size() > 0 && appid == argApplication)
-              {
-                std::vector<IConfigurationElement::Pointer> runs(
-                    (*iter)->GetChildren("run"));
-                app = runs.front()->CreateExecutableExtension<IApplication> (
-                    "class");
-                break;
-              }
-            }
-            else
-              throw CoreException("missing attribute", "id");
-          }
-        }
-      }
-
-      if (app == 0)
-      {
-        CHERRY_ERROR
-            << "Could not create executable application extension for id: "
-            << argApplication << std::endl;
-        returnCode = 1;
-      }
-      else
-      {
-        returnCode = app->Start();
-      }
+      returnCode = app->Start();
     }
-  } catch (...)
-  {
-     platform->Shutdown();
-     throw;
   }
 
   platform->Shutdown();

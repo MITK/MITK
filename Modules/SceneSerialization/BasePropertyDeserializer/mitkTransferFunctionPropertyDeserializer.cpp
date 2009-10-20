aa96f195ac42b72870c0c0f5cfeab992201b3ed7
Jochen Neuhaus <j.neuhaus@dkfz-heidelberg.de>
2009-10-20 11:39:14 +0000

    ENH (#2183): add serialization code for TransferFunctionProperty

--- Modules/SceneSerialization/BasePropertyDeserializer/mitkTransferFunctionPropertyDeserializer.cpp
+++ Modules/SceneSerialization/BasePropertyDeserializer/mitkTransferFunctionPropertyDeserializer.cpp
@@ -35,9 +35,65 @@ class SceneSerialization_EXPORT TransferFunctionPropertyDeserializer : public Ba
 
     virtual BaseProperty::Pointer Deserialize(TiXmlElement* element)
     {
-      if (!element) return NULL;
+      if (!element) 
+        return NULL;
       
-      return NULL;
+      TransferFunction::Pointer tf = TransferFunction::New();
+
+      // deserialize scalar opacity function
+      TiXmlElement* scalarOpacityPointlist = element->FirstChildElement("ScalarOpacity");
+      if (scalarOpacityPointlist == NULL)
+        return NULL;
+      for( TiXmlElement* pointElement = scalarOpacityPointlist->FirstChildElement("point"); pointElement != NULL; pointElement = pointElement->NextSiblingElement("point"))
+      {
+        double x;
+        double y;
+        if (pointElement->QueryDoubleAttribute("x", &x) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        if (pointElement->QueryDoubleAttribute("y", &y) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        tf->AddScalarOpacityPoint(x, y);
+      }
+
+      TiXmlElement* gradientOpacityPointlist = element->FirstChildElement("GradientOpacity");
+      if (gradientOpacityPointlist == NULL)
+        return NULL;
+      for( TiXmlElement* pointElement = gradientOpacityPointlist->FirstChildElement("point"); pointElement != NULL; pointElement = pointElement->NextSiblingElement("point"))
+      {
+        double x;
+        double y;
+        if (pointElement->QueryDoubleAttribute("x", &x) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        if (pointElement->QueryDoubleAttribute("y", &y) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        tf->AddGradientOpacityPoint(x, y);
+      }
+
+      TiXmlElement* rgbPointlist = element->FirstChildElement("Color");
+      if (rgbPointlist == NULL)
+        return NULL;
+      vtkColorTransferFunction* ctf = tf->GetColorTransferFunction();
+      if (ctf == NULL)
+        return NULL;
+      for( TiXmlElement* pointElement = rgbPointlist->FirstChildElement("point"); pointElement != NULL; pointElement = pointElement->NextSiblingElement("point"))
+      {
+        double x;
+        double r,g,b, midpoint, sharpness;
+        if (pointElement->QueryDoubleAttribute("x", &x) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        if (pointElement->QueryDoubleAttribute("r", &r) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        if (pointElement->QueryDoubleAttribute("g", &g) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        if (pointElement->QueryDoubleAttribute("b", &b) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        if (pointElement->QueryDoubleAttribute("midpoint", &midpoint) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        if (pointElement->QueryDoubleAttribute("sharpness", &sharpness) == TIXML_WRONG_TYPE)
+          return NULL; // TODO: can we do a better error handling?
+        ctf->AddRGBPoint(x, r, g, b, midpoint, sharpness);
+      }
+      return TransferFunctionProperty::New(tf).GetPointer();
     }
 
   protected:

18383119820440375a52c7730a1da70ffee86dec
Jochen Neuhaus <j.neuhaus@dkfz-heidelberg.de>
2009-10-20 11:40:07 +0000

    FIX (#2183): fix serialization of color transfer function

--- Modules/SceneSerialization/BasePropertySerializer/mitkTransferFunctionPropertySerializer.cpp
+++ Modules/SceneSerialization/BasePropertySerializer/mitkTransferFunctionPropertySerializer.cpp
@@ -37,16 +37,16 @@ class SceneSerialization_EXPORT TransferFunctionPropertySerializer : public Base
     {
       if (const TransferFunctionProperty* prop = dynamic_cast<const TransferFunctionProperty*>(m_Property.GetPointer()))
       {
-        TransferFunction* tranferfunction = prop->GetValue();
-        if (!tranferfunction) return NULL;
+        TransferFunction* transferfunction = prop->GetValue();
+        if (!transferfunction) 
+          return NULL;
 
         TiXmlElement* element = new TiXmlElement("TransferFunction");
 
         // serialize scalar opacity function
         TiXmlElement* scalarOpacityPointlist = new TiXmlElement( "ScalarOpacity" );
 
-        TransferFunction::ControlPoints scalarOpacityPoints = 
-          tranferfunction->GetScalarOpacityPoints();
+        TransferFunction::ControlPoints scalarOpacityPoints = transferfunction->GetScalarOpacityPoints();
         for ( TransferFunction::ControlPoints::iterator iter = scalarOpacityPoints.begin();
           iter != scalarOpacityPoints.end();
           ++iter )
@@ -56,15 +56,10 @@ class SceneSerialization_EXPORT TransferFunctionPropertySerializer : public Base
           pointel->SetDoubleAttribute("y", iter->second);
           scalarOpacityPointlist->LinkEndChild( pointel );
         }
-
         element->LinkEndChild( scalarOpacityPointlist );
-
-
         // serialize gradient opacity function
         TiXmlElement* gradientOpacityPointlist = new TiXmlElement( "GradientOpacity" );
-
-        TransferFunction::ControlPoints gradientOpacityPoints = 
-          tranferfunction->GetGradientOpacityPoints();
+        TransferFunction::ControlPoints gradientOpacityPoints = transferfunction->GetGradientOpacityPoints();
         for ( TransferFunction::ControlPoints::iterator iter = gradientOpacityPoints.begin();
           iter != gradientOpacityPoints.end();
           ++iter )
@@ -74,43 +69,36 @@ class SceneSerialization_EXPORT TransferFunctionPropertySerializer : public Base
           pointel->SetDoubleAttribute("y", iter->second);
           gradientOpacityPointlist->LinkEndChild( pointel );
         }
-
         element->LinkEndChild( gradientOpacityPointlist );
 
-
         // serialize color function
-        TransferFunction::RGBControlPoints points = tranferfunction->GetRGBPoints();
-        TiXmlElement* pointlist = new TiXmlElement("RGB");
-
-        for ( TransferFunction::RGBControlPoints::iterator iter = points.begin();
-              iter != points.end();
-              ++iter )
+        vtkColorTransferFunction* ctf = transferfunction->GetColorTransferFunction();
+        if (ctf == NULL)
+          return NULL;
+        TiXmlElement* pointlist = new TiXmlElement("Color");
+        for (int i = 0; i < ctf->GetSize(); i++ )
         {
+          double myVal[6];
+          ctf->GetNodeValue(i, myVal);
           TiXmlElement* pointel = new TiXmlElement("point");
-          pointel->SetDoubleAttribute("x", iter->first);
-          pointel->SetDoubleAttribute("r", iter->second[0]);
-          pointel->SetDoubleAttribute("g", iter->second[1]);
-          pointel->SetDoubleAttribute("b", iter->second[2]);
+          pointel->SetDoubleAttribute("x", myVal[0]);
+          pointel->SetDoubleAttribute("r", myVal[1]);
+          pointel->SetDoubleAttribute("g", myVal[2]);
+          pointel->SetDoubleAttribute("b", myVal[3]);
+          pointel->SetDoubleAttribute("midpoint", myVal[4]);
+          pointel->SetDoubleAttribute("sharpness", myVal[5]);
           pointlist->LinkEndChild( pointel );
         }
-
         element->LinkEndChild( pointlist );
-
         return element;
       }
       else return NULL;
     }
-
   protected:
-
     TransferFunctionPropertySerializer() {}
     virtual ~TransferFunctionPropertySerializer() {}
 };
-
 } // namespace
-
 // important to put this into the GLOBAL namespace (because it starts with 'namespace mitk')
 MITK_REGISTER_SERIALIZER(TransferFunctionPropertySerializer);
-
 #endif
-

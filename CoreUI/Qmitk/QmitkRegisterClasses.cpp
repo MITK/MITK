d837ce900d62cc86093bcafc678478ff6b081fd9
Tobias Schwarz <tobias.schwarz@dkfz-heidelberg.de>
2009-10-28 08:38:01 +0000

    FIX (#2413): If GlobalInteraction is not already initialized, do it here. (Needed for tutorial)

--- CoreUI/Qmitk/QmitkRegisterClasses.cpp
+++ CoreUI/Qmitk/QmitkRegisterClasses.cpp
@@ -1,25 +1,26 @@
 /*=========================================================================
- 
+
 Program:   Medical Imaging & Interaction Toolkit
 Module:    $RCSfile: mitkPropertyManager.cpp,v $
 Language:  C++
 Date:      $Date$
 Version:   $Revision$
- 
+
 Copyright (c) German Cancer Research Center, Division of Medical and
 Biological Informatics. All rights reserved.
 See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
- 
+
 This software is distributed WITHOUT ANY WARRANTY; without even
 the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
- 
+
 =========================================================================*/
 
 #include "QmitkRegisterClasses.h"
 
 #include "QmitkApplicationCursor.h"
 #include "QmitkRenderingManagerFactory.h"
+#include "mitkGlobalInteraction.h"
 
 #include <iostream>
 
@@ -29,17 +30,21 @@ void QmitkRegisterClasses()
   if (!alreadyDone)
   {
     std::cout << "QmitkRegisterClasses()" << std::endl;
-  
 
-  //We have to put this in a file containing a class that is directly used
-  //somewhere. Otherwise, e.g. when put in VtkRenderWindowInteractor.cpp, 
-  //it is removed by the linker. 
 
-  // Create and register RenderingManagerFactory for this platform.
-  static QmitkRenderingManagerFactory qmitkRenderingManagerFactory;
-  static QmitkApplicationCursor globalQmitkApplicationCursor; // create one instance
+    //We have to put this in a file containing a class that is directly used
+    //somewhere. Otherwise, e.g. when put in VtkRenderWindowInteractor.cpp, 
+    //it is removed by the linker.
 
-  alreadyDone = true;
+    // Create and initialize GlobalInteraction
+    if(! (mitk::GlobalInteraction::GetInstance()->IsInitialized()))
+      mitk::GlobalInteraction::GetInstance()->Initialize("global");
+
+    // Create and register RenderingManagerFactory for this platform.
+    static QmitkRenderingManagerFactory qmitkRenderingManagerFactory;
+    static QmitkApplicationCursor globalQmitkApplicationCursor; // create one instance
+
+    alreadyDone = true;
   }
 }
 

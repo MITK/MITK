1b80e8e2212d44d741682dc6e45edad886559e27
Klaus Fritzsche <k.fritzsche@dkfz-heidelberg.de>
2009-11-01 12:40:08 +0000

    FIX (#2107): clean up

--- Modules/DiffusionImaging/Reconstruction/itkPointShell.h
+++ Modules/DiffusionImaging/Reconstruction/itkPointShell.h
@@ -4,26 +4,12 @@
 #include "mitkQBallImage.h"
 #include "MitkDiffusionImagingExports.h"
 
-//#if QBALL_ODFSIZE != 12 && QBALL_ODFSIZE != 42 && QBALL_ODFSIZE != 92 \
-//  && QBALL_ODFSIZE != 162 && QBALL_ODFSIZE != 252 && QBALL_ODFSIZE != 362 \
-//   && QBALL_ODFSIZE != 492 && QBALL_ODFSIZE != 642 && QBALL_ODFSIZE != 812 \
-//    && QBALL_ODFSIZE != 1002
-//  #define POINT_SHELL_USE_GENERIC_TEMPLATE
-//#endif
-//
-//#ifdef POINT_SHELL_USE_GENERIC_TEMPLATE
-//#define MY_POINT_SHELL_EXPORT
-//#else
-//#define MY_POINT_SHELL_EXPORT MitkDiffusionImaging_EXPORT
-//#endif
-
 namespace itk
 {
 
 #define DIST_POINTSHELL_PI 3.14159265358979323846
 
   // generate by n-fold subdivisions of an icosahedron
-
   template<int NPoints, class TMatrixType >
   class PointShell
   {
@@ -33,8 +19,6 @@ namespace itk
 
 }
 
-//#ifdef POINT_SHELL_USE_GENERIC_TEMPLATE
 #include "itkPointShell.txx"
-//#endif
 
 #endif //__itkPointShell_h__

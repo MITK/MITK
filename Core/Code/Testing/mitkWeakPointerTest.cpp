2e22e257f4dc4fee801959c02d0c60d042db34d7
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2009-11-09 07:56:43 +0000

    FIX (#2803): Enhanced unit test

--- Core/Code/Testing/mitkWeakPointerTest.cpp
+++ Core/Code/Testing/mitkWeakPointerTest.cpp
@@ -20,39 +20,33 @@
 #include <mitkWeakPointer.h>
 #include <itkObject.h>
 
-namespace itk
-{
-class WeakPointerTestObject : public itk::Object
-{
-public:
-
-  itkTypeMacro(WeakPointerTestObject, Object);
-
-  ~WeakPointerTestObject()
-  {
-    std::cout << "Deleting WeakPointerTestObject\n";
-  }
-};
-}
-
 int mitkWeakPointerTest(int /*argc*/, char* /*argv*/[])
 {
 
   MITK_TEST_BEGIN("WeakPointer")
 
+  mitk::WeakPointer<itk::Object> weakPointer;
+  mitk::WeakPointer<itk::Object> weakPointer2;
+
   // Testing constructors and reference counting
-  itk::Object::Pointer smartPointer = itk::WeakPointerTestObject::New();
-  mitk::WeakPointer<itk::Object> weakPointer(smartPointer);
-  mitk::WeakPointer<itk::Object> weakPointer2(weakPointer);
+  itk::Object::Pointer smartPointer = itk::Object::New();
+  mitk::WeakPointer<itk::Object> weakPointer3(smartPointer);
+  mitk::WeakPointer<itk::Object> weakPointer4(weakPointer);
   {
     itk::Object::Pointer tmpSmartPointer(weakPointer);
     itk::Object::Pointer tmpSmartPointer2(weakPointer2);
     MITK_TEST_CONDITION_REQUIRED(tmpSmartPointer.GetPointer() == tmpSmartPointer2.GetPointer(), "Testing equal pointers");
   }
+
+  weakPointer = smartPointer;
+  weakPointer2 = weakPointer;
+
   MITK_TEST_CONDITION_REQUIRED(1 == smartPointer->GetReferenceCount(), "Testing reference count");
   smartPointer = 0;
-  MITK_TEST_CONDITION_REQUIRED(weakPointer.IsNull(), "Testing expired weak pointer");
-  MITK_TEST_CONDITION_REQUIRED(weakPointer2.IsNull(), "Testing expired weak pointer 2")
+  MITK_TEST_CONDITION_REQUIRED(weakPointer.IsNull(), "Testing expired weak pointer (smart pointer assignment)");
+  MITK_TEST_CONDITION_REQUIRED(weakPointer2.IsNull(), "Testing expired weak pointer (weak pointer assignment)");
+  MITK_TEST_CONDITION_REQUIRED(weakPointer3.IsNull(), "Testing expired weak pointer (smart pointer constructor)");
+  MITK_TEST_CONDITION_REQUIRED(weakPointer4.IsNull(), "Testing expired weak pointer (copy constructor)")
 
   MITK_TEST_END()
 }

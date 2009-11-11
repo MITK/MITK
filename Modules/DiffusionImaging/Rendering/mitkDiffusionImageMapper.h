9512ca0da22ac7360e3f81c6292a7c12518aa3ba
Klaus Fritzsche <k.fritzsche@dkfz-heidelberg.de>
2009-11-11 10:00:03 +0000

    FIX (#2107): initial working version

--- Modules/DiffusionImaging/Rendering/mitkDiffusionImageMapper.h
+++ Modules/DiffusionImaging/Rendering/mitkDiffusionImageMapper.h
@@ -1,54 +1,54 @@
-///*=========================================================================
-//
-//Program:   Medical Imaging & Interaction Toolkit
-//Language:  C++
-//Date:      $Date: 2009-05-12 19:56:03 +0200 (Di, 12 Mai 2009) $
-//Version:   $Revision: 17179 $
-//
-//Copyright (c) German Cancer Research Center, Division of Medical and
-//Biological Informatics. All rights reserved.
-//See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
-//
-//This software is distributed WITHOUT ANY WARRANTY; without even
-//the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-//PURPOSE.  See the above copyright notices for more information.
-//
-//=========================================================================*/
-//
-//
-//#ifndef DiffusionImageMapper_H_HEADER_INCLUDED
-//#define DiffusionImageMapper_H_HEADER_INCLUDED
-//
-//#include "mitkImageMapper2D.h"
-//
-//namespace mitk {
-//
-//  //##Documentation
-//  //## @brief Mapper for raw diffusion weighted images
-//  //## @ingroup Mapper
-//  template<class TPixelType>
-//  class DiffusionImageMapper : public ImageMapper2D
-//  {
-//  public:
-//
-//    mitkClassMacro(DiffusionImageMapper,ImageMapper2D);
-//    itkNewMacro(Self);
-//
-//    void GenerateData( mitk::BaseRenderer *renderer );
-//
-//    static void SetDefaultProperties(DataTreeNode* node, BaseRenderer* renderer = NULL, bool overwrite = false );
-//    
-//  protected:
-//
-//    DiffusionImageMapper();
-//    virtual ~DiffusionImageMapper();
-//
-//  };
-//
-//} // namespace mitk
-//
-//#include "mitkDiffusionImageMapper.cpp"
-//
-//
-//#endif /* COMPOSITEMAPPER_H_HEADER_INCLUDED */
-//
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date: 2009-05-12 19:56:03 +0200 (Di, 12 Mai 2009) $
+Version:   $Revision: 17179 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef DiffusionImageMapper_H_HEADER_INCLUDED
+#define DiffusionImageMapper_H_HEADER_INCLUDED
+
+#include "mitkImageMapper2D.h"
+
+namespace mitk {
+
+  //##Documentation
+  //## @brief Mapper for raw diffusion weighted images
+  //## @ingroup Mapper
+  template<class TPixelType>
+  class DiffusionImageMapper : public ImageMapper2D
+  {
+  public:
+
+    mitkClassMacro(DiffusionImageMapper,ImageMapper2D);
+    itkNewMacro(Self);
+
+    void GenerateData( mitk::BaseRenderer *renderer );
+
+    static void SetDefaultProperties(DataTreeNode* node, BaseRenderer* renderer = NULL, bool overwrite = false );
+    
+  protected:
+
+    DiffusionImageMapper();
+    virtual ~DiffusionImageMapper();
+
+  };
+
+} // namespace mitk
+
+#include "mitkDiffusionImageMapper.cpp"
+
+
+#endif /* COMPOSITEMAPPER_H_HEADER_INCLUDED */
+

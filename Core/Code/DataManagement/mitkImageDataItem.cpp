d01dc82b3a356631be36eae489a6fd703389b12d
Klaus Fritzsche <k.fritzsche@dkfz-heidelberg.de>
2009-11-12 11:54:21 +0000

    FIX (#2892): removing tensor-imaging related code, which now is handled by mitk::TensorImage

--- Core/Code/DataManagement/mitkImageDataItem.cpp
+++ Core/Code/DataManagement/mitkImageDataItem.cpp
@@ -33,16 +33,7 @@ PURPOSE.  See the above copyright notices for more information.
 #include <vtkUnsignedLongArray.h>
 #include <vtkUnsignedShortArray.h>
 
-#include "itkDiffusionTensor3D.h"
-
 #include "ipFunc/mitkIpFunc.h"
-
-#ifdef _OPENMP
-#include "omp.h"
-#endif
-
-#define __IMG_DAT_ITEM__CEIL_ZERO_ONE__(val) (val) =       \
-  ( (val) < 0 ) ? ( 0 ) : ( ( (val)>1 ) ? ( 1 ) : ( (val) ) );
 
 mitk::ImageDataItem::ImageDataItem(const ImageDataItem& aParent, unsigned int dimension, void *data, bool manageMemory, size_t offset) :
   m_Data(NULL), m_ManageMemory(false), m_PicDescriptor(NULL), m_VtkImageData(NULL), m_Offset(offset), m_IsComplete(false), m_Size(0),
@@ -149,106 +140,79 @@ void mitk::ImageDataItem::ConstructVtkImageData() const
     return;
   }
 
-  // special treatment for tensors
-  if(  !(m_PixelType == typeid(itk::DiffusionTensor3D<float>))
-    && !(m_PixelType == typeid(itk::DiffusionTensor3D<double>)) )
+  inData->SetNumberOfScalarComponents(m_PixelType.GetNumberOfComponents());
+
+  if ( ( m_PixelType.GetType() == mitkIpPicInt || m_PixelType.GetType() == mitkIpPicUInt ) && m_PixelType.GetBitsPerComponent() == 1 )
   {
-
-    // in this case we DO NOT have a tensor image
-
-    inData->SetNumberOfScalarComponents(m_PixelType.GetNumberOfComponents());
-
-    if ( ( m_PixelType.GetType() == mitkIpPicInt || m_PixelType.GetType() == mitkIpPicUInt ) && m_PixelType.GetBitsPerComponent() == 1 )
-    {
-      inData->SetScalarType( VTK_BIT );
-      scalars = vtkBitArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicInt && m_PixelType.GetBitsPerComponent() == 8 )
-    {
-      inData->SetScalarType( VTK_CHAR );
-      scalars = vtkCharArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicUInt && m_PixelType.GetBitsPerComponent() == 8 )
-    {
-      inData->SetScalarType( VTK_UNSIGNED_CHAR );
-      scalars = vtkUnsignedCharArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicInt && m_PixelType.GetBitsPerComponent() == 16 )
-    {
-      inData->SetScalarType( VTK_SHORT );
-      scalars = vtkShortArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicUInt && m_PixelType.GetBitsPerComponent() == 16 )
-    {
-      inData->SetScalarType( VTK_UNSIGNED_SHORT );
-      scalars = vtkUnsignedShortArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicInt && m_PixelType.GetBitsPerComponent() == 32 )
-    {
-      inData->SetScalarType( VTK_INT );
-      scalars = vtkIntArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicUInt && m_PixelType.GetBitsPerComponent() == 32 )
-    {
-      inData->SetScalarType( VTK_UNSIGNED_INT );
-      scalars = vtkUnsignedIntArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicInt && m_PixelType.GetBitsPerComponent() == 64 )
-    {
-      inData->SetScalarType( VTK_LONG );
-      scalars = vtkLongArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicUInt && m_PixelType.GetBitsPerComponent() == 64 )
-    {
-      inData->SetScalarType( VTK_UNSIGNED_LONG );
-      scalars = vtkUnsignedLongArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicFloat && m_PixelType.GetBitsPerComponent() == 32 )
-    {
-      inData->SetScalarType( VTK_FLOAT );
-      scalars = vtkFloatArray::New();
-    }
-    else if ( m_PixelType.GetType() == mitkIpPicFloat && m_PixelType.GetBitsPerComponent() == 64 )
-    {
-      inData->SetScalarType( VTK_DOUBLE );
-      scalars = vtkDoubleArray::New();
-    }
-    else
-    {
-      inData->Delete();
-      return;
-    }
-
-    m_VtkImageData = inData;
-
-    // allocate the new scalars
-    scalars->SetNumberOfComponents(m_VtkImageData->GetNumberOfScalarComponents());
-
-    scalars->SetVoidArray(m_PicDescriptor->data, _mitkIpPicElements(m_PicDescriptor)*m_VtkImageData->GetNumberOfScalarComponents(), 1);
-
-    m_VtkImageData->GetPointData()->SetScalars(scalars);
-    scalars->Delete();
+    inData->SetScalarType( VTK_BIT );
+    scalars = vtkBitArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicInt && m_PixelType.GetBitsPerComponent() == 8 )
+  {
+    inData->SetScalarType( VTK_CHAR );
+    scalars = vtkCharArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicUInt && m_PixelType.GetBitsPerComponent() == 8 )
+  {
+    inData->SetScalarType( VTK_UNSIGNED_CHAR );
+    scalars = vtkUnsignedCharArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicInt && m_PixelType.GetBitsPerComponent() == 16 )
+  {
+    inData->SetScalarType( VTK_SHORT );
+    scalars = vtkShortArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicUInt && m_PixelType.GetBitsPerComponent() == 16 )
+  {
+    inData->SetScalarType( VTK_UNSIGNED_SHORT );
+    scalars = vtkUnsignedShortArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicInt && m_PixelType.GetBitsPerComponent() == 32 )
+  {
+    inData->SetScalarType( VTK_INT );
+    scalars = vtkIntArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicUInt && m_PixelType.GetBitsPerComponent() == 32 )
+  {
+    inData->SetScalarType( VTK_UNSIGNED_INT );
+    scalars = vtkUnsignedIntArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicInt && m_PixelType.GetBitsPerComponent() == 64 )
+  {
+    inData->SetScalarType( VTK_LONG );
+    scalars = vtkLongArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicUInt && m_PixelType.GetBitsPerComponent() == 64 )
+  {
+    inData->SetScalarType( VTK_UNSIGNED_LONG );
+    scalars = vtkUnsignedLongArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicFloat && m_PixelType.GetBitsPerComponent() == 32 )
+  {
+    inData->SetScalarType( VTK_FLOAT );
+    scalars = vtkFloatArray::New();
+  }
+  else if ( m_PixelType.GetType() == mitkIpPicFloat && m_PixelType.GetBitsPerComponent() == 64 )
+  {
+    inData->SetScalarType( VTK_DOUBLE );
+    scalars = vtkDoubleArray::New();
   }
   else
   {
-    // in this case we have a tensor image
-
-    // allocate new scalars with three components for RGB
-    inData->SetNumberOfScalarComponents(3);
-    inData->SetScalarType( VTK_UNSIGNED_CHAR );
-    scalars = vtkUnsignedCharArray::New();
-    m_VtkImageData = inData;
-    scalars->SetNumberOfComponents(m_VtkImageData->GetNumberOfScalarComponents());
-
-    // calculate RGB information from tensors
-    if(m_PixelType == typeid(itk::DiffusionTensor3D<float>))
-      scalars->SetVoidArray(ConvertTensorsToRGB<itk::DiffusionTensor3D<float> >(), m_Size/2, 1);
-    if(m_PixelType == typeid(itk::DiffusionTensor3D<double>))
-      scalars->SetVoidArray(ConvertTensorsToRGB<itk::DiffusionTensor3D<double> >(), m_Size/2, 1);
-
-    m_VtkImageData->GetPointData()->SetScalars(scalars);
-    scalars->Delete();
+    inData->Delete();
+    return;
   }
+
+  m_VtkImageData = inData;
+
+  // allocate the new scalars
+  scalars->SetNumberOfComponents(m_VtkImageData->GetNumberOfScalarComponents());
+
+  scalars->SetVoidArray(m_PicDescriptor->data, _mitkIpPicElements(m_PicDescriptor)*m_VtkImageData->GetNumberOfScalarComponents(), 1);
+
+  m_VtkImageData->GetPointData()->SetScalars(scalars);
+  scalars->Delete();
+
 }
 
 void mitk::ImageDataItem::Modified() const
@@ -257,47 +221,4 @@ void mitk::ImageDataItem::Modified() const
     m_VtkImageData->Modified();
 }
 
-/**
- * This method calculates RGB image from tensor information.
- * Templated over pixeltype, output always three uchar components.
- */
-template <class TPixeltype>
-unsigned char *mitk::ImageDataItem::ConvertTensorsToRGB() const
-{
-  const unsigned char *p   = m_Data;
-  unsigned char *out = (unsigned char *) malloc(m_Size/2);
-  const int pixelsize = sizeof(TPixeltype);
-  const int numIts = m_Size/pixelsize;
 
-#ifdef _OPENMP
-#pragma omp parallel for
-#endif
-  for(int i=0; i<numIts; i++)
-  {
-    const TPixeltype* tensor = static_cast<TPixeltype* >((void*)(p+i*pixelsize));
-    typename TPixeltype::EigenValuesArrayType eigenvalues;
-    typename TPixeltype::EigenVectorsMatrixType eigenvectors;
-    tensor->ComputeEigenAnalysis(eigenvalues, eigenvectors);
-
-    int index = 2;
-    if( (eigenvalues[0] >= eigenvalues[1])
-      && (eigenvalues[0] >= eigenvalues[2]) )
-      index = 0;
-    else if(eigenvalues[1] >= eigenvalues[2])
-      index = 1;
-
-    const float fa = tensor->GetFractionalAnisotropy();
-    float r = abs(eigenvectors(index,0)) * fa;
-    float g = abs(eigenvectors(index,1)) * fa;
-    float b = abs(eigenvectors(index,2)) * fa;
-
-    __IMG_DAT_ITEM__CEIL_ZERO_ONE__(r);
-    __IMG_DAT_ITEM__CEIL_ZERO_ONE__(g);
-    __IMG_DAT_ITEM__CEIL_ZERO_ONE__(b);
-
-    *(out+i*3+0) = (unsigned char)( 255.0f * r );
-    *(out+i*3+1) = (unsigned char)( 255.0f * g );
-    *(out+i*3+2) = (unsigned char)( 255.0f * b );
-  }
-  return out;
-}

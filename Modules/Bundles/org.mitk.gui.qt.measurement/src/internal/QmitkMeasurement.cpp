67bb8fc6c6b04c47cfa56c99db65c3ebcbf35b34
Jochen Neuhaus <j.neuhaus@dkfz-heidelberg.de>
2009-11-13 15:47:04 +0000

    FIX (#2655): set state of DrawFourPointAngle action correctly

--- Modules/Bundles/org.mitk.gui.qt.measurement/src/internal/QmitkMeasurement.cpp
+++ Modules/Bundles/org.mitk.gui.qt.measurement/src/internal/QmitkMeasurement.cpp
@@ -335,6 +335,7 @@ void QmitkMeasurement::ActionDrawFourPointAngleTriggered( bool  checked )
 {
   if ( m_SelectedImageNode.IsNull() || !checked )
   {
+    m_DrawFourPointAngle->setChecked(false);
     return;
   }
   mitk::PlanarFourPointAngle::Pointer figure = mitk::PlanarFourPointAngle::New();

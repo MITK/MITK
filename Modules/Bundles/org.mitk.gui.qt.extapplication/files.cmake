5122fb260f5c8b6ed1c104d50aa962594b8a153b
Caspar Goch <c.goch@dkfz-heidelberg.de>
2009-11-20 11:38:31 +0000

    CHG (#2712): Added/updated icons in several places

--- Modules/Bundles/org.mitk.gui.qt.extapplication/files.cmake
+++ Modules/Bundles/org.mitk.gui.qt.extapplication/files.cmake
@@ -11,6 +11,14 @@ SET(MOC_H_FILES
  
 )
 
+SET(RESOURCE_FILES
+# list of resource files which can be used by the plug-in
+# system without loading the plug-ins shared library,
+# for example the icon used in the menu and tabs for the
+# plug-in views in the workbench
+  resources/icon_research.xpm
+)
+
 SET(CPP_FILES manifest.cpp)
 
 foreach(file ${SRC_CPP_FILES})

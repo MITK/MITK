76c5275765816e12956ed1386fc1334c294207b0
Markus Fangerau <m.fangerau@dkfz-heidelberg.de>
2009-11-21 05:34:58 +0000

    ENH (#3068): added colored console log output for windows

--- Utilities/mbilog/mbilog.cpp
+++ Utilities/mbilog/mbilog.cpp
@@ -19,6 +19,11 @@ PURPOSE.  See the above copyright notices for more information.
 
 #include "mbilog.h"
 
+#ifdef _WIN32
+ #define USE_WIN32COLOREDCONSOLE
+ #include <windows.h>
+#endif
+
 static std::list<mbilog::AbstractBackend*> backends;
 
 namespace mbilog {
@@ -94,8 +99,8 @@ void mbilog::BackendCout::FormatSmart(std::ostream &out, const LogMessage &l,int
       break;
 
     case mbilog::Error:
-      c_open='-';
-      c_close='-';
+      c_open='#';
+      c_close='#';
       break;
 
     case mbilog::Fatal:
@@ -104,30 +109,13 @@ void mbilog::BackendCout::FormatSmart(std::ostream &out, const LogMessage &l,int
       break;
 
     case mbilog::Debug:
-      c_open='#';
-      c_close='#';
+      c_open='{';
+      c_close='}';
       break;
   }
 
   out << c_open << std::fixed << std::setw(6) << std::setprecision(2) << ((double)std::clock())/CLOCKS_PER_SEC;
-/*
-  if(threadID)
-  {
-    out << ":" << std::hex << threadID;
-  }
 
-  if(!l.category.empty())
-  {
-    out << ":" << l.category;
-  }
-  else
-  {
-    if(NA_STRING != l.moduleName)
-    {
-      out << ":" << std::string(l.moduleName);
-    }
-  }
-   */
   out << c_close << " ";
   
   if(!l.category.empty())
@@ -210,9 +198,135 @@ void mbilog::BackendCout::FormatFull(std::ostream &out,const LogMessage &l,int t
   out << "\n" << l.message << std::endl;
 }
 
+#ifdef USE_WIN32COLOREDCONSOLE
+
+static int g_LastColor;
+
+static void ChangeColor(int c)
+{
+  if(g_LastColor == c)
+    return;
+    
+  static HANDLE hConsole;
+
+  static bool init=false;
+  if(!init)
+  {
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    init=true;
+  }
+
+  SetConsoleTextAttribute(hConsole,c);
+  
+  g_LastColor=c;  
+}
+
+static void FormatSmartWindows(const mbilog::LogMessage &l,int /*threadID*/)
+{
+  char c_open='[';
+  char c_close=']';
+
+  int colorNormal = FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE;
+
+  int colorTime = FOREGROUND_GREEN;
+  int colorText = colorNormal;
+  
+  g_LastColor = colorNormal;
+
+  switch(l.level)
+  {
+    case mbilog::Info:
+      break;
+
+    case mbilog::Warn:
+      c_open='!';
+      c_close='!';
+      colorTime = FOREGROUND_RED|FOREGROUND_GREEN;
+      colorText |= FOREGROUND_INTENSITY;
+      break;
+
+    case mbilog::Error:
+      c_open='#';
+      c_close='#';
+      colorTime = FOREGROUND_RED|FOREGROUND_INTENSITY;
+      colorText |= FOREGROUND_INTENSITY;
+      break;
+
+    case mbilog::Fatal:
+      c_open='*';
+      c_close='*';
+      colorTime = FOREGROUND_RED|FOREGROUND_INTENSITY;
+      colorText |= FOREGROUND_INTENSITY;
+      break;
+
+    case mbilog::Debug:
+      c_open='{';
+      c_close='}';
+      colorTime = FOREGROUND_BLUE|FOREGROUND_INTENSITY;
+      colorText |= FOREGROUND_INTENSITY;
+      break;
+  }
+
+  ChangeColor( colorTime );
+  std::cout << c_open << std::flush;
+  ChangeColor( colorTime | FOREGROUND_INTENSITY);
+  std::cout << std::fixed << std::setw(6) << std::setprecision(2) << ((double)std::clock())/CLOCKS_PER_SEC << std::flush;
+  ChangeColor( colorTime );
+  std::cout << c_close << " " << std::flush;
+
+  if(!l.category.empty())
+  {
+    ChangeColor( FOREGROUND_BLUE | FOREGROUND_INTENSITY );
+    std::cout << "[" << std::flush;
+    ChangeColor( FOREGROUND_BLUE | FOREGROUND_GREEN );
+    std::cout << l.category << std::flush;
+    ChangeColor( FOREGROUND_BLUE | FOREGROUND_INTENSITY );
+    std::cout << "] " << std::flush;
+  }
+ 
+  switch(l.level)
+  {
+    case mbilog::Info:
+      break;
+
+    case mbilog::Warn:
+      ChangeColor( colorTime );
+      std::cout << "WARNING: " << std::flush;
+      break;
+
+    case mbilog::Error:
+       ChangeColor( colorTime );
+     std::cout << "ERROR: " << std::flush;
+      break;
+
+    case mbilog::Fatal:
+      ChangeColor( colorTime );
+      std::cout << "FATAL: " << std::flush;
+      break;
+
+    case mbilog::Debug:
+      ChangeColor( colorTime );
+      std::cout << "DEBUG: " << std::flush;
+      break;
+  }
+      
+  ChangeColor( colorText );
+
+  std::cout << l.message << std::endl;
+  
+  ChangeColor( colorNormal );
+
+}
+
+#endif
+
 void mbilog::BackendCout::FormatSmart(const LogMessage &l,int threadID)
 {
+#ifdef USE_WIN32COLOREDCONSOLE
+  FormatSmartWindows(l,threadID);
+#else
   mbilog::BackendCout::FormatSmart(std::cout,l,threadID);
+#endif
 }
 
 void mbilog::BackendCout::FormatFull(const LogMessage &l,int threadID)

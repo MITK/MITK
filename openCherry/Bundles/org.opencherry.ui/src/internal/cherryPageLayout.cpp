aeca81f7574ccdafab307376ad3ef4364bc1cf41
Alexander Seitel <a.seitel@dkfz-heidelberg.de>
2009-11-25 20:14:25 +0000

    FIX (#2716): use an empty string as parameter when creating a new ContainerPlacehoder in AddStandaloneViewPlaceholder

--- openCherry/Bundles/org.opencherry.ui/src/internal/cherryPageLayout.cpp
+++ openCherry/Bundles/org.opencherry.ui/src/internal/cherryPageLayout.cpp
@@ -607,7 +607,7 @@ void PageLayout::AddStandaloneViewPlaceholder(const std::string& viewId,
   }
 
   // Create the folder.
-  ContainerPlaceholder::Pointer folder(new ContainerPlaceholder(viewId));
+  ContainerPlaceholder::Pointer folder(new ContainerPlaceholder(""));
   folder->SetContainer(rootLayoutContainer);
   int appearance = PresentationFactoryUtil::ROLE_STANDALONE;
   if (!showTitle)

5ee350e9e21427de0a2862f29da2b98db75ec48b
Klaus Fritzsche <k.fritzsche@dkfz-heidelberg.de>
2009-11-26 15:04:52 +0000

    DOC: anmerkung

--- CMake/MITK_Boost_Config.cmake
+++ CMake/MITK_Boost_Config.cmake
@@ -1,5 +1,8 @@
 FIND_PACKAGE(Boost)
 
+#here we could also choose the desired components:
+#FIND_PACKAGE(Boost COMPONENTS graph math/special_functions)
+
 LIST(APPEND ALL_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
 LIST(APPEND ALL_LIBRARIES ${Boost_LIBRARIES})
 link_directories(${Boost_LIBRARY_DIRS})

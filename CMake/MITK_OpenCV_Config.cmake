e8e3830d87c7bc1761e81863e6ee6342f4a0263b
Michael Mueller <michael.mueller@dkfz-heidelberg.de>
2010-01-14 16:33:53 +0000

    FIX (#2668): Added CMake support for OpenCV 2 and removed old one. OpenCV is now fully integrated in the MITK Module system (one can use PACKAGE_DEPENDS to use OpenCV 2). For the Installer target, all DLLs of OpenCV 2 are added by using the appropriate INSTALL commands in mitk/CMakeLists.txt. Moreover, small bugfixes in the insightsurgery and the landmarktracking bundle. Adapted mitkOpenCVVideoSource.cpp and mitkUndistortCameraImage.cpp to work with OpenCV 2.

---
+++ CMake/MITK_OpenCV_Config.cmake
@@ -0,0 +1,17 @@
+#MESSAGE(STATUS "Searching for OpenCV")
+#FIND_PACKAGE(OpenCV 2 REQUIRED)
+
+find_path(OPENCV_DIR OpenCVConfig.cmake HINTS $ENV{PATH} $ENV{OPENCV_DIR} $ENV{OPENCVDIR} $ENV{OpenCVDIR} $ENV{OPENCV_DIR} $ENV{OpenCV_DIR})
+
+IF(OPENCV_DIR)
+INCLUDE("${OPENCV_DIR}/OpenCVConfig.cmake")
+LOAD_CACHE(${THIS_OPENCV_CONFIG_PATH} READ_WITH_PREFIX OpenCV_ OpenCV_SOURCE_DIR)
+SET(OpenCV_INCLUDE_DIRS "${OpenCV_OpenCV_SOURCE_DIR}/include" "${OpenCV_OpenCV_SOURCE_DIR}/include/opencv")
+	
+LIST(APPEND ALL_INCLUDE_DIRECTORIES ${OpenCV_INCLUDE_DIRS})
+LIST(APPEND ALL_LIBRARIES ${OpenCV_LIBS})
+link_directories(${OpenCV_LIB_DIR})
+ELSE(OPENCV_DIR)
+MESSAGE(SEND_ERROR "OpenCVConfig.cmake not found. Please set OPENCV_DIR manually to guide search.")
+#SET(OPENCV_DIR CACHE PATH "Set the directory containing OpenCVConfig.cmake")
+ENDIF(OPENCV_DIR)

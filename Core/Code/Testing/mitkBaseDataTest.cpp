8eaf47a51c10276e6459c3d33ce2d2dd709a5bce
Sven Mersmann <s.mersmann@dkfz-heidelberg.de>
2010-03-04 08:03:30 +0000

    ENH (#2112): wrote unit test for all BaseData methods

--- Core/Code/Testing/mitkBaseDataTest.cpp
+++ Core/Code/Testing/mitkBaseDataTest.cpp
@@ -16,7 +16,9 @@ PURPOSE.  See the above copyright notices for more information.
 =========================================================================*/
 
 #include "mitkBaseDataImplementation.h"
+#include "mitkStringProperty.h"
 #include "mitkTestingMacros.h"
+#include "itkImage.h"
 
 
 int mitkBaseDataTest(int /*argc*/, char* /*argv*/[])
@@ -26,41 +28,81 @@ int mitkBaseDataTest(int /*argc*/, char* /*argv*/[])
 
   //Create a BaseData implementation
   std::cout << "Creating a base data instance..." << std::endl;
-  mitk::BaseDataImplementation::Pointer bd = mitk::BaseDataImplementation::New();
+  mitk::BaseDataImplementation::Pointer baseDataImpl = mitk::BaseDataImplementation::New();
 
-  MITK_TEST_CONDITION_REQUIRED(bd.IsNotNull(),"Testing instantiation")
+  MITK_TEST_CONDITION_REQUIRED(baseDataImpl.IsNotNull(),"Testing instantiation");
+  MITK_TEST_CONDITION(baseDataImpl->IsInitialized(), "BaseDataImplementation is initialized");
+  MITK_TEST_CONDITION(baseDataImpl->IsEmpty(), "BaseDataImplementation is initialized and empty");
+  MITK_TEST_CONDITION(baseDataImpl->GetExternalReferenceCount()== baseDataImpl->GetReferenceCount(), "Checks external reference count!");
+  
 
-  MITK_TEST_CONDITION_REQUIRED(bd->GetTimeSlicedGeometry(), "Testing creation of TimeSlicedGeometry");
 
   std::cout << "Testing setter and getter for geometries..." << std::endl;
 
-  mitk::TimeSlicedGeometry* geo = NULL;
-  bd->SetGeometry(geo);
+  //test method GetTimeSlicedGeometry()
+  MITK_TEST_CONDITION(baseDataImpl->GetTimeSlicedGeometry(), "Testing creation of TimeSlicedGeometry");
 
-  MITK_TEST_CONDITION_REQUIRED(bd->GetTimeSlicedGeometry() == NULL, "Reset Geometry");
+  mitk::TimeSlicedGeometry* geo = NULL;
+  baseDataImpl->SetGeometry(geo);
+
+  MITK_TEST_CONDITION(baseDataImpl->GetTimeSlicedGeometry() == NULL, "Reset Geometry");
 
   mitk::TimeSlicedGeometry::Pointer geo2 = mitk::TimeSlicedGeometry::New();
-  bd->SetGeometry(geo2);
-  bd->InitializeTimeSlicedGeometry(2);
+  baseDataImpl->SetGeometry(geo2);
+  baseDataImpl->InitializeTimeSlicedGeometry(2);
 
-  MITK_TEST_CONDITION_REQUIRED(bd->GetTimeSlicedGeometry() == geo2, "Correct Reinit of TimeslicedGeometry");
-  MITK_TEST_CONDITION_REQUIRED(bd->GetGeometry(1) != NULL, "... and single Geometries");
-
+  MITK_TEST_CONDITION(baseDataImpl->GetTimeSlicedGeometry() == geo2, "Correct Reinit of TimeslicedGeometry");
+  
+  //test method GetGeometry(int timeStep)  
+  MITK_TEST_CONDITION(baseDataImpl->GetGeometry(1) != NULL, "... and single Geometries");
+    
+  //test method Expand(unsigned int timeSteps)
+  baseDataImpl->Expand(5);
+  MITK_TEST_CONDITION(baseDataImpl->GetTimeSteps() == 5, "Expand the geometry to further time slices!");
+  
+  //test method GetUpdatedGeometry(int timeStep);
   mitk::Geometry3D::Pointer geo3 = mitk::Geometry3D::New();
-  bd->SetGeometry(geo3, 1);
+  baseDataImpl->SetGeometry(geo3, 1);
 
-  MITK_TEST_CONDITION_REQUIRED(bd->GetUpdatedGeometry(1) == geo3, "Set Geometry for time step 1");
-
-  bd->SetClonedGeometry(geo3, 1);
+  MITK_TEST_CONDITION(baseDataImpl->GetUpdatedGeometry(1) == geo3, "Set Geometry for time step 1");
+  MITK_TEST_CONDITION(baseDataImpl->GetMTime()!= NULL, "Check if modified time is set");  
+  baseDataImpl->SetClonedGeometry(geo3, 1);
 
   float x[3];
-  x[0] = 2; x[1]= 4; x[2] = 6;
+  x[0] = 2; 
+  x[1] = 4; 
+  x[2] = 6;
   mitk::Point3D p3d(x);
-  bd->SetOrigin(p3d);
-
+  baseDataImpl->SetOrigin(p3d);
   geo3->SetOrigin(p3d);
 
-  MITK_TEST_CONDITION_REQUIRED(bd->GetGeometry(1)->GetOrigin() == geo3->GetOrigin(), "Testing Origin set");
+  MITK_TEST_CONDITION(baseDataImpl->GetGeometry(1)->GetOrigin() == geo3->GetOrigin(), "Testing Origin set");
 
+  MITK_TEST_CONDITION(!baseDataImpl->IsEmpty(1), "Is not empty before clear()!");
+  baseDataImpl->Clear();
+  MITK_TEST_CONDITION(baseDataImpl->IsEmpty(1), "...but afterwards!");
+  //test method Set-/GetProperty()
+  baseDataImpl->SetProperty("property38", mitk::StringProperty::New("testproperty"));
+  //baseDataImpl->SetProperty("visibility", mitk::BoolProperty::New());
+  MITK_TEST_CONDITION(baseDataImpl->GetProperty("property38")->GetValueAsString() == "testproperty","Check if base property is set correctly!");
+
+  //test method Set-/GetPropertyList
+  mitk::PropertyList::Pointer propertyList = mitk::PropertyList::New();
+  propertyList->SetFloatProperty("floatProperty1", 123.45);
+  propertyList->SetBoolProperty("visibility",true);
+  propertyList->SetStringProperty("nameXY","propertyName");
+  baseDataImpl->SetPropertyList(propertyList);
+  bool value = false;
+  MITK_TEST_CONDITION(baseDataImpl->GetPropertyList() == propertyList, "Check if base property list is set correctly!");
+  MITK_TEST_CONDITION(baseDataImpl->GetPropertyList()->GetBoolProperty("visibility", value) == true, "Check if base property is set correctly in the property list!");
+  
+  //test method UpdateOutputInformation()
+   baseDataImpl->UpdateOutputInformation();
+  MITK_TEST_CONDITION(baseDataImpl->GetUpdatedTimeSlicedGeometry() == geo2, "TimeSlicedGeometry update!");
+  //Test method CopyInformation()
+  mitk::BaseDataImplementation::Pointer newBaseData =  mitk::BaseDataImplementation::New();
+  newBaseData->CopyInformation(baseDataImpl);
+  MITK_TEST_CONDITION(  newBaseData->GetTimeSlicedGeometry()->GetTimeSteps() == 5, "Check copying of of Basedata Data Object!");
+ 
   MITK_TEST_END()
 }

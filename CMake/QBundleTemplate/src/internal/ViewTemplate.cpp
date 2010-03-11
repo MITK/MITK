a9be8b091ac40d538df262ea6ff9b07b6d8fbfa0
Daniel Maleike <d.maleike@dkfz-heidelberg.de>
2010-03-11 18:59:59 +0000

    FIX (#3423): Update template view to reflect recent changes in QmitkFunctionality

--- CMake/QBundleTemplate/src/internal/ViewTemplate.cpp
+++ CMake/QBundleTemplate/src/internal/ViewTemplate.cpp
@@ -1,21 +1,22 @@
 @PLUGIN_COPYRIGHT@
 
+// Blueberry
+#include <berryISelectionService.h>
+#include <berryIWorkbenchWindow.h>
+
+// Qmitk
 #include "@VIEW_CLASS_H@"
-
-#include "mitkNodePredicateDataType.h"
-
-#include "QmitkDataStorageComboBox.h"
 #include "QmitkStdMultiWidget.h"
 
+// Qt
 #include <QMessageBox>
 
 @BEGIN_NAMESPACE@
 
-const std::string @VIEW_CLASS@::VIEW_ID = "@VIEW_ID@";
-
 @VIEW_CLASS@::@VIEW_CLASS@()
 : @VIEW_BASE_CLASS@(),
-  m_MultiWidget(NULL)
+, m_Controls( 0 )
+, m_MultiWidget( NULL )
 {
 }
 
@@ -23,50 +24,64 @@ const std::string @VIEW_CLASS@::VIEW_ID = "@VIEW_ID@";
 {
 }
 
-void @VIEW_CLASS@::CreateQtPartControl(QWidget *parent)
+
+void @VIEW_CLASS@::CreateQtPartControl( QWidget *parent )
 {
-
-  // create GUI widgets
-  m_Controls.setupUi(parent);
-  this->CreateConnections();
-
-  // define data type for combobox
-  m_Controls.m_ImageSelector->SetDataStorage( this->GetDefaultDataStorage() );
-  m_Controls.m_ImageSelector->SetPredicate( mitk::NodePredicateDataType::New("Image") );
+  // build up qt view, unless already done
+  if ( !m_Controls )
+  {
+    // create GUI widgets from the Qt Designer's .ui file
+    m_Controls = new Ui::@VIEW_CLASS@Controls;
+    m_Controls->setupUi( parent );
+ 
+    connect( m_Controls->btnPerformImageProcessing, SIGNAL(clicked()), this, SLOT(DoImageProcessing()) );
+  }
 }
+
 
 void @VIEW_CLASS@::StdMultiWidgetAvailable (QmitkStdMultiWidget &stdMultiWidget)
 {
   m_MultiWidget = &stdMultiWidget;
 }
 
+
 void @VIEW_CLASS@::StdMultiWidgetNotAvailable()
 {
   m_MultiWidget = NULL;
 }
 
-void @VIEW_CLASS@::CreateConnections()
-{
-  connect( (QObject*)(m_Controls.m_Button), SIGNAL(clicked()), this, SLOT(DoSomething()) );
+
+void @VIEW_CLASS@::OnSelectionChanged( std::vector<mitk::DataTreeNode*> nodes )
+{ 
+  // iterate all selected objects, adjust warning visibility
+  for( std::vector<mitk::DataTreeNode*>::iterator it = nodes.begin();
+       it != nodes.end();
+       ++it )
+  {
+    mitk::DataTreeNode::Pointer node = *it;
+  
+    if( node.IsNotNull() && dynamic_cast<mitk::Image*>(node->GetData()) )
+    {
+      m_Controls->lblWarning->setVisible( false );
+      return;
+    }
+  }
+
+  m_Controls->lblWarning->setVisible( true );
 }
 
-void @VIEW_CLASS@::Activated()
-{
-  QmitkFunctionality::Activated();
-}
 
-void @VIEW_CLASS@::Deactivated()
+void @VIEW_CLASS@::DoImageProcessing()
 {
-  QmitkFunctionality::Deactivated();
-}
+  std::vector<mitk::DataTreeNode*> nodes = this->GetDataManagerSelection();
+  if (nodes.empty()) return;
 
-void @VIEW_CLASS@::DoSomething()
-{
-  mitk::DataTreeNode* node = m_Controls.m_ImageSelector->GetSelectedNode();
+  mitk::DataTreeNode* node = nodes.front();
+
   if (!node)
   {
     // Nothing selected. Inform the user and return
-    QMessageBox::information( NULL, "Template functionality", "Please load and select an image before starting some action.");
+    QMessageBox::information( NULL, "Template", "Please load and select an image before starting image processing.");
     return;
   }
 
@@ -76,19 +91,25 @@ void @VIEW_CLASS@::DoSomething()
   mitk::BaseData* data = node->GetData();
   if (data)
   {
-    // test if this data item is really an image or not (could also be a surface or something totally different)
+    // test if this data item is an image or not (could also be a surface or something totally different)
     mitk::Image* image = dynamic_cast<mitk::Image*>( data );
     if (image)
     {
-      std::string name("(no name)");
-      node->GetName(name);
+      std::stringstream message;
+      std::string name;
+      message << "Performing image processing for image ";
+      if (node->GetName(name))
+      {
+        // a property called "name" was found for this DataTreeNode
+        message << "'" << name << "'";
+      }
+      message << ".";
+      MITK_INFO << message.str();
 
-      QMessageBox::information( NULL, "Image processing",
-                                QString( "Doing something to '%1'" ).arg(name.c_str()) );
-
-      // at this point anything can be done using the mitk::Image image.
+      // TODO actually do something here...
     }
   }
 }
 
 @END_NAMESPACE@
+
--- CMake/QBundleTemplate/src/internal/ViewTemplate.h
+++ CMake/QBundleTemplate/src/internal/ViewTemplate.h
@@ -1,63 +1,53 @@
 @PLUGIN_COPYRIGHT@
 
-#ifndef @UBUNDLE_NAMESPACE@_@UVIEW_CLASS@_H_INCLUDED
-#define @UBUNDLE_NAMESPACE@_@UVIEW_CLASS@_H_INCLUDED
+#ifndef @VIEW_CLASS@_h
+#define @VIEW_CLASS@_h
+
+#include <berryISelectionListener.h>
 
 #include <@VIEW_BASE_CLASS_H@>
-
-#include <string>
 
 #include "@VIEW_CONTROLS_FILE@"
 
 @BEGIN_NAMESPACE@
 
 /*!
- * \ingroup @NormalizedPluginID@_internal
- *
- * \brief @VIEW_CLASS@
- *
- * Document your class here.
- *
- * \sa @VIEW_BASE_CLASS@
- */
-class @VIEW_CLASS@ : public QObject, public @VIEW_BASE_CLASS@
-{
+  \brief @VIEW_CLASS@ 
 
-  // this is needed for all Qt objects that should have a MOC object (everything that derives from QObject)
+  \warning  This application module is not yet documented. Use "svn blame/praise/annotate" and ask the author to provide basic documentation.
+
+  \sa QmitkFunctionality
+  \ingroup Functionalities
+*/
+class @VIEW_CLASS@ : public QObject, public QmitkFunctionality
+{  
+  // this is needed for all Qt objects that should have a Qt meta-object
+  // (everything that derives from QObject and wants to have signal/slots)
   Q_OBJECT
+  
+  public:  
 
-  public:
+    @VIEW_CLASS@();
 
-  static const std::string VIEW_ID;
+    virtual void CreateQtPartControl(QWidget *parent);
 
-  @VIEW_CLASS@();
-  virtual ~@VIEW_CLASS@();
+    virtual void StdMultiWidgetAvailable (QmitkStdMultiWidget &stdMultiWidget);
+    virtual void StdMultiWidgetNotAvailable();
 
-  virtual void CreateQtPartControl(QWidget *parent);
+  protected slots:
+  
+    /// \brief Called when the user clicks the GUI button
+    void DoImageProcessing();
 
-  /// \brief Creation of the connections of main and control widget
-  virtual void CreateConnections();
+  protected:
 
-  /// \brief Called when the functionality is activated
-  virtual void Activated();
+    /// \brief called by QmitkFunctionality when DataManager's selection has changed
+    virtual void OnSelectionChanged( std::vector<mitk::DataTreeNode*> nodes );
 
-  virtual void Deactivated();
+    Ui::@VIEW_CONTROLS_CLASS@Controls* m_Controls;
 
-  virtual void StdMultiWidgetAvailable (QmitkStdMultiWidget &stdMultiWidget);
-  virtual void StdMultiWidgetNotAvailable();
-
-protected slots:
-
-  /// \brief Called when the user clicks the GUI button
-  void DoSomething();
-
-protected:
-
-  Ui::@VIEW_CONTROLS_CLASS@ m_Controls;
-
-  QmitkStdMultiWidget* m_MultiWidget;
+    QmitkStdMultiWidget* m_MultiWidget;
 };
-
 
 @END_NAMESPACE@
 

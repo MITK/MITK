d6728f6e5ee4656fe41e4e74ad9a848ebbd8f824
Thomas Kilgus <t.kilgus@dkfz-heidelberg.de>
2010-03-19 09:46:54 +0000

    FIX (#3418): forgot to commit the mitkPaintbrushTool.cpp

--- Modules/MitkExt/Interactions/mitkPaintbrushTool.cpp
+++ Modules/MitkExt/Interactions/mitkPaintbrushTool.cpp
@@ -32,6 +32,9 @@ mitk::PaintbrushTool::PaintbrushTool(int paintingPixelValue)
  m_PaintingPixelValue(paintingPixelValue),
  m_LastContourSize(0) // other than initial mitk::PaintbrushTool::m_Size (around l. 28)
 {
+  m_MasterContour = Contour::New();
+  m_MasterContour->Initialize();
+
 }
 
 mitk::PaintbrushTool::~PaintbrushTool()

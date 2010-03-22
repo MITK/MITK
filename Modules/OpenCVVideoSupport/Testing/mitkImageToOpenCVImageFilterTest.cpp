01bafe0502a5c87deb34ed6cdcf3f2c472293dae
Michael Mueller <michael.mueller@dkfz-heidelberg.de>
2010-03-22 12:00:41 +0000

    COMP (#3262): Renamed wrong include

--- Modules/OpenCVVideoSupport/Testing/mitkImageToOpenCVImageFilterTest.cpp
+++ Modules/OpenCVVideoSupport/Testing/mitkImageToOpenCVImageFilterTest.cpp
@@ -15,7 +15,7 @@ PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
 
-#include "mitkImageToOpenCVImageFilter1.h"
+#include "mitkImageToOpenCVImageFilter.h"
 #include "mitkOpenCVToMitkImageFilter.h"
 #include <mitkTestingMacros.h>
 #include <mitkITKImageImport.h>

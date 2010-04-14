e59cec4768bbaf624116a09db3f04626e708f696
Markus Ritter <markus.ritter@dkfz-heidelberg.de>
2010-04-14 14:19:39 +0000

    FIX (#3476): removed debug message and changed notation in plugin.xml

--- BlueBerry/Bundles/org.blueberry.ui.qt/src/internal/util/berryTabbedStackPresentation.cpp
+++ BlueBerry/Bundles/org.blueberry.ui.qt/src/internal/util/berryTabbedStackPresentation.cpp
@@ -63,7 +63,6 @@ void TabbedStackPresentation::HandleTabFolderEvent(TabFolderEvent::Pointer e)
   else if (type == TabFolderEvent::EVENT_CLOSE)
   {
     IPresentablePart::Pointer part = folder->GetPartForTab(e->tab);
-    BERRY_INFO << "TabFolderEvent::EVENT_CLOSE part = " << (part != 0) << std::endl;
     if (part != 0)
     {
       std::vector<IPresentablePart::Pointer> parts;

4e47b11bbd703e6869afbb4ad35114cc97a661d9
Mathias Seitel <m.seitel@dkfz-heidelberg.de>
2010-09-23 09:41:52 +0000

    ENH (#5424): Add documentation for planar figure properties

--- Modules/PlanarFigure/Rendering/mitkPlanarFigureMapper2D.h
+++ Modules/PlanarFigure/Rendering/mitkPlanarFigureMapper2D.h
@@ -31,51 +31,84 @@ class BaseRenderer;
 class Contour;
 
 
-/** 
- * \brief OpenGL-based mapper to render display sub-class instances of
- * mitk::PlanarFigure
- * 
- * A couple of properties are evaluated to determine 
- * <ol>
- *   <li> color (ColorProperty)
- *   <li> opacity  (FloatProperty)
- *   <li> line width (FloatProperty)
- * </ol>
- *   of several rendering elements:
- * <ul>
- *   <li> Main lines that constitute the planar figure (e.g. a circle)
- *   <ul>
- *     <li> "color"
- *     <li> "opacity"
- *     <li> "width"
- *   </ul>
- *   <li> An outline for the main lines. This is a second line behind the main lines. By default, this line is broader and looks like an outline
- *   <ul>
- *     <li> "draw outline": BoolProperty to control whether the outline is drawn.
- *     <li> "outline color"
- *     <li> "outline opacity"
- *     <li> "outline width"
- *   </ul>
- *   <li> Helper elements, such as arrow heads or the arcs that signal a measured angle
- *   <ul>
- *     <li> "helper color"
- *     <li> "helper opacity"
- *     <li> "helper width"
- *   </ul>
- *   <li> Control points (handles) that are used to edit the planar figure
- *   <ul>
- *     <li> "control point color"
- *     <li> "control point opacity"
- *     <li> "control point width"
- *   </ul>
- * </ul>
- *
- * Futher properties that determine rendering:
- * <li> <b>selected<b> BoolProperty: if true we use a special color instead of the usual one
- * <li> <b>draw quantities<b> BoolProperty: if true, measured quantities of the planar figure (e.g. diameters) are drawn as text
- *
- * \ingroup Mapper
- */
+/**
+* \brief OpenGL-based mapper to render display sub-class instances of mitk::PlanarFigure
+* 
+* The appearance of planar figures can be configured through  properties. If no properties are specified,
+* default values will be used. There are four elements a planar figure consists of:
+* 
+* <ol>
+* <li>"line": the main line segments of the planar figure (note: text is drawn in the same style)
+* <li>"helperline": additional line segments of planar figures, such as arrow tips, arches of angles, etc.
+* <li>"outline": background which is drawn behind the lines and helperlines of the planar figure (optional)
+* <li>"marker": the markers (control points) of a planar figure
+* <li>"markerline": the lines by which markers (control points) are surrounded
+* </ol>
+* 
+* In the following, all planar figure properties are listed:
+* 
+* <ol>
+* <li>Properties controlled by PlanarFigureInteractor
+*   <ul>
+*     <li>"selected": true if the planar figure is selected
+*     <li>"planarfigure.ishovering": true if the mouse "hovers" over the planar figure
+*   </ul>
+* <li>General properties for the planar figure
+*   <ul>
+*     <li>"planarfigure.drawoutline": if true, the "outline" lines is drawn
+*     <li>"planarfigure.drawquantities": if true, the quantities (text) associated with the planar figure is drawn
+*     <li>"planarfigure.controlpointshape": style of the control points (enum)
+*   </ul>
+* <li>Line widths of planar figure elements
+*   <ul>
+*     <li>"planarfigure.line.width": width of "line" segments (float value, in mm)
+*     <li>"planarfigure.outline.width": width of "outline" segments (float value, in mm)
+*     <li>"planarfigure.helperline.width": width of "helperline" segments (float value, in mm)
+*   </ul>
+* <li>Color/opacity of planar figure elements in normal mode (unselected)
+*   <ul>
+*     <li>"planarfigure.default.line.color"
+*     <li>"planarfigure.default.line.opacity"
+*     <li>"planarfigure.default.outline.color"
+*     <li>"planarfigure.default.outline.opacity"
+*     <li>"planarfigure.default.helperline.color"
+*     <li>"planarfigure.default.helperline.opacity"
+*     <li>"planarfigure.default.markerline.color"
+*     <li>"planarfigure.default.markerline.opacity"
+*     <li>"planarfigure.default.marker.color"
+*     <li>"planarfigure.default.marker.opacity"
+*   </ul>
+* <li>Color/opacity of planar figure elements in hover mode (mouse-over)
+*   <ul>
+*     <li>"planarfigure.hover.line.color"
+*     <li>"planarfigure.hover.line.opacity"
+*     <li>"planarfigure.hover.outline.color"
+*     <li>"planarfigure.hover.outline.opacity"
+*     <li>"planarfigure.hover.helperline.color"
+*     <li>"planarfigure.hover.helperline.opacity"
+*     <li>"planarfigure.hover.markerline.color"
+*     <li>"planarfigure.hover.markerline.opacity"
+*     <li>"planarfigure.hover.marker.color"
+*     <li>"planarfigure.hover.marker.opacity"
+*   </ul>
+* <li>Color/opacity of planar figure elements in selected mode
+*   <ul>
+*     <li>"planarfigure.selected.line.color"
+*     <li>"planarfigure.selected.line.opacity"
+*     <li>"planarfigure.selected.outline.color"
+*     <li>"planarfigure.selected.outline.opacity"
+*     <li>"planarfigure.selected.helperline.color"
+*     <li>"planarfigure.selected.helperline.opacity"
+*     <li>"planarfigure.selected.markerline.color;"
+*     <li>"planarfigure.selected.markerline.opacity"
+*     <li>"planarfigure.selected.marker.color"
+*     <li>"planarfigure.selected.marker.opacity"
+*   </ul>
+* </ol>
+*
+* \ingroup Mapper
+*/
+
 class PlanarFigure_EXPORT PlanarFigureMapper2D : public GLMapper2D
 {
 public:

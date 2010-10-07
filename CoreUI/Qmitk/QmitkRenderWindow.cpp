f213a754036d9c785272a8c4475e0207cf8632b3
Alexander Seitel <a.seitel@dkfz-heidelberg.de>
2010-10-07 06:40:52 +0000

    FIX (#5408): changed SLOT to SIGNAL in QmitkRenderWindow::showEvent

--- CoreUI/Qmitk/QmitkRenderWindow.cpp
+++ CoreUI/Qmitk/QmitkRenderWindow.cpp
@@ -211,7 +211,7 @@ void QmitkRenderWindow::showEvent( QShowEvent* event )
 
   // this singleshot is necessary to have the overlays positioned correctly after initial show
   // simple call of Moved() is no use here!!
-  QTimer::singleShot(0, this ,SLOT( Moved() ) );
+  QTimer::singleShot(0, this ,SIGNAL( Moved() ) );
 }
 
 

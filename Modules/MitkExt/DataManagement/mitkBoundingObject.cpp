fa4a2f9a7ed642bd8e3a47de836f4d5cb42ca1b4
Bastian Graser <b.graser@dkfz-heidelberg.de>
2011-01-04 15:16:46 +0000

    COMP (#5557): The function "FitGeometry" was adding 0.5 offset, if it is an imagegeometry! No manual 0.5 offset correction allowed anymore!!! See geometry doc!

--- Modules/MitkExt/DataManagement/mitkBoundingObject.cpp
+++ Modules/MitkExt/DataManagement/mitkBoundingObject.cpp
@@ -43,16 +43,9 @@ void mitk::BoundingObject::FitGeometry(mitk::Geometry3D* aGeometry3D)
 {
   GetGeometry()->SetIdentity();
   GetGeometry()->Compose(aGeometry3D->GetIndexToWorldTransform());
-
-  if(aGeometry3D->GetImageGeometry())
-  {
-    mitk::Vector3D halfSpacingStep = aGeometry3D->GetSpacing()*0.5;
-    GetGeometry()->SetOrigin(aGeometry3D->GetCenter()-halfSpacingStep);
-  }
-  else
-  {
-    GetGeometry()->SetOrigin(aGeometry3D->GetCenter());
-  }
+  
+  
+  GetGeometry()->SetOrigin(aGeometry3D->GetCenter());
 
   mitk::Vector3D size;
   for(unsigned int i=0; i < 3; ++i)

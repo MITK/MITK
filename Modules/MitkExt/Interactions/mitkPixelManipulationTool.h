38e0d7010acecf9b7c23a23becd551d40277d594
Markus Ritter <markus.ritter@dkfz-heidelberg.de>
2011-02-01 10:21:18 +0000

    ADD (#4299): first version of pixel manipulation tool

---
+++ Modules/MitkExt/Interactions/mitkPixelManipulationTool.h
@@ -0,0 +1,65 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision: 28959 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef MITKPIXELMANIPULATIONTOOL_H
+#define MITKPIXELMANIPULATIONTOOL_H
+
+#include "mitkTool.h"
+#include "mitkPixelManipulationTool.xpm"
+#include "MitkExtExports.h"
+
+#include "itkImage.h"
+
+namespace mitk
+{
+  class MitkExt_EXPORT PixelManipulationTool : public Tool
+  {
+  public:
+    mitkClassMacro(PixelManipulationTool, Tool);
+    itkNewMacro(PixelManipulationTool);
+
+    itkSetMacro(Value, int);
+    itkGetMacro(Value, int);
+
+    itkSetMacro(FixedValue, bool);
+    itkGetMacro(FixedValue, bool);
+    itkBooleanMacro(FixedValue);
+       
+    virtual const char* GetName() const;
+    virtual const char** GetXPM() const;
+
+    virtual void Activated();
+    virtual void Deactivated();
+
+    virtual void CalculateImage();
+
+
+  protected:
+    PixelManipulationTool();
+    virtual ~PixelManipulationTool();
+
+    virtual void OnRoiDataChanged();
+    void AddImageToDataStorage(mitk::Image::Pointer image);
+
+    template <typename TPixel, unsigned int VImageDimension>
+    void ITKPixelManipulation( itk::Image<TPixel, VImageDimension>* originalImage, Image* maskImage, Image* newImage, int newValue);
+
+    mitk::DataNode::Pointer m_OriginalImageNode;
+    int m_Value;
+    bool m_FixedValue;
+  };//clas
+}//namespace
+#endif
---
+++ Modules/QmitkExt/QmitkPixelManipulationToolGUI.h
@@ -0,0 +1,53 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision: 28959 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef QMITKPIXELMANIPULATIONTOOLGUI_H
+#define QMITKPIXELMANIPULATIONTOOLGUI_H
+
+#include "QmitkToolGUI.h"
+#include "mitkPixelManipulationTool.h"
+#include "QmitkDataStorageComboBox.h"
+#include <QSlider>
+#include <QSpinBox>
+
+class QmitkPixelManipulationToolGUI : public QmitkToolGUI
+{
+  Q_OBJECT
+public:
+
+  mitkClassMacro(QmitkPixelManipulationToolGUI, QmitkToolGUI);
+  itkNewMacro(QmitkPixelManipulationToolGUI);
+
+  protected slots:
+
+    void OnNewToolAssociated(mitk::Tool*);
+    void OnSliderValueChanged(int);
+    void OnSpinBoxChanged();
+    void OnOkButtonClicked();
+    void SetFixedValueOn(bool);
+    void SetFixedValueOff(bool);
+
+protected:
+
+  QmitkPixelManipulationToolGUI();
+  virtual ~QmitkPixelManipulationToolGUI();
+
+  mitk::PixelManipulationTool::Pointer m_PixelManipulationTool;
+  QSlider* m_Slider;
+  QSpinBox* m_Spinner;
+
+};//class
+#endif

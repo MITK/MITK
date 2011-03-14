e428d93448efcb19279d8dea8c298280beb9a0a1
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2011-03-14 14:11:18 +0100

    Do not install Qt debug libraries on windows

--- CMake/mitkMacroInstallTargets.cmake
+++ CMake/mitkMacroInstallTargets.cmake
@@ -71,17 +71,26 @@ MACRO(MITK_INSTALL_TARGETS)
       IF(NOT _qt_is_system_qt)
 
         IF(QT_PLUGINS_DIR)
+          IF(WIN32)
             INSTALL(DIRECTORY "${QT_PLUGINS_DIR}"
                     DESTINATION ${${_target_location}_qt_plugins_install_dir}
                     CONFIGURATIONS Release
-                    FILES_MATCHING REGEX "[^d]4?\\${CMAKE_SHARED_LIBRARY_SUFFIX}"
+                    FILES_MATCHING REGEX "[^4d]4?${CMAKE_SHARED_LIBRARY_SUFFIX}"
                    )
 
             INSTALL(DIRECTORY "${QT_PLUGINS_DIR}"
                     DESTINATION ${${_target_location}_qt_plugins_install_dir}
                     CONFIGURATIONS Debug
-                    FILES_MATCHING REGEX "d4?\\${CMAKE_SHARED_LIBRARY_SUFFIX}"
+                    FILES_MATCHING REGEX "d4?${CMAKE_SHARED_LIBRARY_SUFFIX}"
                    )
+          ELSE(WIN32)
+            # install everything, see bug 7143
+            INSTALL(DIRECTORY "${QT_PLUGINS_DIR}"
+                    DESTINATION ${${_target_location}_qt_plugins_install_dir}
+                    FILES_MATCHING REGEX "${CMAKE_SHARED_LIBRARY_SUFFIX}"
+                   )
+ 
+          ENDIF(WIN32)
 
         ENDIF()
       ENDIF()

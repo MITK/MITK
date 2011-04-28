e2596a628742ed5b2db98c92b7dd61470f9a990a
Christian Zoellner <c.zoellner@dkfz-heidelberg.de>
2011-04-28 13:39:59 +0200

    Created interface and base class

--- Modules/IGT/IGTFilters/mitkTrackingVolumeGenerator.cpp
+++ Modules/IGT/IGTFilters/mitkTrackingVolumeGenerator.cpp
@@ -1,2 +1,18 @@
 #include "mitkTrackingVolumeGenerator.h"
+#include "mitkSTLFileReader.h";
+#include "mitkStandardFileLocations.h";
+#include "mitkConfig.h"
+#include <vtkCubeSource.h>
+
+mitk::TrackingVolumeGenerator::TrackingVolumeGenerator()
+{
+    std::string m_VolumeDir = MITK_ROOT;
+    m_VolumeDir += "Modules/IGT/IGTTrackingDevices/TrackingVolumeData";
+    mitk::StandardFileLocations::GetInstance()->AddDirectoryForSearch( m_VolumeDir.c_str(), false );
+}
+
+bool mitk::TrackingVolumeGenerator::SetTrackingDevice(mitk::TrackingDevice::Pointer tracker)
+{
+    std::cout << "es geht"<<std::endl;
+}
 
--- Modules/IGT/IGTFilters/mitkTrackingVolumeGenerator.h
+++ Modules/IGT/IGTFilters/mitkTrackingVolumeGenerator.h
@@ -1,4 +1,31 @@
 #ifndef MITKTRACKINGVOLUMEGENERATOR_H
 #define MITKTRACKINGVOLUMEGENERATOR_H
 
+#include <MitkIGTExports.h>
+
+#include "mitkSurfaceSource.h"
+#include "mitkSurface.h"
+#include "vtkPolyData.h"
+#include "mitkTrackingTypes.h"
+#include "mitkTrackingDevice.h"
+
 #endif // MITKTRACKINGVOLUMEGENERATOR_H
+
+namespace mitk
+{
+    class MitkIGT_EXPORT TrackingVolumeGenerator : public mitk::SurfaceSource
+  {
+  public:
+
+      mitkClassMacro(TrackingVolumeGenerator, mitk::SurfaceSource)
+      itkNewMacro(Self);
+
+      bool SetTrackingDevice(mitk::TrackingDevice::Pointer tracker);
+
+      TrackingVolumeGenerator();
+  protected:
+      mitk::TrackingDevice::Pointer m_TrackingDevice;
+  };
+}
+
+

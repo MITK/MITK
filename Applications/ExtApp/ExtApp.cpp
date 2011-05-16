fea2ba9fc80732faffb5487ced87434445ace2df
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2011-05-16 16:55:15 +0200

    Fixed include dirs and QApplication instantiation.

--- Applications/ExtApp/ExtApp.cpp
+++ Applications/ExtApp/ExtApp.cpp
@@ -19,9 +19,61 @@ PURPOSE.  See the above copyright notices for more information.
 #include <Poco/Util/MapConfiguration.h>
 #include <mitkCoreExtObjectFactory.h>
 
+#include <QApplication>
+#include <QMessageBox>
+
+class QtSafeApplication : public QApplication
+{
+
+public:
+
+  QtSafeApplication(int& argc, char** argv) : QApplication(argc, argv)
+  {}
+
+  /**
+   * Reimplement notify to catch unhandled exceptions and open an error message.
+   *
+   * @param receiver
+   * @param event
+   * @return
+   */
+  bool notify(QObject* receiver, QEvent* event)
+  {
+    QString msg;
+    try
+    {
+      return QApplication::notify(receiver, event);
+    }
+    catch (Poco::Exception& e)
+    {
+      msg = QString::fromStdString(e.displayText());
+    }
+    catch (std::exception& e)
+    {
+      msg = e.what();
+    }
+    catch (...)
+    {
+      msg = "Unknown exception";
+    }
+
+    QString text("An error occurred. You should save all data and quit the program to "
+                 "prevent possible data loss.\nSee the error log for details.\n\n");
+    text += msg;
+
+    QMessageBox::critical(0, "Error", text);
+    return false;
+  }
+
+};
+
 int main(int argc, char** argv)
 {
-  
+  // Create a QApplication instance first
+  QtSafeApplication qSafeApp(argc, argv);
+  qSafeApp.setApplicationName("ExtApp");
+  qSafeApp.setOrganizationName("DKFZ");
+
   RegisterCoreExtObjectFactory();
   // These paths replace the .ini file and are tailored for installation
   // packages created with CPack. If a .ini file is presented, it will

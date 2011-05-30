58799417878a4b41d6ec820985fc6174e996150c
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2011-05-30 19:43:12 +0200

    Enhance CreateProvisioningInfo function to cope with Windows file URLs.

--- BlueBerry/CMake/FunctionCreateProvisioningInfo.cmake
+++ BlueBerry/CMake/FunctionCreateProvisioningInfo.cmake
@@ -1,20 +1,37 @@
-function(FunctionCreateProvisioningInfo info_var out_dir)
+function(FunctionCreateProvisioningInfo)
+
+  macro_parse_arguments(_PROV "OUTPUT;INCLUDE;PLUGIN_DIR;PLUGINS" "" ${ARGN})
 
   set(out_var )
-  foreach(plugin ${ARGN})
+  if(WIN32)
+    set(file_url "file:///")
+  else()
+    set(file_url "file://")
+  endif()
+  
+  foreach(incl ${_PROV_INCLUDE})
+    set(out_var "${out_var}READ ${file_url}${incl}\n")
+  endforeach()
+  
+  if(_PROV_INCLUDE)
+    set(out_var "${out_var}\n")
+  endif()
+ 
+  foreach(plugin ${_PROV_PLUGINS})
     ctkFunctionExtractOptionNameAndValue(${plugin} plugin_name_with_dirs plugin_value)
     string(REPLACE "/" ";" _tokens ${plugin_name_with_dirs})
     list(GET _tokens -1 plugin_name)
     string(REPLACE "." "_" plugin_target ${plugin_name})
-    set(plugin_url "file://${out_dir}/${CMAKE_SHARED_LIBRARY_PREFIX}${plugin_target}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    
+    set(plugin_url "${file_url}${_PROV_PLUGIN_DIR}/lib${plugin_target}${CMAKE_SHARED_LIBRARY_SUFFIX}")
     
     if(${${plugin_name_with_dirs}_option_name})
-      set(out_var "${out_var}\nSTART ${plugin_url}")
+      set(out_var "${out_var}START ${plugin_url}\n")
     #else()
-    #  set(out_var "${out_var}\nSTOP ${plugin_url}")
+    #  set(out_var "${out_var}STOP ${plugin_url}\n")
     endif()
   endforeach()
 
-  set(${info_var} ${out_var} PARENT_SCOPE)
+  set(${_PROV_OUTPUT} ${out_var} PARENT_SCOPE)
 
 endfunction() 

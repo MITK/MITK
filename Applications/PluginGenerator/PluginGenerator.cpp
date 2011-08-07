91c71b60d444d904029af4d52d78decb226b7d58
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2011-08-07 12:27:45 +0200

    Added missing Windows header.

--- Applications/PluginGenerator/PluginGenerator.cpp
+++ Applications/PluginGenerator/PluginGenerator.cpp
@@ -26,7 +26,7 @@ PURPOSE.  See the above copyright notices for more information.
 #include <cstdlib>
 #include <iostream>
 #include <limits>
-
+#include <cctype>
 
 bool readAnswer(char defaultAnswer)
 {

f8a3142fbe217abf98192c31c4b0a4d2c10be362
Peter Neher <p.neher@dkfz-heidelberg.de>
2011-08-24 13:48:27 +0200

    dividing matrix values by spacing

--- Modules/DiffusionImaging/Algorithms/itkTractsToProbabilityImageFilter.cpp
+++ Modules/DiffusionImaging/Algorithms/itkTractsToProbabilityImageFilter.cpp
@@ -56,7 +56,7 @@ namespace itk{
     itk::Matrix<double, 3, 3> matrix;
     for (int i=0; i<3; i++)
       for (int j=0; j<3; j++)
-        matrix[j][i] = geometry->GetMatrixColumn(i)[j];
+        matrix[j][i] = geometry->GetMatrixColumn(i)[j]/geometry->GetSpacing().GetElement(i);
     outImage->SetDirection( matrix );  // Set the image direction
 
     float* bounds = m_FiberBundle->GetBounds();

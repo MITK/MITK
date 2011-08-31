2a8255031f9e2215f63bbe3340c7cdbc89c2ea2f
Peter Neher <p.neher@dkfz-heidelberg.de>
2011-08-31 20:12:25 +0200

    added pv view and dependencies

---
+++ Modules/Bundles/org.mitk.gui.qt.diffusionimaging/src/QmitkPartialVolumeAnalysisWidget.h
@@ -0,0 +1,73 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date: 2009-05-15 18:09:46 +0200 (Fr, 15 Mai 2009) $
+Version:   $Revision: 1.12 $
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef QmitkPartialVolumeAnalysisWidget_H_
+#define QmitkPartialVolumeAnalysisWidget_H_
+
+#include "QmitkPlotWidget.h"
+#include <MitkDiffusionImagingExports.h>
+
+#include "QmitkHistogram.h"
+#include "QmitkExtExports.h"
+#include "mitkImage.h"
+#include "mitkPlanarFigure.h"
+#include "mitkPartialVolumeAnalysisClusteringCalculator.h"
+
+#include <qlayout.h>
+#include <qtextedit.h>
+#include <qdialog.h>
+
+#include <itkHistogram.h>
+
+#include <vtkQtChartWidget.h>
+#include <vtkQtBarChart.h>
+
+#include <QStandardItemModel>
+
+
+
+
+/**
+ * \brief Widget for displaying image histograms based on the vtkQtChart
+ * framework
+ */
+class MitkDiffusionImaging_EXPORT QmitkPartialVolumeAnalysisWidget : public QmitkPlotWidget
+{
+
+public:
+  typedef mitk::Image::HistogramType HistogramType;
+  typedef mitk::Image::HistogramType::ConstIterator HistogramConstIteratorType;
+
+  typedef mitk::PartialVolumeAnalysisClusteringCalculator ClusteringType;
+  typedef ClusteringType::ParamsType ParamsType;
+  typedef ClusteringType::ClusterResultType ResultsType;
+  typedef ClusteringType::HistType HistType;
+
+  /** \brief Set histogram to be displayed directly. */
+  void SetParameters( ParamsType *params, ResultsType *results, HistType *hist );
+
+  QmitkPartialVolumeAnalysisWidget( QWidget * /*parent = 0 */);
+  virtual ~QmitkPartialVolumeAnalysisWidget();
+
+  void DrawGauss();
+
+  void ClearItemModel();
+
+  std::vector< std::vector<double>* > m_Vals;
+};
+
+#endif /* QmitkPartialVolumeAnalysisWidget_H_ */
---
+++ Modules/DiffusionImaging/Algorithms/itkDiffusionTensorPrincipleDirectionImageFilter.h
@@ -0,0 +1,89 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkDiffusionTensor3DReconstructionImageFilter.h,v $
+  Language:  C++
+  Date:      $Date: 2006-03-27 17:01:06 $
+  Version:   $Revision: 1.12 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkDiffusionTensorPrincipleDirectionImageFilter_h_
+#define __itkDiffusionTensorPrincipleDirectionImageFilter_h_
+
+#include "MitkDiffusionImagingMBIExports.h"
+#include "itkImageToImageFilter.h"
+//#include "vnl/vnl_matrix.h"
+#include "vnl/vnl_vector_fixed.h"
+#include "vnl/vnl_matrix.h"
+#include "vnl/algo/vnl_svd.h"
+#include "itkVectorContainer.h"
+#include "itkVectorImage.h"
+#include "itkDiffusionTensor3D.h"
+
+namespace itk{
+  /** \class DiffusionTensorPrincipleDirectionImageFilter
+ */
+
+  template< class TTensorPixelType, class TPDPixelType=double>
+  class DiffusionTensorPrincipleDirectionImageFilter :
+      public ImageToImageFilter< Image< DiffusionTensor3D<TTensorPixelType>, 3 >,
+      Image< Vector< TPDPixelType, 3 >, 3 > >
+  {
+
+  public:
+
+    typedef DiffusionTensorPrincipleDirectionImageFilter Self;
+    typedef SmartPointer<Self>                      Pointer;
+    typedef SmartPointer<const Self>                ConstPointer;
+    typedef ImageToImageFilter< Image< DiffusionTensor3D<TTensorPixelType>, 3 >,
+    Image< Vector< TPDPixelType, 3 >, 3 > >
+    Superclass;
+
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+
+    /** Runtime information support. */
+    itkTypeMacro(DiffusionTensorPrincipleDirectionImageFilter,
+                 ImageToImageFilter);
+
+    typedef TTensorPixelType                 TensorComponentType;
+
+    typedef TPDPixelType                  DirectionPixelType;
+
+    typedef typename Superclass::InputImageType      InputImageType;
+
+    typedef typename Superclass::OutputImageType      OutputImageType;
+
+    typedef typename Superclass::OutputImageRegionType
+        OutputImageRegionType;
+
+    void SetImage( const InputImageType *image );
+
+  protected:
+    DiffusionTensorPrincipleDirectionImageFilter();
+    ~DiffusionTensorPrincipleDirectionImageFilter() {};
+    void PrintSelf(std::ostream& os, Indent indent) const;
+
+    void BeforeThreadedGenerateData();
+    void ThreadedGenerateData( const
+                               OutputImageRegionType &outputRegionForThread, int);
+
+  private:
+
+  };
+
+}
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkDiffusionTensorPrincipleDirectionImageFilter.txx"
+#endif
+
+#endif //__itkDiffusionTensorPrincipleDirectionImageFilter_h_
+

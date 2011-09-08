b0a00251f0dc5a3622fcb5abd7b8ecca5fe05073
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2011-09-08 17:12:10 +0200

    Update CTK revision.
    This CTK version has cleaned up it's debug messages in the
    Plugin Framework.

--- CMakeExternals/CTK.cmake
+++ CMakeExternals/CTK.cmake
@@ -15,7 +15,7 @@ IF(MITK_USE_CTK)
 
   IF(NOT DEFINED CTK_DIR)
     
-    SET(revision_tag 5f8872e0)
+    SET(revision_tag cd46fbef)
     IF(${proj}_REVISION_TAG)
       SET(revision_tag ${${proj}_REVISION_TAG})
     ENDIF()

4fea55c6414bd06d2ecbee22f9fc0f6da686203e
Paul Mercea <p.mercea@dkfz-heidelberg.de>
2011-09-13 14:26:30 +0200

    Implemented Plugin Functionalities
    Initial commit with all functionalities implemented.

--- Modules/Bundles/org.mitk.gui.qt.igttrackinglab/src/internal/IGTTrackingLabView.h
+++ Modules/Bundles/org.mitk.gui.qt.igttrackinglab/src/internal/IGTTrackingLabView.h
@@ -25,7 +25,14 @@ PURPOSE.  See the above copyright notices for more information.
 
 #include "ui_IGTTrackingLabViewControls.h"
 
+#include <mitkNavigationDataToPointSetFilter.h>
+#include <mitkNavigationDataLandmarkTransformFilter.h>
+#include <mitkNavigationDataObjectVisualizationFilter.h>
+#include <mitkNavigationDataToPointSetFilter.h>
+#include <mitkTrackingDeviceSource.h>
+
 #include <QToolBox>
+#include <QCheckBox>
 
 class QmitkNDIConfigurationWidget;
 class QmitkFiducialRegistrationWidget;
@@ -59,6 +66,17 @@ class IGTTrackingLabView : public QmitkFunctionality
 
   protected slots:
   
+    void OnAddRegistrationTrackingFiducial();
+    void OnRegisterFiducials();
+    void OnTrackerDisconnected();
+    void OnSetupNavigation();
+    void OnChangeToolName(int index, QString name);
+    void OnToolLoaded(int index, mitk::DataNode::Pointer toolNode);
+    void OnStartNavigation();
+    void OnStopNavigation();
+    
+
+
     /// \brief Called when the user clicks the GUI button
 //    void DoImageProcessing();
 
@@ -69,16 +87,47 @@ class IGTTrackingLabView : public QmitkFunctionality
 
     Ui::IGTTrackingLabViewControls m_Controls;
     void CreateBundleWidgets( QWidget* parent );
+    void CreateConnections();
+
+    void SetupIGTPipeline();
+    void InitializeFilters();
+    void DestroyIGTPipeline();
+    void StartContinuousUpdate();
+    void StopContinuousUpdate();
+    void RenderScene();
+
+    
+    mitk::DataNode::Pointer CreateInstrumentVisualization(mitk::DataStorage* ds, const char* toolName); // create 3D models for all connected tools
+    mitk::DataNode::Pointer CreateConeRepresentation(const char* label = ""); // create a 3D cone as representation for a tool
+  
+
+
+    //void DestroyInstrumentVisualization(mitk::DataStorage* ds, mitk::TrackingDevice::Pointer tracker);
+
+
+
+
+    mitk::TrackingDeviceSource::Pointer m_Source; ///< source that connects to the tracking device
+
+    mitk::NavigationDataLandmarkTransformFilter::Pointer m_FiducialRegistrationFilter; ///< this filter transforms from tracking coordinates into mitk world coordinates
+    mitk::NavigationDataObjectVisualizationFilter::Pointer m_Visualizer; ///< visualization filter
+    mitk::NavigationDataToPointSetFilter::Pointer m_RegistrationTrackingFiducialsFilter;  ///< fill the Registration Tracking Fiducials with instrument position in tracking space 
+  
+
 
 
 private:
 
   QToolBox* m_ToolBox;
+  QCheckBox* m_TrackingVolumeCheckbox;
 
   QmitkNDIConfigurationWidget* m_NDIConfigWidget;  // tracking device configuration widget
   QmitkFiducialRegistrationWidget* m_RegistrationWidget; // landmark registration widget
   QmitkUpdateTimerWidget* m_RenderingTimerWidget; // update timer widget for rendering and updating
   QmitkToolTrackingStatusWidget* m_ToolStatusWidget; // tracking status visualizer widget
+
+  std::string m_RegistrationTrackingFiducialsName;
+  std::string m_RegistrationImageFiducialsName;
 };
 
 #endif // IGTTrackingLabView_h

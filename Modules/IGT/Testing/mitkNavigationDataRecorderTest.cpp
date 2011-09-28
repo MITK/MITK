ac74d120c2f3b28cb66f4cafc8a8b8b1447ba99a
Alfred Franz <a.franz@dkfz-heidelberg.de>
2011-09-28 15:20:38 +0200

    added new test methods

--- Modules/IGT/Testing/mitkNavigationDataRecorderTest.cpp
+++ Modules/IGT/Testing/mitkNavigationDataRecorderTest.cpp
@@ -15,31 +15,38 @@ PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
 
-#include "mitkNavigationDataRecorder.h"
-#include "mitkNavigationData.h"
+#include <mitkNavigationDataRecorder.h>
+#include <mitkNavigationData.h>
+#include <mitkStandardFileLocations.h>
+#include <mitkTestingMacros.h>
 
-#include "mitkTestingMacros.h"
+#include <Poco/Path.h>
+#include <Poco/File.h>
+
 
 #include <iostream>
 #include <sstream>
 
-/**Documentation
- *  test for the class "NavigationDataRecorder".
- */
-int mitkNavigationDataRecorderTest(int /* argc */, char* /*argv*/[])
-{
-  MITK_TEST_BEGIN("NavigationDataRecorder");
-  std::string tmp = "";
+class mitkNavigationDataRecorderTestClass
+  {
+  public:
+
+  static void TestInstantiation()
+    {
+    // let's create an object of our class  
+    mitk::NavigationDataRecorder::Pointer recorder = mitk::NavigationDataRecorder::New();
+    MITK_TEST_CONDITION( recorder.IsNotNull(), "Testing instatiation of NavigationDataRecorder");
+    }
+
+  static void TestRecordingWithGivenStream()
+    {
+    std::string tmp = "";
   std::ostringstream* stream = new std::ostringstream( std::ostringstream::trunc );
   stream->setf( std::ios::fixed, std::ios::floatfield );
 
   // let's create an object of our class  
   mitk::NavigationDataRecorder::Pointer recorder = mitk::NavigationDataRecorder::New();
   
-  // first test: did this work?
-  // using MITK_TEST_CONDITION_REQUIRED makes the test stop after failure, since
-  // it makes no sense to continue without an object.
-  MITK_TEST_CONDITION_REQUIRED(recorder.IsNotNull(), "Testing instantiation");
 
   MITK_TEST_CONDITION(recorder->GetInputs().size() == 0, "testing initial number of inputs"); 
   MITK_TEST_CONDITION(recorder->GetOutputs().size() == 0, "testing initial number of outputs");
@@ -91,6 +98,70 @@ int mitkNavigationDataRecorderTest(int /* argc */, char* /*argv*/[])
   const char* string = recorder->GetFileName();
   MITK_TEST_CONDITION( strcmp(string, "blablabla") == 0, "check if set- and getName-methods work");
 
+    }
+
+  static void TestRecordingOnHarddiscXML()
+    {
+    mitk::NavigationDataRecorder::Pointer recorder = mitk::NavigationDataRecorder::New();
+
+    //create filename
+    std::string filename = mitk::StandardFileLocations::GetInstance()->GetOptionDirectory()+Poco::Path::separator()+"Recordertest.xml";
+
+    recorder->SetFileName(filename.c_str());
+
+    mitk::NavigationData::Pointer naviData = mitk::NavigationData::New();
+    recorder->AddNavigationData( naviData );
+    recorder->StartRecording();
+
+    for ( unsigned int i=0; i<5; i++ )
+     {
+     mitk::Point3D pnt;
+     pnt[0] = i + 1;
+     pnt[1] = i + 1/2;
+     pnt[2] = i +1*3;
+     naviData->SetPosition(pnt);
+     recorder->Update();
+     }
+    
+    recorder->StopRecording();
+      
+    std::string prooffilename = mitk::StandardFileLocations::GetInstance()->GetOptionDirectory()+Poco::Path::separator()+"Recordertest-0.xml";
+    Poco::File myFile(prooffilename);
+    MITK_TEST_CONDITION(myFile.exists(),"Testing recording on harddisc (does file exist?).");
+    }
+
+  static void CleanUp()
+    {
+    std::string prooffilename = mitk::StandardFileLocations::GetInstance()->GetOptionDirectory()+Poco::Path::separator()+"Recordertest-0.xml";
+    MITK_INFO << prooffilename;
+    try
+    {
+    
+    Poco::File myFile(prooffilename);
+    if (myFile.exists()) myFile.remove();
+    }
+    catch(std::exception e)
+    {
+      MITK_INFO << "Exception:" << e.what();
+    }
+    }
+
+  };
+
+/**Documentation
+ *  test for the class "NavigationDataRecorder".
+ */
+int mitkNavigationDataRecorderTest(int /* argc */, char* /*argv*/[])
+{
+  MITK_TEST_BEGIN("NavigationDataRecorder");
+
+  mitkNavigationDataRecorderTestClass::TestInstantiation();
+  mitkNavigationDataRecorderTestClass::TestRecordingWithGivenStream();
+  mitkNavigationDataRecorderTestClass::TestRecordingOnHarddiscXML();
+  Sleep(1000);
+  mitkNavigationDataRecorderTestClass::CleanUp();
+
+  
   // always end with this!
   MITK_TEST_END();
 }

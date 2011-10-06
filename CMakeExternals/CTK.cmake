a28f12ea91f568738396fa209376f58a6a06f6da
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2011-10-07 01:25:29 +0200

    Use CTK debug output cleanup

--- CMakeExternals/CTK.cmake
+++ CMakeExternals/CTK.cmake
@@ -15,7 +15,7 @@ IF(MITK_USE_CTK)
 
   IF(NOT DEFINED CTK_DIR)
     
-    SET(revision_tag 70943814)
+    SET(revision_tag 25b5c22)
     IF(${proj}_REVISION_TAG)
       SET(revision_tag ${${proj}_REVISION_TAG})
     ENDIF()

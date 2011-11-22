6021e4f138e322b110fec82686fcb65d94426216
Jan Hering <j.hering@dkfz-heidelberg.de>
2011-11-22 17:15:56 +0100

    COMP Taking static_cast instead of C-like cast

--- Core/Code/DataManagement/mitkLevelWindow.cpp
+++ Core/Code/DataManagement/mitkLevelWindow.cpp
@@ -288,8 +288,8 @@ void mitk::LevelWindow::SetAuto(const mitk::Image* image, bool tryPicTags, bool 
   // Fix for bug# 344 Level Window wird bei Eris Cut bildern nicht richtig gesetzt
   if (image->GetPixelType()== typeid(int)  && image->GetPixelType().GetBpe() >= 8)
   {
-    // the windows compiler complains about ambiguos 'pow' call, therefore explicit casting to (double, int)
-    if (minValue == -( pow( (double) 2.0, ((int)image->GetPixelType().GetBpe()/2) ) ) )
+    // the windows compiler complains about ambiguos 'pow' call, therefore static casting to (double, int)
+    if (minValue == -( pow( (double) 2.0, static_cast<int>(image->GetPixelType().GetBpe()/2) ) ) )
     {
       minValue = min2ndValue;
     }

887eb527295632a56bb62d842a471dde8b14f000
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2011-12-11 20:33:58 +0100

    Print a warning if an unknown plug-in target is installed.

--- BlueBerry/CMake/MacroInstallCTKPlugin.cmake
+++ BlueBerry/CMake/MacroInstallCTKPlugin.cmake
@@ -9,34 +9,38 @@ MACRO(MACRO_INSTALL_CTK_PLUGIN)
   ENDIF()
   
   FOREACH(_install_target ${_INSTALL_TARGETS})
-    GET_TARGET_PROPERTY(_is_imported ${_install_target} IMPORTED)
-    IF(_is_imported)
-      GET_TARGET_PROPERTY(_import_loc_debug ${_install_target} IMPORTED_LOCATION_DEBUG)
-      GET_TARGET_PROPERTY(_import_loc_release ${_install_target} IMPORTED_LOCATION_RELEASE)
-      GET_FILENAME_COMPONENT(_target_filename_debug "${_import_loc_debug}" NAME)
-      GET_FILENAME_COMPONENT(_target_filename_release "${_import_loc_release}" NAME)
-      INSTALL(FILES ${_import_loc_debug}
-              DESTINATION ${_INSTALL_DESTINATION}
-              CONFIGURATIONS Debug)
-      INSTALL(FILES ${_import_loc_release}
-              DESTINATION ${_INSTALL_DESTINATION}
-              CONFIGURATIONS Release)
-      IF(UNIX AND NOT APPLE)
-        IF(_target_filename_debug)
-          INSTALL(CODE "FILE(RPATH_REMOVE
-                             FILE \"\${CMAKE_INSTALL_PREFIX}/${_INSTALL_DESTINATION}/${_target_filename_debug}\")")
+    IF(TARGET ${_install_target})
+      GET_TARGET_PROPERTY(_is_imported ${_install_target} IMPORTED)
+      IF(_is_imported)
+        GET_TARGET_PROPERTY(_import_loc_debug ${_install_target} IMPORTED_LOCATION_DEBUG)
+        GET_TARGET_PROPERTY(_import_loc_release ${_install_target} IMPORTED_LOCATION_RELEASE)
+        GET_FILENAME_COMPONENT(_target_filename_debug "${_import_loc_debug}" NAME)
+        GET_FILENAME_COMPONENT(_target_filename_release "${_import_loc_release}" NAME)
+        INSTALL(FILES ${_import_loc_debug}
+                DESTINATION ${_INSTALL_DESTINATION}
+                CONFIGURATIONS Debug)
+        INSTALL(FILES ${_import_loc_release}
+                DESTINATION ${_INSTALL_DESTINATION}
+                CONFIGURATIONS Release)
+        IF(UNIX AND NOT APPLE)
+          IF(_target_filename_debug)
+            INSTALL(CODE "FILE(RPATH_REMOVE
+                               FILE \"\${CMAKE_INSTALL_PREFIX}/${_INSTALL_DESTINATION}/${_target_filename_debug}\")")
+          ENDIF()
+          IF(_target_filename_release)
+            INSTALL(CODE "FILE(RPATH_REMOVE
+                               FILE \"\${CMAKE_INSTALL_PREFIX}/${_INSTALL_DESTINATION}/${_target_filename_release}\")")
+          ENDIF()
         ENDIF()
-        IF(_target_filename_release)
-          INSTALL(CODE "FILE(RPATH_REMOVE
-                             FILE \"\${CMAKE_INSTALL_PREFIX}/${_INSTALL_DESTINATION}/${_target_filename_release}\")")
-        ENDIF()
+      ELSE()
+        INSTALL(TARGETS ${_install_target}
+                RUNTIME DESTINATION ${_INSTALL_DESTINATION}
+                LIBRARY DESTINATION ${_INSTALL_DESTINATION}
+                #ARCHIVE DESTINATION ${_INSTALL_DESTINATION}
+                )
       ENDIF()
     ELSE()
-      INSTALL(TARGETS ${_install_target}
-              RUNTIME DESTINATION ${_INSTALL_DESTINATION}
-              LIBRARY DESTINATION ${_INSTALL_DESTINATION}
-              #ARCHIVE DESTINATION ${_INSTALL_DESTINATION}
-              )
+      MESSAGE(WARNING "Ignoring unknown plug-in target \"${_install_target}\" for installation.")
     ENDIF()
   ENDFOREACH()
   

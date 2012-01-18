919bde30a994cba83f331eb179fe49de2d39431d
Peter Neher <p.neher@dkfz-heidelberg.de>
2012-01-18 17:33:52 +0100

    added default values for m_Scalar2ndMin and Max

--- Core/Code/DataManagement/mitkImageStatisticsHolder.cpp
+++ Core/Code/DataManagement/mitkImageStatisticsHolder.cpp
@@ -211,6 +211,8 @@ void mitk::ImageStatisticsHolder::ComputeImageStatistics(int t)
   {
     m_ScalarMin[t] = 0;
     m_ScalarMax[t] = 255;
+    m_Scalar2ndMin[t] = 0;
+    m_Scalar2ndMax[t] = 255;
   }
 }
 

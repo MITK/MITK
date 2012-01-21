f1fe0206ec0cde3c74137df9bbc1be6ec3195595
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2012-01-21 18:11:56 +0100

    Implemented difference, union and intersection operations.

--- Modules/Bundles/org.mitk.gui.qt.segmentation/src/internal/QmitkBooleanOperationsView.cpp
+++ Modules/Bundles/org.mitk.gui.qt.segmentation/src/internal/QmitkBooleanOperationsView.cpp
@@ -21,6 +21,10 @@ PURPOSE.  See the above copyright notices for more information.
 #include <mitkNodePredicateAnd.h>
 #include <mitkNodePredicateNot.h>
 #include <mitkNodePredicateProperty.h>
+#include <mitkImageCast.h>
+#include <itkAndImageFilter.h>
+#include <itkNotImageFilter.h>
+#include <itkOrImageFilter.h>
 
 QmitkBooleanOperationsView::QmitkBooleanOperationsView()
   : m_Parent(NULL)
@@ -88,12 +92,105 @@ void QmitkBooleanOperationsView::OnSegmentationImage2Changed(const mitk::DataNod
 
 void QmitkBooleanOperationsView::OnDifferenceButtonClicked()
 {
+  mitk::DataNode::Pointer dataNode1 = m_Controls.cmbSegmentationImage1->GetSelectedNode();
+  mitk::DataNode::Pointer dataNode2 = m_Controls.cmbSegmentationImage2->GetSelectedNode();
+  
+  mitk::Image::Pointer image1 = dynamic_cast<mitk::Image *>(dataNode1->GetData());
+  mitk::Image::Pointer image2 = dynamic_cast<mitk::Image *>(dataNode2->GetData());
+
+  typedef itk::Image<unsigned char, 3> ImageType;
+
+  ImageType::Pointer itkImage1 = ImageType::New();
+  ImageType::Pointer itkImage2 = ImageType::New();
+
+  mitk::CastToItkImage(image1, itkImage1);
+  mitk::CastToItkImage(image2, itkImage2);
+
+  itk::NotImageFilter<ImageType, ImageType>::Pointer notFilter = itk::NotImageFilter<ImageType, ImageType>::New();
+  notFilter->SetInput(itkImage2);
+
+  itk::AndImageFilter<ImageType>::Pointer andFilter = itk::AndImageFilter<ImageType>::New();
+  andFilter->SetInput1(itkImage1);
+  andFilter->SetInput2(notFilter->GetOutput());
+  andFilter->UpdateLargestPossibleRegion();
+
+  mitk::Image::Pointer image3 = mitk::Image::New();
+  mitk::CastToMitkImage<ImageType>(andFilter->GetOutput(), image3);
+
+  image3->DisconnectPipeline();
+
+  mitk::DataNode::Pointer dataNode3 = mitk::DataNode::New();
+  dataNode3->SetBoolProperty("binary", true);
+  dataNode3->SetName(std::string("Difference_") + dataNode2->GetName());
+  dataNode3->SetData(image3);
+
+  GetDefaultDataStorage()->Add(dataNode3, dataNode1);
 }
 
 void QmitkBooleanOperationsView::OnUnionButtonClicked()
 {
+  mitk::DataNode::Pointer dataNode1 = m_Controls.cmbSegmentationImage1->GetSelectedNode();
+  mitk::DataNode::Pointer dataNode2 = m_Controls.cmbSegmentationImage2->GetSelectedNode();
+  
+  mitk::Image::Pointer image1 = dynamic_cast<mitk::Image *>(dataNode1->GetData());
+  mitk::Image::Pointer image2 = dynamic_cast<mitk::Image *>(dataNode2->GetData());
+
+  typedef itk::Image<unsigned char, 3> ImageType;
+
+  ImageType::Pointer itkImage1 = ImageType::New();
+  ImageType::Pointer itkImage2 = ImageType::New();
+
+  mitk::CastToItkImage(image1, itkImage1);
+  mitk::CastToItkImage(image2, itkImage2);
+
+  itk::OrImageFilter<ImageType>::Pointer orFilter = itk::OrImageFilter<ImageType>::New();
+  orFilter->SetInput1(itkImage1);
+  orFilter->SetInput2(itkImage2);
+  orFilter->UpdateLargestPossibleRegion();
+
+  mitk::Image::Pointer image3 = mitk::Image::New();
+  mitk::CastToMitkImage<ImageType>(orFilter->GetOutput(), image3);
+
+  image3->DisconnectPipeline();
+
+  mitk::DataNode::Pointer dataNode3 = mitk::DataNode::New();
+  dataNode3->SetBoolProperty("binary", true);
+  dataNode3->SetName(std::string("Union_") + dataNode2->GetName());
+  dataNode3->SetData(image3);
+
+  GetDefaultDataStorage()->Add(dataNode3, dataNode1);
 }
 
 void QmitkBooleanOperationsView::OnIntersectionButtonClicked()
 {
+  mitk::DataNode::Pointer dataNode1 = m_Controls.cmbSegmentationImage1->GetSelectedNode();
+  mitk::DataNode::Pointer dataNode2 = m_Controls.cmbSegmentationImage2->GetSelectedNode();
+  
+  mitk::Image::Pointer image1 = dynamic_cast<mitk::Image *>(dataNode1->GetData());
+  mitk::Image::Pointer image2 = dynamic_cast<mitk::Image *>(dataNode2->GetData());
+
+  typedef itk::Image<unsigned char, 3> ImageType;
+
+  ImageType::Pointer itkImage1 = ImageType::New();
+  ImageType::Pointer itkImage2 = ImageType::New();
+
+  mitk::CastToItkImage(image1, itkImage1);
+  mitk::CastToItkImage(image2, itkImage2);
+
+  itk::AndImageFilter<ImageType>::Pointer andFilter = itk::AndImageFilter<ImageType>::New();
+  andFilter->SetInput1(itkImage1);
+  andFilter->SetInput2(itkImage2);
+  andFilter->UpdateLargestPossibleRegion();
+
+  mitk::Image::Pointer image3 = mitk::Image::New();
+  mitk::CastToMitkImage<ImageType>(andFilter->GetOutput(), image3);
+
+  image3->DisconnectPipeline();
+
+  mitk::DataNode::Pointer dataNode3 = mitk::DataNode::New();
+  dataNode3->SetBoolProperty("binary", true);
+  dataNode3->SetName(std::string("Intersection_") + dataNode2->GetName());
+  dataNode3->SetData(image3);
+
+  GetDefaultDataStorage()->Add(dataNode3, dataNode1);
 }

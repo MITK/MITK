443caf5966fb5e7be23e39888246d4d713704c7f
Tobias Schroeder <t.schroeder@dkfz-heidelberg.de>
2012-01-27 11:49:37 +0100

    don't use the new mitkExtractSliceFilter for segmentation until the corresponding overwritting filter is implemented

--- Modules/MitkExt/Interactions/mitkSegTool2D.cpp
+++ Modules/MitkExt/Interactions/mitkSegTool2D.cpp
@@ -25,7 +25,6 @@ PURPOSE.  See the above copyright notices for more information.
 
 #include "mitkExtractImageFilter.h"
 #include "mitkExtractDirectedPlaneImageFilter.h"
-#include "mitkExtractSliceFilter.h"
 
 //Include of the new ImageExtractor
 #include "mitkExtractDirectedPlaneImageFilterNew.h"
@@ -44,19 +43,17 @@ PURPOSE.  See the above copyright notices for more information.
 
 mitk::SegTool2D::SegTool2D(const char* type)
 :Tool(type),
-
  m_LastEventSender(NULL),
  m_LastEventSlice(0),
  m_Contourmarkername ("Position"),
  m_ShowMarkerNodes (true),
  m_3DInterpolationEnabled (true)
-
 {
-	// great magic numbers
-	CONNECT_ACTION( 80, OnMousePressed );
-	CONNECT_ACTION( 90, OnMouseMoved );
-	CONNECT_ACTION( 42, OnMouseReleased );
-	CONNECT_ACTION( 49014, OnInvertLogic );
+  // great magic numbers
+  CONNECT_ACTION( 80, OnMousePressed );
+  CONNECT_ACTION( 90, OnMouseMoved );
+  CONNECT_ACTION( 42, OnMouseReleased );
+  CONNECT_ACTION( 49014, OnInvertLogic );
 }
 
 mitk::SegTool2D::~SegTool2D()
@@ -65,166 +62,209 @@ mitk::SegTool2D::~SegTool2D()
 
 bool mitk::SegTool2D::OnMousePressed (Action*, const StateEvent* stateEvent)
 {
-	const PositionEvent* positionEvent = dynamic_cast<const PositionEvent*>(stateEvent->GetEvent());
-	if (!positionEvent) return false;
+  const PositionEvent* positionEvent = dynamic_cast<const PositionEvent*>(stateEvent->GetEvent());
+  if (!positionEvent) return false;
 
-	if ( positionEvent->GetSender()->GetMapperID() != BaseRenderer::Standard2D ) return false; // we don't want anything but 2D
+  if ( positionEvent->GetSender()->GetMapperID() != BaseRenderer::Standard2D ) return false; // we don't want anything but 2D
 
-	m_LastEventSender = positionEvent->GetSender();
-	m_LastEventSlice = m_LastEventSender->GetSlice();
+  m_LastEventSender = positionEvent->GetSender();
+  m_LastEventSlice = m_LastEventSender->GetSlice();
 
-	return true;
+  return true;
 }
 
 bool mitk::SegTool2D::OnMouseMoved   (Action*, const StateEvent* stateEvent)
 {
-	const PositionEvent* positionEvent = dynamic_cast<const PositionEvent*>(stateEvent->GetEvent());
-	if (!positionEvent) return false;
+  const PositionEvent* positionEvent = dynamic_cast<const PositionEvent*>(stateEvent->GetEvent());
+  if (!positionEvent) return false;
 
-	if ( m_LastEventSender != positionEvent->GetSender() ) return false;
-	if ( m_LastEventSlice  != m_LastEventSender->GetSlice() ) return false;
+  if ( m_LastEventSender != positionEvent->GetSender() ) return false;
+  if ( m_LastEventSlice  != m_LastEventSender->GetSlice() ) return false;
 
-	return true;
+  return true;
 }
 
 bool mitk::SegTool2D::OnMouseReleased(Action*, const StateEvent* stateEvent)
 {
-	const PositionEvent* positionEvent = dynamic_cast<const PositionEvent*>(stateEvent->GetEvent());
-	if (!positionEvent) return false;
+  const PositionEvent* positionEvent = dynamic_cast<const PositionEvent*>(stateEvent->GetEvent());
+  if (!positionEvent) return false;
 
-	if ( m_LastEventSender != positionEvent->GetSender() ) return false;
-	if ( m_LastEventSlice  != m_LastEventSender->GetSlice() ) return false;
+  if ( m_LastEventSender != positionEvent->GetSender() ) return false;
+  if ( m_LastEventSlice  != m_LastEventSender->GetSlice() ) return false;
 
-	return true;
+  return true;
 }
 
 bool mitk::SegTool2D::OnInvertLogic(Action*, const StateEvent* stateEvent)
 {
-	const PositionEvent* positionEvent = dynamic_cast<const PositionEvent*>(stateEvent->GetEvent());
-	if (!positionEvent) return false;
+  const PositionEvent* positionEvent = dynamic_cast<const PositionEvent*>(stateEvent->GetEvent());
+  if (!positionEvent) return false;
 
-	if ( m_LastEventSender != positionEvent->GetSender() ) return false;
-	if ( m_LastEventSlice  != m_LastEventSender->GetSlice() ) return false;
+  if ( m_LastEventSender != positionEvent->GetSender() ) return false;
+  if ( m_LastEventSlice  != m_LastEventSender->GetSlice() ) return false;
 
-	return true;
+  return true;
 }
 
 bool mitk::SegTool2D::DetermineAffectedImageSlice( const Image* image, const PlaneGeometry* plane, int& affectedDimension, int& affectedSlice )
 {
-	assert(image);
-	assert(plane);
+  assert(image);
+  assert(plane);
 
-	// compare normal of plane to the three axis vectors of the image
-	Vector3D normal       = plane->GetNormal();
-	Vector3D imageNormal0 = image->GetSlicedGeometry()->GetAxisVector(0);
-	Vector3D imageNormal1 = image->GetSlicedGeometry()->GetAxisVector(1);
-	Vector3D imageNormal2 = image->GetSlicedGeometry()->GetAxisVector(2);
+  // compare normal of plane to the three axis vectors of the image
+  Vector3D normal       = plane->GetNormal();
+  Vector3D imageNormal0 = image->GetSlicedGeometry()->GetAxisVector(0);
+  Vector3D imageNormal1 = image->GetSlicedGeometry()->GetAxisVector(1);
+  Vector3D imageNormal2 = image->GetSlicedGeometry()->GetAxisVector(2);
 
-	normal.Normalize();
-	imageNormal0.Normalize();
-	imageNormal1.Normalize();
-	imageNormal2.Normalize();
+  normal.Normalize();
+  imageNormal0.Normalize();
+  imageNormal1.Normalize();
+  imageNormal2.Normalize();
 
-	imageNormal0.Set_vnl_vector( vnl_cross_3d<ScalarType>(normal.Get_vnl_vector(),imageNormal0.Get_vnl_vector()) );
-	imageNormal1.Set_vnl_vector( vnl_cross_3d<ScalarType>(normal.Get_vnl_vector(),imageNormal1.Get_vnl_vector()) );
-	imageNormal2.Set_vnl_vector( vnl_cross_3d<ScalarType>(normal.Get_vnl_vector(),imageNormal2.Get_vnl_vector()) );
+  imageNormal0.Set_vnl_vector( vnl_cross_3d<ScalarType>(normal.Get_vnl_vector(),imageNormal0.Get_vnl_vector()) );
+  imageNormal1.Set_vnl_vector( vnl_cross_3d<ScalarType>(normal.Get_vnl_vector(),imageNormal1.Get_vnl_vector()) );
+  imageNormal2.Set_vnl_vector( vnl_cross_3d<ScalarType>(normal.Get_vnl_vector(),imageNormal2.Get_vnl_vector()) );
 
-	double eps( 0.00001 );
-	// transversal
-	if ( imageNormal2.GetNorm() <= eps )
-	{
-		affectedDimension = 2;
-	}
-	// sagittal
-	else if ( imageNormal1.GetNorm() <= eps )
-	{
-		affectedDimension = 1;
-	}
-	// frontal
-	else if ( imageNormal0.GetNorm() <= eps )
-	{
-		affectedDimension = 0;
-	}
-	else
-	{
-		affectedDimension = -1; // no idea
-		return false;
-	}
+  double eps( 0.00001 );
+  // transversal
+  if ( imageNormal2.GetNorm() <= eps )
+  {
+    affectedDimension = 2;
+  }
+  // sagittal
+  else if ( imageNormal1.GetNorm() <= eps )
+  {
+    affectedDimension = 1;
+  }
+  // frontal
+  else if ( imageNormal0.GetNorm() <= eps )
+  {
+    affectedDimension = 0;
+  }
+  else
+  {
+    affectedDimension = -1; // no idea
+    return false;
+  }
 
-	// determine slice number in image
-	Geometry3D* imageGeometry = image->GetGeometry(0);
-	Point3D testPoint = imageGeometry->GetCenter();
-	Point3D projectedPoint;
-	plane->Project( testPoint, projectedPoint );
+  // determine slice number in image
+  Geometry3D* imageGeometry = image->GetGeometry(0);
+  Point3D testPoint = imageGeometry->GetCenter();
+  Point3D projectedPoint;
+  plane->Project( testPoint, projectedPoint );
 
-	Point3D indexPoint;
+  Point3D indexPoint;
 
-	imageGeometry->WorldToIndex( projectedPoint, indexPoint );
-	affectedSlice = ROUND( indexPoint[affectedDimension] );
-	MITK_DEBUG << "indexPoint " << indexPoint << " affectedDimension " << affectedDimension << " affectedSlice " << affectedSlice;
+  imageGeometry->WorldToIndex( projectedPoint, indexPoint );
+  affectedSlice = ROUND( indexPoint[affectedDimension] );
+  MITK_DEBUG << "indexPoint " << indexPoint << " affectedDimension " << affectedDimension << " affectedSlice " << affectedSlice;
 
-	// check if this index is still within the image
-	if ( affectedSlice < 0 || affectedSlice >= static_cast<int>(image->GetDimension(affectedDimension)) ) return false;
+  // check if this index is still within the image
+  if ( affectedSlice < 0 || affectedSlice >= static_cast<int>(image->GetDimension(affectedDimension)) ) return false;
 
-	return true;
+  return true;
 }
 
 mitk::Image::Pointer mitk::SegTool2D::GetAffectedImageSliceAs2DImage(const PositionEvent* positionEvent, const Image* image)
 {
+  if (!positionEvent) return NULL;
 
-	if (!positionEvent) return NULL;
+  assert( positionEvent->GetSender() ); // sure, right?
+  unsigned int timeStep = positionEvent->GetSender()->GetTimeStep( image ); // get the timestep of the visible part (time-wise) of the image
 
-	assert( positionEvent->GetSender() ); // sure, right?
-	unsigned int timeStep = positionEvent->GetSender()->GetTimeStep( image ); // get the timestep of the visible part (time-wise) of the image
+  // first, we determine, which slice is affected
+  const PlaneGeometry* planeGeometry( dynamic_cast<const PlaneGeometry*> (positionEvent->GetSender()->GetCurrentWorldGeometry2D() ) );
 
-	const PlaneGeometry* planeGeometry( dynamic_cast<const PlaneGeometry*> (positionEvent->GetSender()->GetCurrentWorldGeometry2D() ) );
+  if ( !image || !planeGeometry ) return NULL;
 
-	if ( !image || !planeGeometry ) return NULL;
+  int affectedDimension( -1 );
+  int affectedSlice( -1 );
+  //DetermineAffectedImageSlice( image, planeGeometry, affectedDimension, affectedSlice );
+  if ( DetermineAffectedImageSlice( image, planeGeometry, affectedDimension, affectedSlice ) )
+  {
+    try
+    {
+      // now we extract the correct slice from the volume, resulting in a 2D image
+      ExtractImageFilter::Pointer extractor= ExtractImageFilter::New();
+      extractor->SetInput( image );
+      extractor->SetSliceDimension( affectedDimension );
+      extractor->SetSliceIndex( affectedSlice );
+      extractor->SetTimeStep( timeStep );
+      extractor->Update();
 
-	try
-	{
-		ExtractSliceFilter::Pointer extractor = ExtractSliceFilter::New();
-		extractor->SetInput( image );
-		extractor->SetTimeStep( timeStep );
-		extractor->SetWorldGeometry( planeGeometry );
-		extractor->Update();
+      // here we have a single slice that can be modified
+      Image::Pointer slice = extractor->GetOutput();
 
-		Image::Pointer slice = extractor->GetOutput();
+      //Workaround because of bug #7079
+      Point3D origin = slice->GetGeometry()->GetOrigin();
 
-		return slice;
-	}
-	catch(...)
-	{
-		// not working
-		return NULL;
-	}
+      int affectedDimension(-1);
+
+      if(positionEvent->GetSender()->GetRenderWindow() == mitk::BaseRenderer::GetRenderWindowByName("stdmulti.widget1"))
+      {
+          affectedDimension = 2;
+      }
+      if(positionEvent->GetSender()->GetRenderWindow() == mitk::BaseRenderer::GetRenderWindowByName("stdmulti.widget2"))
+      {
+          affectedDimension = 0;
+      }
+      if(positionEvent->GetSender()->GetRenderWindow() == mitk::BaseRenderer::GetRenderWindowByName("stdmulti.widget3"))
+      {
+          affectedDimension = 1;
+      }
+
+      if (affectedDimension != -1)
+      {
+          origin[affectedDimension] = planeGeometry->GetOrigin()[affectedDimension];
+          slice->GetGeometry()->SetOrigin(origin);
+      }
+      //Workaround end
+
+      return slice;
+    }
+    catch(...)
+    {
+      // not working
+      return NULL;
+    }
+  }
+  else
+  {
+    ExtractDirectedPlaneImageFilterNew::Pointer newExtractor = ExtractDirectedPlaneImageFilterNew::New();
+    newExtractor->SetInput( image );
+    newExtractor->SetActualInputTimestep( timeStep );
+    newExtractor->SetCurrentWorldGeometry2D( planeGeometry );
+    newExtractor->Update();
+    Image::Pointer slice = newExtractor->GetOutput();
+    return slice;
+  }
 }
 
 mitk::Image::Pointer mitk::SegTool2D::GetAffectedWorkingSlice(const PositionEvent* positionEvent)
 {
-	DataNode* workingNode( m_ToolManager->GetWorkingData(0) );
-	if ( !workingNode ) return NULL;
+  DataNode* workingNode( m_ToolManager->GetWorkingData(0) );
+  if ( !workingNode ) return NULL;
 
-	Image* workingImage = dynamic_cast<Image*>(workingNode->GetData());
-	if ( !workingImage ) return NULL;
+  Image* workingImage = dynamic_cast<Image*>(workingNode->GetData());
+  if ( !workingImage ) return NULL;
 
-	return GetAffectedImageSliceAs2DImage( positionEvent, workingImage );
+  return GetAffectedImageSliceAs2DImage( positionEvent, workingImage );
 }
 
 mitk::Image::Pointer mitk::SegTool2D::GetAffectedReferenceSlice(const PositionEvent* positionEvent)
 {
-	DataNode* referenceNode( m_ToolManager->GetReferenceData(0) );
-	if ( !referenceNode ) return NULL;
+  DataNode* referenceNode( m_ToolManager->GetReferenceData(0) );
+  if ( !referenceNode ) return NULL;
 
-	Image* referenceImage = dynamic_cast<Image*>(referenceNode->GetData());
-	if ( !referenceImage ) return NULL;
+  Image* referenceImage = dynamic_cast<Image*>(referenceNode->GetData());
+  if ( !referenceImage ) return NULL;
 
-	return GetAffectedImageSliceAs2DImage( positionEvent, referenceImage );
+  return GetAffectedImageSliceAs2DImage( positionEvent, referenceImage );
 }
 
 void mitk::SegTool2D::WriteBackSegmentationResult (const PositionEvent* positionEvent, Image* slice)
 {
-
   const PlaneGeometry* planeGeometry( dynamic_cast<const PlaneGeometry*> (positionEvent->GetSender()->GetCurrentWorldGeometry2D() ) );
 
   DataNode* workingNode( m_ToolManager->GetWorkingData(0) );
@@ -271,19 +311,15 @@ void mitk::SegTool2D::WriteBackSegmentationResult (const PositionEvent* position
 void mitk::SegTool2D::SetShowMarkerNodes(bool status)
 {
     m_ShowMarkerNodes = status;
-
 }
 
 void mitk::SegTool2D::Enable3DInterpolation(bool status)
 {
-
     m_3DInterpolationEnabled = status;
-
 }
 
 unsigned int mitk::SegTool2D::AddContourmarker ( const PositionEvent* positionEvent )
 {
-
   const mitk::Geometry2D* plane = dynamic_cast<const Geometry2D*> (dynamic_cast< const mitk::SlicedGeometry3D*>(
     positionEvent->GetSender()->GetSliceNavigationController()->GetCurrentGeometry3D())->GetGeometry2D(0));
 
@@ -340,24 +376,23 @@ unsigned int mitk::SegTool2D::AddContourmarker ( const PositionEvent* positionEv
     }
   }
   return id;
-
 }
 
 void mitk::SegTool2D::InteractiveSegmentationBugMessage( const std::string& message )
 {
-	MITK_ERROR << "********************************************************************************" << std::endl
-		<< " " << message << std::endl
-		<< "********************************************************************************" << std::endl
-		<< "  " << std::endl
-		<< " If your image is rotated or the 2D views don't really contain the patient image, try to press the button next to the image selection. " << std::endl
-		<< "  " << std::endl
-		<< " Please file a BUG REPORT: " << std::endl
-		<< " http://bugs.mitk.org" << std::endl
-		<< " Contain the following information:" << std::endl
-		<< "  - What image were you working on?" << std::endl
-		<< "  - Which region of the image?" << std::endl
-		<< "  - Which tool did you use?" << std::endl
-		<< "  - What did you do?" << std::endl
-		<< "  - What happened (not)? What did you expect?" << std::endl;
+  MITK_ERROR << "********************************************************************************" << std::endl
+    << " " << message << std::endl
+    << "********************************************************************************" << std::endl
+    << "  " << std::endl
+    << " If your image is rotated or the 2D views don't really contain the patient image, try to press the button next to the image selection. " << std::endl
+    << "  " << std::endl
+    << " Please file a BUG REPORT: " << std::endl
+    << " http://bugs.mitk.org" << std::endl
+    << " Contain the following information:" << std::endl
+    << "  - What image were you working on?" << std::endl
+    << "  - Which region of the image?" << std::endl
+    << "  - Which tool did you use?" << std::endl
+    << "  - What did you do?" << std::endl
+    << "  - What happened (not)? What did you expect?" << std::endl;
 }
 

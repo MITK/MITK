81a4547a96c6c4212c53be06dfe2ecbe38d072ff
Michael Bauer <m.bauer@dkfz-heidelberg.de>
2012-02-23 18:01:00 +0100

    added QmitkDicomDataEventPublisher

---
+++ Modules/Bundles/org.mitk.gui.qt.dicom/src/internal/QmitkDicomDataEventPublisher.h
@@ -0,0 +1,37 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef QmitkDicomDataEventPublisher_H
+#define QmitkDicomDataEventPublisher_H
+
+#include <ctkEventHandler>
+
+class QmitkDicomDataEventPublisher : public QObject, public ctkEventHandler
+{
+	Q_OBJECT
+	Q_INTERFACES(ctkEventHandler)
+    public:
+
+        QmitkDicomDataEventPublisher();
+
+        virtual ~QmitkDicomDataEventPublisher();			
+		
+	private:
+
+};
+#endif // QmitkDicomDataEventPublisher_H

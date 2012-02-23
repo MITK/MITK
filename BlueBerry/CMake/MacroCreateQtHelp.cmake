d2ea22ac9d1004b7b20a0a38622884f7765eb3e1
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2012-02-23 21:33:44 +0100

    Removed CoreUI directory and merged CoreUI/Bundles and Modules/Bundles.
    There is now a top-level Plugins folder containing all MITK plug-ins.
    Further, the BlueBerry build system support for BlueBerry-style plug-ins
    has been dropped.

--- BlueBerry/CMake/MacroCreateQtHelp.cmake
+++ BlueBerry/CMake/MacroCreateQtHelp.cmake
@@ -1,23 +1,3 @@
-MACRO(_MACRO_CREATE_QT_COMPRESSED_HELP qch_file)
-   
-  CONFIGURE_FILE(${BlueBerry_SOURCE_DIR}/CMake/berryQtHelpDoxygen.conf.in
-                 ${PLUGIN_DOXYGEN_OUTPUT_DIR}/doxygen.conf
-                 )
-
-  SET(_generated_qch_file "${PLUGIN_DOXYGEN_OUTPUT_DIR}/qch/${BUNDLE-NAME}.qch")
-  SET(${qch_file} "${PLUGIN_OUTPUT_DIR}/resources/${BUNDLE-SYMBOLICNAME}.qch")
-
-  FILE(GLOB _file_dependencies "${PLUGIN_DOXYGEN_INPUT_DIR}/*.dox")
-  
-  ADD_CUSTOM_COMMAND(OUTPUT ${${qch_file}}
-                     COMMAND ${DOXYGEN_EXECUTABLE} ${PLUGIN_DOXYGEN_OUTPUT_DIR}/doxygen.conf
-                     COMMAND ${CMAKE_COMMAND} -E copy_if_different ${_generated_qch_file} ${${qch_file}} 
-                     DEPENDS ${PLUGIN_DOXYGEN_OUTPUT_DIR}/doxygen.conf ${_file_dependencies}
-                     )
-                     
-  #SET_SOURCE_FILES_PROPERTIES(${qch_file} PROPERTIES GENERATED 1)
-       
-ENDMACRO()
 
 FUNCTION(_FUNCTION_CREATE_CTK_QT_COMPRESSED_HELP qch_file use_xsl)
 

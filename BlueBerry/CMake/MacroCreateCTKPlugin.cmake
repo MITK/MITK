da04a7a52102e6fe17b9ea4f3925013e31ca0efd
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2012-02-29 11:50:22 +0100

    COMP: APPEND_STRING is not available in CMake < 2.8.6.

--- BlueBerry/CMake/MacroCreateCTKPlugin.cmake
+++ BlueBerry/CMake/MacroCreateCTKPlugin.cmake
@@ -98,13 +98,14 @@ MACRO(MACRO_CREATE_CTK_PLUGIN)
   )
   
   # Set compiler flags
-  SET(_plugin_compile_flags )
+  GET_TARGET_PROPERTY(_plugin_compile_flags ${PLUGIN_TARGET} COMPILE_FLAGS)
+  IF(NOT _plugin_compile_flags)
+    SET(_plugin_compile_flags "")
+  ENDIF()
   IF(WIN32)
     SET(_plugin_compile_flags "${_plugin_compile_flags} -DPOCO_NO_UNWINDOWS -DWIN32_LEAN_AND_MEAN")
   ENDIF()
-          
-  SET_PROPERTY(TARGET ${PLUGIN_TARGET} APPEND_STRING 
-               PROPERTY COMPILE_FLAGS "${_plugin_compile_flags}"
+  SET_TARGET_PROPERTIES(${PLUGIN_TARGET} PROPERTIES COMPILE_FLAGS "${_plugin_compile_flags}")
 
   SET(_PLUGIN_META_FILES "${CMAKE_CURRENT_SOURCE_DIR}/manifest_headers.cmake")
   IF(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/plugin.xml")

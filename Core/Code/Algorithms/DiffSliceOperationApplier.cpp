c0a07798d0609aa09ac9ef6f22a82a55ab84999d
Tobias Schroeder <t.schroeder@dkfz-heidelberg.de>
2012-03-07 17:33:19 +0100

    Initial operationActor class

---
+++ Core/Code/Algorithms/DiffSliceOperationApplier.cpp
@@ -0,0 +1,67 @@
+/*=========================================================================
+ 
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision: $
+ 
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+ 
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+ 
+=========================================================================*/
+
+#include "DiffSliceOperationApplier.h"
+#include "mitkRenderingManager.h"
+#include <vtkSmartPointer.h>
+
+mitk::DiffSliceOperationApplier::DiffSliceOperationApplier()
+{
+}
+		
+mitk::DiffSliceOperationApplier::~DiffSliceOperationApplier()
+{
+}
+
+void mitk::DiffSliceOperationApplier::ExecuteOperation( Operation* operation )
+{
+	DiffSliceOperation* imageOperation = dynamic_cast<DiffSliceOperation*>( operation );
+
+	if(!imageOperation)
+		return;
+
+	if(imageOperation->IsValid())
+	{
+		vtkSmartPointer<mitkVtkImageOverwrite> reslice = vtkSmartPointer<mitkVtkImageOverwrite>::New();
+
+		//Set the slice as 'input'
+		reslice->SetInputSlice(imageOperation->GetSlice()->GetVtkImageData(imageOperation->GetTimeStep()));
+
+		//set overwrite mode to true to write back to the image volume
+		reslice->SetOverwriteMode(true);
+		reslice->Modified();
+
+		mitk::ExtractSliceFilter::Pointer extractor =	mitk::ExtractSliceFilter::New(reslice);
+		extractor->SetInput( imageOperation->GetImage() );
+		extractor->SetTimeStep( imageOperation->GetTimeStep() );
+		extractor->SetWorldGeometry( imageOperation->GetWorldGeometry() );
+		extractor->SetVtkOutputRequest(true);
+		extractor->SetResliceTransformByGeometry( imageOperation->GetImage()->GetTimeSlicedGeometry()->GetGeometry3D( imageOperation->GetTimeStep() ) );
+
+		extractor->Modified();
+		extractor->Update();
+
+		RenderingManager::GetInstance()->RequestUpdateAll();
+	}
+}
+
+mitk::DiffSliceOperationApplier* mitk::DiffSliceOperationApplier::GetInstance()
+{
+  static DiffSliceOperationApplier* s_Instance = new DiffSliceOperationApplier();
+
+  return s_Instance;
+}
---
+++ Core/Code/Algorithms/DiffSliceOperationApplier.h
@@ -0,0 +1,46 @@
+/*=========================================================================
+ 
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision: $
+ 
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+ 
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+ 
+=========================================================================*/
+
+#ifndef mitkDiffSliceOpertationApplier_h_Included
+#define mitkDiffSliceOpertationApplier_h_Included
+
+#include "MitkExports.h"
+#include "mitkCommon.h"
+#include <mitkOperationActor.h>
+
+#include "DiffSliceOperation.h"
+#include <mitkExtractSliceFilter.h>
+#include <mitkVtkImageOverwrite.h>
+
+namespace mitk
+{
+	class MITK_CORE_EXPORT DiffSliceOperationApplier : public OperationActor
+	{
+	public:
+		mitkClassMacro(DiffSliceOperationApplier, OperationActor);
+		//itkNewMacro(DiffSliceOperationApplier);
+
+		static DiffSliceOperationApplier* GetInstance();
+
+		virtual void ExecuteOperation(Operation* op);
+	protected:
+		DiffSliceOperationApplier();
+		virtual ~DiffSliceOperationApplier();
+
+	};
+}
+#endif

6e01663c6205325de71ae5333893bf739d5a3b14
Michael Bauer <m.bauer@dkfz-heidelberg.de>
2012-03-26 11:30:53 +0200

    add preference page

---
+++ Modules/Bundles/org.mitk.gui.qt.dicom/src/internal/QmitkDicomPreferencePage.h
@@ -0,0 +1,34 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef QmitkDicomPreferencePage_h
+#define QmitkDicomPreferencePage_h
+
+#include <QString>
+#include <QStringList>
+
+    class QmitkDicomPreferencePage
+    {
+        public:
+
+            QmitkDicomPreferencePage();
+
+            virtual ~QmitkDicomPreferencePage();
+
+    };
+#endif // QmitkQmitkDicomPreferencePage_h

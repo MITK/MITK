1380a07954d4110167248d2503f030f99a9a9428
Alfred Franz <a.franz@dkfz-heidelberg.de>
2012-04-19 15:14:18 +0200

    added test cases for new macro

--- Core/Code/Testing/mitkExceptionTest.cpp
+++ Core/Code/Testing/mitkExceptionTest.cpp
@@ -20,6 +20,13 @@ PURPOSE.  See the above copyright notices for more information.
 #include "mitkTestingMacros.h"
 #include <itkObject.h>
 
+class SpecializedTestException : mitk::Exception
+  {
+  public:
+    SpecializedTestException(const char *file, unsigned int lineNumber, const char *desc, const char *loc) :
+    mitk::Exception(file,lineNumber,desc,loc){}
+  };
+
 class ExceptionTestClass : public itk::Object
 {
 public: 
@@ -31,7 +38,17 @@ public:
 
   void throwExceptionWithMacro()
   {
-  mitkExceptionMacro("test");
+  mitkExceptionMacro("TEST EXCEPION THROWING");
+  }
+
+  void throwSpecializedExceptionWithMacro1()
+  {
+  mitkSpecializedExceptionMacro("TEST EXCEPION SPECIALIZED THROWING", mitk::Exception);
+  }
+
+  void throwSpecializedExceptionWithMacro2()
+  {
+  mitkSpecializedExceptionMacro("TEST EXCEPION SPECIALIZED THROWING", SpecializedTestException);
   }
 
   static void TestExceptionConstructor()
@@ -63,13 +80,43 @@ public:
      }
     MITK_TEST_CONDITION_REQUIRED(exceptionThrown,"Testing mitkExceptionMacro");
     }
+
+  static void TestSpecializedExceptionMacro()
+    {
+    
+    //########## case 1: standard MITK::Exception ###############
+    bool exceptionThrown = false;
+    ExceptionTestClass myExceptionTestObject = ExceptionTestClass();
+    try
+     {
+     myExceptionTestObject.throwSpecializedExceptionWithMacro1();
+     }
+    catch(mitk::Exception)
+     {
+     exceptionThrown = true;
+     }
+    MITK_TEST_CONDITION_REQUIRED(exceptionThrown,"Testing mitkSpecializedExceptionMacro (Case 1: MITK::Exception)");
+
+    //########## case 2: specialized Exception ###############
+    exceptionThrown = false;
+    try
+     {
+     myExceptionTestObject.throwSpecializedExceptionWithMacro2();
+     }
+    catch(SpecializedTestException)
+     {
+     exceptionThrown = true;
+     }
+    MITK_TEST_CONDITION_REQUIRED(exceptionThrown,"Testing mitkSpecializedExceptionMacro (Case 2: specialized Exception)");
+    }
 };
 
 int mitkExceptionTest(int /*argc*/, char* /*argv*/[])
 {
   MITK_TEST_BEGIN("MITKException");
   ExceptionTestClass::TestExceptionConstructor();
-  ExceptionTestClass::TestExceptionMacro(); 
+  ExceptionTestClass::TestExceptionMacro();
+  ExceptionTestClass::TestSpecializedExceptionMacro();
   MITK_TEST_END();
 
 }

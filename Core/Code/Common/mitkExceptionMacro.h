b9edcf72de08bb7cc2a0f88af7b20f0cd55272ce
Alfred Franz <a.franz@dkfz-heidelberg.de>
2012-04-19 15:13:48 +0200

    added exception macro for specialized exceptions

--- Core/Code/Common/mitkExceptionMacro.h
+++ Core/Code/Common/mitkExceptionMacro.h
@@ -20,17 +20,27 @@ PURPOSE.  See the above copyright notices for more information.
 
 #include "mitkException.h"
 #include <itkMacro.h>
+#include <mitkLogMacros.h>
 
 /** The exception macro is used to print error information (i.e., usually
  * a condition that results in program failure). Example usage looks like:
  * mitkExceptionMacro(<< "this is error info" << this->SomeVariable); */
-#define mitkExceptionMacro(x) \
+#define mitkExceptionMacro(messagetext) \
   { \
   ::itk::OStringStream message; \
   message << "mitk::EXCEPTION: " << this->GetNameOfClass() \
-          << "(" << this << "): " x; \
-  mitk::Exception e_(__FILE__, __LINE__, message.str().c_str(),ITK_LOCATION); \
-  throw e_; /* Explicit naming to work around Intel compiler bug.  */ \
+          << "(" << this << "): " messagetext; \
+  MITK_DEBUG << message.str().c_str(); /* Print a MITK debug message to log exceptions in debug mode. */ \
+  throw mitk::Exception(__FILE__, __LINE__, message.str().c_str(),ITK_LOCATION); \
+  }
+
+#define mitkSpecializedExceptionMacro(messagetext,classname) \
+  { \
+  ::itk::OStringStream message; \
+  message << "mitk::EXCEPTION: " << this->GetNameOfClass() \
+          << "(" << this << "): " messagetext; \
+  MITK_DEBUG << message.str().c_str(); /* Print a MITK debug message to log exceptions in debug mode. */ \
+  throw classname(__FILE__, __LINE__, message.str().c_str(),ITK_LOCATION); /* Explicit naming to work around Intel compiler bug.  */ \
   }
   
 #endif

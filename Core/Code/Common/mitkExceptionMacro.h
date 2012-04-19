69464037a9f7e24a64237d910cb518df523fcdcc
Alfred Franz <a.franz@dkfz-heidelberg.de>
2012-04-19 15:52:28 +0200

    added documentation to exception macros and simplified code
    also fixed line endings

--- Core/Code/Common/mitkExceptionMacro.h
+++ Core/Code/Common/mitkExceptionMacro.h
@@ -22,23 +22,27 @@ PURPOSE.  See the above copyright notices for more information.
 #include <itkMacro.h>
 #include <mitkLogMacros.h>
 
-/** The exception macro is used to print error information (i.e., usually
- * a condition that results in program failure). Example usage looks like:
- * mitkExceptionMacro(<< "this is error info" << this->SomeVariable); */
-#define mitkExceptionMacro(messagetext) \
-  { \
-  ::itk::OStringStream message; \
-  message << "mitk::EXCEPTION: " << this->GetNameOfClass() \
-          << "(" << this << "): " messagetext; \
-  MITK_DEBUG << message.str().c_str(); /* Print a MITK debug message to log exceptions in debug mode. */ \
-  throw mitk::Exception(__FILE__, __LINE__, message.str().c_str(),ITK_LOCATION); \
-  }
+/** The exception macro is used to print error information / throw an exception 
+ *  (i.e., usually a condition that results in program failure). 
+ *
+ *  Example usage looks like:
+ *  mitkExceptionMacro("this is error info");
+ */
+#define mitkExceptionMacro(messagetext) {mitkSpecializedExceptionMacro(messagetext,mitk::Exception);}
 
+/** The specialized exception macro is used to print error information / throw exceptions 
+  * in cases of specialized errors. This means the second parameter must be a class which 
+  * inherits from mitk::Exception. An object of this exception is thrown when using the macro.
+  * Thus, more differentiated excaptions can be thrown, when needed.
+  *
+  * Example usage:
+  * mitkSpecializedExceptionMacro("this is error info",mitk::MySpecializedException); 
+  */
 #define mitkSpecializedExceptionMacro(messagetext,classname) \
   { \
   ::itk::OStringStream message; \
-  message << "mitk::EXCEPTION: " << this->GetNameOfClass() \
-          << "(" << this << "): " messagetext; \
+  message << "mitk::EXCEPTION: " \
+  << "(Object:" << this->GetNameOfClass() << "(" << this << ")" << "/File:" << __FILE__ << "/Line:" << __LINE__ << "): " messagetext; \
   MITK_DEBUG << message.str().c_str(); /* Print a MITK debug message to log exceptions in debug mode. */ \
   throw classname(__FILE__, __LINE__, message.str().c_str(),ITK_LOCATION); /* Explicit naming to work around Intel compiler bug.  */ \
   }

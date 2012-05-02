4b11f23db249c3fefd6a7a64ae9e194602ea3dd0
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2012-05-02 16:24:24 +0200

    Patch for ITK clang compatibility

--- CMakeExternals/PatchITK-3.20.cmake
+++ CMakeExternals/PatchITK-3.20.cmake
@@ -16,10 +16,21 @@ configure_file(${TEMPLATE_FILE} CMakeLists.txt @ONLY)
 # read whole file
 file(STRINGS Code/Common/itkLandmarkBasedTransformInitializer.h sourceCode2 NEWLINE_CONSUME)
 
-# substitute dependency to gdcmMSFF by dependencies for more libraries
+# backported fix from ITK4
 string(REGEX REPLACE "typedef typename ParametersType::ValueType +ParameterValueType;" "typedef typename TransformType::ScalarType                      ParameterValueType;" sourceCode2 ${sourceCode2})
 
 # set variable CONTENTS, which is substituted in TEMPLATE_FILE
 set(CONTENTS ${sourceCode2})
 configure_file(${TEMPLATE_FILE} Code/Common/itkLandmarkBasedTransformInitializer.h @ONLY)
+
+# third patch
+# read whole file
+file(STRINGS Code/Common/itkImageSource.h sourceCode3 NEWLINE_CONSUME)
+
+# remove ITK_NO_RETURN since the method sometimes returns which makes clang based builds crash
+string(REGEX REPLACE "ITK_NO_RETURN" "" sourceCode3 ${sourceCode3})
+
+# set variable CONTENTS, which is substituted in TEMPLATE_FILE
+set(CONTENTS ${sourceCode3})
+configure_file(${TEMPLATE_FILE} Code/Common/itkImageSource.h @ONLY)
 

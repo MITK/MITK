e1bfba63f6f26a5bf041e1db7afed7ced555c0d4
Thomas Kilgus <t.kilgus@dkfz-heidelberg.de>
2012-05-09 15:02:16 +0200

    Added default option WITH_CUDA:BOOL=OFF in OpenCV.cmake

--- CMakeExternals/OpenCV.cmake
+++ CMakeExternals/OpenCV.cmake
@@ -48,6 +48,7 @@ if(MITK_USE_OpenCV)
         -DBUILD_TESTS:BOOL=OFF
         -DBUILD_EXAMPLES:BOOL=OFF
         -DBUILD_DOXYGEN_DOCS:BOOL=OFF
+	-DWITH_CUDA:BOOL=OFF
         ${additional_cmake_args}
       DEPENDS ${proj_DEPENDENCIES}
     )

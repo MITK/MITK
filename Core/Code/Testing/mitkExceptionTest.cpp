c096c77911cc92f42363c2277a38033e310b9e3e
Alfred Franz <a.franz@dkfz-heidelberg.de>
2012-05-10 20:13:59 +0200

    added tests for the new macros and the streaming operators

--- Core/Code/Testing/mitkExceptionTest.cpp
+++ Core/Code/Testing/mitkExceptionTest.cpp
@@ -20,7 +20,7 @@ PURPOSE.  See the above copyright notices for more information.
 #include "mitkTestingMacros.h"
 #include <itkObject.h>
 
-class SpecializedTestException : mitk::Exception
+class SpecializedTestException : public mitk::Exception
   {
   public:
     mitkExceptionClassMacro(SpecializedTestException,mitk::Exception);
@@ -29,10 +29,25 @@ class SpecializedTestException : mitk::Exception
 class ExceptionTestClass : public itk::Object
 {
 public: 
-  void throwExceptionManually() //this method is ONLY to test the constructor and no code example
-                                //normally exceptions should only be thrown by using the exception macro!
+  void throwExceptionManually() 
+  //this method is ONLY to test the constructor and no code example
+  //normally exceptions should only be thrown by using the exception macro!
   {
   throw mitk::Exception("test.cpp",155,"","");
+  }
+
+  void throwSpecializedExceptionManually() 
+  //this method is ONLY to test the constructor and no code example
+  //normally exceptions should only be thrown by using the exception macro!
+  {
+  throw SpecializedTestException("test.cpp",155,"","");
+  }
+
+  void throwExceptionManually(std::string message1, std::string message2) 
+  //this method is ONLY to test methods of mitk::Exception and no code example
+  //normally exceptions should only be thrown by using the exception macro!
+  {
+  throw mitk::Exception("testfile.cpp",155,message1.c_str(),"") << message2;
   }
 
   void throwExceptionWithMacro()
@@ -47,12 +62,17 @@ public:
 
   void throwExceptionWithThrowMacro(std::string message)
   {
-  mitkThrow()<<message;
+  mitkThrow()<<message.c_str();
   }
 
   void throwSpecializedExceptionWithThrowMacro(std::string message)
   {
-  mitkThrow(mitk::Exception)<<message;
+  mitkThrowException(mitk::Exception)<<message;
+  }
+
+  void throwSpecializedExceptionWithThrowMacro2(std::string message)
+  {
+  mitkThrowException(SpecializedTestException)<<message;
   }
 
   void throwSpecializedExceptionWithMacro1()
@@ -77,7 +97,58 @@ public:
        {
        exceptionThrown = true;
        }
-    MITK_TEST_CONDITION_REQUIRED(exceptionThrown,"Testing mitkException");
+    MITK_TEST_CONDITION_REQUIRED(exceptionThrown,"Testing constructor of mitkException");
+
+
+    exceptionThrown = false;
+    try
+       {
+       myExceptionTestObject.throwSpecializedExceptionManually();
+       }
+    catch(SpecializedTestException)
+       {
+       exceptionThrown = true;
+       }
+    MITK_TEST_CONDITION_REQUIRED(exceptionThrown,"Testing constructor specialized exception (deriving from mitkException)");  
+    }
+
+  static void TestExceptionMessageStream()
+    {
+    //##### this method is ONLY to test the streaming operators of the exceptions and 
+    //##### NO code example. Please do not instantiate exceptions by yourself in normal code!
+    //##### Normally exceptions should only be thrown by using the exception macro!
+    mitk::Exception myException = mitk::Exception("testfile.cpp",111,"testmessage");
+    myException << " and additional stream";
+    MITK_TEST_CONDITION_REQUIRED(myException.GetDescription() == std::string("testmessage and additional stream"),"Testing mitkException message stream (adding std::string)");
+
+    myException.SetDescription("testmessage2");
+    myException << ' ' << 'a' << 'n' << 'd' << ' ' << 'c' << 'h' << 'a' << 'r' << 's';
+    MITK_TEST_CONDITION_REQUIRED(myException.GetDescription() == std::string("testmessage2 and chars"),"Testing mitkException message stream (adding single chars)");
+
+    myException.SetDescription("testmessage3");
+    myException << myException; //adding the object itself makes no sense but should work
+    MITK_TEST_CONDITION_REQUIRED(myException.GetDescription() != std::string(""),"Testing mitkException message stream (adding object)");
+
+    SpecializedTestException mySpecializedException = SpecializedTestException("testfile.cpp",111,"testmessage","test");
+    mySpecializedException << " and additional stream";
+    MITK_TEST_CONDITION_REQUIRED(mySpecializedException.GetDescription() == std::string("testmessage and additional stream"),"Testing specialized exception message stream (adding std::string)");
+    }
+
+  static void TestExceptionMessageStreamThrowing()
+    {
+    bool exceptionThrown = false;
+    ExceptionTestClass myExceptionTestObject = ExceptionTestClass();
+    std::string thrownMessage = "";
+    try
+     {
+     myExceptionTestObject.throwExceptionManually("message1"," and message2");
+     }
+    catch(mitk::Exception e)
+     {
+     thrownMessage = e.GetDescription();
+     exceptionThrown = true;
+     }
+    MITK_TEST_CONDITION_REQUIRED(exceptionThrown && (thrownMessage == std::string("message1 and message2")),"Testing throwing and streaming of mitk::Exception together.")
     }
 
   static void TestExceptionMacro()
@@ -155,10 +226,14 @@ public:
      {
      exceptionThrown = true;
      messageText = e.GetDescription();
+  
      }
-    MITK_TEST_CONDITION_REQUIRED(messageText=="test123","Testing message test of mitkThrow()");
+    MITK_TEST_CONDITION_REQUIRED((exceptionThrown && (messageText=="test123")),"Testing message test of mitkThrow()");
 
-    //case 3: specialized exception
+    //case 3: specialized exception / command mitkThrow(mitk::Exception)
+
+    exceptionThrown = false;   
+    messageText = "";
 
     try
      {
@@ -169,8 +244,23 @@ public:
      exceptionThrown = true;
      messageText = e.GetDescription();
      }
-    MITK_TEST_CONDITION_REQUIRED(messageText=="test123","Testing message test of mitkThrow()");
+    MITK_TEST_CONDITION_REQUIRED(exceptionThrown && messageText=="test123","Testing special exception with mitkThrow(mitk::Exception)");
 
+    //case 4: specialized exception / command mitkThrow(mitk::SpecializedException)
+
+    exceptionThrown = false;   
+    messageText = "";
+
+    try
+     {
+     myExceptionTestObject.throwSpecializedExceptionWithThrowMacro2("test123");
+     }
+    catch(SpecializedTestException e)
+     {
+     exceptionThrown = true;
+     messageText = e.GetDescription();
+     }
+    MITK_TEST_CONDITION_REQUIRED(exceptionThrown && messageText=="test123","Testing special exception with mitkThrow(mitk::SpecializedException)");
     
     }
 };
@@ -178,7 +268,11 @@ public:
 int mitkExceptionTest(int /*argc*/, char* /*argv*/[])
 {
   MITK_TEST_BEGIN("MITKException");
+
   ExceptionTestClass::TestExceptionConstructor();
+  ExceptionTestClass::TestExceptionMessageStream();
+  ExceptionTestClass::TestExceptionMessageStreamThrowing();
+  
   ExceptionTestClass::TestExceptionMacro();
   ExceptionTestClass::TestSpecializedExceptionMacro();
   ExceptionTestClass::TestMitkThrowMacro();

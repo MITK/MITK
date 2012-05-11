fbec4583cdf958dbc6b7a8c8de096c346d05fd44
Keno Maerz <k.maerz@dkfz-heidelberg.de>
2012-05-11 13:30:08 +0200

    COMP: Deactivated VideoSource Test until update to new openCV complete

--- Modules/US/Testing/files.cmake
+++ Modules/US/Testing/files.cmake
@@ -1,6 +1,6 @@
 SET(MODULE_TESTS
    mitkUSImageTest.cpp
-   mitkUSImageVideoSourceTest.cpp
+   #mitkUSImageVideoSourceTest.cpp // deactivated until updated to current openCV
    mitkUSProbeTest.cpp
    mitkUSDeviceTest.cpp
    #mitkUSPipelineTest.cpp //deactivated until bugfix for mitkImage cloning available

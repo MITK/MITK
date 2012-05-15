d5f82829a62767f95915de67c2d9cc276f5dd1ec
Caspar Goch <c.goch@dkfz-heidelberg.de>
2012-05-15 14:59:18 +0200

    Changed all BlueBerry copyright headers

--- BlueBerry/Bundles/org.blueberry.osgi/src/berryLog.h
+++ BlueBerry/Bundles/org.blueberry.osgi/src/berryLog.h
@@ -1,19 +1,18 @@
-/*=========================================================================
+/*===================================================================
 
-Program:   BlueBerry Platform
-Language:  C++
-Date:      $Date: 2009-03-06 01:07:35 +0100 (Fri, 06 Mar 2009) $
-Version:   $Revision: 16492 $
+The Medical Imaging Interaction Toolkit (MITK)
 
-Copyright (c) German Cancer Research Center, Division of Medical and
-Biological Informatics. All rights reserved.
-See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+Copyright (c) German Cancer Research Center, 
+Division of Medical and Biological Informatics.
+All rights reserved.
 
-This software is distributed WITHOUT ANY WARRANTY; without even
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the above copyright notices for more information.
+This software is distributed WITHOUT ANY WARRANTY; without 
+even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE.
 
-=========================================================================*/
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
 
 #ifndef __BERRY_LOG_H__
 #define __BERRY_LOG_H__

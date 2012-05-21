7d5755031db35f2349d25d5bbcb0788124bff411
Keno Maerz <k.maerz@dkfz-heidelberg.de>
2012-05-21 11:36:33 +0200

    Created Plugin Ultrasound

---
+++ Plugins/org.mitk.gui.qt.ultrasound/src/internal/UltrasoundSupport.cpp
@@ -0,0 +1,102 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$ 
+ 
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+// Blueberry
+#include <berryISelectionService.h>
+#include <berryIWorkbenchWindow.h>
+
+// Qmitk
+#include "UltrasoundSupport.h"
+
+// Qt
+#include <QMessageBox>
+
+
+const std::string UltrasoundSupport::VIEW_ID = "org.mitk.views.ultrasoundsupport";
+
+void UltrasoundSupport::SetFocus()
+{
+  m_Controls.buttonPerformImageProcessing->setFocus();
+}
+
+void UltrasoundSupport::CreateQtPartControl( QWidget *parent )
+{
+  // create GUI widgets from the Qt Designer's .ui file
+  m_Controls.setupUi( parent );
+  connect( m_Controls.buttonPerformImageProcessing, SIGNAL(clicked()), this, SLOT(DoImageProcessing()) );
+}
+
+void UltrasoundSupport::OnSelectionChanged( berry::IWorkbenchPart::Pointer /*source*/,
+                                             const QList<mitk::DataNode::Pointer>& nodes )
+{ 
+  // iterate all selected objects, adjust warning visibility
+  foreach( mitk::DataNode::Pointer node, nodes )
+  {
+    if( node.IsNotNull() && dynamic_cast<mitk::Image*>(node->GetData()) )
+    {
+      m_Controls.labelWarning->setVisible( false );
+      m_Controls.buttonPerformImageProcessing->setEnabled( true );
+      return;
+    }
+  }
+
+  m_Controls.labelWarning->setVisible( true );
+  m_Controls.buttonPerformImageProcessing->setEnabled( false );
+}
+
+
+void UltrasoundSupport::DoImageProcessing()
+{
+  QList<mitk::DataNode::Pointer> nodes = this->GetDataManagerSelection();
+  if (nodes.empty()) return;
+
+  mitk::DataNode* node = nodes.front();
+
+  if (!node)
+  {
+    // Nothing selected. Inform the user and return
+    QMessageBox::information( NULL, "Template", "Please load and select an image before starting image processing.");
+    return;
+  }
+
+  // here we have a valid mitk::DataNode
+
+  // a node itself is not very useful, we need its data item (the image)
+  mitk::BaseData* data = node->GetData();
+  if (data)
+  {
+    // test if this data item is an image or not (could also be a surface or something totally different)
+    mitk::Image* image = dynamic_cast<mitk::Image*>( data );
+    if (image)
+    {
+      std::stringstream message;
+      std::string name;
+      message << "Performing image processing for image ";
+      if (node->GetName(name))
+      {
+        // a property called "name" was found for this DataNode
+        message << "'" << name << "'";
+      }
+      message << ".";
+      MITK_INFO << message.str();
+
+      // actually do something here...
+
+    }
+  }
+}
---
+++ Plugins/org.mitk.gui.qt.ultrasound/src/internal/UltrasoundSupport.h
@@ -0,0 +1,67 @@
+/*=========================================================================
+
+Program:   Medical Imaging & Interaction Toolkit
+Language:  C++
+Date:      $Date$
+Version:   $Revision$ 
+ 
+Copyright (c) German Cancer Research Center, Division of Medical and
+Biological Informatics. All rights reserved.
+See MITKCopyright.txt or http://www.mitk.org/copyright.html for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef UltrasoundSupport_h
+#define UltrasoundSupport_h
+
+#include <berryISelectionListener.h>
+
+#include <QmitkAbstractView.h>
+
+#include "ui_UltrasoundSupportControls.h"
+
+
+/*!
+  \brief UltrasoundSupport
+
+  \warning  This class is not yet documented. Use "git blame" and ask the author to provide basic documentation.
+
+  \sa QmitkFunctionality
+  \ingroup ${plugin_target}_internal
+*/
+class UltrasoundSupport : public QmitkAbstractView
+{  
+  // this is needed for all Qt objects that should have a Qt meta-object
+  // (everything that derives from QObject and wants to have signal/slots)
+  Q_OBJECT
+  
+  public:  
+
+    static const std::string VIEW_ID;
+
+    virtual void CreateQtPartControl(QWidget *parent);
+
+  protected slots:
+  
+    /// \brief Called when the user clicks the GUI button
+    void DoImageProcessing();
+
+  protected:
+  
+    virtual void SetFocus();
+
+    /// \brief called by QmitkFunctionality when DataManager's selection has changed
+    virtual void OnSelectionChanged( berry::IWorkbenchPart::Pointer source,
+                                     const QList<mitk::DataNode::Pointer>& nodes );
+
+    Ui::UltrasoundSupportControls m_Controls;
+
+};
+
+#endif // UltrasoundSupport_h
+

6f1b1e50e357b75375bfcdea32c78cb0e3c37993
Caspar Goch <c.goch@dkfz-heidelberg.de>
2012-05-23 15:10:41 +0200

    Added copyright header to blueberry files missing them

--- BlueBerry/Bundles/org.blueberry.ui/src/internal/berryPartList.h
+++ BlueBerry/Bundles/org.blueberry.ui/src/internal/berryPartList.h
@@ -1,3 +1,18 @@
+/*===================================================================
+
+BlueBerry Platform
+
+Copyright (c) German Cancer Research Center, 
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without 
+even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
 #ifndef BERRYPARTLIST_H_
 #define BERRYPARTLIST_H_
 

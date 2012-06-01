3bdaf3150a2daec6afbe90e397c618cdc296b53f
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2012-06-01 08:42:40 +0200

    Use the proper types for level and window variables.

--- Core/Code/Interactions/mitkDisplayVectorInteractorLevelWindow.cpp
+++ Core/Code/Interactions/mitkDisplayVectorInteractorLevelWindow.cpp
@@ -87,11 +87,11 @@ bool mitk::DisplayVectorInteractorLevelWindow::ExecuteAction(Action* action, mit
 
       mitk::LevelWindow lv = mitk::LevelWindow();
       node->GetLevelWindow(lv);
-      int level = static_cast<int>(lv.GetLevel());
-      int window = static_cast<int>(lv.GetWindow());
+      ScalarType level = lv.GetLevel();
+      ScalarType window = lv.GetWindow();
 
-      level += static_cast<int>(( m_CurrentDisplayCoordinate[0] - m_LastDisplayCoordinate[0] )*2);
-      window += static_cast<int>(( m_CurrentDisplayCoordinate[1] - m_LastDisplayCoordinate[1] )*2);
+      level += ( m_CurrentDisplayCoordinate[0] - m_LastDisplayCoordinate[0] )*static_cast<ScalarType>(2);
+      window += ( m_CurrentDisplayCoordinate[1] - m_LastDisplayCoordinate[1] )*static_cast<ScalarType>(2);
 
       lv.SetLevelWindow( level, window );
       dynamic_cast<mitk::LevelWindowProperty*>(node->GetProperty("levelwindow"))->SetLevelWindow( lv );

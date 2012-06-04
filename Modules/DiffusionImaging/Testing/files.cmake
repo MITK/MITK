222db46c3010628c8cf5c8841251fef9088d317e
Thomas Van Bruggen <t.vanbruggen@dkfz-heidelberg.de>
2012-06-04 13:51:13 +0200

    added test to cmake

--- Modules/DiffusionImaging/Testing/files.cmake
+++ Modules/DiffusionImaging/Testing/files.cmake
@@ -2,6 +2,7 @@ SET(MODULE_CUSTOM_TESTS
   mitkFiberBundleXReaderWriterTest.cpp
   mitkTbssNrrdImageReaderTest.cpp
   mitkTbssRoiNrrdImageReaderTest.cpp
+  mitkTbssNrrdImageWriterTest.cpp
 )
 
 set(MODULE_TESTS    

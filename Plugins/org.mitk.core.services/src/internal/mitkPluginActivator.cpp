d6caa85fbc8359c93201a2c116a13c630f1a3ab2
Alfred Franz <a.franz@dkfz-heidelberg.de>
2012-06-05 16:45:47 +0200

    added command to register logging backend and activated logging file

--- Plugins/org.mitk.core.services/src/internal/mitkPluginActivator.cpp
+++ Plugins/org.mitk.core.services/src/internal/mitkPluginActivator.cpp
@@ -16,6 +16,11 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include "mitkPluginActivator.h"
 
+#include "mitkLog.h"
+
+#include <QString>
+#include <QFileInfo>
+
 #include "internal/mitkDataStorageService.h"
 
 namespace mitk
@@ -25,6 +30,14 @@ const std::string org_mitk_core_services_Activator::PLUGIN_ID = "org.mitk.core.s
 
 void org_mitk_core_services_Activator::start(ctkPluginContext* context)
 {
+  //initialize logging
+  mitk::LoggingBackend::Register(); 
+  QString filename = "mitk.log";
+  QFileInfo path = context->getDataFile(filename);
+  mitk::LoggingBackend::SetLogFile(path.absoluteFilePath().toStdString().c_str());
+  MITK_INFO << "Logfile: " << path.absoluteFilePath().toStdString().c_str();
+
+  //initialize data storage service
   DataStorageService* service = new DataStorageService();
   dataStorageService = IDataStorageService::Pointer(service);
   context->registerService<mitk::IDataStorageService>(service);
@@ -32,6 +45,9 @@ void org_mitk_core_services_Activator::start(ctkPluginContext* context)
 
 void org_mitk_core_services_Activator::stop(ctkPluginContext* /*context*/)
 {
+  //clean up logging
+  mitk::LoggingBackend::Unregister();
+  
   dataStorageService = 0;
 }
 

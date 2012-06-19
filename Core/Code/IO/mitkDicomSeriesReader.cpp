c7e333522f360df51c60ac727b2cfbfab5847e02
Daniel Maleike <d.maleike@mint-medical.de>
2012-06-19 14:52:03 +0200

    Comment on 3D+t problem solution

--- Core/Code/IO/mitkDicomSeriesReader.cpp
+++ Core/Code/IO/mitkDicomSeriesReader.cpp
@@ -886,6 +886,7 @@ DicomSeriesReader::GetSeries(const StringContainer& files, bool sortTo3DPlust, c
     // end of grouping, now post-process groups
   
     // TODO: store and compare tilting property of blocks. they must match!
+    // Solution: already guaranteed by previous sorting. ALL the images in one group already share one direction/tilt/etc.
 
     // PART IV: attempt to group blocks to 3D+t blocks if requested
     //           inspect entries of mapOf3DBlocks

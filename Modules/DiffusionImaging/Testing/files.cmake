09ce6ad213dcbf4e2ddde9f7e6387f563d3e74b3
Thomas Van Bruggen <t.vanbruggen@dkfz-heidelberg.de>
2012-06-20 15:13:09 +0200

    COMP: disabled tests untill data repository is updated

--- Modules/DiffusionImaging/Testing/files.cmake
+++ Modules/DiffusionImaging/Testing/files.cmake
@@ -4,9 +4,9 @@ SET(MODULE_CUSTOM_TESTS
   # mitkFiberBundleXTest.cpp ## deactivated, see bug 12017
   # mitkGibbsTrackingTest.cpp # deactivated until new tarball is available
 
-  mitkTbssNrrdImageReaderTest.cpp
-  mitkTbssRoiNrrdImageReaderTest.cpp
-  mitkTbssNrrdImageWriterTest.cpp
+  #mitkTbssNrrdImageReaderTest.cpp
+  #mitkTbssRoiNrrdImageReaderTest.cpp
+  #mitkTbssNrrdImageWriterTest.cpp
 )
 
 set(MODULE_TESTS  

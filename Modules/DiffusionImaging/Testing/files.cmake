af7e4ea4febc5879660d4e85335fb406e24d3dfd
Peter Neher <p.neher@dkfz-heidelberg.de>
2012-06-20 18:13:00 +0200

    updated data repository hash. added gibbs tracking test.

--- Modules/DiffusionImaging/Testing/files.cmake
+++ Modules/DiffusionImaging/Testing/files.cmake
@@ -1,16 +1,15 @@
 SET(MODULE_CUSTOM_TESTS
   mitkFiberBundleXReaderWriterTest.cpp
-
   # mitkFiberBundleXTest.cpp ## deactivated, see bug 12017
-  # mitkGibbsTrackingTest.cpp # deactivated until new tarball is available
+  mitkGibbsTrackingTest.cpp
 
   #mitkTbssNrrdImageReaderTest.cpp
   #mitkTbssRoiNrrdImageReaderTest.cpp
   #mitkTbssNrrdImageWriterTest.cpp
 )
 
-set(MODULE_TESTS  
-  mitkFactoryRegistrationTest.cpp 
+set(MODULE_TESTS
+  mitkFactoryRegistrationTest.cpp
 )
 
 

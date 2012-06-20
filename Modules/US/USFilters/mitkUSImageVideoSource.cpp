8ab18b4bca342b9474bf80aedf8ad3bcfd2badf5
Keno Maerz <k.maerz@dkfz-heidelberg.de>
2012-06-20 13:11:58 +0200

    Minor Changes to USImageVideoSource

--- Modules/US/USFilters/mitkUSImageVideoSource.cpp
+++ Modules/US/USFilters/mitkUSImageVideoSource.cpp
@@ -14,9 +14,16 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 ===================================================================*/
 
+// MITK HEADER
 #include "mitkUSImageVideoSource.h"
 #include "mitkImage.h"
+
+//OpenCV HEADER
 #include <cv.h>
+#include <highgui.h>
+
+//Other
+#include <stdio.h>  
 
 
 
@@ -52,37 +59,49 @@ void mitk::USImageVideoSource::SetVideoFileInput(std::string path)
     
 void mitk::USImageVideoSource::SetCameraInput(int deviceID)
 {
+
+
+
+  /* Old Code, this will probably not work
   m_OpenCVVideoSource = mitk::OpenCVVideoSource::New();
 
   m_OpenCVVideoSource->SetVideoCameraInput(deviceID);
 
-  //m_OpenCVVideoSource->StartCapturing();
+  m_OpenCVVideoSource->StartCapturing();
   m_OpenCVVideoSource->FetchFrame();
   
   // Let's see if we have been successful
   m_IsVideoReady = m_OpenCVVideoSource->IsCapturingEnabled();
+  */
 }
 
 
 mitk::USImage::Pointer mitk::USImageVideoSource::GetNextImage()
 {
   IplImage *m_cvCurrentVideoFrame = NULL;
+  CvCapture* capture = cvCaptureFromCAM( 50 );
+   if ( !capture ) {
+     fprintf( stderr, "ERROR: capture is NULL \n" );
+     getchar();
+     return NULL;
+   }
+   // Show the image captured from the camera in the window and repeat
+   
+   // Get one frame
+   m_cvCurrentVideoFrame  = cvQueryFrame( capture );
+     
+   
+
+/// WORKING CODE
+
+
+  /*IplImage *m_cvCurrentVideoFrame = NULL;
   int height = m_OpenCVVideoSource->GetImageHeight();
   int width = m_OpenCVVideoSource->GetImageWidth();
   m_cvCurrentVideoFrame = cvCreateImage(cvSize(width,height),8,3);
   m_OpenCVVideoSource->GetCurrentFrameAsOpenCVImage(m_cvCurrentVideoFrame);
   m_OpenCVVideoSource->FetchFrame();
-  //m_WidgetBackground1->Update(m_cvCurrentVideoFrame->imageData, m_cvCurrentVideoFrame->width, m_cvCurrentVideoFrame->height, m_cvCurrentVideoFrame->nChannels);
-  //m_Counter++;
-
-//old code
-  //m_OpenCVVideoSource->FetchFrame();
-
-  // This is a bit of a workaround: We only need to initialize an OpenCV Image. Actual
-  // Initialization happens inside the OpenCVVideoSource
-  //IplImage* iplImage = cvCreateImage(cvSize(320,240),IPL_DEPTH_8U,3); 
-  //m_OpenCVVideoSource->GetCurrentFrameAsOpenCVImage(iplImage);
-  
+  */  
   this->m_OpenCVToMitkFilter->SetOpenCVImage(m_cvCurrentVideoFrame);
   this->m_OpenCVToMitkFilter->Update();
 

1ad222acdc5ae90d8f98c048cbe3c7c0d1fcbf67
Thomas Van Bruggen <t.vanbruggen@dkfz-heidelberg.de>
2012-06-21 12:07:10 +0200

    enable tests

--- Modules/DiffusionImaging/Testing/files.cmake
+++ Modules/DiffusionImaging/Testing/files.cmake
@@ -1,15 +1,16 @@
 SET(MODULE_CUSTOM_TESTS
   mitkFiberBundleXReaderWriterTest.cpp
-  # mitkFiberBundleXTest.cpp ## deactivated, see bug 12017
-  mitkGibbsTrackingTest.cpp
 
-  #mitkTbssNrrdImageReaderTest.cpp
-  #mitkTbssRoiNrrdImageReaderTest.cpp
-  #mitkTbssNrrdImageWriterTest.cpp
+  # mitkFiberBundleXTest.cpp ## deactivated, see bug 12017
+  # mitkGibbsTrackingTest.cpp # deactivated until new tarball is available
+
+  mitkTbssNrrdImageReaderTest.cpp
+  mitkTbssRoiNrrdImageReaderTest.cpp
+  mitkTbssNrrdImageWriterTest.cpp
 )
 
-set(MODULE_TESTS
-  mitkFactoryRegistrationTest.cpp
+set(MODULE_TESTS  
+  mitkFactoryRegistrationTest.cpp 
 )
 
 

00fc3067514a9dcd0b5fc1214b57de575434fc7e
Tobias Schroeder <t.schroeder@dkfz-heidelberg.de>
2012-06-22 13:52:57 +0200

    track renamed files

---
+++ Modules/Segmentation/Algorithms/mitkDiffSliceOperationApplier.h
@@ -0,0 +1,63 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center, 
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without 
+even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+
+#ifndef mitkDiffSliceOpertationApplier_h_Included
+#define mitkDiffSliceOpertationApplier_h_Included
+
+#include "SegmentationExports.h"
+#include "mitkCommon.h"
+#include <mitkOperationActor.h>
+
+#include "DiffSliceOperation.h"
+#include <mitkExtractSliceFilter.h>
+#include <mitkVtkImageOverwrite.h>
+
+namespace mitk
+{
+  /** \brief Executes a DiffSliceOperation. 
+    \sa DiffSliceOperation
+  */
+  class Segmentation_EXPORT DiffSliceOperationApplier : public OperationActor
+  {
+
+  public:
+
+    mitkClassMacro(DiffSliceOperationApplier, OperationActor);
+    
+    //itkNewMacro(DiffSliceOperationApplier);
+
+    /** \brief Returns an instance of the class */
+    static DiffSliceOperationApplier* GetInstance();
+
+    /** \brief Executes a DiffSliceOperation. 
+      \sa DiffSliceOperation
+      Note:
+        Only DiffSliceOperation is supported.
+    */
+    virtual void ExecuteOperation(Operation* op);
+
+
+  protected:
+  
+    DiffSliceOperationApplier();
+    
+    virtual ~DiffSliceOperationApplier();
+
+    //static DiffSliceOperationApplier* s_Instance;
+
+  };
+}
+#endif

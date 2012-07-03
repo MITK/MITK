5c65378cdae8975d5c69a5410461b85dad9c22f7
Keno Maerz <k.maerz@dkfz-heidelberg.de>
2012-07-03 14:47:18 +0200

    USImageVideoSource now employs OpenCV C++ API instead of C API, can now output greyscale and cropped images.

--- Modules/US/USFilters/mitkUSImageVideoSource.cpp
+++ Modules/US/USFilters/mitkUSImageVideoSource.cpp
@@ -32,8 +32,6 @@ mitk::USImageVideoSource::USImageVideoSource()
 : itk::Object()
 {
     m_IsVideoReady = false;
-    m_IsMetadataReady = false;
-    m_IsGeometryReady = false;
     m_IsGreyscale = true;
     this->m_OpenCVToMitkFilter = mitk::OpenCVToMitkImageFilter::New();
 }
@@ -48,7 +46,6 @@ void mitk::USImageVideoSource::SetVideoFileInput(std::string path)
 {
   m_OpenCVVideoSource = mitk::OpenCVVideoSource::New();
 
-  // Example: "C:\\Users\\maerz\\Videos\\Debut\\us.avi"
   m_OpenCVVideoSource->SetVideoFileInput(path.c_str(),true,false);
   m_OpenCVVideoSource->StartCapturing();
   m_OpenCVVideoSource->FetchFrame();
@@ -61,7 +58,6 @@ void mitk::USImageVideoSource::SetVideoFileInput(std::string path)
 void mitk::USImageVideoSource::SetCameraInput(int deviceID)
 {
   m_OpenCVVideoSource = mitk::OpenCVVideoSource::New();
-
   m_OpenCVVideoSource->SetVideoCameraInput(deviceID);
 
   m_OpenCVVideoSource->StartCapturing();
@@ -75,37 +71,58 @@ void mitk::USImageVideoSource::SetColorOutput(bool isColor){
   m_IsGreyscale = !isColor;
 }
 
+void mitk::USImageVideoSource::SetRegionOfInterest(int topLeftX, int topLeftY, int bottomRightX, int bottomRightY)
+{
+  // First, let's do some basic checks to make sure rectangle is inside of actual image
+  if (topLeftX < 0) topLeftX = 0;
+  if (topLeftY < 0) topLeftY = 0;
+
+  if (bottomRightX >  m_OpenCVVideoSource->GetImageWidth()) bottomRightX = m_OpenCVVideoSource->GetImageWidth();
+  if (bottomRightX >  m_OpenCVVideoSource->GetImageHeight()) bottomRightY = m_OpenCVVideoSource->GetImageHeight();
+
+  if (topLeftX > bottomRightX) mitkThrow() << "Invalid boundaries supplied to USImageVideoSource::SetRegionOfInterest()";
+  if (topLeftY > bottomRightY) mitkThrow() << "Invalid boundaries supplied to USImageVideoSource::SetRegionOfInterest()";
+
+  m_CropRegion = cv::Rect(topLeftX, topLeftY, bottomRightX - topLeftX, bottomRightY - topLeftY);
+}
+
+void mitk::USImageVideoSource::RemoveRegionOfInterest(){
+  m_CropRegion.width = 0;
+  m_CropRegion.height = 0;
+}
 
 mitk::USImage::Pointer mitk::USImageVideoSource::GetNextImage()
 {
   // Setup Pointers
-  IplImage *rgbImage = NULL;
-  IplImage *greyImage = NULL;
+  cv::Mat image;
+  cv::Mat buffer;
 
-  //Get Dimensions and init rgb
+  //Get dimensions and init rgb
+  int width  = m_OpenCVVideoSource->GetImageWidth();
   int height = m_OpenCVVideoSource->GetImageHeight();
-  int width = m_OpenCVVideoSource->GetImageWidth();
-  rgbImage = cvCreateImage(cvSize(width,height),8,3);
 
   // Get Frame from Source
-  m_OpenCVVideoSource->GetCurrentFrameAsOpenCVImage(rgbImage);
+  image = m_OpenCVVideoSource->GetImage();
   m_OpenCVVideoSource->FetchFrame();
 
-  // If this is a greyscale image, convert it and put into the filter.
-  if (m_IsGreyscale){
-    greyImage = cvCreateImage(cvSize(width,height),8,1);
-    cvCvtColor( rgbImage, greyImage, CV_RGB2GRAY );
-    this->m_OpenCVToMitkFilter->SetOpenCVImage(greyImage);
-  } else {
-    this->m_OpenCVToMitkFilter->SetOpenCVImage(rgbImage);
+  // if Region of interest is set, crop image
+  if (m_CropRegion.width > 0){
+    buffer = image(m_CropRegion);
+    image = buffer;
   }
+  // If this is a greyscale image, convert it
+  if (m_IsGreyscale)
+  {
+    cv::cvtColor(image, buffer, CV_RGB2GRAY, 1);
+    image = buffer;
+  }
+  IplImage ipl_img = image;
+  this->m_OpenCVToMitkFilter->SetOpenCVImage(&ipl_img);
+
   this->m_OpenCVToMitkFilter->Update();
 
-  // OpenCVToMitkImageFilter returns a standard mit::image. We then transform it into an USImage
+  // OpenCVToMitkImageFilter returns a standard mitk::image. We then transform it into an USImage
   mitk::USImage::Pointer result = mitk::USImage::New(this->m_OpenCVToMitkFilter->GetOutput(0));
-  
-  cvReleaseImage (&rgbImage);
-  cvReleaseImage (&greyImage);
 
   return result;
 }

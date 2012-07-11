a13e0abe3051e558d3e5cdd01116dc420d2e1eda
MattClarkson <m.clarkson@ucl.ac.uk>
2012-07-11 08:34:42 +0100

    CLI: New Qt class CommandLineModulesViewControls

---
+++ Plugins/org.mitk.gui.qt.cli/src/internal/CommandLineModulesViewControls.h
@@ -0,0 +1,47 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center,
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+
+#ifndef CommandLineModulesViewControls_h
+#define CommandLineModulesViewControls_h
+
+#include "ui_CommandLineModulesViewControls.h"
+
+/**
+ * \class CommandLineModulesViewControls
+ * \brief Contains various utilities to remove the responsibility from CommandLineModulesView.
+ */
+class CommandLineModulesViewControls : public QWidget, public Ui_CommandLineModulesViewControls
+{
+  // this is needed for all Qt objects that should have a MOC object (everything that derives from QObject)
+  Q_OBJECT
+
+public:
+
+  CommandLineModulesViewControls(QWidget *parent = 0);
+  virtual ~CommandLineModulesViewControls();
+
+signals:
+
+protected slots:
+
+protected:
+
+private:
+
+};
+
+#endif // CommandLineModulesViewControls_h
+

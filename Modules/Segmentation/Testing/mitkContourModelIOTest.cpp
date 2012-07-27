f7ebdc5fc00010d3bbc5b654a3756f25bf90879a
Tobias Schroeder <t.schroeder@dkfz-heidelberg.de>
2012-07-27 15:26:08 +0200

    redesigned test

--- Modules/Segmentation/Testing/mitkContourModelIOTest.cpp
+++ Modules/Segmentation/Testing/mitkContourModelIOTest.cpp
@@ -26,6 +26,49 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 
 
+static void TestContourModel(mitk::ContourModel* contour, std::string fileName)
+{
+  mitk::ContourModelWriter::Pointer writer = mitk::ContourModelWriter::New();
+
+  writer->SetInput(contour);
+  std::string filename = std::string( MITK_TEST_OUTPUT_DIR ) + fileName;
+
+  writer->SetFileName(filename);
+  writer->Update();
+
+  MITK_TEST_CONDITION(writer->GetSuccess(),"write file");
+
+  mitk::ContourModelReader::Pointer reader = mitk::ContourModelReader::New();
+
+  reader->SetFileName(filename);
+
+  reader->Update();
+
+  MITK_TEST_CONDITION(reader->GetSuccess(), "read file");
+
+  mitk::ContourModel::Pointer contour2 = reader->GetOutput();
+
+  MITK_TEST_CONDITION_REQUIRED(contour2.IsNotNull(),"contour is not null");
+
+  MITK_TEST_CONDITION_REQUIRED(contour->GetNumberOfVertices() == contour2->GetNumberOfVertices(),"contours have the same number of vertices");
+
+  mitk::ContourModel::VertexIterator it = contour2->IteratorBegin();
+  mitk::ContourModel::VertexIterator end = contour2->IteratorEnd();
+
+  mitk::ContourModel::VertexIterator it2 = contour2->IteratorBegin();
+
+  bool areEqual = true;
+
+  while(it != end)
+  {
+    areEqual &= ((*it)->Coordinates == (*it2)->Coordinates);
+    it++; it2++;
+  }
+
+  MITK_TEST_CONDITION(areEqual, "contours are equal");
+}
+
+
 static void TestContourModelIO_OneTimeStep()
 {
   mitk::ContourModel::Pointer contour = mitk::ContourModel::New();
@@ -35,12 +78,10 @@ static void TestContourModelIO_OneTimeStep()
 
   contour->AddVertex(p);
 
-
   mitk::Point3D p2;
   p2[0] = p2[1] = p2[2] = 1;
 
   contour->AddVertex(p2);
-
 
   mitk::Point3D p3;
   p3[0] = -2;
@@ -49,7 +90,6 @@ static void TestContourModelIO_OneTimeStep()
 
   contour->AddVertex(p3);
 
-
   mitk::Point3D p4;
   p4[0] = -3;
   p4[1] = 6;
@@ -57,44 +97,7 @@ static void TestContourModelIO_OneTimeStep()
 
   contour->AddVertex(p4);
 
-  mitk::ContourModelWriter::Pointer writer = mitk::ContourModelWriter::New();
-
-  writer->SetInput(contour);
-  std::string filename = std::string( MITK_TEST_OUTPUT_DIR ) + "/contour.cnt";
-
-  writer->SetFileName(filename);
-  writer->Update();
-
-  MITK_TEST_CONDITION(writer->GetSuccess(),"write file");
-
-  mitk::ContourModelReader::Pointer reader = mitk::ContourModelReader::New();
-
-  reader->SetFileName(filename);
-
-  reader->Update();
-
-  MITK_TEST_CONDITION(reader->GetSuccess(), "read file");
-
-  mitk::ContourModel::Pointer contour2 = reader->GetOutput();
-
-  MITK_TEST_CONDITION_REQUIRED(contour2.IsNotNull(),"contour is not null");
-
-  MITK_TEST_CONDITION_REQUIRED(contour->GetNumberOfVertices() == contour2->GetNumberOfVertices(),"contours have the same number of vertices");
-
-  mitk::ContourModel::VertexIterator it = contour2->IteratorBegin();
-  mitk::ContourModel::VertexIterator end = contour2->IteratorEnd();
-
-  mitk::ContourModel::VertexIterator it2 = contour2->IteratorBegin();
-
-  bool areEqual = true;
-
-  while(it != end)
-  {
-    areEqual &= ((*it)->Coordinates == (*it2)->Coordinates);
-    it++; it2++;
-  }
-
-  MITK_TEST_CONDITION(areEqual, "contours are equal");
+  TestContourModel(contour.GetPointer(), "/contour.cnt");
 }
 
 
@@ -103,47 +106,8 @@ static void TestContourModelIO_EmptyContourModel()
 {
   mitk::ContourModel::Pointer contour = mitk::ContourModel::New();
 
-
-  mitk::ContourModelWriter::Pointer writer = mitk::ContourModelWriter::New();
-
-  writer->SetInput(contour);
-  std::string filename = std::string( MITK_TEST_OUTPUT_DIR ) + "/contourEmpty.cnt";
-
-  writer->SetFileName(filename);
-  writer->Update();
-
-  MITK_TEST_CONDITION(writer->GetSuccess(),"write file");
-
-  mitk::ContourModelReader::Pointer reader = mitk::ContourModelReader::New();
-
-  reader->SetFileName(filename);
-
-  reader->Update();
-
-  MITK_TEST_CONDITION(reader->GetSuccess(), "read file");
-
-  mitk::ContourModel::Pointer contour2 = reader->GetOutput();
-
-  MITK_TEST_CONDITION_REQUIRED(contour2.IsNotNull(),"contour is not null");
-
-  MITK_TEST_CONDITION_REQUIRED(contour->GetNumberOfVertices() == contour2->GetNumberOfVertices(),"contours have the same number of vertices");
-
-  mitk::ContourModel::VertexIterator it = contour2->IteratorBegin();
-  mitk::ContourModel::VertexIterator end = contour2->IteratorEnd();
-
-  mitk::ContourModel::VertexIterator it2 = contour2->IteratorBegin();
-
-  bool areEqual = true;
-
-  while(it != end)
-  {
-    areEqual &= ((*it)->Coordinates == (*it2)->Coordinates);
-    it++; it2++;
-  }
-
-  MITK_TEST_CONDITION(areEqual, "contours are equal");
+  TestContourModel(contour.GetPointer(), "/contourEmpty.cnt");
 }
-
 
 
 

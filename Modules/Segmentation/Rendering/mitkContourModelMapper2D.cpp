139304387c23e1242087599dcad7d24d307e9fef
Tobias Schroeder <t.schroeder@dkfz-heidelberg.de>
2012-07-27 11:40:42 +0200

    initial mapper implementation

--- Modules/Segmentation/Rendering/mitkContourModelMapper2D.cpp
+++ Modules/Segmentation/Rendering/mitkContourModelMapper2D.cpp
@@ -13,3 +13,83 @@ A PARTICULAR PURPOSE.
 See LICENSE.txt or http://www.mitk.org for details.
 
 ===================================================================*/
+#include <mitkContourModelMapper2D.h>
+
+mitk::ContourModelMapper2D::ContourModelMapper2D()
+{
+}
+
+mitk::ContourModelMapper2D::~ContourModelMapper2D()
+{
+}
+
+const mitk::ContourModel* mitk::ContourModelMapper2D::GetInput( void )
+{
+  return static_cast< const mitk::ContourModel * >( this->GetData() );
+}
+
+vtkProp* mitk::ContourModelMapper2D::GetVtkProp(mitk::BaseRenderer* renderer)
+{  
+  //return the actor corresponding to the renderer
+  return m_LSH.GetLocalStorage(renderer)->m_Actor;
+}
+
+void mitk::ContourModelMapper2D::MitkRenderOverlay(BaseRenderer* renderer)
+{
+  if ( this->IsVisible(renderer)==false )
+    return;
+  if ( this->GetVtkProp(renderer)->GetVisibility() )
+  {
+    this->GetVtkProp(renderer)->RenderOverlay(renderer->GetVtkRenderer());
+  }
+}
+
+void mitk::ContourModelMapper2D::MitkRenderOpaqueGeometry(BaseRenderer* renderer)
+{
+  if ( this->IsVisible( renderer )==false )
+    return;
+  if ( this->GetVtkProp(renderer)->GetVisibility() )
+  {
+    this->GetVtkProp(renderer)->RenderOpaqueGeometry( renderer->GetVtkRenderer() );
+  }
+}
+
+void mitk::ContourModelMapper2D::MitkRenderTranslucentGeometry(BaseRenderer* renderer)
+{
+  if ( this->IsVisible(renderer)==false )
+    return;
+  if ( this->GetVtkProp(renderer)->GetVisibility() )
+  {
+    this->GetVtkProp(renderer)->RenderTranslucentPolygonalGeometry(renderer->GetVtkRenderer());
+  }
+}
+
+void mitk::ContourModelMapper2D::MitkRenderVolumetricGeometry(BaseRenderer* renderer)
+{
+  if(IsVisible(renderer)==false)
+    return;
+  if ( GetVtkProp(renderer)->GetVisibility() )
+  {
+    this->GetVtkProp(renderer)->RenderVolumetricGeometry(renderer->GetVtkRenderer());
+  }
+}
+
+void mitk::ContourModelMapper2D::GenerateDataForRenderer( mitk::BaseRenderer *renderer )
+{
+}
+
+mitk::ContourModelMapper2D::LocalStorage* mitk::ContourModelMapper2D::GetLocalStorage(mitk::BaseRenderer* renderer)
+{
+  return m_LSH.GetLocalStorage(renderer);
+}
+
+mitk::ContourModelMapper2D::LocalStorage::LocalStorage()
+{
+
+}
+
+
+void mitk::ContourModelMapper2D::SetDefaultProperties(mitk::DataNode* node, mitk::BaseRenderer* renderer, bool overwrite)
+{
+
+}
--- Modules/Segmentation/Rendering/mitkContourModelMapper2D.h
+++ Modules/Segmentation/Rendering/mitkContourModelMapper2D.h
@@ -13,3 +13,82 @@ A PARTICULAR PURPOSE.
 See LICENSE.txt or http://www.mitk.org for details.
 
 ===================================================================*/
+
+#ifndef _MITK_CONTOURMODEL_MAPPER_H_
+#define _MITK_CONTOURMODEL_MAPPER_H_
+
+
+#include "mitkCommon.h"
+#include "SegmentationExports.h"
+
+#include "mitkBaseRenderer.h"
+#include "mitkVtkMapper2D.h"
+
+#include "mitkContourModel.h"
+//#include "mitkContourModelToVtkPolyDataFilter.h"
+
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+
+
+namespace mitk {
+
+  class Segmentation_EXPORT ContourModelMapper2D : public VtkMapper2D
+  {
+      public:
+    /** Standard class typedefs. */
+    mitkClassMacro( ContourModelMapper2D,VtkMapper2D );
+
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+
+    const mitk::ContourModel* GetInput(void);
+
+        //### methods of MITK-VTK rendering pipeline
+    virtual vtkProp* GetVtkProp(mitk::BaseRenderer* renderer);
+
+    virtual void MitkRenderOverlay(BaseRenderer* renderer);
+    virtual void MitkRenderOpaqueGeometry(BaseRenderer* renderer);
+    virtual void MitkRenderTranslucentGeometry(BaseRenderer* renderer);
+    virtual void MitkRenderVolumetricGeometry(BaseRenderer* renderer);
+    //### end of methods of MITK-VTK rendering pipeline
+
+    class Segmentation_EXPORT LocalStorage : public mitk::Mapper::BaseLocalStorage
+    {
+    public:
+
+      /** \brief Actor of a 2D render window. */
+      vtkSmartPointer<vtkActor> m_Actor;
+      /** \brief Mapper of a 2D render window. */
+      vtkSmartPointer<vtkPolyDataMapper> m_Mapper;
+
+      //mitk::ContourModelToVtkPolyDataFilter::Pointer m_contourToPolyData;
+
+      /** \brief Default constructor of the local storage. */
+      LocalStorage();
+      /** \brief Default deconstructor of the local storage. */
+      ~LocalStorage()
+      {
+      }
+    };
+
+
+    /** \brief The LocalStorageHandler holds all (three) LocalStorages for the three 2D render windows. */
+    mitk::Mapper::LocalStorageHandler<LocalStorage> m_LSH;
+
+    /** \brief Get the LocalStorage corresponding to the current renderer. */
+    LocalStorage* GetLocalStorage(mitk::BaseRenderer* renderer);
+
+        /** \brief Set the default properties for general image rendering. */
+    static void SetDefaultProperties(mitk::DataNode* node, mitk::BaseRenderer* renderer = NULL, bool overwrite = false);
+
+
+  protected:
+    ContourModelMapper2D();
+    virtual ~ContourModelMapper2D();
+
+    void GenerateDataForRenderer( mitk::BaseRenderer *renderer );
+  };
+}
+#endif

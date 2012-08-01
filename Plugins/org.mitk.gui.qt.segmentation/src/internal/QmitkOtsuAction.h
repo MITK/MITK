f58c07cc88dae6b3c79beeeb22370a2d2ea8e771
Tobias Norajitra <t.norajitra@dkfz-heidelberg.de>
2012-08-01 16:52:54 +0200

    wip

---
+++ Plugins/org.mitk.gui.qt.segmentation/src/internal/QmitkOtsuAction.h
@@ -0,0 +1,68 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center, 
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without 
+even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+#ifndef QMITKOTSUACTION_H
+#define QMITKOTSUACTION_H
+
+#include <org_mitk_gui_qt_segmentation_Export.h>
+
+// Parent classes
+#include <QObject>
+#include <mitkIContextMenuAction.h>
+
+// Data members
+#include <mitkDataStorage.h>
+#include <QSpinBox>
+#include <QPushButton>
+
+// Mitk classes
+#include <mitkDataNode.h>
+
+class QDialog;
+class QmitkStdMultiWidget;
+
+class MITK_QT_SEGMENTATION QmitkOtsuAction : public QObject, public mitk::IContextMenuAction
+{
+  Q_OBJECT
+  Q_INTERFACES(mitk::IContextMenuAction)
+
+public:
+  QmitkOtsuAction();
+  ~QmitkOtsuAction();
+
+  // IContextMenuAction
+  void Run(const QList<mitk::DataNode::Pointer> &selectedNodes);
+  void SetDataStorage(mitk::DataStorage *dataStorage);
+  void SetFunctionality(berry::QtViewPart *functionality);
+
+private slots:
+  void OtsuSegmentationDone(int);
+
+private:
+  QmitkOtsuAction(const QmitkOtsuAction &);
+  QmitkOtsuAction & operator=(const QmitkOtsuAction &);
+
+  void PerformOtsuSegmentation();
+
+  mitk::DataStorage::Pointer m_DataStorage;
+  QDialog *m_OtsuSegmentationDialog;
+
+  QSpinBox* m_OtsuSpinBox;
+  QPushButton* m_OtsuPushButton; 
+
+  QList<mitk::DataNode::Pointer>& m_selectedNodes;
+};
+
+#endif

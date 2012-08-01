8e7066835ba98af850ef657962de7f5548d06bf0
Alfred Franz <a.franz@dkfz-heidelberg.de>
2012-08-01 12:03:42 +0200

    adapted reader and writer to new members of navigation tool

--- Modules/IGT/IGTToolManagement/mitkNavigationToolReader.h
+++ Modules/IGT/IGTToolManagement/mitkNavigationToolReader.h
@@ -63,6 +63,8 @@ namespace mitk {
 
     //################### protected help methods ########################
     std::string GetFileWithoutPath(std::string FileWithPath);
+    mitk::PointSet::Pointer ConvertStringToPointSet(std::string string);
+    void split(std::string& text, std::string& separators, std::vector<std::string>& words);
 
   };
 } // namespace mitk

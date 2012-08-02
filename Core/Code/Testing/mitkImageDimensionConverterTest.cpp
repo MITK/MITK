d89be116357e008ebc95b5113be753f579e16b70
Bastian Graser <b.graser@dkfz-heidelberg.de>
2012-08-02 10:29:45 +0200

    Finished Test for 2D 3D Converter. Still need to check manually. Does it freeze when i click anywhere etc

--- Core/Code/Testing/mitkImageDimensionConverterTest.cpp
+++ Core/Code/Testing/mitkImageDimensionConverterTest.cpp
@@ -26,6 +26,8 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <mitkInteractionConst.h>
 #include <mitkImageWriter.h>
 #include <mitkPlaneOperation.h>
+#include "mitkTestingConfig.h"
+#include "mitkItkImageFileReader.h"
 
 // itk includes
 #include <itkImage.h>
@@ -40,6 +42,13 @@ See LICENSE.txt or http://www.mitk.org for details.
 int mitkImageDimensionConverterTest(int argc, char* argv[])
 {
   MITK_TEST_BEGIN(mitkImageDimensionConverterTest);
+
+  // Define an epsilon which is the allowed error
+  float eps = 0.00001;
+
+  // Define helper variables
+  float error = 0;
+
 
   ///////////////////////////////////////
   // Create 2D Image with a 3D rotation from scratch.
@@ -79,12 +88,11 @@ int mitkImageDimensionConverterTest(int argc, char* argv[])
   mitk::RotationOperation op(mitk::OpROTATE, p, v, 35);
   mitkImage2D->GetGeometry()->ExecuteOperation(&op);
 
-  
-
-
-  //mitk::PlaneOperation op( mitk::OpORIENT, p, v );
-  //mitkImage2D->GetGeometry()->ExecuteOperation( &op );
-
+  mitk::Vector3D Original_col0, Original_col1, Original_col2;
+  Original_col0.Set_vnl_vector(mitkImage2D->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(0));
+  Original_col1.Set_vnl_vector(mitkImage2D->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(1));
+  Original_col2.Set_vnl_vector(mitkImage2D->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(2));
+  MITK_INFO << "Rotated Matrix: " << Original_col0 << " " << Original_col1 << " " << Original_col2;
 
   ///////////////////////////////////////
   // mitkImage2D is now a 2D image with 3D Geometry information.
@@ -102,18 +110,21 @@ int mitkImageDimensionConverterTest(int argc, char* argv[])
   // check if geometry is still same  
   mitk::Vector3D Original_Spacing = mitkImage2D->GetGeometry()->GetSpacing();
   mitk::Vector3D Converted_Spacing = mitkImage3D->GetGeometry()->GetSpacing();
-  MITK_TEST_CONDITION_REQUIRED( Original_Spacing == Converted_Spacing    , "Compare Geometry: Spacing");
+  
+  error = abs(Converted_Spacing[0] - Original_Spacing[0]) + 
+     abs(Converted_Spacing[1] - Original_Spacing[1]) +
+     abs(Converted_Spacing[2] - Original_Spacing[2]) ;
+
+  MITK_TEST_CONDITION_REQUIRED( error < eps , "Compare Geometry: Spacing");
 
   mitk::Point3D Original_Origin = mitkImage2D->GetGeometry()->GetOrigin();
   mitk::Point3D Converted_Origin = mitkImage3D->GetGeometry()->GetOrigin();
+
+  error = abs(Converted_Origin[0] - Original_Origin[0]) + 
+     abs(Converted_Origin[1] - Original_Origin[1]) +
+     abs(Converted_Origin[2] - Original_Origin[2]) ;
+
   MITK_TEST_CONDITION_REQUIRED( Original_Origin == Converted_Origin    , "Compare Geometry: Origin");
-
-  mitk::Vector3D Original_col0, Original_col1, Original_col2;
-  Original_col0.Set_vnl_vector(mitkImage2D->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(0));
-  Original_col1.Set_vnl_vector(mitkImage2D->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(1));
-  Original_col2.Set_vnl_vector(mitkImage2D->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(2));
-  MITK_INFO << "Rotated Matrix: " << Original_col0 << " " << Original_col1 << " " << Original_col2;
-
 
   mitk::Vector3D Converted_col0, Converted_col1, Converted_col2;
   Converted_col0.Set_vnl_vector(mitkImage3D->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(0));
@@ -122,7 +133,7 @@ int mitkImageDimensionConverterTest(int argc, char* argv[])
 
   bool matrixIsEqual = true;
 
-  float eps = 0.001;
+
      if (
         (abs(Original_col0[0] - Converted_col0[0]) > eps) ||
         (abs(Original_col0[1] - Converted_col0[1]) > eps) ||
@@ -142,12 +153,75 @@ int mitkImageDimensionConverterTest(int argc, char* argv[])
 
      MITK_TEST_CONDITION_REQUIRED( matrixIsEqual , "Compare Geometry: Matrix");
     
-     // Temporary Tets: Store the rotated 2D image as 3D NRRD image on hdd
+
+     ///////////////////////////////////////
+     // So far it seems good! now try to save and load the file
+
+     std::stringstream sstream;
+     sstream << MITK_TEST_OUTPUT_DIR << "" << "/rotatedImage";
+
      mitk::ImageWriter::Pointer imageWriter = mitk::ImageWriter::New();
      imageWriter->SetInput(mitkImage3D);
-     imageWriter->SetFileName("E://rot");
+     imageWriter->SetFileName(sstream.str().c_str());
      imageWriter->SetExtension(".nrrd");
      imageWriter->Write();
+
+     sstream << ".nrrd";
+
+     mitk::ItkImageFileReader::Pointer reader = mitk::ItkImageFileReader::New();
+     reader->SetFileName(sstream.str().c_str());
+     reader->Update();
+     mitk::Image::Pointer imageLoaded = reader->GetOutput();
+     
+     // check if image can be loaded
+     MITK_TEST_CONDITION_REQUIRED( imageLoaded.IsNotNull() , "Loading saved Image");
+
+
+     // check if loaded image is still what it should be:
+     MITK_TEST_CONDITION_REQUIRED( imageLoaded->GetDimension() == 3   , "Loaded Image is Dimension 3");
+
+     // check if geometry is still same  
+     mitk::Vector3D Loaded_Spacing = imageLoaded->GetGeometry()->GetSpacing();
+     error = abs(Loaded_Spacing[0] - Original_Spacing[0]) + 
+        abs(Loaded_Spacing[1] - Original_Spacing[1]) +
+        abs(Loaded_Spacing[2] - Original_Spacing[2]) ;
+
+     MITK_TEST_CONDITION_REQUIRED( error < eps    , "Compare Geometry: Spacing");
+
+     mitk::Point3D Loaded_Origin = imageLoaded->GetGeometry()->GetOrigin();
+     error = abs(Loaded_Origin[0] - Original_Origin[0]) + 
+        abs(Loaded_Origin[1] - Original_Origin[1]) +
+        abs(Loaded_Origin[2] - Original_Origin[2]) ;
+     MITK_TEST_CONDITION_REQUIRED( error < eps     , "Compare Geometry: Origin");
+
+     mitk::Vector3D Loaded_col0, Loaded_col1, Loaded_col2;
+     Loaded_col0.Set_vnl_vector(imageLoaded->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(0));
+     Loaded_col1.Set_vnl_vector(imageLoaded->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(1));
+     Loaded_col2.Set_vnl_vector(imageLoaded->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().get_column(2));
+
+     matrixIsEqual = true;
+
+     if (
+        (abs(Original_col0[0] - Loaded_col0[0]) > eps) ||
+        (abs(Original_col0[1] - Loaded_col0[1]) > eps) ||
+        (abs(Original_col0[2] - Loaded_col0[2]) > eps) ||
+        (abs(Original_col1[0] - Loaded_col1[0]) > eps) ||
+        (abs(Original_col1[1] - Loaded_col1[1]) > eps) ||
+        (abs(Original_col1[2] - Loaded_col1[2]) > eps) ||
+        (abs(Original_col2[0] - Loaded_col2[0]) > eps) ||
+        (abs(Original_col2[1] - Loaded_col2[1]) > eps) ||
+        (abs(Original_col2[2] - Loaded_col2[2]) > eps) )
+     {
+        MITK_INFO << "Oh No! Original Image Matrix and Converted Image Matrix are different!";
+        MITK_INFO << "original Image:" << Original_col0 << " " << Original_col1 << " " << Original_col2;
+        MITK_INFO << "converted Image:" << Loaded_col0 << " " << Loaded_col1 << " " << Loaded_col2;
+        matrixIsEqual = false;
+     }
+
+     MITK_TEST_CONDITION_REQUIRED( matrixIsEqual , "Compare Geometry: Matrix");
+
+
+
 
    MITK_TEST_END();
 }

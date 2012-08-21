cc075dc969511966fa1c1f413f3828da63cfee85
Daniel Maleike <d.maleike@mint-medical.de>
2012-08-21 14:25:28 +0200

    Update link to MITK-Data
    Conflicts:
    
    	CMakeExternals/MITKData.cmake

--- CMakeExternals/MITKData.cmake
+++ CMakeExternals/MITKData.cmake
@@ -13,7 +13,7 @@ set(MITK-Data_DEPENDS ${proj})
 
 if(BUILD_TESTING)
 
-  set(revision_tag ae9267c8)
+  set(revision_tag 255cba2)
   #if(${proj}_REVISION_TAG)
   #  set(revision_tag ${${proj}_REVISION_TAG})
   #endif()

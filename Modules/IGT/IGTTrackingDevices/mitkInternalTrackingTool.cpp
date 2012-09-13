d20ed04ca0daf7ca2f4e8355f60d97dfef5cf9c4
Keno Maerz <k.maerz@dkfz-heidelberg.de>
2012-09-13 13:43:47 +0200

    Quick fix that undoes Code changes leading to the bug.

--- Modules/IGT/IGTTrackingDevices/mitkInternalTrackingTool.cpp
+++ Modules/IGT/IGTTrackingDevices/mitkInternalTrackingTool.cpp
@@ -34,6 +34,13 @@ m_ToolTipSet(false)
   m_Orientation[1] = 0.0f;
   m_Orientation[2] = 0.0f;
   m_Orientation[3] = 0.0f;
+  m_ToolTip[0] = 0.0f;
+  m_ToolTip[1] = 0.0f;
+  m_ToolTip[2] = 0.0f;
+  m_ToolTipRotation[0] = 0.0f;
+  m_ToolTipRotation[1] = 0.0f;
+  m_ToolTipRotation[2] = 0.0f;
+  m_ToolTipRotation[3] = 1.0f;
 }
 
 mitk::InternalTrackingTool::~InternalTrackingTool()
@@ -70,20 +77,22 @@ void mitk::InternalTrackingTool::SetToolName( const std::string _arg )
 void mitk::InternalTrackingTool::GetPosition(mitk::Point3D& position) const
 {
   MutexLockHolder lock(*m_MyMutex); // lock and unlock the mutex
-  if (m_ToolTipSet)
-    {
-    //compute position of tooltip
-    vnl_vector<float> pos_vnl = (m_ToolTipRotation.rotate(m_Position.Get_vnl_vector()))+ m_ToolTip.Get_vnl_vector();
-    position[0] = pos_vnl[0];
-    position[1] = pos_vnl[1];
-    position[2] = pos_vnl[2];
-    }
-  else
-    {
+  
+  //Commented out: Quickfix of bug 13083 for release
+  //if (m_ToolTipSet) 
+  //  {
+  //  //compute position of tooltip
+  //  vnl_vector<float> pos_vnl = (m_ToolTipRotation.rotate(m_Position.Get_vnl_vector()))+ m_ToolTip.Get_vnl_vector();
+  //  position[0] = pos_vnl[0];
+  //  position[1] = pos_vnl[1];
+  //  position[2] = pos_vnl[2];
+  //  }
+  //else
+  //  {
     position[0] = m_Position[0];
     position[1] = m_Position[1];
     position[2] = m_Position[2];
-    }
+    //}
   this->Modified();
 }
 
@@ -113,22 +122,24 @@ void mitk::InternalTrackingTool::GetOrientation(mitk::Quaternion& orientation) c
 
 void mitk::InternalTrackingTool::SetToolTip(mitk::Point3D toolTipPosition, mitk::Quaternion orientation)
 {
-if( (toolTipPosition[0] == 0) &&
-    (toolTipPosition[1] == 0) &&
-    (toolTipPosition[2] == 0) &&
-    (orientation.x() == 0) &&
-    (orientation.y() == 0) &&
-    (orientation.z() == 0) &&
-    (orientation.r() == 1))
-    {
-    m_ToolTipSet = false;
-    }
-else 
-    {
-    m_ToolTipSet = true;
-    }
-m_ToolTip = toolTipPosition;
-m_ToolTipRotation = orientation;
+MITK_WARN << "Tool Tip feature is commented out because of bug 13083";
+//Commented out: Quickfix of bug 13083 for release
+//if( (toolTipPosition[0] == 0) &&
+//    (toolTipPosition[1] == 0) &&
+//    (toolTipPosition[2] == 0) &&
+//    (orientation.x() == 0) &&
+//    (orientation.y() == 0) &&
+//    (orientation.z() == 0) &&
+//    (orientation.r() == 1))
+//    {
+//    m_ToolTipSet = false;
+//    }
+//else 
+//    {
+//    m_ToolTipSet = true;
+//    }
+//m_ToolTip = toolTipPosition;
+//m_ToolTipRotation = orientation;
 }
 
 void mitk::InternalTrackingTool::SetOrientation(mitk::Quaternion orientation)

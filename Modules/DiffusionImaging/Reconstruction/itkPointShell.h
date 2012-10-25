da474902b1e0a6119b567bc7f4e1081c60994f4f
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2012-10-26 00:36:57 +0200

    add class ThreeLayerPointShell (StaggeredDistribution)

--- Modules/DiffusionImaging/Reconstruction/itkPointShell.h
+++ Modules/DiffusionImaging/Reconstruction/itkPointShell.h
@@ -31,6 +31,15 @@ namespace itk
     static TMatrixType *DistributePointShell();
   };
 
+  template<int NpointsShell1, int NpointsShell2, int NpointsShell3, class TMatrixTypeshell1, class TMatrixTypeshell2, class TMatrixTypeshell3>
+  class ThreeLayerPointShell
+  {
+  public:
+    static TMatrixTypeshell1 *DistributePointShell1();
+    static TMatrixTypeshell2 *DistributePointShell2();
+    static TMatrixTypeshell3 *DistributePointShell3();
+  };
+
 }
 
 #include "itkPointShell.txx"

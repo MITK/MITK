addaf51dd347d4c42624110fbcceb44984e778a6
Bastian Graser <b.graser@dkfz-heidelberg.de>
2012-11-07 16:11:57 +0100

    Adapted commentary and wrote test for both ReOrientSlices Methods.

--- Core/Code/Controllers/mitkSliceNavigationController.h
+++ Core/Code/Controllers/mitkSliceNavigationController.h
@@ -432,8 +432,10 @@ class MITK_CORE_EXPORT SliceNavigationController : public BaseController
      * BaseRenderer has been specified*/
     BaseRenderer *GetRenderer() const;
 
-    /** \brief Re-orients the slice stack to include the plane specified by
-     * the given point an normal vector. 
+    /** \brief Re-orients the slice stack. All slices will be oriented to the given normal vector.
+         The given point (world coordinates) defines the selected slice. 
+         Careful: The resulting axis vectors are not clearly defined this way. If you want to define them clearly, use 
+         ReorientSlices (const mitk::Point3D &point, const mitk::Vector3D &axisVec0, const mitk::Vector3D &axisVec1).         
      */
     void ReorientSlices( 
       const mitk::Point3D &point, const mitk::Vector3D &normal );
--- Core/Code/DataManagement/mitkSlicedGeometry3D.cpp
+++ Core/Code/DataManagement/mitkSlicedGeometry3D.cpp
@@ -812,7 +812,7 @@ mitk::SlicedGeometry3D::ExecuteOperation(Operation* operation)
       //
       // 1st Step: Reorient Normal Vector of first plane
       //
-      Point3D center = m_ReferenceGeometry->GetCenter();
+      Point3D center = planeOp->GetPoint(); //m_ReferenceGeometry->GetCenter();
       mitk::Vector3D currentNormal = planeGeometry->GetNormal();
       mitk::Vector3D newNormal;
       if (planeOp->AreAxisDefined())

86de2952854755406d8cb0588ed654cc6476cc79
Tobias Schroeder <t.schroeder@dkfz-heidelberg.de>
2012-11-07 09:14:35 +0100

    template typename added

--- Modules/Segmentation/DataManagement/mitkContourElement.cpp
+++ Modules/Segmentation/DataManagement/mitkContourElement.cpp
@@ -90,7 +90,7 @@ mitk::ContourElement::VertexType* mitk::ContourElement::GetVertexAt(const mitk::
 
   if(eps > 0)
   {
-    if(true ) //currently no method with better performance is available this->m_Vertices->size() < 1000)
+    if(true ) //currently no method with better performance is available
     {
       return BruteForceGetVertexAt(point, eps);
     }

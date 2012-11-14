7872f62dce350c07abf64670b733a7c7f209605d
Jan Hering <j.hering@dkfz-heidelberg.de>
2012-11-14 18:41:30 +0100

    Added validity check to GetCommandQueue method

--- Modules/OpenCL/mitkOclResourceServiceImpl_Private.cpp
+++ Modules/OpenCL/mitkOclResourceServiceImpl_Private.cpp
@@ -57,6 +57,14 @@ cl_context OclResourceServiceImpl::GetContext() const
 
 cl_command_queue OclResourceServiceImpl::GetCommandQueue() const
 {
+  // check if queue valid
+  cl_int clErr = clGetCommandQueueInfo( m_CommandQueue, CL_QUEUE_CONTEXT, NULL, NULL, NULL );
+  if( clErr != CL_SUCCESS )
+  {
+    MITK_WARN << "Have no valid command queue. Query returned : " << GetOclErrorString( clErr );
+    return NULL;
+  }
+
   return this->m_CommandQueue;
 }
 

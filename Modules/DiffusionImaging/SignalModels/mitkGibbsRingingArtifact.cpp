ad0ce8da9c7d9466b607c1011dde9c8d0673f9ff
Peter Neher <p.neher@dkfz-heidelberg.de>
2012-11-20 10:22:34 +0100

    using circular k-space zero padding for gibbs ringing

--- Modules/DiffusionImaging/SignalModels/mitkGibbsRingingArtifact.cpp
+++ Modules/DiffusionImaging/SignalModels/mitkGibbsRingingArtifact.cpp
@@ -14,6 +14,8 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 ===================================================================*/
 
+#include <math.h>
+
 template< class ScalarType >
 GibbsRingingArtifact< ScalarType >::GibbsRingingArtifact()
     : m_KspaceCropping(0.1)
@@ -32,19 +34,34 @@ void GibbsRingingArtifact< ScalarType >::AddArtifact(typename ComplexSliceType::
 {
     itk::ImageRegion<2> region = slice->GetLargestPossibleRegion();
     itk::ImageRegionIterator<ComplexSliceType> it(slice, region);
-    int x = std::ceil((double)region.GetSize()[0]*m_KspaceCropping);
-    int y = std::ceil((double)region.GetSize()[1]*m_KspaceCropping);
+
+    double x = (double)region.GetSize()[0]/2;
+    double y = (double)region.GetSize()[1]/2;
+
+    double radius = std::min(x,y);
+    radius *= (1-m_KspaceCropping);
+
+    if (region.GetSize()[0]%2 == 0)
+        x -= 0.5;
+    if (region.GetSize()[1]%2 == 0)
+        y -= 0.5;
+
+//    int x = std::ceil((double)region.GetSize()[0]*m_KspaceCropping);
+//    int y = std::ceil((double)region.GetSize()[1]*m_KspaceCropping);
     while(!it.IsAtEnd())
     {
         typename ComplexSliceType::IndexType idx = it.GetIndex();
-        if (idx[0]<x)
-            it.Set(vcl_complex<double>(0,0));
-        if (idx[0]>region.GetSize()[0]-x)
+        double dist = sqrt((idx[0]-x)*(idx[0]-x)+(idx[1]-y)*(idx[1]-y));
+        if (dist>radius)
             it.Set(0);
-        if (idx[1]>region.GetSize()[1]-y)
-            it.Set(0);
-        if (idx[1]<y)
-            it.Set(0);
+//        if (idx[0]<x)
+//            it.Set(vcl_complex<double>(0,0));
+//        if (idx[0]>region.GetSize()[0]-x)
+//            it.Set(0);
+//        if (idx[1]>region.GetSize()[1]-y)
+//            it.Set(0);
+//        if (idx[1]<y)
+//            it.Set(0);
         ++it;
     }
 }

1cb616cdd92122bb87e2796a0f72076a06b5e995
Alfred Franz <a.franz@dkfz-heidelberg.de>
2012-12-05 17:30:41 +0100

    added a test for removing of observers

--- Core/Code/Testing/mitkLevelWindowManagerTest.cpp
+++ Core/Code/Testing/mitkLevelWindowManagerTest.cpp
@@ -17,6 +17,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include "mitkLevelWindowManager.h"
 #include "mitkStandaloneDataStorage.h"
 #include <mitkTestingMacros.h>
+#include <mitkIOUtil.h>
 
 class mitkLevelWindowManagerTestClass
 {
@@ -122,6 +123,31 @@ public:
   */
   }
 
+  static void TestRemoveObserver()
+  {
+  mitk::LevelWindowManager::Pointer manager;
+  manager = mitk::LevelWindowManager::New();
+  mitk::StandaloneDataStorage::Pointer ds = mitk::StandaloneDataStorage::New();
+  manager->SetDataStorage(ds);
+  MITK_INFO << "Number of observers: " << manager->GetNumberOfObservers();
+  //add multiple objects to the data storage => multiple observers should be created
+  mitk::Image::Pointer image1 = mitk::IOUtil::LoadImageA("D:/prg/MITK-02-bin/CMakeExternals/Source/MITK-Data/Pic3D.nrrd");
+  mitk::DataNode::Pointer node1 = mitk::DataNode::New();
+  node1->SetData(image1);
+  mitk::Image::Pointer image2 = mitk::IOUtil::LoadImageA("D:/prg/MITK-02-bin/CMakeExternals/Source/MITK-Data/Pic3D.nrrd");
+  mitk::DataNode::Pointer node2 = mitk::DataNode::New();
+  node2->SetData(image2);
+  ds->Add(node1);
+  ds->Add(node2);
+  MITK_INFO << "Number of observers: " << manager->GetNumberOfObservers();
+  MITK_TEST_CONDITION_REQUIRED(manager->GetRelevantNodes()->size() == 2, "Test if nodes have been added");
+
+  ds->Remove(node1);
+  ds->Remove(node2);
+  //remove them
+  MITK_INFO << "Number of observers: " << manager->GetNumberOfObservers();
+  }
+
 };
 
 int mitkLevelWindowManagerTest(int, char* [])
@@ -130,6 +156,7 @@ int mitkLevelWindowManagerTest(int, char* [])
   mitkLevelWindowManagerTestClass::TestInstantiation();
   mitkLevelWindowManagerTestClass::TestSetGetDataStorage();
   mitkLevelWindowManagerTestClass::TestMethodsWithInvalidParameters();
+  mitkLevelWindowManagerTestClass::TestRemoveObserver();
 
   MITK_TEST_END();
 }

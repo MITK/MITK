e692a748f6ed4b092f55df04d50e25c44725aede
Tobias Schroeder <t.schroeder@dkfz-heidelberg.de>
2012-12-05 10:48:03 +0100

    fixed compiler error

--- Modules/Segmentation/Interactions/mitkSegTool2D.cpp
+++ Modules/Segmentation/Interactions/mitkSegTool2D.cpp
@@ -51,11 +51,11 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 mitk::SegTool2D::SegTool2D(const char* type)
 :Tool(type),
- m_LastEventSender(NULL),
- m_LastEventSlice(0),
- m_Contourmarkername ("Position"),
- m_ShowMarkerNodes (false),
- m_3DInterpolationEnabled(true)
+m_LastEventSender(NULL),
+m_LastEventSlice(0),
+m_Contourmarkername ("Position"),
+m_ShowMarkerNodes (false),
+m_3DInterpolationEnabled(true)
 {
 }
 
@@ -72,8 +72,8 @@ float mitk::SegTool2D::CanHandleEvent( StateEvent const *stateEvent) const
 
   //This are the mouse event that are used by the statemachine patterns for zooming and panning. This must be possible although a tool is activ
   if (stateEvent->GetId() == EIDRIGHTMOUSEBTN || stateEvent->GetId() == EIDMIDDLEMOUSEBTN || stateEvent->GetId() == EIDRIGHTMOUSEBTNANDCTRL ||
-      stateEvent->GetId() == EIDMIDDLEMOUSERELEASE || stateEvent->GetId() == EIDRIGHTMOUSERELEASE || stateEvent->GetId() == EIDRIGHTMOUSEBTNANDMOUSEMOVE ||
-      stateEvent->GetId() == EIDMIDDLEMOUSEBTNANDMOUSEMOVE || stateEvent->GetId() == EIDCTRLANDRIGHTMOUSEBTNANDMOUSEMOVE || stateEvent->GetId() == EIDCTRLANDRIGHTMOUSEBTNRELEASE )
+    stateEvent->GetId() == EIDMIDDLEMOUSERELEASE || stateEvent->GetId() == EIDRIGHTMOUSERELEASE || stateEvent->GetId() == EIDRIGHTMOUSEBTNANDMOUSEMOVE ||
+    stateEvent->GetId() == EIDMIDDLEMOUSEBTNANDMOUSEMOVE || stateEvent->GetId() == EIDCTRLANDRIGHTMOUSEBTNANDMOUSEMOVE || stateEvent->GetId() == EIDCTRLANDRIGHTMOUSEBTNRELEASE )
   {
     //Since the usual segmentation tools currently do not need right click interaction but the mitkDisplayVectorInteractor
     return 0.0;
@@ -228,6 +228,22 @@ void mitk::SegTool2D::WriteBackSegmentationResult (const PositionEvent* position
     unsigned int timeStep = positionEvent->GetSender()->GetTimeStep( image );
     this->WriteBackSegmentationResult(planeGeometry, slice, timeStep);
 
+    slice->DisconnectPipeline();
+    ImageToContourFilter::Pointer contourExtractor = ImageToContourFilter::New();
+    contourExtractor->SetInput(slice);
+    contourExtractor->Update();
+    mitk::Surface::Pointer contour = contourExtractor->GetOutput();
+
+    if (m_3DInterpolationEnabled && contour->GetVtkPolyData()->GetNumberOfPoints() > 0 )
+    {
+      unsigned int pos = this->AddContourmarker(positionEvent);
+      mitk::ServiceReference serviceRef = mitk::GetModuleContext()->GetServiceReference<PlanePositionManagerService>();
+      PlanePositionManagerService* service = dynamic_cast<PlanePositionManagerService*>(mitk::GetModuleContext()->GetService(serviceRef));
+      mitk::SurfaceInterpolationController::GetInstance()->AddNewContour( contour, service->GetPlanePosition(pos));
+      contour->DisconnectPipeline();
+    }
+  }
+
 }
 
 
@@ -240,7 +256,7 @@ void mitk::SegTool2D::WriteBackSegmentationResult (const PlaneGeometry* planeGeo
   Image* image = dynamic_cast<Image*>(workingNode->GetData());
 
 
-    //Make sure that for reslicing and overwriting the same alogrithm is used. We can specify the mode of the vtk reslicer
+  //Make sure that for reslicing and overwriting the same alogrithm is used. We can specify the mode of the vtk reslicer
   vtkSmartPointer<mitkVtkImageOverwrite> reslice = vtkSmartPointer<mitkVtkImageOverwrite>::New();
 
   //Set the slice as 'input'
@@ -281,28 +297,13 @@ void mitk::SegTool2D::WriteBackSegmentationResult (const PlaneGeometry* planeGeo
 
   slice->DisconnectPipeline();
 
-  ImageToContourFilter::Pointer contourExtractor = ImageToContourFilter::New();
-  contourExtractor->SetInput(slice);
-  contourExtractor->Update();
-  mitk::Surface::Pointer contour = contourExtractor->GetOutput();
-
-  if (m_3DInterpolationEnabled && contour->GetVtkPolyData()->GetNumberOfPoints() > 0 )
-  {
-    unsigned int pos = this->AddContourmarker(positionEvent);
-    mitk::ServiceReference serviceRef = mitk::GetModuleContext()->GetServiceReference<PlanePositionManagerService>();
-    PlanePositionManagerService* service = dynamic_cast<PlanePositionManagerService*>(mitk::GetModuleContext()->GetService(serviceRef));
-    mitk::SurfaceInterpolationController::GetInstance()->AddNewContour( contour, service->GetPlanePosition(pos));
-    contour->DisconnectPipeline();
-  }
-
-
   mitk::RenderingManager::GetInstance()->RequestUpdateAll();
 
 }
 
 void mitk::SegTool2D::SetShowMarkerNodes(bool status)
 {
-    m_ShowMarkerNodes = status;
+  m_ShowMarkerNodes = status;
 }
 
 void mitk::SegTool2D::SetEnable3DInterpolation(bool enabled)
@@ -359,23 +360,23 @@ unsigned int mitk::SegTool2D::AddContourmarker ( const PositionEvent* positionEv
     }
     else
     {
-        mitk::NodePredicateProperty::Pointer isMarker = mitk::NodePredicateProperty::New("isContourMarker", mitk::BoolProperty::New(true));
+      mitk::NodePredicateProperty::Pointer isMarker = mitk::NodePredicateProperty::New("isContourMarker", mitk::BoolProperty::New(true));
 
-        mitk::DataStorage::SetOfObjects::ConstPointer markers = m_ToolManager->GetDataStorage()->GetDerivations(workingNode,isMarker);
+      mitk::DataStorage::SetOfObjects::ConstPointer markers = m_ToolManager->GetDataStorage()->GetDerivations(workingNode,isMarker);
 
-        for ( mitk::DataStorage::SetOfObjects::const_iterator iter = markers->begin();
-          iter != markers->end();
-          ++iter)
+      for ( mitk::DataStorage::SetOfObjects::const_iterator iter = markers->begin();
+        iter != markers->end();
+        ++iter)
+      {
+        std::string nodeName = (*iter)->GetName();
+        unsigned int t = nodeName.find_last_of(" ");
+        unsigned int markerId = atof(nodeName.substr(t+1).c_str())-1;
+        if(id == markerId)
         {
-            std::string nodeName = (*iter)->GetName();
-            unsigned int t = nodeName.find_last_of(" ");
-            unsigned int markerId = atof(nodeName.substr(t+1).c_str())-1;
-            if(id == markerId)
-            {
-                return id;
-            }
+          return id;
         }
-        m_ToolManager->GetDataStorage()->Add(rotatedContourNode, workingNode);
+      }
+      m_ToolManager->GetDataStorage()->Add(rotatedContourNode, workingNode);
     }
   }
   return id;

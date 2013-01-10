ef677555cf83ff5d65e1fee2f4b163b1dd5337aa
Sandy Engelhardt <sandy.engelhardt@dkfz-heidelberg.de>
2013-01-10 15:31:27 +0100

    Adding base class for all GLMappers
    This class will be parent class for all current GLMappers

---
+++ Core/Code/Rendering/mitkGLMapper.cpp
@@ -0,0 +1,47 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center,
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+
+
+#include "mitkGL.h"
+#include "mitkGLMapper.h"
+
+mitk::GLMapper::GLMapper()
+{
+
+}
+
+mitk::GLMapper::~GLMapper()
+{
+}
+
+void mitk::GLMapper::MitkRenderOpaqueGeometry(mitk::BaseRenderer* renderer)
+{
+  if(IsVisible(renderer)==false)
+    return;
+
+  Paint(renderer);
+}
+
+void mitk::GLMapper::ApplyProperties(mitk::BaseRenderer* renderer)
+{
+    float rgba[4]={1.0f,1.0f,1.0f,1.0f};
+    // check for color prop and use it for rendering if it exists
+    GetColor(rgba, renderer);
+    // check for opacity prop and use it for rendering if it exists
+    GetOpacity(rgba[3], renderer);
+
+    glColor4fv(rgba);
+}
---
+++ Core/Code/Rendering/mitkGLMapper.h
@@ -0,0 +1,63 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center,
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+
+
+#ifndef MITKGLMAPPER_H_HEADER_INCLUDED_C197C872
+#define MITKGLMAPPER_H_HEADER_INCLUDED_C197C872
+
+#include <MitkExports.h>
+#include "mitkMapper.h"
+#include "mitkBaseRenderer.h"
+
+namespace mitk {
+
+//##Documentation
+//## @brief Base class of all OpenGL-based Mappers
+//##
+//## Those must implement the abstract method Paint(BaseRenderer).
+//## @ingroup Mapper
+class MITK_CORE_EXPORT GLMapper : public Mapper
+{
+  public:
+    //##Documentation
+    //## @brief Do the painting into the @a renderer
+    virtual void Paint(mitk::BaseRenderer *renderer) = 0;
+
+    //##Documentation
+    //## @brief Apply color and opacity read from the PropertyList
+    virtual void ApplyProperties(mitk::BaseRenderer* renderer);
+
+    void MitkRenderOpaqueGeometry(mitk::BaseRenderer* renderer);
+   // void MitkRenderTranslucentGeometry(mitk::BaseRenderer* renderer);
+   // void MitkRenderOverlay(mitk::BaseRenderer* renderer);
+    //void MitkRenderVolumetricGeometry(mitk::BaseRenderer* renderer);
+
+  /**
+   * \brief Returns whether this is an vtk-based mapper
+   */
+   virtual bool IsVtkBased() const { return false; }
+
+  protected:
+
+    GLMapper();
+    virtual ~GLMapper();
+};
+
+} // namespace mitk
+
+
+
+#endif /* MITKGLMAPPER2D_H_HEADER_INCLUDED_C197C872 */

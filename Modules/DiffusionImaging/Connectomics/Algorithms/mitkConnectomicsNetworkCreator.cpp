003905e96d34c3ec04bb8e006e0f388074cad106
Caspar Goch <c.goch@dkfz-heidelberg.de>
2013-01-17 18:18:34 +0100

    Use cloned geometry instead of the same one

--- Modules/DiffusionImaging/Connectomics/Algorithms/mitkConnectomicsNetworkCreator.cpp
+++ Modules/DiffusionImaging/Connectomics/Algorithms/mitkConnectomicsNetworkCreator.cpp
@@ -114,7 +114,10 @@ void mitk::ConnectomicsNetworkCreator::CreateNetworkFromFibersAndSegmentation()
   // Prune unconnected nodes
   m_ConNetwork->PruneUnconnectedSingleNodes();
   // provide network with geometry
-  m_ConNetwork->SetGeometry( m_Segmentation->GetGeometry() );
+  {
+    mitk::Geometry3D::Pointer clonedGeometry = dynamic_cast<mitk::Geometry3D*>(m_Segmentation->GetGeometry()->Clone().GetPointer());
+    m_ConNetwork->SetGeometry( clonedGeometry );
+  }
   m_ConNetwork->UpdateBounds();
   m_ConNetwork->SetIsModified( true );
 

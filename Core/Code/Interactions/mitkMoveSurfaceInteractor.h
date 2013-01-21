3c095bce63750ca37cfe8da3290f73e85d5024b1
Thomas Kilgus <t.kilgus@dkfz-heidelberg.de>
2013-01-21 13:38:37 +0100

    Added deprecated header file for legacy support of old interactor.

---
+++ Core/Code/Interactions/mitkMoveSurfaceInteractor.h
@@ -0,0 +1,28 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center,
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+
+#ifndef MITKMoveSurfaceInteractor_H_HEADER_INCLUDED
+#define MITKMoveSurfaceInteractor_H_HEADER_INCLUDED
+
+#include <mitkMoveBaseDataInteractor.h>
+
+namespace mitk
+{
+  // @deprecated Use MoveBaseDataInteractor instead.
+  typedef MoveSurfaceInteractor MoveBaseDataInteractor;
+}
+
+#endif /* MITKMoveSurfaceInteractor_H_HEADER_INCLUDED */

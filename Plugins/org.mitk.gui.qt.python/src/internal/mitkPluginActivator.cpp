2abffd07bbc2852435f3775e54c698eabd8977da
Michael Mueller <michael.mueller@dkfz-heidelberg.de>
2013-02-05 14:50:16 +0100

    importin ITK works

--- Plugins/org.mitk.gui.qt.python/src/internal/mitkPluginActivator.cpp
+++ Plugins/org.mitk.gui.qt.python/src/internal/mitkPluginActivator.cpp
@@ -16,26 +16,51 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include "mitkPluginActivator.h"
 #include <QtPlugin>
 #include "QmitkPythonView.h"
+#include "PythonPath.h"
 #include <ctkAbstractPythonManager.h>
 
 namespace mitk
 {
 
-void PluginActivator::start(ctkPluginContext* context)
-{
-  BERRY_REGISTER_EXTENSION_CLASS(QmitkPythonView, context)
-}
+    void PluginActivator::start(ctkPluginContext* context)
+    {
+      MITK_INFO << "registering python paths";
 
-void PluginActivator::stop(ctkPluginContext* context)
-{
-    Q_UNUSED(context)
-}
+      QString itkLibDirs(PYTHONPATH_ITK_LIBRARY_DIRS);
+      MITK_INFO << "itkLibDirs" << itkLibDirs.toStdString();
+      QString itkWrapItkDir(PYTHONPATH_WRAP_ITK_DIR);
+      MITK_INFO << "itkWrapItkDir" << itkWrapItkDir.toStdString();
 
-ctkAbstractPythonManager *PluginActivator::GetPythonManager()
-{
-    static ctkAbstractPythonManager PythonManager;
-    return &PythonManager;
-}
+      QString basecommand = "sys.path.append('%1');";
+      QString pythonCommand;
+      if( ! itkWrapItkDir.isEmpty() )
+      {
+         pythonCommand = basecommand.arg(itkLibDirs);
+         MITK_INFO << "issuing command " << pythonCommand.toStdString();
+         GetPythonManager()->executeString(pythonCommand, ctkAbstractPythonManager::SingleInput );
+
+         pythonCommand = basecommand.arg(itkWrapItkDir);
+         MITK_INFO << "issuing command " << pythonCommand.toStdString();
+         GetPythonManager()->executeString(pythonCommand, ctkAbstractPythonManager::SingleInput );
+      }
+      else
+      {
+          MITK_WARN << "No wrapping directory set for WrapITK. ITK Python Wrapping not available!";
+      }
+
+      BERRY_REGISTER_EXTENSION_CLASS(QmitkPythonView, context)
+    }
+
+    void PluginActivator::stop(ctkPluginContext* context)
+    {
+        Q_UNUSED(context)
+    }
+
+    ctkAbstractPythonManager *PluginActivator::GetPythonManager()
+    {
+        static ctkAbstractPythonManager PythonManager;
+        return &PythonManager;
+    }
 
 }
 

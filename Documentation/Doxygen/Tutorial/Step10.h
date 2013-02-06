6f4788895995be68ebfde5b7f1cc77e267d5cd86
Christian Weber <ch.weber@dkfz-heidelberg.de>
2013-02-06 10:07:10 +0100

    Reset Docu

---
+++ Documentation/Doxygen/Tutorial/Step10.h
@@ -0,0 +1,45 @@
+/*===================================================================
+
+ The Medical Imaging Interaction Toolkit (MITK)
+
+ Copyright (c) German Cancer Research Center,
+ Division of Medical and Biological Informatics.
+ All rights reserved.
+
+ This software is distributed WITHOUT ANY WARRANTY; without
+ even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE.
+
+ See LICENSE.txt or http://www.mitk.org for details.
+
+ ===================================================================*/
+
+#include "itkObject.h"
+#include "itkSmartPointer.h"
+#include "itkObjectFactory.h"
+#include "mitkCommon.h"
+#include <MitkExports.h>
+#include "mitkDataInteractor.h"
+#include <mitkPointSet.h>
+
+
+namespace mitk
+{
+  class MITK_CORE_EXPORT ExampleInteractor: public DataInteractor
+  {
+  public:
+    mitkClassMacro(ExampleInteractor, DataInteractor);
+    itkNewMacro(Self);
+
+  protected:
+    ExampleInteractor();
+    virtual ~ExampleInteractor();
+    virtual void ConnectActionsAndFunctions();
+
+  private:
+    bool AddPoint(StateMachineAction* , InteractionEvent*); // function to add new points
+    bool EnoughPoints(StateMachineAction* , InteractionEvent*); // function changes color of pointset to indicate, it is full
+    PointSet::Pointer m_PointSet;
+    int m_NumberOfPoints;
+  };
+}

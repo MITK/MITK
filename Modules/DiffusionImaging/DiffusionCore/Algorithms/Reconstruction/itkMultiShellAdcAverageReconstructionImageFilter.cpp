0473f52589e8929ff2e34e6882aad1ab6bcbd9e2
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2013-02-22 14:31:19 +0100

    add filter conzept

--- Modules/DiffusionImaging/DiffusionCore/Algorithms/Reconstruction/itkMultiShellAdcAverageReconstructionImageFilter.cpp
+++ Modules/DiffusionImaging/DiffusionCore/Algorithms/Reconstruction/itkMultiShellAdcAverageReconstructionImageFilter.cpp
@@ -52,6 +52,35 @@ MultiShellAdcAverageReconstructionImageFilter<TInputScalarType, TOutputScalarTyp
 }
 
 template <class TInputScalarType, class TOutputScalarType>
+void MultiShellAdcAverageReconstructionImageFilter<TInputScalarType, TOutputScalarType>
+::BeforeThreadedGenerateData()
+{
+  // test whether interpolation is necessary
+  // - Gradeint directions on different shells are different
+  //# if interpolation is neccesary and any #ShellDirection < 15 --> itkException (No interpolation possible)
+
+  // test whether BvalueMap contains all necessary information
+  //# BValueMapSize == 0 --> itkWarning (take BValueMap from inputImage)
+  //# BValueMap contains no bZero --> itkException
+
+  // if INTERPOLATION necessary
+  // [allDirectionsContainer] Gradient DirectionContainer containing all unique directions
+  // [sizeAllDirections] size of GradientContainer cointaining all unique directions
+  /* for each shell
+   * - calculate maxShOrder
+   * - calculate Weights [Weigthing = shell_size / max_shell_size]
+   * - get TragetSHBasis using allDirectionsContainer
+   * - get ShellSHBasis using currentShellDirections
+   * - calculate interpolationSHBasis [TargetSHBasis * ShellSHBasis^-1]
+   * - save interpolationSHBasis
+   */
+
+  // calculate average b-Value for target b-Value [bVal_t]
+  // calculate target bZero-Value [b0_t]
+
+}
+
+template <class TInputScalarType, class TOutputScalarType>
 void
 MultiShellAdcAverageReconstructionImageFilter<TInputScalarType, TOutputScalarType>
 ::ThreadedGenerateData(const OutputImageRegionType &outputRegionForThread, int /*threadId*/)
@@ -73,15 +102,41 @@ MultiShellAdcAverageReconstructionImageFilter<TInputScalarType, TOutputScalarTyp
   BValueMap::iterator it = m_BValueMap.begin();
   std::vector<double> adcVec = new Vector<double>(numShells);
 
+
+  // create empty nxm SignalMatrix containing n->signals/directions (in case of interpolation ~ sizeAllDirections otherwise the size of any shell) for m->shells
+  // create nx1 targetSignalVector
+  // ** walking over each Voxel
+  /* for each shell in this Voxel
+   * - get the RawSignal
+   * - interpolate the Signal if necessary using corresponding interpolationSHBasis
+   * - save the (interpolated) ShellSignalVector as the ith column in the SignalMatrix
+   */
+
+  // normalize the signals in SignalMatrix [bZeroAverage????] [S/S0 = E]
+
+  /* for each row in the SignalMatrix
+   * - calculate for each rowentry the ADC [ln(E)/-b = ADC]
+   * - weight the ith ADC entry with the corresponding shellWeighting
+   * - average the Signal over the row [ADC_t]
+   * - calculate target Signal using target b-Value [S_t = b0_t * e^(-bVal_t * ADC_t)
+   * - Save S_t in targetSignalVector
+   */
+
+  // outImageIterator set S_t
+
+  // **
+
+
+
   // initialize output image
   typename OutputImageType::Pointer outImage = OutputImageType::New();
-  outImage->SetSpacing( this->GetInput()->GetSpacing() );   // Set the image spacing
-  outImage->SetOrigin( this->GetInput()->GetOrigin() );     // Set the image origin
-  outImage->SetDirection( this->GetInput()->GetDirection() );  // Set the image direction
+  outImage->SetSpacing( this->GetInput()->GetSpacing() );
+  outImage->SetOrigin( this->GetInput()->GetOrigin() );
+  outImage->SetDirection( this->GetInput()->GetDirection() );  // Set the image direction using bZeroDirection+AllDirectionsContainer
   outImage->SetLargestPossibleRegion( this->GetInput()->GetLargestPossibleRegion());
   outImage->SetBufferedRegion( this->GetInput()->GetLargestPossibleRegion() );
   outImage->SetRequestedRegion( this->GetInput()->GetLargestPossibleRegion() );
-  outImage->SetVectorLength( vecLength ); // Set the vector length
+  outImage->SetVectorLength( vecLength ); // size of 1(bzeroValue) + AllDirectionsContainer
   outImage->Allocate();
 
 

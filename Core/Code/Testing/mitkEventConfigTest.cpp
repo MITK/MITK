26f969a133ef03197e275a80c8eabcaa0927d5ec
Christian Weber <ch.weber@dkfz-heidelberg.de>
2013-04-08 13:12:29 +0200

    Re-enabled event-config test

--- Core/Code/Testing/mitkEventConfigTest.cpp
+++ Core/Code/Testing/mitkEventConfigTest.cpp
@@ -24,9 +24,12 @@
 #include "mitkMouseReleaseEvent.h"
 #include "mitkInteractionKeyEvent.h"
 #include "mitkMousePressEvent.h"
+#include "mitkModule.h"
+#include "mitkGetModuleContext.h"
 #include <string>
+#include <iostream>
 
-int mitkEventConfigTest(int /*argc*/, char* /*argv*/[])
+int mitkEventConfigTest(int argc, char* argv[])
 {
   MITK_TEST_BEGIN("EventConfig")
 
@@ -34,50 +37,86 @@ int mitkEventConfigTest(int /*argc*/, char* /*argv*/[])
    * Loads a test a Config file and test if Config is build up correctly,
    * and if mapping from mitkEvents to EventVariant names works properly.
    * Indirectly this also tests the EventFactory Class, since we also test if the events have been constructed properly.
+   *
+   * The configuration object is constructed in three different ways,
+   * each one is tested here.
    */
-    mitk::EventConfig* smc = mitk::EventConfig::New();
+
+
+  // Construction using compiled-in resrouces:
+   mitk::Module *module = mitk::GetModuleContext()->GetModule();
+   mitk::EventConfig newConfig("StatemachineConfigTest.xml",module);
 
   MITK_TEST_CONDITION_REQUIRED(
-      smc->LoadConfig("Tests/StatemachineConfigTest.xml") == true
-       , "01 Check if file can be loaded" );
-
+         newConfig.IsValid() == true
+          , "01 Check if file can be loaded and is valid" );
   /*
    * Test the global properties:
    * Test if stored values match the ones in the test config file.
    */
-  mitk::PropertyList::Pointer properties = smc->GetAttributes();
+  mitk::PropertyList::Pointer properties = newConfig.GetAttributes();
   std::string prop1, prop2;
+  MITK_TEST_CONDITION_REQUIRED(
+        properties->GetStringProperty("property1",prop1) &&
+        prop1 == "yes" &&
+        properties->GetStringProperty("scrollModus",prop2) &&
+        prop2 == "leftright"
+        , "02 Check Global Properties");
+
+  /*
+   * Check if Events get mapped to the proper Variants
+   */
+  mitk::Point2D pos;
+  mitk::MousePressEvent::Pointer mpe1 = mitk::MousePressEvent::New(NULL,pos,mitk::InteractionEvent::MiddleMouseButton | mitk::InteractionEvent::LeftMouseButton ,mitk::InteractionEvent::ControlKey | mitk::InteractionEvent::AltKey,mitk::InteractionEvent::LeftMouseButton  );
+  mitk::MousePressEvent::Pointer standard1 = mitk::MousePressEvent::New(NULL,pos,mitk::InteractionEvent::LeftMouseButton,mitk::InteractionEvent::NoKey ,mitk::InteractionEvent::LeftMouseButton );
+  mitk::MouseMoveEvent::Pointer mme1 = mitk::MouseMoveEvent::New(NULL,pos,mitk::InteractionEvent::RightMouseButton | mitk::InteractionEvent::LeftMouseButton,mitk::InteractionEvent::ShiftKey );
+  mitk::MouseMoveEvent::Pointer mme2 = mitk::MouseMoveEvent::New(NULL,pos,mitk::InteractionEvent::RightMouseButton,mitk::InteractionEvent::ShiftKey );
+  mitk::MouseWheelEvent::Pointer mwe1 = mitk::MouseWheelEvent::New(NULL,pos,mitk::InteractionEvent::RightMouseButton,mitk::InteractionEvent::ShiftKey,-2 );
+  mitk::InteractionKeyEvent::Pointer ke = mitk::InteractionKeyEvent::New(NULL,"l",mitk::InteractionEvent::NoKey );
+
 
   MITK_TEST_CONDITION_REQUIRED(
-      properties->GetStringProperty("property1",prop1) &&
-      prop1 == "yes" &&
-      properties->GetStringProperty("scrollModus",prop2) &&
-      prop2 == "leftright"
-       , "02 Check Global Properties");
+        newConfig.GetMappedEvent(mpe1.GetPointer()) == "Variant1" &&
+        newConfig.GetMappedEvent(standard1.GetPointer()) == "Standard1" &&
+        newConfig.GetMappedEvent(mme1.GetPointer()) == "Move2" &&
+        newConfig.GetMappedEvent(ke.GetPointer()) == "Key1" &&
+        newConfig.GetMappedEvent(mme2.GetPointer()) == "" // does not exist in file
+        , "03 Check Mouse- and Key-Events "  );
 
+
+  // Construction providing a input stream
+
+  std::ifstream* configStream = new std::ifstream("/MITK/MITK/Core/Code/Testing/Resources/Interactions/StatemachineConfigTest.xml");
+  // if (myfile.is_open())
+  newConfig(configStream);
+
+  delete configStream;
+  MITK_TEST_CONDITION_REQUIRED(
+         newConfig.IsValid() == true
+          , "01 Check if file can be loaded and is valid" );
+  /*
+   * Test the global properties:
+   * Test if stored values match the ones in the test config file.
+   */
+  properties = newConfig.GetAttributes();
+    MITK_TEST_CONDITION_REQUIRED(
+        properties->GetStringProperty("property1",prop1) &&
+        prop1 == "yes" &&
+        properties->GetStringProperty("scrollModus",prop2) &&
+        prop2 == "leftright"
+        , "02 Check Global Properties");
 
   /*
    * Check if Events get mapped to the proper Variants
    */
 
-  mitk::Point2D pos;
-  mitk::MousePressEvent::Pointer mpe1 = mitk::MousePressEvent::New(NULL,pos,mitk::MiddleMouseButton | mitk::LeftMouseButton ,mitk::ControlKey | mitk::AltKey,mitk::LeftMouseButton  );
-  mitk::MousePressEvent::Pointer standard1 = mitk::MousePressEvent::New(NULL,pos,mitk::LeftMouseButton,mitk::NoKey ,mitk::LeftMouseButton );
-  mitk::MouseMoveEvent::Pointer mme1 = mitk::MouseMoveEvent::New(NULL,pos,mitk::RightMouseButton | mitk::LeftMouseButton,mitk::ShiftKey );
-  mitk::MouseMoveEvent::Pointer mme2 = mitk::MouseMoveEvent::New(NULL,pos,mitk::RightMouseButton,mitk::ShiftKey );
-  mitk::MouseWheelEvent::Pointer mwe1 = mitk::MouseWheelEvent::New(NULL,pos,mitk::RightMouseButton,mitk::ShiftKey,-2 );
-  mitk::InteractionKeyEvent::Pointer ke = mitk::InteractionKeyEvent::New(NULL,"l",mitk::NoKey );
-
-
   MITK_TEST_CONDITION_REQUIRED(
-       smc->GetMappedEvent(mpe1.GetPointer()) == "Variant1" &&
-       smc->GetMappedEvent(standard1.GetPointer()) == "Standard1" &&
-       smc->GetMappedEvent(mme1.GetPointer()) == "Move2" &&
-       smc->GetMappedEvent(ke.GetPointer()) == "Key1" &&
-       smc->GetMappedEvent(mme2.GetPointer()) == "" // does not exist in file
+        newConfig.GetMappedEvent(mpe1.GetPointer()) == "Variant1" &&
+        newConfig.GetMappedEvent(standard1.GetPointer()) == "Standard1" &&
+        newConfig.GetMappedEvent(mme1.GetPointer()) == "Move2" &&
+        newConfig.GetMappedEvent(ke.GetPointer()) == "Key1" &&
+        newConfig.GetMappedEvent(mme2.GetPointer()) == "" // does not exist in file
         , "03 Check Mouse- and Key-Events "  );
-
-
 
 
   // always end with this!

779e9a5bbbba8915ab5ca95377578fb80fe305db
Sandy Engelhardt <sandy.engelhardt@dkfz-heidelberg.de>
2013-04-11 18:49:44 +0200

    Added new files to files.cmake

--- Core/Code/files.cmake
+++ Core/Code/files.cmake
@@ -309,7 +309,7 @@ set(CPP_FILES
   Rendering/mitkGradientBackground.cpp
   Rendering/mitkManufacturerLogo.cpp
   Rendering/mitkMapper.cpp
-  Rendering/mitkPointSetGLMapper2D.cpp
+  Rendering/mitkPointSetVtkMapper2D.cpp
   Rendering/mitkPointSetVtkMapper3D.cpp
   Rendering/mitkPolyDataGLMapper2D.cpp
   Rendering/mitkSurfaceGLMapper2D.cpp

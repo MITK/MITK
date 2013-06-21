00a94d63041e2213d4044825c9ebb807ca2d1370
Tobias Schroeder <t.schroeder@dkfz-heidelberg.de>
2013-06-21 11:25:18 +0200

    Updated MITK-Data revision tag.

--- CMakeExternals/MITKData.cmake
+++ CMakeExternals/MITKData.cmake
@@ -13,7 +13,7 @@ set(MITK-Data_DEPENDS ${proj})
 
 if(BUILD_TESTING)
 
-  set(revision_tag 1d8634e6)
+  set(revision_tag 108eface)
 
   ExternalProject_Add(${proj}
     URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/MITK-Data_${revision_tag}.tar.gz

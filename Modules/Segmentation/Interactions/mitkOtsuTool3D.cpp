16624bd8670d8eaac8cdd9db8d49d3e0ceede75f
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2013-06-25 13:45:25 +0200

    Added icon for Otsu.

--- Modules/Segmentation/Interactions/mitkOtsuTool3D.cpp
+++ Modules/Segmentation/Interactions/mitkOtsuTool3D.cpp
@@ -83,7 +83,12 @@ void mitk::OtsuTool3D::Deactivated()
 
 const char** mitk::OtsuTool3D::GetXPM() const
 {
-  return mitkRegionGrow3DTool_xpm;
+  return NULL;
+}
+
+std::string mitk::OtsuTool3D::GetIconPath() const
+{
+  return ":/Segmentation/Otsu_48x48.png";
 }
 
 void mitk::OtsuTool3D::RunSegmentation(int regions)
@@ -182,7 +187,7 @@ void mitk::OtsuTool3D::UpdateBinaryPreview(int regionID)
 
 const char* mitk::OtsuTool3D::GetName() const
 {
-  return "Otsu Segmentation";
+  return "Otsu";
 }
 
 void mitk::OtsuTool3D::UpdateVolumePreview(bool volumeRendering)

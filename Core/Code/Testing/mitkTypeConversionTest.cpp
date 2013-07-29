038e1a69a87cb036da2945218ba717540e9a45e4
Sebastian Wirkert <wirkert@dkfz-heidelberg.de>
2013-07-29 15:16:03 +0200

    Restructurings in test organisation

--- Core/Code/Testing/mitkTypeConversionTest.cpp
+++ Core/Code/Testing/mitkTypeConversionTest.cpp
@@ -26,49 +26,43 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 using namespace mitk;
 
-static mitk::Point3D         point3D;
-static itk::Point<ScalarType, 3> itkPoint3D;
+static mitk::Point3D                point3D;
+static itk::Point<ScalarType, 3>    itkPoint3D;
 
 static vtkSmartPointer<vtkPoints>   a_vtkPoints = vtkSmartPointer<vtkPoints>::New();
 
-//static vnl_vector_fixed<ScalarType, 3> vnl_vector;
+static const ScalarType originalValues[] =  {1.0, 2.0, 3.0};
+static const ScalarType copiedValues[]   =  {4.0, 5.0, 6.0};
 
 static void Setup(void)
 {
-  const ScalarType  mitkValues[]= {1.0, 2.0, 3.0}; /* the values for the mitk point */
-  const double  othersValues[]  = {4.0, 5.0, 6.0}; /* the values for all other data representations */
-
-  point3D    = mitkValues;
-  itkPoint3D = othersValues;
-
   a_vtkPoints->Initialize();
-  a_vtkPoints->InsertNextPoint(othersValues);
-
-
-  //vnl_vector.copy_in(othersValues2);
 }
 
 static void Test_Mitk2Itk_PointCompatibility()
 {
   Setup();
-  const itk::Point<ScalarType, 3> copiedPoint = point3D;
 
-  MITK_TEST_CONDITION(
-       Equal(copiedPoint[0], point3D[0])
-    && Equal(copiedPoint[1], point3D[1])
-    && Equal(copiedPoint[2], point3D[2]), "mitk point assigned to itk point")
+  itkPoint3D = originalValues;
+  point3D    = copiedValues;
+
+  itkPoint3D = point3D;
+
+  MITK_TEST_CONDITION(itkPoint3D == point3D, "mitk point assigned to itk point")
+  MITK_TEST_CONDITION(itkPoint3D == copiedValues, "correct values were assigned")
 }
 
 static void Test_Itk2Mitk_PointCompatibility()
 {
   Setup();
 
-  const mitk::Point3D copiedPoint = itkPoint3D;
+  point3D    = originalValues;
+  itkPoint3D = copiedValues;
 
-  MITK_TEST_CONDITION(
-       Equal(copiedPoint[0], itkPoint3D[0])
-    && Equal(copiedPoint[1], itkPoint3D[1])
-    && Equal(copiedPoint[2], itkPoint3D[2]), "itk point assigned to mitk point")
+  point3D    = itkPoint3D;
+
+  MITK_TEST_CONDITION(point3D == itkPoint3D, "itk point assigned to mitk point")
+  MITK_TEST_CONDITION(point3D == copiedValues, "correct values were assigned")
 }
 
 
@@ -77,19 +71,24 @@ static void Test_Vtk2Mitk_PointCompatibility()
 {
   Setup();
 
+  point3D = originalValues;
+  a_vtkPoints->InsertNextPoint(copiedValues);
+
   double vtkPoint[3];
   a_vtkPoints->GetPoint(0, vtkPoint);
 
-  //mitk::Point3D copiedPoint = vtkPoint;
+  point3D = vtkPoint;
 
-  MITK_TEST_CONDITION(false, "this test is passed if there is no compile error in this method when not commented out :)")
+  MITK_TEST_CONDITION(point3D == vtkPoint, "vtkPoint assigned to mitk point")
+  MITK_TEST_CONDITION(point3D == copiedValues, "correct values were assigned")
 }
 
 static void Test_Vnl2Mitk_PointCompatibility()
 {
   Setup();
 
-  Point3D copiedPoint;
+  point3D = originalValues;
+  vnl_vector_fixed<ScalarType, 3> vnlVectorFixed(copiedValues);
 
   //  copiedPoint = vnl2mitk(vnl_vector);
 
@@ -113,9 +112,10 @@ static void Test_Mitk2Vnl_PointCompatibility()
   //  && Equal(static_cast<ScalarType>(copiedPoint[2]), point3D[2]), "mitk point assigned to vnl point")
 }
 
+
 /**
- * Test the conversions from and to mitk types
- */
+* Test the conversions from and to mitk types
+*/
 int mitkTypeConversionTest(int /*argc*/ , char* /*argv*/[])
 {
   // always start with this!

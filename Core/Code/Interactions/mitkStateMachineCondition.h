e74613096a1d3f09958a04faa337c6bd9e5777e8
Markus Engel <m.engel@mint-medical.de>
2013-07-30 16:28:27 +0200

    slimmed down StateMachineCondition, it's no longer a itk::LightObject, no longer exported

--- Core/Code/Interactions/mitkStateMachineCondition.h
+++ Core/Code/Interactions/mitkStateMachineCondition.h
@@ -17,9 +17,6 @@
 #ifndef StateMachineCondition_H_HEADER_INCLUDED_C19AE06B
 #define StateMachineCondition_H_HEADER_INCLUDED_C19AE06B
 
-#include <MitkExports.h>
-#include "mitkCommon.h"
-#include <itkLightObject.h>
 #include <string>
 
 namespace mitk
@@ -49,12 +46,12 @@ namespace mitk
   *
   * \ingroup Interaction
   */
-  class MITK_CORE_EXPORT StateMachineCondition: public itk::LightObject
+  class StateMachineCondition
   {
   public:
-    mitkClassMacro( StateMachineCondition, itk::Object )
-    mitkNewMacro1Param( Self, const std::string& )
-    mitkNewMacro2Param( Self, const std::string&, const bool )
+
+    StateMachineCondition( const std::string&, const bool inverted = false );
+    ~StateMachineCondition();
 
     /**
      * @brief Returns the String-Id of this action.
@@ -63,10 +60,6 @@ namespace mitk
 
     bool IsInverted() const;
 
-  protected:
-
-    StateMachineCondition( const std::string&, const bool inverted = false );
-    ~StateMachineCondition();
   private:
     /**
      * @brief The Id-Name of this action.

da9d6a5c2aeaa5415371bc1682ed303dc62d8320
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2013-08-12 21:02:02 +0200

    Squashed 'Core/CppMicroServices/' content from commit 49a40c9
    git-subtree-dir: Core/CppMicroServices
    git-subtree-split: 49a40c9b59ab2184c2046247ff57a6cae69a0f9a

---
+++ documentation/snippets/uServices-activator/main.cpp
@@ -0,0 +1,27 @@
+#include <usModuleActivator.h>
+
+US_USE_NAMESPACE
+
+//! [0]
+class MyActivator : public ModuleActivator
+{
+
+public:
+
+  void Load(ModuleContext* /*context*/)
+  { /* register stuff */ }
+
+
+  void Unload(ModuleContext* /*context*/)
+  { /* cleanup */ }
+
+};
+
+US_EXPORT_MODULE_ACTIVATOR(mylibname, MyActivator)
+//![0]
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+  MyActivator ma;
+  return 0;
+}

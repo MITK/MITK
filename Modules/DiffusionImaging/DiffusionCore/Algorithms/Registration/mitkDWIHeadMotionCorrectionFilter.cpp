d4e6a447ada0c07ca206e189c03e5c1fda794a9f
Jan Hering <j.hering@dkfz-heidelberg.de>
2013-08-16 18:09:04 +0200

    WIP(unstable) Finalizing the head motion correction
    * use the 3d+t --> DWI image caster to compose the registered data
    * back together

--- Modules/DiffusionImaging/DiffusionCore/Algorithms/Registration/mitkDWIHeadMotionCorrectionFilter.cpp
+++ Modules/DiffusionImaging/DiffusionCore/Algorithms/Registration/mitkDWIHeadMotionCorrectionFilter.cpp
@@ -26,6 +26,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include "mitkImageTimeSelector.h"
 
 #include "mitkPyramidImageRegistrationMethod.h"
+#include "mitkImageToDiffusionImageSource.h"
 
 #include "mitkIOUtil.h"
 
@@ -44,11 +45,11 @@ void mitk::DWIHeadMotionCorrectionFilter<DiffusionPixelType>
 
   DiffusionImageType* input = const_cast<DiffusionImageType*>(this->GetInput(0));
 
-  /*
-   * (1) Extract the b-zero images to a 3d+t image, register them by NCorr metric and
-   *     rigid registration : they will then be used are reference image for registering
-   *     the gradient images
-   */
+  //
+  //  (1) Extract the b-zero images to a 3d+t image, register them by NCorr metric and
+  //     rigid registration : they will then be used are reference image for registering
+  //     the gradient images
+  //
   typedef itk::B0ImageExtractionToSeparateImageFilter< DiffusionPixelType, DiffusionPixelType> B0ExtractorType;
   typename B0ExtractorType::Pointer b0_extractor = B0ExtractorType::New();
   b0_extractor->SetInput( input->GetVectorImage() );
@@ -60,7 +61,7 @@ void mitk::DWIHeadMotionCorrectionFilter<DiffusionPixelType>
   b0Image->SetImportChannel( b0_extractor->GetOutput()->GetBufferPointer(),
                              mitk::Image::CopyMemory );
 
-  /* (2.1) Use the extractor to access the extracted b0 volumes */
+  // (2.1) Use the extractor to access the extracted b0 volumes
   mitk::ImageTimeSelector::Pointer t_selector =
       mitk::ImageTimeSelector::New();
 
@@ -97,7 +98,7 @@ void mitk::DWIHeadMotionCorrectionFilter<DiffusionPixelType>
 
     try
     {
-      MITK_INFO << " === (" << i <<"/"<< numberOfb0Images << ") :: Starting registration";
+      MITK_INFO << " === (" << i <<"/"<< numberOfb0Images-1 << ") :: Starting registration";
       registrationMethod->Update();
     }
     catch( const itk::ExceptionObject& e)
@@ -112,9 +113,9 @@ void mitk::DWIHeadMotionCorrectionFilter<DiffusionPixelType>
   }
 
 
-  /*
-   * (2) Split the diffusion image into a 3d+t regular image, extract only the weighted images
-   */
+  //
+  // (2) Split the diffusion image into a 3d+t regular image, extract only the weighted images
+  //
   typename SplitFilterType::Pointer split_filter = SplitFilterType::New();
   split_filter->SetInput (input->GetVectorImage() );
   split_filter->SetExtractAllAboveThreshold(20, input->GetB_ValueMap() );
@@ -134,38 +135,47 @@ void mitk::DWIHeadMotionCorrectionFilter<DiffusionPixelType>
                                    mitk::Image::CopyMemory );
 
 
-  /*
-   * (3) Use again the time-selector to access the components separately in order
-   *     to perform the registration of  Image -> unweighted reference
-   */
+  //
+  // (3) Use again the time-selector to access the components separately in order
+  //     to perform the registration of  Image -> unweighted reference
+  //
 
   mitk::PyramidImageRegistrationMethod::Pointer weightedRegistrationMethod
       = mitk::PyramidImageRegistrationMethod::New();
 
   weightedRegistrationMethod->SetTransformToAffine();
   weightedRegistrationMethod->SetCrossModalityOn();
-  /*
-   *   - (3.1) Create a reference image by averaging the aligned b0 images
-   *
-   *   !!!FIXME: For rapid prototyping using the first one
-   */
+  //
+  //   - (3.1) Create a reference image by averaging the aligned b0 images
+  //
+  //   !!!FIXME: For rapid prototyping using the first one
+  //
 
   weightedRegistrationMethod->SetFixedImage( b0referenceImage );
 
-  /*
-   *   - (3.2) Register all timesteps in the splitted image onto the first reference
-   */
+  //
+  //   - (3.2) Register all timesteps in the splitted image onto the first reference
+  //
+  unsigned int maxImageIdx = splittedImage->GetTimeSteps();
+  mitk::TimeSlicedGeometry* tsg = splittedImage->GetTimeSlicedGeometry();
+  tsg->ExpandToNumberOfTimeSteps( maxImageIdx+1 );
 
+  mitk::Image::Pointer registeredWeighted = mitk::Image::New();
+  registeredWeighted->Initialize( splittedImage->GetPixelType(0), *tsg );
+
+  // insert the first unweighted reference as the first volume
+  registeredWeighted->SetImportVolume( b0referenceImage->GetData(),
+                                      0,0, mitk::Image::CopyMemory );
+
+
+  // mitk::Image::Pointer registeredWeighted = splittedImage->Clone();
+  // this time start at 0, we have only gradient images in the 3d+t file
+  // the reference image comes form an other image
   mitk::ImageTimeSelector::Pointer t_selector_w =
       mitk::ImageTimeSelector::New();
 
-  mitk::Image::Pointer registeredWeighted = splittedImage->Clone();
-
   t_selector_w->SetInput( splittedImage );
-  unsigned int maxImageIdx = splittedImage->GetTimeSteps();
 
-  // this time start at 0, we have only gradient images in the 3d+t file
-  // the reference image comes form an other image
   for( unsigned int i=0; i<maxImageIdx; i++)
   {
     t_selector_w->SetTimeNr(i);
@@ -175,7 +185,7 @@ void mitk::DWIHeadMotionCorrectionFilter<DiffusionPixelType>
 
     try
     {
-      MITK_INFO << " === (" << i <<"/"<< maxImageIdx << ") :: Starting registration";
+      MITK_INFO << " === (" << i+1 <<"/"<< maxImageIdx << ") :: Starting registration";
       weightedRegistrationMethod->Update();
     }
     catch( const itk::ExceptionObject& e)
@@ -183,12 +193,53 @@ void mitk::DWIHeadMotionCorrectionFilter<DiffusionPixelType>
       mitkThrow() << "Failed to register the b0 images, the PyramidRegistration threw an exception: \n" << e.what();
     }
 
-
+    // allow expansion
     registeredWeighted->SetImportVolume( weightedRegistrationMethod->GetResampledMovingImage()->GetData(),
-                                         i, 0, mitk::Image::ReferenceMemory);
-
-
+                                         i+1, 0, mitk::Image::CopyMemory);
   }
+
+
+  //
+  // (4) Cast the resulting image back to an diffusion weighted image
+  //
+
+  typename DiffusionImageType::GradientDirectionContainerType *gradients = input->GetDirections();
+  typename DiffusionImageType::GradientDirectionContainerType::Pointer gradients_new =
+      DiffusionImageType::GradientDirectionContainerType::New();
+  typename DiffusionImageType::GradientDirectionType bzero_vector;
+  bzero_vector.fill(0);
+  // compose the direction vector
+  //  - no direction for the first image
+  //  - correct ordering of the directions based on the index list
+  gradients_new->push_back( bzero_vector );
+
+  typename SplitFilterType::IndexListType index_list = split_filter->GetIndexList();
+  typename SplitFilterType::IndexListType::const_iterator lIter = index_list.begin();
+
+  while( lIter != index_list.end() )
+  {
+    gradients_new->push_back( gradients->at( *lIter ) );
+    ++lIter;
+  }
+
+  typename mitk::ImageToDiffusionImageSource< DiffusionPixelType >::Pointer caster =
+      mitk::ImageToDiffusionImageSource< DiffusionPixelType >::New();
+
+  caster->SetImage( registeredWeighted );
+  caster->SetBValue( input->GetB_Value() );
+  caster->SetGradientDirections( gradients_new );
+
+  try
+  {
+    caster->Update();
+    this->itk::ProcessObject::SetOutput(0 , caster->GetOutput() );
+  }
+  catch( const itk::ExceptionObject& e)
+  {
+    MITK_ERROR << "Casting back to diffusion image failed: ";
+    mitkThrow() << "Subprocess failed with exception: " << e.what();
+  }
+
 
 }
 

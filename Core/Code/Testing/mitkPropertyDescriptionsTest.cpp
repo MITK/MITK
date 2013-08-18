3e6c3dc26cf90e0186f7be40bce7f1c1e22c01e5
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2013-08-18 16:36:37 +0200

    Wrote property descriptions unit test.

--- Core/Code/Testing/mitkPropertyDescriptionsTest.cpp
+++ Core/Code/Testing/mitkPropertyDescriptionsTest.cpp
@@ -25,5 +25,28 @@ int mitkPropertyDescriptionsTest(int, char* [])
   mitk::IPropertyDescriptions* propertyDescriptions = mitk::CoreServices::GetPropertyDescriptions();
   MITK_TEST_CONDITION_REQUIRED(propertyDescriptions != NULL, "Get property descriptions service");
 
+  propertyDescriptions->AddDescription("propertyName1", "description1a");
+  propertyDescriptions->AddDescription("propertyName1", "description1b");
+  std::string description1 = propertyDescriptions->GetDescription("propertyName1");
+
+  MITK_TEST_CONDITION(
+    description1 == "description1a",
+    "Get description of \"propertyName1\"");
+
+  propertyDescriptions->AddDescription("propertyName1", "description1b", "", true);
+  description1 = propertyDescriptions->GetDescription("propertyName1");
+
+  MITK_TEST_CONDITION(
+    description1 == "description1b",
+    "Get overwritten description of \"propertyName1\"");
+
+  propertyDescriptions->AddDescription("propertyName1", "description1c", "className");
+  std::string description2 = propertyDescriptions->GetDescription("propertyName1", "className");
+  description1 = propertyDescriptions->GetDescription("propertyName1");
+
+  MITK_TEST_CONDITION(
+    description1 == "description1b" && description2 == "description1c",
+    "Get description of \"propertyName1\" restricted to \"className\"");
+
   MITK_TEST_END();
 }

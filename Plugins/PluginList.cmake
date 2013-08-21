08243b4938b7725081863e39a764309dd8a85e5f
Christoph Kolb <c.kolb@dkfz-heidelberg.de>
2013-08-21 20:03:21 +0200

    remove overlayexampleplugin from PluginList.cmake

--- Plugins/PluginList.cmake
+++ Plugins/PluginList.cmake
@@ -45,5 +45,4 @@ set(MITK_EXT_PLUGINS
   org.mitk.gui.qt.ugvisualization:OFF
   org.mitk.gui.qt.ultrasound:OFF
   org.mitk.gui.qt.volumevisualization:OFF
-  org.mitk.gui.qt.overlayexampleplugin:OFF
 )

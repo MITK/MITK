895847a4d911440c6b9823f0a720d8fb89fac781
Sebastian Wirkert <s.wirkert@dkfz-heidelberg.de>
2013-08-23 18:08:42 +0200

    First two tests for new mitk::Vector added.

--- Core/Code/Testing/mitkTypeVectorConversionTest.cpp
+++ Core/Code/Testing/mitkTypeVectorConversionTest.cpp
@@ -17,6 +17,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include "mitkTestingMacros.h"
 #include "mitkTypeBasics.h"
 #include "mitkVector.h"
+#include "mitkTypes.h" // for Equals
 #include "itkVector.h"
 #include <vnl/vnl_vector_ref.h>
 #include <vnl/vnl_vector_fixed_ref.h>
@@ -29,6 +30,28 @@ using namespace mitk;
 static const ScalarType originalValues[] =  {1.0, 2.0, 3.0};
 static const ScalarType valuesToCopy[]   =  {4.0, 5.0, 6.0};
 
+
+static void Test_itk2Mitk_Compatibility(void)
+{
+  Vector3D vector3D = originalValues;
+  itk::Vector<ScalarType, 3> itkVector = valuesToCopy;
+
+  vector3D = itkVector;
+
+  MITK_TEST_CONDITION(vector3D == itkVector, "itk vector assigned to mitk vector")
+  MITK_TEST_CONDITION(vector3D == valuesToCopy, "correct values were assigned")
+}
+
+static void Test_Mitk2itk_Compatibility(void)
+{
+  Vector3D vector3D = valuesToCopy;
+  itk::Vector<ScalarType, 3> itkVector = originalValues;
+
+  itkVector = vector3D;
+
+  MITK_TEST_CONDITION(vector3D == itkVector, "mitk vector assigned to itk vector")
+  MITK_TEST_CONDITION(itkVector == valuesToCopy, "correct values were assigned")
+}
 
 
 template< class T, unsigned int NVectorDimension>
@@ -79,6 +102,9 @@ int mitkTypeVectorConversionTest(int /*argc*/ , char* /*argv*/[])
 {
   // always start with this!
   MITK_TEST_BEGIN("VectorConversionTest")
+
+  Test_itk2Mitk_Compatibility();
+  Test_Mitk2itk_Compatibility();
 
   Test_Vnl2Mitk_VectorFixedCompatibility();
   Test_Vnl2Mitk_VectorCompatibility();

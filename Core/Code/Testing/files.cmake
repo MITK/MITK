9169145727f18aae4585c4665fcd910e55829487
Christoph Kolb <c.kolb@dkfz-heidelberg.de>
2013-08-26 11:04:04 +0200

    made rendering tests custom only

--- Core/Code/Testing/files.cmake
+++ Core/Code/Testing/files.cmake
@@ -72,14 +72,6 @@ set(MODULE_TESTS
   mitkPropertyDescriptionsTest.cpp
   mitkPropertyExtensionsTest.cpp
   mitkPropertyFiltersTest.cpp
-  mitkLabelOverlay3DRendering2DTest.cpp
-  mitkLabelOverlay3DRendering3DTest.cpp
-  mitkTextOverlay2DRenderingTest.cpp
-  mitkTextOverlay2DLayouterRenderingTest.cpp
-  #mitkTextOverlay2DSymbolsRenderingTest.cpp
-  mitkTextOverlay3DRendering2DTest.cpp
-  mitkTextOverlay3DRendering3DTest.cpp
-  mitkTextOverlay3DColorRenderingTest.cpp
 )
 
 # test with image filename as an extra command line parameter
@@ -138,6 +130,13 @@ set(MODULE_CUSTOM_TESTS
     mitkPointSetVtkMapper2DTest.cpp
     mitkPointSetVtkMapper2DImageTest.cpp
     mitkPointSetVtkMapper2DGlyphTypeTest.cpp
+    mitkLabelOverlay3DRendering2DTest.cpp
+    mitkLabelOverlay3DRendering3DTest.cpp
+    mitkTextOverlay2DRenderingTest.cpp
+    mitkTextOverlay2DLayouterRenderingTest.cpp
+    mitkTextOverlay3DRendering2DTest.cpp
+    mitkTextOverlay3DRendering3DTest.cpp
+    mitkTextOverlay3DColorRenderingTest.cpp
 )
 
 set(MODULE_RESOURCE_FILES

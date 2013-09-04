3cfcab64142ad20cead862899dfab48ed5ff2704
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2013-09-04 15:07:26 +0200

    "Image.Vector Image" property is automatically set according to image type.

--- Core/Code/Rendering/mitkImageVtkMapper2D.cpp
+++ Core/Code/Rendering/mitkImageVtkMapper2D.cpp
@@ -763,7 +763,10 @@ void mitk::ImageVtkMapper2D::SetDefaultProperties(mitk::DataNode* node, mitk::Ba
     node->AddProperty( "color", ColorProperty::New(1.0,1.0,1.0), renderer, overwrite );
     node->AddProperty( "binary", mitk::BoolProperty::New( false ), renderer, overwrite );
     node->AddProperty("layer", mitk::IntProperty::New(0), renderer, overwrite);
-    node->AddProperty("Image.Vector Image", mitk::BoolProperty::New(false), renderer, overwrite);
+
+    bool isVectorImage = image->GetPixelType().GetPixelTypeAsString() == "vector";
+
+    node->AddProperty("Image.Vector Image", mitk::BoolProperty::New(isVectorImage), renderer, overwrite);
     node->AddProperty("Image.Vector Image.Visible Component", mitk::IntProperty::New(0), renderer, overwrite);
   }
 

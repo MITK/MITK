d68bd5ab5c5a5c8d7bedb16af9bf78912f57423f
Michael Brehler <m.brehler@dkfz-heidelberg.de>
2013-09-24 09:55:12 +0200

    Corrected SmartPointer usage

--- Modules/ImageStatistics/mitkImageStatisticsCalculator.cpp
+++ Modules/ImageStatistics/mitkImageStatisticsCalculator.cpp
@@ -1183,7 +1183,7 @@ void ImageStatisticsCalculator::InternalCalculateMaskFromPlanarFigure(
   imageStencilFilter->Update();
 
   // Export from VTK back to ITK
-  vtkSmartPointer<vtkImageExport> vtkExporter = vtkImageExport::New(); // TODO: this is WRONG, should be vtkSmartPointer<vtkImageExport>::New(), but bug # 14455
+  vtkSmartPointer<vtkImageExport> vtkExporter = vtkSmartPointer<vtkImageExport>::New();
   vtkExporter->SetInputConnection( imageStencilFilter->GetOutputPort() );
   vtkExporter->Update();
 

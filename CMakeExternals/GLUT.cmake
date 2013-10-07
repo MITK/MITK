d37eaa81737cc64ed968b7d29cab18e7be8fab65
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2013-10-07 13:33:03 +0200

    Added freeglut to external projects and made SOFA using MITK superbuild versions of freeglut and GLEW.

---
+++ CMakeExternals/GLUT.cmake
@@ -0,0 +1,38 @@
+#-----------------------------------------------------------------------------
+# freeglut
+#-----------------------------------------------------------------------------
+
+# Sanity checks
+if(DEFINED GLUT_DIR AND NOT EXISTS ${GLUT_DIR})
+  message(FATAL_ERROR "GLUT_DIR variable is defined but corresponds to non-existing directory")
+endif()
+
+set(proj GLUT)
+set(proj_DEPENDENCIES )
+set(${proj}_DEPENDS ${proj})
+
+if(NOT DEFINED GLUT_DIR)
+
+  set(patch_cmd ${CMAKE_COMMAND} -Dproj:STRING=${proj} -Dproj_target:STRING=GLUT -P ${CMAKE_CURRENT_LIST_DIR}/GenerateDefaultCMakeBuildSystem.cmake)
+
+  ExternalProject_Add(${proj}
+     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}-src
+     BINARY_DIR ${proj}-build
+     PREFIX ${proj}-cmake
+     URL http://dl.dropboxusercontent.com/u/5822501/freeglut-2.8.1.tar.gz
+     URL_MD5 918ffbddcffbac83c218bc52355b6d5a
+     PATCH_COMMAND ${patch_cmd}
+     INSTALL_COMMAND ""
+     CMAKE_GENERATOR ${gen}
+     CMAKE_ARGS
+       ${ep_common_args}
+     DEPENDS ${proj_DEPENDENCIES}
+    )
+
+  set(GLUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/${proj}-build)
+
+else()
+
+  mitkMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
+
+endif()

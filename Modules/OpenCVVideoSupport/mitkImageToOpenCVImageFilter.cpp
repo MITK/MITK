0a873d8ca7658158150ef32eadd21beadeb1165e
Sven Mersmann <s.mersmann@dkfz-heidelberg.de>
2013-10-21 13:23:32 +0200

    Enhanced the unit test for conversion of mitkImage to OpenCV image and back. Excluded the itkImageBridge for conversion to mitk for floating point images do not work with this class. Included namespace mitk.

--- Modules/OpenCVVideoSupport/mitkImageToOpenCVImageFilter.cpp
+++ Modules/OpenCVVideoSupport/mitkImageToOpenCVImageFilter.cpp
@@ -20,73 +20,82 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <itkRGBPixel.h>
 #include <mitkImageAccessByItk.h>
 
-mitk::ImageToOpenCVImageFilter::ImageToOpenCVImageFilter()
-: m_OpenCVImage(0)
-{
-}
+namespace mitk{
 
-mitk::ImageToOpenCVImageFilter::~ImageToOpenCVImageFilter()
-{
-  m_OpenCVImage = 0;
-}
-
-bool mitk::ImageToOpenCVImageFilter::CheckImage( mitk::Image* image )
-{
-  if(image == 0)
+  ImageToOpenCVImageFilter::ImageToOpenCVImageFilter()
+    : m_OpenCVImage(0)
   {
-    MITK_WARN << "MITK Image is 0";
-    return false;
   }
-  if(image->GetDimension() != 2)
+
+  ImageToOpenCVImageFilter::~ImageToOpenCVImageFilter()
   {
-    MITK_WARN << "Only 2D Images allowed";
-    return false;
+    m_OpenCVImage = 0;
   }
-  return true;
-}
 
-IplImage* mitk::ImageToOpenCVImageFilter::GetOpenCVImage()
-{
-  m_OpenCVImage = 0;
-  if(!this->CheckImage( m_Image ))
-    return 0;
 
+  void ImageToOpenCVImageFilter::SetImage( Image* _Image )
+  {
+    m_Image = _Image;
+  }
+
+
+  bool ImageToOpenCVImageFilter::CheckImage( Image* image )
+  {
+    if(image == 0)
+    {
+      MITK_WARN << "MITK Image is 0";
+      return false;
+    }
+    if(image->GetDimension() > 2 )
+    {
+      MITK_WARN << "Only 2D Images allowed";
+      return false;
+    }
+    return true;
+  }
+
+  IplImage* ImageToOpenCVImageFilter::GetOpenCVImage()
+  {
+
+    if(!this->CheckImage( m_Image ))
+      return 0;
+
+    m_OpenCVImage = (0);
 
     try
     {
       AccessFixedTypeByItk(m_Image.GetPointer(), ItkImageProcessing,
-                           MITK_ACCESSBYITK_PIXEL_TYPES_SEQ  // gray image
-                           (UCRGBPixelType)(USRGBPixelType)(FloatRGBPixelType)(DoubleRGBPixelType), // rgb image
-                           (2) // dimensions
-                           )
+        MITK_ACCESSBYITK_PIXEL_TYPES_SEQ  // gray image
+        (UCRGBPixelType)(USRGBPixelType)(FloatRGBPixelType)(DoubleRGBPixelType), // rgb image
+        (2) // dimensions
+        )
     }
     catch (const AccessByItkException& e) {
       std::cout << "Caught exception [from AccessFixedTypeByItk]: \n" << e.what() << "\n";
       return 0;
     }
+    return m_OpenCVImage;
+  }
 
-  return m_OpenCVImage;
-}
-
-cv::Mat mitk::ImageToOpenCVImageFilter::GetOpenCVMat()
-{
+  cv::Mat ImageToOpenCVImageFilter::GetOpenCVMat()
+  {
     IplImage* img = this->GetOpenCVImage();
-    //cvNamedWindow("debug2", CV_WINDOW_FREERATIO);
-    //cvShowImage("debug2", img);
-    //cv::waitKey(0);
+
     cv::Mat mat;
     if( img )
     {
-        // do not copy data, then release just the header
-        mat = cv::Mat ( img, false );
-        cvReleaseImageHeader( &img );
+      // do not copy data, then release just the header
+      mat = cv::Mat ( img, false );
+      cvReleaseImageHeader( &img );
     }
 
     return mat;
-}
+  }
 
-void mitk::ImageToOpenCVImageFilter::SetImage( mitk::Image* _Image )
-{
-//  if(m_Image == _Image) return;
-  m_Image = _Image;
-}
+  template<typename TPixel, unsigned int VImageDimension>
+  void ImageToOpenCVImageFilter::ItkImageProcessing( itk::Image<TPixel,VImageDimension>* image )
+  {
+    m_OpenCVImage = itk::OpenCVImageBridge::ITKImageToIplImage(image);
+  }
+
+} // end namespace mitk

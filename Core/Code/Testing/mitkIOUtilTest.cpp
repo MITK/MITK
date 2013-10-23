9751bf680fbacd1cb86eb765a156a8f7f7b5e907
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2013-10-24 01:43:15 +0200

    Converted Geometry3DEqual and IOUtil tests to CppUnit tests.

--- Core/Code/Testing/mitkIOUtilTest.cpp
+++ Core/Code/Testing/mitkIOUtilTest.cpp
@@ -16,77 +16,101 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include "mitkTestingMacros.h"
 #include <mitkTestingConfig.h>
+#include <mitkParameterizedTestFixture.h>
+
 #include <mitkIOUtil.h>
 #include <mitkImageGenerator.h>
 
 #include <itksys/SystemTools.hxx>
 
-void TestTempMethods()
+class mitkIOUtilTestSuite : public mitk::ParameterizedTestFixture
 {
-  std::string tmpPath = mitk::IOUtil::GetTempPath();
-  MITK_TEST_CONDITION_REQUIRED(!tmpPath.empty(), "Get temporary path")
 
-  std::ofstream tmpFile;
-  std::string tmpFilePath = mitk::IOUtil::CreateTemporaryFile(tmpFile);
-  MITK_TEST_CONDITION_REQUIRED(tmpFile && tmpFile.is_open(), "Temp file stream");
-  MITK_TEST_CONDITION_REQUIRED(tmpFilePath.size() > tmpPath.size(), "Temp file path size")
-  MITK_TEST_CONDITION(tmpFilePath.substr(0, tmpPath.size()) == tmpPath, "Temp file is in temp path")
+  CPPUNIT_TEST_SUITE(mitkIOUtilTestSuite);
+  CPPUNIT_TEST(TestTempMethods);
+  MITK_PARAMETERIZED_TEST(TestLoadAndSaveMethods);
+  CPPUNIT_TEST_SUITE_END();
 
-  tmpFile.close();
-  MITK_TEST_CONDITION(std::remove(tmpFilePath.c_str()) == 0, "Remove temp file")
+private:
 
-  std::string programPath = mitk::IOUtil::GetProgramPath();
-  MITK_TEST_CONDITION_REQUIRED(!programPath.empty(), "Get program file path")
-  std::ofstream tmpFile2;
-  std::string tmpFilePath2 = mitk::IOUtil::CreateTemporaryFile(tmpFile2, "my-XXXXXX", programPath);
-  MITK_TEST_CONDITION_REQUIRED(tmpFile2 && tmpFile2.is_open(), "Temp file 2 stream")
-  MITK_TEST_CONDITION_REQUIRED(tmpFilePath2.size() > programPath.size(), "Temp file 2 path size")
-  MITK_TEST_CONDITION(tmpFilePath2.substr(0, programPath.size()) == programPath, "Temp file 2 is in program path")
-  tmpFile2.close();
-  MITK_TEST_CONDITION(std::remove(tmpFilePath2.c_str()) == 0, "Remove temp file 2")
+  std::string pathToImage;
+  std::string pathToPointSet;
+  std::string pathToSurface;
 
-  std::ofstream tmpFile3;
-  std::string tmpFilePath3 = mitk::IOUtil::CreateTemporaryFile(tmpFile3, std::ios_base::binary,
-                                                               "my-XXXXXX.TXT", programPath);
-  MITK_TEST_CONDITION_REQUIRED(tmpFile3 && tmpFile3.is_open(), "Temp file 3 stream")
-  MITK_TEST_CONDITION_REQUIRED(tmpFilePath3.size() > programPath.size(), "Temp file 3 path size")
-  MITK_TEST_CONDITION(tmpFilePath3.substr(0, programPath.size()) == programPath, "Temp file 23 is in program path")
-  MITK_TEST_CONDITION(tmpFilePath3.substr(tmpFilePath3.size() - 13, 3) == "my-", "Temp file 3 starts with 'my-'")
-  MITK_TEST_CONDITION(tmpFilePath3.substr(tmpFilePath3.size() - 4) == ".TXT", "Temp file 3 ends with '.TXT'")
-  tmpFile3.close();
-  //MITK_TEST_CONDITION(std::remove(tmpFilePath3.c_str()) == 0, "Remove temp file 3")
+public:
 
-  MITK_TEST_FOR_EXCEPTION(mitk::Exception&, mitk::IOUtil::CreateTemporaryFile(tmpFile2, "XX"))
+  static CppUnit::Test* suite(int argc, char* argv[])
+  {
+    CppUnit::TestSuite* testSuite = new CppUnit::TestSuite("mitkIOUtilTest");
+    testSuite->addTest(new mitk::ParameterizedTestCaller<mitkIOUtilTestSuite>(
+                         "bla", &mitkIOUtilTestSuite::TestTempMethods));
+    return testSuite;
+  }
 
-  std::string tmpDir = mitk::IOUtil::CreateTemporaryDirectory();
-  MITK_TEST_CONDITION_REQUIRED(tmpDir.size() > tmpPath.size(), "Temp dir size")
-  MITK_TEST_CONDITION(tmpDir.substr(0, tmpPath.size()) == tmpPath, "Temp dir is in temp path")
-  MITK_TEST_CONDITION(itksys::SystemTools::RemoveADirectory(tmpDir.c_str()), "Remove temp dir")
+  void setUpParameter(const std::vector<std::string>& parameter)
+  {
+    CPPUNIT_ASSERT(parameter.size() == 3);
+    pathToImage = parameter[0];
+    pathToPointSet = parameter[1];
+    pathToSurface = parameter[2];
+  }
 
-  std::string tmpDir2 = mitk::IOUtil::CreateTemporaryDirectory("my-XXXXXX", programPath);
-  MITK_TEST_CONDITION_REQUIRED(tmpDir2.size() > programPath.size(), "Temp dir 2 size")
-  MITK_TEST_CONDITION(tmpDir2.substr(0, programPath.size()) == programPath, "Temp dir 2 is in temp path")
-  MITK_TEST_CONDITION(itksys::SystemTools::RemoveADirectory(tmpDir2.c_str()), "Remove temp dir 2")
-}
+  void TestTempMethods()
+  {
+    std::string tmpPath = mitk::IOUtil::GetTempPath();
+    CPPUNIT_ASSERT(!tmpPath.empty());
 
-int mitkIOUtilTest(int  argc , char* argv[])
-{
-    // always start with this!
-    MITK_TEST_BEGIN("mitkIOUtilTest");
-    MITK_TEST_CONDITION_REQUIRED( argc >= 4, "Testing if input parameters are set.");
+    std::ofstream tmpFile;
+    std::string tmpFilePath = mitk::IOUtil::CreateTemporaryFile(tmpFile);
+    CPPUNIT_ASSERT(tmpFile && tmpFile.is_open());
+    CPPUNIT_ASSERT(tmpFilePath.size() > tmpPath.size());
+    CPPUNIT_ASSERT(tmpFilePath.substr(0, tmpPath.size()) == tmpPath);
 
-    TestTempMethods();
+    tmpFile.close();
+    CPPUNIT_ASSERT(std::remove(tmpFilePath.c_str()) == 0);
 
-    std::string pathToImage = argv[1];
-    std::string pathToPointSet = argv[2];
-    std::string pathToSurface = argv[3];
+    std::string programPath = mitk::IOUtil::GetProgramPath();
+    CPPUNIT_ASSERT(!programPath.empty());
+    std::ofstream tmpFile2;
+    std::string tmpFilePath2 = mitk::IOUtil::CreateTemporaryFile(tmpFile2, "my-XXXXXX", programPath);
+    CPPUNIT_ASSERT(tmpFile2 && tmpFile2.is_open());
+    CPPUNIT_ASSERT(tmpFilePath2.size() > programPath.size());
+    CPPUNIT_ASSERT(tmpFilePath2.substr(0, programPath.size()) == programPath);
+    tmpFile2.close();
+    CPPUNIT_ASSERT(std::remove(tmpFilePath2.c_str()) == 0);
 
+    std::ofstream tmpFile3;
+    std::string tmpFilePath3 = mitk::IOUtil::CreateTemporaryFile(tmpFile3, std::ios_base::binary,
+                                                                 "my-XXXXXX.TXT", programPath);
+    CPPUNIT_ASSERT(tmpFile3 && tmpFile3.is_open());
+    CPPUNIT_ASSERT(tmpFilePath3.size() > programPath.size());
+    CPPUNIT_ASSERT(tmpFilePath3.substr(0, programPath.size()) == programPath);
+    CPPUNIT_ASSERT(tmpFilePath3.substr(tmpFilePath3.size() - 13, 3) == "my-");
+    CPPUNIT_ASSERT(tmpFilePath3.substr(tmpFilePath3.size() - 4) == ".TXT");
+    tmpFile3.close();
+    //CPPUNIT_ASSERT(std::remove(tmpFilePath3.c_str()) == 0)
+
+    CPPUNIT_ASSERT_THROW(mitk::IOUtil::CreateTemporaryFile(tmpFile2, "XX"), mitk::Exception);
+
+    std::string tmpDir = mitk::IOUtil::CreateTemporaryDirectory();
+    CPPUNIT_ASSERT(tmpDir.size() > tmpPath.size());
+    CPPUNIT_ASSERT(tmpDir.substr(0, tmpPath.size()) == tmpPath);
+    CPPUNIT_ASSERT(itksys::SystemTools::RemoveADirectory(tmpDir.c_str()));
+
+    std::string tmpDir2 = mitk::IOUtil::CreateTemporaryDirectory("my-XXXXXX", programPath);
+    CPPUNIT_ASSERT(tmpDir2.size() > programPath.size());
+    CPPUNIT_ASSERT(tmpDir2.substr(0, programPath.size()) == programPath);
+    CPPUNIT_ASSERT(itksys::SystemTools::RemoveADirectory(tmpDir2.c_str()));
+  }
+
+  void TestLoadAndSaveMethods()
+  {
     mitk::Image::Pointer img1 = mitk::IOUtil::LoadImage(pathToImage);
-    MITK_TEST_CONDITION( img1.IsNotNull(), "Testing if image 1 could be loaded");
+    CPPUNIT_ASSERT( img1.IsNotNull());
     mitk::PointSet::Pointer pointset = mitk::IOUtil::LoadPointSet(pathToPointSet);
-    MITK_TEST_CONDITION( pointset.IsNotNull(), "Testing if pointset could be loaded");
+    CPPUNIT_ASSERT( pointset.IsNotNull());
     mitk::Surface::Pointer surface = mitk::IOUtil::LoadSurface(pathToSurface);
-    MITK_TEST_CONDITION( surface.IsNotNull(), "Testing if surface could be loaded");
+    CPPUNIT_ASSERT( surface.IsNotNull());
 
     std::string outDir = MITK_TEST_OUTPUT_DIR;
     std::string imagePath = outDir+"/diffpic3d.nrrd";
@@ -97,18 +121,18 @@ int mitkIOUtilTest(int  argc , char* argv[])
     std::string pointSetPathWithoutDefaultExtension = outDir + "/diffpointset2.xXx";
 
     // the cases where no exception should be thrown
-    MITK_TEST_CONDITION(mitk::IOUtil::SaveImage(img1, imagePath), "Testing if the image could be saved");
-    MITK_TEST_CONDITION(mitk::IOUtil::SaveBaseData(img1.GetPointer(), imagePath2), "Testing if the image could be saved");
-    MITK_TEST_CONDITION(mitk::IOUtil::SavePointSet(pointset, pointSetPath), "Testing if the pointset could be saved");
-    MITK_TEST_CONDITION(mitk::IOUtil::SaveSurface(surface, surfacePath), "Testing if the surface could be saved");
+    CPPUNIT_ASSERT(mitk::IOUtil::SaveImage(img1, imagePath));
+    CPPUNIT_ASSERT(mitk::IOUtil::SaveBaseData(img1.GetPointer(), imagePath2));
+    CPPUNIT_ASSERT(mitk::IOUtil::SavePointSet(pointset, pointSetPath));
+    CPPUNIT_ASSERT(mitk::IOUtil::SaveSurface(surface, surfacePath));
 
     // test if defaultextension is inserted if no extension is present
-    MITK_TEST_CONDITION(mitk::IOUtil::SavePointSet(pointset, pointSetPathWithoutDefaultExtension.c_str()), "Testing if the pointset could be saved");
+    CPPUNIT_ASSERT(mitk::IOUtil::SavePointSet(pointset, pointSetPathWithoutDefaultExtension.c_str()));
 
     // test if exception is thrown as expected on unknown extsension
-    MITK_TEST_FOR_EXCEPTION(mitk::Exception, mitk::IOUtil::SaveSurface(surface,"testSurface.xXx"));
+    CPPUNIT_ASSERT_THROW(mitk::IOUtil::SaveSurface(surface,"testSurface.xXx"), mitk::Exception);
     //load data which does not exist
-    MITK_TEST_FOR_EXCEPTION(mitk::Exception, mitk::IOUtil::LoadImage("fileWhichDoesNotExist.nrrd"));
+    CPPUNIT_ASSERT_THROW(mitk::IOUtil::LoadImage("fileWhichDoesNotExist.nrrd"), mitk::Exception);
 
     //delete the files after the test is done
     remove(imagePath.c_str());
@@ -119,16 +143,9 @@ int mitkIOUtilTest(int  argc , char* argv[])
 
     mitk::Image::Pointer relativImage = mitk::ImageGenerator::GenerateGradientImage<float>(4,4,4,1);
     mitk::IOUtil::SaveImage(relativImage, "tempfile.nrrd");
-    try
-    {
-      mitk::IOUtil::LoadImage("tempfile.nrrd");
-      MITK_TEST_CONDITION(true, "Temporary image is in right place");
-      remove("tempfile.nrrd");
-    }
-    catch (mitk::Exception &e)
-    {
-      MITK_TEST_CONDITION(false, "Temporary image is in right place");
-    }
+    CPPUNIT_ASSERT_NO_THROW(mitk::IOUtil::LoadImage("tempfile.nrrd"));
+    remove("tempfile.nrrd");
+  }
+};
 
-    MITK_TEST_END();
-}
+MITK_TEST_SUITE_REGISTRATION(mitkIOUtil)

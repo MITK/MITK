04071b9d5e093d126ecbc834a71e6124e8f8985a
Keno Maerz <k.maerz@dkfz-heidelberg.de>
2013-10-28 14:17:27 +0100

    Fixed DelayFilter (Now inherits from NavigationDataToNavigationDataFilter)

--- Modules/IGT/IGTFilters/mitkDelayFilter.cpp
+++ Modules/IGT/IGTFilters/mitkDelayFilter.cpp
@@ -16,32 +16,31 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include "mitkDelayFilter.h"
 
-
-mitk::DelayFilter::DataObjectPointer mitk::DelayFilter::MakeOutput ( DataObjectPointerArraySizeType )
+/**mitk::DelayFilter::DataObjectPointer mitk::DelayFilter::MakeOutput ( DataObjectPointerArraySizeType )
 {
-    mitkThrow() << "Unsupported Operation. Please override method in sublclass if you need this functionality.";
+mitkThrow() << "Unsupported Operation. Please override method in sublclass if you need this functionality.";
 }
-
 
 mitk::DelayFilter::DataObjectPointer mitk::DelayFilter::MakeOutput( const DataObjectIdentifierType& )
 {
-    mitkThrow() << "Unsupported Operation. Please override method in sublclass if you need this functionality.";
+mitkThrow() << "Unsupported Operation. Please override method in sublclass if you need this functionality.";
 }
+*/
 
 void mitk::DelayFilter::GenerateData()
 {
-    //get each input and transfer the data
-    DataObjectPointerArray inputs = this->GetInputs(); //get all inputs
-    for (unsigned int index=0; index < inputs.size(); index++)
-    {
-      //get the needed variables
-      //const mitk::BaseData* input = this->GetInput(index);
-      //assert(input);
+  //get each input and transfer the data
+  DataObjectPointerArray inputs = this->GetInputs(); //get all inputs
+  for (unsigned int index=0; index < inputs.size(); index++)
+  {
+    //get the needed variables
+    //const mitk::BaseData* input = this->GetInput(index);
+    //assert(input);
 
-      //mitk::BaseData* output = this->GetOutput(index);
-      //assert(output);
+    //mitk::BaseData* output = this->GetOutput(index);
+    //assert(output);
 
-      // TODO: this should be delayed for this filter
-      //output->Graft(input); // copy all information from input to output
-    }
+    // TODO: this should be delayed for this filter
+    //output->Graft(input); // copy all information from input to output
+  }
 }

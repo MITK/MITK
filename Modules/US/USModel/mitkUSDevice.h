a79373c3026563148b9f5d53639d9a2043c473f5
Keno Maerz <k.maerz@dkfz-heidelberg.de>
2013-11-07 16:54:38 +0100

    Removed methods that were commented out

--- Modules/US/USModel/mitkUSDevice.h
+++ Modules/US/USModel/mitkUSDevice.h
@@ -191,44 +191,6 @@ namespace mitk {
     void UpdateServiceProperty(std::string key, double value);
     void UpdateServiceProperty(std::string key, bool value);
 
-    /**
-    *\brief return the output (output with id 0) of the filter
-    */
-    //Image* GetOutput(void);
-
-    /**
-    *\brief return the output with id idx of the filter
-    */
-    //Image* GetOutput(unsigned int idx);
-
-    /**
-    *\brief Graft the specified DataObject onto this ProcessObject's output.
-    *
-    * See itk::ImageSource::GraftNthOutput for details
-    */
-    //virtual void GraftNthOutput(unsigned int idx, itk::DataObject *graft);
-
-    /**
-    * \brief Graft the specified DataObject onto this ProcessObject's output.
-    *
-    * See itk::ImageSource::Graft Output for details
-    */
-    //virtual void GraftOutput(itk::DataObject *graft);
-
-    //      /**
-    //      * \brief Make a DataObject of the correct type to used as the specified output.
-    //      *
-    //      * This method is automatically called when DataObject::DisconnectPipeline()
-    //      * is called.  DataObject::DisconnectPipeline, disconnects a data object
-    //      * from being an output of its current source.  When the data object
-    //      * is disconnected, the ProcessObject needs to construct a replacement
-    //      * output data object so that the ProcessObject is in a valid state.
-    //      * Subclasses of USImageVideoSource that have outputs of different
-    //      * data types must overwrite this method so that proper output objects
-    //      * are created.
-    //      */
-    //      virtual DataObjectPointer MakeOutput(DataObjectPointerArraySizeType idx);
-
     //########### GETTER & SETTER ##################//
 
     /**

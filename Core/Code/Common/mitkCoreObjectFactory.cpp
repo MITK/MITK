015699f8e13b346bfb96b7df264c3e7aebea3810
Adrian Winterstein <a.winterstein@dkfz-heidelberg.de>
2013-11-27 15:08:42 +0100

    For loops replaced by range versions of the insert functions.

--- Core/Code/Common/mitkCoreObjectFactory.cpp
+++ Core/Code/Common/mitkCoreObjectFactory.cpp
@@ -417,35 +417,28 @@ mitk::CoreObjectFactoryBase::MultimapType mitk::CoreObjectFactory::GetSaveFileEx
   return m_SaveFileExtensionsMap;
 }
 
-mitk::CoreObjectFactory::FileWriterList mitk::CoreObjectFactory::GetFileWriters() {
+mitk::CoreObjectFactory::FileWriterList mitk::CoreObjectFactory::GetFileWriters()
+{
   FileWriterList allWriters = m_FileWriters;
   //sort to merge lists later on
   typedef std::set<mitk::FileWriterWithInformation::Pointer> FileWriterSet;
   FileWriterSet fileWritersSet;
 
-  for( FileWriterList::iterator iter = allWriters.begin();
-       iter != allWriters.end(); ++iter) {
-      fileWritersSet.insert(*iter);
-  }
+  fileWritersSet.insert(allWriters.begin(), allWriters.end());
 
   //collect all extra factories
   for (ExtraFactoriesContainer::iterator it = m_ExtraFactories.begin();
-       it != m_ExtraFactories.end(); it++ ) {
-      FileWriterList list2 = (*it)->GetFileWriters();
+       it != m_ExtraFactories.end(); it++ )
+  {
+    FileWriterList list2 = (*it)->GetFileWriters();
 
     //add them to the sorted set
-    for( FileWriterList::iterator iter = list2.begin();
-       iter != list2.end(); ++iter) {
-        fileWritersSet.insert(*iter);
-    }
+    fileWritersSet.insert(list2.begin(), list2.end());
   }
 
   //write back to allWriters to return a list
   allWriters.clear();
-  for (FileWriterSet::iterator iter = fileWritersSet.begin();
-       iter != fileWritersSet.end(); ++iter) {
-      allWriters.insert(allWriters.end(), *iter);
-  }
+  allWriters.insert(allWriters.end(), fileWritersSet.begin(), fileWritersSet.end());
 
   return allWriters;
 }

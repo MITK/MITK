267509073e7c3c615849d01a0bb9d9c6e8b59bf5
Caspar Goch <c.goch@dkfz-heidelberg.de>
2013-12-10 16:47:20 +0100

    Reduced epsilon for testing

--- Modules/DiffusionImaging/Connectomics/Testing/files.cmake
+++ Modules/DiffusionImaging/Connectomics/Testing/files.cmake
@@ -1,8 +1,8 @@
 set(MODULE_TESTS
   mitkConnectomicsNetworkTest.cpp
-  mitkConnectomicsStatisticsCalculatorTest.cpp
 )
 
 SET(MODULE_CUSTOM_TESTS
   mitkConnectomicsNetworkCreationTest.cpp
+  mitkConnectomicsStatisticsCalculatorTest.cpp
 )

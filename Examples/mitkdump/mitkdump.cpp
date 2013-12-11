6ec4b43ec5f6c4d2199a5e01af64dbd33c5fd87b
Daniel Maleike <d.maleike@mint-medical.de>
2013-12-11 15:23:09 +0100

    Use DICOMTag in mitkdump

--- Examples/mitkdump/mitkdump.cpp
+++ Examples/mitkdump/mitkdump.cpp
@@ -18,6 +18,8 @@
 #include "mitkDICOMTagBasedSorter.h"
 #include "mitkDICOMSortByTag.h"
 
+using mitk::DICOMTag;
+
 int main(int argc, char* argv[])
 {
   mitk::StringList inputFiles; // TODO
@@ -35,24 +37,24 @@ int main(int argc, char* argv[])
     mitk::DICOMTagBasedSorter::New();
 
   // all the things that split by tag in DicomSeriesReader
-  tagSorter->AddDistinguishingTag( std::make_pair(0x0028, 0x0010) ); // Number of Rows
-  tagSorter->AddDistinguishingTag( std::make_pair(0x0028, 0x0011) ); // Number of Columns
-  tagSorter->AddDistinguishingTag( std::make_pair(0x0028, 0x0030) ); // Pixel Spacing
-  tagSorter->AddDistinguishingTag( std::make_pair(0x0018, 0x1164) ); // Imager Pixel Spacing
-  tagSorter->AddDistinguishingTag( std::make_pair(0x0020, 0x0037) ); // Image Orientation (Patient) // TODO add tolerance parameter (l. 1572 of original code)
-  tagSorter->AddDistinguishingTag( std::make_pair(0x0020, 0x000e) ); // Series Instance UID
-  tagSorter->AddDistinguishingTag( std::make_pair(0x0018, 0x0050) ); // Slice Thickness
-  tagSorter->AddDistinguishingTag( std::make_pair(0x0028, 0x0008) ); // Number of Frames
-  tagSorter->AddDistinguishingTag( std::make_pair(0x0020, 0x0052) ); // Frame of Reference UID
+  tagSorter->AddDistinguishingTag( DICOMTag(0x0028, 0x0010) ); // Number of Rows
+  tagSorter->AddDistinguishingTag( DICOMTag(0x0028, 0x0011) ); // Number of Columns
+  tagSorter->AddDistinguishingTag( DICOMTag(0x0028, 0x0030) ); // Pixel Spacing
+  tagSorter->AddDistinguishingTag( DICOMTag(0x0018, 0x1164) ); // Imager Pixel Spacing
+  tagSorter->AddDistinguishingTag( DICOMTag(0x0020, 0x0037) ); // Image Orientation (Patient) // TODO add tolerance parameter (l. 1572 of original code)
+  tagSorter->AddDistinguishingTag( DICOMTag(0x0020, 0x000e) ); // Series Instance UID
+  tagSorter->AddDistinguishingTag( DICOMTag(0x0018, 0x0050) ); // Slice Thickness
+  tagSorter->AddDistinguishingTag( DICOMTag(0x0028, 0x0008) ); // Number of Frames
+  tagSorter->AddDistinguishingTag( DICOMTag(0x0020, 0x0052) ); // Frame of Reference UID
 
   // a sorter...
   // TODO ugly syntax, improve..
   mitk::DICOMSortCriterion::ConstPointer sorting =
-    mitk::DICOMSortByTag::New( std::make_pair(0x0020, 0x0013), // instance number
-      mitk::DICOMSortByTag::New( std::make_pair(0x0020, 0x0012), // aqcuisition number
-        mitk::DICOMSortByTag::New( std::make_pair(0x0008, 0x0032), // aqcuisition time
-          mitk::DICOMSortByTag::New( std::make_pair(0x0018, 0x1060), // trigger time
-            mitk::DICOMSortByTag::New( std::make_pair(0x0008, 0x0018) // SOP instance UID (last resort, not really meaningful but decides clearly)
+    mitk::DICOMSortByTag::New( DICOMTag(0x0020, 0x0013), // instance number
+      mitk::DICOMSortByTag::New( DICOMTag(0x0020, 0x0012), // aqcuisition number
+        mitk::DICOMSortByTag::New( DICOMTag(0x0008, 0x0032), // aqcuisition time
+          mitk::DICOMSortByTag::New( DICOMTag(0x0018, 0x1060), // trigger time
+            mitk::DICOMSortByTag::New( DICOMTag(0x0008, 0x0018) // SOP instance UID (last resort, not really meaningful but decides clearly)
             ).GetPointer()
           ).GetPointer()
         ).GetPointer()

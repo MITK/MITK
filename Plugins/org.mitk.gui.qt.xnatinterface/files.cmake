9e60570b42ae4c93133be271e057881bc17d20d3
Daniel Knorr <d.knorr@dkfz-heidelberg.de>
2014-01-13 12:06:57 +0100

    Some additional changes to the renamed classes. And deleted old classes.

--- Plugins/org.mitk.gui.qt.xnatinterface/files.cmake
+++ Plugins/org.mitk.gui.qt.xnatinterface/files.cmake
@@ -1,19 +1,27 @@
 set(SRC_CPP_FILES
-  
+
 )
 
 set(INTERNAL_CPP_FILES
   org_mitk_gui_qt_xnatinterface_Activator.cpp
-  XNATProjects.cpp
+  QmitkXnatEditor.cpp
+  QmitkXnatSimpleSearchView.cpp
+  QmitkXnatTreeBrowserView.cpp
+  QmitkXnatObjectEditorInput.cpp
 )
 
 set(UI_FILES
-  src/internal/XNATProjectsControls.ui
+  src/internal/QmitkXnatEditorControls.ui
+  src/internal/QmitkXnatSimpleSearchViewControls.ui
+  src/internal/QmitkXnatTreeBrowserViewControls.ui
 )
 
 set(MOC_H_FILES
   src/internal/org_mitk_gui_qt_xnatinterface_Activator.h
-  src/internal/XNATProjects.h
+  src/internal/QmitkXnatEditor.h
+  src/internal/QmitkXnatSimpleSearchView.h
+  src/internal/QmitkXnatTreeBrowserView.h
+  src/internal/QmitkXnatObjectEditorInput.h
 )
 
 # list of resource files which can be used by the plug-in
@@ -28,7 +36,7 @@ set(CACHED_RESOURCE_FILES
 # list of Qt .qrc files which contain additional resources
 # specific to this plugin
 set(QRC_FILES
-	src/internal/images.qrc
+  src/internal/images.qrc
 )
 
 set(CPP_FILES )
@@ -40,4 +48,3 @@ endforeach(file ${SRC_CPP_FILES})
 foreach(file ${INTERNAL_CPP_FILES})
   set(CPP_FILES ${CPP_FILES} src/internal/${file})
 endforeach(file ${INTERNAL_CPP_FILES})
-

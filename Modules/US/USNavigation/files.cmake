248d852a6fc9b983f01ade21cec71de263d1bad8
Adrian Winterstein <a.winterstein@dkfz-heidelberg.de>
2014-01-18 17:53:44 +0100

    Interactors moved to subdirectory.

--- Modules/US/USNavigation/files.cmake
+++ Modules/US/USNavigation/files.cmake
@@ -1,11 +1,12 @@
 SET(CPP_FILES
   mitkUSCombinedModality.cpp
-  mitkUSZonesInteractor.cpp
-  mitkUSPointMarkInteractor.cpp
 
   Filter/mitkNavigationDataPassThroughFilter.cpp
   Filter/mitkNeedleProjectionFilter.cpp
   Filter/mitkNodeDisplacementFilter.cpp
+
+  Interactors/mitkUSZonesInteractor.cpp
+  Interactors/mitkUSPointMarkInteractor.cpp
 )
 
 SET(RESOURCE_FILES

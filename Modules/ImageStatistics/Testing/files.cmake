6dddedb740135c0d7b87a15b9b805ae312903246
Daniel Maleike <d.maleike@mint-medical.de>
2014-02-07 14:28:41 +0100

    Fix test definition

--- Modules/ImageStatistics/Testing/files.cmake
+++ Modules/ImageStatistics/Testing/files.cmake
@@ -2,9 +2,9 @@ set(MODULE_TESTS
   mitkImageStatisticsCalculatorTest.cpp
   mitkPointSetStatisticsCalculatorTest.cpp
   mitkPointSetDifferenceStatisticsCalculatorTest.cpp
-  mitkMultiGaussianTest.cpp
 )
 
 set(MODULE_CUSTOM_TESTS
   mitkImageStatisticsHotspotTest.cpp
+#  mitkMultiGaussianTest.cpp # TODO: activate test to generate new test cases for mitkImageStatisticsHotspotTest
 )

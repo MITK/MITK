f05e7e7847e65130e5a137b2c3878989a984bd95
Daniel Maleike <d.maleike@mint-medical.de>
2014-02-10 18:01:36 +0100

    Fix position verification

--- Modules/ImageStatistics/Testing/mitkImageStatisticsHotspotTest.cpp
+++ Modules/ImageStatistics/Testing/mitkImageStatisticsHotspotTest.cpp
@@ -535,9 +535,9 @@ struct mitkImageStatisticsHotspotTestClass
 
   static void ValidateStatisticsItem(const std::string& label, const vnl_vector<int>& testvalue, const vnl_vector<int>& reference)
   {
-    double diffX = testvalue[0] - reference[0];
-    double diffY = testvalue[1] - reference[1];
-    double diffZ = testvalue[2] - reference[2];
+    double diffX = ::fabs(testvalue[0] - reference[0]);
+    double diffY = ::fabs(testvalue[1] - reference[1]);
+    double diffZ = ::fabs(testvalue[2] - reference[2]);
 
     std::stringstream testPosition;
     testPosition << testvalue[0] << "," << testvalue[1] << "," << testvalue[2];

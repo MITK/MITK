48e5cb0ea6ac9d19e904ada566ff997d8d4df443
Christoph Kolb <c.kolb@dkfz-heidelberg.de>
2014-02-24 15:34:27 +0100

    add test to check if the resulting segmentation is correct

--- Core/Code/Testing/mitkSurfaceToImageFilterTest.cpp
+++ Core/Code/Testing/mitkSurfaceToImageFilterTest.cpp
@@ -16,6 +16,8 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include <mitkTestingMacros.h>
 #include <mitkIOUtil.h>
+#include <mitkImagePixelReadAccessor.h>
+#include <mitkImageWriteAccessor.h>
 
 #include "mitkSurfaceToImageFilter.h"
 
@@ -26,13 +28,14 @@ See LICENSE.txt or http://www.mitk.org for details.
 class mitkSurfaceToImageFilterTestSuite : public mitk::TestFixture
 {
   CPPUNIT_TEST_SUITE(mitkSurfaceToImageFilterTestSuite);
-  MITK_TEST(testReadFile);
+  MITK_TEST(test3DSurfaceValidOutput);
+  MITK_TEST(test3DSurfaceCorrect);
   CPPUNIT_TEST_SUITE_END();
 
 private:
 
   /** Members used inside the different test methods. All members are initialized via setUp().*/
-  std::string m_SurfacePath;
+  mitk::Surface::Pointer m_Surface;
 
 public:
 
@@ -41,34 +44,64 @@ public:
    */
   void setUp()
   {
-    m_SurfacePath = GetTestDataFilePath("ball.stl");
+    m_Surface = mitk::IOUtil::LoadSurface(GetTestDataFilePath("ball.stl"));
   }
 
   void tearDown()
   {
   }
 
-  void test3DSurface()
+  void test3DSurfaceValidOutput()
   {
     mitk::SurfaceToImageFilter::Pointer surfaceToImageFilter = mitk::SurfaceToImageFilter::New();
 
-     //todo I don't know if this image is always needed. There is no documentation of the filter. Use git blame and ask the author.
-     mitk::Image::Pointer additionalInputImage = mitk::Image::New();
-     additionalInputImage->Initialize( mitk::MakeScalarPixelType<unsigned int>(), *m_SurfacePath->GetGeometry());
+    //todo I don't know if this image is always needed. There is no documentation of the filter. Use git blame and ask the author.
+    mitk::Image::Pointer additionalInputImage = mitk::Image::New();
+    additionalInputImage->Initialize( mitk::MakeScalarPixelType<unsigned int>(), *m_Surface->GetTimeGeometry());
 
-     //Arrange the filter
-     //The docu does not really tell if this is always needed. Could we skip SetImage in any case?
-     surfaceToImageFilter->MakeOutputBinaryOn();
-     surfaceToImageFilter->SetInput(m_SurfacePath);
-     surfaceToImageFilter->SetImage(additionalInputImage);
-     surfaceToImageFilter->Update();
+    //Arrange the filter
+    //The docu does not really tell if this is always needed. Could we skip SetImage in any case?
+    surfaceToImageFilter->MakeOutputBinaryOn();
+    surfaceToImageFilter->SetInput(m_Surface);
+    surfaceToImageFilter->SetImage(additionalInputImage);
+    surfaceToImageFilter->Update();
 
-     CPPUNIT_ASSERT_MESSAGE("SurfaceToImageFilter_AnyInputImageAndModeSetToBinary_ResultIsImageWithUCHARPixelType", surfaceToImageFilter->GetOutput()->GetPixelType().GetComponentType() == itk::ImageIOBase::UCHAR);
+    CPPUNIT_ASSERT_MESSAGE("SurfaceToImageFilter_AnyInputImageAndModeSetToBinary_ResultIsImageWithUCHARPixelType", surfaceToImageFilter->GetOutput()->GetPixelType().GetComponentType() == itk::ImageIOBase::UCHAR);
   }
 
-  void test4DSurface()
+  void test3DSurfaceCorrect()
   {
+    mitk::SurfaceToImageFilter::Pointer surfaceToImageFilter = mitk::SurfaceToImageFilter::New();
 
+    //todo I don't know if this image is always needed. There is no documentation of the filter. Use git blame and ask the author.
+    mitk::Image::Pointer additionalInputImage = mitk::Image::New();
+    additionalInputImage->Initialize( mitk::MakeScalarPixelType<unsigned int>(), *m_Surface->GetTimeGeometry());
+
+    //Arrange the filter
+    //The docu does not really tell if this is always needed. Could we skip SetImage in any case?
+    surfaceToImageFilter->MakeOutputBinaryOn();
+    surfaceToImageFilter->SetInput(m_Surface);
+    surfaceToImageFilter->SetImage(additionalInputImage);
+    surfaceToImageFilter->Update();
+
+    mitk::ImagePixelReadAccessor<unsigned char,3> outputReader(surfaceToImageFilter->GetOutput());
+    itk::Index<3> idx;
+    bool valuesCorrect = true;
+    //Values outside the ball should be 0
+    idx[0] =  0; idx[1] =  0, idx[2] =  0; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 0);
+    idx[0] =  0; idx[1] = 15, idx[2] = 15; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 0);
+    idx[0] = 15; idx[1] = 15, idx[2] =  0; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 0);
+    idx[0] = 15; idx[1] =  0, idx[2] = 15; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 0);
+    idx[0] =  4; idx[1] =  8, idx[2] = 23; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 0);
+    //Values inside the ball should be 1
+    idx[0] = 15; idx[1] = 15, idx[2] = 15; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 1);
+    idx[0] = 29; idx[1] = 15, idx[2] = 15; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 1);
+    idx[0] =  1; idx[1] = 15, idx[2] = 15; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 1);
+    idx[0] = 15; idx[1] = 15, idx[2] =  1; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 1);
+    idx[0] = 15; idx[1] =  1, idx[2] = 15; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 1);
+    idx[0] =  4; idx[1] =  9, idx[2] = 23; valuesCorrect = valuesCorrect && (outputReader.GetPixelByIndex(idx) == 1);
+
+    CPPUNIT_ASSERT_MESSAGE("SurfaceToImageFilter_BallSurfaceAsInput_OutputCorrect", valuesCorrect == true);
   }
 
 };

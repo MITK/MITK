3e07d16957aa036f17810c9c282ad9f330c30795
Markus Engel <m.engel@mint-medical.de>
2014-02-24 08:28:23 +0100

    fixed additional interaction bug in PlanarFigureInteractor

--- Modules/PlanarFigure/Interactions/mitkPlanarFigureInteractor.cpp
+++ Modules/PlanarFigure/Interactions/mitkPlanarFigureInteractor.cpp
@@ -270,7 +270,7 @@ bool mitk::PlanarFigureInteractor::AddPoint(StateMachineAction*, InteractionEven
   * In the second case we need to determine the proper insertion index. In the first case the index always has
   * to be -1 so that the point is appended to the end.
   *
-  * These changes are neccessary because of a mac os x specific issue: If a users draws a PlanarPolygon then the
+  * These changes are necessary because of a mac os x specific issue: If a users draws a PlanarPolygon then the
   * next point to be added moves according to the mouse position. If then the user left clicks in order to add
   * a point one would assume the last move position is identical to the left click position. This is actually the
   * case for windows and linux but somehow NOT for mac. Because of the insertion logic of a new point in the

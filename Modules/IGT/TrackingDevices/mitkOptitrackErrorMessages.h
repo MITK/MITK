a80d2077cfbf8423ea6b31e577fb9afc32750217
Alfred Franz <a.franz@dkfz-heidelberg.de>
2014-02-26 14:28:52 +0100

    COMP(#16504): Moved implementation of method to cpp file to avoid linux compiler errors

--- Modules/IGT/TrackingDevices/mitkOptitrackErrorMessages.h
+++ Modules/IGT/TrackingDevices/mitkOptitrackErrorMessages.h
@@ -31,35 +31,12 @@ See LICENSE.txt or http://www.mitk.org for details.
 #define OPTITRACK_FRAME_RATE 10
 #endif
 
-
-
 namespace mitk
 {
-/**
-  * \brief Helper function to get the error messages from Optitrack API
-  * \author E. Marinetto (emarinetto@hggm.es) Instituto de Investigación Sanitaria Gregorio Marañón, Madrid, Spain. & M. Noll (matthias.noll@igd.fraunhofer.de) Cognitive Computing & Medical Imaging | Fraunhofer IGD
-  */
-
-  static std::string GetOptitrackErrorMessage(int result)
-  {
-      std::string message = "";
-      switch(result)
-      {
-        case 0: message = "[Optitrack API] Successful Result"; break;
-        case 1: message = "[Optitrack API] File Not Found"; break;
-        case 2:  message = "[Optitrack API] Load Failed"; break;
-        case 3:    message = "[Optitrack API] Failed"; break;
-        case 8:  message = "[Optitrack API] Invalid File"; break;
-        case 9:  message = "[Optitrack API] Invalid Calibration File"; break;
-        case 10:  message = "[Optitrack API] Unable To Initialize"; break;
-        case 11:  message = "[Optitrack API] Invalid License"; break;
-        case 14:  message = "[Optitrack API] No Frames Available"; break;
-        case 100:  message = "Optitrack Tracking Tools is not installed in the computer"; break;
-        default: message = "[Optitrack API] Unknown error occured"; break;
-      }
-      return message;
-    }
+  /**
+   * \brief Helper function to get the error messages from Optitrack API
+   * \author E. Marinetto (emarinetto@hggm.es) Instituto de Investigación Sanitaria Gregorio Marañón, Madrid, Spain. & M. Noll (matthias.noll@igd.fraunhofer.de) Cognitive Computing & Medical Imaging | Fraunhofer IGD
+   */
+  static std::string GetOptitrackErrorMessage(int result);
 }
-
-
 #endif

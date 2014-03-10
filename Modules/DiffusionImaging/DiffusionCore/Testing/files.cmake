d93007702427696d9afbf40a3de8c2e159905af1
Moritz Petry <m.petry@dkfz-heidelberg.de>
2014-03-10 15:12:45 +0100

    Nonlocalmeans test as module test.

--- Modules/DiffusionImaging/DiffusionCore/Testing/files.cmake
+++ Modules/DiffusionImaging/DiffusionCore/Testing/files.cmake
@@ -1,12 +1,12 @@
 set(MODULE_TESTS
   mitkFactoryRegistrationTest.cpp
   mitkDiffusionImageEqualTest.cpp
+  mitkNonLocalMeansDenoisingTest.cpp
 )
 
 set(MODULE_CUSTOM_TESTS
   mitkPyramidImageRegistrationMethodTest.cpp
   mitkDWHeadMotionCorrectionTest.cpp
   mitkImageReconstructionTest.cpp
-  mitkNonLocalMeansDenoisingTest.cpp
 )
 

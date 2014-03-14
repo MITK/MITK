fc194a4c4aca4201e7fa165c06b9579133145d5a
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2014-03-14 01:05:34 +0100

    Build the MitkTestingHelper module even if BUILD_TESTING is OFF.

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -112,10 +112,6 @@ if(MITK_USE_QT)
   endif()
 endif()
 
-if(MITK_USE_BLUEBERRY)
-  set(MITK_USE_CppUnit 1)
-endif()
-
 if(MITK_USE_SOFA)
   set(MITK_USE_GLUT 1)
 endif()
@@ -147,7 +143,6 @@ if(BUILD_TESTING)
   if(EXTERNAL_MITK_DATA_DIR)
     set(MITK_DATA_DIR ${EXTERNAL_MITK_DATA_DIR})
   endif()
-  set(MITK_USE_CppUnit 1)
 endif()
 
 # Look for git early on, if needed

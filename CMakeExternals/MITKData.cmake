c7037baa27d31cbc97ada1964f6b4e4e5fa3307d
Peter Neher <p.neher@dkfz-heidelberg.de>
2014-04-10 09:09:25 +0200

    adjusted signal generation test and data to new situation

--- CMakeExternals/MITKData.cmake
+++ CMakeExternals/MITKData.cmake
@@ -12,7 +12,7 @@ set(proj_DEPENDENCIES)
 set(MITK-Data_DEPENDS ${proj})
 
 if(BUILD_TESTING)
-  set(revision_tag f31f5df7)
+  set(revision_tag c96a0e6f)
 #                  ^^^^^^^^  these are just to check correct length of hash part
 
   ExternalProject_Add(${proj}

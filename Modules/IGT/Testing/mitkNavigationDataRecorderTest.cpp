9a5697c701b67d77efd838efc91d0ffd85192665
Keno März <k.maerz@dkfz-heidelberg.de>
2014-04-30 14:12:24 +0200

    Removed unnecessary code and moved code not related to the recorder to the setup-method

--- Modules/IGT/Testing/mitkNavigationDataRecorderTest.cpp
+++ Modules/IGT/Testing/mitkNavigationDataRecorderTest.cpp
@@ -36,11 +36,19 @@ class mitkNavigationDataRecorderTestSuite : public mitk::TestFixture
   CPPUNIT_TEST_SUITE_END();
 
 private:
+  mitk::NavigationDataSet::Pointer m_NavigationDataSet;
+  mitk::NavigationDataSequentialPlayer::Pointer m_Player;
 
 public:
 
   void setUp()
   {
+    mitk::NavigationDataReaderXML::Pointer reader = mitk::NavigationDataReaderXML::New();
+    std::string path = GetTestDataFilePath("IGT-Data/RecordedNavigationData.xml");
+    m_NavigationDataSet = reader->Read(path);
+
+    m_Player = mitk::NavigationDataSequentialPlayer::New();
+    m_Player->SetNavigationDataSet(m_NavigationDataSet);
   }
 
   void tearDown()
@@ -52,35 +60,18 @@ public:
     // Aim is to read an xml into a pointset, play that set with a sequentialplayer, record it
     // again, write the result to xml , and compare the output
 
-    mitk::NavigationDataReaderXML::Pointer reader = mitk::NavigationDataReaderXML::New();
-    std::string path = GetTestDataFilePath("IGT-Data/RecordedNavigationData.xml");
-    mitk::NavigationDataSet::Pointer set = reader->Read(path);
-
-    mitk::NavigationDataSequentialPlayer::Pointer player = mitk::NavigationDataSequentialPlayer::New();
-    player->SetNavigationDataSet(set);
-
     mitk::NavigationDataRecorder::Pointer recorder = mitk::NavigationDataRecorder::New();
     recorder->SetStandardizeTime(false);
 
     // connect player to recorder
-    for (int i = 0; i < player->GetNumberOfIndexedOutputs(); i++)
-    {
-      recorder->SetInput(i, player->GetOutput(i));
-    }
+    recorder->ConnectTo(m_Player);
 
     recorder->StartRecording();
-    while (!player->IsAtEnd())
+    while (!m_Player->IsAtEnd())
     {
       recorder->Update();
-      player->GoToNextSnapshot();
+      m_Player->GoToNextSnapshot();
     }
-
-    mitk::NavigationDataSetWriterXML writer;
-
-    writer.Write("F://Build//MITK-Data//IGT-Data//NavigationDataSet2.xml", recorder->GetNavigationDataSet());
-
-    //MITK_TEST_CONDITION_REQUIRED(mitkNavigationDataRecorderTestClass::CompareFiles("F://Build//MITK-Data//IGT-Data//NavigationDataSet.xml",
-    //  "F://Build//MITK-Data//IGT-Data//NavigationDataSet2.xml"), "Asserting that played back file has been recorded correctly");
 
     recorder->StopRecording();
     MITK_TEST_CONDITION_REQUIRED(! recorder->GetRecording(), "Test if StopRecording is working");
@@ -89,38 +80,18 @@ public:
 
     //Reset Player
     //player = mitk::NavigationDataSequentialPlayer::New();
-    player->SetNavigationDataSet(set);
+    m_Player->SetNavigationDataSet(m_NavigationDataSet);
 
     // Check if Limiting recording works
     recorder->SetRecordCountLimit(100);
     recorder->StartRecording();
-    while (!player->IsAtEnd())
+    while (!m_Player->IsAtEnd())
     {
       recorder->Update();
-      player->GoToNextSnapshot();
+      m_Player->GoToNextSnapshot();
     }
 
     MITK_TEST_CONDITION_REQUIRED(recorder->GetNavigationDataSet()->Size() == 100, "Test if SetRecordCountLimit works as intended.");
-  }
-
-  static bool CompareFiles(std::string file1, std::string file2)
-  {
-    FILE* f1 = fopen (file1.c_str() , "r");
-    FILE* f2 = fopen (file2.c_str() , "r");
-    char buf1[10000];
-    char buf2[10000];
-
-    do {
-      size_t r1 = fread(buf1, 1, 10000, f1);
-      size_t r2 = fread(buf2, 1, 10000, f2);
-
-      if (r1 != r2 ||
-        memcmp(buf1, buf2, r1)) {
-          return false;  // Files are not equal
-      }
-    } while (!feof(f1) && !feof(f2));
-
-    return feof(f1) && feof(f2);
   }
 };
 MITK_TEST_SUITE_REGISTRATION(mitkNavigationDataRecorder)

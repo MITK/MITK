a8d0cdb79c2eeee51e28f11403b2466cebb3757b
Andreas Fetzer <a.fetzer@dkfz-heidelberg.de>
2014-05-19 09:15:05 +0200

    Writing contours back into the image is working if the contours lies either on the axial, sagittal or frontal plane.

--- Plugins/org.mitk.gui.qt.segmentation/src/internal/SegmentationUtilities/ContourModelToImage/QmitkContourModelToImageWidget.cpp
+++ Plugins/org.mitk.gui.qt.segmentation/src/internal/SegmentationUtilities/ContourModelToImage/QmitkContourModelToImageWidget.cpp
@@ -26,6 +26,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <mitkProgressBar.h>
 #include <mitkSegTool2D.h>
 #include <mitkSliceNavigationController.h>
+#include <mitkVtkImageOverwrite.h>
 
 #include <qmessagebox.h>
 
@@ -92,89 +93,85 @@ void QmitkContourModelToImageWidget::EnableButtons(bool enable)
 
 void QmitkContourModelToImageWidget::OnProcessPressed()
 {
-  //Disable Buttons during calculation and initialize Progressbar
-  this->EnableButtons(false);
-  mitk::ProgressBar::GetInstance()->AddStepsToDo(4);
-  mitk::ProgressBar::GetInstance()->Progress();
-
   QmitkDataSelectionWidget* dataSelectionWidget = m_Controls.dataSelectionWidget;
 
-  //create result image, get mask node and reference image
-  mitk::Image::Pointer resultImage(0);
   mitk::DataNode::Pointer segmentationNode = dataSelectionWidget->GetSelection(0);
+
+  if (segmentationNode.IsNull())
+  {
+    MITK_ERROR<<"Error writing contours into binary image! No image selected!";
+    return;
+  }
+
   mitk::Image::Pointer segmentationImage = static_cast<mitk::Image*>(segmentationNode->GetData());
 
-  //TODO Use one ContourModelSet!!!
-  //TODO Extract logic to somewhere
-  std::vector<mitk::ContourModel::Pointer> contourList;
+  if (segmentationImage.IsNull())
+  {
+    MITK_ERROR<<"Error writing contours into binary image! Invalid image data selected!";
+    return;
+  }
+
+  unsigned int timeStep = this->GetTimeNavigationController()->GetTime()->GetPos();
+
+  m_SegmentationImage = segmentationImage;
+  m_SegmentationImageGeometry = segmentationImage->GetTimeGeometry()->GetGeometryForTimeStep(timeStep);
+
+  mitk::ContourModelSet::Pointer contourSet;
   mitk::DataNode::Pointer contourNode = dataSelectionWidget->GetSelection(1);
-  mitk::ContourModel::Pointer contour = static_cast<mitk::ContourModel*>(contourNode->GetData());
+  mitk::ContourModel::Pointer contour = dynamic_cast<mitk::ContourModel*>(contourNode->GetData());
   if (contour.IsNotNull())
   {
-    contourList.push_back(contour);
+    contourSet = mitk::ContourModelSet::New();
+    contourSet->AddContourModel(contour);
   }
   else
   {
-    // TODO move to utils!
-    mitk::ContourModelSet::Pointer contourSet = static_cast<mitk::ContourModelSet*>(contourNode->GetData());
-    mitk::ContourModelSet::ContourModelSetIterator it = contourSet->Begin();
-    while (it != contourSet->End())
+    contourSet = static_cast<mitk::ContourModelSet*>(contourNode->GetData());
+    if (contourSet.IsNull())
     {
-      contourList.push_back(*it);
-      ++it;
+      MITK_ERROR<<"Error writing contours into binary image! Invalid contour data selected!";
+      return;
     }
   }
 
+  //Disable Buttons during calculation and initialize Progressbar
+  this->EnableButtons(false);
+  unsigned int num_contours = contourSet->GetContourModelList()->size();
+  mitk::ProgressBar::GetInstance()->AddStepsToDo(num_contours);
+  mitk::ProgressBar::GetInstance()->Progress();
+
   // Do actual filling
-  mitk::Geometry3D::Pointer segmentationGeometry = segmentationImage->GetGeometry();
-  mitk::PlaneGeometry::Pointer plane = mitk::PlaneGeometry::New();
-  mitk::Point3D point3D;
-  mitk::Vector3D normal;
-
-  mitk::ExtractSliceFilter::Pointer extractor = mitk::ExtractSliceFilter::New();
-  mitk::Image::Pointer slice;
   mitk::ContourModel::Pointer projectedContour;
-
-  unsigned int timeStep = this->GetTimeNavigationController()->GetTime()->GetPos();
-  int sliceIndex;
-  bool isFrontside = true;
-  bool isRotated = false;
-
-  foreach (mitk::ContourModel::Pointer contour, contourList)
+  mitk::ContourModelSet::ContourModelSetIterator it = contourSet->Begin();
+  mitk::ContourModelSet::ContourModelSetIterator end = contourSet->End();
+  while (it != end)
   {
+    mitk::ContourModel* contour = it->GetPointer();
+
     //Check Controls
-    // BUG save project with contours not working!!
-//    contour->GetGeometry()->Print(std::cout);
 
-    // 1. construct geometry -> extract slice
-    // TODO Detect contour orientation
-    point3D = contour->GetVertexAt(0)->Coordinates;
-    segmentationGeometry->WorldToIndex(point3D, point3D);
-    plane->InitializeStandardPlane(segmentationGeometry, mitk::PlaneGeometry::Axial, point3D[2], isFrontside, isRotated);
-    point3D = plane->GetOrigin();
-    normal = plane->GetNormal();
-    normal.Normalize();
-    point3D += normal * 0.5;//pixelspacing is 1, so half the spacing is 0.5
-    plane->SetOrigin(point3D);
+    // 1. Get corresponding image slice
+    mitk::Image::Pointer slice = this->GetSliceForContour(contour, timeStep);
 
-    // 2. fill into it
-    extractor->SetInput( segmentationImage );
-//    extractor->SetTimeStep( timeStep );
-    extractor->SetWorldGeometry( plane );
-    extractor->SetVtkOutputRequest(false);
-    extractor->SetResliceTransformByGeometry( segmentationImage->GetTimeGeometry()->GetGeometryForTimeStep( timeStep ) );
-
-    extractor->Modified();
-    extractor->Update();
-
-    slice = extractor->GetOutput();
+    // 2. Fill contour into slice
     projectedContour = mitk::ContourModelUtils::ProjectContourTo2DSlice(slice, contour, true, false);
     mitk::ContourModelUtils::FillContourInSlice(projectedContour, slice);
-    mitk::DataNode::Pointer filled = mitk::DataNode::New();
-    filled->SetName("FILLED");
-    filled->SetData(slice);
-    dataSelectionWidget->GetDataStorage()->Add(filled);
+//    mitk::DataNode::Pointer filled = mitk::DataNode::New();
+//    filled->SetName("FILLED");
+//    filled->SetData(slice);
+//    dataSelectionWidget->GetDataStorage()->Add(filled);
 
+    // 3. Write slice back into image volume
+    if (slice.IsNotNull())
+    {
+      this->WriteBackSlice(slice, timeStep);
+    }
+    else
+    {
+      MITK_ERROR<<"Contour to image failed!";
+    }
+    mitk::ProgressBar::GetInstance()->Progress();
+    ++it;
   }
 
   if(segmentationNode.IsNull() || contourNode.IsNull() )
@@ -185,42 +182,96 @@ void QmitkContourModelToImageWidget::OnProcessPressed()
     m_Controls.btnProcess->setEnabled(false);
     return;
   }
-
-  //Do Image-Masking
-  mitk::ProgressBar::GetInstance()->Progress();
-
-  // Do contour filling
-
-  mitk::ProgressBar::GetInstance()->Progress();
-
-  if( resultImage.IsNull() )
-  {
-    MITK_ERROR << "Contour To Image failed";
-    QMessageBox::information( this, "Contour To Image", "Failed to write contours into image. For more information please see logging window.", QMessageBox::Ok );
-    this->EnableButtons();
-    mitk::ProgressBar::GetInstance()->Progress(4);
-    return;
-  }
-
-  //Add result to data storage
-  this->AddToDataStorage(
-        dataSelectionWidget->GetDataStorage(),
-        resultImage,
-        dataSelectionWidget->GetSelection(0)->GetName() + "_" + dataSelectionWidget->GetSelection(1)->GetName(),
-        dataSelectionWidget->GetSelection(0));
-
   this->EnableButtons();
-
-  mitk::ProgressBar::GetInstance()->Progress();
 }
 
-
-void QmitkContourModelToImageWidget::AddToDataStorage(mitk::DataStorage::Pointer dataStorage, mitk::Image::Pointer segmentation, const std::string& name, mitk::DataNode::Pointer parent )
+mitk::Image::Pointer QmitkContourModelToImageWidget::GetSliceForContour(mitk::ContourModel::Pointer contour, unsigned int timestep)
 {
-  mitk::DataNode::Pointer dataNode = mitk::DataNode::New();
+  mitk::PlaneGeometry::Pointer plane = mitk::PlaneGeometry::New();
+  mitk::Point3D point3D, tempPoint;
+  mitk::Vector3D normal;
 
-  dataNode->SetName(name);
-  dataNode->SetData(segmentation);
+  mitk::ExtractSliceFilter::Pointer extractor = mitk::ExtractSliceFilter::New();
+  mitk::Image::Pointer slice;
 
-  dataStorage->Add(dataNode, parent);
+  int sliceIndex;
+  bool isFrontside = true;
+  bool isRotated = false;
+
+  point3D = contour->GetVertexAt(0)->Coordinates;
+  tempPoint = contour->GetVertexAt(contour->GetNumberOfVertices()*0.5)->Coordinates;
+  mitk::Vector3D vec = point3D - tempPoint;
+  vec.Normalize();
+//  MITK_INFO<<setprecision(20)<<"VEC: ["<<vec[0]<<","<<vec[1]<<","<<vec[2]<<"] MATRIX: "<<m_SegmentationImageGeometry->GetIndexToWorldTransform()->GetMatrix();
+  m_SegmentationImageGeometry->WorldToIndex(point3D, point3D);
+  mitk::PlaneGeometry::PlaneOrientation orientation;
+  if (mitk::Equal(vec[0], 0))
+  {
+    orientation = mitk::PlaneGeometry::Sagittal;
+    sliceIndex = point3D[0];
+  }
+  else if (mitk::Equal(vec[1], 0))
+  {
+    orientation = mitk::PlaneGeometry::Frontal;
+    sliceIndex = point3D[1];
+  }
+  else if (mitk::Equal(vec[2], 0))
+  {
+    orientation = mitk::PlaneGeometry::Axial;
+    sliceIndex = point3D[2];
+  }
+  else
+  {
+    // Maybe rotate geometry to extract slice?
+    MITK_ERROR<<"Cannot detect correct slice number! Only axial, sagittal and frontal oriented contours are supported!";
+    return 0;
+  }
+
+  plane->InitializeStandardPlane(m_SegmentationImageGeometry, orientation, sliceIndex, isFrontside, isRotated);
+  point3D = plane->GetOrigin();
+  normal = plane->GetNormal();
+  normal.Normalize();
+  point3D += normal * 0.5;//pixelspacing is 1, so half the spacing is 0.5
+  plane->SetOrigin(point3D);
+
+  m_CurrentPlane = plane;
+
+  // 2. fill into it
+  extractor->SetInput( m_SegmentationImage );
+//    extractor->SetTimeStep( timeStep );
+  extractor->SetWorldGeometry( plane );
+  extractor->SetVtkOutputRequest(false);
+  extractor->SetResliceTransformByGeometry( m_SegmentationImageGeometry );
+
+  extractor->Modified();
+  extractor->Update();
+
+  slice = extractor->GetOutput();
+  return slice;
+}
+
+void QmitkContourModelToImageWidget::WriteBackSlice(mitk::Image::Pointer slice, unsigned int timestep)
+{
+  //Make sure that for reslicing and overwriting the same alogrithm is used. We can specify the mode of the vtk reslicer
+  vtkSmartPointer<mitkVtkImageOverwrite> reslice = vtkSmartPointer<mitkVtkImageOverwrite>::New();
+
+  //Set the slice as 'input'
+  reslice->SetInputSlice(slice->GetVtkImageData());
+
+  //set overwrite mode to true to write back to the image volume
+  reslice->SetOverwriteMode(true);
+  reslice->Modified();
+
+  mitk::ExtractSliceFilter::Pointer extractor =  mitk::ExtractSliceFilter::New(reslice);
+  extractor->SetInput( m_SegmentationImage );
+  extractor->SetTimeStep( timestep );
+  extractor->SetWorldGeometry( m_CurrentPlane );
+  extractor->SetVtkOutputRequest(true);
+  extractor->SetResliceTransformByGeometry( m_SegmentationImage->GetGeometry( timestep ) );
+
+  extractor->Modified();
+  extractor->Update();
+
+  m_SegmentationImage->Modified();
+  m_SegmentationImage->GetVtkImageData()->Modified();
 }
--- Plugins/org.mitk.gui.qt.segmentation/src/internal/SegmentationUtilities/ContourModelToImage/QmitkContourModelToImageWidget.h
+++ Plugins/org.mitk.gui.qt.segmentation/src/internal/SegmentationUtilities/ContourModelToImage/QmitkContourModelToImageWidget.h
@@ -20,12 +20,12 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include "../QmitkSegmentationUtilityWidget.h"
 #include <ui_QmitkContourModelToImageWidgetControls.h>
 
-#include <mitkSurface.h>
-
 namespace mitk {
   class Image;
   class ContourModelSet;
   class ContourModel;
+  class Geometry3D;
+  class PlaneGeometry;
 }
 
 /*!
@@ -70,17 +70,23 @@ private:
   /** @brief Enable buttons if data selction is valid. */
   void EnableButtons(bool enable = true);
 
+  /** @brief Extracts the slice which correspond to the position of the contour*/
+  itk::SmartPointer<mitk::Image> GetSliceForContour(itk::SmartPointer<mitk::ContourModel> contour, unsigned int timestep);
+
+  /** @brief Extracts the slice which correspond to the position of the contour*/
+  void WriteBackSlice (itk::SmartPointer<mitk::Image> slice, unsigned int timestep);
+
   /** @brief Fills a mitk::ContourModel into a given segmentation image */
   itk::SmartPointer<mitk::Image> ContourModelToImage(itk::SmartPointer<mitk::Image> segmenationImage, itk::SmartPointer<mitk::ContourModel> contour );
 
   /** @brief Fills a whole ContourModelSet into a given segmentation image */
   itk::SmartPointer<mitk::Image> ContourModelSetToImage( itk::SmartPointer<mitk::Image> segmenationImage, itk::SmartPointer<mitk::ContourModelSet> contourSet );
 
-  /** @brief Adds a new data object to the DataStorage.*/
-  void AddToDataStorage(mitk::DataStorage::Pointer dataStorage, itk::SmartPointer<mitk::Image> segmentation,
-                        const std::string& name, mitk::DataNode::Pointer parent = NULL);
-
   Ui::QmitkContourModelToImageWidgetControls m_Controls;
+
+  itk::SmartPointer<mitk::Image> m_SegmentationImage;
+  mitk::Geometry3D::Pointer m_SegmentationImageGeometry;
+  itk::SmartPointer<mitk::PlaneGeometry> m_CurrentPlane;
 };
 
 #endif

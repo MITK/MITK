f58504a35304e45b7029b24c9270497924295152
Eric Heim <e.heim@dkfz-heidelberg.de>
2014-06-05 16:01:19 +0200

    step in the right direction

--- CMake/mitkMacroInstallPython.cmake
+++ CMake/mitkMacroInstallPython.cmake
@@ -4,25 +4,48 @@ macro(MITK_INSTALL_PYTHON)
 find_package(VTK REQUIRED)
 
 # Wrapping tools
-set(_VTK_PYTHON_TARGETS vtkWrappingPythonCore vtkWrappingTools)
+#set(_VTK_PYTHON_TARGETS vtkWrappingPythonCore vtkWrappingTools)
+set(_VTK_PYTHON_TARGETS )
 
 # find all vtk python wrapped targets
 foreach(_lib ${VTK_LIBRARIES})
   # exclude system libs
   if(${_lib} MATCHES "^vtk.+")
-    # use only python wrapped targets
+    # use only python wrapped modules
     if(TARGET ${_lib}PythonD)
-      list(APPEND _VTK_PYTHON_TARGETS ${_lib}PythonD)
+      list(APPEND _VTK_PYTHON_TARGETS ${_lib}Python)
     endif()
   endif()
 endforeach()
 
-foreach(_lib ${_VTK_PYTHON_TARGETS})
+foreach(_target ${_VTK_PYTHON_TARGETS})
+
+  # the python target library of the module
+  set(_lib ${_target}D)
+
   # get target properties
   get_target_property(_target_lib_debug  ${_lib} IMPORTED_LOCATION_DEBUG)
   get_target_property(_target_lib_release ${_lib} IMPORTED_LOCATION_RELEASE)
   get_filename_component(_target_filename_debug "${_target_lib_debug}" NAME)
   get_filename_component(_target_filename_release "${_target_lib_release}" NAME)
+
+  get_filename_component(_filepath_debug  ${_target_lib_debug} PATH)
+  get_filename_component(_filepath_release  ${_target_lib_release} PATH)
+
+  MESSAGE(${_lib})
+  MESSAGE(${_filepath_debug})
+
+   if(_filepath_debug)
+     set(_python_mod_file_debug ${_target}${CMAKE_SHARED_LIBRARY_SUFFIX})
+     install(FILES "${_filepath_debug}/${_python_mod_file_debug}"
+                   DESTINATION bin
+                   CONFIGURATIONS Debug)
+        install(CODE "file(RPATH_REMOVE
+                              FILE \"\${CMAKE_INSTALL_PREFIX}/bin/${_python_mod_file_debug}\")")
+        MESSAGE("${_filepath_debug}/${_python_mod_file_debug}")
+   endif()
+
+  #MESSAGE(${_target_lib_debug})
 
   if(_target_lib_debug)
     install(FILES ${_target_lib_debug}

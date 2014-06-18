a0e9df743e4aee7d6ca2dffcd7ffff25c8495313
Peter Neher <p.neher@dkfz-heidelberg.de>
2014-06-18 15:23:03 +0200

    using double in streamline tracking filter

--- Modules/DiffusionImaging/FiberTracking/Algorithms/itkStreamlineTrackingFilter.h
+++ Modules/DiffusionImaging/FiberTracking/Algorithms/itkStreamlineTrackingFilter.h
@@ -64,6 +64,7 @@ namespace itk{
     typedef typename Superclass::OutputImageType        OutputImageType;
     typedef typename Superclass::OutputImageRegionType  OutputImageRegionType;
     typedef itk::Image<unsigned char, 3>                ItkUcharImgType;
+    typedef itk::Image<double, 3>                       ItkDoubleImgType;
     typedef itk::Image<float, 3>                        ItkFloatImgType;
     typedef itk::Image< vnl_vector_fixed<double,3>, 3>  ItkPDImgType;
     typedef vtkSmartPointer< vtkPolyData >              FiberPolyDataType;
@@ -73,15 +74,15 @@ namespace itk{
     itkSetMacro( MaskImage, ItkUcharImgType::Pointer)   ///< Tracking is only performed inside of this mask image.
     itkSetMacro( FaImage, ItkFloatImgType::Pointer)     ///< Use this FA image instead of the automatically calculated one. Necessary for multi tensor tracking.
     itkSetMacro( SeedsPerVoxel, int)                    ///< One seed placed in the center of each voxel or multiple seeds randomly placed inside each voxel.
-    itkSetMacro( FaThreshold, float)                    ///< FA termination criterion.
-    itkSetMacro( StepSize, float)                       ///< Integration step size in mm
-    itkSetMacro( F, float )                             ///< Tensor deflection parameter f
-    itkSetMacro( G, float )                             ///< Tensor deflection parameter g
+    itkSetMacro( FaThreshold, double)                    ///< FA termination criterion.
+    itkSetMacro( StepSize, double)                       ///< Integration step size in mm
+    itkSetMacro( F, double )                             ///< Tensor deflection parameter f
+    itkSetMacro( G, double )                             ///< Tensor deflection parameter g
     itkSetMacro( Interpolate, bool )                    ///< Toggle between nearest neighbour (false) and trilinear interpolation (true)
-    itkSetMacro( MinTractLength, float )                ///< Shorter tracts are discarded.
-    itkGetMacro( MinTractLength, float )
-    itkSetMacro( MinCurvatureRadius, float )            ///< Tracking is stopped if curvature radius (in mm) is too small.
-    itkGetMacro( MinCurvatureRadius, float )
+    itkSetMacro( MinTractLength, double )                ///< Shorter tracts are discarded.
+    itkGetMacro( MinTractLength, double )
+    itkSetMacro( MinCurvatureRadius, double )            ///< Tracking is stopped if curvature radius (in mm) is too small.
+    itkGetMacro( MinCurvatureRadius, double )
     itkSetMacro( ResampleFibers, bool )                 ///< If enabled, the resulting fibers are resampled to feature point distances of 0.5*MinSpacing. This is recommendable for very short integration steps and many seeds. If disabled, the resulting fiber bundle might become very large.
 
   protected:
@@ -90,8 +91,8 @@ namespace itk{
     void PrintSelf(std::ostream& os, Indent indent) const;
 
     void CalculateNewPosition(itk::ContinuousIndex<double, 3>& pos, vnl_vector_fixed<double,3>& dir, typename InputImageType::IndexType& index);    ///< Calculate next integration step.
-    float FollowStreamline(itk::ContinuousIndex<double, 3> pos, int dirSign, vtkPoints* points, std::vector< vtkIdType >& ids, int imageIdx);       ///< Start streamline in one direction.
-    bool IsValidPosition(itk::ContinuousIndex<double, 3>& pos, typename InputImageType::IndexType& index, vnl_vector_fixed< float, 8 >& interpWeights, int imageIdx);   ///< Are we outside of the mask image? Is the FA too low?
+    double FollowStreamline(itk::ContinuousIndex<double, 3> pos, int dirSign, vtkPoints* points, std::vector< vtkIdType >& ids, int imageIdx);       ///< Start streamline in one direction.
+    bool IsValidPosition(itk::ContinuousIndex<double, 3>& pos, typename InputImageType::IndexType& index, vnl_vector_fixed< double, 8 >& interpWeights, int imageIdx);   ///< Are we outside of the mask image? Is the FA too low?
 
     double RoundToNearest(double num);
     void BeforeThreadedGenerateData();
@@ -104,25 +105,25 @@ namespace itk{
     vtkSmartPointer<vtkPoints>      m_Points;
     vtkSmartPointer<vtkCellArray>   m_Cells;
 
-    std::vector< ItkFloatImgType::Pointer >         m_EmaxImage;    ///< Stores largest eigenvalues per voxel (one for each tensor)
+    std::vector< ItkDoubleImgType::Pointer >         m_EmaxImage;    ///< Stores largest eigenvalues per voxel (one for each tensor)
     ItkFloatImgType::Pointer                        m_FaImage;      ///< FA image used to determine streamline termination.
     std::vector< ItkPDImgType::Pointer >            m_PdImage;      ///< Stores principal direction of each tensor in each voxel.
     std::vector< typename InputImageType::Pointer > m_InputImage;   ///< Input tensor images. For multi tensor tracking provide multiple tensor images.
 
     int     m_NumberOfInputs;
-    float   m_FaThreshold;
-    float   m_MinCurvatureRadius;
-    float   m_StepSize;
+    double   m_FaThreshold;
+    double   m_MinCurvatureRadius;
+    double   m_StepSize;
     int     m_MaxLength;
-    float   m_MinTractLength;
+    double   m_MinTractLength;
     int     m_SeedsPerVoxel;
-    float   m_F;
-    float   m_G;
+    double   m_F;
+    double   m_G;
     bool    m_Interpolate;
-    float   m_PointPistance;
+    double   m_PointPistance;
     bool    m_ResampleFibers;
     std::vector< int >          m_ImageSize;
-    std::vector< float >        m_ImageSpacing;
+    std::vector< double >        m_ImageSpacing;
     ItkUcharImgType::Pointer    m_SeedImage;
     ItkUcharImgType::Pointer    m_MaskImage;
 

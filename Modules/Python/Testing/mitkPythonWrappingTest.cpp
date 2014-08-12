c712c9b29de0793cff533862112937649778c996
Eric Heim <e.heim@dkfz-heidelberg.de>
2014-08-12 17:42:55 +0200

    using mitk equal method for images and surfaces

--- Modules/Python/Testing/mitkPythonWrappingTest.cpp
+++ Modules/Python/Testing/mitkPythonWrappingTest.cpp
@@ -43,101 +43,9 @@ See LICENSE.txt or http://www.mitk.org for details.
 namespace sitk = itk::simple;
 
 namespace mitk {
-  static bool Equal ( mitk::Image* img1, mitk::Image* img2 );
-  static bool Equal ( mitk::Surface* s1, Surface *s2 );
-  mitk::Image::Pointer SimpleItkToMitkImage( sitk::Image& sitkImage );
-  sitk::Image MitkToSimpleItkImage( mitk::Image* image );
+  static mitk::Image::Pointer SimpleItkToMitkImage( sitk::Image& sitkImage );
+  static sitk::Image MitkToSimpleItkImage( mitk::Image* image );
 }
-
-bool mitk::Equal ( mitk::Image* img1, mitk::Image* img2 )
-{
-  MITK_INFO << "hallo";
-  mitk::ImageReadAccessor ra1(img1);
-  mitk::ImageReadAccessor ra2(img2);
-  const unsigned int* img1Dims = img1->GetDimensions();
-
-  if ( img1->GetDimension() != img2->GetDimension() )
-  {
-    MITK_ERROR << "Dimension do not match";
-    return false;
-  }
-
-  if( img1Dims[0] != img2->GetDimensions()[0] ||
-      img1Dims[1] != img2->GetDimensions()[1] ||
-      img1Dims[2] != img2->GetDimensions()[2] )
-  {
-    MITK_ERROR << "Dimensions do not match";
-    return false;
-  }
-
-  if ( img1->GetPixelType().GetPixelType() != img2->GetPixelType().GetPixelType())
-  {
-    MITK_ERROR << "Pixeltypes do not match";
-    return false;
-  }
-
-  if ( img1->GetGeometry()->GetSpacing()[0] != img2->GetGeometry()->GetSpacing()[0] ||
-       img1->GetGeometry()->GetSpacing()[1] != img2->GetGeometry()->GetSpacing()[1] ||
-       img1->GetGeometry()->GetSpacing()[2] != img2->GetGeometry()->GetSpacing()[2] )
-  {
-    MITK_ERROR << "Spacing does not match";
-    return false;
-  }
-
-  if ( img1->GetGeometry()->GetOrigin()[0] != img2->GetGeometry()->GetOrigin()[0] ||
-       img1->GetGeometry()->GetOrigin()[1] != img2->GetGeometry()->GetOrigin()[1] ||
-       img1->GetGeometry()->GetOrigin()[2] != img2->GetGeometry()->GetOrigin()[2] )
-  {
-    MITK_ERROR << "Origin does not match";
-    return false;
-  }
-
-  size_t size = img1Dims[0] * img1Dims[1] * img1Dims[2] * img1->GetPixelType().GetSize();
-
-  // bytewise compare the image
-  for ( size_t i = 0; i < size; ++i )
-  {
-    if ( ((char*)ra1.GetData())[i] != ((char*)ra2.GetData())[i] ) {
-        MITK_INFO << "Byte does not match";
-        return false;
-    }
-  }
-
-  return true;
-}
-
-bool mitk::Equal( mitk::Surface* s1, mitk::Surface* s2 )
-{
-  vtkPolyData* poly1 = s1->GetVtkPolyData();
-  vtkPolyData* poly2 = s2->GetVtkPolyData();
-  double p1[3] = { 0.0,0.0,0.0 };
-  double p2[3] = { 0.0,0.0,0.0 };
-
-  if ( poly1->GetNumberOfPoints() != poly2->GetNumberOfPoints() ) {
-      MITK_WARN << "Size does not match : s1 = " << poly1->GetNumberOfPoints()
-                << ", s2 = " << poly2->GetNumberOfPoints();
-      return false;
-  }
-
-  for ( vtkIdType i = 0; i < poly1->GetNumberOfPoints(); ++i )
-  {
-    poly1->GetPoint(i,p1);
-    poly2->GetPoint(i,p2);
-
-    if  ( !mitk::Equal(p1[0],p2[0]) ||
-          !mitk::Equal(p1[1],p2[1]) ||
-          !mitk::Equal(p1[2],p2[2]) )
-    {
-      MITK_WARN << "Points do not match: i: "<< i << "p1("
-                << p1[0] << "," << p1[1] << "," << p1[2] << "), p2("
-                << p2[0] << "," << p2[1] << "," << p2[2] << ")" ;
-      return false;
-    }
-  }
-
-  return true;
-}
-
 
 sitk::Image mitk::MitkToSimpleItkImage( mitk::Image* image )
 {
@@ -319,7 +227,7 @@ public:
     mitk::Image::Pointer pythonImage = m_PythonService->CopySimpleItkImageFromPython(varName);
 
     CPPUNIT_ASSERT_MESSAGE( "Compare if images are equal after transfer.",
-                            mitk::Equal(pythonImage,m_Image) );
+                            mitk::Equal(*pythonImage.GetPointer(),*m_Image.GetPointer(), mitk::eps,true) );
   }
 
   void testCVImageTransfer()
@@ -348,7 +256,7 @@ public:
 
     mitk::Surface::Pointer pythonSurface = m_PythonService->CopyVtkPolyDataFromPython(varName);
 
-    CPPUNIT_ASSERT_MESSAGE( "Compare if surfaces are equal after transfer.", mitk::Equal(pythonSurface,m_Surface) );
+    CPPUNIT_ASSERT_MESSAGE( "Compare if surfaces are equal after transfer.", mitk::Equal(*pythonSurface.GetPointer(),*m_Surface.GetPointer(),mitk::eps,true) );
   }
 
   void testVtkCreateConePythonSnippet()
@@ -369,7 +277,7 @@ public:
 
     mitk::Surface::Pointer pythonSurface = m_PythonService->CopyVtkPolyDataFromPython("cone");
 
-    CPPUNIT_ASSERT_MESSAGE( "Compare if cones are equal.", mitk::Equal(pythonSurface, mitkSurface) );
+    CPPUNIT_ASSERT_MESSAGE( "Compare if cones are equal.", mitk::Equal(*pythonSurface.GetPointer(), *mitkSurface.GetPointer(), mitk::eps,true) );
   }
 
   void testVtkDecimateProPythonSnippet()
@@ -393,7 +301,7 @@ public:
 
     mitk::Surface::Pointer pythonSurface = m_PythonService->CopyVtkPolyDataFromPython("mitkSurface_new");
 
-    CPPUNIT_ASSERT_MESSAGE( "Compare if surfaces are equal.", mitk::Equal(pythonSurface, mitkSurface) );
+    CPPUNIT_ASSERT_MESSAGE( "Compare if surfaces are equal.", mitk::Equal(*pythonSurface.GetPointer(), *mitkSurface.GetPointer(), mitk::eps,true) );
   }
 
   void testSimpleITKMedianFilterSnippet()
@@ -414,7 +322,7 @@ public:
 
     mitk::Image::Pointer pythonImage = m_PythonService->CopySimpleItkImageFromPython("mitkImage_new");
 
-    CPPUNIT_ASSERT_MESSAGE( "Compare if images are equal.", mitk::Equal(pythonImage, mitkImage) );
+    CPPUNIT_ASSERT_MESSAGE( "Compare if images are equal.", mitk::Equal(*pythonImage.GetPointer(), *mitkImage.GetPointer(),mitk::eps,true) );
   }
 
   //TODO opencv median filter, add cpp test code

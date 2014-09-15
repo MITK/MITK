bc4fdd02bfd6520be0d221a0942a2ca69fc10dad
Eric Heim <e.heim@dkfz-heidelberg.de>
2014-09-15 15:56:24 +0200

    COMP: added patch to build 32 bit python

--- CMakeExternals/PatchPython.cmake
+++ CMakeExternals/PatchPython.cmake
@@ -2,7 +2,9 @@
 #  Windows python 2.7.3 patches for the CMake build system
 #  https://github.com/davidsansome/python-cmake-buildsystem/tree/master/cmake/patches-win32
 #
-set(in ${CMAKE_CURRENT_LIST_DIR}/msvc9compiler.py.patched)
-set(out ${PYTHON_SOURCE_DIR}/Lib/distutils/msvc9compiler.py)
+if(WIN32)
+  set(in ${CMAKE_CURRENT_LIST_DIR}/msvc9compiler.py.patched)
+  set(out ${PYTHON_SOURCE_DIR}/Lib/distutils/msvc9compiler.py)
 
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${in} ${out})
+  execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${in} ${out})
+endif()

84b4464d717ba3fe40a62495b2e63ca99abe1b62
Taylor Braun-Jones <taylor.braun-jones@ge.com>
2014-09-22 09:21:40 -0400

    Fix missing and incorrect copyright headers in QmlItems
    Signed-off-by: Taylor Braun-Jones <taylor.braun-jones@ge.com>

--- Modules/QmlItems/QmlMitkBigRenderLock.cpp
+++ Modules/QmlItems/QmlMitkBigRenderLock.cpp
@@ -1,3 +1,19 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center,
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+
 #include "QmlMitkBigRenderLock.h"
 
 #include "QVTKQuickItem.h"

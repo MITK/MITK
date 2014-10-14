3c6a5c6c8011f0166a0b1d6c99172b1c0bee6704
MartinKlemm <m.klemm@dkfz-heidelberg.de>
2014-10-14 17:13:06 +0200

    Fixed compile error by setting an include for fstream

--- Modules/IGT/IO/mitkNavigationDataReaderCSV.cpp
+++ Modules/IGT/IO/mitkNavigationDataReaderCSV.cpp
@@ -15,6 +15,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 ===================================================================*/
 
 #include "mitkNavigationDataReaderCSV.h"
+#include <fstream>
 
 mitk::NavigationDataReaderCSV::NavigationDataReaderCSV()
 {

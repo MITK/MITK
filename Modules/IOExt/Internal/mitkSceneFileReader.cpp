5c644958f6ad6484063035dda5f13f12ec4d9110
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2014-10-16 10:22:19 +0200

    Do not call std find on empty containers anymore and fixed accessing of already released object.

--- Modules/IOExt/Internal/mitkSceneFileReader.cpp
+++ Modules/IOExt/Internal/mitkSceneFileReader.cpp
@@ -38,22 +38,30 @@ SceneFileReader::SceneFileReader()
 
 DataStorage::SetOfObjects::Pointer SceneFileReader::Read(DataStorage& ds)
 {
-  const DataStorage::SetOfObjects::STLContainerType& oldNodes = ds.GetAll()->CastToSTLConstContainer();
+  //const DataStorage::SetOfObjects::STLContainerType& oldNodes = ds.GetAll()->CastToSTLConstContainer();
+  DataStorage::SetOfObjects::ConstPointer oldNodes = ds.GetAll();
   SceneIO::Pointer sceneIO = SceneIO::New();
   sceneIO->LoadScene(this->GetLocalFileName(), &ds, false);
   DataStorage::SetOfObjects::ConstPointer newNodes = ds.GetAll();
 
   // Compute the difference
   DataStorage::SetOfObjects::Pointer result = DataStorage::SetOfObjects::New();
+
   unsigned int index = 0;
   for (DataStorage::SetOfObjects::ConstIterator iter = newNodes->Begin(),
-       iterEnd = newNodes->End(); iter != iterEnd; ++iter)
+        iterEnd = newNodes->End(); iter != iterEnd; ++iter)
   {
-    if (std::find(oldNodes.begin(), oldNodes.end(), iter.Value()) == oldNodes.end())
+    if (!oldNodes->empty())
+    {
+      if (std::find(oldNodes->begin(), oldNodes->end(), iter.Value()) == oldNodes->end())
+        result->InsertElement(index++, iter.Value());
+    }
+    else
     {
       result->InsertElement(index++, iter.Value());
     }
   }
+
   return result;
 }
 

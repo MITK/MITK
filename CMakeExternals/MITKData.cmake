3ef29f5d10ea32b1499ca2b46f455a091b8848d0
Christian Weber <ch.weber@dkfz-heidelberg.de>
2014-10-22 15:21:17 +0200

    set revision tag

--- CMakeExternals/MITKData.cmake
+++ CMakeExternals/MITKData.cmake
@@ -13,7 +13,7 @@ set(MITK-Data_DEPENDS ${proj})
 
 if(BUILD_TESTING)
 
-  set(revision_tag d96c6dab)
+  set(revision_tag a6f397a4)
 #                  ^^^^^^^^  these are just to check correct length of hash part
 
   ExternalProject_Add(${proj}

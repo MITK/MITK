3e6bbd88bc4bb13afc94ca86d5e238f63a52df1e
Christian Weber <ch.weber@dkfz-heidelberg.de>
2014-10-22 15:24:03 +0200

    correct tag

--- CMakeExternals/MITKData.cmake
+++ CMakeExternals/MITKData.cmake
@@ -13,7 +13,7 @@ set(MITK-Data_DEPENDS ${proj})
 
 if(BUILD_TESTING)
 
-  set(revision_tag a6f397a4)
+  set(revision_tag ef63f005) # first 8 characters of hash-tag
 #                  ^^^^^^^^  these are just to check correct length of hash part
 
   ExternalProject_Add(${proj}

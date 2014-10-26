c6df3291abb1e1d893a444598a6a2579221628ed
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2014-10-27 00:38:17 +0100

    Made examples Qt5 compatible.

--- Examples/Plugins/org.mitk.example.gui.multipleperspectives/src/internal/org_mitk_example_gui_multipleperspectives_Activator.cpp
+++ Examples/Plugins/org.mitk.example.gui.multipleperspectives/src/internal/org_mitk_example_gui_multipleperspectives_Activator.cpp
@@ -39,4 +39,6 @@ void org_mitk_example_gui_multipleperspectives_Activator::stop(ctkPluginContext*
   Q_UNUSED(context)
 }
 
-Q_EXPORT_PLUGIN2(org_mitk_example_gui_multipleperspectives, org_mitk_example_gui_multipleperspectives_Activator)
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+  Q_EXPORT_PLUGIN2(org_mitk_example_gui_multipleperspectives, org_mitk_example_gui_multipleperspectives_Activator)
+#endif

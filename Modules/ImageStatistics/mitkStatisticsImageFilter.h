c3e83f75b9f7e3e44860aea1df1934d67469d1f4
Thorge Mueller <tmueller@mint-medical.de>
2014-10-29 16:00:38 +0100

    Switch the header name form mitkLabel... to mitkExtendedLabel. On top of that we add some comments and style the code

--- Modules/ImageStatistics/mitkStatisticsImageFilter.h
+++ Modules/ImageStatistics/mitkStatisticsImageFilter.h
@@ -1,55 +1,50 @@
 /*=========================================================================
- *
- *  Copyright Insight Software Consortium
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0.txt
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *=========================================================================*/
+*
+*  Copyright Insight Software Consortium
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
 #ifndef __mitkExtendedStatisticsImageFilter_h
 #define __mitkExtendedStatisticsImageFilter_h
 
 #include "itkStatisticsImageFilter.h"
-
 #include "itkDataObject.h"
-
 #include "ImageStatisticsExports.h"
-
 #include "mitkCommon.h"
 
 namespace itk
 {
-/**
-* \class ExtendedStatisticsImageFilter
-* \brief
-*
-*/
-template< class TInputImage >
-class ImageStatistics_EXPORT ExtendedStatisticsImageFilter : public StatisticsImageFilter< TInputImage >
-{
-public:
-  /** Standard Self typedef */
-  typedef ExtendedStatisticsImageFilter                    Self;
-  typedef StatisticsImageFilter< TInputImage >             Superclass;
-  typedef SmartPointer< Self >                             Pointer;
-  typedef SmartPointer< const Self >                       ConstPointer;
+  /**
+  * \class ExtendedStatisticsImageFilter
+  * \brief
+  *
+  */
+  template< class TInputImage >
+  class ImageStatistics_EXPORT ExtendedStatisticsImageFilter : public StatisticsImageFilter< TInputImage >
+  {
+  public:
+    /** Standard Self typedef */
+    typedef ExtendedStatisticsImageFilter                    Self;
+    typedef StatisticsImageFilter< TInputImage >             Superclass;
+    typedef SmartPointer< Self >                             Pointer;
+    typedef SmartPointer< const Self >                       ConstPointer;
 
-  /** Method for creation through the object factory. */
-  itkNewMacro( Self );
+    /** Method for creation through the object factory. */
+    itkNewMacro( Self );
 
-  /** Runtime information support. */
-  itkTypeMacro(ExtendedStatisticsImageFilter, StatisticsImageFilter);
-
-  typedef typename Superclass::DataObjectPointer DataObjectPointer;
+    /** Runtime information support. */
+    itkTypeMacro(ExtendedStatisticsImageFilter, StatisticsImageFilter);
 
   /** Return the computed Skewness. */
   RealType GetSkewness() const
@@ -58,8 +53,14 @@ public:
   }
   RealObjectType* GetSkewnessOutput();
 
-  const RealObjectType* GetSkewnessOutput() const;
+    /** Return the computed Skewness. */
+    RealType GetSkewness() const
+    {
+      return this->GetSkewnessOutput()->Get();
+    }
+    RealObjectType* GetSkewnessOutput();
 
+    const RealObjectType* GetSkewnessOutput() const;
 
   /** Return the computed Kurtosis. */
   RealType GetKurtosis() const
@@ -67,39 +68,43 @@ public:
     return this->GetKurtosisOutput()->Get();
   }
 
-  RealObjectType* GetKurtosisOutput();
+    /** Return the computed Kurtosis. */
+    RealType GetKurtosis() const
+    {
+      return this->GetKurtosisOutput()->Get();
+    }
 
-  const RealObjectType* GetKurtosisOutput() const;
+    RealObjectType* GetKurtosisOutput();
 
   //using Superclass::MakeOutput;
 
   virtual DataObject::Pointer MakeOutput( ProcessObject::DataObjectPointerArraySizeType idx );
 
-  /* Compute the Skewness Curtosis*/
-  void computeTheSkewnessAndCurtosis();
+    //using Superclass::MakeOutput;
 
 
+    /* Compute the Skewness and Kurtosis with the mean value and sigma of the threades generate data method of the base class*/
+    void computeTheSkewnessAndCurtosis();
 
-protected:
+  protected:
 
-  ExtendedStatisticsImageFilter();
+    ExtendedStatisticsImageFilter();
 
-  virtual ~ExtendedStatisticsImageFilter(){};
+    virtual ~ExtendedStatisticsImageFilter(){};
 
-  void AfterThreadedGenerateData();
+    void AfterThreadedGenerateData();
+
+  private:
+
+    //  Array< RealType >       m_ThreadSum;
 
 
-private:
-
-//  Array< RealType >       m_ThreadSum;
-
-
-}; // end of class
+  }; // end of class
 
 } // end namespace itk
 
 #ifndef ITK_MANUAL_INSTANTIATION
-  #include "mitkStatisticsImageFilter.hxx"
+#include "mitkExtendedStatisticsImageFilter.hxx"
 #endif
 
 #endif

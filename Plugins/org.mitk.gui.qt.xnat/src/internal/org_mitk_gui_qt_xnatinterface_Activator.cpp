f50dcdfcbc7abacc27066e66fe6f90541b35a89b
Daniel Knorr <d.knorr@dkfz-heidelberg.de>
2014-10-30 13:55:07 +0100

    COMP: Fixed Module init

--- Plugins/org.mitk.gui.qt.xnat/src/internal/org_mitk_gui_qt_xnatinterface_Activator.cpp
+++ Plugins/org.mitk.gui.qt.xnat/src/internal/org_mitk_gui_qt_xnatinterface_Activator.cpp
@@ -25,7 +25,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <usGetModuleContext.h>
 #include <usModuleInitialization.h>
 
-US_INITIALIZE_MODULE("MitkXNAT", "MitkXNAT")
+US_INITIALIZE_MODULE
 
 namespace mitk {
 

61f549645348e272f21bfdd77b3735beb3258025
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2014-11-26 22:11:44 +0100

    Fixed Qt help check for Workbench releases.

--- CMake/mitkBuildConfigurationWorkbenchRelease.cmake
+++ CMake/mitkBuildConfigurationWorkbenchRelease.cmake
@@ -3,9 +3,4 @@ include(${CMAKE_CURRENT_LIST_DIR}/mitkBuildConfigurationDefault.cmake)
 set(MITK_VTK_DEBUG_LEAKS OFF CACHE BOOL "Enable VTK Debug Leaks" FORCE)
 
 # Ensure that the in-application help can be build
-if(NOT DOXYGEN_EXECUTABLE)
-  message(FATAL_ERROR "DOXYGEN_EXECUTABLE is not set")
-endif()
-if(NOT BLUEBERRY_USE_QT_HELP)
-  message(FATAL_ERROR "BLUEBERRY_USE_QT_HELP is required to be set to ON")
-endif()
+set(BLUEBERRY_QT_HELP_REQUIRED ON CACHE BOOL "Required Qt help documentation in plug-ins" FORCE)

22cbdacc492516885b7942e08c1e7e69993f07cf
Nico Riecker <n.riecker@dkfz-heidelberg.de>
2014-12-05 14:11:36 +0100

    added some methods to the filter for multiple inputs added a new test for the multiple inputs and outputs had to add a graft method for mitkUnstructuredGrid

--- Modules/AlgorithmsExt/Testing/mitkUnstructuredGridToUnstructuredGridFilterTest.cpp
+++ Modules/AlgorithmsExt/Testing/mitkUnstructuredGridToUnstructuredGridFilterTest.cpp
@@ -28,22 +28,27 @@ See LICENSE.txt or http://www.mitk.org for details.
 class mitkUnstructuredGridToUnstructuredGridFilterTestSuite : public mitk::TestFixture
 {
   CPPUNIT_TEST_SUITE(mitkUnstructuredGridToUnstructuredGridFilterTestSuite);
-  MITK_TEST(testImageToUnstructuredGridFilterInitialization);
+  MITK_TEST(testUnstructuredGridToUnstructuredGridFilterInitialization);
   MITK_TEST(testInput);
+  MITK_TEST(testMultipleInputs);
   MITK_TEST(testUnstructuredGridGeneration);
   CPPUNIT_TEST_SUITE_END();
 
 private:
 
   mitk::UnstructuredGrid::Pointer m_UnstructuredGrid;
+  mitk::UnstructuredGrid::Pointer m_2ndUnstructuredGrid;
 
 public:
 
   void setUp()
   {
     m_UnstructuredGrid = mitk::UnstructuredGrid::New();
+    m_2ndUnstructuredGrid = mitk::UnstructuredGrid::New();
     vtkSmartPointer<vtkUnstructuredGrid> vtkGrid = vtkSmartPointer<vtkUnstructuredGrid>::New();
+    vtkSmartPointer<vtkUnstructuredGrid> vtkGrid2 = vtkSmartPointer<vtkUnstructuredGrid>::New();
     vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+    vtkSmartPointer<vtkPoints> points2 = vtkSmartPointer<vtkPoints>::New();
 
     for(int i=0; i<3; i++)
     {
@@ -57,15 +62,18 @@ public:
           point[2]=k;
 
           points->InsertNextPoint(point[0],point[1],point[2]);
+          points2->InsertNextPoint(point[0]+5,point[1]+5,point[2]+5);
         }
       }
     }
 
     vtkGrid->SetPoints(points);
+    vtkGrid2->SetPoints(points2);
     m_UnstructuredGrid->SetVtkUnstructuredGrid(vtkGrid);
+    m_2ndUnstructuredGrid->SetVtkUnstructuredGrid(vtkGrid2);
   }
 
-  void testImageToUnstructuredGridFilterInitialization()
+  void testUnstructuredGridToUnstructuredGridFilterInitialization()
   {
     mitk::UnstructuredGridToUnstructuredGridFilter::Pointer testFilter = mitk::UnstructuredGridToUnstructuredGridFilter::New();
     CPPUNIT_ASSERT_MESSAGE("Testing instantiation of test object", testFilter.IsNotNull());
@@ -78,12 +86,21 @@ public:
     CPPUNIT_ASSERT_MESSAGE("Testing set / get input!", testFilter->GetInput() == m_UnstructuredGrid);
   }
 
+  void testMultipleInputs()
+  {
+    mitk::UnstructuredGridToUnstructuredGridFilter::Pointer testFilter = mitk::UnstructuredGridToUnstructuredGridFilter::New();
+    testFilter->SetInput(0, m_UnstructuredGrid);
+    testFilter->SetInput(1, m_2ndUnstructuredGrid);
+    CPPUNIT_ASSERT_MESSAGE("Testing first input!", testFilter->GetInput(0) == m_UnstructuredGrid);
+    CPPUNIT_ASSERT_MESSAGE("Testing second input!", testFilter->GetInput(1) == m_2ndUnstructuredGrid);
+  }
+
   void testUnstructuredGridGeneration()
   {
     mitk::UnstructuredGridToUnstructuredGridFilter::Pointer testFilter = mitk::UnstructuredGridToUnstructuredGridFilter::New();
     testFilter->SetInput(m_UnstructuredGrid);
     testFilter->Update();
-    CPPUNIT_ASSERT_MESSAGE("Testing surface generation!", testFilter->GetOutput() != NULL);
+    CPPUNIT_ASSERT_MESSAGE("Testing unstructured grid generation!", testFilter->GetOutput() != NULL);
   }
 
 };

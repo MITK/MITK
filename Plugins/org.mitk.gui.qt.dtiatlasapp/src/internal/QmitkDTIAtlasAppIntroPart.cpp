5dc6f4297330a5b0b5d16bf64394f97d1aaeb76d
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2014-12-06 02:00:16 +0100

    Revert "COMP: Replaced DataNodeFactory with IOUtils."
    This reverts commit c59263d6cad22d79a63be5da78422ded8b9f7164.

--- Plugins/org.mitk.gui.qt.dtiatlasapp/src/internal/QmitkDTIAtlasAppIntroPart.cpp
+++ Plugins/org.mitk.gui.qt.dtiatlasapp/src/internal/QmitkDTIAtlasAppIntroPart.cpp
@@ -53,7 +53,6 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include "mitkProgressBar.h"
 #include "mitkNodePredicateNot.h"
 #include "mitkNodePredicateProperty.h"
-#include <mitkIOUtil.h>
 
 QmitkDTIAtlasAppIntroPart::QmitkDTIAtlasAppIntroPart()
   : m_Controls(NULL)
@@ -206,16 +205,17 @@ void QmitkDTIAtlasAppIntroPart::DelegateMeTo(const QUrl& showMeNext)
       }
       else
       {
+        mitk::DataNodeFactory::Pointer nodeReader = mitk::DataNodeFactory::New();
         try
         {
-          std::vector<BaseData::Pointer> baseData = mitk::IOUtil::Load(fileName->toLocal8Bit().data());
-
-          for ( unsigned int i = 0 ; i < baseData.size(); ++i )
+          nodeReader->SetFileName(fileName->toLocal8Bit().data());
+          nodeReader->Update();
+          for ( unsigned int i = 0 ; i < nodeReader->GetNumberOfOutputs( ); ++i )
           {
-            if (baseData[i].IsNotNull())
+            mitk::DataNode::Pointer node;
+            node = nodeReader->GetOutput(i);
+            if ( node->GetData() != NULL )
             {
-              mitk::DataNode::Pointer node = mitk::DataNode::New();
-              node->SetData(baseData[i]);
               dataStorage->Add(node);
               dsmodified = true;
             }
@@ -290,7 +290,7 @@ void QmitkDTIAtlasAppIntroPart::DelegateMeTo(const QUrl& showMeNext)
 
 #endif
 
-void QmitkDTIAtlasAppIntroPart::StandbyStateChanged(bool)
+void QmitkDTIAtlasAppIntroPart::StandbyStateChanged(bool standby)
 {
 
 }

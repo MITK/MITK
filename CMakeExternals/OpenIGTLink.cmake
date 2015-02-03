651bc0a129360178eb0c0c0a2b132b7ef24ff935
Eric Heim <e.heim@dkfz-heidelberg.de>
2015-02-03 17:02:03 +0100

    moved to third party repository

--- CMakeExternals/OpenIGTLink.cmake
+++ CMakeExternals/OpenIGTLink.cmake
@@ -1,7 +1,6 @@
 #-----------------------------------------------------------------------------
 # OpenIGTLink
 #-----------------------------------------------------------------------------
-
 if(MITK_USE_OpenIGTLink)
 
   # Sanity checks
@@ -22,20 +21,13 @@ if(MITK_USE_OpenIGTLink)
           -DCMAKE_USE_PTHREADS:BOOL=OFF)
     endif()
 
-    #list(APPEND additional_cmake_args
-    #     -DUSE_WRAP_ITK:BOOL=OFF
-    #    )
-
-   # set(ITK_PATCH_COMMAND ${CMAKE_COMMAND} -DTEMPLATE_FILE:FILEPATH=${MITK_SOURCE_DIR}/CMakeExternals/EmptyFileForPatching.dummy -P ${MITK_SOURCE_DIR}/CMakeExternals/PatchITK-4.5.1.cmake)
-
     ExternalProject_Add(${proj}
        SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}-src
        BINARY_DIR ${proj}-build
        PREFIX ${proj}-cmake
-       URL /projects/OpenIGTLink-54df50de62511f8fd07a7ce5b308f1202b30933a.tar.gz
+       URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/OpenIGTLink-54df50de.tar.gz
        URL_MD5 b9fd8351b059f4ec615f2dfd74ab2458
        INSTALL_COMMAND ""
-       #PATCH_COMMAND ${ITK_PATCH_COMMAND}
        CMAKE_GENERATOR ${gen}
        CMAKE_ARGS
          ${ep_common_args}
@@ -45,8 +37,6 @@ if(MITK_USE_OpenIGTLink)
          -DOpenIGTLink_PROTOCOL_VERSION_2:BOOL=ON
          -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
          -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
-         #-DITK_USE_SYSTEM_GDCM:BOOL=ON
-         #-DGDCM_DIR:PATH=${GDCM_DIR}
        DEPENDS ${proj_DEPENDENCIES}
       )
 

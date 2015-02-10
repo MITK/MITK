bb3e83ccf9adddac3d6061ab5161c4acc2bec961
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2015-02-10 02:34:07 +0100

    COMP: Don't use MITK_USE variables of external projects in the setup script.

--- CMake/MITKDashboardSetup.cmake
+++ CMake/MITKDashboardSetup.cmake
@@ -48,17 +48,19 @@ if(MITK_USE_QT)
 endif()
 
 set(OPENCV_DIR)
-if(WIN32 AND MITK_USE_OpenCV)
-  find_package(OpenCV REQUIRED CONFIG PATHS "${CTEST_BINARY_DIRECTORY}/ep")
-  set(_opencv_link_directories
-    "${OpenCV_LIB_DIR_DBG}"
-    "${OpenCV_LIB_DIR_OPT}"
-    "${OpenCV_3RDPARTY_LIB_DIR_DBG}"
-    "${OpenCV_3RDPARTY_LIB_DIR_OPT}")
-  list(REMOVE_DUPLICATES _opencv_link_directories)
-  foreach(_opencv_link_directory ${_opencv_link_directories})
-    list(APPEND OPENCV_DIR "${_opencv_link_directory}/../bin")
-  endforeach()
+if(WIN32)
+  find_package(OpenCV CONFIG PATHS "${CTEST_BINARY_DIRECTORY}/ep")
+  if(OpenCV_FOUND)
+    set(_opencv_link_directories
+      "${OpenCV_LIB_DIR_DBG}"
+      "${OpenCV_LIB_DIR_OPT}"
+      "${OpenCV_3RDPARTY_LIB_DIR_DBG}"
+      "${OpenCV_3RDPARTY_LIB_DIR_OPT}")
+    list(REMOVE_DUPLICATES _opencv_link_directories)
+    foreach(_opencv_link_directory ${_opencv_link_directories})
+      list(APPEND OPENCV_DIR "${_opencv_link_directory}/../bin")
+    endforeach()
+  endif()
 endif()
 
 #

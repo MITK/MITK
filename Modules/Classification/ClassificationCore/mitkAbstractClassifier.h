99f01e152ba36ae004ae1fe97c508f3bf467ed5e
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2015-03-22 10:33:00 +0100

    add documentation, rename methods, now inherits from mitk::BaseData GetConfiguration to GetConfigurationCopy GetPointWiseWeight to GetPointWiseWeightCopy

--- Modules/Classification/ClassificationCore/mitkAbstractClassifier.h
+++ Modules/Classification/ClassificationCore/mitkAbstractClassifier.h
@@ -20,7 +20,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include <MitkClassificationCoreExports.h>
 
-//#include <mitkBaseData.h>
+#include <mitkBaseData.h>
 
 // Eigen
 #include <Eigen/Dense>
@@ -32,25 +32,79 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 namespace mitk
 {
-  class MITKCLASSIFICATIONCORE_EXPORT AbstractClassifier // : public BaseData
+  class MITKCLASSIFICATIONCORE_EXPORT AbstractClassifier : public BaseData
   {
   public:
+    ///
+    /// @brief MatrixType
+    ///
     typedef Eigen::MatrixXd MatrixType;
+
+    ///
+    /// @brief VectorType
+    ///
     typedef Eigen::VectorXd VectorType;
 
-    virtual void Train(const MatrixType &X, const VectorType &Y) = 0;
+    ///
+    /// @brief Build a forest of trees from the training set (X, y).
+    /// @param X, The training input samples. Matrix of shape = [n_samples, n_features]
+    /// @param Y, The target values (class labels in classification, real numbers in regression). Array of shape = [n_samples]
+    ///
+    virtual void Fit(const MatrixType &X, const VectorType &Y) = 0;
+
+    ///
+    /// @brief Predict class for X.
+    /// @param X, The input samples.
+    /// @return The predicted classes. Y array of shape = [n_samples]
+    ///
     virtual VectorType Predict(const MatrixType &X) = 0;
 
-    void SetConfiguration(const ConfigurationHolder& value);
+    ///
+    /// @brief SetConfiguration, handing over classifier custom configurations
+    /// @param conf
+    ///
+    void SetConfiguration(const ConfigurationHolder& conf);
+
+    ///
+    /// @brief Configuration
+    /// @return none-const refernce to the custom configuration
+    ///
     ConfigurationHolder& Configuration();
-    ConfigurationHolder GetConfiguration();
 
-    void SetPointWiseWeight(const VectorType& w);
-    VectorType GetPointWiseWeight();
+    ///
+    /// @brief GetConfigurationCopy
+    /// @return Create and return a deep copy of the configuration
+    ///
+    ConfigurationHolder GetConfigurationCopy();
 
+    ///
+    /// @brief SetPointWiseWeight
+    /// @param W, The pointwise weights. W array of shape = [n_samples]
+    ///
+    void SetPointWiseWeight(const VectorType& W);
+
+    ///
+    /// @brief GetPointWiseWeightCopy
+    /// @return Create and return a copy of W
+    ///
+    VectorType GetPointWiseWeightCopy();
+
+    ///
+    /// @brief UsePointWiseWeight
+    /// @param toggle weighting on/off
+    ///
     void UsePointWiseWeight(bool value);
+
+    ///
+    /// @brief IsUsingPointWiseWeight
+    /// @return true if pointewise weighting is enabled.
+    ///
     bool IsUsingPointWiseWeight();
 
+    ///
+    /// @brief SupportsPointWiseWeight
+    /// @return True if the classifier supports pointwise weighting else false
+    ///
     virtual bool SupportsPointWiseWeight();
 
   protected:

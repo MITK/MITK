199ae989c0608b051399aca683dfccc286711ab6
Esther Wild <e.wild@dkfz.de>
2015-03-24 13:46:15 +0100

    new methods to insert opencv images as time slice and vice versa

--- Modules/OpenCVVideoSupport/mitkImageToOpenCVImageFilter.h
+++ Modules/OpenCVVideoSupport/mitkImageToOpenCVImageFilter.h
@@ -68,6 +68,16 @@ class MITKOPENCVVIDEOSUPPORT_EXPORT ImageToOpenCVImageFilter : public itk::Objec
         ///
         cv::Mat GetOpenCVMat();
 
+        //##Documentation
+        //## @brief Convenient method to set a certain slice of a 3D or 4D mitk::Image as input to convert it to an openCV image
+        //##
+        //## This methods sets the input. Call GetOpenCVMat() or GetOpenCVImage() to get the image.
+        //##
+        //## @param mitkImage - the image that should be converted to an openCVImage
+        //## @param timeStep - the time step, which is converted to openCV
+        //## @param slice - the slice which is converted to openCV
+        void SetInputFromTimeSlice(Image::Pointer mitkImage, int timeStep, int slice);
+
     protected:
         ///
         /// the actual templated conversion method
@@ -83,6 +93,9 @@ class MITKOPENCVVIDEOSUPPORT_EXPORT ImageToOpenCVImageFilter : public itk::Objec
         ///
         mitk::WeakPointer<mitk::Image> m_Image;
         IplImage* m_OpenCVImage;
+
+  private:
+    ImageSliceSelector::Pointer m_sliceSelector;
 };
 
 } // namespace

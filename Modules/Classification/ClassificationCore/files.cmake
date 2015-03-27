a205f64a1b947746a859ff6ef14dcdf2f3f0c46b
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2015-03-27 11:54:11 +0100

    add ClassifierVigraRandomForest to top level classification cmake

--- Modules/Classification/ClassificationCore/files.cmake
+++ Modules/Classification/ClassificationCore/files.cmake
@@ -3,6 +3,7 @@ file(GLOB_RECURSE H_FILES RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURREN
 set(CPP_FILES
   mitkConfigurationHolder.cpp
   mitkAbstractClassifier.cpp
+  Algorithms/itkLabelSampler.cpp
 )
 
 set( TOOL_FILES
---
+++ Modules/Classification/ClassificationCore/src/Algorithms/itkLabelSampler.cpp
@@ -0,0 +1,64 @@
+#ifndef itkLabelSampler_cpp
+#define itkLabelSampler_cpp
+
+#include "itkLabelSampler.h"
+#include "itkObjectFactory.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageRegionConstIterator.h"
+#include "itkImageIterator.h"
+#include "itkImageConstIterator.h"
+
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+namespace itk
+{
+
+template< class TImage>
+void LabelSampler< TImage>
+::GenerateData()
+{
+    typename TImage::ConstPointer input = this->GetInput();
+    typename TImage::Pointer output = this->GetOutput();
+
+    this->AllocateOutputs();
+    srand (time(NULL));
+
+    m_LabelNumberOfVoxels = 0;
+
+    ImageRegionConstIterator<TImage> inputIter(input, input->GetLargestPossibleRegion());
+    ImageRegionIterator<TImage> outputIter(output, output->GetLargestPossibleRegion());
+    while (!inputIter.IsAtEnd())
+    {
+        if (inputIter.Get() > 0)
+        {
+
+            if(m_LabelVoxelCountMap.find(inputIter.Get()) == m_LabelVoxelCountMap.end())
+                m_LabelVoxelCountMap[inputIter.Get()] = 0;
+            m_LabelVoxelCountMap[inputIter.Get()]++;
+
+            double r;
+            if(inputIter.Get() == m_Label || m_Label == -1 ){
+                r = (double)(rand()) / RAND_MAX;
+                if (r < m_AcceptRate)
+                {
+                    outputIter.Set(inputIter.Get());
+                    m_LabelNumberOfVoxels++;
+                }
+            }else
+                outputIter.Set(0);
+
+        }
+        else
+        {
+            outputIter.Set(0);
+        }
+        ++inputIter;
+        ++outputIter;
+    }
+
+}
+
+}// end namespace
+
+#endif // itkLabelSampler_cpp

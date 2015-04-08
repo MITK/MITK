11d3f2272939d7e63fb41ed8f2a6345460ce5923
Michael Brehler <m.brehler@dkfz-heidelberg.de>
2015-04-08 14:19:33 +0200

    fixed msvc compiler issues

--- CMakeExternals/Numpy.cmake
+++ CMakeExternals/Numpy.cmake
@@ -41,12 +41,17 @@ if( MITK_USE_Python AND NOT MITK_USE_SYSTEM_PYTHON )
         mitkFunctionExternalPythonBuildStep(${proj} configure \"${PYTHON_EXECUTABLE}\" \"${CMAKE_BINARY_DIR}\" setup.py config)
        ")
 
+    set(_numpy_compiler )
+    if(WIN32)
+     set(_numpy_compiler --compiler=msvc)
+    endif()
+
     # build step
     set(_build_step ${ep_prefix}/tmp/${proj}_build_step.cmake)
     file(WRITE ${_build_step}
        "${_numpy_env}
         include(\"${_numpy_build_step}\")
-        mitkFunctionExternalPythonBuildStep(${proj} build \"${PYTHON_EXECUTABLE}\" \"${CMAKE_BINARY_DIR}\" setup.py build --fcompiler=none)
+        mitkFunctionExternalPythonBuildStep(${proj} build \"${PYTHON_EXECUTABLE}\" \"${CMAKE_BINARY_DIR}\" setup.py build ${_numpy_compiler})
        ")
 
     # install step
@@ -63,8 +68,10 @@ if( MITK_USE_Python AND NOT MITK_USE_SYSTEM_PYTHON )
         mitkFunctionExternalPythonBuildStep(${proj} install \"${PYTHON_EXECUTABLE}\" \"${CMAKE_BINARY_DIR}\" setup.py install --prefix=\${_install_dir})
        ")
 
-    set(Numpy_URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/numpy-1.4.1.tar.gz)
-    set(Numpy_MD5 "5c7b5349dc3161763f7f366ceb96516b")
+    set(Numpy_URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/numpy-1.9.2.tar.gz)
+    #set(Numpy_URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/numpy-1.4.1.tar.gz)
+    #set(Numpy_MD5 "5c7b5349dc3161763f7f366ceb96516b")
+    set(Numpy_MD5 "a1ed53432dbcd256398898d35bc8e645")
 
     # escape spaces
     if(UNIX)
@@ -82,7 +89,7 @@ if( MITK_USE_Python AND NOT MITK_USE_SYSTEM_PYTHON )
       LIST_SEPARATOR ${sep}
       URL ${Numpy_URL}
       URL_MD5 ${Numpy_MD5}
-      ${NUMPY_PATCH_COMMAND}
+    #  ${NUMPY_PATCH_COMMAND}
       BUILD_IN_SOURCE 1
       CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${_configure_step}
       BUILD_COMMAND   ${CMAKE_COMMAND} -P ${_build_step}

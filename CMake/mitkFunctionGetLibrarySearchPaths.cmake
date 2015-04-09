00fb47247b9765a585a271c77b0f0c13514202d8
Michael Brehler <m.brehler@dkfz-heidelberg.de>
2015-04-09 15:14:49 +0200

    wip

--- CMake/mitkFunctionGetLibrarySearchPaths.cmake
+++ CMake/mitkFunctionGetLibrarySearchPaths.cmake
@@ -80,7 +80,7 @@ function(mitkFunctionGetLibrarySearchPaths search_path intermediate_dir)
 
   if(MITK_USE_Python)
     list(APPEND _dir_candidates "${CTK_DIR}/CMakeExternals/Install/bin")
-    list(APPEND _dir_candidates "${Python_DIR}/lib/python2.7/bin")
+    list(APPEND _dir_candidates "${MITK_EXTERNAL_PROJECT_PREFIX}/lib/python2.7/bin")
   endif()
 
   if(MITK_USE_TOF_PMDO3 OR MITK_USE_TOF_PMDCAMCUBE OR MITK_USE_TOF_PMDCAMBOARD)
--- CMake/mitkFunctionInstallPython.cmake
+++ CMake/mitkFunctionInstallPython.cmake
@@ -7,7 +7,7 @@
 #!  _python_libs Returns a list of the installed libraries. Used to fixup the bundle.
 #! _app_bundle App bundle name in case of a Apple bundle.
 #!
-function(mitkFunctionInstallPython _python_libs_out _app_bundle)
+function(mitkFunctionInstallPython _python_libs_out _search_path_out _app_bundle)
 
   # install the python runtime from the superbuild
   if(NOT MITK_USE_SYSTEM_PYTHON)
@@ -17,6 +17,7 @@ function(mitkFunctionInstallPython _python_libs_out _app_bundle)
     find_package(PythonInterp REQUIRED)
 
     set(_python_libs )
+    set(_search_path )
 
     # set the destination bundle
     set(_destination bin)
@@ -28,26 +29,31 @@ function(mitkFunctionInstallPython _python_libs_out _app_bundle)
       list(APPEND _python_dirs "${MITK_EXTERNAL_PROJECT_PREFIX}/lib")
       set(PYTHON_LIB_SUFFIX ".so")
       set(_py_include_postfix python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/)
-      set(_python_runtime_dir python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR})
+      set(_python_runtime_dir python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/)
     else() #WIN32
-      list(APPEND _python_dirs "${Python_DIR}/libs")
-      list(APPEND _python_dirs "${Python_DIR}/bin")
+      list(APPEND _python_dirs "${MITK_EXTERNAL_PROJECT_PREFIX}/lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/Lib")
+      #list(APPEND _python_dirs "${Python_DIR}/bin")
       set(PYTHON_LIB_SUFFIX .pyd)
       set(_py_include_postfix )
-      set(_python_runtime_dir Lib)
+      set(_python_runtime_dir )
+      set(_search_path "${MITK_EXTERNAL_PROJECT_PREFIX}/lib/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}/bin")
     endif()
 
-    file(GLOB_RECURSE item RELATIVE "${_python_dirs}/${_python_runtime_dir}" "${_python_dirs}/${_python_runtime_dir}/*")
+    file(GLOB_RECURSE item RELATIVE "${_python_dirs}/${_python_runtime_dir}" "${_python_dirs}/${_python_runtime_dir}*")
     foreach(f ${item})
       get_filename_component(_filepath "${f}" PATH)
-      install(FILES "${_python_dirs}/${_python_runtime_dir}/${f}" DESTINATION "${_destination}/Python/lib/${_python_runtime_dir}/${_filepath}")
+      #message("f: " ${f})
+      #message("filepath: " ${_filepath})
+      install(FILES "${_python_dirs}/${_python_runtime_dir}${f}" DESTINATION "${_destination}/Python/lib/${_python_runtime_dir}${_filepath}")
 
-      #MESSAGE("For loop: " "${_python_dirs}/${_python_runtime_dir}/${f}")
+      #MESSAGE("in: " "${_python_dirs}/${_python_runtime_dir}${f}")
+      #MESSAGE("out: " "${_destination}/Python/lib/${_python_runtime_dir}${_filepath}")
       if(${f} MATCHES "(${PYTHON_LIB_SUFFIX})$")
-        list(APPEND _python_libs "Python/lib/${_python_runtime_dir}/${_filepath}")
+        MESSAGE("F: " ${f})
+        list(APPEND _python_libs "Python/lib/${_python_runtime_dir}${_filepath}")
         if(UNIX AND NOT APPLE)
           install(CODE "file(RPATH_REMOVE
-          FILE \"\${CMAKE_INSTALL_PREFIX}/bin/Python/lib/${_python_runtime_dir}/${f}\")")
+          FILE \"\${CMAKE_INSTALL_PREFIX}/bin/Python/lib/${_python_runtime_dir}${f}\")")
         endif()
       endif()
     endforeach()
@@ -56,6 +62,7 @@ function(mitkFunctionInstallPython _python_libs_out _app_bundle)
     install(FILES "${PYTHON_INCLUDE_DIR}/pyconfig.h" DESTINATION ${_destination}/Python/include/${_py_include_postfix})
 
     set(${_python_libs_out} ${_python_libs} PARENT_SCOPE)
+    set(${_search_path_out} ${_search_path} PARENT_SCOPE)
   endif()
 endfunction()
 

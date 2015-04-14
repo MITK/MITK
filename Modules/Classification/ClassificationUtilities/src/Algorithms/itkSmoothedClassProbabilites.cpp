a491b1f58a0f06fd285087d1a1a4d6c646e0f122
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2015-04-14 16:58:05 +0200

    update smoothed class proba filter

--- Modules/Classification/ClassificationUtilities/src/Algorithms/itkSmoothedClassProbabilites.cpp
+++ Modules/Classification/ClassificationUtilities/src/Algorithms/itkSmoothedClassProbabilites.cpp
@@ -5,13 +5,15 @@
 #include "itkObjectFactory.h"
 #include "itkImageRegionIterator.h"
 #include "itkImageRegionConstIterator.h"
-#include "itkImageIterator.h"
-#include "itkImageConstIterator.h"
 #include <itkDiscreteGaussianImageFilter.h>
+
+
+#include <mitkImageCast.h>
+#include <mitkIOUtil.h>
 
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
-
+#include <sstream>
 namespace itk
 {
 
@@ -19,51 +21,44 @@ template< class TImage>
 void SmoothedClassProbabilites< TImage>
 ::GenerateData()
 {
-  //    typename TImage::ConstPointer input = this->GetInput();
-  typename TImage::Pointer output = this->GetOutput();
-  std::vector<ImageRegionConstIterator<TImage> > vec_iter;
-  auto arr = this->GetIndexedInputs();
 
-  for(auto it = arr.begin(); it != arr.end(); it ++)
+  typename TImage::Pointer out = this->GetOutput(0);
+  out->SetRegions(this->GetInput(0)->GetLargestPossibleRegion());
+  out->Allocate();
+
+  for(unsigned int i = 0 ; i < this->GetNumberOfInputs(); i++)
   {
-    typename itk::DiscreteGaussianImageFilter<TImage,TImage>::Pointer gf = itk::DiscreteGaussianImageFilter<TImage,TImage>::New();
-    gf->SetInput(static_cast<TImage *>(it->GetPointer()));
+
+    auto gf = itk::DiscreteGaussianImageFilter<TImage,TImage>::New();
+    gf->SetInput(this->GetInput(i));
     gf->SetVariance(this->m_Sigma);
     gf->Update();
-    *it = gf->GetOutput();
 
-    ImageRegionConstIterator<TImage> imageiterator(static_cast<TImage *>(it->GetPointer()),static_cast<TImage *>(it->GetPointer())->GetLargestPossibleRegion());
-    vec_iter.push_back(imageiterator);
-  }
+    ImageRegionConstIterator<TImage> git(gf->GetOutput(),gf->GetOutput()->GetLargestPossibleRegion());
+    ImageRegionIterator<TImage> maskiter(m_MaskImage, m_MaskImage->GetLargestPossibleRegion());
+    ImageRegionIterator<TImage> outputIter(out, out->GetLargestPossibleRegion());
 
-  ImageRegionConstIterator<TImage> maskiter(m_MaskImage, m_MaskImage->GetLargestPossibleRegion());
-  ImageRegionIterator<TImage> outputIter(output, output->GetLargestPossibleRegion());
-  while (!outputIter.IsAtEnd())
-  {
-
-    if(maskiter.Value() > 0 ){
-      int max_class = 0;
-      double prob = 0.0;
-      for(unsigned int i = 0 ; i < vec_iter.size(); i++)
-      {
-        if(vec_iter[i].Value() > prob)
-        {
-          max_class = i+1;
-          prob = vec_iter[i].Value();
-        }
-      }
-      outputIter.Set(max_class);
-    }else
+    while (!outputIter.IsAtEnd())
     {
-      outputIter.Set(0);
+      if(maskiter.Value() > 0 ){
+
+        if(git.Value() > outputIter.Value())
+          outputIter.Set(i);
+      }else
+      {
+        outputIter.Set(0);
+      }
+
+      ++git;
+      ++outputIter;
+      ++maskiter;
     }
-
-    for(unsigned int i = 0 ; i < vec_iter.size(); i++)
-      ++vec_iter[i];
-
-    ++outputIter;
-    ++maskiter;
   }
+
+
+
+
+
 
 }
 

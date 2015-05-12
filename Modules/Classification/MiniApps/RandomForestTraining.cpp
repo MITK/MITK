7f6007bf2882f08ec648e2811ab6d5a0134cca1d
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2015-05-12 17:59:02 +0200

    update RandomForestTraining miniapp

--- Modules/Classification/MiniApps/RandomForestTraining.cpp
+++ Modules/Classification/MiniApps/RandomForestTraining.cpp
@@ -43,103 +43,107 @@ using namespace mitk;
  */
 int main(int argc, char* argv[])
 {
-    mitkCommandLineParser parser;
-    parser.setArgumentPrefix("--", "-");
+  mitkCommandLineParser parser;
+  parser.setArgumentPrefix("--", "-");
 
-    // required params
-    parser.addArgument("inputdir", "i", mitkCommandLineParser::InputDirectory, "Input Directory", "Contains input feature files.", us::Any(), false);
-    parser.addArgument("outputdir", "o", mitkCommandLineParser::OutputDirectory, "Output Directory", "Destination of output files.", us::Any(), false);
-    parser.addArgument("classmask", "m", mitkCommandLineParser::InputFile, "Class mask image", "Contains several classes.", us::Any(), false);
+  // required params
+  parser.addArgument("inputdir", "i", mitkCommandLineParser::InputDirectory, "Input Directory", "Contains input feature files.", us::Any(), false);
+  parser.addArgument("outputdir", "o", mitkCommandLineParser::OutputDirectory, "Output Directory", "Destination of output files.", us::Any(), false);
+  parser.addArgument("classmask", "m", mitkCommandLineParser::InputFile, "Class mask image", "Contains several classes.", us::Any(), false);
 
-    // optional params
-    parser.addArgument("select", "s", mitkCommandLineParser::String, "Item selection", "Using Regular expression, seperated by space e.g.: '*.nrrd *.vtk *test*'","*.nrrd",true);
-    parser.addArgument("treecount", "tc", mitkCommandLineParser::Int, "Treecount", "Number of trees.",50,true);
-    parser.addArgument("treedepth", "td", mitkCommandLineParser::Int, "Treedepth", "Maximal tree depth.",50,true);
-    parser.addArgument("minsplitnodesize", "min", mitkCommandLineParser::Int, "Minimum split node size.", "Minimum split node size.",2,true);
-    parser.addArgument("precision", "p", mitkCommandLineParser::Float, "Split precision.", "Precision.", mitk::eps,true);
-    parser.addArgument("fraction", "f", mitkCommandLineParser::Float, "Fraction of samples per tree.", "Fraction of samples per tree.", 0.6f,true);
-    parser.addArgument("replacment", "r", mitkCommandLineParser::Bool, "Sample with replacement.", "Sample with replacement.", true,true);
+  // optional params
+  parser.addArgument("select", "s", mitkCommandLineParser::String, "Item selection", "Using Regular expression, seperated by space e.g.: '*.nrrd *.vtk *test*'",std::string("*.nrrd"),true);
+  parser.addArgument("treecount", "tc", mitkCommandLineParser::Int, "Treecount", "Number of trees.",50,true);
+  parser.addArgument("treedepth", "td", mitkCommandLineParser::Int, "Treedepth", "Maximal tree depth.",50,true);
+  parser.addArgument("minsplitnodesize", "min", mitkCommandLineParser::Int, "Minimum split node size.", "Minimum split node size.",2,true);
+  parser.addArgument("precision", "p", mitkCommandLineParser::Float, "Split precision.", "Precision.", mitk::eps,true);
+  parser.addArgument("fraction", "f", mitkCommandLineParser::Float, "Fraction of samples per tree.", "Fraction of samples per tree.", 0.6f,true);
+  parser.addArgument("replacment", "r", mitkCommandLineParser::Bool, "Sample with replacement.", "Sample with replacement.", true,true);
 
-    // Miniapp Infos
-    parser.setCategory("Classification Tools");
-    parser.setTitle("Random Forest Training");
-    parser.setDescription("Vigra RF impl.");
-    parser.setContributor("MBI");
+  // Miniapp Infos
+  parser.setCategory("Classification Tools");
+  parser.setTitle("Random Forest Training");
+  parser.setDescription("Vigra RF impl.");
+  parser.setContributor("MBI");
 
-    // Params parsing
-    map<string, us::Any> parsedArgs = parser.parseArguments(argc, argv);
-    if (parsedArgs.size()==0)
-        return EXIT_FAILURE;
+  // Params parsing
+  map<string, us::Any> parsedArgs = parser.parseArguments(argc, argv);
+  if (parsedArgs.size()==0)
+    return EXIT_FAILURE;
 
-    std::string inputdir = us::any_cast<string>(parsedArgs["inputdir"]);
-    std::string outputdir = us::any_cast<string>(parsedArgs["outputdir"]);
-    std::string classmask = us::any_cast<string>(parsedArgs["classmask"]);
+  std::string inputdir = us::any_cast<string>(parsedArgs["inputdir"]);
+  std::string outputdir = us::any_cast<string>(parsedArgs["outputdir"]);
+  std::string classmask = us::any_cast<string>(parsedArgs["classmask"]);
 
-    int treecount = parsedArgs.count("treecount") ? us::any_cast<int>(parsedArgs["treecount"]) : 50;
-    int treedepth = parsedArgs.count("treedepth") ? us::any_cast<int>(parsedArgs["treedepth"]) : 50;
-    int minsplitnodesize = parsedArgs.count("minsplitnodesize") ? us::any_cast<int>(parsedArgs["minsplitnodesize"]) : 2;
-    float precision = parsedArgs.count("precision") ? us::any_cast<float>(parsedArgs["precision"]) : mitk::eps;
-    float fraction = parsedArgs.count("fraction") ? us::any_cast<float>(parsedArgs["fraction"]) : 0.6;
-    bool withreplacement = parsedArgs.count("replacment") ? us::any_cast<float>(parsedArgs["replacment"]) : true;
-    QString filter = parsedArgs.count("select") ? us::any_cast<std::string>(parsedArgs["select"]).c_str(): "*.nrrd";
+  int treecount = parsedArgs.count("treecount") ? us::any_cast<int>(parsedArgs["treecount"]) : 50;
+  int treedepth = parsedArgs.count("treedepth") ? us::any_cast<int>(parsedArgs["treedepth"]) : 50;
+  int minsplitnodesize = parsedArgs.count("minsplitnodesize") ? us::any_cast<int>(parsedArgs["minsplitnodesize"]) : 2;
+  float precision = parsedArgs.count("precision") ? us::any_cast<float>(parsedArgs["precision"]) : mitk::eps;
+  float fraction = parsedArgs.count("fraction") ? us::any_cast<float>(parsedArgs["fraction"]) : 0.6;
+  bool withreplacement = parsedArgs.count("replacment") ? us::any_cast<float>(parsedArgs["replacment"]) : true;
+  std::string filt_select =/* parsedArgs.count("select") ? us::any_cast<string>(parsedArgs["select"]) :*/ "*.nrrd";
 
-    // Get nrrd filepath
-    QDir dir(inputdir.c_str());
-    auto strl = dir.entryList(filter.split(" "),QDir::Files);
+  QString filter(filt_select.c_str());
 
-    // load class mask
-    mitk::Image::Pointer mask = mitk::IOUtil::LoadImage(classmask);
-    unsigned int num_samples = 0;
-    AccessFixedDimensionByItk_1(mask,mitk::CLUtil::CountLabledVoxels, 3, num_samples)
+  // **** in principle repeat this block to create a feature matrix X_all for all subjects (in dir)
+  // Get nrrd filepath
+  QDir dir(inputdir.c_str());
+  auto strl = dir.entryList(filter.split(" "),QDir::Files);
 
-    // initialize featurematrix [num_samples, num_featureimages]
-    Eigen::MatrixXd X(num_samples, strl.size());
+  // load class mask
+  mitk::Image::Pointer mask = mitk::IOUtil::LoadImage(classmask);
+  unsigned int num_samples = 0;
+  AccessFixedDimensionByItk_1(mask,mitk::CLUtil::CountLabledVoxels, 3, num_samples);
 
-    for(int i = 0 ; i < strl.size(); i++)
-    {
-      // load feature image
-      mitk::Image::Pointer img = mitk::IOUtil::LoadImage(inputdir + strl[i].toStdString());
-      // transfom it into a [num_samples, 1] vector depending on the classmask
-      Eigen::MatrixXd _x = mitk::CLUtil::Transform<double>(img,mask);
-      // replace i-th col with _x
-      X.block(0,i,num_samples,1) = _x;
-    }
+  // initialize featurematrix [num_samples, num_featureimages]
+  Eigen::MatrixXd X(num_samples, strl.size());
 
-    // transform classmask into the label-vector [num_samples, 1]
-    Eigen::MatrixXi Y = mitk::CLUtil::Transform<int>(mask,mask);
+  for(int i = 0 ; i < strl.size(); i++)
+  {
+    // load feature image
+    mitk::Image::Pointer img = mitk::IOUtil::LoadImage(inputdir + strl[i].toStdString());
+    // transfom it into a [num_samples, 1] vector depending on the classmask
+    Eigen::MatrixXd _x = mitk::CLUtil::Transform<double>(img,mask);
+    // replace i-th (empty) col with feature vector in _x
+    X.block(0,i,num_samples,1) = _x;
+  }
+  // ****
 
-    mitk::VigraRandomForestClassifier::Pointer classifier = mitk::VigraRandomForestClassifier::New();
-    classifier->SetTreeCount(treecount);
-    classifier->SetMaximumTreeDepth(treedepth);
-    classifier->SetMinimumSplitNodeSize(minsplitnodesize);
-    classifier->SetPrecision(precision);
-    classifier->SetSamplesPerTree(fraction);
-    classifier->UseSampleWithReplacement(withreplacement);
+  // transform classmask into the label-vector [num_samples, 1]
+  Eigen::MatrixXi Y = mitk::CLUtil::Transform<int>(mask,mask);
 
-    classifier->PrintParameter();
-    classifier->Train(X,Y);
+  mitk::VigraRandomForestClassifier::Pointer classifier = mitk::VigraRandomForestClassifier::New();
+  classifier->SetTreeCount(treecount);
+  classifier->SetMaximumTreeDepth(treedepth);
+  classifier->SetMinimumSplitNodeSize(minsplitnodesize);
+  classifier->SetPrecision(precision);
+  classifier->SetSamplesPerTree(fraction);
+  classifier->UseSampleWithReplacement(withreplacement);
 
-    MITK_INFO << classifier->IsEmpty();
+  classifier->PrintParameter();
+  classifier->Train(X,Y);
 
-    // no metainformations are saved currently
-    // only the raw vigra rf data
-    mitk::IOUtil::Save(classifier, outputdir + "RandomForest.hdf5");
+  MITK_INFO << classifier->IsEmpty();
 
-    Eigen::MatrixXi Y_pred = classifier->Predict(X);
-    Eigen::MatrixXd Probs = classifier->GetPointWiseProbabilities();
+  // no metainformations are saved currently
+  // only the raw vigra rf data
+  mitk::IOUtil::Save(classifier, outputdir + "RandomForest.hdf5");
 
-    MITK_INFO << Y_pred.rows() << " " << Y_pred.cols();
-    MITK_INFO << Probs.rows() << " " << Probs.cols();
+  Eigen::MatrixXi Y_pred = classifier->Predict(X);
+  Eigen::MatrixXd Probs = classifier->GetPointWiseProbabilities();
 
-//    mitk::Image::Pointer prediction = mitk::CLUtil::Transform<int>(Y_pred,mask);
-    mitk::Image::Pointer probs_1 = mitk::CLUtil::Transform<double>(Probs.col(0),mask);
-    mitk::Image::Pointer probs_2 = mitk::CLUtil::Transform<double>(Probs.col(1),mask);
-    mitk::Image::Pointer probs_3 = mitk::CLUtil::Transform<double>(Probs.col(2),mask);
+  MITK_INFO << Y_pred.rows() << " " << Y_pred.cols();
+  MITK_INFO << Probs.rows() << " " << Probs.cols();
 
-    mitk::IOUtil::Save(probs_1, outputdir + "probs_1.nrrd");
-    mitk::IOUtil::Save(probs_2, outputdir + "probs_2.nrrd");
-    mitk::IOUtil::Save(probs_3, outputdir + "probs_3.nrrd");
-//    mitk::IOUtil::Save(probs_2, outputdir + "test.nrrd");
+  //    mitk::Image::Pointer prediction = mitk::CLUtil::Transform<int>(Y_pred,mask);
+  mitk::Image::Pointer probs_1 = mitk::CLUtil::Transform<double>(Probs.col(0),mask);
+  mitk::Image::Pointer probs_2 = mitk::CLUtil::Transform<double>(Probs.col(1),mask);
+  mitk::Image::Pointer probs_3 = mitk::CLUtil::Transform<double>(Probs.col(2),mask);
 
-    return EXIT_SUCCESS;
+  mitk::IOUtil::Save(probs_1, outputdir + "probs_1.nrrd");
+  mitk::IOUtil::Save(probs_2, outputdir + "probs_2.nrrd");
+  mitk::IOUtil::Save(probs_3, outputdir + "probs_3.nrrd");
+  //    mitk::IOUtil::Save(probs_2, outputdir + "test.nrrd");
+
+  return EXIT_SUCCESS;
 }

b7f15e099d268403bfb3d0489be3ebba38583c61
Jonas Cordes <j.cordes@dkfz-heidelberg.de>
2015-07-03 12:01:10 +0200

    edit cmake listst

--- Plugins/PluginList.cmake
+++ Plugins/PluginList.cmake
@@ -70,5 +70,4 @@ set(MITK_PLUGINS
   org.mitk.gui.qt.xnat:OFF
   org.mitk.gui.qt.spectrocamrecorder:OFF
   org.mitk.gui.qt.classificationsegmentation:OFF
-  org.mitk.gui.qt.advanceimageprocessing:OFF
 )

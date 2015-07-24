772e7dab10cd031feec46c54d17f8c63400267e4
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2015-07-24 03:13:53 +0200

    Set initial focus of color widget to its line edit.

--- Plugins/org.mitk.gui.qt.properties/src/internal/QmitkPropertyItemDelegate.cpp
+++ Plugins/org.mitk.gui.qt.properties/src/internal/QmitkPropertyItemDelegate.cpp
@@ -38,7 +38,6 @@ QmitkColorWidget::QmitkColorWidget(QWidget* parent)
     m_Button(new QToolButton)
 {
   m_LineEdit->setText(m_Color.name());
-
   m_Button->setText("...");
 
   QHBoxLayout* layout = new QHBoxLayout;
@@ -47,6 +46,8 @@ QmitkColorWidget::QmitkColorWidget(QWidget* parent)
 
   layout->addWidget(m_LineEdit);
   layout->addWidget(m_Button);
+
+  this->setFocusProxy(m_LineEdit);
   this->setLayout(layout);
 
   connect(m_LineEdit, SIGNAL(editingFinished()), this, SLOT(OnLineEditEditingFinished()));

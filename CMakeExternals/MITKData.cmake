5d60d2044add1b4bb03cf0670571e7df15c758d9
Peter Neher <p.neher@dkfz-heidelberg.de>
2015-07-31 13:18:43 +0200

    using new hash

--- CMakeExternals/MITKData.cmake
+++ CMakeExternals/MITKData.cmake
@@ -12,7 +12,7 @@ set(proj_DEPENDENCIES)
 set(MITK-Data_DEPENDS ${proj})
 
 if(BUILD_TESTING)
-  set(revision_tag e33e2f66) # first 8 characters of hash-tag
+  set(revision_tag 2ffa73f0) # first 8 characters of hash-tag
 #                  ^^^^^^^^  these are just to check correct length of hash part
 
   ExternalProject_Add(${proj}

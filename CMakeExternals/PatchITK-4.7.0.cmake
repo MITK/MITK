64ecce8a38a368f3980297f72cb955bcd6506ff9
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2015-08-12 12:50:42 +0200

    Removed unused patch file for ITK 4.7.0.

--- CMakeExternals/PatchITK-4.7.0.cmake
+++
@@ -1,9 +0,0 @@
-# Called by ITK.cmake (ExternalProject_Add) as a patch for ITK to work with external GDCM 2.2.1
-
-set(path "Modules/ThirdParty/GDCM/src/CMakeLists.txt")
-file(STRINGS ${path} contents NEWLINE_CONSUME)
-# more robust search for replacement
-string(REPLACE "gdcmMSFF" "gdcmMSFF gdcmDSED gdcmCommon" contents ${contents})
-set(CONTENTS ${contents})
-configure_file(${TEMPLATE_FILE} ${path} @ONLY)
-

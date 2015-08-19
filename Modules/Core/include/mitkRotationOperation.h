829bc5803635f38d048a207f0bedb41d5d2b63cc
Thomas Kilgus <t.kilgus@dkfz-heidelberg.de>
2015-08-19 16:18:16 +0200

    Cosmetic fixes.

--- Modules/Core/include/mitkRotationOperation.h
+++ Modules/Core/include/mitkRotationOperation.h
@@ -15,8 +15,8 @@ See LICENSE.txt or http://www.mitk.org for details.
 ===================================================================*/
 
 
-#ifndef MITKROTATIONOPERATION_H_HEADER_INCLUDED
-#define MITKROTATIONOPERATION_H_HEADER_INCLUDED
+#ifndef mitkRotationOperation_h
+#define mitkRotationOperation_h
 
 #include "mitkOperation.h"
 #include "mitkNumericTypes.h"
@@ -24,16 +24,38 @@ See LICENSE.txt or http://www.mitk.org for details.
 namespace mitk {
 
 //##Documentation
-//## @brief Operation, that holds everything necessary for an rotation operation
+//## @brief Operation, that holds everything necessary for an rotation operation on mitk::BaseData.
 //##
 //## @ingroup Undo
 class MITKCORE_EXPORT RotationOperation : public Operation
 {
 public:
+  /**
+   * @brief RotationOperation constructor to create the operation.
+   * @param operationType this has to be set to OpROTATE.
+   * @param pointOfRotation Anchor point for rotation.
+   * @param vectorOfRotation Axis for rotation.
+   * @param angleOfRotation Angle for rotation in degree.
+   */
   RotationOperation(OperationType operationType, Point3D pointOfRotation, Vector3D vectorOfRotation, ScalarType angleOfRotation);
   virtual ~RotationOperation(void);
+
+  /**
+   * @brief GetAngleOfRotation getter for rotation angle.
+   * @return Angle in degree.
+   */
   virtual ScalarType GetAngleOfRotation();
+
+  /**
+   * @brief GetCenterOfRotation getter for the anchor point of rotation.
+   * @return The anchor point to rotate the base data around.
+   */
   virtual const Point3D GetCenterOfRotation();
+
+  /**
+   * @brief GetVectorOfRotation getter for the rotation axis.
+   * @return Rotation axis as vector.
+   */
   virtual const Vector3D GetVectorOfRotation();
 
 protected:
@@ -44,4 +66,4 @@ protected:
 
 } // namespace mitk
 
-#endif /* MITKROTATIONOPERATION_H_HEADER_INCLUDED */
+#endif /* mitkRotationOperation_h */

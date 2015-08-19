88c75e48f766b1a606c223875f577cc6599f4561
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2015-08-19 14:10:37 +0200

    Quote command line arguments.

--- CMake/RunInstalledApp.sh
+++ CMake/RunInstalledApp.sh
@@ -2,4 +2,4 @@
 binpath=$(dirname "$(readlink -f "$0")")
 appname=$(basename "$0" .sh)
 export LD_LIBRARY_PATH="$binpath/bin":"$binpath/bin/plugins":$LD_LIBRARY_PATH
-"$binpath/bin/$appname" $*
+"$binpath/bin/$appname" "$@"

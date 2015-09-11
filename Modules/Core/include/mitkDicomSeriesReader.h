b8d04a1fe81949f307b5cc797f0350a67bfc1a97
is.moshkov <il.moshkov@gmail.com>
2015-09-11 17:03:30 +0400

    fix bug with reference in thread (AUT-243)

--- Modules/Core/include/mitkDicomSeriesReader.h
+++ Modules/Core/include/mitkDicomSeriesReader.h
@@ -555,7 +555,7 @@ public:
   static void LoadSeries(const DicomSeriesReader::StringContainer& filenames, itk::SmartPointer<Image> image, itk::ImageIOBase::IOComponentType comptype,  void* command); 
 
   template <typename PixelType>
-  static void LoadSeriesTemplate(const DicomSeriesReader::StringContainer& filenames, itk::SmartPointer<Image> image, void* command);
+  static void LoadSeriesTemplate(const DicomSeriesReader::StringContainer filenames, itk::SmartPointer<Image> image, void* command);
 
 protected:
 

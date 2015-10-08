f6988a961b4a3f1308b711511d3cb81867d6cf4e
Daniel Maleike <d.maleike@mint-medical.de>
2015-10-08 17:13:10 +0200

    Fix typo

--- Modules/Core/src/IO/mitkProportionalTimeGeometryToXML.cpp
+++ Modules/Core/src/IO/mitkProportionalTimeGeometryToXML.cpp
@@ -16,7 +16,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include "mitkProportionalTimeGeometryToXML.h"
 
-#include "mitkGeometry3DToXml.h"
+#include "mitkGeometry3DToXML.h"
 
 #include <tinyxml.h>
 

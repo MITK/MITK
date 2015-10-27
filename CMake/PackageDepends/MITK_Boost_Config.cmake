4a5b1a9d6f56291f7340f8410092a38fc4d5e487
mpkh <mikhail.pukhlikov@outlook.com>
2015-10-27 18:38:43 +0400

    boost log linking support

--- CMake/PackageDepends/MITK_Boost_Config.cmake
+++ CMake/PackageDepends/MITK_Boost_Config.cmake
@@ -11,6 +11,9 @@ if(Boost_LIBRARIES)
     # Force dynamic linking
     list(APPEND ALL_COMPILE_OPTIONS -DBOOST_ALL_DYN_LINK)
   else()
+    if ("${Boost_LIBRARIES}" MATCHES "boost_log")
+      list(APPEND Boost_LIBRARIES boost_log_setup)
+    endif()
     # Boost has an auto link feature (pragma comment lib) for Windows
     list(APPEND ALL_LIBRARIES ${Boost_LIBRARIES})
   endif()

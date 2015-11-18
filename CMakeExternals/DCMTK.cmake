1930c9d830375a894c7636928efcd67b52ed6efe
Miklos Espak <m.espak@ucl.ac.uk>
2015-11-18 19:18:03 +0000

    Use DCMTK_ROOT variable instead of DCMTK_DIR to work around problems with DCMTKConfig
    Signed-off-by: Miklos Espak <m.espak@ucl.ac.uk>

--- CMakeExternals/DCMTK.cmake
+++ CMakeExternals/DCMTK.cmake
@@ -59,6 +59,7 @@ if(MITK_USE_DCMTK)
       DEPENDS ${proj_DEPENDENCIES}
       )
     set(DCMTK_DIR ${ep_prefix})
+    set(DCMTK_ROOT ${ep_prefix})
     mitkFunctionInstallExternalCMakeProject(${proj})
 
   else()

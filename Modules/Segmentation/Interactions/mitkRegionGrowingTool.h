6420533f778e984dae8d900f4043c6450e227c88
Jens Petersen <jens.petersen@dkfz-heidelberg.de>
2015-12-02 18:06:02 +0100

    Have first iteration of region grower, still crash

--- Modules/Segmentation/Interactions/mitkRegionGrowingTool.h
+++ Modules/Segmentation/Interactions/mitkRegionGrowingTool.h
@@ -86,6 +86,12 @@ class MITKSEGMENTATION_EXPORT RegionGrowingTool : public FeedbackContourTool
     virtual void OnMouseMoved   ( StateMachineAction*, InteractionEvent* interactionEvent );
     virtual void OnMouseReleased( StateMachineAction*, InteractionEvent* interactionEvent );
 
+    template <typename TPixel, unsigned int imageDimension>
+    void IsInsideSegmentation(itk::Image<TPixel, imageDimension>* itkImage, itk::Index<imageDimension> index, bool result);
+
+    template<typename TPixel, unsigned int imageDimension>
+    void StartRegionGrowing(itk::Image<TPixel, imageDimension>* itkImage, itk::Index<imageDimension> seedPoint, ScalarType lowerThreshold, ScalarType upperThreshold);
+
     mitkIpPicDescriptor* PerformRegionGrowingAndUpdateContour(int timestep=0);
 
     Image::Pointer m_ReferenceSlice;

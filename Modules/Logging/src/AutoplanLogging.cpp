83ff546d103a61b7c00df3ff53e1a73039e6fe65
Mikhail Pukhlikov <mikhail.pukhlikov@outlook.com>
2016-01-25 16:36:49 +0400

    add DllExport for logging so there should be only one class instance per whole application

--- Modules/Logging/src/AutoplanLogging.cpp
+++ Modules/Logging/src/AutoplanLogging.cpp
@@ -1,5 +1,15 @@
 #include "AutoplanLogging.h"
 
 namespace Logger {
+  Options& Options::get()
+  {
+    static Options INSTANCE;
+    return INSTANCE;
+  }
 
+  Log& Log::get()
+  {
+    static Log INSTANCE;
+    return INSTANCE;
+  }
 }

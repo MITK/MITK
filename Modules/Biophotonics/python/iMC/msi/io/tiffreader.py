f75e22d307f1a95b9f31412589b3f5d85a204dcb
Sebastian Wirkert <s.wirkert@dkfz-heidelberg.de>
2016-02-25 15:23:29 +0100

    Try to give TiffReader and TiffRingReader same functionality.
    Better would be implementing this in a common base class.

--- Modules/Biophotonics/python/iMC/msi/io/tiffreader.py
+++ Modules/Biophotonics/python/iMC/msi/io/tiffreader.py
@@ -8,6 +8,7 @@ Created on Sep 28, 2015
 import os
 
 import Image
+import scipy
 import numpy as np
 
 from msi.io.reader import Reader
@@ -43,10 +44,15 @@ class TiffReader(Reader):
     Assumes bitdepth 16bit on a 12bit camera.
     TODO SW: document and test"""
 
+    # static member to globally set resizing for all images read by the reader
+    # 1.0 for no resizing
+    RESIZE_FACTOR = 1.0
+
     def __init__(self, shift_bits=4):
         self.shift_bits = shift_bits
 
-    def read(self, file_to_read, sorting_function=sort_by_filter):
+    def read(self, file_to_read, resize_factor=None,
+             sorting_function=sort_by_filter):
         """ read the msi from tiffs.
         The fileToRead is a string prefix, all files starting with this
         prefix will be summarized to one msi. they will be sorted as specified
@@ -58,21 +64,33 @@ class TiffReader(Reader):
                 lexicographical sorting is wished
             file_to_read: the prefix of the tiff file which shall be read
         """
+
+        if resize_factor is None:
+            resize_factor = TiffReader.RESIZE_FACTOR
+
         path, file_prefix = os.path.split(file_to_read)
         files = os.listdir(path)
         files_to_read = [os.path.join(path, f) for f in files
                          if file_prefix[2:] in f]
         files_to_read.sort(cmp=sorting_function)
-        image_array = [self.to_image(f)
+        image_array = [self.to_image(f, resize_factor=resize_factor)
                        for f in files_to_read]
         image = reduce(lambda x, y: np.dstack((x, y)), image_array)
 
         msi = Msi(image)
         return msi
 
-    def to_image(self, f):
+    def to_image(self, f, resize_factor):
         im = Image.open(f)
         im_array = np.array(im)
+
+        if do_resize(resize_factor):
+            im_array= scipy.misc.imresize(im_array, resize_factor,
+                                          interp="bilinear", mode="F")
+
         im_array >>= self.shift_bits
         return im_array.astype('float')
 
+
+def do_resize(resize_factor):
+    return not np.isclose(resize_factor, 1.0) and (resize_factor is not None)
--- Modules/Biophotonics/python/iMC/msi/io/tiffringreader.py
+++ Modules/Biophotonics/python/iMC/msi/io/tiffringreader.py
@@ -24,8 +24,8 @@ class TiffRingReader(Reader):
     # 1.0 for no resizing
     RESIZE_FACTOR = 1.0
 
-    def __init__(self):
-        pass
+    def __init__(self, shift_bits=4):
+        self.shift_bits = shift_bits
 
     def read(self, fileToRead, n, resize_factor=None,
              segmentation=None):
@@ -52,7 +52,7 @@ class TiffRingReader(Reader):
         files_in_folder.sort()
         position = files_in_folder.index(fileToRead)
         # take n images from found position
-        image_array = [to_image(f, resize_factor)
+        image_array = [self.to_image(f, resize_factor)
                        for f in files_in_folder[position:position + n]]
         image = reduce(lambda x, y: np.dstack((x, y)), image_array)
 
@@ -66,7 +66,7 @@ class TiffRingReader(Reader):
 
         # we pass an explicic image as segmentation
         if segmentation is not None:
-            segmentation = to_image(segmentation, resize_factor)
+            segmentation = self.to_image(segmentation, resize_factor)
         else: # otherwise: search for numpy segmentations
             try:
                 segmentation_array = [to_segmentation(f)
@@ -80,22 +80,21 @@ class TiffRingReader(Reader):
                 logging.info("didn't find segmentation for all images")
         return msi, segmentation
 
+    def to_image(self, f, resize_factor):
+        im = Image.open(f)
+        im_array = np.array(im)
+        im_array >>= self.shift_bits
 
-def do_resize(resize_factor):
-    return not np.isclose(resize_factor, 1.0) and (resize_factor is not None)
-
-
-def to_image(f, resize_factor):
-    im = Image.open(f)
-    im_array = np.array(im)
-    im_array >>= 4
-
-    if do_resize(resize_factor):
-        im_array= scipy.misc.imresize(im_array, resize_factor,
-                                     interp="bilinear", mode="F")
-    return im_array
+        if do_resize(resize_factor):
+            im_array= scipy.misc.imresize(im_array, resize_factor,
+                                          interp="bilinear", mode="F")
+        return im_array
 
 
 def to_segmentation(f):
     seg = np.load(f + ".seg.npy")
     return seg
+
+
+def do_resize(resize_factor):
+    return not np.isclose(resize_factor, 1.0) and (resize_factor is not None)

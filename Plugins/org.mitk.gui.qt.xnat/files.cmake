d9f58edbb99a87fdbe389b967f8ed1ba72e09073
Andreas Fetzer <a.fetzer@dkfz-heidelberg.de>
2016-02-26 17:14:58 +0100

    Added documentation for XNAT tree-browser plugin

--- Plugins/org.mitk.gui.qt.xnat/files.cmake
+++ Plugins/org.mitk.gui.qt.xnat/files.cmake
@@ -27,9 +27,6 @@ set(MOC_H_FILES
 # for example the icon used in the menu and tabs for the
 # plug-in views in the workbench
 set(CACHED_RESOURCE_FILES
-  resources/icon.xpm
-  resources/xnat_treebrowser_icon.xpm
-  resources/xnat-icon.png
   plugin.xml
 )
 

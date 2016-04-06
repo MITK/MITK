3a0dd0c762e5c61558f749313bf1f6fdb4980574
Sebastian Wirkert <s.wirkert@dkfz-heidelberg.de>
2016-04-06 19:02:20 +0200

    First version with two convolutional layers.

--- Modules/Biophotonics/python/iMC/scripts/ipcai_to_tensorflow/script_train_tensorflow_model.py
+++ Modules/Biophotonics/python/iMC/scripts/ipcai_to_tensorflow/script_train_tensorflow_model.py
@@ -1,6 +1,8 @@
 
 import os
 
+import  numpy as np
+
 import commons
 
 sc = commons.ScriptCommons()
@@ -33,12 +35,12 @@ display_step = 1
 
 # Network Parameters
 n_hidden = 100 # hidden layers number of elements
-n_input = 21 # number of features (wavelengths)
+n_bands = 21 # number of features (wavelengths)
 n_classes = 1 # number of outputs (one for oxygenation)
 
 # tf Graph input
 
-x = tf.placeholder("float", [None, n_input])
+x = tf.placeholder("float", [None, n_bands, 1, 1])
 y = tf.placeholder("float", [None, n_classes])
 
 keep_prob = tf.placeholder("float")
@@ -54,36 +56,54 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-def add_fully_connected_layer(input, n_inputs, n_outputs):
-    w = weight_variable([n_inputs, n_outputs])
+def conv2d(x, W, padding='SAME'):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding=padding)
+
+
+def max_pool_2x1(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 1, 1],
+                          strides=[1, 2, 1, 1], padding='SAME')
+
+
+def add_hidden_layer(input, n_inputs, n_outputs, kernel_size, padding='SAME'):
+    #w = weight_variable([n_inputs, n_outputs])
+    #b = bias_variable([n_outputs])
+    W = weight_variable([kernel_size, 1, n_inputs, n_outputs])
     b = bias_variable([n_outputs])
     # Hidden layer with RELU activation
-    new_layer = tf.nn.relu(tf.add(tf.matmul(input, w), b))
+    #new_layer = tf.nn.relu(tf.add(tf.matmul(input, w), b))
+    h_conv = tf.nn.relu(conv2d(input, W, padding=padding) + b)
     # Add dropout regularization
-    new_layer_with_dropout = tf.nn.dropout(new_layer, keep_prob)
-    return new_layer_with_dropout
+    #new_layer_with_dropout = tf.nn.dropout(new_layer, keep_prob)
+    h_pool = max_pool_2x1(h_conv)
+    return h_pool
 
 
 # Create model
 def multilayer_perceptron(_X):
+    # two convolutional layers
+    layer_1 = add_hidden_layer(_X, 1, 32, 6, padding='VALID')
+    layer_2 = add_hidden_layer(layer_1, 32, 64, 4)
+    # flatten last one to be able to apply it to fully connected layer
+    final_number_of_dimensions = 4*64
+    layer_2_flat = tf.reshape(layer_2, [-1, final_number_of_dimensions])
 
-    layer_1 = add_fully_connected_layer(_X, n_input, n_hidden)
-    layer_2 = add_fully_connected_layer(layer_1, n_hidden, n_hidden)
-    last_hidden_layer = add_fully_connected_layer(layer_2, n_hidden, n_hidden)
+    # fully connected layer to bring information together
+    W_fc1 = weight_variable([final_number_of_dimensions, 100])
+    b_fc1 = bias_variable([100])
+    h_fc1 = tf.nn.relu(tf.matmul(layer_2_flat, W_fc1) + b_fc1)
+    # with dropout regularization
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-    # linear output layer
-    w_out = weight_variable([n_hidden, n_classes])
-    b_out = bias_variable([n_classes])
-    return tf.matmul(last_hidden_layer, w_out) + b_out
-
+    # return linear output layer
+    W_fc2 = weight_variable([100, n_classes])
+    b_fc2 = bias_variable([n_classes])
+    return tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 # Construct model
 pred = multilayer_perceptron(x)
 
 # Define loss and optimizer
-
-#cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y)) # Softmax loss
-#cost = -tf.reduce_mean(y*tf.log(pred)+(1-y)*tf.log(1-pred))
 cost = tf.reduce_mean(tf.square(pred - y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost) # Adam Optimizer
 
@@ -102,10 +122,11 @@ with tf.Session() as sess:
         for i in range(total_batch):
             batch_xs, batch_ys = ipcai.train.next_batch(batch_size)
             # Fit training using batch data
-            sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys,
+            x_image = np.reshape(batch_xs, [-1, n_bands, 1, 1])
+            sess.run(optimizer, feed_dict={x: x_image, y: batch_ys,
                                            keep_prob: 0.5})
             # Compute average loss
-            avg_cost += sess.run(cost, feed_dict={x: batch_xs, y: batch_ys,
+            avg_cost += sess.run(cost, feed_dict={x: x_image, y: batch_ys,
                                                   keep_prob: 1.0})/total_batch
         # Display logs per epoch step
         if epoch % display_step == 0:
@@ -115,6 +136,7 @@ with tf.Session() as sess:
 
     # Test model
     accuracy = tf.reduce_mean(tf.cast(tf.abs(pred-y), "float"))
+    x_test_image = np.reshape(ipcai.test.images, [-1, n_bands, 1, 1])
     print "Median testing error:", accuracy.eval({x: ipcai.test.images,
                                                   y: ipcai.test.labels,
                                                   keep_prob:1.0})

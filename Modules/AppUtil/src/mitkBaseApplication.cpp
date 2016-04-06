ee3bc0e32d275c53bf353dd00d9502fbf27fc55e
Aurélien Labrosse <aurelien@pollen-metrology.com>
2016-04-06 13:51:45 +0200

    fix for bug 19635
    * Added a configuration option which allow to give an image
      filename used as splash screen
    
    * Added a inner QRunnable which allow CTK to close the splash
      screen once the platform is started
    
    Signed-off-by: Aurélien Labrosse <aurelien@pollen-metrology.com>

--- Modules/AppUtil/src/mitkBaseApplication.cpp
+++ Modules/AppUtil/src/mitkBaseApplication.cpp
@@ -30,6 +30,10 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include <Poco/Util/HelpFormatter.h>
 
+#include <QSplashscreen>
+#include <QRunnable> 
+#include <QFileInfo>
+
 #include <QTime>
 #include <QDir>
 #include <QDesktopServices>
@@ -58,6 +62,8 @@ QString BaseApplication::ARG_CONSOLELOG = "BlueBerry.consoleLog";
 QString BaseApplication::ARG_TESTPLUGIN = "BlueBerry.testplugin";
 QString BaseApplication::ARG_TESTAPPLICATION = "BlueBerry.testapplication";
 
+QString BaseApplication::ARG_SPLASH_IMAGE = "BlueBerry.splashscreen";
+
 QString BaseApplication::ARG_NO_REGISTRY_CACHE = "BlueBerry.noRegistryCache";
 QString BaseApplication::ARG_NO_LAZY_REGISTRY_CACHE_LOADING = "BlueBerry.noLazyRegistryCacheLoading";
 QString BaseApplication::ARG_REGISTRY_MULTI_LANGUAGE = "BlueBerry.registryMultiLanguage";
@@ -77,6 +83,19 @@ QString BaseApplication::PROP_TESTPLUGIN = "BlueBerry.testplugin";
 QString BaseApplication::PROP_TESTAPPLICATION = "BlueBerry.testapplication";
 
 
+class SplashCloserCallback : public QRunnable
+{
+public:
+  SplashCloserCallback(QSplashScreen* splashscreen) {
+    this->m_Splashscreen = splashscreen;
+  }
+  void SplashCloserCallback::run() {
+    this->m_Splashscreen->close();
+  }
+private:
+  QSplashScreen* m_Splashscreen;
+};
+
 struct BaseApplication::Impl
 {
   ctkProperties m_FWProps;
@@ -92,6 +111,9 @@ struct BaseApplication::Impl
 
   bool m_SingleMode;
   bool m_SafeMode;
+  
+  QSplashScreen* m_Splashscreen;
+  SplashCloserCallback* m_SplashscreenClosingCallback;
 
   QStringList m_PreloadLibs;
   QString m_ProvFile;
@@ -101,6 +123,8 @@ struct BaseApplication::Impl
     , m_Argv(argv)
     , m_SingleMode(false)
     , m_SafeMode(true)
+    ,m_Splashscreen(0)
+    , m_SplashscreenClosingCallback(NULL)
   {
 #ifdef Q_OS_MAC
     /*
@@ -301,6 +325,7 @@ struct BaseApplication::Impl
     }
   }
 
+
 };
 
 BaseApplication::BaseApplication(int argc, char** argv)
@@ -311,7 +336,14 @@ BaseApplication::BaseApplication(int argc, char** argv)
 
 BaseApplication::~BaseApplication()
 {
-
+  if (d->m_Splashscreen != 0)
+  {
+    delete(d->m_Splashscreen);
+  }
+  if (d->m_SplashscreenClosingCallback != 0)
+  {
+    delete(d->m_SplashscreenClosingCallback);
+  }
 }
 
 void BaseApplication::printHelp(const std::string& /*name*/, const std::string& /*value*/)
@@ -517,14 +549,18 @@ void BaseApplication::initialize(Poco::Util::Application& self)
   //    framework properties.
   d->initializeCTKPluginFrameworkProperties(this->config());
 
-  // 6. Set the custom CTK Plugin Framework storage directory
+  // 6. Initialize splash screen if an image path is provided
+  //    in the .ini file
+  this->initializeSplashScreen(qApp);
+
+  // 7. Set the custom CTK Plugin Framework storage directory
   QString storageDir = this->getCTKFrameworkStorageDir();
   if (!storageDir.isEmpty())
   {
     d->m_FWProps[ctkPluginConstants::FRAMEWORK_STORAGE] = storageDir;
   }
 
-  // 7. Set the library search paths and the pre-load library property
+  // 8. Set the library search paths and the pre-load library property
   this->initializeLibraryPaths();
   QStringList preloadLibs = this->getPreloadLibraries();
   if (!preloadLibs.isEmpty())
@@ -532,13 +568,13 @@ void BaseApplication::initialize(Poco::Util::Application& self)
       d->m_FWProps[ctkPluginConstants::FRAMEWORK_PRELOAD_LIBRARIES] = preloadLibs;
   }
 
-  // 8. Initialize the CppMicroServices library.
+  // 9. Initialize the CppMicroServices library.
   //    The initializeCppMicroServices() method reuses the
   //    FRAMEWORK_STORAGE property, so we call it after the
   //    getCTKFrameworkStorageDir method.
   this->initializeCppMicroServices();
 
-  // 9. Parse the (optional) provisioning file and set the
+  // 10. Parse the (optional) provisioning file and set the
   //    correct framework properties.
   d->parseProvisioningFile(this->getProvisioningFilePath());
 
@@ -694,7 +730,14 @@ int BaseApplication::main(const std::vector<std::string>& args)
     arguments.push_back(QString::fromStdString(arg));
   }
 
-  return ctkPluginFrameworkLauncher::run(NULL, QVariant::fromValue(arguments)).toInt();
+  if (d->m_Splashscreen != 0)
+  {
+    // a splash screen is displayed,
+    // creating the closing callback
+    d->m_SplashscreenClosingCallback = new SplashCloserCallback(d->m_Splashscreen);
+  }
+
+  return ctkPluginFrameworkLauncher::run(d->m_SplashscreenClosingCallback, QVariant::fromValue(arguments)).toInt();
 }
 
 void BaseApplication::defineOptions(Poco::Util::OptionSet& options)
@@ -763,6 +806,10 @@ void BaseApplication::defineOptions(Poco::Util::OptionSet& options)
   registryMultiLanguageOption.callback(Poco::Util::OptionCallback<Impl>(d.data(), &Impl::handleBooleanOption));
   options.addOption(registryMultiLanguageOption);
 
+  Poco::Util::Option splashScreenOption(ARG_SPLASH_IMAGE.toStdString(), "", "optional picture to use as a splash screen");
+  splashScreenOption.argument("<filename>").binding(ARG_SPLASH_IMAGE.toStdString());
+  options.addOption(splashScreenOption);
+
   Poco::Util::Option xargsOption(ARG_XARGS.toStdString(), "", "Extended argument list");
   xargsOption.argument("<args>").binding(ARG_XARGS.toStdString());
   options.addOption(xargsOption);
@@ -786,6 +833,21 @@ ctkPluginContext* BaseApplication::getFrameworkContext() const
 QHash<QString, QVariant> BaseApplication::getFrameworkProperties() const
 {
   return d->m_FWProps;
+}
+
+void BaseApplication::initializeSplashScreen(QCoreApplication * application) const
+{
+  QVariant pixmapFileNameProp = d->getProperty(ARG_SPLASH_IMAGE);
+  if (!pixmapFileNameProp.isNull()) {
+    QString pixmapFileName = pixmapFileNameProp.toString();
+    QFileInfo checkFile(pixmapFileName);
+    if (checkFile.exists() && checkFile.isFile()) {
+      QPixmap pixmap(checkFile.absoluteFilePath());
+      d->m_Splashscreen = new QSplashScreen(pixmap, Qt::WindowStaysOnTopHint);
+      d->m_Splashscreen->show();
+      application->processEvents();
+    }
+  }
 }
 
 int BaseApplication::run()

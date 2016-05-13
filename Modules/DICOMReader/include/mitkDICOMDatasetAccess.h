83db370a885550613e7d8a546c5a56aed00f5013
Ralf Floca <r.floca@dkfz-heidelberg.de>
2016-05-13 10:41:20 +0200

    fixed outdated documentation
    Signed-off-by: Ralf Floca <r.floca@dkfz-heidelberg.de>

--- Modules/DICOMReader/include/mitkDICOMDatasetAccess.h
+++ Modules/DICOMReader/include/mitkDICOMDatasetAccess.h
@@ -50,7 +50,8 @@ class MITKDICOMREADER_EXPORT DICOMDatasetAccess
     /// If DICOM is not read from file but from somewhere else (network, database), we might not have files.
     virtual std::string GetFilenameIfAvailable() const = 0;
 
-    /** \brief Return the raw value of the tag as a string.
+    /** \brief Return a DICOMDatasetFinding instance of the tag.
+    The return containes (if valid) the raw value of the tag as a string.
     \param tag Tag which value should be retreived.
     */
     virtual DICOMDatasetFinding GetTagValueAsString(const DICOMTag& tag) const = 0;

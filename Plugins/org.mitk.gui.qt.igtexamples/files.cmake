85b2a4755075e69df8c7c148afe90fd1c5e9615a
Esther Stenau <e.stenau@dkfz-heidelberg.de>
2016-09-02 13:00:35 +0200

    Remove deleted images from cmake list. Todo: Check which images are still needed.

--- Plugins/org.mitk.gui.qt.igtexamples/files.cmake
+++ Plugins/org.mitk.gui.qt.igtexamples/files.cmake
@@ -29,18 +29,18 @@ set(MOC_H_FILES
 )
 
 set(QRC_FILES
-  resources/QmitkIGTTrackingLabView.qrc
+  #resources/QmitkIGTTrackingLabView.qrc
   #resources/QmitkIGTTutorialView.qrc
 )
 
 set(CACHED_RESOURCE_FILES
-  resources/IgtTrackingLab.png
+  #resources/IgtTrackingLab.png
   resources/start_rec.png
   resources/stop_rec.png
-  resources/IGTTutorial.png
-  resources/icon.xpm
-  resources/example.png
-  resources/provider.png
+  #resources/IGTTutorial.png
+  #resources/icon.xpm
+  #resources/example.png
+  #resources/provider.png
   resources/icon_igt_simple_example.svg
   resources/icon_igt_tracking_lab.svg
   resources/icon_openigtlink_client_example.svg

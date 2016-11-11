fc2c65f6e1e5934286bc1134e2b3b1b382141b8f
Christoph Kolb <c.kolb@dkfz-heidelberg.de>
2016-11-11 16:48:18 +0100

    fix layouter test

--- Modules/Overlays/test/mitkTextOverlay2DLayouterRenderingTest.cpp
+++ Modules/Overlays/test/mitkTextOverlay2DLayouterRenderingTest.cpp
@@ -17,21 +17,20 @@ See LICENSE.txt or http://www.mitk.org for details.
 // MITK
 #include "mitkRenderingTestHelper.h"
 #include "mitkTestingMacros.h"
-#include <mitkOverlayManager.h>
 
 // VTK
-#include "mitkOverlay2DLayouter.h"
+#include "mitkOverlayLayouter2D.h"
 #include "mitkTextOverlay2D.h"
 #include <vtkRegressionTestImage.h>
 
-void createTextOverlayWithLayouter(mitk::OverlayManager::Pointer OverlayManager,
-                                   const std::string &layouter,
-                                   mitk::BaseRenderer *renderer,
-                                   int fontsize,
-                                   float red,
-                                   float green,
-                                   float blue,
-                                   std::string text)
+mitk::TextOverlay2D::Pointer createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Alignment align,
+                                                           mitk::BaseRenderer *renderer,
+                                                           int fontsize,
+                                                           float red,
+                                                           float green,
+                                                           float blue,
+                                                           int prio,
+                                                           std::string text)
 {
   // Create a textOverlay2D
   mitk::TextOverlay2D::Pointer textOverlay = mitk::TextOverlay2D::New();
@@ -41,12 +40,9 @@ void createTextOverlayWithLayouter(mitk::OverlayManager::Pointer OverlayManager,
   textOverlay->SetColor(red, green, blue);
   textOverlay->SetOpacity(1);
 
-  // Position is committed as a Point2D Property
-  mitk::Point2D pos;
-  pos[0] = 0, pos[1] = 0;
-  textOverlay->SetPosition2D(pos);
-  OverlayManager->AddOverlay(textOverlay.GetPointer());
-  OverlayManager->SetLayouter(textOverlay.GetPointer(), layouter, renderer);
+  mitk::OverlayLayouter2D::AddOverlay(textOverlay, renderer, align, 30, 30, prio);
+
+  return textOverlay;
 }
 
 int mitkTextOverlay2DLayouterRenderingTest(int argc, char *argv[])
@@ -61,66 +57,42 @@ int mitkTextOverlay2DLayouterRenderingTest(int argc, char *argv[])
   //  renderingHelper.SetAutomaticallyCloseRenderWindow(false);
 
   mitk::BaseRenderer *renderer = mitk::BaseRenderer::GetInstance(renderingHelper.GetVtkRenderWindow());
-  mitk::OverlayManager::Pointer OverlayManager = mitk::OverlayManager::New();
-  renderer->SetOverlayManager(OverlayManager);
 
-  OverlayManager->AddLayouter(
-    mitk::Overlay2DLayouter::CreateLayouter(mitk::Overlay2DLayouter::STANDARD_2D_TOP(), renderer).GetPointer());
-  OverlayManager->AddLayouter(
-    mitk::Overlay2DLayouter::CreateLayouter(mitk::Overlay2DLayouter::STANDARD_2D_TOPLEFT(), renderer).GetPointer());
-  OverlayManager->AddLayouter(
-    mitk::Overlay2DLayouter::CreateLayouter(mitk::Overlay2DLayouter::STANDARD_2D_TOPRIGHT(), renderer).GetPointer());
-  OverlayManager->AddLayouter(
-    mitk::Overlay2DLayouter::CreateLayouter(mitk::Overlay2DLayouter::STANDARD_2D_BOTTOM(), renderer).GetPointer());
-  OverlayManager->AddLayouter(
-    mitk::Overlay2DLayouter::CreateLayouter(mitk::Overlay2DLayouter::STANDARD_2D_BOTTOMLEFT(), renderer).GetPointer());
-  OverlayManager->AddLayouter(
-    mitk::Overlay2DLayouter::CreateLayouter(mitk::Overlay2DLayouter::STANDARD_2D_BOTTOMRIGHT(), renderer).GetPointer());
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::TopLeft, renderer, 20, 1.0, 1.0, 1.0, 1, "TopLeft1");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::TopLeft, renderer, 15, 1.0, 1.0, 1.0, 3, "TopLeft3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::TopLeft, renderer, 25, 1.0, 0.0, 1.0, 2, "TopLeft2");
 
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_TOP(), renderer, 15, 1, 1, 1, "TOP_1");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_TOP(), renderer, 15, 1, 1, 1, "TOP_2");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_TOP(), renderer, 20, 1, 1, 1, "TOP_3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Top, renderer, 15, 1.0, 1.0, 1.0, 3, "Top3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Top, renderer, 20, 1.0, 1.0, 1.0, 1, "Top1");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Top, renderer, 25, 1.0, 0.0, 1.0, 2, "Top2");
 
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_TOPLEFT(), renderer, 15, 1, 1, 1, "TOPLEFT_1");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_TOPLEFT(), renderer, 20, 1, 1, 1, "TOPLEFT_2");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_TOPLEFT(), renderer, 15, 1, 1, 1, "TOPLEFT_3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::TopRight, renderer, 20, 1.0, 1.0, 1.0, 1, "TopRight1");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::TopRight, renderer, 15, 1.0, 1.0, 1.0, 3, "TopRight3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::TopRight, renderer, 25, 1.0, 0.0, 1.0, 2, "TopRight2");
 
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_TOPRIGHT(), renderer, 20, 1, 1, 1, "TOPRIGHT_1");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_TOPRIGHT(), renderer, 20, 1, 1, 1, "TOPRIGHT_2");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_TOPRIGHT(), renderer, 15, 1, 1, 1, "TOPRIGHT_3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Left, renderer, 20, 1.0, 1.0, 1.0, 1, "Left1");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Left, renderer, 15, 1.0, 1.0, 1.0, 3, "Left3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Left, renderer, 25, 1.0, 0.0, 1.0, 2, "Left2");
 
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_BOTTOM(), renderer, 15, 1, 1, 1, "BOTTOM_1");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_BOTTOM(), renderer, 15, 1, 1, 1, "BOTTOM_2");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_BOTTOM(), renderer, 20, 1, 1, 1, "BOTTOM_3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Right, renderer, 25, 1.0, 0.0, 1.0, 2, "Right2");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Right, renderer, 20, 1.0, 1.0, 1.0, 1, "Right1");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Right, renderer, 15, 1.0, 1.0, 1.0, 3, "Right3");
 
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_BOTTOMLEFT(), renderer, 15, 1, 1, 1, "BOTTOMLEFT_1");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_BOTTOMLEFT(), renderer, 20, 1, 1, 1, "BOTTOMLEFT_2");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_BOTTOMLEFT(), renderer, 15, 1, 1, 1, "BOTTOMLEFT_3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::BottomLeft, renderer, 25, 1.0, 0.0, 1.0, 2, "BottomLeft2");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::BottomLeft, renderer, 20, 1.0, 1.0, 1.0, 1, "BottomLeft1");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::BottomLeft, renderer, 15, 1.0, 1.0, 1.0, 3, "BottomLeft3");
 
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_BOTTOMRIGHT(), renderer, 20, 1, 1, 1, "BOTTOMRIGHT_1");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_BOTTOMRIGHT(), renderer, 20, 1, 1, 1, "BOTTOMRIGHT_2");
-  createTextOverlayWithLayouter(
-    OverlayManager, mitk::Overlay2DLayouter::STANDARD_2D_BOTTOMRIGHT(), renderer, 15, 1, 1, 1, "BOTTOMRIGHT_3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Bottom, renderer, 15, 1.0, 1.0, 1.0, 3, "Bottom3");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Bottom, renderer, 20, 1.0, 1.0, 1.0, 1, "Bottom1");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::Bottom, renderer, 25, 1.0, 0.0, 1.0, 2, "Bottom2");
+
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::BottomRight, renderer, 25, 1.0, 0.0, 1.0, 2, "BottomRight2");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::BottomRight, renderer, 20, 1.0, 1.0, 1.0, 1, "BottomRight1");
+  createTextOverlayWithLayouter(mitk::OverlayLayouter2D::BottomRight, renderer, 15, 1.0, 1.0, 1.0, 3, "BottomRight3");
 
   renderingHelper.Render();
 
+  renderingHelper.SetAutomaticallyCloseRenderWindow(false);
   // use this to generate a reference screenshot or save the file:
   bool generateReferenceScreenshot = false;
   if (generateReferenceScreenshot)

060c5d450a5f851076089642c3d1fafd91ccc5a7
KuznetsovAlexander <KuznetsovAlexander@rambler.ru>
2016-11-23 11:33:03 +0400

    Fixed number of progress steps in plane suggestion in 3D-interpolation.
    Signed-off-by: KuznetsovAlexander <KuznetsovAlexander@rambler.ru>

--- Modules/SegmentationUI/Qmitk/QmitkSlicesInterpolator.cpp
+++ Modules/SegmentationUI/Qmitk/QmitkSlicesInterpolator.cpp
@@ -885,7 +885,7 @@ void ::QmitkSlicesInterpolator::OnSuggestPlaneClicked()
 void ::QmitkSlicesInterpolator::RunPlaneSuggestion()
 {
   if (m_FirstRun)
-    mitk::ProgressBar::GetInstance()->AddStepsToDo(8);
+    mitk::ProgressBar::GetInstance()->AddStepsToDo(7);
   else
     mitk::ProgressBar::GetInstance()->AddStepsToDo(3);
 

b21bca76e7e5c51fb07d6b41de28fc0a328a4806
Ralf Floca <r.floca@dkfz-heidelberg.de>
2017-01-10 10:29:30 +0100

    Corrected function description and parameter naming.
    Signed-off-by: Ralf Floca <r.floca@dkfz-heidelberg.de>

--- Modules/MatchPointRegistration/Helper/mitkResultNodeGenerationHelper.cpp
+++ Modules/MatchPointRegistration/Helper/mitkResultNodeGenerationHelper.cpp
@@ -21,7 +21,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <mitkImage.h>
 
 mitk::DataNode::Pointer
-  mitk::generateRegistrationResultNode(const std::string& nodeName, mitk::MAPRegistrationWrapper::Pointer resultReg, const std::string& algorithmUID, const std::string& movingNodeUID, const std::string& targetNodeUID)
+  mitk::generateRegistrationResultNode(const std::string& nodeName, mitk::MAPRegistrationWrapper::Pointer resultReg, const std::string& algorithmUID, const std::string& movingDataUID, const std::string& targetDataUID)
 {
   if (resultReg.IsNull())
   {
@@ -32,8 +32,8 @@ mitk::DataNode::Pointer
   m_spRegNode->SetData(resultReg);
   m_spRegNode->SetName(nodeName);
   resultReg->SetProperty(mitk::Prop_RegAlgUsed, mitk::StringProperty::New(algorithmUID));
-  resultReg->SetProperty(mitk::Prop_RegAlgMovingData, mitk::StringProperty::New(movingNodeUID));
-  resultReg->SetProperty(mitk::Prop_RegAlgTargetData, mitk::StringProperty::New(targetNodeUID));
+  resultReg->SetProperty(mitk::Prop_RegAlgMovingData, mitk::StringProperty::New(movingDataUID));
+  resultReg->SetProperty(mitk::Prop_RegAlgTargetData, mitk::StringProperty::New(targetDataUID));
   resultReg->SetProperty(mitk::Prop_RegUID, mitk::StringProperty::New(resultReg->GetRegistration()->getRegistrationUID()));
 
   return m_spRegNode;
@@ -41,7 +41,7 @@ mitk::DataNode::Pointer
 
 
 mitk::DataNode::Pointer
-  mitk::generateMappedResultNode(const std::string& nodeName, mitk::BaseData::Pointer mappedData, const std::string& regUID, const std::string& inputNodeUID, const bool refinedGeometry, const std::string& interpolator)
+  mitk::generateMappedResultNode(const std::string& nodeName, mitk::BaseData::Pointer mappedData, const std::string& regUID, const std::string& inputDataUID, const bool refinedGeometry, const std::string& interpolator)
 {
   if (mappedData.IsNull())
   {
@@ -56,7 +56,7 @@ mitk::DataNode::Pointer
   {
     mappedData->SetProperty(mitk::Prop_RegUID, mitk::StringProperty::New(regUID));
   }
-  mappedData->SetProperty(mitk::Prop_MappingInputData, mitk::StringProperty::New(inputNodeUID));
+  mappedData->SetProperty(mitk::Prop_MappingInputData, mitk::StringProperty::New(inputDataUID));
   if (refinedGeometry)
   {
     mappedData->SetProperty(mitk::Prop_MappingInterpolator, mitk::StringProperty::New("None"));

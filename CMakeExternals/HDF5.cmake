1001be579d9ad1a1e560993084989de0662c42b6
Peter Neher <p.neher@dkfz-heidelberg.de>
2017-01-24 15:13:31 +0100

    adjusted hdf5 version and mitk-data hash

--- CMakeExternals/HDF5.cmake
+++ CMakeExternals/HDF5.cmake
@@ -27,8 +27,8 @@ if(MITK_USE_HDF5)
     # bad :( This would be fixed by using an external HDF-Installation with ITK/VTK
 
     ExternalProject_Add(${proj}
-       URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/hdf5-1.8.12.tar.gz
-       URL_MD5 d804802feb99b87fc668a90e6fa34411
+       URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/hdf5-1.8.17.tar.gz
+       URL_MD5 7d572f8f3b798a628b8245af0391a0ca
        CMAKE_GENERATOR ${gen}
        CMAKE_ARGS
          ${ep_common_args}
--- CMakeExternals/MITKData.cmake
+++ CMakeExternals/MITKData.cmake
@@ -13,7 +13,7 @@ set(MITK-Data_DEPENDS ${proj})
 
 if(BUILD_TESTING)
 
-  set(revision_tag 826b368f) # first 8 characters of hash-tag
+  set(revision_tag f81328b8) # first 8 characters of hash-tag
 #                  ^^^^^^^^  these are just to check correct length of hash part
 
   ExternalProject_Add(${proj}

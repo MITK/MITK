f21cf14b7a4db7c95f91d0a1ac91ef3ebbf3d62a
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2017-02-17 13:51:04 +0100

    use SmartPointer instead of standard pointer

--- Plugins/org.blueberry.ui.qt/src/internal/berryWorkbenchSourceProvider.h
+++ Plugins/org.blueberry.ui.qt/src/internal/berryWorkbenchSourceProvider.h
@@ -114,7 +114,7 @@ private:
    */
   const IWorkbenchPartSite* lastActivePartSite;
 
-  const Object* lastShowInInput;
+  SmartPointer<const Object> lastShowInInput;
 
   void PartActivated(const SmartPointer<IWorkbenchPartReference>& part) override;
   void PartBroughtToTop(const SmartPointer<IWorkbenchPartReference>& part) override;

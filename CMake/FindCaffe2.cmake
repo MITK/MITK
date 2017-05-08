7ef1e8a39d47272a89ffdf4ba21eeec3b159091c
Peter Neher <p.neher@dkfz-heidelberg.de>
2017-05-08 09:14:02 +0200

    added caffe2

---
+++ CMake/FindCaffe2.cmake
@@ -0,0 +1,50 @@
+
+set(Caffe2_DIR ${MITK_EXTERNAL_PROJECT_PREFIX})
+set(Caffe2_INCLUDE_DIR ${Caffe2_DIR}/include)
+set(Caffe2_CMAKE_DEBUG_POSTFIX d)
+
+set(Caffe2_LIBRARIES)
+  
+FOREACH(POSTFIX _CPU;_GPU)
+
+# Find all libraries, store debug and release separately
+  # Find Release libraries
+  find_library(Caffe2_LIBRARY_RELEASE
+    Caffe2${POSTFIX}
+    PATHS
+    ${Caffe2_DIR}/lib
+    ${Caffe2_DIR}/lib/Release
+    ${Caffe2_DIR}/lib/RelWithDebInfo
+    ${Caffe2_DIR}/bin
+    ${Caffe2_DIR}/bin/Release
+    ${Caffe2_DIR}/bin/RelWithDebInfo
+    NO_DEFAULT_PATH
+    )
+
+  # Find Debug libraries
+  find_library(Caffe2_LIBRARY_DEBUG
+    Caffe2${POSTFIX}${Caffe2_CMAKE_DEBUG_POSTFIX}
+    PATHS
+    ${Caffe2_DIR}/lib
+    ${Caffe2_DIR}/lib/Debug
+    ${Caffe2_DIR}/bin
+    ${Caffe2_DIR}/bin/Debug
+    NO_DEFAULT_PATH
+    )
+
+  # Add libraries to variable according to build type
+
+  if(Caffe2_LIBRARY_RELEASE)
+    list(APPEND Caffe2_LIBRARIES optimized ${Caffe2_LIBRARY_RELEASE})
+  endif()
+
+  if(Caffe2_LIBRARY_DEBUG)
+    list(APPEND Caffe2_LIBRARIES debug ${Caffe2_LIBRARY_DEBUG})
+  endif()
+  
+ENDFOREACH()
+
+find_package_handle_standard_args(Caffe2
+  FOUND_VAR Caffe2_FOUND
+  REQUIRED_VARS Caffe2_INCLUDE_DIR Caffe2_LIBRARIES
+)

8e73fbac64733eaba1e5354de672463f4f9b3b34
Peter Neher <p.neher@dkfz-heidelberg.de>
2017-05-19 12:04:01 +0200

    imagenet test

--- Modules/DiffusionImaging/DeepDiffusion/MiniApps/Caffe2Test.cpp
+++ Modules/DiffusionImaging/DeepDiffusion/MiniApps/Caffe2Test.cpp
@@ -20,9 +20,9 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <mitkImage.h>
 #include <mitkIOUtil.h>
 #include "mitkCommandLineParser.h"
-
 #include <boost/lexical_cast.hpp>
 
+// Caffe2
 #include <google/protobuf/text_format.h>
 #include "caffe2/core/context.h"
 #include "caffe2/core/operator.h"
@@ -32,9 +32,12 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include <gtest/gtest.h>
 
+// OpenCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 /*!
-* \brief
+* \brief Minimal example for Caffe2 prediction in MITK
 *
 */
 int main(int argc, char* argv[])
@@ -43,14 +46,29 @@ int main(int argc, char* argv[])
   parser.setTitle("Caffe2Test");
   parser.setCategory("Fiber Tracking and Processing Methods");
   parser.setContributor("MIC");
-  parser.setDescription("Caffe2 test");
+  parser.setDescription("Minimal example for Caffe2 prediction in MITK (Imagenet)");
   parser.setArgumentPrefix("--", "-");
+  parser.addArgument("image", "", mitkCommandLineParser::String, "image:", "", us::Any());
   parser.addArgument("init_net", "", mitkCommandLineParser::String, "init_net:", "the network weights (.pb)", us::Any());
   parser.addArgument("predict_net", "", mitkCommandLineParser::String, "predict_net:", "the network definition (.pb)", us::Any());
 
   std::map<std::string, us::Any> parsedArgs = parser.parseArguments(argc, argv);
   std::string init_net_filename = us::any_cast<std::string>(parsedArgs["init_net"]);
   std::string predict_net_filename = us::any_cast<std::string>(parsedArgs["predict_net"]);
+  std::string image_filename = us::any_cast<std::string>(parsedArgs["image"]);
+
+  cv::Mat img = cv::imread(image_filename, CV_LOAD_IMAGE_COLOR);
+
+  cv::Mat img2;
+  img.convertTo(img2, CV_32FC3);
+  //img2 /= 255.0;
+  //img2 -= 0.5;
+  //img2 -= 127;
+  //img2 *= 0.0;
+
+  cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
+  cv::imshow( "Display window", img2 );                   // Show our image inside it.
+  cv::waitKey(0);                                          // Wait for a keystroke in the window
 
   caffe2::NetDef init_net, predict_net;
   CAFFE_ENFORCE(ReadProtoFromFile(init_net_filename, &init_net));
@@ -66,8 +84,8 @@ int main(int argc, char* argv[])
   int w,h,c; w=227; h=227; c=3;
   caffe2::TensorCPU input(std::vector<int>({1, c, h, w}));
 
-  float input_data[h*w*c];
-  memcpy(input.mutable_data<float>(), input_data, h * w * c * sizeof(float));
+  //float input_data[h*w*c];
+  memcpy(input.mutable_data<float>(), (float*)img2.ptr(), h * w * c * sizeof(float));
 
   caffe2::Predictor::TensorVector inputVec{&input};
 
@@ -79,8 +97,16 @@ int main(int argc, char* argv[])
 //  for (auto el : output->dims())
 //    MITK_INFO << el;
 //  MITK_INFO << output->size();
+  int max_class = -1;
+  float max_prob = 0;
   for (int i=0; i<output->size(); i++)
-      MITK_INFO << output_data[i];
+      if(output_data[i]>max_prob)
+      {
+          max_prob = output_data[i];
+          max_class = i;
+      }
+  MITK_INFO << "Class: " << max_class;
+  MITK_INFO << "Probability: " << max_prob;
 
   return EXIT_SUCCESS;
 }
--- Modules/DiffusionImaging/DeepDiffusion/files.cmake
+++ Modules/DiffusionImaging/DeepDiffusion/files.cmake
@@ -1,11 +1,6 @@
 set(CPP_FILES
-
+  mitkDeepDiffusionModuleActivator.cpp
 )
 
 set(H_FILES
-
-)
-
-set(RESOURCE_FILES
-
 )

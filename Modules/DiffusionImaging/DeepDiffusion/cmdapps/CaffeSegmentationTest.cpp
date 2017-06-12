e79f8f1a8be9a9b87693ddb22e260588655d5b16
Peter Neher <p.neher@dkfz-heidelberg.de>
2017-06-12 11:23:25 +0200

    added caffe segmentation example

--- Modules/DiffusionImaging/DeepDiffusion/cmdapps/CaffeSegmentationTest.cpp
+++ Modules/DiffusionImaging/DeepDiffusion/cmdapps/CaffeSegmentationTest.cpp
@@ -34,30 +34,6 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-caffe::shared_ptr<caffe::Net<float>> train(caffe::Solver<float>* solver)
-{
-  int num_samples = 64;
-  int num_features = 100;
-  float *data = new float[1*1*num_features*num_samples];
-  float *label = new float[1*1*1*num_samples];
-  float *directions = new float[1*1*3*num_samples];
-
-  caffe::MemoryDataLayer<float> *image_input_layer = (caffe::MemoryDataLayer<float> *) (solver->net()->layer_by_name("image_data").get());
-  caffe::MemoryDataLayer<float> *target_input_layer = (caffe::MemoryDataLayer<float> *) (solver->net()->layer_by_name("target_data").get());
-
-  image_input_layer->Reset(data, label, num_samples);
-  target_input_layer->Reset(directions, label, num_samples);
-  solver->Solve();
-  return solver->net();
-}
-
-caffe::Solver<float>* read_solver(std::string solver_file)
-{
-  caffe::SolverParameter solver_param;
-  caffe::ReadSolverParamsFromTextFileOrDie(solver_file, &solver_param);
-  return new caffe::AdamSolver<float>(solver_param);
-}
-
 std::default_random_engine generator;
 std::normal_distribution<double> distribution(0.0,0.1);
 
@@ -88,6 +64,10 @@ void predict(caffe::shared_ptr<caffe::Net<float>> network)
             counter++;
           }
 
+//  // not necessary here
+//  input_layer->Reshape(1, num_channels, height, width);
+//  network->Reshape();
+
   network->Forward();
 
   cv::Mat input_img = cv::Mat(input_layer->height(), input_layer->width(), CV_32FC1, input_data) * 100;
@@ -104,38 +84,6 @@ void predict(caffe::shared_ptr<caffe::Net<float>> network)
   cv::waitKey(0);
 }
 
-std::vector<float> predict_data_layer(const cv::Mat& img, caffe::shared_ptr<caffe::Net<float>> network) {
-  caffe::Blob<float>* input_layer = network->input_blobs()[0];
-
-  assert(img.channels() == input_layer->channels());
-  assert(img.cols == input_layer->width());
-  assert(img.rows == input_layer->height());
-
-  cv::Mat img2;
-  // convert to float
-  if (img.channels()==3 && img.type()!=CV_32FC3)
-      img.convertTo(img2, CV_32FC3);
-  else if(img.channels()==1 && img.type()!=CV_32FC1)
-      img.convertTo(img2, CV_32FC1);
-
-  float* input_data = input_layer->mutable_cpu_data();  // pointer to the memory data layer
-  std::vector<cv::Mat> bgr;
-  for (int i = 0; i < img2.channels(); ++i) {
-    cv::Mat channel(input_layer->height(), input_layer->width(), CV_32FC1, input_data);
-    bgr.push_back(channel);
-    input_data += input_layer->width() * input_layer->height();
-  }
-  cv::split(img2, bgr);
-
-  network->Forward();
-
-  /* Copy the output layer to a std::vector */
-  caffe::Blob<float>* output_layer = network->output_blobs()[0];
-  const float* begin = output_layer->cpu_data();
-  const float* end = begin + output_layer->channels();
-  return std::vector<float>(begin, end);
-}
-
 /*!
 * \brief Segmentation example for Caffe in MITK
 *
@@ -150,26 +98,15 @@ int main(int argc, char* argv[])
   parser.setContributor("MIC");
   parser.setDescription("Minimal example for Caffe usage in MITK");
   parser.setArgumentPrefix("--", "-");
-  parser.addArgument("image", "", mitkCommandLineParser::String, "image:", "", us::Any());
-  parser.addArgument("solver_file", "", mitkCommandLineParser::String, "solver_file:", "the caffe solver file (.prototxt)", us::Any());
   parser.addArgument("network_file", "", mitkCommandLineParser::String, "network_file:", ".prototxt", us::Any());
   parser.addArgument("weights_file", "", mitkCommandLineParser::String, "weights_file:", ".caffemodel", us::Any());
+
+//  google::InitGoogleLogging(argv[0]);
+//  google::SetCommandLineOption("GLOG_minloglevel", "0");
 
   std::map<std::string, us::Any> parsedArgs = parser.parseArguments(argc, argv);
   if (parsedArgs.size()==0)
     return EXIT_FAILURE;
-
-  std::string image_filename = us::any_cast<std::string>(parsedArgs["image"]);
-  cv::Mat img = cv::imread(image_filename, CV_LOAD_IMAGE_GRAYSCALE);
-//  cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
-//  cv::imshow( "Display window", img );                   // Show our image inside it.
-//  cv::waitKey(0);                                          // Wait for a keystroke in the window
-
-  //// TRAINING
-//  std::string solver_file = us::any_cast<std::string>(parsedArgs["solver_file"]);
-//  caffe::Solver<float>* solver = read_solver(solver_file);
-//  caffe::shared_ptr<caffe::Net<float>> trained_net = train(solver);
-//  const float* test_output = test(trained_net);
 
   std::string network_file = us::any_cast<std::string>(parsedArgs["network_file"]);
   std::string weights_file = us::any_cast<std::string>(parsedArgs["weights_file"]);
@@ -179,10 +116,7 @@ int main(int argc, char* argv[])
   network.reset(new caffe::Net<float>(network_file, caffe::TEST, 0, &stages));
   network->CopyTrainedLayersFrom(weights_file);
 
-  MITK_INFO << "predicting";
   predict(network);
-  MITK_INFO << "DONE";
-
 
   return EXIT_SUCCESS;
 }

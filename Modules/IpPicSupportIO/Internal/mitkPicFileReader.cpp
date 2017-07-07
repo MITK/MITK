c3f65c26ab867900a8b4a2bf64ac77d6b8c333d4
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2017-07-07 15:35:13 +0200

    Remove LegacyAdaptors module

--- Modules/IpPicSupportIO/Internal/mitkPicFileReader.cpp
+++ Modules/IpPicSupportIO/Internal/mitkPicFileReader.cpp
@@ -15,13 +15,112 @@ See LICENSE.txt or http://www.mitk.org for details.
 ===================================================================*/
 
 #include "mitkPicFileReader.h"
-#include "mitkLegacyAdaptors.h"
 #include "mitkPicHelper.h"
 
 #include "mitkCustomMimeType.h"
 #include "mitkImageWriteAccessor.h"
 
 #include <mitkIpPic.h>
+
+
+static mitk::PixelType CastToPixelType(mitkIpPicType_t pictype, size_t bpe)
+{
+  const bool isSignedIntegralType = (pictype == mitkIpPicInt);
+  const bool isUnsignedIntegralType = (pictype == mitkIpPicUInt);
+
+  if (isSignedIntegralType)
+  {
+    switch (bpe)
+    {
+    case sizeof(char) :
+      return mitk::MakeScalarPixelType<char>();
+    case sizeof(short) :
+      return mitk::MakeScalarPixelType<short>();
+    default:
+      return mitk::MakeScalarPixelType<int>();
+    }
+  }
+  else if (isUnsignedIntegralType)
+  {
+    switch (bpe)
+    {
+    case sizeof(unsigned char) :
+      return mitk::MakeScalarPixelType<unsigned char>();
+    case sizeof(unsigned short) :
+      return mitk::MakeScalarPixelType<unsigned short>();
+    default:
+      return mitk::MakeScalarPixelType<unsigned int>();
+    }
+  }
+  else // is floating point type
+  {
+    switch (bpe)
+    {
+    case sizeof(float) :
+      return mitk::MakeScalarPixelType<float>();
+    default:
+      return mitk::MakeScalarPixelType<double>();
+    }
+  }
+}
+
+static mitk::ImageDescriptor::Pointer CastToImageDescriptor(mitkIpPicDescriptor *desc)
+{
+  mitk::ImageDescriptor::Pointer imDescriptor = mitk::ImageDescriptor::New();
+
+  imDescriptor->Initialize(desc->n, desc->dim);
+
+  mitk::PixelType ptype = ::CastToPixelType(desc->type, (desc->bpe / 8));
+  imDescriptor->AddNewChannel(ptype, "imported by pic");
+
+  return imDescriptor;
+}
+
+static mitkIpPicType_t CastToIpPicType(int intype)
+{
+  const bool isSignedIntegralType = (intype == itk::ImageIOBase::INT || intype == itk::ImageIOBase::SHORT ||
+    intype == itk::ImageIOBase::CHAR || intype == itk::ImageIOBase::LONG);
+
+  const bool isUnsignedIntegralType = (intype == itk::ImageIOBase::UINT || intype == itk::ImageIOBase::USHORT ||
+    intype == itk::ImageIOBase::UCHAR || intype == itk::ImageIOBase::ULONG);
+
+  const bool isFloatingPointType = (intype == itk::ImageIOBase::FLOAT || intype == itk::ImageIOBase::DOUBLE);
+
+  if (isSignedIntegralType)
+    return mitkIpPicInt;
+  if (isUnsignedIntegralType)
+    return mitkIpPicUInt;
+  if (isFloatingPointType)
+    return mitkIpPicFloat;
+  return mitkIpPicUnknown;
+}
+
+static mitkIpPicDescriptor * CastToIpPicDescriptor(mitk::Image::Pointer refImg,
+  mitk::ImageWriteAccessor *imageAccess,
+  mitkIpPicDescriptor *picDesc)
+{
+  const mitk::ImageDescriptor::Pointer imDesc = refImg->GetImageDescriptor();
+
+  // initialize dimension information
+  for (unsigned int i = 0; i < 8; i++)
+  {
+    picDesc->n[i] = 1;
+  }
+
+  // set dimension information
+  picDesc->dim = refImg->GetDimension();
+  memcpy(picDesc->n, imDesc->GetDimensions(), picDesc->dim * sizeof(unsigned int));
+
+  picDesc->type = ::CastToIpPicType(refImg->GetPixelType().GetComponentType());
+  picDesc->bpe = refImg->GetPixelType().GetBpe();
+  if (imageAccess != nullptr)
+  {
+    picDesc->data = imageAccess->GetData();
+  }
+
+  return picDesc;
+}
+
 
 mitk::PicFileReader::PicFileReader() : AbstractFileReader()
 {

f5e13f41e64470abecc1263f3429dab0ae2d2fa3
Michael Goetz <m.goetz@dkfz-heidelberg.de>
2017-07-13 12:02:36 +0200

    Linux compabtibiliy

--- Modules/Classification/CLUtilities/src/GlobalImageFeatures/mitkGIFFirstOrderStatistics.cpp
+++ Modules/Classification/CLUtilities/src/GlobalImageFeatures/mitkGIFFirstOrderStatistics.cpp
@@ -43,7 +43,7 @@ CalculateFirstOrderStatistics(itk::Image<TPixel, VImageDimension>* itkImage, mit
   mitk::CastToItkImage(mask, maskImage);
 
   double voxelVolume = 1;
-  for (int i = 0; i < min(3, VImageDimension), ++i)
+  for (int i = 0; i < std::min<int>(3, VImageDimension), ++i)
     voxelVolume *= itkImage->GetSpacing[i];
   double voxelSpace = 1;
   for (int i = 0; i < VImageDimension, ++i)

0f4fa75053e2f13ddec8d2ed8ce656048ab48127
Mikhail Pukhlikov <cynede@gentoo.org>
2017-07-20 14:18:48 +0400

    English corrections AUT-2682

--- Plugins/org.mitk.gui.qt.segmentation/src/internal/SegmentationUtilities/ContourModelToImage/QmitkContourModelToImageWidget.cpp
+++ Plugins/org.mitk.gui.qt.segmentation/src/internal/SegmentationUtilities/ContourModelToImage/QmitkContourModelToImageWidget.cpp
@@ -27,7 +27,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <QFutureWatcher>
 #include <qmessagebox.h>
 
-const QString HelpText = QObject::tr("Select a image and a contour(set)");
+const QString HelpText = QObject::tr("Select an image and a contour(set)");
 
 class QmitkContourModelToImageWidgetPrivate
 {

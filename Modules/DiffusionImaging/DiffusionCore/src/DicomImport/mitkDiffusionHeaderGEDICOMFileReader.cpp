2c18d69e0f2f952b8c08be0e25a853f6bcfd8b5d
KuznetsovAlexander <KuznetsovAlexander@rambler.ru>
2017-08-15 11:54:37 +0400

    GE diffusion loading fix.
    AUT-2857
    Signed-off-by: KuznetsovAlexander <KuznetsovAlexander@rambler.ru>

--- Modules/DiffusionImaging/DiffusionCore/src/DicomImport/mitkDiffusionHeaderGEDICOMFileReader.cpp
+++ Modules/DiffusionImaging/DiffusionCore/src/DicomImport/mitkDiffusionHeaderGEDICOMFileReader.cpp
@@ -38,9 +38,9 @@ bool mitk::DiffusionHeaderGEDICOMFileReader
   // b value stored in the first bytes
   // typical example:  "1000\8\0\0" for bvalue=1000
   //                   "40\8\0\0" for bvalue=40
-  // so we need to cut off the last 6 elements
-  const char* bval_string = ge_tagvalue_string.substr(0,ge_tagvalue_string.length()-6).c_str();
-  header_info.b_value = static_cast<unsigned int>(strtod( bval_string, &pEnd ));
+  int slashPos = ge_tagvalue_string.find('\\');
+  std::string bval_string = ge_tagvalue_string.substr(0, slashPos);
+  header_info.b_value = std::stoi(bval_string);
 
   // now retrieve the gradient direction
   if(success &&

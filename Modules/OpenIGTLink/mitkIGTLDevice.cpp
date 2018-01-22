644d1620a6d89766face2f37764be24d734484b5
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2018-01-22 17:45:35 +0100

    Fix reorder warning raised as error on Linux systems

--- Modules/OpenIGTLink/mitkIGTLDevice.cpp
+++ Modules/OpenIGTLink/mitkIGTLDevice.cpp
@@ -38,9 +38,9 @@ mitk::IGTLDevice::IGTLDevice(bool ReadFully) :
 m_State(mitk::IGTLDevice::Setup),
 m_Name("Unspecified Device"),
 m_StopCommunication(false),
-m_LogMessages(false),
 m_Hostname("127.0.0.1"),
 m_PortNumber(-1),
+m_LogMessages(false),
 m_MultiThreader(nullptr), m_SendThreadID(0), m_ReceiveThreadID(0), m_ConnectThreadID(0)
 {
   m_ReadFully = ReadFully;

c8bd2122ebc712c83d253ff12e6fc191cef20457
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2018-03-07 13:43:46 +0100

    Set z-axis as default axis for tool projection

--- Modules/IGT/Algorithms/mitkNeedleProjectionFilter.cpp
+++ Modules/IGT/Algorithms/mitkNeedleProjectionFilter.cpp
@@ -17,7 +17,6 @@ See LICENSE.txt or http://www.mitk.org for details.
 // MITK
 #include "mitkNeedleProjectionFilter.h"
 #include <mitkPlaneGeometry.h>
-// Vermutung - wird nicht benötigt: #include "mitkUSCombinedModality.h"
 
 // VTK
 #include <vtkPlane.h>
@@ -28,12 +27,12 @@ mitk::NeedleProjectionFilter::NeedleProjectionFilter()
   m_ShowToolAxis(false),
   m_SelectedInput(-1)
 {
-  // Tool Coordinates:x axis is chosen as default axis when no axis is specified
+  // Tool Coordinates: z-axis is chosen as default axis when no axis is specified
 
   MITK_DEBUG << "Constructor called";
 
   mitk::Point3D toolAxis;
-  mitk::FillVector3D(toolAxis, 1, 0, 0);
+  mitk::FillVector3D(toolAxis, 0, 0, 1);
   m_ToolAxis = toolAxis;
   InitializeOriginalPoints(toolAxis, m_ShowToolAxis);
 

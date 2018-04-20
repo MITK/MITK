5037f8aa35a0758a3dbea5a6c4996477bdbad3f0
Michael Goetz <m.goetz@dkfz-heidelberg.de>
2018-04-20 09:11:41 +0200

    Added FileConverter MiniApp

---
+++ Modules/BasicImageProcessing/MiniApps/FileConverter.cpp
@@ -0,0 +1,89 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center,
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+
+#include "mitkProperties.h"
+
+#include "mitkCommandLineParser.h"
+#include "mitkIOUtil.h"
+
+#include <itksys/SystemTools.hxx>
+
+#include "mitkPreferenceListReaderOptionsFunctor.h"
+
+
+int main(int argc, char* argv[])
+{
+  mitkCommandLineParser parser;
+
+  parser.setTitle("File Converter");
+  parser.setCategory("Basic Image Processing");
+  parser.setDescription("");
+  parser.setContributor("MBI");
+
+  parser.setArgumentPrefix("--","-");
+  // Add command line argument names
+  parser.addArgument("help", "h",mitkCommandLineParser::Bool, "Help:", "Show this help text");
+  parser.addArgument("input", "i", mitkCommandLineParser::InputFile, "Input file:", "Input File",us::Any(),false);
+  parser.addArgument("output", "o", mitkCommandLineParser::OutputFile, "Output file:", "Output file", us::Any(), false);
+  parser.addArgument("reader", "r", mitkCommandLineParser::String, "Reader Name", "Reader Name", us::Any());
+  parser.addArgument("list-readers", "lr", mitkCommandLineParser::Bool, "Reader Name", "Reader Name", us::Any());
+
+
+  std::map<std::string, us::Any> parsedArgs = parser.parseArguments(argc, argv);
+
+  if (parsedArgs.size()==0)
+      return EXIT_FAILURE;
+
+  // Show a help message
+  if ( parsedArgs.count("help") || parsedArgs.count("h"))
+  {
+    std::cout << parser.helpText();
+    return EXIT_SUCCESS;
+  }
+
+  std::string inputFilename = us::any_cast<std::string>(parsedArgs["input"]);
+  std::string outputFilename = us::any_cast<std::string>(parsedArgs["output"]);
+
+  mitk::PreferenceListReaderOptionsFunctor::ListType preference = {};
+  if (parsedArgs.count("reader"))
+  {
+    preference.push_back(us::any_cast<std::string>(parsedArgs["reader"]));
+  }
+  mitk::PreferenceListReaderOptionsFunctor::ListType emptyList = {};
+  mitk::IOUtil::LoadInfo info(inputFilename);
+  mitk::PreferenceListReaderOptionsFunctor functor = mitk::PreferenceListReaderOptionsFunctor(preference, emptyList);
+  functor(info);
+
+  std::string extension = itksys::SystemTools::GetFilenameExtension(inputFilename);
+  std::string filename = itksys::SystemTools::GetFilenameWithoutExtension(outputFilename);
+  std::string path = itksys::SystemTools::GetFilenamePath(outputFilename);
+
+  auto nodes = mitk::IOUtil::Load(inputFilename, &functor);
+
+  unsigned count = 0;
+  for (auto node : nodes)
+  {
+    std::string writeName = path + "/" + filename + extension;
+    if (count > 0)
+    {
+      writeName = path + "/" + filename + "_" + std::to_string(count) + extension;
+    }
+    mitk::IOUtil::Save(node, writeName);
+    ++count;
+  }
+
+  return EXIT_SUCCESS;
+}

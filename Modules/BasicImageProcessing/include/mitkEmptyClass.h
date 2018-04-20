1c7357a6772b70de23d7959231ea428c72f421e5
Michael Goetz <m.goetz@dkfz-heidelberg.de>
2018-04-20 09:20:33 +0200

    Having a working version of BasicImageProcessing

---
+++ Modules/BasicImageProcessing/include/mitkEmptyClass.h
@@ -0,0 +1,11 @@
+#ifndef mitkEmptyClass_h
+#define mitkEmptyClass_h
+
+
+class mitkEmptyClass {
+  public:
+    int m_Meaningless;
+    void foo();
+}
+
+#enddef
---
+++ Modules/BasicImageProcessing/src/mitkEmptyClass.cpp
@@ -0,0 +1,5 @@
+#include <mitkEmptyClass.h>
+
+void mitkEmptyClass::foo(){
+  
+};

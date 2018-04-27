0bad323643186c81247d498c5c8dacaedc46a983
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2018-04-27 09:50:14 +0200

    Remove install rules related to MINGW

--- CMake/mitkInstallRules.cmake
+++ CMake/mitkInstallRules.cmake
@@ -105,37 +105,6 @@ if(WIN32)
             CONFIGURATIONS Debug)
   endif()
 
-  #DCMTK Dlls install target (shared libs on gcc only)
-  if(MINGW AND DCMTK_ofstd_LIBRARY)
-    set(_dcmtk_libs
-        ${DCMTK_dcmdata_LIBRARY}
-        ${DCMTK_dcmimgle_LIBRARY}
-        ${DCMTK_dcmnet_LIBRARY}
-        ${DCMTK_ofstd_LIBRARY}
-       )
-
-    foreach(_dcmtk_lib ${_dcmtk_libs})
-      MITK_INSTALL(FILES ${_dcmtk_lib} )
-    endforeach()
-  endif()
-
-  #MinGW dll
-  if(MINGW)
-    find_library(MINGW_RUNTIME_DLL "mingwm10.dll" HINTS ${CMAKE_FIND_ROOT_PATH}/sys-root/mingw/bin)
-    if(MINGW_RUNTIME_DLL)
-      MITK_INSTALL(FILES ${MINGW_RUNTIME_DLL} )
-    else()
-      message(SEND_ERROR "Could not find mingwm10.dll which is needed for a proper install")
-    endif()
-
-    find_library(MINGW_GCC_RUNTIME_DLL "libgcc_s_dw2-1.dll" HINTS ${CMAKE_FIND_ROOT_PATH}/sys-root/mingw/bin)
-    if(MINGW_GCC_RUNTIME_DLL)
-      MITK_INSTALL(FILES ${MINGW_GCC_RUNTIME_DLL} )
-    else()
-      message(SEND_ERROR "Could not find libgcc_s_dw2-1.dll which is needed for a proper install")
-    endif()
-  endif()
-
 else()
 
   #DCMTK Dlls install target (shared libs on gcc only)

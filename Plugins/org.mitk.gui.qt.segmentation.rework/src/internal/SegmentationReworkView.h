3d415bac16a861aeb584238d59aa240702054c4a
Tobias Stein <t.stein@dkfz-heidelberg.de>
2018-08-29 09:14:44 +0200

    add initial segmentation rework plugin

---
+++ Plugins/org.mitk.gui.qt.segmentation.rework/src/internal/SegmentationReworkView.h
@@ -0,0 +1,59 @@
+/*===================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center,
+Division of Medical and Biological Informatics.
+All rights reserved.
+
+This software is distributed WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.
+
+See LICENSE.txt or http://www.mitk.org for details.
+
+===================================================================*/
+
+
+#ifndef SegmentationReworkView_h
+#define SegmentationReworkView_h
+
+#include <berryISelectionListener.h>
+
+#include <QmitkAbstractView.h>
+
+#include "ui_SegmentationReworkViewControls.h"
+
+/**
+  \brief SegmentationReworkView
+
+  \warning  This class is not yet documented. Use "git blame" and ask the author to provide basic documentation.
+
+  \sa QmitkAbstractView
+  \ingroup ${plugin_target}_internal
+*/
+class SegmentationReworkView : public QmitkAbstractView
+{
+  // this is needed for all Qt objects that should have a Qt meta-object
+  // (everything that derives from QObject and wants to have signal/slots)
+  Q_OBJECT
+
+public:
+  static const std::string VIEW_ID;
+
+protected:
+  virtual void CreateQtPartControl(QWidget *parent) override;
+
+  virtual void SetFocus() override;
+
+  /// \brief called by QmitkFunctionality when DataManager's selection has changed
+  virtual void OnSelectionChanged(berry::IWorkbenchPart::Pointer source,
+                                  const QList<mitk::DataNode::Pointer> &nodes) override;
+
+  /// \brief Called when the user clicks the GUI button
+  void DoImageProcessing();
+
+  Ui::SegmentationReworkViewControls m_Controls;
+};
+
+#endif // SegmentationReworkView_h

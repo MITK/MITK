62b01833b96629d8a3102acf67c45ed05f0999a3
KuznetsovAlexander <KuznetsovAlexander@rambler.ru>
2018-09-14 17:06:14 +0400

    Ability to override displayed names in data storage combobox.
    AUT-3892
    Signed-off-by: KuznetsovAlexander <KuznetsovAlexander@rambler.ru>

--- Modules/QtWidgets/include/QmitkDataStorageComboBox.h
+++ Modules/QtWidgets/include/QmitkDataStorageComboBox.h
@@ -200,6 +200,8 @@ class MITKQTWIDGETS_EXPORT QmitkDataStorageComboBox : public QComboBox
     ///
     virtual void Reset();
 
+    virtual QString GetDisplayedNodeName(const mitk::DataNode* node);
+
   protected:
     //#PROTECTED MEMBER VARS
     ///

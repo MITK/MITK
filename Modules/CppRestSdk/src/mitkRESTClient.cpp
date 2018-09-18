f843011e7fcfa86c4ed77b46b45e1de7feb96e12
Tobias Stein <t.stein@dkfz-heidelberg.de>
2018-09-18 16:25:39 +0200

    rest calls are returning a task on which can be waited to allow fully async processing image series instances can be downloaded parallel but displayed when all are finished

--- Modules/CppRestSdk/src/mitkRESTClient.cpp
+++ Modules/CppRestSdk/src/mitkRESTClient.cpp
@@ -18,11 +18,12 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include <mitkCommon.h>
 
-mitk::RESTClient::RESTClient(utility::string_t url) : m_Client(url) {}
+mitk::RESTClient::RESTClient(utility::string_t url) : m_Client(url) {
+}
 
 mitk::RESTClient::~RESTClient() {}
 
-void mitk::RESTClient::executeGETRequest(utility::string_t filePath, utility::string_t uri)
+pplx::task<void> mitk::RESTClient::executeGETRequest(utility::string_t filePath, utility::string_t uri)
 {
   MITK_INFO << "Calling GET with " << utility::conversions::to_utf8string(uri) << " on client "
             << utility::conversions::to_utf8string(m_Client.base_uri().to_string()) << " save into "
@@ -30,7 +31,7 @@ void mitk::RESTClient::executeGETRequest(utility::string_t filePath, utility::st
 
   auto fileBuffer = std::make_shared<concurrency::streams::streambuf<uint8_t>>();
 
-  concurrency::streams::file_buffer<uint8_t>::open(filePath, std::ios::out).then([=](concurrency::streams::streambuf<uint8_t> outFile) -> pplx::task<MitkResponse>
+  return concurrency::streams::file_buffer<uint8_t>::open(filePath, std::ios::out).then([=](concurrency::streams::streambuf<uint8_t> outFile) -> pplx::task<MitkResponse>
   {
     *fileBuffer = outFile;
 
@@ -47,14 +48,13 @@ void mitk::RESTClient::executeGETRequest(utility::string_t filePath, utility::st
     .then([=](size_t)
   {
     return fileBuffer->close();
-  })
-    // Wait for the entire response body to be written into the file.
-    .wait();
+  });
+  //  // Wait for the entire response body to be written into the file.
+  //  .wait();
 
-  return;
 }
 
-void mitk::RESTClient::executeWADOGET(utility::string_t filePath, std::string studyUID, std::string seriesUID, std::string instanceUID)
+pplx::task<void> mitk::RESTClient::executeWADOGET(utility::string_t filePath, std::string studyUID, std::string seriesUID, std::string instanceUID)
 {
   MitkUriBuilder builder(U("wado"));
   builder.append_query(U("requestType"), U("WADO"));
@@ -62,16 +62,21 @@ void mitk::RESTClient::executeWADOGET(utility::string_t filePath, std::string st
   builder.append_query(U("seriesUID"), utility::conversions::to_string_t(seriesUID));
   builder.append_query(U("objectUID"), utility::conversions::to_string_t(instanceUID));
   builder.append_query(U("contentType"), U("application/dicom"));
-  executeGETRequest(filePath, builder.to_string());
+  return executeGETRequest(filePath, builder.to_string());
 }
 
-void mitk::RESTClient::executeWADOGET(const utility::string_t folderPath, std::string studyUID, std::string seriesUID)
+pplx::task<void> mitk::RESTClient::executeWADOGET(const utility::string_t folderPath, std::string studyUID, std::string seriesUID)
 {
-  MitkUriBuilder builder(U("instances"));
-  builder.append_query(U("studyUID"), utility::conversions::to_string_t(studyUID));
-  builder.append_query(U("seriesUID"), utility::conversions::to_string_t(seriesUID));
+  MitkUriBuilder builder(U("rs/instances"));
+  builder.append_query(U("StudyInstanceUID"), utility::conversions::to_string_t(studyUID));
+  builder.append_query(U("SeriesInstanceUID"), utility::conversions::to_string_t(seriesUID));
 
-  m_Client.request(MitkRESTMethods::GET, builder.to_string()).then([=](MitkResponse response)
+  MITK_INFO << utility::conversions::to_utf8string(builder.to_string());
+  MitkRequest getSeries(MitkRESTMethods::GET);
+  getSeries.set_request_uri(builder.to_string());
+  getSeries.headers().add(U("Accept"), U("application/json"));
+
+  return m_Client.request(getSeries).then([=](MitkResponse response)
   {
     MITK_INFO << "search for instances in series with uid " << seriesUID
               << " status: " << response.status_code();
@@ -79,19 +84,32 @@ void mitk::RESTClient::executeWADOGET(const utility::string_t folderPath, std::s
     auto jsonListResult = response.extract_json().get();
     auto resultArray = jsonListResult.as_array();
 
+    std::vector<pplx::task<void>> tasks;
+
     for (unsigned short i = 0; i < resultArray.size(); i++)
     {
-      auto firstResult = resultArray[i];
-      auto sopInstanceUIDKey = firstResult.at(U("00080018"));
-      auto sopInstanceObject = sopInstanceUIDKey.as_object();
-      auto valueKey = sopInstanceObject.at(U("Value"));
-      auto sopInstanceUID = valueKey.as_string();
+      try 
+      {
+        auto firstResult = resultArray[i];
+        auto sopInstanceUIDKey = firstResult.at(U("00080018"));
+        auto sopInstanceObject = sopInstanceUIDKey.as_object();
+        auto valueKey = sopInstanceObject.at(U("Value"));
+        auto valueArray = valueKey.as_array();
+        auto sopInstanceUID = valueArray[0].as_string();
 
-      auto filePath = utility::string_t(folderPath)
-                        .append(sopInstanceUID)
-                        .append(U(".dcm"));
-      executeWADOGET(filePath, studyUID, seriesUID, utility::conversions::to_utf8string(sopInstanceUID));
+        auto filePath = utility::string_t(folderPath)
+          .append(sopInstanceUID)
+          .append(U(".dcm"));
+        auto task = executeWADOGET(filePath, studyUID, seriesUID, utility::conversions::to_utf8string(sopInstanceUID));
+        tasks.push_back(task);
+      }
+      catch (const web::json::json_exception& e) 
+      {
+        MITK_ERROR << e.what();
+      }
     }
-  });
 
+    auto joinTask = pplx::when_all(begin(tasks), end(tasks));
+    return joinTask;
+  });
 }

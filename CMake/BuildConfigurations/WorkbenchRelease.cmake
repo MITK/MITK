a831e1a0c0d1ee96d1acc94866db3865cdb7f82b
Marco Nolden <m.nolden@dkfz-heidelberg.de>
2018-10-12 11:01:27 +0200

    Improve CMake doxygen handling for WorkbenchRelease configuration
    Ref T25469

--- CMake/BuildConfigurations/WorkbenchRelease.cmake
+++ CMake/BuildConfigurations/WorkbenchRelease.cmake
@@ -18,5 +18,7 @@ set(MITK_CONFIG_PLUGINS ${MITK_CONFIG_PLUGINS}
 
 set(MITK_VTK_DEBUG_LEAKS OFF CACHE BOOL "Enable VTK Debug Leaks" FORCE)
 
+find_package(Doxygen REQUIRED)
+
 # Ensure that the in-application help can be build
 set(BLUEBERRY_QT_HELP_REQUIRED ON CACHE BOOL "Required Qt help documentation in plug-ins" FORCE)

62d5a57ea862a0c1686cddb4e660f902011b1cb9
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2018-12-12 15:37:59 +0100

    Fix static initialization
    The thread-safe static access code of Visual Studio 2017 15.9.4
    fails with a write access exception in Debug config otherwise.

--- Modules/DICOMQI/mitkDICOMQIIOMimeTypes.cpp
+++ Modules/DICOMQI/mitkDICOMQIIOMimeTypes.cpp
@@ -127,10 +127,8 @@ namespace mitk
   std::string MitkDICOMQIIOMimeTypes::DICOMSEG_MIMETYPE_NAME()
   {
     // create a unique and sensible name for this mime type
-    static std::string name = IOMimeTypes::DEFAULT_BASE_NAME() + ".image.dicom.seg";
-    return name;
+    return IOMimeTypes::DEFAULT_BASE_NAME() + ".image.dicom.seg";
   }
-
 
   //======= Mime Type DICOM PM =======
   //...

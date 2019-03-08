61ab7ce0948b792467622706d0d57fd24c72910c
Kim-Celine Kahl <k.kahl@dkfz-heidelberg.de>
2019-03-08 10:25:25 +0100

    removed old classes

--- Modules/CppRestSdk/files.cmake
+++ Modules/CppRestSdk/files.cmake
@@ -1,9 +1,6 @@
 file(GLOB_RECURSE H_FILES RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/include/*")
 
 set(CPP_FILES
-  mitkRESTServer.cpp
-  mitkRESTClient.cpp
-  mitkDICOMWeb.cpp
   mitkRESTClientMicroService.cpp
   mitkRESTServerMicroService.cpp
   mitkCppRestSdkActivator.cpp

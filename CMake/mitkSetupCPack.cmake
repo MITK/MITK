c42f88ba4a1f4f1cda6b21d02249090d59dce4df
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2019-03-09 20:57:39 +0100

    Improve installer versioning using git describe

--- CMake/mitkSetupCPack.cmake
+++ CMake/mitkSetupCPack.cmake
@@ -91,60 +91,44 @@ set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "MITK is a medical image processing tool")
 set(CPACK_PACKAGE_VENDOR "German Cancer Research Center (DKFZ)")
 set(CPACK_PACKAGE_DESCRIPTION_FILE "${MITK_SOURCE_DIR}/LICENSE.txt")
 set(CPACK_RESOURCE_FILE_LICENSE "${MITK_SOURCE_DIR}/LICENSE.txt")
-set(CPACK_PACKAGE_VERSION_MAJOR "${MITK_VERSION_MAJOR}")
-set(CPACK_PACKAGE_VERSION_MINOR "${MITK_VERSION_MINOR}")
+
+set(CPACK_PACKAGE_VERSION_MAJOR "${MITK_REVISION_DESC}")
+string(REPLACE " [local changes]" "-local_changes" CPACK_PACKAGE_VERSION_MAJOR ${CPACK_PACKAGE_VERSION_MAJOR})
 
 # tell cpack to strip all debug symbols from all files
 set(CPACK_STRIP_FILES ON)
 
-# append revision number if available
-if(MITK_REVISION_ID AND MITK_VERSION_PATCH STREQUAL "99")
-  if(MITK_WC_TYPE STREQUAL "git")
-    set(git_hash ${MITK_REVISION_ID})
-    string(LENGTH "${git_hash}" hash_length)
-    if(hash_length GREATER 6)
-      string(SUBSTRING ${git_hash} 0 6 git_hash)
-    endif()
-    set(CPACK_PACKAGE_VERSION_PATCH "${MITK_VERSION_PATCH}_r${git_hash}")
-  else()
-    set(CPACK_PACKAGE_VERSION_PATCH "${MITK_VERSION_PATCH}_r${MITK_REVISION_ID}")
-  endif()
-else()
-  set(CPACK_PACKAGE_VERSION_PATCH "${MITK_VERSION_PATCH}")
-endif()
-
 # set version
-set(CPACK_PACKAGE_VERSION
-  "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
+set(CPACK_PACKAGE_VERSION ${CPACK_PACKAGE_VERSION_MAJOR})
 
 # determine possible system specific extension
 set(CPACK_PACKAGE_ARCH "unkown-architecture")
 
 if(${CMAKE_SYSTEM_NAME} MATCHES Windows)
   if(CMAKE_CL_64)
-    set(CPACK_PACKAGE_ARCH "win64")
+    set(CPACK_PACKAGE_ARCH "windows-x86_64")
   elseif(WIN32)
-    set(CPACK_PACKAGE_ARCH "win32")
+    set(CPACK_PACKAGE_ARCH "windows-x86")
   endif()
-endif(${CMAKE_SYSTEM_NAME} MATCHES Windows)
+endif()
 
 if(${CMAKE_SYSTEM_NAME} MATCHES Linux)
   if(${CMAKE_SYSTEM_PROCESSOR} MATCHES i686)
-    set(CPACK_PACKAGE_ARCH "linux32")
+    set(CPACK_PACKAGE_ARCH "linux-x86")
   elseif(${CMAKE_SYSTEM_PROCESSOR} MATCHES x86_64)
     if(${CMAKE_CXX_FLAGS} MATCHES " -m32 ")
-      set(CPACK_PACKAGE_ARCH "linux32")
+      set(CPACK_PACKAGE_ARCH "linux-x86")
     else()
-      set(CPACK_PACKAGE_ARCH "linux64")
-    endif(${CMAKE_CXX_FLAGS} MATCHES " -m32 ")
+      set(CPACK_PACKAGE_ARCH "linux-x86_64")
+    endif()
   else()
     set(CPACK_PACKAGE_ARCH "linux")
   endif()
-endif(${CMAKE_SYSTEM_NAME} MATCHES Linux)
+endif()
 
 if(${CMAKE_SYSTEM_NAME} MATCHES Darwin)
-  set(CPACK_PACKAGE_ARCH "mac64")
-endif(${CMAKE_SYSTEM_NAME} MATCHES Darwin)
+  set(CPACK_PACKAGE_ARCH "macos-x86_64")
+endif()
 
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_ARCH}")
 

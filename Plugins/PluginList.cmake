2d1e095159f9b5f67f3f2a5505a606eeb8da230c
Kim-Celine Kahl <k.kahl@dkfz-heidelberg.de>
2019-03-22 11:05:29 +0100

    removed testing plugin

--- Plugins/PluginList.cmake
+++ Plugins/PluginList.cmake
@@ -112,5 +112,4 @@ set(MITK_PLUGINS
   org.mitk.gui.qt.pharmacokinetics.simulation:OFF
   org.mitk.gui.qt.pharmacokinetics.curvedescriptor:OFF
   org.mitk.gui.qt.pharmacokinetics.concentration.mri:OFF
-  org.mitk.gui.qt.cpprestexample:OFF
 )

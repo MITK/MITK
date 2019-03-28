f52a208ca6eebe3324445bd763f5258b2be85e95
Amir Kalali <a.kalali@dkfz-heidelberg.de>
2019-03-28 10:24:25 +0100

    Add class documentation and remove unnecessary code

--- Modules/QtWidgets/include/QmitkRenderWindowWidget.h
+++ Modules/QtWidgets/include/QmitkRenderWindowWidget.h
@@ -33,9 +33,12 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include <QHBoxLayout>
 
 /**
-* @brief 
-*
-*
+* @brief The 'QmitkRenderWindowWidget' is a QWidget that holds a render window
+*        and some associates properties, like a crosshair (pointset) and decorations.
+*        Decorations are corner annotation (text and color), frame color or background color
+*        and can be set using this class.
+*        The 'QmitkRenderWindowWidget' is used inside the 'QmitkMxNMultiWidget', where a map contains
+*        several render window widgets to create the MxN display.
 */
 class MITKQTWIDGETS_EXPORT QmitkRenderWindowWidget : public QWidget
 {
@@ -78,14 +81,6 @@ public:
   void SetCornerAnnotationText(const std::string& cornerAnnotation);
   std::string GetCornerAnnotationText() const;
 
-  /**
-  * @brief Create a corner annotation and a colored rectangle for this widget.
-  *
-  * @par text   The text of the corner annotation.
-  * @par color  The color of the text and the rectangle.
-  */
-  void SetDecorationProperties(std::string text, mitk::Color color);
-
   bool IsRenderWindowMenuActivated() const;
 
   void ActivateCrosshair(bool activate);
@@ -105,7 +100,6 @@ private:
 
   mitk::RenderingManager::Pointer m_RenderingManager;
   mitk::BaseRenderer::RenderingMode::Type m_RenderingMode;
-  mitk::SliceNavigationController* m_TimeNavigationController;
 
   mitk::DataNode::Pointer m_PointSetNode;
   mitk::PointSet::Pointer m_PointSet;

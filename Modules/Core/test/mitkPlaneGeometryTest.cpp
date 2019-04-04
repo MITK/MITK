6b4ea15d815533e680b372f31aed11fe4afe57b9
Peter Neher <p.neher@dkfz-heidelberg.de>
2019-04-04 10:44:43 +0200

    Check if matrix is rotation. Safe calculate dominant axes.
    Summary: Add tests if rotation matrix check and default dominant axes  calculation work.
    
    Test Plan: Added additonal tests if imperfect rotation matrices are caught. Normal behaviour should be covered by current tests.
    
    Reviewers: O1 MITK Core, kislinsk
    
    Reviewed By: O1 MITK Core, kislinsk
    
    Subscribers: kislinsk
    
    Maniphest Tasks: T25682
    
    Differential Revision: https://phabricator.mitk.org/D192

--- Modules/Core/test/mitkPlaneGeometryTest.cpp
+++ Modules/Core/test/mitkPlaneGeometryTest.cpp
@@ -32,6 +32,8 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include <fstream>
 #include <iomanip>
+#include <mitkIOUtil.h>
+#include <mitkImage.h>
 
 static const mitk::ScalarType testEps = 1E-9; // the epsilon used in this test == at least float precision.
 
@@ -50,6 +52,8 @@ class mitkPlaneGeometryTestSuite : public mitk::TestFixture
   MITK_TEST(TestFrontalInitialization);
   MITK_TEST(TestSaggitalInitialization);
   MITK_TEST(TestLefthandedCoordinateSystem);
+  MITK_TEST(TestDominantAxesError);
+  MITK_TEST(TestCheckRotationMatrix);
 
   // Currently commented out, see See bug 15990
   // MITK_TEST(testPlaneGeometryInitializeOrder);
@@ -112,6 +116,21 @@ public:
     plane = mitk::PlaneGeometry::New();
     mitk::AbstractTransformGeometry::Pointer atg = dynamic_cast<mitk::AbstractTransformGeometry *>(plane.GetPointer());
     CPPUNIT_ASSERT_MESSAGE("PlaneGeometry should not be castable to AbstractTransofrmGeometry", atg.IsNull());
+  }
+
+  void TestDominantAxesError()
+  {
+    auto image = mitk::IOUtil::Load<mitk::Image>(GetTestDataFilePath("NotQuiteARotationMatrix.nrrd"));
+    auto matrix = image->GetGeometry()->GetIndexToWorldTransform()->GetMatrix().GetVnlMatrix().transpose();
+    std::vector< int > axes = mitk::PlaneGeometry::CalculateDominantAxes(matrix);
+    CPPUNIT_ASSERT_MESSAGE("Domiant axes cannot be determined in this dataset. Output should be default ordering.", axes.at(0)==0 && axes.at(1)==1 && axes.at(2)==2);
+  }
+
+  void TestCheckRotationMatrix()
+  {
+    auto image = mitk::IOUtil::Load<mitk::Image>(GetTestDataFilePath("NotQuiteARotationMatrix.nrrd"));
+    bool is_rotation = mitk::PlaneGeometry::CheckRotationMatrix(image->GetGeometry()->GetIndexToWorldTransform());
+    CPPUNIT_ASSERT_MESSAGE("Since the test data matrix is not quite a rotation matrix, this should be detected.", !is_rotation);
   }
 
   void TestLefthandedCoordinateSystem()

6b8db8bc0ed3bff88cd4f3ed9ae899932137c344
Clemens Hentschke <c.hentschke@dkfz-heidelberg.de>
2019-04-09 16:56:13 +0200

    removed unused include

--- Modules/ImageStatisticsUI/Qmitk/QmitkImageStatisticsTreeItem.cpp
+++ Modules/ImageStatisticsUI/Qmitk/QmitkImageStatisticsTreeItem.cpp
@@ -14,8 +14,6 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 ===================================================================*/
 
-#include <QStringList>
-
 #include "QmitkImageStatisticsTreeItem.h"
 
 QmitkImageStatisticsTreeItem::QmitkImageStatisticsTreeItem(

13d93c5bb6a583cc29b489971080d1c512f22681
Peter Neher <p.neher@dkfz-heidelberg.de>
2019-04-10 10:22:47 +0200

    Remove tests

--- Modules/QtPython/Testing/files.cmake
+++ Modules/QtPython/Testing/files.cmake
@@ -1,13 +1,3 @@
 set(MODULE_TESTS
   mitkPythonTest.cpp
 )
-
-#TODO: temporarily disabled untill segfault is fixed (bug-19152)
-if(UNIX)
-  set(MODULE_TESTS ${MODULE_TESTS} mitkVtkPythonTest.cpp)
-endif()
-
-if(MITK_USE_OpenCV)
-  set(MODULE_TESTS ${MODULE_TESTS} mitkCvPythonTest.cpp)
-endif()
-

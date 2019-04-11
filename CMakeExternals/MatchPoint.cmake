6052e9546c03866ebd37ecb408cee175c20cd3a1
Amir Kalali <a.kalali@dkfz-heidelberg.de>
2019-04-11 18:00:34 +0200

    Use new version of MatchPoint

--- CMakeExternals/MatchPoint.cmake
+++ CMakeExternals/MatchPoint.cmake
@@ -26,8 +26,8 @@ if(MITK_USE_MatchPoint)
       set(download_step SOURCE_DIR ${MatchPoint_SOURCE_DIR})
     else()
       set(download_step
-          URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/MatchPoint_rev_24ef6072.tar.gz
-          URL_MD5 b0a0d7d63da5071db8e73dd6e6b4db7c
+          URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/MatchPoint_rev_75012b13.tar.gz
+          URL_MD5 233c2c717bd11b25bc12c2ed54abb3f1
          )
     endif()
 

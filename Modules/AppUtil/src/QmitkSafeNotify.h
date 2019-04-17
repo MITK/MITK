1f011ade53f7bf47da5fa226a7cc94926a4dee41
Sergey Khlutchin <sercxjo@gmail.com>
2019-04-17 19:01:45 +0400

    Removed yet another default catch

--- Modules/AppUtil/src/QmitkSafeNotify.h
+++ Modules/AppUtil/src/QmitkSafeNotify.h
@@ -25,56 +25,7 @@ See LICENSE.txt or http://www.mitk.org for details.
 template<class A>
 bool QmitkSafeNotify(A* app, QObject* receiver, QEvent* event)
 {
-  QString msg;
-#ifdef NDEBUG
-  try
-#endif
-  {
-    return app->A::notify(receiver, event);
-  }
-#ifdef NDEBUG
-  catch (mitk::Exception& e)
-  {
-    msg = QString("MITK Exception:\n\n")
-          + QString("Description: ")
-          + QString(e.GetDescription()) + QString("\n\n")
-          + QString("Filename: ") + QString(e.GetFile()) + QString("\n\n")
-          + QString("Line: ") + QString::number(e.GetLine());
-  }
-  catch (std::exception& e)
-  {
-    msg = e.what();
-  }
-  catch (...)
-  {
-    msg = "Unknown exception";
-  }
-  MITK_ERROR << "An error occurred: " << msg.toStdString();
-
-  QMessageBox msgBox;
-  msgBox.setText(QObject::tr("An error occurred. You should save all data and quit the program to prevent possible data loss."));
-  msgBox.setDetailedText(msg);
-  msgBox.setIcon(QMessageBox::Critical);
-  /*: Button in dialog due to exception */
-  msgBox.addButton(QObject::tr("Exit immediately"), QMessageBox::YesRole);
-  /*: Button to ignore exception and continue */
-  msgBox.addButton(QObject::tr("Ignore"), QMessageBox::NoRole);
-
-  int ret = msgBox.exec();
-
-  switch(ret)
-  {
-  case 0:
-    MITK_ERROR << "The program was closed.";
-    app->closeAllWindows();
-    break;
-  case 1:
-    MITK_ERROR << "The error was ignored by the user. The program may be in a corrupt state and don't behave like expected!";
-    break;
-  }
-
-  return false;
-#endif /*NDEBUG*/
+  return app->A::notify(receiver, event);
 }
 
 #endif // QMITKSAFENOTIFY_H

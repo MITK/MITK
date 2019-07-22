617e8f0688df80276f1f82dd18b2aeccc2db7707
Ina Kompan <i.kompan@dkfz-heidelberg.de>
2019-07-22 08:06:27 +0200

    Move mitkTestDynamicImageGenerator to mitkCore/TestingHelper

--- Modules/Core/TestingHelper/include/mitkTestDynamicImageGenerator.h
+++ Modules/Core/TestingHelper/include/mitkTestDynamicImageGenerator.h
@@ -23,23 +23,22 @@ See LICENSE.txt or http://www.mitk.org for details.
 #include "mitkImage.h"
 #include "mitkImagePixelReadAccessor.h"
 
-#include "MitkModelFitExports.h"
+#include <MitkTestingHelperExports.h>
 
 namespace mitk
 {
   typedef itk::Image<int> TestImageType;
   typedef itk::Image<unsigned char> TestMaskType;
 
-  TestImageType::Pointer MITKMODELFIT_EXPORT GenerateTestImage(int factor = 1);
+  TestImageType::Pointer MITKTESTINGHELPER_EXPORT GenerateTestImage(int factor = 1);
 
-  TestMaskType::Pointer MITKMODELFIT_EXPORT GenerateTestMask();
+  TestMaskType::Pointer MITKTESTINGHELPER_EXPORT GenerateTestMask();
 
-  Image::Pointer MITKMODELFIT_EXPORT GenerateTestFrame(double timePoint);
+  Image::Pointer MITKTESTINGHELPER_EXPORT GenerateTestFrame(double timePoint);
 
-  Image::Pointer MITKMODELFIT_EXPORT GenerateTestMaskMITK();
+  Image::Pointer MITKTESTINGHELPER_EXPORT GenerateTestMaskMITK();
 
-  Image::Pointer MITKMODELFIT_EXPORT GenerateDynamicTestImageMITK();
-
+  Image::Pointer MITKTESTINGHELPER_EXPORT GenerateDynamicTestImageMITK();
 }
 
 #endif

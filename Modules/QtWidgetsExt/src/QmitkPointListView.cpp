2eadf42af764dbf778d425fbf936a3fa1729d807
Amir Kalali <a.kalali@dkfz-heidelberg.de>
2019-08-06 18:31:13 +0200

    Fix multi widget render window access

--- Modules/QtWidgetsExt/src/QmitkPointListView.cpp
+++ Modules/QtWidgetsExt/src/QmitkPointListView.cpp
@@ -81,9 +81,12 @@ const mitk::PointSet *QmitkPointListView::GetPointSet() const
 void QmitkPointListView::SetMultiWidget(QmitkStdMultiWidget *multiWidget)
 {
   m_MultiWidget = multiWidget;
-  this->AddSliceNavigationController(multiWidget->GetRenderWindow1()->GetSliceNavigationController());
-  this->AddSliceNavigationController(multiWidget->GetRenderWindow1()->GetSliceNavigationController());
-  this->AddSliceNavigationController(multiWidget->GetRenderWindow1()->GetSliceNavigationController());
+  if (nullptr != m_MultiWidget)
+  {
+    AddSliceNavigationController(m_MultiWidget->GetRenderWindow1()->GetSliceNavigationController());
+    AddSliceNavigationController(m_MultiWidget->GetRenderWindow2()->GetSliceNavigationController());
+    AddSliceNavigationController(m_MultiWidget->GetRenderWindow3()->GetSliceNavigationController());
+  }
 }
 
 QmitkStdMultiWidget *QmitkPointListView::GetMultiWidget() const

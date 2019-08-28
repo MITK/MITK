b34059bcb29a143adf78f2f8a6e996e3bec0a7dd
Daniel Maleike <daniel.maleike@stryker.com>
2019-08-28 09:20:44 +0200

    Comment on an implementation detail that might look strange
    Signed-off-by: Daniel Maleike <daniel.maleike@stryker.com>

--- Modules/Gizmo/src/mitkGizmoInteractor.cpp
+++ Modules/Gizmo/src/mitkGizmoInteractor.cpp
@@ -199,6 +199,12 @@ void mitk::GizmoInteractor::DecideInteraction(StateMachineAction *, InteractionE
     case Gizmo::ScaleX:
     case Gizmo::ScaleY:
     case Gizmo::ScaleZ:
+      // Implementation note: Why didn't we implement per-axis scaling yet?
+      // When this was implemented, the mitk::ScaleOperation was able to only describe
+      // uniform scaling around a central point. Since we use operations for all modifications
+      // in order to have undo/redo working, any axis-specific scaling should also
+      // use operations.
+      // Consequence: enhance ScaleOperation when there is need to have scaling per axis.
       decision = InternalEvent::New(interactionEvent->GetSender(), this, "ScaleEqually");
       break;
     default:

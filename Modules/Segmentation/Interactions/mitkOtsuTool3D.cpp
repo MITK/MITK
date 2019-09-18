6a5056d1e6d3a1508c4499e7bbd8d39782a43e47
BlackCatter <iblackcatw@gmail.com>
2019-09-18 14:19:46 +0400

    Fix ul and otsu on binary segmentations. AUT-4364

--- Modules/Segmentation/Interactions/mitkOtsuTool3D.cpp
+++ Modules/Segmentation/Interactions/mitkOtsuTool3D.cpp
@@ -175,8 +175,29 @@ void mitk::OtsuTool3D::ConfirmSegmentation()
 {
   mitk::LabelSetImage::Pointer resultImage = mitk::LabelSetImage::New();
   resultImage->InitializeByLabeledImage(dynamic_cast<mitk::Image*>(m_BinaryPreviewNode->GetData()));
-  GetTargetSegmentationNode()->SetData(resultImage);
 
+  auto targetSeg = GetTargetSegmentationNode();
+  if (0 == strcmp(targetSeg->GetData()->GetNameOfClass(), resultImage->GetNameOfClass())) {
+    targetSeg->SetData(resultImage);
+  } else {
+    auto propList = targetSeg->GetPropertyList();
+    
+    std::string name = "No Name!", caption = "No Name!";
+    mitk::ColorProperty::Pointer color;
+    float opacity = 1.f;
+    propList->GetStringProperty("name", name);
+    propList->GetStringProperty("caption", caption);
+    propList->GetFloatProperty("opacity", opacity);
+    color = dynamic_cast<mitk::ColorProperty*>(propList->GetProperty("color"));
+
+    targetSeg->SetData(resultImage);
+
+    targetSeg->SetStringProperty("name", name.c_str());
+    targetSeg->SetStringProperty("caption", caption.c_str());
+    targetSeg->SetFloatProperty("opactity", opacity);
+    targetSeg->SetProperty("color", color);
+  }
+   
   m_ToolManager->ActivateTool(-1);
 }
 

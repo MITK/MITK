65ee9adb4601837116f422d18ccb9ebc24f6678a
Ralf Floca <r.floca@dkfz-heidelberg.de>
2020-01-23 15:56:36 +0100

    Fixed T27026
    Deprecate MatchPoint Batch Registration View
    
    Signed-off-by: Ralf Floca <r.floca@dkfz-heidelberg.de>

--- Plugins/org.mitk.gui.qt.matchpoint.algorithm.batch/manifest_headers.cmake
+++
@@ -1,5 +0,0 @@
-set(Plugin-Name "MatchPoint Registration Batch Processing")
-set(Plugin-Version "0.2")
-set(Plugin-Vendor "German Cancer Research Center (DKFZ)")
-set(Plugin-ContactAddress "http://www.mitk.org")
-set(Require-Plugin org.mitk.gui.qt.common org.mitk.gui.qt.datamanager org.mitk.matchpoint.core.helper)

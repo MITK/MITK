6886faaa489a5eef783b416de94fb6539ad41211
Sergey Khlutchin <sercxjo@gmail.com>
2020-03-06 12:57:29 +0400

    Slash in path. AUT-3733

--- Modules/Core/src/Rendering/mitkVtkFont.cpp
+++ Modules/Core/src/Rendering/mitkVtkFont.cpp
@@ -8,7 +8,7 @@ void mitk::setUnicodeFont(vtkTextProperty* prop)
   prop->SetFontFamily(VTK_FONT_FILE);
   if (s_FontPath.empty())
   {
-    s_FontPath = Utilities::preferredPath(Utilities::absPath(std::string("Fonts\\DejaVuSans.ttf")));
+    s_FontPath = Utilities::preferredPath(Utilities::absPath(std::string("Fonts/DejaVuSans.ttf")));
   }
   prop->SetFontFile(s_FontPath.c_str());
 }

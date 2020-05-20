0fffdc36edf150fecb15a2f22a56237f7488903e
Sergey Denisov <s.s.denisov@yandex.ru>
2020-05-20 17:18:04 +0400

    show TrackingPreferencePage by default in release
    NAVI-1232

--- Plugins/org.mitk.gui.qt.application/src/QmitkPreferencesDialog.cpp
+++ Plugins/org.mitk.gui.qt.application/src/QmitkPreferencesDialog.cpp
@@ -66,7 +66,11 @@ namespace {
     , "org.mitk.gui.qt.application.LanguagePreferencePage"
     , "org.mitk.gui.qt.application.UserDataPreferencePagePage"
     , "org.mitk.gui.qt.application.SeriesTagsPreferencePage"
-    , "org.mitk.gui.qt.application.PacsPreferencePage" });
+    , "org.mitk.gui.qt.application.PacsPreferencePage"
+#ifdef DELIVERY_BUILD
+    , "org.mitk.gui.qt.application.TrackingPreferencePage"
+#endif
+    });
 }
 
 class QmitkPreferencesDialogPrivate : public Ui::QmitkPreferencesDialog

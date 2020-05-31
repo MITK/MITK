54790aa7c230b671ad021190a02125d37d2f4770
Ralf Floca <r.floca@dkfz-heidelberg.de>
2020-05-31 12:36:05 +0200

    Added more information according the persistance of mitk::Identifiable UIDs.
    Signed-off-by: Ralf Floca <r.floca@dkfz-heidelberg.de>

--- Modules/Core/include/mitkIdentifiable.h
+++ Modules/Core/include/mitkIdentifiable.h
@@ -21,7 +21,23 @@ namespace mitk
   /**
    * \brief Base class of identifiable objects.
    *
-   * If you want to change the unique ID after creation time, you can use
+   * Offers an interface to query a UID for the instance. Can be inherited
+   * by other classes to provide this capability.\n\n
+   * What does the UID stand for/what is its scope?\n
+   * - It is unique in its creation, but it is not a content specific unique ID. Therfore:
+   *  - A class instance, associated with a UID, may change its values over its lifetime,
+        but still have the same UID.
+      - If a class instance gets persistet and loaded multiple times, then theire could
+        be several instances with the same UID.
+      - UIDs are therefore more simelar to git paths than to git hashes. They identify something, but the state of
+        something can change (stream of commits). The difference of the UID compared to using e.g. plain instance
+        pointers to identify an object is that UIDs allow the feature of serialization as they abstract from the
+        memory location of the current runtime environment.
+     - It is up to the application that builds upon that feature to ensure appropriate usage within
+       the application scope.
+   * @remark It is not a feature of mitk::Identifiable per se to be persistable. It depends on classes that derive
+   * from mitk::Identifiable, if and how they implement a persistancy mechanism for their MITK UID.
+   * @remark If you want to change the unique ID after creation time, you can use
    * the mitk::UIDManipulator class. The reationale behind this pattern is
    * to ensure that you think twice before doing this. It is intended to be
    * used by data readers if necessary at all.

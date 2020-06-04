91cb0d9f7abb5010a2533e511b6c9b9b16d9420c
Sergey Khlutchin <sercxjo@gmail.com>
2020-06-04 13:38:22 +0400

    Improved cmake-files for yaml-cpp library. AUT-4716

--- CMakeExternals/yaml-cpp.cmake
+++ CMakeExternals/yaml-cpp.cmake
@@ -6,23 +6,18 @@ set(proj yaml-cpp)
 set(proj_DEPENDENCIES )
 set(${proj}_DEPENDS ${proj})
 
-message(STATUS "yaml-cpp: ${YAML_CPP_DIR} , ${yaml-cpp_DIR} , ${YAML_CPP_INCLUDE_DIR} , ${YAML_CPP_LIBRARIES} .")
-
 if(NOT DEFINED yaml-cpp_DIR OR NOT EXISTS ${yaml-cpp_DIR})
   find_package(yaml-cpp)
-  message(STATUS "yaml-cpp2: ${YAML_CPP_DIR} , ${yaml-cpp_DIR} , ${YAML_CPP_INCLUDE_DIR} , ${YAML_CPP_LIBRARIES} .")
 endif()
 
 if(NOT DEFINED yaml-cpp_DIR OR NOT EXISTS ${yaml-cpp_DIR})
-      # add_library(GLUT SHARED IMPORTED)
-      # set_property(TARGET GLUT PROPERTY IMPORTED_LOCATION ${GLUT_LIBRARY})
-  #mitkMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
 
   set(additional_cmake_args )
 
   ExternalProject_Add(${proj}
     LIST_SEPARATOR ${sep}
     GIT_REPOSITORY "git@github.com:samsmu/yaml-cpp.git"
+    GIT_TAG "yaml-cpp-0.6.3"
     CMAKE_GENERATOR ${gen}
     CMAKE_ARGS
       ${ep_common_args}
@@ -34,15 +29,11 @@ if(NOT DEFINED yaml-cpp_DIR OR NOT EXISTS ${yaml-cpp_DIR})
     DEPENDS ${proj_DEPENDENCIES}
   )
 
-  message(STATUS "yaml-cpp3: ${YAML_CPP_DIR} , ${yaml-cpp_DIR} , ${YAML_CPP_INCLUDE_DIR} , ${YAML_CPP_LIBRARIES} .")
-
   set(${proj}_DIR ${ep_prefix})
   mitkFunctionInstallExternalCMakeProject(${proj})
 
-  message(STATUS "yaml-cpp4: ${YAML_CPP_DIR} , ${yaml-cpp_DIR} , ${YAML_CPP_INCLUDE_DIR} , ${YAML_CPP_LIBRARIES} .")
 else()
 
   mitkMacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
-  message(STATUS "yaml-cpp found: ${YAML_CPP_DIR} , ${yaml-cpp_DIR} , ${YAML_CPP_INCLUDE_DIR} , ${YAML_CPP_LIBRARIES} .")
 
 endif()

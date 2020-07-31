b9bfb2ec4e41b044bb30718bc97fdd6e49f7e546
Ralf Floca <r.floca@dkfz-heidelberg.de>
2020-07-31 12:45:46 +0200

    Corrected mitk::AutoSegmentationTool::GetTargetSegmentationNode() to use the current segementation as template if a new segmentation should be generated.

--- Modules/Segmentation/Interactions/mitkAutoSegmentationTool.cpp
+++ Modules/Segmentation/Interactions/mitkAutoSegmentationTool.cpp
@@ -72,12 +72,8 @@ std::string mitk::AutoSegmentationTool::GetCurrentSegmentationName()
 
 mitk::DataNode *mitk::AutoSegmentationTool::GetTargetSegmentationNode()
 {
-  mitk::DataNode::Pointer emptySegmentation;
-  if (m_OverwriteExistingSegmentation)
-  {
-    emptySegmentation = m_ToolManager->GetWorkingData(0);
-  }
-  else
+  mitk::DataNode::Pointer segmentationNode = m_ToolManager->GetWorkingData(0);
+  if (!m_OverwriteExistingSegmentation)
   {
     mitk::DataNode::Pointer refNode = m_ToolManager->GetReferenceData(0);
     if (refNode.IsNull())
@@ -86,13 +82,16 @@ mitk::DataNode *mitk::AutoSegmentationTool::GetTargetSegmentationNode()
       MITK_ERROR << "No valid reference data!";
       return nullptr;
     }
+
     std::string nodename = m_ToolManager->GetReferenceData(0)->GetName() + "_" + this->GetName();
     mitk::Color color;
     color.SetRed(1);
     color.SetBlue(0);
     color.SetGreen(0);
-    emptySegmentation = CreateEmptySegmentationNode(dynamic_cast<mitk::Image *>(refNode->GetData()), nodename, color);
-    m_ToolManager->GetDataStorage()->Add(emptySegmentation, refNode);
+    //create a new segmentation node based on the current segmentation as template
+    segmentationNode = CreateEmptySegmentationNode(dynamic_cast<mitk::Image *>(segmentationNode->GetData()), nodename, color);
+
+    m_ToolManager->GetDataStorage()->Add(segmentationNode, refNode);
   }
-  return emptySegmentation;
+  return segmentationNode;
 }

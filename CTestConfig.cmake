19ef2f4d14d022af31939e54c63b398f76823e31
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2020-08-04 10:34:27 +0200

    Remove everything related to subprojects/labels
    This happened in preparation of the dashboard script rewrite to unravel
    things a bit. We planned to get rid of the current subprojects anyway,
    as they were not maintained for years and do not actually have any
    surplus value on the dashboard.
    
    We may consider reintroducing subprojects based on a different concept
    like MITK extensions.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,5 +1,3 @@
-set(CTEST_PROJECT_NAME_SUPERBUILD "MITK-superbuild")
 set(CTEST_PROJECT_NAME "MITK")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
-
+set(CTEST_NIGHTLY_START_TIME "23:00:00 UTC")
 set(CTEST_SUBMIT_URL "https://cdash.mitk.org/submit.php?project=MITK")

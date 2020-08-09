35c2ceeae5171c4ff33b65b6be198a4fd1d05c3a
Ralf Floca <r.floca@dkfz-heidelberg.de>
2020-08-09 16:20:40 +0200

    Improved stitching mini app handling

--- Modules/MatchPointRegistration/cmdapps/StitchImagesMiniApp.cpp
+++ Modules/MatchPointRegistration/cmdapps/StitchImagesMiniApp.cpp
@@ -66,16 +66,16 @@ void setupParser(mitkCommandLineParser& parser)
     parser.endGroup();
 
     parser.beginGroup("Optional parameters");
-    parser.addArgument("reference",
-      "x",
+    parser.addArgument("template",
+      "t",
       mitkCommandLineParser::File,
-      "Output reference image.",
-      "File path to an image that serves as reference for the output geometry.",
+      "Output template image.",
+      "File path to an image that serves as template for the output geometry.",
       us::Any(),
       false, false, false, mitkCommandLineParser::Input);
     parser.addArgument(
       "registrations", "r", mitkCommandLineParser::StringList, "Registration files", "Pathes to the registrations that should be used to map the input images. If this parameter is not set, identity transforms are assumed. If this parameter is set, it must have the same number of entries then the parameter inputs. If you want to use and identity transform for a specific input, specify an empty string. The application assumes that inputs and registrations have the same order, so the n-th input should use thr n-th registration.", us::Any(), true, false, false, mitkCommandLineParser::Input);
-    parser.addArgument("interpolator", "i", mitkCommandLineParser::Int, "Interpolator type", "Interpolator used for mapping the images. Default: 2; allowed values: 1: Nearest Neighbour, 2: Linear, 3: BSpline 3, 4: WSinc Hamming, 5: WSinc Welch", us::Any(2), true);
+    parser.addArgument("interpolator", "n", mitkCommandLineParser::Int, "Interpolator type", "Interpolator used for mapping the images. Default: 2; allowed values: 1: Nearest Neighbour, 2: Linear, 3: BSpline 3, 4: WSinc Hamming, 5: WSinc Welch", us::Any(2), true);
     parser.addArgument("strategy", "s", mitkCommandLineParser::Int, "Stitch strategy", "Strategy used for stitching the images. 0: Mean -> computes the mean value of all input images that cover an output pixel (default strategy). 1: BorderDistance -> Uses the input pixel that has the largest minimal distance to its image borders", us::Any(2), true);
     parser.addArgument("padding", "p", mitkCommandLineParser::Float, "Padding value", "Value used for output voxels that are not covered by any input image.", us::Any(0.), true);
     parser.addArgument("help", "h", mitkCommandLineParser::Bool, "Help:", "Show this help text");
@@ -85,15 +85,17 @@ void setupParser(mitkCommandLineParser& parser)
 
 bool configureApplicationSettings(std::map<std::string, us::Any> parsedArgs)
 {
+  try
+  {
     if (parsedArgs.size() == 0)
-        return false;
+      return false;
 
     inFilenames = us::any_cast<mitkCommandLineParser::StringContainerType>(parsedArgs["inputs"]);
     outFileName = us::any_cast<std::string>(parsedArgs["output"]);
 
-    if (parsedArgs.count("reference"))
+    if (parsedArgs.count("template"))
     {
-      refGeometryFileName = us::any_cast<std::string>(parsedArgs["reference"]);
+      refGeometryFileName = us::any_cast<std::string>(parsedArgs["template"]);
     }
 
     if (parsedArgs.count("registrations"))
@@ -122,8 +124,13 @@ bool configureApplicationSettings(std::map<std::string, us::Any> parsedArgs)
       auto temp = us::any_cast<int>(parsedArgs["strategy"]);
       stitchStratgy = static_cast<itk::StitchStrategy>(temp);
     }
+  }
+  catch (...)
+  {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 int main(int argc, char* argv[])
@@ -136,7 +143,8 @@ int main(int argc, char* argv[])
     const std::map<std::string, us::Any>& parsedArgs = parser.parseArguments(argc, argv);
     if (!configureApplicationSettings(parsedArgs))
     {
-        return EXIT_FAILURE;
+      MITK_ERROR << "Command line arguments are invalid. To see the correct usage please call with -h or --help to show the help information.";
+      return EXIT_FAILURE;
     };
 
     // Show a help message

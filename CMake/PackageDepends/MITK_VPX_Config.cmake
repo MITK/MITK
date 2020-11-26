6aba543698a3b1ae09272b7b33016fdd96880ddb
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2020-11-26 16:42:09 +0100

    Remove dangling package depends file

--- CMake/PackageDepends/MITK_VPX_Config.cmake
+++
@@ -1,9 +0,0 @@
-set( VPX_LIB "" CACHE FILEPATH "Search for the vpx library file" )
-set( VPX_HEADER_DIR "" CACHE PATH "The path containing vpy headers" )
-set( VPX_FOUND FALSE )
-
-if(VPX_LIB AND VPX_HEADER_DIR)
-  list(APPEND ALL_LIBRARIES ${VPX_LIB})
-  list(APPEND ALL_INCLUDE_DIRECTORIES ${VPX_HEADER_DIR})
-  set( VPX_FOUND TRUE )
-endif()

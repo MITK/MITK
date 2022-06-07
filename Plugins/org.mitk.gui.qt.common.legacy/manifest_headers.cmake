5a1a565bc552dfbc1377d6c80ca588b947e4533c
Amir Kalali <a.kalali@dkfz-heidelberg.de>
2022-06-07 21:14:05 +0200

    Remove superfluous org.mitk.gui.qt.common.legacy plugin

--- Plugins/org.mitk.gui.qt.common.legacy/manifest_headers.cmake
+++
@@ -1,6 +0,0 @@
-set(Plugin-Name "Legacy Plug-in for QmitkStdMultiWidgetEditor")
-set(Plugin-Version "1.0.0")
-set(Plugin-Vendor "German Cancer Research Center (DKFZ)")
-set(Plugin-ContactAddress "http://www.mitk.org")
-set(Require-Plugin org.mitk.gui.qt.stdmultiwidgeteditor)
-

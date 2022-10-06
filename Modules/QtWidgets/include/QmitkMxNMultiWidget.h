d8230d497e9d15dda830595345c1d1cc2d47a0c1
Amir Kalali <a.kalali@dkfz-heidelberg.de>
2022-10-06 17:13:22 +0200

    Provide function for setting the reference geometry
    The MxnMultiWidget will reset the reference geometry of
    the active render window.

--- Modules/QtWidgets/include/QmitkMxNMultiWidget.h
+++ Modules/QtWidgets/include/QmitkMxNMultiWidget.h
@@ -43,6 +43,16 @@ public:
   QmitkRenderWindow* GetRenderWindow(const mitk::BaseRenderer::ViewDirection& viewDirection) const override;
 
   void SetActiveRenderWindowWidget(RenderWindowWidgetPointer activeRenderWindowWidget) override;
+  /**
+   * @brief Set the reference geometry for interaction inside the active render windows of the MxNMultiWidget.
+   *
+   * @param referenceGeometry   The reference geometry which is used for updating the
+   *                            time geometry inside the active render window.
+   * @param resetCamera         If true, the camera and crosshair will be reset to the default view (centered, no zoom).
+   *                            If false, the current crosshair position and the camera zoom will be stored and reset
+   *                            after the reference geometry has been updated.
+   */
+  void SetReferenceGeometry(const mitk::TimeGeometry* referenceGeometry, bool resetCamera) override;
 
   void SetSelectedPosition(const mitk::Point3D& newPosition, const QString& widgetName) override;
   const mitk::Point3D GetSelectedPosition(const QString& widgetName) const override;

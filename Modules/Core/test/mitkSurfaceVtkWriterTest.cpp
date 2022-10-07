b7577dd1ffe00151727f5cf8716e0dada9399d03
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2022-10-08 00:37:58 +0200

    Improve SurfaceVTKWriter test

--- Modules/Core/test/mitkSurfaceVtkWriterTest.cpp
+++ Modules/Core/test/mitkSurfaceVtkWriterTest.cpp
@@ -10,62 +10,59 @@ found in the LICENSE file.
 
 ============================================================================*/
 
-#include "mitkIOUtil.h"
-#include "mitkSurface.h"
-#include "mitkTestingMacros.h"
+#include <mitkIOUtil.h>
+#include <mitkSurface.h>
+#include <mitkTestingMacros.h>
+#include <mitkTestManager.h>
 
-#include <vtkPolyDataReader.h>
-#include <vtkPolyDataWriter.h>
+#include <cstdio>
+#include <string>
+#include <fstream>
 
-#include <iostream>
-
-/**
- *  Simple example for a test for the (non-existent) class "ClassName".
- *
- *  argc and argv are the command line parameters which were passed to
- *  the ADD_TEST command in the CMakeLists.txt file. For the automatic
- *  tests, argv is either empty for the simple tests or contains the filename
- *  of a test image for the image tests (see CMakeLists.txt).
- */
-int mitkSurfaceVtkWriterTest(int /*argc*/, char *argv[])
+int mitkSurfaceVtkWriterTest(int, char *argv[])
 {
-  // always start with this!
   MITK_TEST_BEGIN("SurfaceVtkWriter")
 
-  // create contour
-  vtkPolyDataReader *reader = vtkPolyDataReader::New();
-  reader->SetFileName(argv[1]);
-  reader->Update();
-  if (reader->GetOutput())
-  {
-    mitk::Surface::Pointer surface = mitk::Surface::New();
-    surface->SetVtkPolyData(reader->GetOutput());
-    surface->Update();
+  mitk::Surface::Pointer surface;
 
-    MITK_TEST_CONDITION_REQUIRED(surface.IsNotNull(), "Surface creation")
+  try
+  {
+    MITK_TEST_OUTPUT(<< "Load surface from \"" << argv[1] << "\"")
+    surface = mitk::IOUtil::Load<mitk::Surface>(argv[1]);
+  }
+  catch (...)
+  {
+  }
+
+  if (surface.IsNotNull())
+  {
+    std::string path;
 
     try
     {
-      // test for exception handling
-      mitk::IOUtil::Save(surface, "/usr/bin");
-      MITK_TEST_FAILED_MSG(<< "itk::ExceptionObject expected")
-    }
-    catch (const mitk::Exception &)
-    { /* this is expected */
+      std::ofstream stream;
+      path = mitk::IOUtil::CreateTemporaryFile(stream, "XXXXXX.vtp");
     }
     catch (...)
     {
-      // this means that a wrong exception (i.e. no itk:Exception) has been thrown
-      MITK_TEST_FAILED_MSG(<< "Wrong exception (i.e. no itk:Exception) caught during write [FAILED]")
     }
 
-    // write your own tests here and use the macros from mitkTestingMacros.h !!!
-    // do not write to std::cout and do not return from this function yourself!
+    if (!path.empty())
+    {
+      try
+      {
+        MITK_TEST_OUTPUT(<< "Save surface as \"" << path << "\"")
+        mitk::IOUtil::Save(surface, path);
+      }
+      catch (const std::exception& e)
+      {
+        std::remove(path.c_str());
+        MITK_TEST_FAILED_MSG(<< "Caught exception: " << e.what() << " [FAILED]")
+      }
+
+      mitk::TestManager::GetInstance()->TestPassed();
+    }
   }
 
-  // Delete reader correctly
-  reader->Delete();
-
-  // always end with this!
   MITK_TEST_END()
 }

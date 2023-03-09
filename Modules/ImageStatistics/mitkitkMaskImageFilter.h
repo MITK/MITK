3ac48d9e0e6f77eb110a171eed54578f8baee80f
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2023-03-09 13:29:17 +0100

    Fix copyright headers

--- Modules/ImageStatistics/mitkitkMaskImageFilter.h
+++ Modules/ImageStatistics/mitkitkMaskImageFilter.h
@@ -1,22 +1,19 @@
-/*=========================================================================
- *
- *  Copyright Insight Software Consortium
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *         https://www.apache.org/licenses/LICENSE-2.0.txt
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *=========================================================================*/
-#ifndef __itkMaskImageFilter2_h
-#define __itkMaskImageFilter2_h
+/*============================================================================
+
+The Medical Imaging Interaction Toolkit (MITK)
+
+Copyright (c) German Cancer Research Center (DKFZ)
+All rights reserved.
+
+Use of this source code is governed by a 3-clause BSD license that can be
+found in the LICENSE file.
+
+============================================================================*/
+
+#ifndef mitkitkMaskImageFilter_h
+#define mitkitkMaskImageFilter_h
+
+// This file is based on ITK's itkMaskImageFilter.h
 
 #include "itkBinaryFunctorImageFilter.h"
 #include "itkNumericTraits.h"

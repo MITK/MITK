9ed9e1dd2da021b344a5673784b61e89f5f6ef58
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2023-07-26 15:37:10 +0200

    Remove abandoned modules

--- Modules/Classification/CLVigraRandomForest/test/files.cmake
+++
@@ -1,5 +0,0 @@
-set(MODULE_TESTS "")
-
-if(NOT APPLE)
-  list(APPEND MODULE_TESTS mitkVigraRandomForestTest.cpp)
-endif()
--- Modules/Classification/DataCollection/Testing/files.cmake
+++
@@ -1,9 +0,0 @@
-SET(MODULE_TESTS
-  mitkDataCollectionImageIteratorTest.cpp
-)
-
-SET(MODULE_CUSTOM_TESTS
-#  mitkDataCollectionFromXMLTest.cpp
-#  mitkDataCollectionImageIteratorTest.cpp
-#  mitkDataCollectionBySuffix.cpp
-)

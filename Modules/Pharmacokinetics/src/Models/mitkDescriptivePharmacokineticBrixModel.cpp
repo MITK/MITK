cda9ac0ba22249cb1dfa890034cb82a82f88c825
Ina Kompan <i.kompan@dkfz-heidelberg.de>
2023-09-05 09:27:37 +0200

    Rework Descriptive Pharmacokinetic Brix Model.

--- Modules/Pharmacokinetics/src/Models/mitkDescriptivePharmacokineticBrixModel.cpp
+++ Modules/Pharmacokinetics/src/Models/mitkDescriptivePharmacokineticBrixModel.cpp
@@ -15,30 +15,38 @@ found in the LICENSE file.
 const std::string mitk::DescriptivePharmacokineticBrixModel::MODEL_DISPLAY_NAME =
   "Descriptive Pharmacokinetic Brix Model";
 
+const std::string mitk::DescriptivePharmacokineticBrixModel::MODEL_TYPE =
+"Perfusion.MR";
+
 const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_PARAMETER_A = "A";
-const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_PARAMETER_kep = "kep";
-const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_PARAMETER_kel = "kel";
-//tlag in minutes
-const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_PARAMETER_tlag = "tlag";
+const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_PARAMETER_kep = "k_{e->p}";
+const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_PARAMETER_kel = "k_{el}";
+const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_PARAMETER_BAT = "BAT";
 
 const std::string mitk::DescriptivePharmacokineticBrixModel::UNIT_PARAMETER_A = "a.u.";
 const std::string mitk::DescriptivePharmacokineticBrixModel::UNIT_PARAMETER_kep = "1/min";
 const std::string mitk::DescriptivePharmacokineticBrixModel::UNIT_PARAMETER_kel = "1/min";
-//tlag in minutes
-const std::string mitk::DescriptivePharmacokineticBrixModel::UNIT_PARAMETER_tlag = "min";
+const std::string mitk::DescriptivePharmacokineticBrixModel::UNIT_PARAMETER_BAT = "min";
 
-const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_STATIC_PARAMETER_Tau = "Tau";
-const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_STATIC_PARAMETER_S0 = "S0";
+const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_STATIC_PARAMETER_tau = "Injection_time";
+const std::string mitk::DescriptivePharmacokineticBrixModel::NAME_STATIC_PARAMETER_s0 = "Pre_contrast_signal";
 
-const std::string mitk::DescriptivePharmacokineticBrixModel::UNIT_STATIC_PARAMETER_Tau = "min";
-const std::string mitk::DescriptivePharmacokineticBrixModel::UNIT_STATIC_PARAMETER_S0 = "I";
+const std::string mitk::DescriptivePharmacokineticBrixModel::UNIT_STATIC_PARAMETER_tau = "min";
+const std::string mitk::DescriptivePharmacokineticBrixModel::UNIT_STATIC_PARAMETER_s0 = "a.u.";
 
 const unsigned int mitk::DescriptivePharmacokineticBrixModel::POSITION_PARAMETER_A = 0;
 const unsigned int mitk::DescriptivePharmacokineticBrixModel::POSITION_PARAMETER_kep = 1;
 const unsigned int mitk::DescriptivePharmacokineticBrixModel::POSITION_PARAMETER_kel = 2;
-const unsigned int mitk::DescriptivePharmacokineticBrixModel::POSITION_PARAMETER_tlag = 3;
+const unsigned int mitk::DescriptivePharmacokineticBrixModel::POSITION_PARAMETER_BAT = 3;
 
 const unsigned int mitk::DescriptivePharmacokineticBrixModel::NUMBER_OF_PARAMETERS = 4;
+const unsigned int mitk::DescriptivePharmacokineticBrixModel::NUMBER_OF_STATIC_PARAMETERS = 2;
+
+const std::string mitk::DescriptivePharmacokineticBrixModel::X_AXIS_NAME = "Time";
+const std::string mitk::DescriptivePharmacokineticBrixModel::X_AXIS_UNIT = "s";
+const std::string mitk::DescriptivePharmacokineticBrixModel::Y_AXIS_NAME = "Signal";
+const std::string mitk::DescriptivePharmacokineticBrixModel::Y_AXIS_UNIT = "a.u.";
+
 
 std::string mitk::DescriptivePharmacokineticBrixModel::GetModelDisplayName() const
 {
@@ -47,27 +55,27 @@ std::string mitk::DescriptivePharmacokineticBrixModel::GetModelDisplayName() con
 
 std::string mitk::DescriptivePharmacokineticBrixModel::GetModelType() const
 {
-  return "Perfusion.MR";
+  return MODEL_TYPE;
 };
 
 std::string mitk::DescriptivePharmacokineticBrixModel::GetXAxisName() const
 {
-  return "Time";
+  return X_AXIS_NAME;
 };
 
 std::string mitk::DescriptivePharmacokineticBrixModel::GetXAxisUnit() const
 {
-  return "s";
+  return X_AXIS_UNIT;
 }
 
 std::string mitk::DescriptivePharmacokineticBrixModel::GetYAxisName() const
 {
-  return "";
+  return Y_AXIS_NAME;
 };
 
 std::string mitk::DescriptivePharmacokineticBrixModel::GetYAxisUnit() const
 {
-  return "";
+  return Y_AXIS_UNIT;
 }
 
 mitk::DescriptivePharmacokineticBrixModel::DescriptivePharmacokineticBrixModel(): m_Tau(0), m_S0(1)
@@ -88,7 +96,7 @@ mitk::DescriptivePharmacokineticBrixModel::GetParameterNames() const
   result.push_back(NAME_PARAMETER_A);
   result.push_back(NAME_PARAMETER_kep);
   result.push_back(NAME_PARAMETER_kel);
-  result.push_back(NAME_PARAMETER_tlag);
+  result.push_back(NAME_PARAMETER_BAT);
 
   return result;
 }
@@ -107,7 +115,7 @@ mitk::DescriptivePharmacokineticBrixModel::GetParameterUnits() const
   result.insert(std::make_pair(NAME_PARAMETER_A, UNIT_PARAMETER_A));
   result.insert(std::make_pair(NAME_PARAMETER_kep, UNIT_PARAMETER_kep));
   result.insert(std::make_pair(NAME_PARAMETER_kel, UNIT_PARAMETER_kel));
-  result.insert(std::make_pair(NAME_PARAMETER_tlag, UNIT_PARAMETER_tlag));
+  result.insert(std::make_pair(NAME_PARAMETER_BAT, UNIT_PARAMETER_BAT));
 
   return result;
 };
@@ -117,8 +125,8 @@ mitk::DescriptivePharmacokineticBrixModel::GetStaticParameterNames() const
 {
   ParameterNamesType result;
 
-  result.push_back(NAME_STATIC_PARAMETER_Tau);
-  result.push_back(NAME_STATIC_PARAMETER_S0);
+  result.push_back(NAME_STATIC_PARAMETER_tau);
+  result.push_back(NAME_STATIC_PARAMETER_s0);
 
   return result;
 }
@@ -128,8 +136,8 @@ mitk::DescriptivePharmacokineticBrixModel::GetStaticParameterUnits() const
 {
   ParamterUnitMapType result;
 
-  result.insert(std::make_pair(NAME_STATIC_PARAMETER_Tau, UNIT_STATIC_PARAMETER_Tau));
-  result.insert(std::make_pair(NAME_STATIC_PARAMETER_S0, UNIT_STATIC_PARAMETER_S0));
+  result.insert(std::make_pair(NAME_STATIC_PARAMETER_tau, UNIT_STATIC_PARAMETER_tau));
+  result.insert(std::make_pair(NAME_STATIC_PARAMETER_s0, UNIT_STATIC_PARAMETER_s0));
 
   return result;
 };
@@ -137,7 +145,7 @@ mitk::DescriptivePharmacokineticBrixModel::GetStaticParameterUnits() const
 mitk::DescriptivePharmacokineticBrixModel::ParametersSizeType
 mitk::DescriptivePharmacokineticBrixModel::GetNumberOfStaticParameters() const
 {
-  return 2;
+  return NUMBER_OF_STATIC_PARAMETERS;
 }
 
 
@@ -162,7 +170,7 @@ const
   double amplitude = parameters[POSITION_PARAMETER_A];
   double       kel = parameters[POSITION_PARAMETER_kel];
   double       kep = parameters[POSITION_PARAMETER_kep];
-  double      tlag = parameters[POSITION_PARAMETER_tlag];
+  double       bat = parameters[POSITION_PARAMETER_BAT];
 
   if (kep == kel)
   {
@@ -178,21 +186,21 @@ const
     double t = (*gridPos) / 60.0; //convert from [sec] to [min]
 
 
-    if (t <= tlag)
+    if (t <= bat)
     {
       tx = 0;
     }
-    else if ((t > tlag) && (t < (m_Tau + tlag)))
+    else if ((t > bat) && (t < (m_Tau + bat)))
     {
-      tx = t - tlag;
+      tx = t - bat;
     }
-    else if (t >= (m_Tau + tlag))
+    else if (t >= (m_Tau + bat))
     {
       tx = m_Tau;
     }
 
     double kDiff  = kep - kel;
-    double tDiff  = t - tlag;
+    double tDiff  = t - bat;
 
     double expkel   = (kep * exp(-kel * tDiff));
     double expkeltx =        exp(kel * tx);
@@ -212,12 +220,12 @@ const
 void mitk::DescriptivePharmacokineticBrixModel::SetStaticParameter(const ParameterNameType& name,
     const StaticParameterValuesType& values)
 {
-  if (name == NAME_STATIC_PARAMETER_Tau)
+  if (name == NAME_STATIC_PARAMETER_tau)
   {
     SetTau(values[0]);
   }
 
-  if (name == NAME_STATIC_PARAMETER_S0)
+  if (name == NAME_STATIC_PARAMETER_s0)
   {
     SetS0(values[0]);
   }
@@ -229,12 +237,12 @@ const
 {
   StaticParameterValuesType result;
 
-  if (name == NAME_STATIC_PARAMETER_Tau)
+  if (name == NAME_STATIC_PARAMETER_tau)
   {
     result.push_back(GetTau());
   }
 
-  if (name == NAME_STATIC_PARAMETER_S0)
+  if (name == NAME_STATIC_PARAMETER_s0)
   {
     result.push_back(GetS0());
   }
@@ -259,5 +267,5 @@ void mitk::DescriptivePharmacokineticBrixModel::PrintSelf(std::ostream& os,
   Superclass::PrintSelf(os, indent);
 
   os << indent << "Tau (injection time): " << m_Tau;
-  os << indent << "S0 (base value): " << m_S0;
+  os << indent << "S0 (baseline value): " << m_S0;
 };

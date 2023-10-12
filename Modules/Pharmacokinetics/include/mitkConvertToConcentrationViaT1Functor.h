0633ae0e4599a8f804cea93a358edfbf297c9d02
Ina Kompan <i.kompan@dkfz-heidelberg.de>
2023-10-12 13:28:09 +0200

    Const correctness in mitkConvertToConcentrationViaT1Functor.

--- Modules/Pharmacokinetics/include/mitkConvertToConcentrationViaT1Functor.h
+++ Modules/Pharmacokinetics/include/mitkConvertToConcentrationViaT1Functor.h
@@ -52,15 +52,15 @@ namespace mitk {
           if (baseline != 0 && pdw != 0 && value != 0)
           {
             // calculate signal scaling factor S0 and pre-contrast T1 relaxation time T10
-            double a = pdw * sin(m_flipangle) / (baseline * sin(m_flipanglePDW));
-            double b = (a - 1.0) / (a * cos(m_flipanglePDW) - cos(m_flipangle));
-            double T10 = static_cast<double>((-1.0) * m_TR / log(b));
-            double lambda = exp((-1.0) * m_TR / T10);
-            double S0 = pdw * (1-lambda * cos(m_flipanglePDW)) / ((1 - lambda) * sin(m_flipanglePDW));
+            const double a = pdw * sin(m_flipangle) / (baseline * sin(m_flipanglePDW));
+            const double b = (a - 1.0) / (a * cos(m_flipanglePDW) - cos(m_flipangle));
+            const double T10 = static_cast<double>((-1.0) * m_TR / log(b));
+            const double lambda = exp((-1.0) * m_TR / T10);
+            const double S0 = pdw * (1-lambda * cos(m_flipanglePDW)) / ((1 - lambda) * sin(m_flipanglePDW));
 
             // calculate T1
-            double c = (value - S0 * sin(m_flipangle)) / (value * cos(m_flipangle) - S0 * sin(m_flipangle));
-            double T1 = static_cast<double>((-1.0) * m_TR / log(c));
+            const double c = (value - S0 * sin(m_flipangle)) / (value * cos(m_flipangle) - S0 * sin(m_flipangle));
+            const double T1 = static_cast<double>((-1.0) * m_TR / log(c));
 
             //calculate concentration
             concentration = static_cast<double>((1.0 / T1 - 1.0 / T10) / (m_relaxivity/1000.0));

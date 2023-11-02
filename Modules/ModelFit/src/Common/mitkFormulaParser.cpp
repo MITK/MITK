c5bc8ba32dfbf025ed34ea02aedd0a551469ee0c
Stephen Schaumann <s.schaumann@dkfz-heidelberg.de>
2023-11-02 10:36:22 +0100

    adjust whitespace

--- Modules/ModelFit/src/Common/mitkFormulaParser.cpp
+++ Modules/ModelFit/src/Common/mitkFormulaParser.cpp
@@ -212,7 +212,7 @@ namespace mitk
           | ('/' >> factor[_val /= _1]));
 
       factor = primary[_val = _1]
-      >> *('^' >> primary[_val = phx::bind<FormulaParser::ValueType, FormulaParser::ValueType, FormulaParser::ValueType>(std::pow, _val, _1)]);
+        >> *('^' >> primary[_val = phx::bind<FormulaParser::ValueType, FormulaParser::ValueType, FormulaParser::ValueType>(std::pow, _val, _1)]);
 
       variable = as_string[alpha >> *(alnum | char_('_'))]
         [_val = phx::bind(&FormulaParser::lookupVariable, &formulaParser, _1)];

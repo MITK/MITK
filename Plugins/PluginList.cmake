36d20826616b64be430c18d51616e533733c2d08
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2023-12-19 09:23:26 +0100

    Remove org.mitk.planarfigure plugin from list

--- Plugins/PluginList.cmake
+++ Plugins/PluginList.cmake
@@ -13,7 +13,6 @@ set(MITK_PLUGINS
   org.blueberry.ui.qt.objectinspector:OFF
   org.mitk.core.services:ON
   org.mitk.gui.common:ON
-  org.mitk.planarfigure:ON
   org.mitk.core.jobs:OFF
   org.mitk.gui.qt.application:ON
   org.mitk.gui.qt.ext:OFF

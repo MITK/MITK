059003befe2618bf2daa0017a0249f92a9edd2e4
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2024-02-20 09:51:12 +0100

    Do not enable or use EGL

--- CMake/RunInstalledApp.sh
+++ CMake/RunInstalledApp.sh
@@ -3,4 +3,5 @@ binpath=$(dirname "$(readlink -f "$0")")
 appname=$(basename "$0" .sh)
 export LD_LIBRARY_PATH="$binpath/bin":"$binpath/bin/plugins":$LD_LIBRARY_PATH
 export QT_PLUGIN_PATH="$binpath/bin":"$binpath/bin/plugins"
+export QT_QPA_PLATFORM=xcb
 "$binpath/bin/$appname" "$@"

6d2828c2ed4104dda0b941d86c037afe74c363d2
floca <r.floca@dkfz-heidelberg.de>
2024-04-06 13:07:28 +0200

    BREAKING!!! What: - Reworked BooleanOperation to support multi label and 3d+t - Changed from class pattern to global functions for generating the mask
    Why:
    - Functor pattern (class instances configured through contructur)
      is superfluous and not stateless. Lambdas and global functions
      are a better approach.
    - Added multi label support and allow nary operations (more then two
      labels at once)

--- Modules/Segmentation/SegmentationUtilities/BooleanOperations/mitkBooleanOperation.cpp
+++ Modules/Segmentation/SegmentationUtilities/BooleanOperations/mitkBooleanOperation.cpp
@@ -11,175 +11,172 @@ found in the LICENSE file.
 ============================================================================*/
 
 #include "mitkBooleanOperation.h"
-#include <itkAndImageFilter.h>
-#include <itkNotImageFilter.h>
-#include <itkOrImageFilter.h>
+#include <itkNaryFunctorImageFilter.h>
 #include <mitkExceptionMacro.h>
 #include <mitkImageCast.h>
 #include <mitkImageTimeSelector.h>
+#include <mitkITKEventObserverGuard.h>
 
-typedef itk::Image<mitk::Label::PixelType, 3> ImageType;
+using ITKGroupImageType = itk::Image<mitk::Label::PixelType, 3>;
+using BoolOpsFunctionType = mitk::Label::PixelType(const std::vector<mitk::Label::PixelType>& inputArray);
 
-static mitk::Image::Pointer Get3DSegmentation(mitk::Image::Pointer segmentation, mitk::TimePointType time)
+/** Functor class that implements the label transfer and is used in conjunction with the itk::BinaryFunctorImageFilter.
+* For details regarding the usage of the filter and the functor patterns, please see info of itk::BinaryFunctorImageFilter.
+*/
+template <class TPixelType>
+class BoolOpsFunctor
 {
-  if (segmentation->GetDimension() != 4)
-    return segmentation;
 
-  auto imageTimeSelector = mitk::ImageTimeSelector::New();
-  imageTimeSelector->SetInput(segmentation);
-  imageTimeSelector->SetTimeNr(segmentation->GetTimeGeometry()->TimePointToTimeStep(time));
+public:
+  BoolOpsFunctor() {};
 
-  imageTimeSelector->UpdateLargestPossibleRegion();
-
-  return imageTimeSelector->GetOutput();
-}
-
-static ImageType::Pointer CastTo3DItkImage(mitk::Image::Pointer segmentation, mitk::TimePointType time)
-{
-  ImageType::Pointer result;
-  mitk::CastToItkImage(Get3DSegmentation(segmentation, time), result);
-  return result;
-}
-
-mitk::BooleanOperation::BooleanOperation(Type type,
-                                         mitk::Image::Pointer segmentationA,
-                                         mitk::Image::Pointer segmentationB,
-                                         TimePointType time)
-  : m_Type(type), m_SegmentationA(segmentationA), m_SegmentationB(segmentationB), m_TimePoint(time)
-{
-  this->ValidateSegmentations();
-}
-
-mitk::BooleanOperation::~BooleanOperation()
-{
-}
-
-mitk::LabelSetImage::Pointer mitk::BooleanOperation::GetResult() const
-{
-  switch (m_Type)
+  BoolOpsFunctor(std::function<BoolOpsFunctionType> opsFunction) : m_Function(opsFunction)
   {
-    case Difference:
-      return this->GetDifference();
+  };
 
-    case Intersection:
-      return this->GetIntersection();
+  ~BoolOpsFunctor() {};
 
-    case Union:
-      return this->GetUnion();
-
-    default:
-      mitkThrow() << "Unknown boolean operation type '" << m_Type << "'!";
-  }
-}
-
-mitk::LabelSetImage::Pointer mitk::BooleanOperation::GetDifference() const
-{
-  auto input1 = CastTo3DItkImage(m_SegmentationA, m_TimePoint);
-  auto input2 = CastTo3DItkImage(m_SegmentationB, m_TimePoint);
-
-  auto notFilter = itk::NotImageFilter<ImageType, ImageType>::New();
-  notFilter->SetInput(input2);
-
-  auto andFilter = itk::AndImageFilter<ImageType, ImageType>::New();
-  andFilter->SetInput1(input1);
-  andFilter->SetInput2(notFilter->GetOutput());
-
-  andFilter->UpdateLargestPossibleRegion();
-
-  auto tempResult = Image::New();
-  CastToMitkImage<ImageType>(andFilter->GetOutput(), tempResult);
-
-  tempResult->DisconnectPipeline();
-
-  auto result = mitk::LabelSetImage::New();
-  result->InitializeByLabeledImage(tempResult);
-
-  return result;
-}
-
-mitk::LabelSetImage::Pointer mitk::BooleanOperation::GetIntersection() const
-{
-  auto input1 = CastTo3DItkImage(m_SegmentationA, m_TimePoint);
-  auto input2 = CastTo3DItkImage(m_SegmentationB, m_TimePoint);
-
-  auto andFilter = itk::AndImageFilter<ImageType, ImageType>::New();
-  andFilter->SetInput1(input1);
-  andFilter->SetInput2(input2);
-
-  andFilter->UpdateLargestPossibleRegion();
-
-  auto tempResult = Image::New();
-  CastToMitkImage<ImageType>(andFilter->GetOutput(), tempResult);
-
-  tempResult->DisconnectPipeline();
-
-  auto result = mitk::LabelSetImage::New();
-  result->InitializeByLabeledImage(tempResult);
-
-  return result;
-}
-
-mitk::LabelSetImage::Pointer mitk::BooleanOperation::GetUnion() const
-{
-  auto input1 = CastTo3DItkImage(m_SegmentationA, m_TimePoint);
-  auto input2 = CastTo3DItkImage(m_SegmentationB, m_TimePoint);
-
-  auto orFilter = itk::OrImageFilter<ImageType, ImageType>::New();
-  orFilter->SetInput1(input1);
-  orFilter->SetInput2(input2);
-
-  orFilter->UpdateLargestPossibleRegion();
-
-  auto tempResult = Image::New();
-  CastToMitkImage<ImageType>(orFilter->GetOutput(), tempResult);
-
-  tempResult->DisconnectPipeline();
-
-  auto result = mitk::LabelSetImage::New();
-  result->InitializeByLabeledImage(tempResult);
-
-  return result;
-}
-
-void mitk::BooleanOperation::ValidateSegmentation(mitk::Image::Pointer segmentation) const
-{
-  if (segmentation.IsNull())
-    mitkThrow() << "Segmentation is nullptr!";
-
-  if (segmentation->GetImageDescriptor()->GetNumberOfChannels() != 1)
-    mitkThrow() << "Segmentation has more than one channel!";
-
-  auto pixelType = segmentation->GetImageDescriptor()->GetChannelDescriptor().GetPixelType();
-
-  if (pixelType.GetPixelType() != itk::IOPixelEnum::SCALAR ||
-      (pixelType.GetComponentType() != itk::IOComponentEnum::UCHAR &&
-       pixelType.GetComponentType() != itk::IOComponentEnum::USHORT))
-    mitkThrow() << "Segmentation is neither of type 'unsigned char' nor type 'unsigned short'!";
-
-  auto dimension = segmentation->GetDimension();
-
-  if (dimension > 4)
-    mitkThrow() << "Segmentation has more than four dimensions!";
-
-  if (dimension < 3)
-    mitkThrow() << "Segmentation has less than three dimensions!";
-
-  if (!segmentation->GetTimeGeometry()->IsValidTimePoint(m_TimePoint))
-    mitkThrow() << "Segmentation is not defined for specified time point. Time point: " << m_TimePoint;
-}
-
-void mitk::BooleanOperation::ValidateSegmentations() const
-{
-  this->ValidateSegmentation(m_SegmentationA);
-  this->ValidateSegmentation(m_SegmentationB);
-
-  if (m_SegmentationA->GetDimension() != m_SegmentationB->GetDimension())
-    mitkThrow() << "Segmentations have different dimensions!";
-
-  const auto geometryA = m_SegmentationA->GetTimeGeometry()->GetGeometryForTimePoint(m_TimePoint);
-  const auto geometryB = m_SegmentationB->GetTimeGeometry()->GetGeometryForTimePoint(m_TimePoint);
-  if (!mitk::Equal(*(geometryA.GetPointer()), *(geometryB.GetPointer()),eps,false))
+  bool operator!=(const BoolOpsFunctor& other)const
   {
-    mitkThrow() << "Segmentations have different geometries and cannot be used for boolean operations!";
+    return !(*this == other);
   }
+
+  bool operator==(const BoolOpsFunctor& other) const
+  {
+    if ((this->m_Function.target_type() == other.m_Function.target_type())
+      && (this->m_Function.target<int(*)(int)>() && other.m_Function.target<int(*)(int)>())
+      // If both std::function objects hold function pointers of the same signature,
+      // we can compare the pointers to check if they point to the same function.
+      && (*this->m_Function.target<int(*)(int)>() == *other.m_Function.target<int(*)(int)>()))
+    {
+      return true;
+    }
+    return false;
+  }
+
+  BoolOpsFunctor& operator=(const BoolOpsFunctor& other)
+  {
+    this->m_Function = other.m_Function;
+
+    return *this;
+  }
+
+  inline TPixelType operator()(const std::vector<TPixelType>& inputArray)
+  {
+    return m_Function(inputArray);
+  }
+
+private:
+  std::function<BoolOpsFunctionType> m_Function;
+};
+
+
+mitk::Image::Pointer GenerateInternal(const mitk::LabelSetImage* segmentation, mitk::LabelSetImage::LabelValueVectorType labeValues, std::function<BoolOpsFunctionType> opsFunction,
+  std::function<void(float progress)> progressCallback = [](float) {})
+{
+  if (nullptr == segmentation) mitkThrow() << "Cannot perform boolean operation. Passed segmentation is not valid";
+
+  mitk::Image::Pointer result = mitk::Image::New();
+  result->Initialize(mitk::MakeScalarPixelType<mitk::LabelSetImage::LabelValueType>(), *(segmentation->GetTimeGeometry()));
+
+  const auto timeStepCount = segmentation->GetTimeGeometry()->CountTimeSteps();
+
+  for (mitk::TimeStepType i = 0; i < timeStepCount; ++i)
+  {
+    using OpsFilterType = itk::NaryFunctorImageFilter<ITKGroupImageType, ITKGroupImageType, BoolOpsFunctor<mitk::LabelSetImage::LabelValueType> >;
+    auto opsFilter = OpsFilterType::New();
+
+    mitk::ITKEventObserverGuard eventGuard(opsFilter, itk::ProgressEvent(), [&opsFilter, progressCallback, timeStepCount](const itk::EventObject& event)
+      { progressCallback(opsFilter->GetProgress() / static_cast<float>(timeStepCount)); });
+
+    BoolOpsFunctor<mitk::LabelSetImage::LabelValueType> functor(opsFunction);
+    opsFilter->SetFunctor(functor);
+    std::vector < ITKGroupImageType::ConstPointer > inputImages;
+
+    unsigned int inputIndex = 0;
+    for (auto value : labeValues)
+    {
+      auto groupImage = segmentation->GetGroupImage(segmentation->GetGroupIndexOfLabel(value));
+      auto groupImageAtTS = mitk::SelectImageByTimeStep(groupImage, i);
+
+      ITKGroupImageType::Pointer itkImage;
+      mitk::CastToItkImage(groupImageAtTS, itkImage);
+      inputImages.push_back(itkImage);
+      opsFilter->SetInput(inputIndex, itkImage);
+      ++inputIndex;
+    }
+
+    opsFilter->Update();
+    auto resultTS = opsFilter->GetOutput();
+
+    result->SetVolume(resultTS->GetBufferPointer(), i);
+  }
+
+  return result;
+}
+
+
+mitk::Image::Pointer mitk::BooleanOperation::GenerateUnion(const LabelSetImage* segmentation, LabelSetImage::LabelValueVectorType labelValues,
+  std::function<void(float progress)> progressCallback)
+{
+  auto unionOps = [labelValues](const std::vector<mitk::Label::PixelType>& inputArray)
+    {
+      mitk::Label::PixelType result = 0;
+      for (auto inIt = inputArray.cbegin(), refIt = labelValues.cbegin(); inIt != inputArray.cend(); ++inIt, ++refIt)
+      {
+        if (*inIt == *refIt)
+        {
+          result = 1;
+          break;
+        }
+      }
+      return result;
+    };
+
+  return GenerateInternal(segmentation, labelValues, unionOps, progressCallback);
+}
+
+mitk::Image::Pointer mitk::BooleanOperation::GenerateIntersection(const LabelSetImage* segmentation, LabelSetImage::LabelValueVectorType labelValues,
+  std::function<void(float progress)> progressCallback)
+{
+  auto intersectOps = [labelValues](const std::vector<mitk::Label::PixelType>& inputArray)
+    {
+      mitk::Label::PixelType result = 1;
+      for (auto inIt = inputArray.cbegin(), refIt = labelValues.cbegin(); inIt != inputArray.cend(); ++inIt, ++refIt)
+      {
+        if (*inIt != *refIt)
+        {
+          result = 0;
+          break;
+        }
+      }
+      return result;
+    };
+
+  return GenerateInternal(segmentation, labelValues, intersectOps, progressCallback);
+}
+
+mitk::Image::Pointer mitk::BooleanOperation::GenerateDifference(const LabelSetImage* segmentation, LabelSetImage::LabelValueType minuendLabelValue,
+  LabelSetImage::LabelValueVectorType subtrahendLabelValues, std::function<void(float progress)> progressCallback)
+{
+  auto intersectOps = [minuendLabelValue, subtrahendLabelValues](const std::vector<mitk::Label::PixelType>& inputArray)
+    {
+      if (minuendLabelValue != inputArray.front())
+        return mitk::Label::PixelType(0);
+
+      mitk::Label::PixelType result = 1;
+      for (auto inIt = inputArray.cbegin()+1, refIt = subtrahendLabelValues.cbegin(); inIt != inputArray.cend(); ++inIt, ++refIt)
+      {
+        if (*inIt == *refIt)
+        {
+          result = 0;
+          break;
+        }
+      }
+      return result;
+    };
+
+  auto labelValues = subtrahendLabelValues;
+  labelValues.insert(labelValues.begin(), minuendLabelValue);
+  return GenerateInternal(segmentation, labelValues, intersectOps, progressCallback);
 }
--- Modules/Segmentation/SegmentationUtilities/BooleanOperations/mitkBooleanOperation.h
+++ Modules/Segmentation/SegmentationUtilities/BooleanOperations/mitkBooleanOperation.h
@@ -19,54 +19,19 @@ found in the LICENSE file.
 namespace mitk
 {
   /** \brief Executes a boolean operation on two different segmentations.
-   *
    * All parameters of the boolean operations must be specified during construction.
    * The actual operation is executed when calling GetResult().
    */
-  class MITKSEGMENTATION_EXPORT BooleanOperation
+  namespace BooleanOperation
   {
-  public:
-    enum Type
-    {
-      None,
-      Difference,
-      Intersection,
-      Union
-    };
 
-    /* \brief Construct a boolean operation.
-     *
-     * Throws an mitk::Exception when segmentations are somehow invalid.
-     *
-     * \param[in] type The type of the boolean operation.
-     * \param[in] segmentationA The first operand of the boolean operation.
-     * \param[in] segmentationB The second operand of the boolean operation.
-     * \param[in] The time point at which the operation will be executed.
-     */
-    BooleanOperation(Type type, Image::Pointer segmentationA, Image::Pointer segmentationB, TimePointType time = 0.);
-    ~BooleanOperation();
+    Image::Pointer MITKSEGMENTATION_EXPORT GenerateUnion(const LabelSetImage*, LabelSetImage::LabelValueVectorType labeValues,
+      std::function<void(float progress)> progressCallback = [](float) {});
+    Image::Pointer MITKSEGMENTATION_EXPORT GenerateIntersection(const LabelSetImage*, LabelSetImage::LabelValueVectorType labelValues,
+      std::function<void(float progress)> progressCallback = [](float) {});
+    Image::Pointer MITKSEGMENTATION_EXPORT GenerateDifference(const LabelSetImage*, LabelSetImage::LabelValueType minuendLabelValue,
+      LabelSetImage::LabelValueVectorType subtrahendLabelValues, std::function<void(float progress)> progressCallback = [](float) {});
 
-    /* \brief Execute boolean operation and return resulting segmentation.
-     *
-     * \return The resulting segmentation.
-     */
-    LabelSetImage::Pointer GetResult() const;
-
-  private:
-    BooleanOperation(const BooleanOperation &);
-    BooleanOperation &operator=(const BooleanOperation &);
-
-    LabelSetImage::Pointer GetDifference() const;
-    LabelSetImage::Pointer GetIntersection() const;
-    LabelSetImage::Pointer GetUnion() const;
-
-    void ValidateSegmentation(Image::Pointer segmentation) const;
-    void ValidateSegmentations() const;
-
-    Type m_Type;
-    Image::Pointer m_SegmentationA;
-    Image::Pointer m_SegmentationB;
-    TimePointType m_TimePoint;
   };
 }
 

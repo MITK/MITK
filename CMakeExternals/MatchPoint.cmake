2ef3ea1e694599ee94b7062f9428679254011d81
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2024-06-04 08:14:50 +0200

    Make MatchPoint compatible to Ubuntu 20.04 and 22.04

--- CMakeExternals/MatchPoint.cmake
+++ CMakeExternals/MatchPoint.cmake
@@ -29,7 +29,7 @@ if(MITK_USE_MatchPoint)
     else()
       set(download_step
           GIT_REPOSITORY https://github.com/MIC-DKFZ/MatchPoint.git
-          GIT_TAG 6a425290639f9dfc1637b4e456bae2979d410970 # 03/06/2024
+          GIT_TAG 56957430a6a1665707bcbcec36ca9dc0248e7fc7 # 04/06/2024
          )
     endif()
 

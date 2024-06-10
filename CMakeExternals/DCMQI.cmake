4ef6f53b973dbd3aa27b21225a700698d59e1283
floca <r.floca@dkfz-heidelberg.de>
2024-06-10 23:05:51 +0200

    Bump to DCMQI v1.3.2

--- CMakeExternals/DCMQI.cmake
+++ CMakeExternals/DCMQI.cmake
@@ -27,7 +27,7 @@ if(MITK_USE_DCMQI)
     ExternalProject_Add(${proj}
       LIST_SEPARATOR ${sep}
       GIT_REPOSITORY https://github.com/QIICR/dcmqi.git
-      GIT_TAG v1.3.1
+      GIT_TAG v1.3.2
       UPDATE_COMMAND ""
       INSTALL_COMMAND ""
       CMAKE_GENERATOR ${gen}

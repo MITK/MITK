85321d325b6e23e3fd93fdf18c62ba454c7107f5
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2024-10-23 08:17:54 +0200

    Enable MTT scheduler only in Visual Studio 2019 v16.9 and later

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -115,9 +115,14 @@ set(gen_platform ${CMAKE_GENERATOR_PLATFORM})
 # Use this value where semi-colons are needed in ep_add args:
 set(sep "^^")
 
-if(MSVC_VERSION)
+if(MSVC)
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /bigobj")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj")
+
+  if(MSVC_VERSION VERSION_LESS 1928)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /MP")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
+  endif()
 endif()
 
 # This is a workaround for passing linker flags
@@ -181,13 +186,18 @@ set(ep_common_args
   -DCMAKE_MODULE_LINKER_FLAGS:STRING=${CMAKE_MODULE_LINKER_FLAGS}
 )
 
-if(MSVC_VERSION)
+if(MSVC)
   list(APPEND ep_common_args
     -DCMAKE_DEBUG_POSTFIX:STRING=d
-    "-DCMAKE_VS_GLOBALS:STRING=UseMultiToolTask=true${sep}EnforceProcessCountAcrossBuilds=true"
   )
 
   set(DCMTK_CMAKE_DEBUG_POSTFIX d)
+
+  if(MSVC_VERSION VERSION_GREATER_EQUAL 1928)
+    list(APPEND ep_common_args
+      "-DCMAKE_VS_GLOBALS:STRING=UseMultiToolTask=true${sep}EnforceProcessCountAcrossBuilds=true"
+    )
+  endif()
 endif()
 
 set(ep_common_cache_args

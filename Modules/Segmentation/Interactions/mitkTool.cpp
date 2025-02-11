4d6c68d31a46a08d0ce9c8662d42907884a03dd5
floca <r.floca@dkfz-heidelberg.de>
2025-02-11 20:30:29 +0100

    BREAKING: Removed obsolete mitk::Tool::CreateEmptySegmentationNode and mitk::Tool::CreateSegmentationNode. Why: Code is only used in one place and redundant to helper functions in mitkMultiLabelSegmentationHelper.h How to adapt: see changes in mitkToolInteractionTest.cpp
    + Removed own predicated code, used mitkMultiLabelSegmentationPredicateHelper.h
    instead

--- Modules/Segmentation/Interactions/mitkTool.cpp
+++ Modules/Segmentation/Interactions/mitkTool.cpp
@@ -21,6 +21,7 @@ found in the LICENSE file.
 #include "mitkVtkResliceInterpolationProperty.h"
 #include <mitkDICOMSegmentationPropertyHelper.cpp>
 #include <mitkToolManager.h>
+#include <mitkMultiLabelPredicateHelper.h>
 
 // us
 #include <usGetModuleContext.h>
@@ -37,23 +38,6 @@ namespace mitk
 mitk::Tool::Tool(const char *type, const us::Module *interactorModule)
   : m_EventConfig(""),
     m_ToolManager(nullptr),
-    m_PredicateImages(NodePredicateDataType::New("Image")), // for reference images
-    m_PredicateDim3(NodePredicateDimension::New(3, 1)),
-    m_PredicateDim4(NodePredicateDimension::New(4, 1)),
-    m_PredicateDimension(mitk::NodePredicateOr::New(m_PredicateDim3, m_PredicateDim4)),
-    m_PredicateImage3D(NodePredicateAnd::New(m_PredicateImages, m_PredicateDimension)),
-    m_PredicateBinary(NodePredicateProperty::New("binary", BoolProperty::New(true))),
-    m_PredicateNotBinary(NodePredicateNot::New(m_PredicateBinary)),
-    m_PredicateSegmentation(NodePredicateProperty::New("segmentation", BoolProperty::New(true))),
-    m_PredicateNotSegmentation(NodePredicateNot::New(m_PredicateSegmentation)),
-    m_PredicateHelper(NodePredicateProperty::New("helper object", BoolProperty::New(true))),
-    m_PredicateNotHelper(NodePredicateNot::New(m_PredicateHelper)),
-    m_PredicateImageColorful(NodePredicateAnd::New(m_PredicateNotBinary, m_PredicateNotSegmentation)),
-    m_PredicateImageColorfulNotHelper(NodePredicateAnd::New(m_PredicateImageColorful, m_PredicateNotHelper)),
-    m_PredicateReference(NodePredicateAnd::New(m_PredicateImage3D, m_PredicateImageColorfulNotHelper)),
-    m_IsSegmentationPredicate(
-      NodePredicateAnd::New(NodePredicateOr::New(m_PredicateBinary, m_PredicateSegmentation), m_PredicateNotHelper)),
-    m_InteractorType(type),
     m_DisplayInteractionConfigs(),
     m_InteractorModule(interactorModule)
 {
@@ -212,123 +196,12 @@ itk::Object::Pointer mitk::Tool::GetGUI(const std::string &toolkitPrefix, const 
 
 mitk::NodePredicateBase::ConstPointer mitk::Tool::GetReferenceDataPreference() const
 {
-  return m_PredicateReference.GetPointer();
+  return GetSegmentationReferenceImagePredicate();
 }
 
 mitk::NodePredicateBase::ConstPointer mitk::Tool::GetWorkingDataPreference() const
 {
-  return m_IsSegmentationPredicate.GetPointer();
-}
-
-mitk::DataNode::Pointer mitk::Tool::CreateEmptySegmentationNode(const Image *original,
-                                                                const std::string &organName,
-                                                                const mitk::Color &color) const
-{
-  // we NEED a reference image for size etc.
-  if (!original)
-    return nullptr;
-
-  // actually create a new empty segmentation
-  PixelType pixelType(mitk::MakeScalarPixelType<DefaultSegmentationDataType>());
-  MultiLabelSegmentation::Pointer segmentation = MultiLabelSegmentation::New();
-
-  if (original->GetDimension() == 2)
-  {
-    const unsigned int dimensions[] = {original->GetDimension(0), original->GetDimension(1), 1};
-    segmentation->Initialize(pixelType, 3, dimensions);
-    segmentation->AddLayer();
-    segmentation->SetActiveLayer(0);
-  }
-  else
-  {
-    segmentation->Initialize(original);
-  }
-
-  mitk::Label::Pointer label = mitk::Label::New();
-  label->SetName(organName);
-  label->SetColor(color);
-  label->SetValue(1);
-  segmentation->AddLabel(label,segmentation->GetActiveLayer());
-  segmentation->SetActiveLabel(label->GetValue());
-
-  unsigned int byteSize = sizeof(mitk::Label::PixelType);
-
-  if (segmentation->GetDimension() < 4)
-  {
-    for (unsigned int dim = 0; dim < segmentation->GetDimension(); ++dim)
-    {
-      byteSize *= segmentation->GetDimension(dim);
-    }
-
-    mitk::ImageWriteAccessor writeAccess(segmentation.GetPointer(), segmentation->GetVolumeData(0));
-
-    memset(writeAccess.GetData(), 0, byteSize);
-  }
-  else
-  {
-    // if we have a time-resolved image we need to set memory to 0 for each time step
-    for (unsigned int dim = 0; dim < 3; ++dim)
-    {
-      byteSize *= segmentation->GetDimension(dim);
-    }
-
-    for (unsigned int volumeNumber = 0; volumeNumber < segmentation->GetDimension(3); volumeNumber++)
-    {
-      mitk::ImageWriteAccessor writeAccess(segmentation.GetPointer(), segmentation->GetVolumeData(volumeNumber));
-
-      memset(writeAccess.GetData(), 0, byteSize);
-    }
-  }
-
-  if (original->GetTimeGeometry())
-  {
-    TimeGeometry::Pointer originalGeometry = original->GetTimeGeometry()->Clone();
-    segmentation->SetTimeGeometry(originalGeometry);
-  }
-  else
-  {
-    Tool::ErrorMessage("Original image does not have a 'Time sliced geometry'! Cannot create a segmentation.");
-    return nullptr;
-  }
-
-  return CreateSegmentationNode(segmentation, organName, color);
-}
-
-mitk::DataNode::Pointer mitk::Tool::CreateSegmentationNode(Image *image,
-                                                           const std::string &organName,
-                                                           const mitk::Color &color) const
-{
-  if (!image)
-    return nullptr;
-
-  // decorate the datatreenode with some properties
-  DataNode::Pointer segmentationNode = DataNode::New();
-  segmentationNode->SetData(image);
-
-  // name
-  segmentationNode->SetProperty("name", StringProperty::New(organName));
-
-  // visualization properties
-  segmentationNode->SetProperty("binary", BoolProperty::New(true));
-  segmentationNode->SetProperty("color", ColorProperty::New(color));
-  mitk::LookupTable::Pointer lut = mitk::LookupTable::New();
-  lut->SetType(mitk::LookupTable::MULTILABEL);
-  mitk::LookupTableProperty::Pointer lutProp = mitk::LookupTableProperty::New();
-  lutProp->SetLookupTable(lut);
-  segmentationNode->SetProperty("LookupTable", lutProp);
-  segmentationNode->SetProperty("texture interpolation", BoolProperty::New(false));
-  segmentationNode->SetProperty("layer", IntProperty::New(10));
-  segmentationNode->SetProperty("levelwindow", LevelWindowProperty::New(LevelWindow(0.5, 1)));
-  segmentationNode->SetProperty("opacity", FloatProperty::New(0.3));
-  segmentationNode->SetProperty("segmentation", BoolProperty::New(true));
-  segmentationNode->SetProperty("reslice interpolation",
-                                VtkResliceInterpolationProperty::New()); // otherwise -> segmentation appears in 2
-                                                                         // slices sometimes (only visual effect, not
-                                                                         // different data)
-  // For MITK-3M3 release, the volume of all segmentations should be shown
-  segmentationNode->SetProperty("showVolume", BoolProperty::New(true));
-
-  return segmentationNode;
+  return GetMultiLabelSegmentationPredicate();
 }
 
 us::ModuleResource mitk::Tool::GetIconResource() const

dd1c33e65e7acb9644b5cf2d9558f398e4dbdd41
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2025-03-10 11:01:11 +0100

    Revert cursor changes

--- Modules/Segmentation/Interactions/mitknnInteractiveTool.cpp
+++ Modules/Segmentation/Interactions/mitknnInteractiveTool.cpp
@@ -262,7 +262,6 @@ void mitk::nnInteractiveTool::ResetInteractions()
                             "   session.reset_interactions()\n";
     m_PythonContext->ExecuteString(pyCommand);
   }
-  this->PopAllCursors();
 }
 
 void mitk::nnInteractiveTool::BlockLMBDisplayInteraction()
@@ -316,7 +315,7 @@ mitk::DataNode::Pointer mitk::nnInteractiveTool::CreatePointSetNode(PromptType p
   node->SetColor(this->GetColor(promptType, Intensity::Vibrant), nullptr, "selectedcolor");
   node->SetProperty("Pointset.2D.shape", PointSetShapeProperty::New(PointSetShapeProperty::CIRCLE));
   node->SetIntProperty("Pointset.2D.resolution", 64);
-  //node->SetFloatProperty("point 2D size", 10.0f);
+  node->SetFloatProperty("point 2D size", 5.0f);
   node->SetFloatProperty("Pointset.2D.distance to plane", 0.1f);
   node->SetBoolProperty("Pointset.2D.keep shape when selected", true);
   node->SetBoolProperty("Pointset.2D.fill shape", true);

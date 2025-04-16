8a0eead08723fef4bbf82bc11163960cc65ab3c2
e468e <eliasstefan.imorde@dkfz-heidelberg.de>
2025-04-16 15:47:16 +0200

    added support for v2.8.0

--- Plugins/org.mitk.gui.qt.segmentation/src/QmitkTotalSegmentatorToolInstaller.cpp
+++ Plugins/org.mitk.gui.qt.segmentation/src/QmitkTotalSegmentatorToolInstaller.cpp
@@ -18,10 +18,9 @@ found in the LICENSE file.
 #include <QMutex>
 
 const QString QmitkTotalSegmentatorToolInstaller::VENV_NAME = ".totalsegmentator_v2";
-const QString QmitkTotalSegmentatorToolInstaller::TOTALSEGMENTATOR_VERSION = "2.4.0";
+const QString QmitkTotalSegmentatorToolInstaller::TOTALSEGMENTATOR_VERSION = "2.8.0";
 const std::vector<QString> QmitkTotalSegmentatorToolInstaller::PACKAGES = {
     "Totalsegmentator==" + TOTALSEGMENTATOR_VERSION,
-    "acvl_utils==0.2",
     "SimpleITK<=2.3.1",
     "p-tqdm<=1.4.0",
     "xvfbwrapper<=0.2.9",

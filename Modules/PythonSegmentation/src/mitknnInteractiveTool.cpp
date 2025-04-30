74ee6e3679432d58333e904b729cc80c16af0727
floca <r.floca@dkfz-heidelberg.de>
2025-04-30 17:38:54 +0200

    fixed compile error

--- Modules/PythonSegmentation/src/mitknnInteractiveTool.cpp
+++ Modules/PythonSegmentation/src/mitknnInteractiveTool.cpp
@@ -469,7 +469,7 @@ void mitk::nnInteractiveTool::StartSession()
   auto imageAtTimeStep = this->GetImageByTimeStep(image, timeStep);
   const auto spacing = imageAtTimeStep->GetGeometry()->GetSpacing();
 
-  const auto maskPixelType = MultiLabelSegmentation::GetPixelType>();
+  const auto maskPixelType = MultiLabelSegmentation::GetPixelType();
   m_Impl->TargetBuffer->Initialize(maskPixelType, *(imageAtTimeStep->GetTimeGeometry()));
 
   pythonContext->TransferBaseDataToPython(imageAtTimeStep, "mitk_image");

7066ec4332f9ba75affa11cefcce2b1de5d8993d
Ashis Ravindran <ashis.ravindran@dkfz-heidelberg.de>
2025-06-24 11:33:24 +0200

    Set nnunetv2 dependency to v2.6.0

--- Plugins/org.mitk.gui.qt.segmentation/src/QmitkTotalSegmentatorToolInstaller.cpp
+++ Plugins/org.mitk.gui.qt.segmentation/src/QmitkTotalSegmentatorToolInstaller.cpp
@@ -21,7 +21,7 @@ const QString QmitkTotalSegmentatorToolInstaller::VENV_NAME = ".totalsegmentator
 const QString QmitkTotalSegmentatorToolInstaller::TOTALSEGMENTATOR_VERSION = "2.9.0";
 const QStringList QmitkTotalSegmentatorToolInstaller::PACKAGES = {
     "Totalsegmentator==" + TOTALSEGMENTATOR_VERSION,
-    "nnunetv2<=2.6.0",
+    "nnunetv2==2.6.0",
     "SimpleITK<=2.4.1",
     "xvfbwrapper<=0.2.10",
     "dicom2nifti<=2.6.0",

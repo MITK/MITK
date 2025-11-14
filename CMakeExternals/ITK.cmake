f3f42d3b2282ae4188540a2dcaa8503806e6a056
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2025-11-14 10:56:42 +0100

    Update ITK from v5.4.3 to v5.4.4

--- CMakeExternals/ITK.cmake
+++ CMakeExternals/ITK.cmake
@@ -41,7 +41,7 @@ if(NOT DEFINED ITK_DIR)
      LIST_SEPARATOR ${sep}
      UPDATE_COMMAND ""
      GIT_REPOSITORY https://github.com/MITK/ITK.git
-     GIT_TAG 080810f0334a08592168a9dc1d67ec50510a818f # branch: v5.4.3-patched
+     GIT_TAG b60e158fb235badc6226d06ca883e4221d8640e8 # branch: v5.4.4-patched
      CMAKE_GENERATOR ${gen}
      CMAKE_GENERATOR_PLATFORM ${gen_platform}
      CMAKE_ARGS

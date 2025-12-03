a7130f0defb25071522d251eb2364f23a0748d21
floca <r.floca@dkfz-heidelberg.de>
2025-12-03 14:33:55 +0100

    Add undo/redo support to preset loading. All time steps including images and group names will be captured.
    + corrected variable naming QmitkLoadMultiLabelPresetAction::Run()

--- Modules/SegmentationUI/Qmitk/QmitkMultiLabelPresetHelper.cpp
+++ Modules/SegmentationUI/Qmitk/QmitkMultiLabelPresetHelper.cpp
@@ -14,6 +14,8 @@ found in the LICENSE file.
 
 #include <mitkMultiLabelIOHelper.h>
 
+#include <mitkSegChangeOperationApplier.h>
+
 #include <QFileDialog>
 #include <QMessageBox>
 
@@ -43,11 +45,27 @@ void QmitkLoadMultiLabelPreset(const std::vector<mitk::MultiLabelSegmentation::P
   if (filename.empty())
     return;
 
+  std::vector<std::unique_ptr<mitk::SegGroupModifyUndoRedoHelper>> undoHelpers;
+
   for (const auto& segmentation : segmentations)
   {
     if (segmentation.IsNull())
       continue;
 
+    //create a set with all group IDs as all groups should be captured.
+    std::vector<mitk::MultiLabelSegmentation::GroupIndexType> temp(segmentation->GetNumberOfGroups());
+    std::iota(temp.begin(), temp.end(), 0);
+    std::set<int> s(temp.begin(), temp.end());
+    mitk::SegGroupModifyUndoRedoHelper::GroupIndexSetType groupIDs(temp.begin(), temp.end());
+
+    undoHelpers.push_back(std::unique_ptr<mitk::SegGroupModifyUndoRedoHelper>(new mitk::SegGroupModifyUndoRedoHelper(segmentation, groupIDs,
+      true)));
+
     mitk::MultiLabelIOHelper::LoadMultiLabelSegementationPreset(filename, segmentation);
+  }
+
+  for (auto& undoHelper : undoHelpers)
+  {
+    undoHelper->RegisterUndoRedoOperationEvent("Apply label preset on segmentation");
   }
 }

6ff9d45b2c55f32e15bf5668b0305a84b136cca9
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2026-01-30 11:20:58 +0100

    Migrate PythonContext from Python C API to pybind11
    I also reduced and adapted the public API to our current needs.

--- Modules/Python/src/mitkPythonContext.cpp
+++ Modules/Python/src/mitkPythonContext.cpp
@@ -14,22 +14,12 @@ found in the LICENSE file.
 #include <mitkPythonHelper.h>
 #include <mitkIOUtil.h>
 
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-
-#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
 
 namespace py = pybind11;
 
 namespace
 {
-  struct PyObjectDeleter
-  {
-    void operator()(PyObject *obj) const { Py_XDECREF(obj); }
-  };
-
-  using PyObjectPtr = std::unique_ptr<PyObject, PyObjectDeleter>;
-
   void ActivateVirtualEnv(const std::string& venvName)
   {
     if (!mitk::PythonHelper::VirtualEnvExists(venvName))
@@ -51,34 +41,50 @@ namespace
 
 struct mitk::PythonContext::Impl
 {
-  PyObjectPtr GetVariable(const std::string& varName)
+  py::object GetVariable(const std::string& varName)
   {
-    auto obj = PyDict_GetItemString(LocalDictionary.get(), varName.c_str());
+    py::gil_scoped_acquire gil;
 
-    if (!obj)
-      obj = PyDict_GetItemString(GlobalDictionary.get(), varName.c_str());
+    if (this->LocalDictionary.contains(varName))
+      return this->LocalDictionary[py::str(varName)];
 
-    if (obj)
-      Py_INCREF(obj); // Promote from borrowed to owned reference.
+    if (this->GlobalDictionary.contains(varName))
+      return this->GlobalDictionary[py::str(varName)];
 
-    return PyObjectPtr(obj);
+    return py::none();
   }
 
-  PyObjectPtr GlobalDictionary;
-  PyObjectPtr LocalDictionary;
+  template <typename T>
+  std::optional<T> GetVariableAs(const std::string& varName)
+  {
+    auto var = this->GetVariable(varName);
+
+    if (var.is_none())
+      return std::nullopt;
+
+    try
+    {
+      return var.cast<T>();
+    }
+    catch (const py::cast_error&)
+    {
+      return std::nullopt;
+    }
+  }
+
+  py::dict GlobalDictionary;
+  py::dict LocalDictionary;
 };
 
 mitk::PythonContext::PythonContext(const std::string& venvName)
-  : m_Impl(std::make_unique<Impl>())
 {
   if (!venvName.empty())
     ActivateVirtualEnv(venvName);
 
   if (!Py_IsInitialized())
-    Py_Initialize();
+    py::initialize_interpreter();
 
-  m_Impl->GlobalDictionary.reset(PyDict_New());
-  m_Impl->LocalDictionary.reset(PyDict_New());
+  m_Impl = std::make_unique<Impl>();
 }
 
 void mitk::PythonContext::Activate()
@@ -110,231 +116,68 @@ void mitk::PythonContext::Activate()
     << "if venv:\n"
     << "    add_site_packages(venv)\n";
 
-  this->ExecuteString(pyCommands.str());
+  this->Execute(pyCommands.str());
 }
 
 mitk::PythonContext::~PythonContext()
 {
-  PyDict_Clear(m_Impl->LocalDictionary.get());
-  PyDict_Clear(m_Impl->GlobalDictionary.get());
+  m_Impl->LocalDictionary.clear();
+  m_Impl->GlobalDictionary.clear();
 }
 
-std::string mitk::PythonContext::ExecuteFile(const std::string &filePath)
+void mitk::PythonContext::Execute(const std::string &expression)
 {
-  PyGILState_STATE state = PyGILState_Ensure();
-  int commandType = Py_file_input;
-  FILE *file = fopen(filePath.c_str(), "rb");
-  if (nullptr == file)
-  {
-    mitkThrow() << "An error occured while reading python file.";
-  }
-  PyObjectPtr executionResult(
-    PyRun_File(file, "script.py", commandType, m_Impl->GlobalDictionary.get(), m_Impl->LocalDictionary.get()));
-  if (!executionResult)
-  {
-    // PyErr_Print(); prints stacktrace on console
-    std::string traceback = this->GetPythonExceptionTraceback();
-    mitkThrow() << "An error occured while running the Python code. " << traceback;
-  }
-  PyGILState_Release(state);
-  return std::string(this->GetStdOut());
-}
-
-std::string mitk::PythonContext::ExecuteString(const std::string &pyCommands)
-{
-  PyGILState_STATE state = PyGILState_Ensure();
-  int commandType = Py_file_input;
-  PyObjectPtr executionResult(
-    PyRun_String(pyCommands.c_str(), commandType, m_Impl->GlobalDictionary.get(), m_Impl->LocalDictionary.get()));
-  if (!executionResult)
-  {
-    //PyErr_Print();  prints stacktrace on console
-    std::string traceback = this->GetPythonExceptionTraceback();
-    mitkThrow() << "An error occured while running the Python code. " << traceback;
-  }
-  PyGILState_Release(state);
-  return this->GetStdOut();
-}
-
-std::string mitk::PythonContext::GetStdOut(const std::string &varName)
-{
-  PyGILState_STATE state = PyGILState_Ensure();
-  std::string _mitk_stdout;
-  PyObject *capture_output = PyDict_GetItemString(m_Impl->LocalDictionary.get(), varName.c_str());
-  if (capture_output)
-  {
-    PyObjectPtr output_val(PyObject_CallMethod(capture_output, "getvalue", nullptr));
-    _mitk_stdout = PyUnicode_AsUTF8(output_val.get());
-    MITK_INFO << varName << " : " << _mitk_stdout;
-  }
-  else
-  {
-    PyErr_Print();
-  }
-  PyGILState_Release(state);
-  return _mitk_stdout;
-}
-
-mitk::Image* mitk::PythonContext::LoadImageFromPython(const std::string &varName)
-{
-  PyGILState_STATE state = PyGILState_Ensure();
+  py::gil_scoped_acquire gil;
 
   try
   {
-    py::dict localDict = py::reinterpret_borrow<py::dict>(m_Impl->LocalDictionary.get());
-    py::dict globalDict = py::reinterpret_borrow<py::dict>(m_Impl->GlobalDictionary.get());
-
-    py::object pyImage;
-
-    if (localDict.contains(varName.c_str()))
-    {
-      pyImage = localDict[varName.c_str()];
-    }
-    else if (globalDict.contains(varName.c_str()))
-    {
-      pyImage = globalDict[varName.c_str()];
-    }
-    else
-    {
-      mitkThrow() << "Cloud not get image from Python: " << varName;
-    }
-
-    auto image = pyImage.cast<Image::Pointer>();
-
-    MITK_INFO << "C++ received image has dimension: " << image->GetDimension();
-
-    PyGILState_Release(state);
-    return image;
+    py::exec(expression, m_Impl->GlobalDictionary, m_Impl->LocalDictionary);
   }
-  catch (const py::cast_error& e)
+  catch (py::error_already_set& e)
   {
-    PyGILState_Release(state);
-    mitkThrow() << "Could not cast Python object to mitk::Image: " << e.what();
+    mitkThrow() << "An error occurred while executing Python code: " << e.what();
   }
 }
 
-void mitk::PythonContext::TransferBaseDataToPython(mitk::BaseData* baseData, const std::string& varName)
+void mitk::PythonContext::BindImage(Image* image, const std::string& varName)
 {
-  auto state = PyGILState_Ensure();
+  py::gil_scoped_acquire gil;
+
+  if (image == nullptr)
+  {
+    m_Impl->GlobalDictionary[py::str(varName)] = py::none();
+    return;
+  }
 
   try
   {
-    auto globals = py::reinterpret_borrow<py::dict>(m_Impl->GlobalDictionary.get());
-
-    if (auto image = dynamic_cast<Image*>(baseData))
-    {
-      globals[varName.c_str()] = py::cast(image, py::return_value_policy::reference);
-    }
-    else
-    {
-      MITK_ERROR << "Unsupported BaseData type";
-    }
+    m_Impl->GlobalDictionary[py::str(varName)] = py::cast(image, py::return_value_policy::reference);
   }
   catch (const py::error_already_set& e)
   {
-    MITK_ERROR << "Python error: " << e.what();
+    mitkThrow() << "Could not bind image to Python variable \"" << varName << "\": " << e.what();
   }
-
-  PyGILState_Release(state);
 }
 
 bool mitk::PythonContext::HasVariable(const std::string &varName)
 {
-  PyObject *pyVar = PyDict_GetItemString(m_Impl->LocalDictionary.get(), varName.c_str());
-  if (pyVar == nullptr && !(pyVar = PyDict_GetItemString(m_Impl->GlobalDictionary.get(), varName.c_str())))
-  {
-    return false;
-  }
-  return true;
+  py::gil_scoped_acquire gil;
+
+  return m_Impl->LocalDictionary.contains(varName) ||
+         m_Impl->GlobalDictionary.contains(varName);
 }
 
-template <>
-std::optional<bool> mitk::PythonContext::GetVariableAs<bool>(const std::string& varName)
+std::optional<bool> mitk::PythonContext::GetVariableAsBool(const std::string& varName)
 {
-  auto obj = m_Impl->GetVariable(varName);
-
-  if (!obj)
-    return std::nullopt;
-
-  auto isTrue = PyObject_IsTrue(obj.get());
-
-  if (isTrue == -1)
-    return std::nullopt;
-
-  return isTrue != 0;
+  return m_Impl->GetVariableAs<bool>(varName);
 }
 
-template <>
-std::optional<int> mitk::PythonContext::GetVariableAs<int>(const std::string& varName)
+std::optional<int> mitk::PythonContext::GetVariableAsInt(const std::string& varName)
 {
-  PyObjectPtr obj = m_Impl->GetVariable(varName);
-
-  if (!obj || !PyLong_Check(obj.get()))
-    return std::nullopt;
-
-  return static_cast<int>(PyLong_AsLong(obj.get()));
+  return m_Impl->GetVariableAs<int>(varName);
 }
 
-template <>
-std::optional<double> mitk::PythonContext::GetVariableAs<double>(const std::string& varName)
+std::optional<std::string> mitk::PythonContext::GetVariableAsString(const std::string& varName)
 {
-  PyObjectPtr obj = m_Impl->GetVariable(varName);
-
-  if (!obj || !PyFloat_Check(obj.get()))
-    return std::nullopt;
-
-  return PyFloat_AsDouble(obj.get());
+  return m_Impl->GetVariableAs<std::string>(varName);
 }
-
-template <>
-std::optional<std::string> mitk::PythonContext::GetVariableAs<std::string>(const std::string& varName)
-{
-  PyObjectPtr obj = m_Impl->GetVariable(varName);
-
-  if (!obj || !PyUnicode_Check(obj.get()))
-    return std::nullopt;
-
-  auto str = PyUnicode_AsUTF8(obj.get());
-
-  if (str == nullptr)
-    return std::nullopt;
-
-  return str;
-}
-
-std::string mitk::PythonContext::GetPythonExceptionTraceback()
-{
-  std::string errorMessage;
-#if PY_MINOR_VERSION >= 12
-  PyObjectPtr pException(PyErr_GetRaisedException());
-  PyObjectPtr pTracebackMod(PyImport_ImportModule("traceback"));
-  PyObjectPtr pFormatExceptionMethod(PyObject_GetAttrString(pTracebackMod.get(), "format_exception"));
-  if (nullptr != pException)
-  {
-    PyObjectPtr pExceptionType(PyObject_Type(pException.get()));
-    PyObjectPtr pExceptionTypeTb(PyException_GetTraceback(pException.get()));
-    PyObjectPtr pExceptionList(PyObject_CallFunctionObjArgs(
-      pFormatExceptionMethod.get(), pExceptionType.get(), pException.get(), pExceptionTypeTb.get(), nullptr));
-#else
-  PyObject* ptype, * pvalue, * ptraceback;
-  PyErr_Fetch(&ptype, &pvalue, &ptraceback); // Deprecated since python 3.12
-  PyObjectPtr pTracebackMod(PyImport_ImportModule("traceback"));
-  PyObjectPtr pFormatExceptionMethod(PyObject_GetAttrString(pTracebackMod.get(), "format_exception"));
-  if (nullptr != ptype)
-  {
-    PyObjectPtr pExceptionList(
-      PyObject_CallFunctionObjArgs(pFormatExceptionMethod.get(), ptype, pvalue, ptraceback, nullptr));
-#endif
-    if (pExceptionList && PyList_Check(pExceptionList.get()))
-    {
-      PyObjectPtr pExceptionText(PyUnicode_Join(PyUnicode_FromString(""), pExceptionList.get()));
-      errorMessage = pExceptionText ? PyUnicode_AsUTF8(pExceptionText.get()) : "UnknownError Caught.";
-    }
-    else
-    {
-      errorMessage = "Failed to format exception";
-    }
-  }
-  return errorMessage;
-}
-
--- Modules/PythonSegmentation/src/mitknnInteractiveTool.cpp
+++ Modules/PythonSegmentation/src/mitknnInteractiveTool.cpp
@@ -127,12 +127,18 @@ namespace mitk
 
     PythonContext* GetPythonContext() const
     {
-      return m_PythonContext;
+      return m_PythonContext.get();
     }
 
-    void SetPythonContext(PythonContext* pythonContext)
+    void CreatePythonContext(const std::string& venvName)
     {
-      m_PythonContext = pythonContext;
+      m_PythonContext = std::make_unique<PythonContext>(venvName);
+      m_PythonContext->Activate();
+    }
+
+    void DestroyPythonContext()
+    {
+      m_PythonContext = nullptr;
     }
 
     // Methods that execute Python code are defined at the bottom of this file.
@@ -153,7 +159,7 @@ namespace mitk
 
   private:
     std::optional<Backend> m_Backend;
-    PythonContext::Pointer m_PythonContext;
+    std::unique_ptr<PythonContext> m_PythonContext;
     ToolManager::Pointer m_ToolManager;
   };
 }
@@ -435,10 +441,7 @@ bool mitk::nnInteractiveTool::CreatePythonContext()
 {
   try
   {
-    auto pythonContext = PythonContext::New(this->GetVirtualEnvName());
-    pythonContext->Activate();
-
-    m_Impl->SetPythonContext(pythonContext);
+    m_Impl->CreatePythonContext(this->GetVirtualEnvName());
   }
   catch (const Exception& e)
   {
@@ -465,9 +468,9 @@ bool mitk::nnInteractiveTool::IsInstalled() const
     << "is_installed = importlib.util.find_spec('nnInteractive') is not None\n";
 
   auto pythonContext = m_Impl->GetPythonContext();
-  pythonContext->ExecuteString(pyCommands.str());
+  pythonContext->Execute(pyCommands.str());
 
-  return pythonContext->GetVariableAs<bool>("is_installed").value_or(false);
+  return pythonContext->GetVariableAsBool("is_installed").value_or(false);
 }
 
 bool mitk::nnInteractiveTool::GetCUDADeviceInfo(CUDADeviceInfo& info) const
@@ -496,15 +499,15 @@ bool mitk::nnInteractiveTool::GetCUDADeviceInfo(CUDADeviceInfo& info) const
   try
   {
     auto pythonContext = m_Impl->GetPythonContext();
-    pythonContext->ExecuteString(pyCommands.str());
+    pythonContext->Execute(pyCommands.str());
 
-    if (!pythonContext->GetVariableAs<bool>("is_cuda_available").value_or(false))
+    if (!pythonContext->GetVariableAsBool("is_cuda_available").value_or(false))
       return false;
 
-    info.Name = pythonContext->GetVariableAs<std::string>("name").value_or("");
-    info.TotalMemoryMB = pythonContext->GetVariableAs<int>("total_memory_mb").value_or(0);
-    info.Major = pythonContext->GetVariableAs<int>("major").value_or(0);
-    info.Minor = pythonContext->GetVariableAs<int>("minor").value_or(0);
+    info.Name = pythonContext->GetVariableAsString("name").value_or("");
+    info.TotalMemoryMB = pythonContext->GetVariableAsInt("total_memory_mb").value_or(0);
+    info.Major = pythonContext->GetVariableAsInt("major").value_or(0);
+    info.Minor = pythonContext->GetVariableAsInt("minor").value_or(0);
 
     return true;
   }
@@ -588,7 +591,7 @@ void mitk::nnInteractiveTool::StartSession()
       << "    force_download = False\n"
       << ")\n"
       << "checkpoint_path = Path(download_path).joinpath('" << modelCheckpoint << "')\n";
-    pythonContext->ExecuteString(pyCommands.str());
+    pythonContext->Execute(pyCommands.str());
   }
 
   {
@@ -605,7 +608,7 @@ void mitk::nnInteractiveTool::StartSession()
       << "    inference_class,\n"
       << "    'nnInteractive.inference'\n"
       << ")\n";
-    pythonContext->ExecuteString(pyCommands.str());
+    pythonContext->Execute(pyCommands.str());
   }
 
   m_Impl->ResetBackend();
@@ -623,7 +626,7 @@ void mitk::nnInteractiveTool::StartSession()
       << "    do_autozoom=" << m_Impl->AutoZoom << '\n'
       << ")\n"
       << "session.initialize_from_trained_model_folder(checkpoint_path)\n";
-    pythonContext->ExecuteString(pyCommands.str());
+    pythonContext->Execute(pyCommands.str());
   }
 
   m_Impl->SetBackend(useCUDADevice
@@ -642,8 +645,8 @@ void mitk::nnInteractiveTool::StartSession()
   m_Impl->TargetBuffer->Initialize(maskPixelType, *(imageAtTimeStep->GetTimeGeometry()));
   ::InitializeVolume(m_Impl->TargetBuffer);
 
-  pythonContext->TransferBaseDataToPython(imageAtTimeStep, "mitk_image");
-  pythonContext->TransferBaseDataToPython(m_Impl->TargetBuffer.GetPointer(), "mitk_target_buffer");
+  pythonContext->BindImage(imageAtTimeStep, "mitk_image");
+  pythonContext->BindImage(m_Impl->TargetBuffer.GetPointer(), "mitk_target_buffer");
 
   {
     std::ostringstream pyCommands; pyCommands
@@ -656,7 +659,7 @@ void mitk::nnInteractiveTool::StartSession()
       << "torch_target_buffer = torch.from_numpy(target_buffer)\n"
       << "session.set_image(image[None], {'spacing': spacing})\n"
       << "session.set_target_buffer(torch_target_buffer)\n";
-    pythonContext->ExecuteString(pyCommands.str());
+    pythonContext->Execute(pyCommands.str());
   }
 }
 
@@ -676,8 +679,8 @@ void mitk::nnInteractiveTool::EndSession()
   if (m_Impl->GetBackend() == Backend::CUDA)
     pyCommands << "torch.cuda.empty_cache()\n";
 
-  m_Impl->GetPythonContext()->ExecuteString(pyCommands.str());
-  m_Impl->SetPythonContext(nullptr);
+  m_Impl->GetPythonContext()->Execute(pyCommands.str());
+  m_Impl->DestroyPythonContext();
 }
 
 bool mitk::nnInteractiveTool::IsSessionRunning() const
@@ -695,7 +698,7 @@ void mitk::nnInteractiveTool::Impl::SetAutoZoom() const
   std::ostringstream pyCommands; pyCommands
     << "session.set_do_autozoom(" << (this->AutoZoom ? "True" : "False") << ")\n";
 
-  m_PythonContext->ExecuteString(pyCommands.str());
+  m_PythonContext->Execute(pyCommands.str());
 }
 
 void mitk::nnInteractiveTool::Impl::AddPointInteraction(const Point3D& point, const Image* inputAtTimeStep) const
@@ -709,7 +712,7 @@ void mitk::nnInteractiveTool::Impl::AddPointInteraction(const Point3D& point, co
     << "    include_interaction=" << (this->PromptType == PromptType::Positive ? "True" : "False") << '\n'
     << ")\n";
 
-  m_PythonContext->ExecuteString(pyCommands.str());
+  m_PythonContext->Execute(pyCommands.str());
 }
 
 void mitk::nnInteractiveTool::Impl::AddBoxInteraction(const PlanarFigure* box, const Image* inputAtTimeStep) const
@@ -738,12 +741,12 @@ void mitk::nnInteractiveTool::Impl::AddBoxInteraction(const PlanarFigure* box, c
     << "    include_interaction=" << (this->PromptType == PromptType::Positive ? "True" : "False") << '\n'
     << ")\n";
 
-  m_PythonContext->ExecuteString(pyCommands.str());
+  m_PythonContext->Execute(pyCommands.str());
 }
 
 void mitk::nnInteractiveTool::Impl::AddScribbleInteraction(const Image* mask) const
 {
-  m_PythonContext->TransferBaseDataToPython(const_cast<Image*>(mask), "mitk_scribble_mask");
+  m_PythonContext->BindImage(const_cast<Image*>(mask), "mitk_scribble_mask");
 
   std::ostringstream pyCommands; pyCommands
     << "scribble_mask = mitk_scribble_mask.as_numpy()\n"
@@ -753,12 +756,12 @@ void mitk::nnInteractiveTool::Impl::AddScribbleInteraction(const Image* mask) co
     << ")\n"
     << "del scribble_mask\n";
 
-  m_PythonContext->ExecuteString(pyCommands.str());
+  m_PythonContext->Execute(pyCommands.str());
 }
 
 void mitk::nnInteractiveTool::Impl::AddLassoInteraction(const Image* mask) const
 {
-  m_PythonContext->TransferBaseDataToPython(const_cast<Image*>(mask), "mitk_lasso_mask");
+  m_PythonContext->BindImage(const_cast<Image*>(mask), "mitk_lasso_mask");
 
   std::ostringstream pyCommands; pyCommands
     << "lasso_mask = mitk_lasso_mask.as_numpy()\n"
@@ -768,12 +771,12 @@ void mitk::nnInteractiveTool::Impl::AddLassoInteraction(const Image* mask) const
     << ")\n"
     << "del lasso_mask\n";
 
-  m_PythonContext->ExecuteString(pyCommands.str());
+  m_PythonContext->Execute(pyCommands.str());
 }
 
 void mitk::nnInteractiveTool::Impl::AddInitialSegInteraction(MultiLabelSegmentation* previewImage, TimeStepType timeStep) const
 {
-  m_PythonContext->TransferBaseDataToPython(this->InitialSeg, "mitk_initial_seg");
+  m_PythonContext->BindImage(this->InitialSeg, "mitk_initial_seg");
 
   std::ostringstream pyCommands; pyCommands
     << "initial_seg = mitk_initial_seg.as_numpy()\n"
@@ -783,12 +786,12 @@ void mitk::nnInteractiveTool::Impl::AddInitialSegInteraction(MultiLabelSegmentat
     << ")\n"
     << "del initial_seg\n";
 
-  m_PythonContext->ExecuteString(pyCommands.str());
+  m_PythonContext->Execute(pyCommands.str());
 
   previewImage->UpdateGroupImage(previewImage->GetActiveLayer(), this->TargetBuffer, timeStep, 0, ImageAccessorBase::IgnoreLock);
 }
 
 void mitk::nnInteractiveTool::Impl::ResetInteractions() const
 {
-  m_PythonContext->ExecuteString("session.reset_interactions()\n");
+  m_PythonContext->Execute("session.reset_interactions()\n");
 }

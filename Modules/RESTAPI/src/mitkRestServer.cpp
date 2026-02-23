4f990540ca029864ea9cd0296f6cc2290dc8f974
floca <r.floca@dkfz-heidelberg.de>
2026-02-23 10:31:27 +0100

    Removed dublicated code

--- Modules/RESTAPI/src/mitkRestServer.cpp
+++ Modules/RESTAPI/src/mitkRestServer.cpp
@@ -831,27 +831,6 @@ void RestServer::RegisterRoutes()
     [this](const httplib::Request& req, httplib::Response& res) {
       m_SwaggerController->HandleGET_openapi(req, res);
     });
-
-  // Documentation endpoints (Swagger UI and OpenAPI spec)
-  m_Server->Get(apiBase + "/docs",
-    [this](const httplib::Request& req, httplib::Response& res) {
-      m_SwaggerController->HandleGET_docs(req, res);
-    });
-
-  m_Server->Get(apiBase + "/docs/swagger-ui.css",
-    [this](const httplib::Request& req, httplib::Response& res) {
-      m_SwaggerController->HandleGET_docs_css(req, res);
-    });
-
-  m_Server->Get(apiBase + "/docs/swagger-ui-bundle.js",
-    [this](const httplib::Request& req, httplib::Response& res) {
-      m_SwaggerController->HandleGET_docs_js(req, res);
-    });
-
-  m_Server->Get(apiBase + "/openapi.json",
-    [this](const httplib::Request& req, httplib::Response& res) {
-      m_SwaggerController->HandleGET_openapi(req, res);
-    });
 }
 
 void RestServer::ServerThreadFunc()

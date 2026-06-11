590c418a830e1b924ab9d8d68e45201187788ca5
floca <r.floca@dkfz-heidelberg.de>
2026-06-11 12:55:16 +0200

    Reverted pinned MITK Data commit for #323

--- CMakeExternals/MITKData.cmake
+++ CMakeExternals/MITKData.cmake
@@ -19,20 +19,13 @@ if(BUILD_TESTING)
 
   else()
 
-  # Pin mitk-data to the parent of its commit "Update file to reflect changes
-  # introduced by #800". That commit tailors the liver-spine labelmap reference
-  # data to #800, but #800 depends on this branch (#323) landing first. Until
-  # then #323 must build against the pre-#800 reference data; otherwise the
-  # DICOM Seg labelmap regression test fails.
-  # TODO: Revert this pin once #800 has landed, i.e. remove GIT_TAG again so
-  #       mitk-data is tracked at HEAD (or advance it to the #800 commit).
-  set(revision_tag a1e677b4) # first 8 characters of hash-tag
+  # set(revision_tag a1e677b4) # first 8 characters of hash-tag
   #                  ^^^^^^^^  these are just to check correct length of hash part
 
     ExternalProject_Add(${proj}
       SOURCE_DIR ${proj}
       GIT_REPOSITORY https://codebase.helmholtz.cloud/mitk/mitk-data.git
-      GIT_TAG ${revision_tag}
+  #    GIT_TAG ${revision_tag}
   #   URL ${MITK_THIRDPARTY_DOWNLOAD_PREFIX_URL}/mitk-data_${revision_tag}.tar.gz
   #   UPDATE_COMMAND ""
       CONFIGURE_COMMAND ""
